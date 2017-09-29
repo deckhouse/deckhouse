f9f490c4352fa484bebc5903fe03fd913f5070a5
Timofey Kirillov <timofey.kirillov@flant.com>
2017-09-29 17:38:22 +0300

    Общая структура ++

--- scripts_manager.go
+++ scripts_manager.go
@@ -6,24 +6,31 @@ import (
 
 var (
 	ScriptsCommitted chan string
+	lastRepo         map[string]string
 )
 
 func FetchScripts(Repo map[string]string) {
-	// todo: git clone или fetch + смотрим изменение коммита, шлем сигнал в ScriptsUpdated
+	// todo: git clone или fetch + смотрим изменение коммита, шлем сигнал в ScriptsCommitted
+	// посылаем новый коммит только если он поменялся с последнего раза
+	ScriptsCommitted <- "997c972a602521e68f10f018344c9aed6734792b"
 }
 
 func InitScriptsManager() {
+	lastRepo = make(map[string]string)
 }
 
-// Запускается в отдельной goroutine
 func RunScriptsManager() {
+	timer := time.NewTimer(time.Duration(10) * time.Second)
+
 	for {
 		select {
 		case repo := <-RepoUpdated:
 			FetchScripts(repo)
+			lastRepo = repo
+		case <-timer.C:
+			if len(lastRepo) != 0 {
+				FetchScripts(lastRepo)
+			}
 		}
-		// Ловим RepoUpdated -> запускаем FetchScripts
-		// Периодически запускаем FetchScripts
-		time.Sleep(time.Duration(1) * time.Second)
 	}
 }
