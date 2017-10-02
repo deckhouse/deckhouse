8b9ddec515aac832fd4b7f8681231935b28f7c7a
Timofey Kirillov <timofey.kirillov@flant.com>
2017-10-02 20:04:58 +0300

    Тупая запускалка скриптов

--- main.go
+++ main.go
@@ -1,26 +1,101 @@
 package main
 
 import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+
 	"github.com/romana/rlog"
 )
 
 var (
-	lastModulesInitialized bool
-	lastModules            []map[string]string
-
-	lastScriptsCommitInitialized bool
-	lastScriptsCommit            string
+	lastScriptsDir    string
+	lastModules       []map[string]string
+	retryModulesQueue []map[string]string
 )
 
 func Init() {
 	rlog.Info("Init")
 
-	lastModulesInitialized = false
-	lastScriptsCommitInitialized = false
+	lastScriptsDir = ""
+	lastModules = make([]map[string]string, 0)
+	retryModulesQueue = make([]map[string]string, 0)
 
 	InitKube()
 	InitConfigManager()
 	InitScriptsManager()
+}
+
+func prepareScripts(commit string) (string, error) {
+	var err error
+	var cmd *exec.Cmd
+
+	if len(lastKnownRepo) == 0 {
+		return "", errors.New("Repo is not initialized yet")
+	}
+
+	dir, err := ioutil.TempDir("", "antiopa-scripts-tree-")
+	if err != nil {
+		return "", err
+	}
+
+	cmd = exec.Command("git", "clone", lastKnownRepo["url"], dir)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	cmd = exec.Command("git", "--git-dir", path.Join(dir, ".git"), "checkout", commit)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return dir, nil
+}
+
+func RunModule(scriptsDir string, module map[string]string) error {
+	entrypoint := module["entrypoint"]
+	if entrypoint == "" {
+		entrypoint = "./ctl.sh"
+	} else {
+		entrypoint = "./" + entrypoint
+	}
+
+	cmd := exec.Command(entrypoint, strings.Fields(module["args"])...)
+	cmd.Dir = filepath.Join(scriptsDir, "modules", module["name"])
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	rlog.Infof("Running module %s ...", module["name"])
+	err := cmd.Run()
+	if err == nil {
+		rlog.Infof("Module %s OK", module["name"])
+	} else {
+		retryModulesQueue = append(retryModulesQueue, module)
+
+		rlog.Errorf("Module %s FAILED: %s", module["name"], err)
+	}
+
+	return err
+}
+
+func RunModules(scriptsDir string, modules []map[string]string) {
+	if scriptsDir == "" {
+		return
+	}
+	for _, module := range modules {
+		RunModule(scriptsDir, module)
+	}
 }
 
 func Run() {
@@ -29,24 +104,39 @@ func Run() {
 	go RunConfigManager()
 	go RunScriptsManager()
 
+	retryModuleTicker := time.NewTicker(time.Duration(10) * time.Second)
+
 	for {
 		select {
 		case modules := <-ModulesUpdated:
-			if lastScriptsCommitInitialized {
-				// TODO: Заметить разницу между modules и запустить только новые скрипты
-				RunScripts(modules, lastScriptsCommit)
-			}
-
 			lastModules = modules
-			lastModulesInitialized = true
+
+			RunModules(lastScriptsDir, lastModules)
+
 		case commit := <-ScriptsCommitted:
-			if lastModulesInitialized {
-				// TODO: Заметить разницу между modules и запустить только новые скрипты
-				RunScripts(lastModules, commit)
+			// TODO: получать repo-dir+commit из канала
+
+			scriptsDir, err := prepareScripts(commit)
+			if err != nil {
+				rlog.Errorf("Unable to prepare scripts from repo %v at commit %s", lastKnownRepo, commit)
 			}
 
-			lastScriptsCommit = commit
-			lastScriptsCommitInitialized = true
+			if lastScriptsDir != "" {
+				os.RemoveAll(lastScriptsDir)
+			}
+			lastScriptsDir = scriptsDir
+
+			RunModules(lastScriptsDir, lastModules)
+
+		case <-retryModuleTicker.C:
+			if len(retryModulesQueue) > 0 && lastScriptsDir != "" {
+				retryModule := retryModulesQueue[0]
+				retryModulesQueue = retryModulesQueue[1:]
+
+				rlog.Infof("Retrying module %s", retryModule["name"])
+
+				RunModule(lastScriptsDir, retryModule)
+			}
 		}
 	}
 }
