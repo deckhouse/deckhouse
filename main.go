b195b741d601595a81047e7c5b817d351dd4f75a
Timofey Kirillov <timofey.kirillov@flant.com>
2017-10-03 20:59:05 +0300

    Поддержка first_run_args для модулей

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"os"
@@ -17,6 +18,8 @@ var (
 	lastScriptsDir    string
 	lastModules       []map[string]string
 	retryModulesQueue []map[string]string
+
+	modulesStatusDir string
 )
 
 func Init() {
@@ -25,6 +28,13 @@ func Init() {
 	lastScriptsDir = ""
 	lastModules = make([]map[string]string, 0)
 	retryModulesQueue = make([]map[string]string, 0)
+
+	wd, err := os.Getwd()
+	if err != nil {
+		rlog.Error("Terminating: %s", err)
+		os.Exit(1)
+	}
+	modulesStatusDir = path.Join(wd, "antiopa-status")
 
 	InitKube()
 	InitConfigManager()
@@ -63,6 +73,48 @@ func prepareScripts(commit string) (string, error) {
 	return dir, nil
 }
 
+func getModuleStatus(moduleName string) (res map[string]string) {
+	p := path.Join(modulesStatusDir, moduleName)
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		return make(map[string]string)
+	}
+
+	dat, err := ioutil.ReadFile(p)
+	if err != nil {
+		return make(map[string]string)
+	}
+	if len(dat) > 0 {
+		dat = dat[:len(dat)-1]
+	}
+
+	if err := json.Unmarshal(dat, &res); err != nil {
+		return make(map[string]string)
+	}
+
+	return
+}
+
+func setModuleStatus(moduleName string, moduleStatus map[string]string) error {
+	dat, err := json.Marshal(moduleStatus)
+	if err != nil {
+		return err
+	}
+
+	dat = append(dat, []byte("\n")...)
+
+	if _, err := os.Stat(modulesStatusDir); os.IsNotExist(err) {
+		if err = os.MkdirAll(modulesStatusDir, 0777); err != nil {
+			return err
+		}
+	}
+
+	if err = ioutil.WriteFile(path.Join(modulesStatusDir, moduleName), dat, 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func RunModule(scriptsDir string, module map[string]string) error {
 	entrypoint := module["entrypoint"]
 	if entrypoint == "" {
@@ -71,14 +123,29 @@ func RunModule(scriptsDir string, module map[string]string) error {
 		entrypoint = "./" + entrypoint
 	}
 
-	cmd := exec.Command(entrypoint, strings.Fields(module["args"])...)
+	is_first_run := (getModuleStatus(module["name"])["installed"] != "true")
+
+	var args string
+	if is_first_run {
+		args = module["first_run_args"]
+	} else {
+		args = module["args"]
+	}
+
+	cmd := exec.Command(entrypoint, strings.Fields(args)...)
 	cmd.Dir = filepath.Join(scriptsDir, "modules", module["name"])
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	rlog.Infof("Running module %s ...", module["name"])
+	rlog.Debugf("Module %s command: `%s %s", module["name"], entrypoint, args)
+
 	err := cmd.Run()
 	if err == nil {
+		if is_first_run {
+			setModuleStatus(module["name"], map[string]string{"installed": "true"})
+		}
+
 		rlog.Infof("Module %s OK", module["name"])
 	} else {
 		retryModulesQueue = append(retryModulesQueue, module)
