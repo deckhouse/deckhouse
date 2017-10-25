67e5b12c9ec3aebbbdb16dd1c41c380022a6d014
Timofey Kirillov <timofey.kirillov@flant.com>
2017-10-25 18:34:59 +0300

    Фикс MergeValues: функция не изменяет входные данные

--- merge_values_test.go
+++ merge_values_test.go
@@ -6,22 +6,28 @@ import (
 )
 
 func TestMergeValues(t *testing.T) {
-	a := map[interface{}]interface{}{
-		"one": map[interface{}]interface{}{
-			"from-a":    []interface{}{"one", "two", "three"},
-			"from-both": []interface{}{"one", "three", "five", "seven", "nine", "eleven"},
-		},
-		"two":   2,
-		"three": "3",
+	data := make(map[string]map[interface{}]interface{})
+	for _, key := range []string{"a", "original-a"} {
+		data[key] = map[interface{}]interface{}{
+			"one": map[interface{}]interface{}{
+				"from-a":    []interface{}{"one", "two", "three"},
+				"from-both": []interface{}{"one", "three", "five", "seven", "nine", "eleven"},
+			},
+			"two":   2,
+			"three": "3",
+			"array": []string{"a", "b", "c"},
+		}
 	}
-
-	b := map[interface{}]interface{}{
-		"one": map[interface{}]interface{}{
-			"from-b":    []interface{}{"one", "two", "three"},
-			"from-both": []interface{}{"two", "four", "six", "eight", "ten", "twelve"},
-		},
-		"three": 3,
-		"four":  4,
+	for _, key := range []string{"b", "original-b"} {
+		data[key] = map[interface{}]interface{}{
+			"one": map[interface{}]interface{}{
+				"from-b":    []interface{}{"one", "two", "three"},
+				"from-both": []interface{}{"two", "four", "six", "eight", "ten", "twelve"},
+			},
+			"three": 3,
+			"four":  4,
+			"array": []int{1, 2, 3},
+		}
 	}
 
 	expectedRes := map[interface{}]interface{}{
@@ -33,11 +39,20 @@ func TestMergeValues(t *testing.T) {
 		"two":   2,
 		"three": 3,
 		"four":  4,
+		"array": []int{1, 2, 3},
 	}
 
-	res := MergeValues(a, b)
+	res := MergeValues(data["a"], data["b"])
 
 	if !reflect.DeepEqual(res, expectedRes) {
-		t.Errorf("MERGE FAILED\nMAP-1:\n%v\nMAP-2:\n%v\nEXPECTED:\n%v\nGOT:\n%v", a, b, expectedRes, res)
+		t.Errorf("MergeValues FAILED\nmap-1:\n%v\nmap-2:\n%v\nEXPECTED:\n%v\nGOT:\n%v", data["a"], data["b"], expectedRes, res)
+	}
+
+	// Проверяем, что функция MergeValues не меняет входные данные-параметры
+	if !reflect.DeepEqual(data["a"], data["original-a"]) {
+		t.Errorf("MergeValues FAILED: function changed input params\noriginal map a:\n%v\nmap a after call:\n%v", data["original-a"], data["a"])
+	}
+	if !reflect.DeepEqual(data["b"], data["original-b"]) {
+		t.Errorf("MergeValues FAILED: function changed input params\noriginal map b:\n%v\nmap b after call:\n%v", data["original-b"], data["b"])
 	}
 }
