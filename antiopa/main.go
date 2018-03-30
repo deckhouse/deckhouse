e71a7f4798e6e7b9b8403e319c0504a50f03dd8d
Alexey igrychev <alexey.igrychev@flant.com>
2018-03-30 14:27:45 +0300

    Revert: Reset global/module changed flag before running related action

--- antiopa/main.go
+++ antiopa/main.go
@@ -234,17 +234,18 @@ func OnKubeNodeChanged() {
 
 	if valuesChanged {
 		rlog.Debug("Global values changed: run all modules")
-		valuesChanged = false
 		RunModules()
 	} else {
 		for _, moduleName := range modulesOrder {
 			if changed, exist := modulesValuesChanged[moduleName]; exist && changed {
 				rlog.Debugf("Module `%s` values changed: run module", moduleName)
-				modulesValuesChanged[moduleName] = false
 				RunModule(moduleName)
 			}
 		}
 	}
+
+	valuesChanged = false
+	modulesValuesChanged = make(map[string]bool)
 }
 
 // Вызов хуков при изменении опций узлов и самих узлов.
