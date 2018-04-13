e28277c75883fc8b57032b435c5124fdcfc675be
Timofey Kirillov <timofey.kirillov@flant.com>
2018-04-13 17:11:48 +0300

    [core,antiopa-2] Fix DeepMerge error on map[string]interface{}

--- antiopa/utils/deep_merge_test.go
+++ antiopa/utils/deep_merge_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -76,5 +77,99 @@ func TestMergeValuesWithNullValue(t *testing.T) {
 		},
 	}) {
 		t.Errorf("MergeValues FAILED: got unexpected merge result: %v", res)
+	}
+}
+
+func castMapShouldEqual(input interface{}, expectedResult map[interface{}]interface{}) error {
+	res := CastArbitraryMap(input)
+
+	if !reflect.DeepEqual(res, expectedResult) {
+		return fmt.Errorf("expected %+v map after cast, got %+v", expectedResult, res)
+	}
+
+	return nil
+}
+
+func castSliceShouldEqual(input interface{}, expectedResult []interface{}) error {
+	res := CastArbitrarySlice(input)
+
+	if !reflect.DeepEqual(res, expectedResult) {
+		return fmt.Errorf("expected %+v slice after cast, got %+v", expectedResult, res)
+	}
+
+	return nil
+}
+
+func TestCast(t *testing.T) {
+	var err error
+
+	err = castMapShouldEqual(
+		map[string]interface{}{"hello": 3, "world": "hello", "goodbye": []byte("aloha")},
+		map[interface{}]interface{}{"hello": 3, "world": "hello", "goodbye": []byte("aloha")})
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = castSliceShouldEqual(
+		[]string{"one", "two", "three"},
+		[]interface{}{"one", "two", "three"},
+	)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDeepMergeWithArbitraryMap(t *testing.T) {
+	m1 := map[interface{}]interface{}{
+		"key1": map[string]int{
+			"one":   1,
+			"two":   2,
+			"three": 3,
+		},
+		"key2": map[string][]byte{
+			"four": []byte{0x4},
+			"five": []byte{0x5},
+			"six":  []byte{0x6},
+		},
+		"key3": map[string]interface{}{
+			"myarr":  []string{"alpha", "bravo", "charlie"},
+			"myarr2": []string{"some", "strings"},
+		},
+	}
+
+	m2 := map[interface{}]interface{}{
+		"key1": map[string]int{
+			"one":   10,
+			"three": 30,
+		},
+		"key2": map[string][]byte{
+			"four": []byte{0x4},
+		},
+		"key3": map[string]interface{}{
+			"myarr":  []string{"delta"},
+			"myarr2": []int{123, 456},
+		},
+	}
+
+	res := DeepMerge(m1, m2)
+	expectedResult := map[interface{}]interface{}{
+		"key1": map[interface{}]interface{}{
+			"one":   10,
+			"two":   2,
+			"three": 30,
+		},
+		"key2": map[interface{}]interface{}{
+			"four": []interface{}{uint8(0x4), uint8(0x4)},
+			"five": []uint8{uint8(0x5)}, // will not be casted to []interface{}!
+			"six":  []uint8{uint8(0x6)}, // will not be casted to []interface{}!
+		},
+		"key3": map[interface{}]interface{}{
+			"myarr":  []interface{}{"alpha", "bravo", "charlie", "delta"},
+			"myarr2": []int{123, 456}, // will not be casted to []interface{}!
+		},
+	}
+
+	if !reflect.DeepEqual(res, expectedResult) {
+		t.Errorf("Expected %+v, got %+v", expectedResult, res)
 	}
 }
