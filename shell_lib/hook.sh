a6f5ad1cbdade348a440c7edaf35cd92da847395
Dmitry Stolyarov <dmitry.stolyarov@flant.com>
2018-04-24 01:06:03 +0300

    [modules] Убрана поддержка __main_old__ для хуков

--- shell_lib/hook.sh
+++ shell_lib/hook.sh
@@ -3,9 +3,7 @@
 function hook::run() {
   if [[ "${1:-}" == "--config" ]] ; then
     __config__
-  elif [[ -n "${CONFIG_VALUES_PATH:-}" && -n "${VALUES_PATH:-}" ]] ; then
-    __main__
   else
-    __main_old__
+    __main__
   fi
 }
