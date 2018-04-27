d3c46b5a583479eb1df67b16ac3ebf8d270b00ee
Dmitry Stolyarov <dmitry.stolyarov@flant.com>
2018-04-28 00:33:50 +0300

    [shell-lib] Переход с kubectl apply на kubectl replace

--- shell_lib/kubectl/apply_jq.sh
+++ shell_lib/kubectl/apply_jq.sh
@@ -10,20 +10,13 @@ function kubectl::jq_patch() {
   local tmp=$(mktemp)
 
   local cleanup_filter='. | del(
-    .metadata.annotations."deployment.kubernetes.io/revision",
-    .metadata.annotations."kubectl.kubernetes.io/last-applied-configuration",
-    .metadata.creationTimestamp,
-    .metadata.generation,
-    .metadata.resourceVersion,
-    .metadata.selfLink,
-    .metadata.uid,
-    .status
+    .metadata.annotations."kubectl.kubernetes.io/last-applied-configuration"
   )'
 
   if ! kubectl -n $namespace get $resource -o json > $tmp ||
      ! jq "$cleanup_filter" $tmp > $a ||
      ! jq "$filter" $a > $b ||
-     ! kubectl apply -f $b ;
+     ! kubectl replace -f $b ;
   then
     echo FILTER: "$filter"
 
