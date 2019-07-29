d7e318930c2a6ae476abd13d125cc95fe68043d6
Andrey Klimentyev <andrey.klimentyev@flant.com>
2019-07-29 17:03:12 +0300

    [node-local-dns] Ensure iptables rule superiority

--- modules/350-node-local-dns/images/sidecar/hooks/iptables.sh
+++ modules/350-node-local-dns/images/sidecar/hooks/iptables.sh
@@ -18,5 +18,10 @@ if [[ $1 == "--config" ]] ; then
 }
 EOF
 else
-  iptables -w 600 -t nat -C "$chain_name" "${rule[@]}" >/dev/null 2>&1 || iptables -w 600 -t nat -I "$chain_name" 1 "${rule[@]}" >/dev/null 2>&1
+  if [ "$(iptables -w 600 -nvL -t nat --line-numbers | grep -P "$KUBE_DNS_SVC_IP.*socket --nowildcard" | grep -Po '^[0-9]+')" != "1" ]; then
+    if iptables -w 600 -t nat -C "$chain_name" "${rule[@]}" >/dev/null 2>&1 ; then
+      iptables -w 600 -t nat -D "$chain_name" "${rule[@]}" >/dev/null 2>&1
+    fi
+    iptables -w 600 -t nat -I "$chain_name" 1 "${rule[@]}" >/dev/null 2>&1
+  fi
 fi
