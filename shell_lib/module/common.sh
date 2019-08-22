c2f211b06946832e448efe50e816a9e24474b978
Andrey Sidorov <andrey.sidorov@flant.com>
2019-08-22 22:33:14 +0300

    [prometheus-metrics-adapter] Новая надежда

--- shell_lib/module/common.sh
+++ shell_lib/module/common.sh
@@ -1,5 +1,11 @@
+#!/bin/bash -e
+
+function module::name() {
+  # /antiopa/modules/301-prometheus-metrics-adapter/hooks/superhook.sh -> prometheus-metrics-adapter
+  echo $0 | sed -r 's/^\/antiopa\/modules\/\d+-([a-zA-Z0-9-]+)\/.+/\1/' | awk -F - '{printf "%s", $1; for(i=2; i<=NF; i++) printf "%s", toupper(substr($i,1,1)) substr($i,2); print"";}'
+}
+
 function module::path() {
   # /antiopa/modules/301-prometheus-metrics-adapter/hooks/superhook.sh -> /antiopa/modules/301-prometheus-metrics-adapter
   echo $0 | sed -r 's/^(\/antiopa\/modules\/\d+-[a-zA-Z0-9-]+)\/.+/\1/'
 }
-
--- shell_lib/module/name.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-function module::name() {
-  # /antiopa/modules/301-prometheus-metrics-adapter/hooks/superhook.sh -> prometheusMetricsAdapter
-  echo $0 | sed -r 's/^\/antiopa\/modules\/\d+-([a-zA-Z0-9-]+)\/.+/\1/' | awk -F - '{printf "%s", $1; for(i=2; i<=NF; i++) printf "%s", toupper(substr($i,1,1)) substr($i,2); print"";}'
-}
