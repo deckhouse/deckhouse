ebcbbf9296c78dcfdc495854d6cd13c6da23ee5e
Andrey Sidorov <andrey.sidorov@flant.com>
2019-12-10 18:49:51 +0300

    [deckhouse] json log in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,16 @@ declare -A bundles_map; bundles_map=( ["Default"]="default" ["Minimal"]="minimal
 bundle=${DECKHOUSE_BUNDLE:-Default}
 if [[ ! ${bundles_map[$bundle]+_} ]]; then
     cat <<EOF
--- Deckhouse bundle "$bundle" doesn't exists! --
-
-  Possible bundles:
-$(for variant in "${!bundles_map[@]}" ; do echo "  - $variant" ; done)
+{"msg": "-- Deckhouse bundle $bundle doesn't exists! -- Possible bundles: $(for variant in "${!bundles_map[@]}" ; do echo -n " $variant" ; done)"}
 
 EOF
     exit 1
   fi
 
-echo "-- Starting Deckhouse using bundle \"$bundle\" --"
+cat <<EOF
+{"msg": "-- Starting Deckhouse using bundle $bundle --"}
+EOF
+
 ln -s ${MODULES_DIR}/values-${bundles_map[$bundle]}.yaml ${MODULES_DIR}/values.yaml
 
 exec /deckhouse/deckhouse-controller "$@"
