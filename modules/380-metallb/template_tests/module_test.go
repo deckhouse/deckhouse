fa6e4620cbbbb1c6144dcbce76b45f86e57c89e5
Andrey Polovov <andrey.polovov@flant.com>
2019-12-18 13:37:09 +0300

    nodeSelector:         node-role.flant.com/frontend: ""

---
+++ modules/380-metallb/template_tests/module_test.go
@@ -0,0 +1,63 @@
+package template_tests
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/deckhouse/deckhouse/testing/helm"
+)
+
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "")
+}
+
+const (
+	bgpPeers = `
+    - peer-address: 1.1.1.1
+      peer-asn: 65000
+      my-asn: 64000
+      hold-time: 3s
+    - peer-address: 1.1.1.2
+      peer-asn: 65000
+      my-asn: 64000
+      hold-time: 3s`
+
+	addressPools = `
+    - name: mypool
+      protocol: bgp
+      addresses:
+      - 192.168.0.0/24`
+)
+
+var _ = Describe("Module :: metallb :: helm template ::", func() {
+	f := SetupHelmConfig([]byte{})
+
+	BeforeEach(func() {
+		f.ValuesSet("global.discovery.clusterVersion", "1.15.6")
+		f.ValuesSet("global.modulesImages.registry", "registryAddr")
+		f.ValuesSet("global.modulesImages.tags.common.kubeCaAuthProxy", "xxx")
+
+		f.ValuesSetFromYaml("global.discovery.nodeCountByRole", []byte("{}"))
+		f.ValuesSet("metallb.speaker.nodeSelector.mylabel", "myvalue")
+	})
+
+	Context("bgpPeers and addressPools are set", func() {
+		BeforeEach(func() {
+			f.ValuesSetFromYaml("metallb.bgpPeers", []byte(bgpPeers))
+			f.ValuesSetFromYaml("metallb.addressPools", []byte(addressPools))
+			f.HelmRender()
+		})
+
+		It("Should create a ConfigMap `config` with our values", func() {
+			crb := f.KubernetesResource("ConfigMap", "d8-metallb", "config")
+			Expect(crb.Exists()).To(BeTrue())
+
+			Expect(crb.Field("data.config").String()).To(Equal("\npeers:\n- hold-time: 3s\n  my-asn: 64000\n  peer-address: 1.1.1.1\n  peer-asn: 65000\n- hold-time: 3s\n  my-asn: 64000\n  peer-address: 1.1.1.2\n  peer-asn: 65000\n\naddress-pools:\n- addresses:\n  - 192.168.0.0/24\n  name: mypool\n  protocol: bgp\n"))
+		})
+
+	})
+
+})
