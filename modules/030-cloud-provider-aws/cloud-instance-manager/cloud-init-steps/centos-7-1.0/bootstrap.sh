60172f3530bae20dc111e74457e957750f664ee4
Andrey Klimentyev <andrey.klimentyev@flant.com>
2020-02-06 17:25:23 +0300

    [cloud-provider-aws] Proper hostnames for Kubernetes Nodes

--- modules/030-cloud-provider-aws/cloud-instance-manager/cloud-init-steps/centos-7-1.0/bootstrap.sh
+++ modules/030-cloud-provider-aws/cloud-instance-manager/cloud-init-steps/centos-7-1.0/bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+set -Eeuxo pipefail
+
 # {{ .instanceGroup.name }}
 # {{ .zoneName }}
 
-hostnamectl set-hostname "$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)"
+wget https://github.com/flant/go-ec2-describe-tags/releases/download/v0.0.1-flant.1/ec2_describe_tags
+chmod +x ec2_describe_tags
+instance_name=$(./ec2_describe_tags -query_meta | grep -Po '(?<=Name=).+')
+hostnamectl set-hostname "$instance_name"
