d3802d2e044620dfc9724a5fbb276622763fc2f8
Vitaliy Snurnitsin <vitaliy.snurnitsin@gmail.com>
2020-02-28 13:55:24 +0300

    [node-local-dns] Add check /metrics in liveness probe

--- modules/350-node-local-dns/images/coredns/liveness.sh
+++ modules/350-node-local-dns/images/coredns/liveness.sh
@@ -9,6 +9,7 @@ if [[ -f /tmp/shutting_down ]] ; then
 fi
 
 curl -sS "127.0.0.1:9225/health" >/dev/null
+curl -sS "127.0.0.1:9254/metrics" >/dev/null
 
 # Check internal cluster DNS name
 dig kubernetes.default.svc.${KUBE_CLUSTER_DOMAIN}. @169.254.20.10 +short +timeout=2 +tries=2 | grep -v -e '^$' >/dev/null
