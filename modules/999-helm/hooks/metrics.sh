50cddfdeba7df17da155fa6dd79f7679a7dfc34d
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2020-05-27 20:43:41 +0300

    [helm] fix metrics.sh crash, when tiller not found

--- modules/999-helm/hooks/metrics.sh
+++ modules/999-helm/hooks/metrics.sh
@@ -61,26 +61,27 @@ function __main__() {
     else
       HELM_HOST="$D8_HELM_HOST"
     fi
-    release_names="$(helm --tiller-namespace "$namespace" list --output json | jq -r '.Releases[].Name')"
-    for release_name in $release_names; do
-      manifest="$(helm --tiller-namespace "$namespace" get manifest "$release_name" | yq read -d"*" -j -)"
-      metrics="$metrics\n$(jq -rc --arg release_name "$release_name" --argjson d "$unsupported_versions" '
-        .[] | select(.kind != null) |
-        {
-          "name": "resource_versions_compatibility",
-          "set": 0,
-          "labels": {
-            "helm_release_name": $release_name,
-            "resource_name": .metadata.name,
-            "kind": .kind,
-            "api_version": .apiVersion,
-            "namespace": .metadata.namespace
+    if release_names="$(helm --tiller-namespace "$namespace" list --output json 2>/dev/null | jq -r '.Releases[].Name')"; then
+      for release_name in $release_names; do
+        manifest="$(helm --tiller-namespace "$namespace" get manifest "$release_name" | yq read -d"*" -j -)"
+        metrics="$metrics\n$(jq -rc --arg release_name "$release_name" --argjson d "$unsupported_versions" '
+          .[] | select(.kind != null) |
+          {
+            "name": "resource_versions_compatibility",
+            "set": 0,
+            "labels": {
+              "helm_release_name": $release_name,
+              "resource_name": .metadata.name,
+              "kind": .kind,
+              "api_version": .apiVersion,
+              "namespace": .metadata.namespace
+            }
           }
-        }
-        | . as $in
-        | if $d[.labels.kind] != null then if $d[$in.labels.kind] | has($in.labels.api_version) then .set = 1 else . end else . end
-      ' <<< "$manifest")"
-    done
+          | . as $in
+          | if $d[.labels.kind] != null then if $d[$in.labels.kind] | has($in.labels.api_version) then .set = 1 else . end else . end
+        ' <<< "$manifest")"
+      done
+    fi
   done
 
   echo -e "$metrics" >> $METRICS_PATH
