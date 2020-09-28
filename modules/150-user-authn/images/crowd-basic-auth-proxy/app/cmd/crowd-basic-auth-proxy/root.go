7b518f63e81ae488668afc98dea6cf7f4e7a6f3f
Maksim Nabokikh <maksim.nabokikh@flant.com>
2020-09-28 18:03:00 +0300

    [user-authn] Crowd basic auth proxy refactoring

--- modules/150-user-authn/images/crowd-basic-auth-proxy/app/cmd/crowd-basic-auth-proxy/root.go
+++ modules/150-user-authn/images/crowd-basic-auth-proxy/app/cmd/crowd-basic-auth-proxy/root.go
@@ -3,42 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 
 	"crowd-auth-proxy/pkg/proxy"
 )
 
-var (
-	listenAddress            string
-	kubernetesApiServerURL   string
-	certPath                 string
-	crowdBaseUrl             string
-	crowdApplicationLogin    string
-	crowdApplicationPassword string
-	cacheTTL                 int
-)
-
-var rootCmd = &cobra.Command{
-	Use:   "crowd-auth-proxy",
-	Short: "Basic auth proxy for Kubernetes Api Server with Atlassian Crowd",
-	Long:  `Basic auth proxy for Kubernetes Api Server with Atlassian Crowd`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("-------------------------------------")
-		fmt.Println("[😈 Starting Crowd Basic auth proxy ]")
-		fmt.Println("-------------------------------------")
-		proxy.RunProxy(listenAddress, kubernetesApiServerURL, crowdApplicationLogin, crowdApplicationPassword, certPath, crowdBaseUrl, cacheTTL)
-	},
-}
-
 func Execute() {
-	rootCmd.PersistentFlags().StringVar(&listenAddress, "listen", ":7332", "listen address and port")
-	rootCmd.PersistentFlags().StringVar(&certPath, "cert-path", "/some/cert/path", "directory with client.crt and client.key files")
-	rootCmd.PersistentFlags().StringVar(&crowdBaseUrl, "crowd-base-url", "https://crowd.example.com", "URL of Atlassian Crowd")
-	rootCmd.PersistentFlags().StringVar(&crowdApplicationLogin, "crowd-application-login", "crowd", "login of Atlassian Crowd application")
-	rootCmd.PersistentFlags().StringVar(&crowdApplicationPassword, "crowd-application-password", "user123", "password of Atlassian Crowd application")
-	rootCmd.PersistentFlags().StringVar(&kubernetesApiServerURL, "api-server-url", "https://api.example.com", "Kubernetes api server URL")
-	rootCmd.PersistentFlags().IntVar(&cacheTTL, "cache-ttl", 10, "cache TTL in seconds")
+	handler := proxy.NewHandler()
+
+	rootCmd := &cobra.Command{
+		Use:   "crowd-auth-proxy",
+		Short: "Basic auth proxy for Kubernetes API Server with Atlassian Crowd",
+		Long:  `Basic auth proxy for Kubernetes API Server with Atlassian Crowd`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("------------------------------------")
+			fmt.Println("[ Starting Crowd Basic auth proxy ]")
+			fmt.Println("------------------------------------")
+			handler.Run()
+		},
+	}
+
+	rootCmd.PersistentFlags().StringVar(&handler.ListenAddress, "listen", ":7332", "listen address and port")
+	rootCmd.PersistentFlags().StringVar(&handler.CertPath, "cert-path", "/some/cert/path", "directory with client.crt and client.key files")
+	rootCmd.PersistentFlags().StringVar(&handler.CrowdBaseURL, "crowd-base-url", "https://crowd.example.com", "URL of Atlassian Crowd")
+	rootCmd.PersistentFlags().StringVar(&handler.CrowdApplicationLogin, "crowd-application-login", "crowd", "login of Atlassian Crowd application")
+	rootCmd.PersistentFlags().StringVar(&handler.CrowdApplicationPassword, "crowd-application-password", "user123", "password of Atlassian Crowd application")
+	rootCmd.PersistentFlags().StringArrayVar(&handler.CrowdGroups, "crowd-allowed-group", nil, "Allowed Crowd groups")
+	rootCmd.PersistentFlags().StringVar(&handler.KubernetesAPIServerURL, "api-server-url", "https://api.example.com", "Kubernetes api server URL")
+	rootCmd.PersistentFlags().DurationVar(&handler.CacheTTL, "cache-ttl", 10*time.Second, "Crowd cache TTL")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Printf("starting crowd proxy error: %s", err)
