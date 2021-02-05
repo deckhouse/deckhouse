fe6853fd585d031330dc7d142e04e591e0e273fa
m.nabokikh <maksim.nabokikh@flant.com>
2021-02-05 13:08:31 +0400

    [candictl] Fix skip updating resources on "no" answer and add a test

--- candictl/pkg/terraform/runner_test.go
+++ candictl/pkg/terraform/runner_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/deckhouse/deckhouse/candictl/pkg/util/input"
 )
 
 func TestCheckPlanDestructiveChanges(t *testing.T) {
@@ -37,6 +39,67 @@ func TestCheckPlanDestructiveChanges(t *testing.T) {
 			}
 
 			require.Equal(t, tc.destructive, code)
+		})
+	}
+}
+
+func newTestRunnerWithChanges() *Runner {
+	r := NewRunner("a", "b", "c", "d")
+	r.changesInPlan = PlanHasChanges
+	return r
+}
+
+func TestCheckRunnerHandleChanges(t *testing.T) {
+	tests := []struct {
+		name   string
+		runner *Runner
+		skip   bool
+		err    error
+	}{
+		{
+			name: "Yes and skip must not skip",
+			skip: false,
+			err:  nil,
+			runner: newTestRunnerWithChanges().
+				WithSkipChangesOnDeny(true).
+				WithConfirm(func() *input.Confirmation {
+					return input.NewConfirmation().WithYesByDefault()
+				}),
+		},
+		{
+			name: "Yes without skip must not skip",
+			skip: false,
+			err:  nil,
+			runner: newTestRunnerWithChanges().
+				WithConfirm(func() *input.Confirmation {
+					return input.NewConfirmation().WithYesByDefault()
+				}),
+		},
+		{
+			name: "No and skip must skip",
+			skip: true,
+			err:  nil,
+			runner: newTestRunnerWithChanges().
+				WithSkipChangesOnDeny(true),
+		},
+		{
+			name:   "No without skip must throw an error",
+			skip:   false,
+			err:    ErrTerraformApplyAborted,
+			runner: newTestRunnerWithChanges(),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			skip, err := tc.runner.handleChanges()
+			require.Equal(t, tc.skip, skip)
+			if tc.err != nil {
+				require.Error(t, err)
+				require.EqualError(t, tc.err, err.Error())
+			} else {
+				require.NoError(t, err)
+			}
 		})
 	}
 }
