a39809bcc5f496ec588a36e7429bd924659115fb
Pavel Golovin <pavel.golovin@flant.com>
2021-02-12 12:05:07 +0300

    [prometheus] fixup grafana plugins immutable

--- modules/300-prometheus/images/grafana/run.sh
+++ modules/300-prometheus/images/grafana/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -e
 
 : "${GF_PATHS_CONFIG:=/etc/grafana/grafana.ini}"
-: "${GF_PATHS_DATA:=/var/lib/grafana}"
-: "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
+: "${GF_PATHS_DATA:=/var/lib/grafana/data}"
+: "${GF_PATHS_PLUGINS:=/etc/grafana/plugins}"
 : "${GF_PATHS_PROVISIONING:=/etc/grafana/provisioning}"
+
+PLUGINS_BUNDLED=/var/lib/grafana/plugins
+if [ -d "$PLUGINS_BUNDLED" ]; then
+  cp -TR "$PLUGINS_BUNDLED" "$GF_PATHS_PLUGINS"
+fi
 
 IFS=","
 for plugin in ${GF_CUSTOM_PLUGINS}; do
