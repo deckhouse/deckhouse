ebdd31542ec7ec7ff5386fa8f3b12ceac65a10cb
konstantin.aksenov <konstantin.aksenov@flant.com>
2021-02-15 16:37:20 +0300

    [candi] fix persist session path for vsphere provider

--- candi/cloud-providers/vsphere/terraform-modules/providers.tf
+++ candi/cloud-providers/vsphere/terraform-modules/providers.tf
@@ -4,4 +4,6 @@ provider "vsphere" {
   password = var.providerClusterConfiguration.provider.password
   allow_unverified_ssl = lookup(var.providerClusterConfiguration.provider, "insecure", false)
   persist_session = true
+  vim_session_path = "/tmp/.govmomi/sessions"
+  rest_session_path = "/tmp/.govmomi/rest_sessions"
 }
