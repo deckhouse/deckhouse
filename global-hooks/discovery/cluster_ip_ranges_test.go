2eb19d9477d0d03315ac2cabda522052ab0b47d7
Andrey Polovov <andrey.polovov@flant.com>
2021-04-12 15:18:00 +0000

    [global-hooks] Getting rid of global.discovery.nodeInternalIPs value

--- global-hooks/discovery/cluster_ip_ranges_test.go
+++ global-hooks/discovery/cluster_ip_ranges_test.go
@@ -1,9 +1,8 @@
 /*
 
 User-stories:
-1. There are Nodes in cluster. They have information about InternalIP in .status.addresses list. Hook must parse all InternalIPs and store them to `global.discovery.nodeInternalIPs` as array.
-2. There is kube-controller-manager in cluster. It has --cluster-cidr=<subnet> in his args. Hook must parse subnet and store it to `global.discovery.podSubnet`.
-3. There is kube-apiserver in cluster. It has --service-cluster-ip-range=<subnet> in his args. Hook must parse subnet and store it to `global.discovery.serviceSubnet`.
+1. There is kube-controller-manager in cluster. It has --cluster-cidr=<subnet> in his args. Hook must parse subnet and store it to `global.discovery.podSubnet`.
+2. There is kube-apiserver in cluster. It has --service-cluster-ip-range=<subnet> in his args. Hook must parse subnet and store it to `global.discovery.serviceSubnet`.
 */
 
 package hooks
@@ -22,65 +21,6 @@ var _ = Describe("Global hooks :: discovery/cluster_ip_ranges ::", func() {
 	)
 
 	const (
-		stateMaster = `
----
-apiVersion: v1
-kind: Node
-metadata:
-  name: sandbox-21-master
-status:
-  addresses:
-  - address: 10.0.3.192
-    type: ExternalIP
-  - address: 192.168.199.10
-    type: InternalIP
-`
-		stateMasterAndHisFriends = `
----
-apiVersion: v1
-kind: Node
-metadata:
-  name: sandbox-21-master
-status:
-  addresses:
-  - address: 10.0.3.192
-    type: ExternalIP
-  - address: 192.168.199.10
-    type: InternalIP
----
-apiVersion: v1
-kind: Node
-metadata:
-  name: nif-nif
-status:
-  addresses:
-  - address: 10.0.3.192
-    type: ExternalIP
-  - address: 192.168.199.20
-    type: InternalIP
----
-apiVersion: v1
-kind: Node
-metadata:
-  name: naf-naf
-status:
-  addresses:
-  - address: 10.0.3.192
-    type: ExternalIP
-  - address: 192.168.199.30
-    type: InternalIP
-  - address: 192.168.199.31
-    type: InternalIP
----
-apiVersion: v1
-kind: Node
-metadata:
-  name: nuf-nuf
-status:
-  addresses:
-  - address: 10.0.3.192
-    type: ExternalIP
-`
 		stateControllerManagerK8SApp = `
 ---
 apiVersion: v1
@@ -173,28 +113,26 @@ data:
 
 	f := HookExecutionConfigInit(initValuesString, initConfigValuesString)
 
-	Context("Single master; controller-manager by k8s-app; apiserver by k8s-app", func() {
+	Context("controller-manager by k8s-app; apiserver by k8s-app", func() {
 		BeforeEach(func() {
-			f.BindingContexts.Set(f.KubeStateSet(stateMaster + stateControllerManagerK8SApp + stateApiserverK8SApp))
+			f.BindingContexts.Set(f.KubeStateSet(stateControllerManagerK8SApp + stateApiserverK8SApp))
 			f.RunHook()
 		})
 
-		It("Expect: nodeInternalIPs — array of 1 ips; podSubnet = '192.168.10.0/24'; serviceSubnet = '192.168.30.0/24'", func() {
+		It("Expect: podSubnet = '192.168.10.0/24'; serviceSubnet = '192.168.30.0/24'", func() {
 			Expect(f).To(ExecuteSuccessfully())
-			Expect(f.ValuesGet("global.discovery.nodeInternalIPs").String()).To(MatchJSON(`["192.168.199.10"]`))
 			Expect(f.ValuesGet("global.discovery.podSubnet").String()).To(Equal("192.168.10.0/24"))
 			Expect(f.ValuesGet("global.discovery.serviceSubnet").String()).To(Equal("192.168.30.0/24"))
 		})
 
-		Context("master + additional nodes; controller-manager by k8s-app; apiserver by k8s-app", func() {
+		Context("controller-manager by k8s-app; apiserver by k8s-app", func() {
 			BeforeEach(func() {
-				f.BindingContexts.Set(f.KubeStateSet(stateMasterAndHisFriends + stateControllerManagerK8SApp + stateApiserverK8SApp))
+				f.BindingContexts.Set(f.KubeStateSet(stateControllerManagerK8SApp + stateApiserverK8SApp))
 				f.RunHook()
 			})
 
-			It("Expect: nodeInternalIPs — array of 4 ips; podSubnet = '192.168.10.0/24'; serviceSubnet = '192.168.30.0/24'", func() {
+			It("Expect: podSubnet = '192.168.10.0/24'; serviceSubnet = '192.168.30.0/24'", func() {
 				Expect(f).To(ExecuteSuccessfully())
-				Expect(f.ValuesGet("global.discovery.nodeInternalIPs").String()).To(MatchJSON(`["192.168.199.10","192.168.199.20","192.168.199.30","192.168.199.31"]`))
 				Expect(f.ValuesGet("global.discovery.podSubnet").String()).To(Equal("192.168.10.0/24"))
 				Expect(f.ValuesGet("global.discovery.serviceSubnet").String()).To(Equal("192.168.30.0/24"))
 			})
@@ -204,7 +142,7 @@ data:
 
 	Context("With d8-cluster-configuration secret", func() {
 		BeforeEach(func() {
-			f.BindingContexts.Set(f.KubeStateSet(stateMaster + stateControllerManagerK8SApp + stateApiserverK8SApp + stateClusterConfiguration))
+			f.BindingContexts.Set(f.KubeStateSet(stateControllerManagerK8SApp + stateApiserverK8SApp + stateClusterConfiguration))
 			f.RunHook()
 		})
 
@@ -215,28 +153,26 @@ data:
 		})
 	})
 
-	Context("multiple nodes; controller-manager by component; apiserver by component", func() {
+	Context("controller-manager by component; apiserver by component", func() {
 		BeforeEach(func() {
-			f.BindingContexts.Set(f.KubeStateSet(stateMasterAndHisFriends + stateControllerManagerComponent + stateApiserverComponent))
+			f.BindingContexts.Set(f.KubeStateSet(stateControllerManagerComponent + stateApiserverComponent))
 			f.RunHook()
 		})
 
-		It("Expect: nodeInternalIPs — array of 4 ips; podSubnet = '192.168.20.0/24'; serviceSubnet = '192.168.40.0/24'", func() {
+		It("Expect: podSubnet = '192.168.20.0/24'; serviceSubnet = '192.168.40.0/24'", func() {
 			Expect(f).To(ExecuteSuccessfully())
-			Expect(f.ValuesGet("global.discovery.nodeInternalIPs").String()).To(MatchJSON(`["192.168.199.10","192.168.199.20","192.168.199.30","192.168.199.31"]`))
 			Expect(f.ValuesGet("global.discovery.podSubnet").String()).To(Equal("192.168.20.0/24"))
 			Expect(f.ValuesGet("global.discovery.serviceSubnet").String()).To(Equal("192.168.40.0/24"))
 		})
 
-		Context("single master; controller-manager by component; apiserver by component", func() {
+		Context("controller-manager by component; apiserver by component", func() {
 			BeforeEach(func() {
-				f.BindingContexts.Set(f.KubeStateSet(stateMaster + stateControllerManagerComponent + stateApiserverComponent))
+				f.BindingContexts.Set(f.KubeStateSet(stateControllerManagerComponent + stateApiserverComponent))
 				f.RunHook()
 			})
 
-			It("Expect: nodeInternalIPs — array of 1 ips; podSubnet = '192.168.20.0/24'; serviceSubnet = '192.168.40.0/24'", func() {
+			It("Expect: podSubnet = '192.168.20.0/24'; serviceSubnet = '192.168.40.0/24'", func() {
 				Expect(f).To(ExecuteSuccessfully())
-				Expect(f.ValuesGet("global.discovery.nodeInternalIPs").String()).To(MatchJSON(`["192.168.199.10"]`))
 				Expect(f.ValuesGet("global.discovery.podSubnet").String()).To(Equal("192.168.20.0/24"))
 				Expect(f.ValuesGet("global.discovery.serviceSubnet").String()).To(Equal("192.168.40.0/24"))
 			})
