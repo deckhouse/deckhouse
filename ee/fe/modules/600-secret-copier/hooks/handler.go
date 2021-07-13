f8f42f4e4671ff4899a743d1a07bc2e7f7529e19
Denis Romanenko <denis.romanenko@flant.com>
2021-07-13 18:13:28 +0000

    [secret-copier] fix hook

--- ee/fe/modules/600-secret-copier/hooks/handler.go
+++ ee/fe/modules/600-secret-copier/hooks/handler.go
@@ -6,20 +6,28 @@ Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https
 package hooks
 
 import (
+	"context"
 	"fmt"
 	"reflect"
+	"time"
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"github.com/deckhouse/deckhouse/go_lib/dependency"
+	"github.com/deckhouse/deckhouse/go_lib/dependency/k8s"
 )
 
 type Secret struct {
-	ObjectMeta metav1.ObjectMeta `json:"metadata,omitempty"`
-	Type       v1.SecretType     `json:"type,omitempty"`
-	Data       map[string][]byte `json:"data,omitempty"`
+	Name      string
+	Namespace string
+	Labels    map[string]string
+	Type      v1.SecretType     `json:"type,omitempty"`
+	Data      map[string][]byte `json:"data,omitempty"`
 }
 
 type Namespace struct {
@@ -28,7 +36,7 @@ type Namespace struct {
 }
 
 func SecretPath(s *Secret) string {
-	return fmt.Sprintf("%s/%s", s.ObjectMeta.Namespace, s.ObjectMeta.Name)
+	return fmt.Sprintf("%s/%s", s.Namespace, s.Name)
 }
 
 func ApplyCopierSecretFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
@@ -39,12 +47,14 @@ func ApplyCopierSecretFilter(obj *unstructured.Unstructured) (go_hook.FilterResu
 	}
 
 	s := &Secret{
-		ObjectMeta: secret.ObjectMeta,
-		Type:       secret.Type,
-		Data:       secret.Data,
+		Name:      secret.Name,
+		Namespace: secret.Namespace,
+		Labels:    secret.Labels,
+		Type:      secret.Type,
+		Data:      secret.Data,
 	}
 	// Secrets with that label lead to D8CertmanagerOrphanSecretsChecksFailed alerts.
-	delete(s.ObjectMeta.Labels, "certmanager.k8s.io/certificate-name")
+	delete(s.Labels, "certmanager.k8s.io/certificate-name")
 
 	return s, nil
 }
@@ -65,6 +75,11 @@ func ApplyCopierNamespaceFilter(obj *unstructured.Unstructured) (go_hook.FilterR
 }
 
 var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Settings: &go_hook.HookConfigSettings{
+		ExecutionMinInterval: 5 * time.Second,
+		ExecutionBurst:       3,
+	},
+	Queue: "/modules/secret-copier",
 	Kubernetes: []go_hook.KubernetesConfig{
 		{
 			Name:       "secrets",
@@ -75,7 +90,8 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 					"secret-copier.deckhouse.io/enabled": "",
 				},
 			},
-			FilterFunc: ApplyCopierSecretFilter,
+			FilterFunc:             ApplyCopierSecretFilter,
+			WaitForSynchronization: go_hook.Bool(false),
 		},
 		{
 			Name:       "namespaces",
@@ -92,18 +108,13 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 					},
 				},
 			},
-			FilterFunc: ApplyCopierNamespaceFilter,
+			FilterFunc:             ApplyCopierNamespaceFilter,
+			WaitForSynchronization: go_hook.Bool(false),
 		},
 	},
-	Schedule: []go_hook.ScheduleConfig{
-		{
-			Name:    "cron",
-			Crontab: "0 3 * * *",
-		},
-	},
-}, copierHandler)
+}, dependency.WithExternalDependencies(copierHandler))
 
-func copierHandler(input *go_hook.HookInput) error {
+func copierHandler(input *go_hook.HookInput, dc dependency.Container) error {
 	secrets, ok := input.Snapshots["secrets"]
 	if !ok {
 		input.LogEntry.Info("No Secrets received, skipping execution")
@@ -115,12 +126,17 @@ func copierHandler(input *go_hook.HookInput) error {
 		return nil
 	}
 
+	k8, err := dc.GetK8sClient()
+	if err != nil {
+		return fmt.Errorf("can't init Kubernetes client: %v", err)
+	}
+
 	secretsExists := make(map[string]*Secret)
 	secretsDesired := make(map[string]*Secret)
 	for _, s := range secrets {
 		secret := s.(*Secret)
 		// Secrets that are not in namespace `default` are existing Secrets.
-		if secret.ObjectMeta.Namespace != "default" {
+		if secret.Namespace != v1.NamespaceDefault {
 			path := SecretPath(secret)
 			secretsExists[path] = secret
 			continue
@@ -128,15 +144,16 @@ func copierHandler(input *go_hook.HookInput) error {
 		// Secrets in namespace `default` should be propagated to all other active namespaces.
 		for _, n := range namespaces {
 			namespace := n.(*Namespace)
-			if namespace.IsTerminating || namespace.Name == "default" {
+			if namespace.IsTerminating || namespace.Name == v1.NamespaceDefault {
 				continue
 			}
 			secretDesired := &Secret{
-				ObjectMeta: secret.ObjectMeta,
-				Type:       secret.Type,
-				Data:       secret.Data,
+				Name:      secret.Name,
+				Namespace: namespace.Name,
+				Labels:    secret.Labels,
+				Type:      secret.Type,
+				Data:      secret.Data,
 			}
-			secretDesired.ObjectMeta.Namespace = namespace.Name
 			path := SecretPath(secretDesired)
 			secretsDesired[path] = secretDesired
 		}
@@ -146,12 +163,15 @@ func copierHandler(input *go_hook.HookInput) error {
 		secretDesired, desired := secretsDesired[path]
 		if !desired {
 			// Secret exists, but not desired - delete it.
-			_ = input.ObjectPatcher.DeleteObject("", "Secret", secretExist.ObjectMeta.Namespace, secretExist.ObjectMeta.Name, "")
+			err := deleteSecret(k8, secretExist)
+			if err != nil {
+				return err
+			}
 			continue
 		}
 		if !reflect.DeepEqual(secretDesired, secretExist) {
 			// Secret changed - update it.
-			err := CreateOrUpdateSecret(input, secretDesired)
+			err = updateSecret(k8, secretDesired)
 			if err != nil {
 				return err
 			}
@@ -163,7 +183,7 @@ func copierHandler(input *go_hook.HookInput) error {
 			continue
 		}
 		// Secret not exists, create it.
-		err := CreateOrUpdateSecret(input, secretDesired)
+		err := createSecret(k8, secretDesired)
 		if err != nil {
 			return err
 		}
@@ -172,26 +192,62 @@ func copierHandler(input *go_hook.HookInput) error {
 	return nil
 }
 
-func CreateOrUpdateSecret(input *go_hook.HookInput, secret *Secret) error {
+func createSecret(k8 k8s.Client, secret *Secret) error {
 	s := &v1.Secret{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "v1",
 			Kind:       "Secret",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      secret.ObjectMeta.Name,
-			Namespace: secret.ObjectMeta.Namespace,
-			Labels:    secret.ObjectMeta.Labels,
+			Name:      secret.Name,
+			Namespace: secret.Namespace,
+			Labels:    secret.Labels,
 		},
 		Data: secret.Data,
 		Type: secret.Type,
 	}
-	su, err := sdk.ToUnstructured(s)
-	if err != nil {
-		return fmt.Errorf("can't convert Secret to Unstructured: %v", err)
+	if _, err := k8.CoreV1().Secrets(secret.Namespace).Create(context.TODO(), s, metav1.CreateOptions{}); err != nil {
+		return fmt.Errorf("can't create secret object: %v", err)
 	}
-	if err := input.ObjectPatcher.CreateOrUpdateObject(su, ""); err != nil {
-		return fmt.Errorf("can't CreateOrUpdateObject Secret object: %v", err)
+
+	return nil
+}
+
+func deleteSecret(k8 k8s.Client, secret *Secret) error {
+	if err := k8.CoreV1().Secrets(secret.Namespace).Delete(context.TODO(), secret.Name, metav1.DeleteOptions{}); err != nil {
+		return fmt.Errorf("can't delete secret object: %v", err)
+	}
+
+	return nil
+}
+
+func updateSecret(k8 k8s.Client, secret *Secret) error {
+	s := &v1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Secret",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secret.Name,
+			Namespace: secret.Namespace,
+			Labels:    secret.Labels,
+		},
+		Data: secret.Data,
+		Type: secret.Type,
+	}
+
+	if _, err := k8.CoreV1().Secrets(secret.Namespace).Update(context.TODO(), s, metav1.UpdateOptions{}); err != nil {
+		// deleting and create Secret if its validation fails
+		// usually means that we are trying to change an immutable field
+		if errors.IsInvalid(err) {
+			err := deleteSecret(k8, secret)
+			if err != nil {
+				return err
+			}
+			err = createSecret(k8, secret)
+			return err
+		}
+		return fmt.Errorf("can't update secret object: %v", err)
 	}
 
 	return nil
