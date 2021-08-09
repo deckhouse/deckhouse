632a5a168ae78fca89ca5885b3a9003412f77a3a
Ivan Mikheykin <ivan.mikheykin@flant.com>
2021-08-10 01:11:40 +0300

    login to readonly for matrix tests

--- testing/validate_doc_changes.sh
+++ testing/validate_doc_changes.sh
@@ -16,10 +16,6 @@
 
 set -Eeo pipefail
 
-function request_gitlab_api() {
-  curl --silent -f -H "PRIVATE-TOKEN: ${JOB_TOKEN}"  https://fox.flant.com/api/v4/projects/${CI_PROJECT_ID}/${1}
-}
-
 # $1 - filename we test
 # $2 - filename in other language
 function checks() {
@@ -88,39 +84,18 @@ function check_resource() {
     fi
 }
 
-JOB_TOKEN=$1
-SKIP_LABEL_NAME='Skip doc validation'
-hasErrors=0
-pattern_resources="openapi/config-values.y[a]?ml$|crds/.+.y[a]?ml$"
-pattern_yaml=".+\.y[a]?ml$"
 
-if [[ -z $CI_OPEN_MERGE_REQUESTS ]]; then
-  echo "There are no merge requests found"
-  exit 0
-fi
-
-IFS=',' read -r -a MERGE_REQUESTS_ARRRAY <<< "$CI_OPEN_MERGE_REQUESTS"
-for MERGE_REQUEST_ID in ${MERGE_REQUESTS_ARRRAY[*]}; do
-  MERGE_REQUEST_ID=$( cut -d \! -f 2 <<<${MERGE_REQUEST_ID})
-  MERGE_REQUEST_DATA=$(request_gitlab_api "merge_requests/${MERGE_REQUEST_ID}/" | jq -r '.')
-
-  if [[ $(jq -rc --arg label_name "${SKIP_LABEL_NAME}" '.labels[] | select ( . == $label_name)' <<< ${MERGE_REQUEST_DATA}) != "" ]]; then
-    echo "Validation skipped...";
-    exit 0
-  fi
-done
-
-DIFF_DATA=$(git diff origin/main... --name-status -w --ignore-blank-lines --diff-filter=ACMD | awk '{print $2}' )
+DIFF_DATA=$(git diff origin/main --name-only -w --ignore-blank-lines --diff-filter=ACMD)
 
 if [[ -z $DIFF_DATA ]]; then
   echo "Empty diff data"
   exit 0
 fi
 
-echo
-
+# Check only .md files and resource files (openapi specs and crds).
+pattern_resources="openapi/config-values.y[a]?ml$|crds/.+.y[a]?ml$"
+hasErrors=0
 for item in ${DIFF_DATA}; do
-    # skip other than .md files and resource files (openapi specs and crds)
     if [[ "$item" == *".md" ]]; then
         if ! check_doc "${item}" ; then
             hasErrors=1
@@ -134,8 +109,8 @@ for item in ${DIFF_DATA}; do
     fi
 done
 
-if [[ $hasErrors -gt 0 ]] ; then
-  echo -e "\nFix the problem or use '${SKIP_LABEL_NAME}' MR label to skip."
+if [[ $hasErrors == 0 ]] ; then
+  echo "Validation successful."
 fi
 
 exit $hasErrors
--- testing/validate_no_cyrillic.sh
+++ testing/validate_no_cyrillic.sh
@@ -14,88 +14,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-
-function request_gitlab_api() {
-  curl --silent -f -H "PRIVATE-TOKEN: ${JOB_TOKEN}"  https://fox.flant.com/api/v4/projects/${PROJECT_ID}/${1}
-}
+set -Eeo pipefail
 
 function check_cyrillic_letters() {
-  if words=$(grep -Eo "[А-Яа-яЁё]+" <<< ${1}) ; then
+  if words=$(grep -Eno "[А-Яа-яЁё]+" <<< "${1}") ; then
     echo "  ERROR: Cyrillic letters found!"
-    echo "${words}" | jq -R '.' | jq -sc '.'
+    echo "${words}"
+     #| jq -R '.' | jq -sc '.'
     return 1
   fi
   echo "  OK!"
 }
 
-function main() {
-  MERGE_REQUEST_ID=$(request_gitlab_api "merge_requests?state=opened" | jq -r --arg c ${COMMIT_SHA} '.[]|select(.sha == $c) | .iid')
-  if [[ "$MERGE_REQUEST_ID" == "" ]]; then
-    echo "No merge request found for commit sha: ${COMMIT_SHA}"
-    exit 0
-  fi
+hasCyrillicLetters=0
 
-  echo "Merge Request ID = ${MERGE_REQUEST_ID}"
-
-  MERGE_REQUEST_DATA=$(request_gitlab_api "merge_requests/${MERGE_REQUEST_ID}/changes" | jq -r '.')
-  if [[ $(jq -rc '.labels[] | select ( . == "Content: Cyrillic")' <<< ${MERGE_REQUEST_DATA}) != "" ]]; then
-    echo '"Content: Cyrillic" label is present. End.'
-    exit 0
-  fi
-
-  hasCyrillicLetters=0
-
+if [[ -n $VALIDATE_TITLE ]] ; then
   echo -n "Check title:"
-  if ! check_cyrillic_letters "$(jq -r '.title' <<< ${MERGE_REQUEST_DATA})" ; then
+  if ! check_cyrillic_letters "$VALIDATE_TITLE" ; then
     hasCyrillicLetters=1
   fi
+fi
 
+if [[ -n $VALIDATE_DESCRIPTION ]] ; then
   echo -n "Check description:"
-  if ! check_cyrillic_letters "$(jq -r '.description' <<< ${MERGE_REQUEST_DATA})" ; then
+  if ! check_cyrillic_letters "$VALIDATE_DESCRIPTION" ; then
     hasCyrillicLetters=1
   fi
+fi
 
-  echo "Check diff:"
-  CHANGES=$(jq -rc '.changes' <<< ${MERGE_REQUEST_DATA})
+echo "Check new and updated lines:"
 
-  for key in $(jq -rc 'keys[]' <<< "${CHANGES}"); do
-    FILE_DIFF=$(jq -rc --arg key "$key" '.[$key | tonumber]' <<< "${CHANGES}")
+DIFF_DATA="$(git diff origin/main... --name-only -w --ignore-blank-lines --diff-filter=AM)"
 
-    new_path=$(jq -rc '.new_path' <<< ${FILE_DIFF})
-
-    # skip documentation
-    pattern="doc-ru-.+.y[a]?ml$|_RU.md$|_ru.html$|docs/site/_.+|docs/documentation/_.+"
-    if [[ "$new_path" =~ $pattern ]] ; then
-      echo "  * skip documentation: $new_path"
-      continue
-    fi
-
-    # skip translations
-    if [[ "$new_path" == *"/i18n/"* ]] ; then
-      echo "  * skip translations: $new_path"
-      continue
-    fi
-
-    # skip self
-    if [[ "$new_path" == *"validate_no_cyrillic.sh" ]] ; then
-      echo "  * skip self: $new_path"
-      continue
-    fi
-
-    # Check only new or updated lines in diff
-    # Ignore latin only lines.
-    if ! diff_error=$(check_cyrillic_letters "$(jq -rc '.diff' <<< ${FILE_DIFF} | grep '+')" ) ; then
-      echo "  * diff: ${new_path} ${diff_error}"
-      hasCyrillicLetters=1
-    fi
-  done
-
+if [[ -z $DIFF_DATA ]]; then
+  echo "  * diff is empty"
   exit $hasCyrillicLetters
-}
+fi
 
-PROJECT_ID=$1
-COMMIT_SHA=$2
-JOB_TOKEN=$3
+for FILE_NAME in $DIFF_DATA; do
+  # skip documentation
+  pattern="doc-ru-.+.y[a]?ml$|_RU.md$|_ru.html$|docs/site/_.+|docs/documentation/_.+"
+  if [[ "$FILE_NAME" =~ $pattern ]] ; then
+    echo "  * $FILE_NAME: skip documentation"
+    continue
+  fi
 
-main
+  # skip translations
+  if [[ "$FILE_NAME" == *"/i18n/"* ]] ; then
+    echo "  * $FILE_NAME: skip translations"
+    continue
+  fi
+
+  # skip self
+  if [[ "$FILE_NAME" == *"validate_no_cyrillic.sh" ]] ; then
+    echo "  * $FILE_NAME: skip self"
+    continue
+  fi
+
+  # Check only new or updated lines in diff and ignore empty diffs.
+  if ! FILE_DIFF=$(git diff origin/main -- $FILE_NAME | grep -v '^+++\ b' | grep '^+') ; then
+    continue
+  fi
+  if ! check_msg=$(check_cyrillic_letters "${FILE_DIFF}" ) ; then
+    echo "  * $FILE_NAME: ${check_msg}"
+    hasCyrillicLetters=1
+  fi
+done
+
+
+if [[ $hasCyrillicLetters == 0 ]]; then
+  echo "Validation successful."
+fi
+
+exit $hasCyrillicLetters
