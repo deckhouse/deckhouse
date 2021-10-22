2e522d18ec5e4d0f4688b20c0d8075342ac7e657
Konstantin Aksenov <konstantin.aksenov@flant.com>
2021-10-22 08:22:02 +0300

    [upmeter] don't wait for synchronization in reschedule hook

--- modules/500-upmeter/hooks/smokemini/reschedule.go
+++ modules/500-upmeter/hooks/smokemini/reschedule.go
@@ -49,10 +49,11 @@ var _ = sdk.RegisterFunc(
 		Queue: "/modules/upmeter/update_selector",
 		Kubernetes: []go_hook.KubernetesConfig{
 			{
-				Name:       "nodes",
-				ApiVersion: "v1",
-				Kind:       "Node",
-				FilterFunc: snapshot.NewNode,
+				Name:                   "nodes",
+				ApiVersion:             "v1",
+				Kind:                   "Node",
+				FilterFunc:             snapshot.NewNode,
+				WaitForSynchronization: pointer.BoolPtr(false),
 			},
 			{
 				Name:              "statefulsets",
@@ -62,7 +63,8 @@ var _ = sdk.RegisterFunc(
 				LabelSelector:     labelSelector,
 				FilterFunc:        snapshot.NewStatefulSet,
 
-				ExecuteHookOnEvents: pointer.BoolPtr(false),
+				ExecuteHookOnEvents:    pointer.BoolPtr(false),
+				WaitForSynchronization: pointer.BoolPtr(false),
 			},
 			{
 				Name:              "pods",
@@ -72,7 +74,8 @@ var _ = sdk.RegisterFunc(
 				LabelSelector:     labelSelector,
 				FilterFunc:        snapshot.NewPod,
 
-				ExecuteHookOnEvents: pointer.BoolPtr(false),
+				ExecuteHookOnEvents:    pointer.BoolPtr(false),
+				WaitForSynchronization: pointer.BoolPtr(false),
 			},
 			{
 				Name:              "pdb",
@@ -82,13 +85,15 @@ var _ = sdk.RegisterFunc(
 				LabelSelector:     labelSelector,
 				FilterFunc:        snapshot.NewDisruption,
 
-				ExecuteHookOnEvents: pointer.BoolPtr(false),
+				ExecuteHookOnEvents:    pointer.BoolPtr(false),
+				WaitForSynchronization: pointer.BoolPtr(false),
 			},
 			{
-				Name:       "default_sc",
-				ApiVersion: "storage.k8s.io/v1",
-				Kind:       "StorageClass",
-				FilterFunc: snapshot.NewStorageClass,
+				Name:                   "default_sc",
+				ApiVersion:             "storage.k8s.io/v1",
+				Kind:                   "StorageClass",
+				FilterFunc:             snapshot.NewStorageClass,
+				WaitForSynchronization: pointer.BoolPtr(false),
 			},
 		},
 	},
