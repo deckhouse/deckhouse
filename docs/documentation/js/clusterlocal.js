22a8509f6500936b436ce04bf36080f859b62bb9
Artem Kladov <artem.kladov@flant.com>
2021-11-10 14:49:54 +0300

    [deckhouse-web] Don't show info about current release channel

--- docs/documentation/js/clusterlocal.js
+++ docs/documentation/js/clusterlocal.js
@@ -2,31 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     $.getJSON('/config/data.json', {_: new Date().getTime()}).done(function (resp) {
         // data.json example: {"channel":"stable", "version":"xxxx", "edition":"FE"}
 
-        let deckhouseVersionInfo = "Unknown";
+        let deckhouseVersionInfo = "";
         let update_channels_list = ['alpha', 'beta', 'early-access', 'stable', 'rock-solid'];
 
         if (resp) {
             if (resp['channel']) {
                 $(`.releases__menu-item.releases__menu--channel--${resp['channel']}`).addClass("releases__menu-item-block-active");
                 $(`.releases__menu-item-title.releases__menu--channel--${resp['channel']}`).addClass("releases__menu-item-title-active");
-                if (update_channels_list.indexOf(resp['channel']) < 0) {
-                    let isRuLang = document.location.pathname.match(/^\/ru\//);
-                    if (isRuLang) {
-                        deckhouseVersionInfo = `на ветке '${resp['channel']}'`;
-                    } else {
-                        deckhouseVersionInfo = `on the '${resp['channel']}' branch`;
-                    }
-                    $("#releases__stale__block").css({display: "block"});
-                } else {
-                    deckhouseVersionInfo = resp['channel'];
-                    $("#releases__stale__block").css({display: "none"});
-                }
             } else {
                 console.log('UpdateChannel is not defined.');
             }
 
-            if (resp['version'] && resp['version'] !== "dev") {
-                deckhouseVersionInfo = `${resp['version']} (${deckhouseVersionInfo})`;
+            if (resp['version']) {
+                deckhouseVersionInfo = `${resp['version']}`;
             }
 
             if (resp['edition']) {
