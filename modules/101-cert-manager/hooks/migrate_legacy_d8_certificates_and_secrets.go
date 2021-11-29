05239f408d2327d658ded0ac7c755083e00db508
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2021-11-29 17:33:54 +0300

    [cert-manager] Fix race between helm and migrate_legacy_d8_certificates_and_secrets

--- modules/101-cert-manager/hooks/migrate_legacy_d8_certificates_and_secrets.go
+++ modules/101-cert-manager/hooks/migrate_legacy_d8_certificates_and_secrets.go
@@ -57,6 +57,9 @@ func applyLegacySecretFilter(obj *unstructured.Unstructured) (go_hook.FilterResu
 
 var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 	Queue: internal.Queue("certificates"),
+	OnBeforeHelm: &go_hook.OrderedConfig{
+		Order: 10,
+	},
 	Kubernetes: []go_hook.KubernetesConfig{
 		{
 			Name:       "certificates_legacy",
