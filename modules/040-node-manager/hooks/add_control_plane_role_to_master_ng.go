b0c4efbdafa8303145719ed5961ba684c48fefe1
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2022-01-11 20:20:14 +0300

    [dhctl][global] Add control-plane role for master node group

---
+++ modules/040-node-manager/hooks/add_control_plane_role_to_master_ng.go
@@ -0,0 +1,51 @@
+// Copyright 2021 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hooks
+
+// TODO remove after 1.32 release
+// add control-plane role for all master nodes over master node group
+// At current moment, first bootstrapped master get 'control-plane' role,
+// but other master nodes don't get this role, because
+// first master bootstrapped with kubeadm (kubeadm set role to node over label), but
+// master node group was created on bootstrap does not contain label with role
+// we will add label to nodegroup template for existing clusters
+
+import (
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube/object_patch"
+)
+
+const controlPlaneRoleLabel = "node-role.kubernetes.io/control-plane"
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	OnAfterHelm: &go_hook.OrderedConfig{Order: 10},
+}, addControlPlaneRoleToMasterNodeGroup)
+
+func addControlPlaneRoleToMasterNodeGroup(input *go_hook.HookInput) error {
+	patch := map[string]interface{}{
+		"spec": map[string]interface{}{
+			"nodeTemplate": map[string]interface{}{
+				"labels": map[string]interface{}{
+					"node-role.kubernetes.io/control-plane": "",
+				},
+			},
+		},
+	}
+
+	input.PatchCollector.MergePatch(patch, "deckhouse.io/v1", "NodeGroup", "", "master", object_patch.IgnoreMissingObject())
+
+	return nil
+}
---
+++ modules/040-node-manager/hooks/add_control_plane_role_to_master_ng_test.go
@@ -0,0 +1,164 @@
+// Copyright 2021 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hooks
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	. "github.com/deckhouse/deckhouse/testing/hooks"
+)
+
+var _ = Describe("Global hooks :: migrate :: add_control_plane_role_to_master_ng_test ::", func() {
+	f := HookExecutionConfigInit(`{}`, `{}`)
+
+	var nodeGroupResource = schema.GroupVersionResource{Group: "deckhouse.io", Version: "v1", Resource: "nodegroups"}
+	f.RegisterCRD(nodeGroupResource.Group, nodeGroupResource.Version, "NodeGroup", false)
+
+	const (
+		workerNgYAML = `
+apiVersion: deckhouse.io/v1
+kind: NodeGroup
+metadata:
+  name: worker
+spec:
+  cloudInstances:
+    classReference:
+      kind: OpenStackInstanceClass
+      name: worker-big
+    maxPerZone: 1
+    maxSurgePerZone: 1
+    maxUnavailablePerZone: 0
+    minPerZone: 1
+  disruptions:
+    approvalMode: Automatic
+  nodeTemplate:
+    labels:
+      node.deckhouse.io/group: worker-big
+  nodeType: CloudEphemeral
+`
+		masterNgWithoutRole = `
+apiVersion: deckhouse.io/v1
+kind: NodeGroup
+metadata:
+  name: master
+spec:
+  disruptions:
+    approvalMode: Manual
+  nodeTemplate:
+    labels:
+      node-role.kubernetes.io/master: ""
+    taints:
+    - effect: NoSchedule
+      key: node-role.kubernetes.io/master
+  nodeType: CloudStatic
+`
+		masterNgWithRole = `
+apiVersion: deckhouse.io/v1
+kind: NodeGroup
+metadata:
+  name: master
+spec:
+  disruptions:
+    approvalMode: Manual
+  nodeTemplate:
+    labels:
+      node-role.kubernetes.io/master: ""
+      node-role.kubernetes.io/control-plane: ""
+    taints:
+    - effect: NoSchedule
+      key: node-role.kubernetes.io/master
+  nodeType: CloudStatic
+`
+	)
+
+	Context("Cluster without node groups", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(""))
+			f.RunHook()
+		})
+
+		It("Hook execute successfully and node groups should not created", func() {
+			Expect(f).To(ExecuteSuccessfully())
+
+			nodeGroups, err := f.KubeClient().Dynamic().Resource(nodeGroupResource).Namespace("").List(context.TODO(), v1.ListOptions{})
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(nodeGroups.Items).To(HaveLen(0))
+		})
+	})
+
+	Context("Cluster has master node group without role", func() {
+		BeforeEach(func() {
+			JoinKubeResourcesAndSet(f, masterNgWithoutRole, workerNgYAML)
+			f.RunHook()
+		})
+
+		It("Sets role for master node group and not affect another labels", func() {
+			Expect(f).To(ExecuteSuccessfully())
+
+			masterNg := f.KubernetesResource("NodeGroup", "", "master")
+			labels := masterNg.Field("spec.nodeTemplate.labels").Map()
+
+			Expect(labels).To(HaveKey(controlPlaneRoleLabel))
+			Expect(labels).To(HaveKey("node-role.kubernetes.io/master"))
+		})
+
+		It("Should not affect another fields in spec", func() {
+			Expect(f).To(ExecuteSuccessfully())
+
+			masterNg := f.KubernetesResource("NodeGroup", "", "master")
+			labels := masterNg.Field("spec.nodeTemplate.labels").Map()
+
+			Expect(labels).To(HaveKey("node-role.kubernetes.io/master"))
+
+			taints := masterNg.Field("spec.nodeTemplate.taints").Array()
+
+			Expect(taints).To(HaveLen(1))
+			Expect(taints[0].Value()).To(Equal(map[string]interface{}{
+				"effect": "NoSchedule",
+				"key":    "node-role.kubernetes.io/master",
+			}))
+		})
+
+		It("Does not affect another ng", func() {
+			Expect(f).To(ExecuteSuccessfully())
+
+			workerNg := f.KubernetesResource("NodeGroup", "", "worker")
+			Expect(workerNg.ToYaml()).To(MatchYAML(workerNgYAML))
+		})
+	})
+
+	Context("Cluster has master node group with role", func() {
+		BeforeEach(func() {
+			JoinKubeResourcesAndSet(f, masterNgWithRole, workerNgYAML)
+			f.RunHook()
+		})
+
+		It("Should not affect node groups", func() {
+			Expect(f).To(ExecuteSuccessfully())
+
+			masterNg := f.KubernetesResource("NodeGroup", "", "master")
+			workerNg := f.KubernetesResource("NodeGroup", "", "worker")
+
+			Expect(masterNg.ToYaml()).To(MatchYAML(masterNgWithRole))
+			Expect(workerNg.ToYaml()).To(MatchYAML(workerNgYAML))
+		})
+	})
+})
