f13861104e2f8d72b5da0bf911b58613aeff730d
m.nabokikh <maksim.nabokikh@flant.com>
2022-02-01 09:12:11 +0400

    [dhctl] Fix CI script
    Signed-off-by: m.nabokikh <maksim.nabokikh@flant.com>

--- dhctl/hack/coverage.sh
+++ dhctl/hack/coverage.sh
@@ -14,8 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -Eeuo pipefail
-shopt -s failglob
+set -euo pipefail
 
 tmpfile=$(mktemp /tmp/coverage-report.XXXXXX)
 
