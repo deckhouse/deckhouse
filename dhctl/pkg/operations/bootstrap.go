48ad3e335e1e26f8ebcc23ba593b0718135aa420
Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>
2022-03-01 19:12:17 +0300

    Update dhctl/pkg/operations/bootstrap.go
    Co-authored-by: Maksim Nabokikh <maksim.nabokikh@flant.com>

--- dhctl/pkg/operations/bootstrap.go
+++ dhctl/pkg/operations/bootstrap.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO structure this functions into classes and move to operations/bootstrap module
+// TODO structure these functions into classes and move to the operations/bootstrap module
 // TODO move states saving to operations/bootstrap/state.go
 
 package operations
