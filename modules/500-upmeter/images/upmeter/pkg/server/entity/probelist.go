c29708f1824793d7c4e9d6f6feb028b5530907dc
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2022-03-24 14:15:40 +0300

    omit groups in probe list

--- modules/500-upmeter/images/upmeter/pkg/server/entity/probelist.go
+++ modules/500-upmeter/images/upmeter/pkg/server/entity/probelist.go
@@ -18,15 +18,20 @@ package entity
 
 import (
 	"d8.io/upmeter/pkg/check"
+	"d8.io/upmeter/pkg/db/dao"
 )
 
 func FilterDisabledProbesFromGroupProbeList(probeRefs []check.ProbeRef) []check.ProbeRef {
 	res := make([]check.ProbeRef, 0)
 
 	for _, probeRef := range probeRefs {
-		if check.IsProbeEnabled(probeRef.Id()) {
-			res = append(res, probeRef)
+		if !check.IsProbeEnabled(probeRef.Id()) {
+			continue
 		}
+		if probeRef.Probe == dao.GroupAggregation {
+			continue
+		}
+		res = append(res, probeRef)
 	}
 
 	return res
