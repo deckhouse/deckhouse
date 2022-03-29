7a14bd358ca41dcabeb2eeb2e406060eb297e538
Andrey Klimentyev <andrey.klimentyev@flant.com>
2022-03-29 18:19:19 +0300

    +

--- ee/modules/350-node-local-dns/images/coredns/start.sh
+++ ee/modules/350-node-local-dns/images/coredns/start.sh
@@ -29,4 +29,6 @@ if iptables -w 60 -t raw -C PREROUTING -d "${KUBE_DNS_SVC_IP}/32" -m socket --no
   iptables -w 60 -t raw -D PREROUTING -d "${KUBE_DNS_SVC_IP}/32" -m socket --nowildcard -j NOTRACK >/dev/null 2>&1
 fi
 
+echo -n "not-ready" > /tmp/coredns-readiness
+
 exec /coredns -conf /etc/coredns/Corefile
