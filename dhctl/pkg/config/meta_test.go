6cfbeaa624f79a34ca1015047113ea16a7a6d11d
Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>
2022-03-31 17:43:48 +0300

    [dhctl] Trim ending slash for imagesRepo (#1232)
    (cherry picked from commit ac5abe2ea60e4b64f751a0d224b71724f54dbf90)

--- dhctl/pkg/config/meta_test.go
+++ dhctl/pkg/config/meta_test.go
@@ -15,8 +15,13 @@
 package config
 
 import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
 	"testing"
+	"text/template"
 
+	"github.com/Masterminds/sprig/v3"
 	"github.com/stretchr/testify/require"
 )
 
@@ -53,4 +58,109 @@ func TestGetDNSAddress(t *testing.T) {
 			require.Equal(t, testCase.result, getDNSAddress(testCase.cidr))
 		})
 	}
+}
+
+func renderTestConfig(data map[string]interface{}) string {
+	config := `
+apiVersion: deckhouse.io/v1
+kind: ClusterConfiguration
+clusterType: Cloud
+cloud:
+  provider: Yandex
+  prefix: cluster
+podSubnetCIDR: 10.111.0.0/16
+serviceSubnetCIDR: 10.222.0.0/16
+kubernetesVersion: "1.21"
+clusterDomain: "cluster.local"
+---
+apiVersion: deckhouse.io/v1
+kind: InitConfiguration
+deckhouse:
+  devBranch: main
+  # address of the registry where the installer image is located; in this case, the default value for Deckhouse CE is set
+  imagesRepo: {{ .imagesRepo }}
+  # a special string with parameters to access Docker registry
+  registryDockerCfg: {{ .dockerCfg | b64enc }}
+  configOverrides:
+    prometheusMadisonIntegrationEnabled: false
+    global:
+      modules:
+        publicDomainTemplate: "%s.example.com"
+    nginxIngressEnabled: false
+---
+apiVersion: deckhouse.io/v1
+kind: YandexClusterConfiguration
+layout: WithoutNAT
+masterNodeGroup:
+  replicas: 1
+  instanceClass:
+    cores: 4
+    memory: 8192
+    imageID: id
+    diskSizeGB: 50
+    externalIPAddresses:
+      - Auto
+sshPublicKey: ssh-rsa AAAA
+nodeNetworkCIDR: 10.100.0.0/21
+provider:
+  cloudID: idCloud
+  folderID: idFolder
+  serviceAccountJSON: |
+    {
+       "id": "id",
+       "service_account_id": "saID",
+       "created_at": "2020-01-01T00:00:00Z",
+       "key_algorithm": "RSA_2048",
+       "public_key": "publicKey",
+       "private_key": "privateKey"
+    }
+`
+	t := template.New("testconfig_template").Funcs(sprig.TxtFuncMap())
+	t, err := t.Parse(config)
+	if err != nil {
+		panic(err)
+	}
+
+	var tpl bytes.Buffer
+
+	err = t.Execute(&tpl, data)
+	if err != nil {
+		panic(err)
+	}
+
+	return tpl.String()
+}
+
+func generateDockerCfg() string {
+	dockerCfgAuth := base64.StdEncoding.EncodeToString([]byte("a:b"))
+	return fmt.Sprintf(`{ "auths": { "r.example.com": { "auth": "%s" } } }`, dockerCfgAuth)
+}
+
+func TestPrepare(t *testing.T) {
+	t.Run("Registry", func(t *testing.T) {
+		dataToRender := map[string]interface{}{
+			"dockerCfg":  generateDockerCfg(),
+			"imagesRepo": "r.example.com/deckhouse/ce/",
+		}
+		configData := renderTestConfig(dataToRender)
+
+		cfg, err := ParseConfigFromData(configData)
+		require.NoError(t, err)
+
+		t.Run("Trim right slash for imagesRepo", func(t *testing.T) {
+			require.Equal(t, cfg.DeckhouseConfig.ImagesRepo, "r.example.com/deckhouse/ce")
+		})
+
+		t.Run("Correct prepare registry object", func(t *testing.T) {
+			expectedData := RegistryData{
+				Address:   "r.example.com",
+				Path:      "/deckhouse/ce",
+				Scheme:    "https",
+				CA:        "",
+				DockerCfg: "eyAiYXV0aHMiOiB7ICJyLmV4YW1wbGUuY29tIjogeyAiYXV0aCI6ICJZVHBpIiB9IH0gfQ==",
+			}
+
+			require.Equal(t, cfg.Registry, expectedData)
+		})
+	})
 }
