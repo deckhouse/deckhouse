a663f2dbc7716d56fd2abc5664408000248f33ec
Andrey Klimentyev <andrey.klimentyev@flant.com>
2022-04-04 12:04:31 +0300

    +

--- modules/040-node-manager/hooks/handle_node_templates.go
+++ modules/040-node-manager/hooks/handle_node_templates.go
@@ -21,14 +21,12 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
-	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
-	"github.com/flant/addon-operator/sdk"
-
 	ngv1 "github.com/deckhouse/deckhouse/go_lib/api/v1"
-
 	"github.com/deckhouse/deckhouse/go_lib/set"
 	"github.com/deckhouse/deckhouse/go_lib/taints"
 )
