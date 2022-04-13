009b55d6045b9210b776fb1ee43d4a63368e3a38
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2022-04-13 14:21:56 +0300

    ++

--- modules/040-control-plane-manager/hooks/etcd_quota_backend_bytes_test.go
+++ modules/040-control-plane-manager/hooks/etcd_quota_backend_bytes_test.go
@@ -126,147 +126,148 @@ var _ = FDescribe("Modules :: controler-plane-manager :: hooks :: etcd-quota-bac
 		}{
 			{
 				title: "For single node always return this node",
-				nodes: []go_hook.FilterResult{{
-					memory:      gb(8),
-					isDedicated: false,
-				}},
-				expectedNode: &etcdNode{
-					memory:      gb(8),
-					isDedicated: false,
-				},
-			},
-
-			{
-				title: "For all different nodes return with minimal memory",
 				nodes: []go_hook.FilterResult{
-					{
+					&etcdNode{
 						memory:      gb(8),
-						isDedicated: false,
-					},
-					{
-						memory:      gb(12),
-						isDedicated: false,
-					},
-				},
-
+						isDedicated: true,
+					}},
 				expectedNode: &etcdNode{
 					memory:      gb(8),
-					isDedicated: false,
+					isDedicated: true,
 				},
 			},
 
 			{
 				title: "For all different nodes return with minimal memory",
 				nodes: []go_hook.FilterResult{
-					{
+					&etcdNode{
+						memory:      gb(8),
+						isDedicated: true,
+					},
+					&etcdNode{
+						memory:      gb(12),
+						isDedicated: true,
+					},
+				},
+
+				expectedNode: &etcdNode{
+					memory:      gb(8),
+					isDedicated: true,
+				},
+			},
+
+			{
+				title: "For all different nodes return with minimal memory",
+				nodes: []go_hook.FilterResult{
+					&etcdNode{
 						memory:      gb(16),
-						isDedicated: false,
+						isDedicated: true,
 					},
-					{
+					&etcdNode{
 						memory:      gb(8),
-						isDedicated: false,
+						isDedicated: true,
 					},
-					{
+					&etcdNode{
 						memory:      gb(12),
-						isDedicated: false,
+						isDedicated: true,
 					},
 				},
 
 				expectedNode: &etcdNode{
 					memory:      gb(8),
-					isDedicated: false,
+					isDedicated: true,
 				},
 			},
 
 			{
 				title: "For all different nodes return with minimal memory",
 				nodes: []go_hook.FilterResult{
-					{
+					&etcdNode{
 						memory:      gb(16),
-						isDedicated: false,
+						isDedicated: true,
 					},
-					{
+					&etcdNode{
 						memory:      gb(12),
-						isDedicated: false,
+						isDedicated: true,
 					},
-					{
+					&etcdNode{
 						memory:      gb(8),
-						isDedicated: false,
+						isDedicated: true,
 					},
 				},
 
 				expectedNode: &etcdNode{
 					memory:      gb(8),
-					isDedicated: false,
+					isDedicated: true,
 				},
 			},
 
 			{
 				title: "For all different nodes return with minimal memory",
 				nodes: []go_hook.FilterResult{
-					{
+					&etcdNode{
 						memory:      gb(8),
-						isDedicated: false,
+						isDedicated: true,
 					},
-					{
+					&etcdNode{
 						memory:      gb(12),
-						isDedicated: false,
+						isDedicated: true,
 					},
-					{
+					&etcdNode{
 						memory:      gb(16),
-						isDedicated: false,
+						isDedicated: true,
 					},
 				},
 
 				expectedNode: &etcdNode{
 					memory:      gb(8),
-					isDedicated: false,
+					isDedicated: true,
 				},
 			},
 
 			{
 				title: "If have dedicated node, return dedicated node",
 				nodes: []go_hook.FilterResult{
-					{
+					&etcdNode{
 						memory:      gb(8),
-						isDedicated: false,
-					},
-					{
-						memory:      gb(12),
 						isDedicated: true,
 					},
-					{
-						memory:      gb(16),
+					&etcdNode{
+						memory:      gb(12),
 						isDedicated: false,
+					},
+					&etcdNode{
+						memory:      gb(16),
+						isDedicated: true,
 					},
 				},
 
 				expectedNode: &etcdNode{
 					memory:      gb(12),
-					isDedicated: true,
+					isDedicated: false,
 				},
 			},
 
 			{
 				title: "If have two dedicated nodes, return first dedicated node",
 				nodes: []go_hook.FilterResult{
-					{
+					&etcdNode{
 						memory:      gb(8),
+						isDedicated: true,
+					},
+					&etcdNode{
+						memory:      gb(12),
 						isDedicated: false,
 					},
-					{
-						memory:      gb(12),
-						isDedicated: true,
-					},
-					{
+					&etcdNode{
 						memory:      gb(16),
-						isDedicated: true,
+						isDedicated: false,
 					},
 				},
 
 				expectedNode: &etcdNode{
 					memory:      gb(12),
-					isDedicated: true,
+					isDedicated: false,
 				},
 			},
 		}
@@ -285,20 +286,274 @@ var _ = FDescribe("Modules :: controler-plane-manager :: hooks :: etcd-quota-bac
 		initValuesString = `{"controlPlaneManager":{"internal": {}, "apiserver": {"authn": {}, "authz": {}}}}`
 	)
 
-	_ = HookExecutionConfigInit(initValuesString, "")
+	f := HookExecutionConfigInit(initValuesString, "")
+
+	getNodeManifest := func(name string, memory int64, setTaint bool) string {
+		spec := ""
+		if setTaint {
+			spec = `
+spec:
+  taints:
+  - effect: NoSchedule
+    key: node-role.kubernetes.io/master
+`
+		}
+		return fmt.Sprintf(`
+apiVersion: v1
+kind: Node
+metadata:
+  name: %s
+  labels:
+    node-role.kubernetes.io/master: ""
+%s
+status:
+  addresses:
+    - address: 192.168.1.2
+      type: InternalIP
+  capacity:
+    cpu: "4"
+    ephemeral-storage: 20145724Ki
+    hugepages-1Gi: "0"
+    hugepages-2Mi: "0"
+    memory: %d
+    pods: "110"
+
+`, name, spec, memory)
+	}
+
+	assertNewQuotaBackends := func(f *HookExecutionConfig, newSize int64) {
+		size := f.ValuesGet("controlPlaneManager.internal.etcdQuotaBackendBytes").Int()
+		Expect(size).To(Equal(newSize))
+	}
+
+	assertClearMetrics := func(f *HookExecutionConfig) {
+		metrics := f.MetricsCollector.CollectedMetrics()
+
+		Expect(metrics).To(HaveLen(1))
+
+		Expect(metrics[0].Action).To(Equal("expire"))
+		Expect(metrics[0].Group).To(Equal(etcdBackendBytesGroup))
+	}
 
 	Context("Single master", func() {
 		Context("etcd does not have quota-backend-bytes parameter", func() {
-			Context("etcd db size is 0 bytes", func() {
+			nodeName := "control-plane-0"
+			ip := "192.162.0.1"
+			Context("node has less than 24 Gb", func() {
+				BeforeEach(func() {
+					podManifest := etcdPodManifest(map[string]interface{}{
+						"name":     "etcd-pod1",
+						"nodeName": nodeName,
+						"hostIP":   ip,
+					})
+					nodeManifest := getNodeManifest(nodeName, gb(8), true)
 
+					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+
+					f.RunHook()
+				})
+
+				It("set default backend size as quota-backend-bytes", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(2))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
 			})
 
-			Context("etcd has quota-backend-bytes parameter", func() {
-				_ = func(data map[string]interface{}) string {
-					data["maxDbSize"] = 4 * 1024 * 1024 * 1024
-					return etcdPodManifest(data)
+			Context("node has great equal 24 Gb", func() {
+				BeforeEach(func() {
+					podManifest := etcdPodManifest(map[string]interface{}{
+						"name":     "etcd-pod1",
+						"nodeName": nodeName,
+						"hostIP":   ip,
+					})
+					nodeManifest := getNodeManifest(nodeName, gb(24), true)
+
+					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+
+					f.RunHook()
+				})
+
+				It("set increase on 1 gb of default size as quota-backend-bytes", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(3))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("node has 64 Gb", func() {
+				BeforeEach(func() {
+					podManifest := etcdPodManifest(map[string]interface{}{
+						"name":     "etcd-pod1",
+						"nodeName": nodeName,
+						"hostIP":   ip,
+					})
+					nodeManifest := getNodeManifest(nodeName, gb(64), true)
+
+					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+
+					f.RunHook()
+				})
+
+				It("set maximum size for quota-backend-bytes", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(8))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("node has greater than 24 Gb and is not dedicated", func() {
+				BeforeEach(func() {
+					podManifest := etcdPodManifest(map[string]interface{}{
+						"name":     "etcd-pod1",
+						"nodeName": nodeName,
+						"hostIP":   ip,
+					})
+					nodeManifest := getNodeManifest(nodeName, gb(32), false)
+
+					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+
+					f.RunHook()
+				})
+
+				It("set default backend size as quota-backend-bytes", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(2))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+		})
+
+		Context("etcd has quota-backend-bytes parameter", func() {
+			nodeName := "control-plane-1"
+
+			etcdManifest := func(quotaBackendSize int64) string {
+				data := map[string]interface{}{
+					"maxDbSize": quotaBackendSize,
+					"nodeName":  nodeName,
+					"hostIP":    "192.162.1.1",
 				}
 
+				return etcdPodManifest(data)
+			}
+
+			Context("node has less than 24 Gb", func() {
+				quotaBackend := gb(2)
+				BeforeEach(func() {
+					podManifest := etcdManifest(quotaBackend)
+					nodeManifest := getNodeManifest(nodeName, gb(16), true)
+
+					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+
+					f.RunHook()
+				})
+
+				It("does not change quota-backend-bytes", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(2))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("node has 24 Gb, quota backends is default", func() {
+				BeforeEach(func() {
+					podManifest := etcdManifest(2)
+					nodeManifest := getNodeManifest(nodeName, gb(24), true)
+
+					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+
+					f.RunHook()
+				})
+
+				It("increase on 1 gb of default size as quota-backend-bytes", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(3))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("node has 64 Gb, current backend size is 3 gb, increase node by 40 Gb", func() {
+				BeforeEach(func() {
+					podManifest := etcdManifest(gb(3))
+					nodeManifest := getNodeManifest(nodeName, gb(64), true)
+
+					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+
+					f.RunHook()
+				})
+
+				It("set maximum size for quota-backend-bytes", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(8))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("node has greater than 24 Gb and node is not dedicated", func() {
+				quotaBackend := gb(3)
+
+				BeforeEach(func() {
+					podManifest := etcdManifest(quotaBackend)
+					nodeManifest := getNodeManifest(nodeName, gb(32), false)
+
+					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+
+					f.RunHook()
+				})
+
+				It("set current backend size", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, quotaBackend)
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
 			})
 		})
 
