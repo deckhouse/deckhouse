03467b867a9d4dd9380b924b496bef0bb0a316bc
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2022-04-13 17:22:43 +0300

    ++

--- modules/040-control-plane-manager/hooks/etcd_quota_backend_bytes_test.go
+++ modules/040-control-plane-manager/hooks/etcd_quota_backend_bytes_test.go
@@ -20,14 +20,14 @@ import (
 	"fmt"
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"k8s.io/utils/pointer"
 
 	. "github.com/deckhouse/deckhouse/testing/hooks"
 )
 
-var _ = FDescribe("Modules :: controler-plane-manager :: hooks :: etcd-quota-backend-bytes ::", func() {
+var _ = Describe("Modules :: controler-plane-manager :: hooks :: etcd-quota-backend-bytes ::", func() {
 	Context("CalcNewQuota", func() {
 		casesIncrementalIncrease := []struct {
 			nodeSize, newQuota int64
@@ -335,6 +335,19 @@ status:
 		Expect(metrics[0].Group).To(Equal(etcdBackendBytesGroup))
 	}
 
+	assertAddErrorMetric := func(f *HookExecutionConfig) {
+		metrics := f.MetricsCollector.CollectedMetrics()
+
+		Expect(metrics).To(HaveLen(2))
+
+		Expect(metrics[0].Action).To(Equal("expire"))
+		Expect(metrics[0].Group).To(Equal(etcdBackendBytesGroup))
+
+		Expect(metrics[1].Name).To(Equal("d8_etcd_quota_backend_should_decrease"))
+		Expect(metrics[1].Group).To(Equal(etcdBackendBytesGroup))
+		Expect(metrics[1].Value).To(Equal(pointer.Float64Ptr(1.0)))
+	}
+
 	Context("Single master", func() {
 		Context("etcd does not have quota-backend-bytes parameter", func() {
 			nodeName := "control-plane-0"
@@ -348,7 +361,7 @@ status:
 					})
 					nodeManifest := getNodeManifest(nodeName, gb(8), true)
 
-					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+					JoinKubeResourcesAndSet(f, podManifest, nodeManifest)
 
 					f.RunHook()
 				})
@@ -375,7 +388,7 @@ status:
 					})
 					nodeManifest := getNodeManifest(nodeName, gb(24), true)
 
-					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+					JoinKubeResourcesAndSet(f, podManifest, nodeManifest)
 
 					f.RunHook()
 				})
@@ -402,7 +415,7 @@ status:
 					})
 					nodeManifest := getNodeManifest(nodeName, gb(64), true)
 
-					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+					JoinKubeResourcesAndSet(f, podManifest, nodeManifest)
 
 					f.RunHook()
 				})
@@ -429,7 +442,7 @@ status:
 					})
 					nodeManifest := getNodeManifest(nodeName, gb(32), false)
 
-					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+					JoinKubeResourcesAndSet(f, podManifest, nodeManifest)
 
 					f.RunHook()
 				})
@@ -467,7 +480,7 @@ status:
 					podManifest := etcdManifest(quotaBackend)
 					nodeManifest := getNodeManifest(nodeName, gb(16), true)
 
-					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+					JoinKubeResourcesAndSet(f, podManifest, nodeManifest)
 
 					f.RunHook()
 				})
@@ -490,7 +503,7 @@ status:
 					podManifest := etcdManifest(2)
 					nodeManifest := getNodeManifest(nodeName, gb(24), true)
 
-					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+					JoinKubeResourcesAndSet(f, podManifest, nodeManifest)
 
 					f.RunHook()
 				})
@@ -513,7 +526,7 @@ status:
 					podManifest := etcdManifest(gb(3))
 					nodeManifest := getNodeManifest(nodeName, gb(64), true)
 
-					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+					JoinKubeResourcesAndSet(f, podManifest, nodeManifest)
 
 					f.RunHook()
 				})
@@ -538,7 +551,7 @@ status:
 					podManifest := etcdManifest(quotaBackend)
 					nodeManifest := getNodeManifest(nodeName, gb(32), false)
 
-					JoinKubeResourcesAndSet(f, testETCDSecret, podManifest, nodeManifest)
+					JoinKubeResourcesAndSet(f, podManifest, nodeManifest)
 
 					f.RunHook()
 				})
@@ -555,24 +568,448 @@ status:
 					assertClearMetrics(f)
 				})
 			})
-		})
 
-		Context("Multi-master", func() {
-			_ = func(ips []string, namePrefix, nodePrefix string) []string {
-				res := make([]string, 0, len(ips))
-				for i, ip := range ips {
-					res = append(res, etcdPodManifest(map[string]interface{}{
-						"name":     fmt.Sprintf("%s-%d", namePrefix, i),
-						"nodeName": fmt.Sprintf("%s-%d", nodePrefix, i),
-						"hostIP":   ip,
-					}))
+			Context("all nodes less memory than need for apply current quota-backend-bytes (decrease node case)", func() {
+				quotaBackend := gb(3)
+
+				BeforeEach(func() {
+					podManifest := etcdManifest(quotaBackend)
+					nodeManifest := getNodeManifest(nodeName, gb(8), true)
+
+					JoinKubeResourcesAndSet(f, podManifest, nodeManifest)
+
+					f.RunHook()
+				})
+
+				It("set current backend size", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, quotaBackend)
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertAddErrorMetric(f)
+				})
+			})
+		})
+	})
+
+	Context("Multi-master", func() {
+		type testNode struct {
+			memory   int64
+			etcQuota int64
+			setTaint bool
+		}
+		getManifests := func(nodePrefix string, nodes []testNode) []string {
+			res := make([]string, 0, len(nodes))
+			for i, n := range nodes {
+				node := fmt.Sprintf("%s-%d", nodePrefix, i)
+				data := map[string]interface{}{
+					"name":     fmt.Sprintf("%s-control-plane-%d", node, i),
+					"nodeName": node,
+					"hostIP":   fmt.Sprintf("192.162.0.%d", i),
+				}
+				if n.etcQuota > 0 {
+					data["maxDbSize"] = n.etcQuota
 				}
 
-				return res
+				res = append(res, etcdPodManifest(data))
+				res = append(res, getNodeManifest(node, n.memory, n.setTaint))
 			}
 
-			Context("all instances don't have quota-backend-bytes", func() {
+			return res
+		}
 
+		Context("all etcd instances don't have quota-backend-bytes", func() {
+			Context("all nodes have less than 24 Gb memory", func() {
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-1", []testNode{
+						{
+							memory:   gb(8),
+							etcQuota: 0,
+							setTaint: true,
+						},
+						{
+							memory:   gb(12),
+							etcQuota: 0,
+							setTaint: true,
+						},
+						{
+							memory:   gb(16),
+							etcQuota: 0,
+							setTaint: true,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("set default backend quota", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(2))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("one node have great equal than 24 Gb memory", func() {
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-2", []testNode{
+						{
+							memory:   gb(8),
+							etcQuota: 0,
+							setTaint: true,
+						},
+						{
+							memory:   gb(24),
+							etcQuota: 0,
+							setTaint: true,
+						},
+						{
+							memory:   gb(16),
+							etcQuota: 0,
+							setTaint: true,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("set default backend quota", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(2))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("all nodes have 24 Gb memory", func() {
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-2", []testNode{
+						{
+							memory:   gb(24),
+							etcQuota: 0,
+							setTaint: true,
+						},
+						{
+							memory:   gb(24),
+							etcQuota: 0,
+							setTaint: true,
+						},
+						{
+							memory:   gb(24),
+							etcQuota: 0,
+							setTaint: true,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("increase quota backends on 1 gb (set 3gb)", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(3))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("all nodes have 32 Gb memory, but one not dedicated", func() {
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-2", []testNode{
+						{
+							memory:   gb(32),
+							etcQuota: 0,
+							setTaint: true,
+						},
+						{
+							memory:   gb(32),
+							etcQuota: 0,
+							setTaint: true,
+						},
+						{
+							memory:   gb(32),
+							etcQuota: 0,
+							setTaint: false,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("set default backend size", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(2))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+		})
+
+		Context("one etcd instance has quota-backend-bytes", func() {
+			Context("all nodes have 24 Gb memory but quota-backend-bytes not default on etcd instance", func() {
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-1", []testNode{
+						{
+							memory:   gb(24),
+							etcQuota: 0,
+							setTaint: true,
+						},
+						{
+							memory:   gb(24),
+							etcQuota: gb(1),
+							setTaint: true,
+						},
+						{
+							memory:   gb(24),
+							etcQuota: 0,
+							setTaint: true,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("set backend for 24Gb instance (3gb)", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(3))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+		})
+
+		Context("all etcd instances have quota-backend-bytes", func() {
+			Context("all nodes have less than 24 Gb memory and quota-backend-bytes set to 2gb", func() {
+				etcdQuota := gb(2)
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-1", []testNode{
+						{
+							memory:   gb(16),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+						{
+							memory:   gb(8),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+						{
+							memory:   gb(16),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("does not change quota", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, etcdQuota)
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("all nodes have 24 Gb memory and quota-backend-bytes set to 2gb", func() {
+				etcdQuota := gb(2)
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-1", []testNode{
+						{
+							memory:   gb(24),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+						{
+							memory:   gb(24),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+						{
+							memory:   gb(24),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("increases quota (set 3gb)", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, gb(3))
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("one node have greater than another memory and quota-backend-bytes set to for another nodes", func() {
+				etcdQuota := gb(3)
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-1", []testNode{
+						{
+							memory:   gb(24),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+						{
+							memory:   gb(32),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+						{
+							memory:   gb(24),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("stay current quota backend", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, etcdQuota)
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
+			})
+
+			Context("all nodes less memory than need for apply current quota-backend-bytes (decrease node case)", func() {
+				etcdQuota := gb(3)
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-1", []testNode{
+						{
+							memory:   gb(8),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+						{
+							memory:   gb(8),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+						{
+							memory:   gb(8),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("do not decrease quota backends stay as is", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, etcdQuota)
+				})
+
+				It("add decrease error metric", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertAddErrorMetric(f)
+				})
+			})
+
+			Context("etcd instances have non default quota, but have one not dedicated node", func() {
+				etcdQuota := gb(3)
+				BeforeEach(func() {
+					manifests := getManifests("control-plane-1", []testNode{
+						{
+							memory:   gb(32),
+							etcQuota: etcdQuota,
+							setTaint: false,
+						},
+						{
+							memory:   gb(32),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+						{
+							memory:   gb(32),
+							etcQuota: etcdQuota,
+							setTaint: true,
+						},
+					})
+
+					JoinKubeResourcesAndSet(f, manifests...)
+
+					f.RunHook()
+				})
+
+				It("stay quota backends as is (from instances)", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertNewQuotaBackends(f, etcdQuota)
+				})
+
+				It("clean all metrics in group", func() {
+					Expect(f).Should(ExecuteSuccessfully())
+
+					assertClearMetrics(f)
+				})
 			})
 		})
 	})
