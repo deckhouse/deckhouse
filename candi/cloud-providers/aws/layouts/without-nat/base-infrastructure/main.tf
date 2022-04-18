af46af344d9be6efb237b281216b8337d4053d9c
Konstantin Aksenov <konstantin.aksenov@flant.com>
2022-04-18 15:10:32 +0300

    Revert "[cloud-provider-aws] allow ssh access to master nodes only from bastion for with-nat layout (#1217)" (#1400)
    This reverts commit 8527bd90b654ccc16314d440cf9fda349f2ddd4d.

--- candi/cloud-providers/aws/layouts/without-nat/base-infrastructure/main.tf
+++ candi/cloud-providers/aws/layouts/without-nat/base-infrastructure/main.tf
@@ -13,40 +13,25 @@
 # limitations under the License.
 
 module "vpc" {
-  source          = "../../../terraform-modules/vpc"
-  prefix          = local.prefix
+  source = "../../../terraform-modules/vpc"
+  prefix = local.prefix
   existing_vpc_id = local.existing_vpc_id
-  cidr_block      = local.vpc_network_cidr
-  tags            = local.tags
+  cidr_block = local.vpc_network_cidr
+  tags = local.tags
 }
 
 module "security-groups" {
-  source       = "../../../terraform-modules/security-groups"
-  prefix       = local.prefix
+  source = "../../../terraform-modules/security-groups"
+  prefix = local.prefix
   cluster_uuid = var.clusterUUID
-  vpc_id       = module.vpc.id
-  tags         = local.tags
-}
-
-resource "aws_security_group" "ssh-accessible" {
-  name   = "${local.prefix}-ssh-accessible"
   vpc_id = module.vpc.id
-  tags   = local.tags
-}
-
-resource "aws_security_group_rule" "allow-ssh-for-everyone" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ssh-accessible.id
+  tags = local.tags
 }
 
 data "aws_availability_zones" "available" {}
 
 locals {
-  az_count    = length(data.aws_availability_zones.available.names)
+  az_count = length(data.aws_availability_zones.available.names)
   subnet_cidr = lookup(var.providerClusterConfiguration, "nodeNetworkCIDR", module.vpc.cidr_block)
 }
 
@@ -58,9 +43,9 @@ resource "aws_subnet" "kube_public" {
   map_public_ip_on_launch = true
 
   tags = merge(local.tags, {
-    Name                                       = "${local.prefix}-public-${count.index}"
+    Name = "${local.prefix}-public-${count.index}"
     "kubernetes.io/cluster/${var.clusterUUID}" = "shared"
-    "kubernetes.io/cluster/${local.prefix}"    = "shared"
+    "kubernetes.io/cluster/${local.prefix}" = "shared"
   })
 }
 
@@ -76,9 +61,9 @@ resource "aws_route_table" "kube_public" {
   vpc_id = module.vpc.id
 
   tags = merge(local.tags, {
-    Name                                       = "${local.prefix}-public"
+    Name = "${local.prefix}-public"
     "kubernetes.io/cluster/${var.clusterUUID}" = "shared"
-    "kubernetes.io/cluster/${local.prefix}"    = "shared"
+    "kubernetes.io/cluster/${local.prefix}" = "shared"
   })
 }
 
@@ -144,7 +129,7 @@ resource "aws_iam_instance_profile" "node" {
 }
 
 resource "aws_key_pair" "ssh" {
-  key_name   = local.prefix
+  key_name = local.prefix
   public_key = var.providerClusterConfiguration.sshPublicKey
 
   tags = merge(local.tags, {
