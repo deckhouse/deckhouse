f81aad3413b1a18cea766f4d194fc1b9f18f463d
Andrey Klimentyev <andrey.klimentyev@flant.com>
2022-04-20 13:14:16 +0300

    +

--- ee/fe/modules/500-openvpn/images/easyrsa-migrator/go.mod
+++ ee/fe/modules/500-openvpn/images/easyrsa-migrator/go.mod
@@ -3,6 +3,13 @@ module easyrsa-migrator
 go 1.17
 
 require (
+	github.com/sirupsen/logrus v1.8.1
+	k8s.io/api v0.23.2
+	k8s.io/apimachinery v0.23.2
+	k8s.io/client-go v0.23.2
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.2.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -13,7 +20,6 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/net v0.0.0-20220121210141-e204ce36a2ba // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
@@ -25,9 +31,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-	k8s.io/api v0.23.2 // indirect
-	k8s.io/apimachinery v0.23.2 // indirect
-	k8s.io/client-go v0.23.2 // indirect
 	k8s.io/klog/v2 v2.40.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20220124234850-424119656bbf // indirect
 	k8s.io/utils v0.0.0-20211208161948-7d6a63dca704 // indirect
---
+++ modules/340-monitoring-kubernetes/hooks/remove_deprecated_ebpf_exporter_annotation.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2021 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hooks
+
+import (
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+const (
+	deprecatedEbpfSchedulingLabelKey = "monitoring-kubernetes.deckhouse.io/ebpf-supported"
+)
+
+type NodeWithLabel struct {
+	Name string
+}
+
+func getNodeNameWithSupportedDistro(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	node := &v1.Node{}
+	err := sdk.FromUnstructured(obj, node)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := node.Labels[deprecatedEbpfSchedulingLabelKey]; ok {
+		return &NodeWithLabel{Name: node.Name}, nil
+	}
+
+	return nil, nil
+}
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 10},
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "nodes",
+			ApiVersion: "v1",
+			Kind:       "Node",
+			FilterFunc: getNodeNameWithSupportedDistro,
+		},
+	},
+}, unlabelNodes)
+
+func unlabelNodes(input *go_hook.HookInput) error {
+
+	snapshot := input.Snapshots["nodes"]
+	for _, labeledNodeRaw := range snapshot {
+		if labeledNodeRaw == nil {
+			continue
+		}
+		labeledNode := labeledNodeRaw.(*NodeWithLabel)
+
+		input.PatchCollector.Filter(func(obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+			var node v1.Node
+			err := sdk.FromUnstructured(obj, &node)
+			if err != nil {
+				return nil, err
+			}
+
+			if _, ok := node.Labels[deprecatedEbpfSchedulingLabelKey]; ok {
+				delete(node.Labels, deprecatedEbpfSchedulingLabelKey)
+			}
+
+			return sdk.ToUnstructured(&node)
+		}, "v1", "Node", "", labeledNode.Name)
+	}
+
+	return nil
+}
