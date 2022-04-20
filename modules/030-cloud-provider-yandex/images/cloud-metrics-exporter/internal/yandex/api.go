1fffcb29ced8280cc2779f83d605c2fe95a08481
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2022-04-21 01:58:56 +0300

    ++

--- modules/030-cloud-provider-yandex/images/cloud-metrics-exporter/internal/yandex/api.go
+++ modules/030-cloud-provider-yandex/images/cloud-metrics-exporter/internal/yandex/api.go
@@ -16,7 +16,6 @@ package yandex
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -27,9 +26,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
-	ycsdk "github.com/yandex-cloud/go-sdk"
 	"github.com/yandex-cloud/go-sdk/iamkey"
 )
 
@@ -141,34 +138,6 @@ func (a *CloudApi) InitWithAPIKey(key string) {
 	a.isInit = true
 }
 
-func (a *CloudApi) InitWithServiceAccount(serviceAccount io.Reader) error {
-	if a.isInit {
-		a.logger.Warningln("Yandex cloud api already init")
-		return nil
-	}
-
-	var iamKey iamkey.Key
-	decoder := json.NewDecoder(serviceAccount)
-
-	err := decoder.Decode(&iamKey)
-	if err != nil {
-		return errors.Wrap(err, "malformed service account json")
-	}
-
-	a.iamKey = &iamKey
-
-	err = a.renewToken()
-	if err != nil {
-		return err
-	}
-
-	go a.startAutoRenewToken()
-
-	a.isInit = true
-
-	return nil
-}
-
 func (a *CloudApi) RequestMetrics(ctx context.Context, serviceId string) ([]byte, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, a.url(serviceId), nil)
 	if err != nil {
@@ -227,100 +196,4 @@ func (a *CloudApi) setToken(token string) {
 	defer a.tokenMutex.Unlock()
 
 	a.token = token
-}
-
-func (a *CloudApi) startAutoRenewToken() {
-	a.logger.Info("Start auto renew IAM-token")
-	a.logger.Warn("Stop auto renew IAM-token")
-
-	t := time.NewTicker(a.autoRenewPeriod)
-	defer t.Stop()
-
-	for {
-		select {
-		case <-a.stopCh:
-			return
-		case <-t.C:
-			err := a.renewToken()
-			if err != nil {
-				a.logger.Errorf("Cannot auto-renew IAM-token: %v", err)
-				if a.onRenewError != nil {
-					a.onRenewError()
-				}
-				return
-			}
-		}
-	}
-}
-
-func (a *CloudApi) renewToken() error {
-	a.logger.Info("Start getting new IAM-token")
-
-	rawCreds, err := ycsdk.ServiceAccountKey(a.iamKey)
-	if err != nil {
-		return errors.Wrap(err, "invalid auth credentials")
-	}
-
-	iamCreds, ok := rawCreds.(ycsdk.ExchangeableCredentials)
-	if !ok {
-		return fmt.Errorf("cannot convert rawCreds to ExchangeableCredentials")
-	}
-
-	request, err := iamCreds.IAMTokenRequest()
-	if err != nil {
-		return fmt.Errorf("cannot get IAMToken request: %v", err)
-	}
-
-	jwtForExchange := request.GetJwt()
-
-	token := ""
-	var lastErr error
-
-	for i := 1; i <= retries; i++ {
-		token, lastErr = a.requestToken(jwtForExchange)
-		if lastErr == nil {
-			break
-		}
-	}
-
-	if token == "" {
-		return fmt.Errorf("cannot get IAM-token after %d retries, last error: %v", retries, lastErr)
-	}
-
-	a.setToken(token)
-
-	a.logger.Info("Getting new IAM-token was successfully")
-
-	return nil
-}
-
-func (a *CloudApi) requestToken(jwtForExchange string) (string, error) {
-	body := strings.NewReader(fmt.Sprintf(`{"jwt":"%s"}`, jwtForExchange))
-	req, err := http.NewRequest("POST", "https://iam.api.cloud.yandex.net/iam/v1/tokens", body)
-	if err != nil {
-		return "", err
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := a.client.Do(req)
-	if err != nil {
-		return "", err
-	}
-
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		body, _ := ioutil.ReadAll(resp.Body)
-		panic(fmt.Sprintf("%s: %s", resp.Status, body))
-	}
-
-	var data struct {
-		IAMToken string `json:"iamToken"`
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
-		return "", err
-	}
-
-	return data.IAMToken, err
 }
