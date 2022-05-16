e9249b50d670613ebd906bafabd97ccbe3195092
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2022-05-16 18:17:04 +0300

    ++

--- dhctl/pkg/kubernetes/actions/deckhouse/pod.go
+++ dhctl/pkg/kubernetes/actions/deckhouse/pod.go
@@ -30,7 +30,11 @@ func GetPod(kubeCl *client.KubernetesClient) (*v1.Pod, error) {
 		return nil, ErrListPods
 	}
 
-	if len(pods.Items) != 1 {
+	if len(pods.Items) == 0 {
+		return nil, ErrListPods
+	}
+
+	if len(pods.Items) > 1 {
 		return nil, ErrMultiplePodsFound
 	}
 
