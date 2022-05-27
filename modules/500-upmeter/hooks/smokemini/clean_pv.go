784b47df2d50df1973f2f558c17ef035127db25d
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2022-05-27 22:48:41 +0300

    [Upmeter] Turn off smoke-mini storage class (#1635)
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

---
+++ modules/500-upmeter/hooks/smokemini/clean_pv.go
@@ -0,0 +1,83 @@
+// Copyright 2021 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package smokemini
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/deckhouse/deckhouse/go_lib/dependency"
+	"github.com/deckhouse/deckhouse/go_lib/dependency/k8s"
+)
+
+// migration(smokemini-no-storage-class): Delete obsolete upmeter PV if they are stuck with
+// reclaim retention policy or by any other reason.
+//
+// TODO (shvgn): Delete this hook in Deckhouse v1.34, it is only for migration(smokemini-no-storage-class)
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Schedule: []go_hook.ScheduleConfig{
+		{
+			Name:    "clean_obsolete_pv",
+			Crontab: "33 * * * *",
+		},
+	},
+}, dependency.WithExternalDependencies(func(input *go_hook.HookInput, dc dependency.Container) error {
+	k := dc.MustGetK8sClient()
+	return removeSmokeMiniPersistentVolumes(context.TODO(), k)
+}))
+
+func removeSmokeMiniPersistentVolumes(ctx context.Context, k k8s.Client) error {
+	pvs, err := k.CoreV1().PersistentVolumes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("listing PVs: %v", err)
+	}
+
+	limit := 33 // being gentle
+	for _, pv := range pvs.Items {
+		if name, ok := shouldDeletePV(&pv); ok && limit > 0 {
+			err = k.CoreV1().PersistentVolumes().Delete(ctx, name, metav1.DeleteOptions{})
+			if err != nil {
+				return fmt.Errorf("deleting PV %s: %v", name, err)
+			}
+			limit--
+		}
+	}
+
+	return nil
+}
+
+func shouldDeletePV(pv *v1.PersistentVolume) (string, bool) {
+	// Skip ones which are already terminating
+	if pv.GetDeletionTimestamp() != nil {
+		return "", false
+	}
+
+	// Skip ones not from smoke-mini
+	if pv.Spec.ClaimRef == nil {
+		return "", false
+	}
+	pvcName := pv.Spec.ClaimRef.Name
+	if !strings.HasPrefix(pvcName, "disk-smoke-mini-") {
+		return "", false
+	}
+
+	return pv.GetName(), true
+}
