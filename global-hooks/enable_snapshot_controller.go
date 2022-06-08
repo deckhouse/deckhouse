7a6384cc9940ab7c9ad81ddb10bfc78cfceb7dc6
Andrei Kvapil <kvapss@gmail.com>
2022-06-08 16:03:33 +0200

    [snapshot-controller] Move CRDs to separate module
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

---
+++ global-hooks/enable_snapshot_controller.go
@@ -0,0 +1,53 @@
+// Copyright 2022 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hooks
+
+import (
+	"fmt"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+)
+
+var (
+	supportedModules = []string{
+		"cloudProviderOpenstack",
+		"cloudProviderAws",
+		"cloudProviderGcp",
+		"cloudProviderYandex",
+		"cloudProviderVsphere",
+		"cloudProviderAzure",
+		"linstor",
+	}
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	OnBeforeAll: &go_hook.OrderedConfig{Order: 20},
+}, enableSnapshotController)
+
+func enableSnapshotController(input *go_hook.HookInput) error {
+
+	for _, module := range supportedModules {
+		moduleEnable := fmt.Sprintf("%sEnabled", module)
+		enabled := input.Values.Get(moduleEnable).Bool()
+		if enabled {
+			input.Values.Set("snapshotControllerCrdEnabled", true)
+			input.Values.Set("snapshotControllerEnabled", true)
+			break
+		}
+	}
+
+	return nil
+}
---
+++ global-hooks/enable_snapshot_controller_test.go
@@ -0,0 +1,52 @@
+// Copyright 2021 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hooks
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/deckhouse/deckhouse/testing/hooks"
+)
+
+var _ = Describe("Global hooks :: calculate_resources_requests", func() {
+
+	f := HookExecutionConfigInit(`{}`, `{}`)
+	Context("Cluster without supported modules", func() {
+		BeforeEach(func() {
+			f.RunHook()
+		})
+
+		It("Hook should not enable snapshot-controller, because no supported modules are enabled", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.ValuesGet("snapshotControllerCrdEnabled").Bool()).ToNot(BeTrue())
+			Expect(f.ValuesGet("snapshotControllerEnabled").Bool()).ToNot(BeTrue())
+		})
+	})
+
+	Context("Cluster with linstor enabled", func() {
+		BeforeEach(func() {
+			f.ValuesSet("linstorEnabled", true)
+			f.RunHook()
+		})
+
+		It("Hook should enable snapshot-controller, because linstor is one of supported modules", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.ValuesGet("snapshotControllerCrdEnabled").Bool()).To(BeTrue())
+			Expect(f.ValuesGet("snapshotControllerEnabled").Bool()).To(BeTrue())
+		})
+	})
+
+})
