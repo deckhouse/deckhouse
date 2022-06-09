5481915ee116e5c4fe9521ab2087ab6f3c64e631
Andrey Klimentyev <andrey.klimentyev@flant.com>
2022-06-09 10:17:42 +0300

    [node-manager] Delete preemptible Yandex.Cloud instances early
    Signed-off-by: Andrey Klimentyev <andrey.klimentyev@flant.com>

---
+++ modules/040-node-manager/hooks/yandex_preemptibly_delete_preemtible_instances.go
@@ -0,0 +1,224 @@
+/*
+Copyright 2022 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hooks
+
+import (
+	"fmt"
+	"sort"
+	"time"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+const (
+	preemtibleVMDeletionDuration = 24 * time.Hour
+)
+
+type Machine struct {
+	Name              string
+	CreationTimestamp metav1.Time
+	Terminating       bool
+	MachineClassKind  string
+	MachineClassName  string
+}
+
+type YandexInstanceClass struct {
+	Name        string
+	Preemptible bool
+}
+
+func applyMachineFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	var terminating bool
+	if obj.GetDeletionTimestamp() != nil {
+		terminating = true
+	}
+
+	classKind, _, err := unstructured.NestedString(obj.UnstructuredContent(), "spec", "class", "kind")
+	if err != nil {
+		return nil, fmt.Errorf("can't access class name of Machine %q: %s", obj.GetName(), err)
+	}
+	if len(classKind) == 0 {
+		return nil, fmt.Errorf("spec.class.kind is empty in %q", obj.GetName())
+	}
+
+	className, _, err := unstructured.NestedString(obj.UnstructuredContent(), "spec", "class", "name")
+	if err != nil {
+		return nil, fmt.Errorf("can't access class name of Machine %q: %s", obj.GetName(), err)
+	}
+	if len(className) == 0 {
+		return nil, fmt.Errorf("spec.class.name is empty in %q", obj.GetName())
+	}
+
+	return &Machine{
+		Name:              obj.GetName(),
+		CreationTimestamp: obj.GetCreationTimestamp(),
+		Terminating:       terminating,
+		MachineClassKind:  classKind,
+		MachineClassName:  className,
+	}, nil
+}
+
+func isPreemptibleFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	preemptible, ok, err := unstructured.NestedBool(obj.UnstructuredContent(), "spec", "preemptible")
+	if err != nil {
+		return nil, fmt.Errorf("can't access field \"preemptible\" of YandexInstanceClass %q: %s", obj.GetName(), err)
+	}
+
+	preemptible = ok && preemptible
+
+	return &YandexInstanceClass{
+		Name:        obj.GetName(),
+		Preemptible: preemptible,
+	}, nil
+}
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 10},
+	Queue:        "/modules/cloud-provider-yandex/preemtibly-delete-preemtible-instances",
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "machines",
+			ApiVersion: "machine.sapcloud.io/v1alpha1",
+			Kind:       "Machine",
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"d8-cloud-instance-manager"},
+				},
+			},
+			FilterFunc: applyMachineFilter,
+		},
+		{
+			Name:       "ics",
+			ApiVersion: "deckhouse.io/v1",
+			Kind:       "YandexInstanceClass",
+			FilterFunc: isPreemptibleFilter,
+		},
+	},
+}, deleteMachines)
+
+func deleteMachines(input *go_hook.HookInput) error {
+	timeNow := time.Now().UTC()
+
+	var (
+		icsSnapshot                   = input.Snapshots["ics"]
+		machineSnapshot               = input.Snapshots["machines"]
+		preemptibleInstanceClassesSet = make(map[string]struct{})
+		machines                      []*Machine
+	)
+
+	for _, icRaw := range icsSnapshot {
+		ic, ok := icRaw.(*YandexInstanceClass)
+		if !ok {
+			return fmt.Errorf("failed to assert to *YandexInstanceClass")
+		}
+
+		if ic.Preemptible {
+			preemptibleInstanceClassesSet[ic.Name] = struct{}{}
+		}
+	}
+
+	for _, machineRaw := range machineSnapshot {
+		machine, ok := machineRaw.(*Machine)
+		if !ok {
+			return fmt.Errorf("failed to assert to *Machine")
+		}
+
+		if machine.Terminating {
+			continue
+		}
+
+		if machine.MachineClassKind != "YandexInstanceClass" {
+			continue
+		}
+
+		if _, ok := preemptibleInstanceClassesSet[machine.MachineClassName]; !ok {
+			continue
+		}
+
+		machines = append(machines, machine)
+	}
+
+	if len(machines) == 0 {
+		return nil
+	}
+
+	for _, m := range getMachinesToDelete(timeNow, machines) {
+		input.PatchCollector.Delete("machine.sapcloud.io/v1alpha1", "Machine", "d8-cloud-instance-manager", m)
+	}
+
+	return nil
+}
+
+// delete all after 23h mark
+// afterwards delete in 15 minutes increments, no more than batch size
+func getMachinesToDelete(timeNow time.Time, machines []*Machine) (machinesToDelete []string) {
+	const durationIterations = 12
+	var slidingDuration = preemtibleVMDeletionDuration - time.Hour
+
+	sort.Slice(machines, func(i, j int) bool {
+		return machines[i].CreationTimestamp.Before(&machines[j].CreationTimestamp)
+	})
+
+	batch := len(machines) / durationIterations
+	if batch == 0 {
+		batch = 1
+	}
+
+	var (
+		cursor       int
+		machineCount = len(machines)
+	)
+
+	// short-circuit if there are Machines older than 23 hours
+	for _, m := range machines {
+		if expires(timeNow, m.CreationTimestamp.Time, slidingDuration) {
+			machinesToDelete = append(machinesToDelete, m.Name)
+			cursor++
+		}
+	}
+	if len(machinesToDelete) != 0 {
+		return machinesToDelete
+	}
+
+	for t := 0; t < 12; t++ {
+		slidingDuration -= 15 * time.Minute
+
+		for cursor < machineCount {
+			if len(machinesToDelete) >= batch {
+				break
+			}
+
+			if expires(timeNow, machines[cursor].CreationTimestamp.Time, slidingDuration) {
+				machinesToDelete = append(machinesToDelete, machines[cursor].Name)
+				cursor++
+			} else {
+				break
+			}
+		}
+	}
+
+	return
+}
+
+func expires(now, timestamp time.Time, expirationDuration time.Duration) bool {
+	expirationTs := timestamp.Add(expirationDuration)
+	return now.After(expirationTs)
+}
---
+++ modules/040-node-manager/hooks/yandex_preemptibly_delete_preemtible_instances_test.go
@@ -0,0 +1,172 @@
+/*
+Copyright 2022 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hooks
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/deckhouse/deckhouse/testing/hooks"
+)
+
+var _ = Describe("Modules :: cloud-provider-yandex :: hooks :: preemptibly_delete_preemtible_instances ::", func() {
+	f := HookExecutionConfigInit(`{}`, `{}`)
+	f.RegisterCRD("deckhouse.io", "v1", "YandexInstanceClass", false)
+	f.RegisterCRD("machine.sapcloud.io", "v1alpha1", "Machine", true)
+
+	Context("With no proper Machines", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(generateNGsAndICs()))
+			f.RunHook()
+		})
+
+		It("Should succeed and no machine should be deleted", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "wrong-instance-class").Exists()).To(BeTrue())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "terminating").Exists()).To(BeTrue())
+		})
+	})
+
+	Context("With proper machines", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(generateNGsAndICs(
+				"23h10m", "23h30m", "23h40m", "22h",
+			)))
+			f.RunHook()
+		})
+
+		It("All machines after 23h mark should be deleted", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-0").Exists()).To(BeFalse())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-1").Exists()).To(BeFalse())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-2").Exists()).To(BeFalse())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-3").Exists()).To(BeTrue())
+		})
+	})
+
+	Context("With proper machines", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(generateNGsAndICs(
+				"23h10m", "23h30m", "23h40m", "22h",
+			)))
+			f.RunHook()
+		})
+
+		It("All machines after 23h mark should be deleted", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-0").Exists()).To(BeFalse())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-1").Exists()).To(BeFalse())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-2").Exists()).To(BeFalse())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-3").Exists()).To(BeTrue())
+		})
+	})
+
+	Context("With proper machines", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(generateNGsAndICs(
+				"22h10m", "22h5m", "22h2m", "21h",
+			)))
+			f.RunHook()
+		})
+
+		It("Oldest machine should be deleted", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-0").Exists()).To(BeFalse())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-1").Exists()).To(BeTrue())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-2").Exists()).To(BeTrue())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-3").Exists()).To(BeTrue())
+		})
+	})
+})
+
+func generateNGsAndICs(durationStrings ...string) string {
+	timeNow := time.Now().UTC()
+
+	var offsets []time.Duration
+	for _, d := range durationStrings {
+		duration, err := time.ParseDuration(d)
+		if err != nil {
+			panic(err)
+		}
+
+		offsets = append(offsets, duration)
+	}
+
+	var builder strings.Builder
+	builder.WriteString(`---
+apiVersion: deckhouse.io/v1
+kind: YandexInstanceClass
+metadata:
+  name: test
+spec:
+  preemptible: true
+---
+apiVersion: deckhouse.io/v1
+kind: YandexInstanceClass
+metadata:
+  name: not-preemptible
+spec:
+  preemptible: false
+---
+apiVersion: machine.sapcloud.io/v1alpha1
+kind: Machine
+metadata:
+  name: wrong-instance-class
+  namespace: d8-cloud-instance-manager
+spec:
+  class:
+    kind: AWSInstanceClass
+    name: test
+---
+apiVersion: machine.sapcloud.io/v1alpha1
+kind: Machine
+metadata:
+  name: terminating
+  namespace: d8-cloud-instance-manager
+  deletionTimestamp: "1970-01-01T00:00:00Z"
+spec:
+  class:
+    kind: YandexInstanceClass
+    name: test
+`)
+
+	for i, offset := range offsets {
+		ts, err := timeNow.Add(-offset).MarshalJSON()
+		if err != nil {
+			panic(err)
+		}
+
+		_, _ = builder.WriteString(fmt.Sprintf(`---
+apiVersion: machine.sapcloud.io/v1alpha1
+kind: Machine
+metadata:
+  name: test-%d
+  namespace: d8-cloud-instance-manager
+  creationTimestamp: %s
+spec:
+  class:
+    kind: YandexInstanceClass
+    name: test
+`, i, string(ts)))
+	}
+
+	return builder.String()
+}
