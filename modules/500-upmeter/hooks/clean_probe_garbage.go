684de413f0f46390186dc64e1673399c2bd46796
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2022-07-11 16:17:46 +0300

    earlier crontab
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- modules/500-upmeter/hooks/clean_probe_garbage.go
+++ modules/500-upmeter/hooks/clean_probe_garbage.go
@@ -36,7 +36,7 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 	Schedule: []go_hook.ScheduleConfig{
 		{
 			Name:    "delete_probe_garbage",
-			Crontab: "17 * * * *",
+			Crontab: "25 * * * *",
 		},
 	},
 }, dependency.WithExternalDependencies(
