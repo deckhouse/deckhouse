44cd0a22b57bd6e2c9c61941637121a3f3fb9633
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2022-07-12 15:14:13 +0300

    Added descriptions for nodegroup and nginx groups
    Rephrased other descriptions for uniformity
    
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- modules/500-upmeter/images/status/src/en.ts
+++ modules/500-upmeter/images/status/src/en.ts
@@ -1,49 +1,57 @@
 interface IGroupData {
-	name: string
-	description: string
+  name: string
+  description: string
 }
 
 const defaultGroupData: IGroupData = {
-	name: "TODO",
-	description: "TODO",
-};
+  name: "TODO",
+  description: "TODO",
+}
 
 const known: { [name: string]: IGroupData } = {
-	"control-plane": {
-		name: "Control plane",
-		description: "Cluster control-plane is available. Self-healing is working.",
-	},
-	"synthetic": {
-		name: "Synthetic",
-		description: "Availability of sample application running in cluster.",
-	},
-	"monitoring-and-autoscaling": {
-		name: "Monitoring and Autoscaling",
-		description: "Availability of monitoring and autoscaling applications in the cluster.",
-	},
-	"extensions": {
-		name: "Extensions",
-		description: "Availability of extensions apps.",
-	},
-	"load-balancing": {
-		name: "Load Balancing",
-		description: "Availability of traffic load balancing and its configuration controllers.",
-	},
-	"deckhouse": {
-		name: "Deckhouse",
-		description: "The availability of deckhouse and working hook.",
-	}
-};
+  "control-plane": {
+    name: "Control plane",
+    description: "The availability of Kubernetes control-plane",
+  },
+  "synthetic": {
+    name: "Synthetic",
+    description: "The availability of sample application, and network connectivity",
+  },
+  "monitoring-and-autoscaling": {
+    name: "Monitoring and Autoscaling",
+    description: "The availability of monitoring and autoscaling applications in the cluster",
+  },
+  "extensions": {
+    name: "Extensions",
+    description: "The availability of extensions apps",
+  },
+  "load-balancing": {
+    name: "Load Balancing",
+    description: "The availability of traffic load balancing and its configuration controllers",
+  },
+  "deckhouse": {
+    name: "Deckhouse",
+    description: "The availability of deckhouse and working hook",
+  },
+  "nodegroups": {
+    name: "Node Groups",
+    description: "The availability of CloudEphemeral nodes",
+  },
+  "nginx": {
+    name: "Nginx",
+    description: "The availability of Nginx Ingress Controllers",
+  },
+}
 
 export function getGroupData(name: string): IGroupData {
-	const data = known[name];
+  const data = known[name]
 
-	if (!data) {
-		return {
-			...defaultGroupData,
-			name,
-		};
-	}
+  if (!data) {
+    return {
+      ...defaultGroupData,
+      name,
+    }
+  }
 
-	return data;
+  return data
 }
