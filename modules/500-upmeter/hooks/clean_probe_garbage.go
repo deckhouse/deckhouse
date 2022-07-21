7afcd9918a74e05ccf842a1947187f1797eb9e93
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2022-07-21 13:39:57 +0300

    revert to 15 min
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- modules/500-upmeter/hooks/clean_probe_garbage.go
+++ modules/500-upmeter/hooks/clean_probe_garbage.go
@@ -37,7 +37,7 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 	Schedule: []go_hook.ScheduleConfig{
 		{
 			Name:    "delete_probe_garbage",
-			Crontab: "*/2 * * * *",
+			Crontab: "*/15 * * * *",
 		},
 	},
 }, dependency.WithExternalDependencies(
