a558a27f0548bc3720f9d11a99265ca299f808ad
Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>
2022-07-26 17:45:11 +0300

    [node-local-dns] Revert service account for prevent pod stuck in Terminating state (#2111)
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
    (cherry picked from commit 29398556d90b7bc2924f9c48201433d2e3707e96)

--- modules/040-control-plane-manager/hooks/audit_policy_basic_targets_generated.go
+++ modules/040-control-plane-manager/hooks/audit_policy_basic_targets_generated.go
@@ -87,6 +87,7 @@ var auditPolicyBasicServiceAccounts = []string{
 	"network-policy-engine",
 	"node-exporter",
 	"node-group",
+	"node-local-dns",
 	"node-termination-handler",
 	"okmeter",
 	"openvpn",
