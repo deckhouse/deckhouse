831cadadca8109957a1ce63993834c4018c37eac
Andrei Kvapil <kvapss@gmail.com>
2022-08-02 16:57:20 +0200

    fix threewaymergepatch
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

---
+++ modules/031-linstor/hooks/fix_lastapplied.go
@@ -0,0 +1,112 @@
+/*
+Copyright 2022 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hooks
+
+import (
+	"fmt"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+/*
+Temporary hook, it could be removed after 01.11.2022
+*/
+
+/*
+Piraeus-operator prior v1.9.0 incorrectly set the last applied annotation, which makes thee way
+merge patch not working for removing such keys as nodeAffinity and tollerations. In Deckhouse v1.35
+we moved LINSTOR from master to system nodes, so we should to take care about all affected objects.
+Thuse we just remove them, piraeus-operator will create new ones with correct settings.
+More details here:
+- https://github.com/piraeusdatastore/piraeus-operator/issues/321
+- https://github.com/piraeusdatastore/piraeus-operator/pull/323
+*/
+
+type LinstorDeploymentsSnapshot struct {
+	Name          string
+	Affinity      v1.Affinity
+	Tollertations []v1.Toleration
+}
+
+func applyLinstorDeplotmentsFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	deployment := &appsv1.Deployment{}
+	err := sdk.FromUnstructured(obj, deployment)
+	if err != nil {
+		return nil, fmt.Errorf("cannot convert deployment: %v", err)
+	}
+
+	return &LinstorDeploymentsSnapshot{
+		Name:          deployment.Name,
+		Affinity:      *deployment.Spec.Template.Spec.Affinity,
+		Tollertations: deployment.Spec.Template.Spec.Tolerations,
+	}, nil
+}
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 5},
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "deployments",
+			ApiVersion: "apps/v1",
+			Kind:       "Deployment",
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"d8-linstor"},
+				},
+			},
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{"linstor-controller", "linstor-csi-controller"},
+			},
+			FilterFunc: applyLinstorDeplotmentsFilter,
+		},
+	},
+}, checkAndDeleteWrongResources)
+
+func checkAndDeleteWrongResources(input *go_hook.HookInput) error {
+	snaps := input.Snapshots["deployments"]
+LOOP:
+	for _, snap := range snaps {
+		s := snap.(*LinstorDeploymentsSnapshot)
+		for _, tolleration := range s.Tollertations {
+			if tolleration.Key == "node-role.kubernetes.io/master" {
+				deleteWrongResources(input, s.Name)
+				continue LOOP
+			}
+		}
+		for _, terms := range s.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms {
+			for _, a := range terms.MatchExpressions {
+				if a.Key == "node-role.kubernetes.io/master" {
+					deleteWrongResources(input, s.Name)
+					continue LOOP
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func deleteWrongResources(input *go_hook.HookInput, name string) {
+	input.PatchCollector.Delete("apps/v1", "Deployment", "d8-linstor", name)
+	input.PatchCollector.Delete("piraeus.linbit.com/v1", "LinstorController", "d8-linstor", "linstor")
+	input.PatchCollector.Delete("piraeus.linbit.com/v1", "LinstorCSIDriver", "d8-linstor", "linstor")
+}
---
+++ modules/031-linstor/hooks/fix_lastapplied_test.go
@@ -0,0 +1,217 @@
+/*
+Copyright 2022 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hooks
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/deckhouse/deckhouse/testing/hooks"
+)
+
+var _ = Describe("Modules :: linstor :: hooks :: fix_lastapplied ::", func() {
+	f := HookExecutionConfigInit(initValuesString, initConfigValuesString)
+	f.RegisterCRD("piraeus.linbit.com", "v1", "LinstorController", true)
+	f.RegisterCRD("piraeus.linbit.com", "v1", "LinstorCSIDriver", true)
+
+	Context("Linstor deployments created with affinity on master", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(
+				f.KubeStateSet(`
+---
+apiVersion: piraeus.linbit.com/v1
+kind: LinstorCSIDriver
+metadata:
+  name: linstor
+  namespace: d8-linstor
+---
+apiVersion: piraeus.linbit.com/v1
+kind: LinstorController
+metadata:
+  name: linstor
+  namespace: d8-linstor
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: linstor-controller
+  namespace: d8-linstor
+spec:
+  template:
+    spec:
+      affinity:
+        nodeAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+            nodeSelectorTerms:
+            - matchExpressions:
+              - key: node-role.kubernetes.io/master
+                operator: In
+                values:
+                - ""
+      tolerations:
+      - key: node-role.kubernetes.io/master
+      - key: dedicated.deckhouse.io
+        operator: Exists
+      - key: dedicated
+        operator: Exists
+      - key: ToBeDeletedByClusterAutoscaler
+      - key: node.kubernetes.io/not-ready
+      - key: node.kubernetes.io/out-of-disk
+      - key: node.kubernetes.io/memory-pressure
+      - key: node.kubernetes.io/disk-pressure
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: linstor-csi-controller
+  namespace: d8-linstor
+spec:
+  template:
+    spec:
+      affinity:
+        nodeAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+            nodeSelectorTerms:
+            - matchExpressions:
+              - key: node-role.kubernetes.io/master
+                operator: In
+                values:
+                - ""
+      tolerations:
+      - key: node-role.kubernetes.io/master
+      - key: dedicated.deckhouse.io
+        operator: Exists
+      - key: dedicated
+        operator: Exists
+      - key: ToBeDeletedByClusterAutoscaler
+      - key: node.kubernetes.io/not-ready
+      - key: node.kubernetes.io/out-of-disk
+      - key: node.kubernetes.io/memory-pressure
+      - key: node.kubernetes.io/disk-pressure
+			`),
+				f.GenerateBeforeHelmContext(),
+			)
+			f.RunHook()
+		})
+
+		It("Must delete linstor-controller deployment and CR with nodeAffinity and tolerations set for master", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.KubernetesResource("Deployment", "d8-linstor", "linstor-controller").Exists()).To(BeFalse())
+			Expect(f.KubernetesResource("LinstorController", "d8-linstor", "linstor").Exists()).To(BeFalse())
+		})
+
+		It("Must delete linstor-csi-controller deployment and CR with nodeAffinity and tolerations set for master", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.KubernetesResource("Deployment", "d8-linstor", "linstor-csi-controller").Exists()).To(BeFalse())
+			Expect(f.KubernetesResource("LinstorCSIDriver", "d8-linstor", "linstor").Exists()).To(BeFalse())
+		})
+	})
+
+	Context("Linstor deployments created with affinity on system", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(
+				f.KubeStateSet(`
+---
+apiVersion: piraeus.linbit.com/v1
+kind: LinstorCSIDriver
+metadata:
+  name: linstor
+  namespace: d8-linstor
+---
+apiVersion: piraeus.linbit.com/v1
+kind: LinstorController
+metadata:
+  name: linstor
+  namespace: d8-linstor
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: linstor-controller
+  namespace: d8-linstor
+spec:
+  template:
+    spec:
+      affinity:
+        nodeAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+            nodeSelectorTerms:
+            - matchExpressions:
+              - key: node-role.kubernetes.io/system
+                operator: In
+                values:
+                - ""
+      tolerations:
+      - key: node-role.kubernetes.io/system
+      - key: dedicated.deckhouse.io
+        operator: Exists
+      - key: dedicated
+        operator: Exists
+      - key: ToBeDeletedByClusterAutoscaler
+      - key: node.kubernetes.io/not-ready
+      - key: node.kubernetes.io/out-of-disk
+      - key: node.kubernetes.io/memory-pressure
+      - key: node.kubernetes.io/disk-pressure
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: linstor-csi-controller
+  namespace: d8-linstor
+spec:
+  template:
+    spec:
+      affinity:
+        nodeAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+            nodeSelectorTerms:
+            - matchExpressions:
+              - key: node-role.kubernetes.io/system
+                operator: In
+                values:
+                - ""
+      tolerations:
+      - key: node-role.kubernetes.io/system
+      - key: dedicated.deckhouse.io
+        operator: Exists
+      - key: dedicated
+        operator: Exists
+      - key: ToBeDeletedByClusterAutoscaler
+      - key: node.kubernetes.io/not-ready
+      - key: node.kubernetes.io/out-of-disk
+      - key: node.kubernetes.io/memory-pressure
+      - key: node.kubernetes.io/disk-pressure
+			`),
+				f.GenerateBeforeHelmContext(),
+			)
+			f.RunHook()
+		})
+
+		It("Must keep linstor-controller deployment and CRwith nodeAffinity and tolerations set for system", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.KubernetesResource("Deployment", "d8-linstor", "linstor-controller").Exists()).To(BeTrue())
+			Expect(f.KubernetesResource("LinstorController", "d8-linstor", "linstor").Exists()).To(BeTrue())
+		})
+
+		It("Must keep linstor-csi-controller deployment and CRwith nodeAffinity and tolerations set for system", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.KubernetesResource("Deployment", "d8-linstor", "linstor-csi-controller").Exists()).To(BeTrue())
+			Expect(f.KubernetesResource("LinstorCSIDriver", "d8-linstor", "linstor").Exists()).To(BeTrue())
+		})
+	})
+
+})
