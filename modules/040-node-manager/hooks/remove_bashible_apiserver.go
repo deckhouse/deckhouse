9f56f435fb83962047a7208b1e6d44344644d050
Yuriy Losev <yuriy.losev@flant.com>
2022-08-11 20:24:50 +0400

    [node-manager] Bashbile apiserver migration (#2199)
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>

--- modules/040-node-manager/hooks/remove_bashible_apiserver.go
+++ modules/040-node-manager/hooks/remove_bashible_apiserver.go
@@ -16,6 +16,10 @@ package hooks
 import (
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/utils/pointer"
 )
 
 // this hook is needed only for release 1.34.12
@@ -26,10 +30,63 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 	OnBeforeHelm: &go_hook.OrderedConfig{
 		Order: 5,
 	},
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "bashible-apiserver-remove",
+			ApiVersion: "apps/v1",
+			Kind:       "Deployment",
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{bashibleNamespace},
+				},
+			},
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{bashibleName},
+			},
+			ExecuteHookOnEvents: pointer.BoolPtr(false),
+			FilterFunc:          bashibleDeploymentFilter,
+		},
+	},
 }, removeBashibleHandler)
 
 func removeBashibleHandler(input *go_hook.HookInput) error {
-	input.PatchCollector.Delete("apps/v1", "Deployment", "d8-cloud-instance-manager", "bashible-apiserver")
+	snap := input.Snapshots["bashible-apiserver-remove"]
+	if len(snap) == 0 {
+		return nil
+	}
+
+	dep := snap[0].(migrateBashibleDeployment)
+
+	if dep.NeedReboot {
+		input.PatchCollector.Delete("apps/v1", "Deployment", dep.Namespace, dep.Name)
+	}
 
 	return nil
+}
+
+func bashibleDeploymentFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	var dep v1.Deployment
+	err := sdk.FromUnstructured(obj, &dep)
+	if err != nil {
+		return nil, err
+	}
+
+	needReboot := true
+	if v, ok := dep.Annotations["node.deckhouse.io/migrated"]; ok {
+		if v == "1.34" {
+			needReboot = false
+		}
+	}
+
+	return migrateBashibleDeployment{
+		Namespace:  dep.Namespace,
+		Name:       dep.Name,
+		NeedReboot: needReboot,
+	}, nil
+}
+
+type migrateBashibleDeployment struct {
+	Namespace  string
+	Name       string
+	NeedReboot bool
 }
