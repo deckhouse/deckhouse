f9c1c94a204a16ab393c313efaacb0a9a6f2a94d
Denis Romanenko <denis.romanenko@flant.com>
2022-09-05 15:35:02 +0300

    ++
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- modules/021-cni-cilium/hooks/set_cilium_mode.go
+++ modules/021-cni-cilium/hooks/set_cilium_mode.go
@@ -19,12 +19,13 @@ package hooks
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
+
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
 	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"strings"
 )
 
 type CiliumConfig struct {
