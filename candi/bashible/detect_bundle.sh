cfae4ddba25d372e23d593d58d4929a641594f7d
Andrei Kvapil <kvapss@gmail.com>
2022-09-06 08:10:11 +0200

    [candi] fix detecting bundle (#2378)
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

--- candi/bashible/detect_bundle.sh
+++ candi/bashible/detect_bundle.sh
@@ -12,36 +12,58 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-if [ -e /etc/os-release ]; then
-  . /etc/os-release
-  bundleName="${ID}-${VERSION_ID}"
-  case $bundleName in
-    \(centos|rocky|almalinux|rhel\)-\(7|7.*|8|8.*|9|9.*\))
-      echo "centos"
-      exit 0
-    ;;
-    ubuntu-\(16.04|18.04|20.04|22.04\))
-      echo "ubuntu-lts"
-      exit 0
-    ;;
-    debian-\(9|10|11\))
-      echo "debian"
-      exit 0
-    ;;
-    "-")
-      >&2 echo "ERROR: Can't determine OS! No ID and VERSION_ID in /etc/os-release."
-      exit 1
-    ;;
-  esac
-  # try to determine os by packet manager
-  bundle="debian"
-  if yum -q --version >/dev/null 2>/dev/null; then
-    bundle="centos"
-  fi
-  >&2 echo "WARNING: Trying to use ${bundle} bundle as default for: ${PRETTY_NAME}"
-  echo "${bundle}"
-  exit 0
+if [ ! -e /etc/os-release ]; then
+  >&2 echo "ERROR: Can't determine OS! /etc/os-release is not found."
+  exit 1
 fi
 
->&2 echo "ERROR: Can't determine OS! /etc/os-release is not found."
-exit 1
+. /etc/os-release
+case "$ID" in
+  centos|rocky|almalinux|rhel)
+    case "$VERSION_ID" in 7|7.*|8|8.*|9|9.*)
+      echo "centos" && exit 0 ;;
+    esac
+    >&2 echo "ERROR: ${PRETTY_NAME} is not supported."
+    exit 1
+  ;;
+  ubuntu)
+    case "$VERSION_ID" in 16.04|18.04|20.04|22.04)
+      echo "ubuntu-lts" && exit 0 ;;
+    esac
+    >&2 echo "ERROR: ${PRETTY_NAME} is not supported."
+    exit 1
+  ;;
+  debian)
+    case "$VERSION_ID" in 9|10|11)
+      echo "debian" && exit 0 ;;
+    esac
+    >&2 echo "ERROR: ${PRETTY_NAME} is not supported."
+    exit 1
+  ;;
+  "")
+    >&2 echo "ERROR: Can't determine OS! No ID in /etc/os-release."
+    exit 1
+  ;;
+esac
+
+# try to determine os by ID_LIKE
+for ID in $ID_LIKE; do
+  case "$ID" in
+    centos|rhel)
+      >&2 echo "WARNING: Trying to use centos bundle as default for: ${PRETTY_NAME}"
+      echo "centos" && exit 0
+    ;;
+    debian)
+      >&2 echo "WARNING: Trying to use debian bundle as default for: ${PRETTY_NAME}"
+      echo "debian" && exit 0
+    ;;
+  esac
+done
+
+# try to determine os by packet manager
+bundle="debian"
+if yum -q --version >/dev/null 2>/dev/null; then
+  bundle="centos"
+fi
+>&2 echo "WARNING: Trying to use ${bundle} bundle as default for: ${PRETTY_NAME}"
+echo "${bundle}"
