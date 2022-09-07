631ea3eba5843dc2ac6d93ff1124481616b2ced0
Pavel Tishkov <pavel.tishkov@flant.com>
2022-09-07 18:16:55 +0300

    [istio] add missing license information
    Signed-off-by: Pavel Tishkov <pavel.tishkov@flant.com>

--- ee/modules/110-istio/hooks/internal/testing_template.go
+++ ee/modules/110-istio/hooks/internal/testing_template.go
@@ -1,3 +1,8 @@
+/*
+Copyright 2022 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
 package internal
 
 import (
