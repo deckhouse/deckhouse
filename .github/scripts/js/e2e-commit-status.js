95d4b9e288c11cbe436c419487c9dc1b0072d3bd
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2022-09-07 23:46:13 +0300

    [ci] require e2e tests per PR
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

---
+++ .github/scripts/js/e2e-commit-status.js
@@ -0,0 +1,63 @@
+const {
+  sleep
+} = require('./time');
+
+function workflowUrl({server_url, repository, run_id}) {
+  return `${server_url}/${repository}/actions/runs/${run_id}}`
+}
+
+async function sendCreateCommitStatus({github, context, core, state, description, url}) {
+  for(let i = 0; i < 3; i++) {
+    const response = await github.rest.repos.createCommitStatus({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      sha: context.sha,
+      state: state,
+      description: description,
+      target_url: url,
+      context: 'deckhouse/e2e-requirement'
+    });
+
+    core.debug(`rest.repos.createCommitStatus response: ${JSON.stringify(response)}`);
+    if (response.status === 200) {
+      return true;
+    }
+
+    // wait 3s for retry request
+    await sleep(3000);
+  }
+
+  return false
+}
+
+module.exports.setWait = async ({github, context, core}) => {
+  return sendCreateCommitStatus({
+    github,
+    context,
+    core,
+    state: 'pending',
+    description: 'Waiting for run e2e test.'
+  })
+};
+
+module.exports.setFail = async ({github, context, core}) => {
+  return sendCreateCommitStatus({
+    github,
+    context,
+    core,
+    state: 'failure',
+    description: 'E2e test was failed.',
+    url: workflowUrl(github),
+  })
+};
+
+module.exports.setSuccess = async ({github, context, corel}) => {
+  return sendCreateCommitStatus({
+    github,
+    context,
+    core,
+    state: 'success',
+    description: 'E2e test was passed.',
+    url: workflowUrl(github),
+  })
+};
