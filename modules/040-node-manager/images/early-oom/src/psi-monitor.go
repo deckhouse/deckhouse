af08bbc6f0aa7a1bc4762377173a7ac20190c622
Andrey Klimentyev <andrey.klimentyev@flant.com>
2022-09-16 10:20:37 +0300

    [node-manager] Fail early-oom gracefully (#2451)
    Signed-off-by: Andrey Klimentyev <andrey.klimentyev@flant.com>

--- modules/040-node-manager/images/early-oom/src/psi-monitor.go
+++ modules/040-node-manager/images/early-oom/src/psi-monitor.go
@@ -17,12 +17,17 @@ limitations under the License.
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/procfs"
 )
 
@@ -35,16 +40,42 @@ const (
 	sysrqOOMCharacter = "f"
 )
 
+var psiUnavailable = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "early_oom_psi_unavailable",
+	Help: "Whether PSI subsystem is unavailable on a given system",
+})
+
+func init() {
+	prometheus.MustRegister(psiUnavailable)
+}
+
 func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	server := &http.Server{Addr: "127.0.0.1:8080"}
+	http.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	psiSupportErr := probePSISupport()
+	if psiSupportErr != nil {
+		log.Println(psiSupportErr)
+		psiUnavailable.Set(1)
+
+		sig := <-c
+		shutdown(sig, server)
+	}
 
 	t := time.NewTimer(0)
 	for {
 		select {
 		case sig := <-c:
-			log.Printf("Caught signal %s, exiting", sig.String())
-			os.Exit(0)
+			shutdown(sig, server)
 		case <-t.C:
 			if iteration() {
 				t.Reset(recoveryInterval)
@@ -55,6 +86,25 @@ func main() {
 	}
 }
 
+func probePSISupport() error {
+	fs, err := procfs.NewDefaultFS()
+	if err != nil {
+		return err
+	}
+
+	_, err = fs.PSIStatsForResource("memory")
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return errors.New("pressure information is unavailable, you need a Linux kernel >= 4.20 and/or CONFIG_PSI enabled for your kernel")
+		}
+		if errors.Is(err, syscall.ENOTSUP) {
+			return errors.New("pressure information is disabled, add psi=1 kernel command line to enable it")
+		}
+	}
+
+	return nil
+}
+
 func iteration() bool {
 	fs, err := procfs.NewDefaultFS()
 	if err != nil {
@@ -63,7 +113,12 @@ func iteration() bool {
 
 	stats, err := fs.PSIStatsForResource("memory")
 	if err != nil {
-		log.Fatal(err)
+		if errors.Is(err, os.ErrNotExist) {
+			log.Println("Pressure information is unavailable, you need a Linux kernel >= 4.20 and/or CONFIG_PSI enabled for your kernel")
+		}
+		if errors.Is(err, syscall.ENOTSUP) {
+			log.Println("pressure information is disabled, add psi=1 kernel command line to enable it")
+		}
 	}
 
 	if stats.Full.Avg10 > memoryThreshold {
@@ -83,4 +138,17 @@ func iteration() bool {
 
 func triggerSystemOOM(sysrqTriggerFile string) error {
 	return os.WriteFile(sysrqTriggerFile, []byte(sysrqOOMCharacter), 0666)
+}
+
+func shutdown(sig os.Signal, server *http.Server) {
+	log.Printf("Caught signal %s, exiting", sig.String())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Failed to gracefully shutdown http server: %s", err)
+	}
+
+	os.Exit(0)
 }
