9cacc83647d13c4053dc33870ad9e50ee3c1e04a
Pavel Tishkov <pavel.tishkov@flant.com>
2022-09-22 17:18:33 +0300

    [istio] add alert for deprecated tlsMode param
    Signed-off-by: Pavel Tishkov <pavel.tishkov@flant.com>

---
+++ ee/modules/110-istio/hooks/deprecated_module_params_monitoring.go
@@ -0,0 +1,35 @@
+/*
+Copyright 2022 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package hooks
+
+import (
+	"github.com/deckhouse/deckhouse/ee/modules/110-istio/hooks/internal"
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook/metrics"
+	"github.com/flant/addon-operator/sdk"
+)
+
+const (
+	deprecatedModuleParamMetricName             = "d8_istio_deprecated_module_param"
+	deprecatedModuleParamMonitoringMetricsGroup = "deprecated_module_param"
+	istioTlsModePath                            = "istio.tlsMode"
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Queue:        internal.Queue("deprecated-parameters-monitoring"),
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 10},
+}, getRidOfDeprecatedParams)
+
+func getRidOfDeprecatedParams(input *go_hook.HookInput) error {
+	input.MetricsCollector.Expire(deprecatedModuleParamMonitoringMetricsGroup)
+	if input.ConfigValues.Get(istioTlsModePath).Exists() {
+		labels := map[string]string{
+			"param": "tlsMode",
+		}
+		input.MetricsCollector.Set(deprecatedModuleParamMetricName, 1, labels, metrics.WithGroup(deprecatedModuleParamMonitoringMetricsGroup))
+	}
+	return nil
+}
