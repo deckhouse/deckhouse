c5f3762208f32e96c7030c5595fc77e386ea2e55
Denis Romanenko <denis.romanenko@flant.com>
2022-09-28 16:24:47 +0300

    [metallb] bump version to 0.13.5 and migrate to crds
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- ee/modules/380-metallb/template_tests/module_test.go
+++ ee/modules/380-metallb/template_tests/module_test.go
@@ -33,9 +33,15 @@ const (
         matchLabels:
         - node: metallb`
 
-	addressPools = `
+	addressPoolsBGP = `
     - name: mypool
       protocol: bgp
+      addresses:
+      - 192.168.0.0/24`
+
+	addressPoolsL2 = `
+    - name: mypool
+      protocol: layer2
       addresses:
       - 192.168.0.0/24`
 )
@@ -51,37 +57,73 @@ var _ = Describe("Module :: metallb :: helm template ::", func() {
 		f.ValuesSet("metallb.speaker.nodeSelector.mylabel", "myvalue")
 	})
 
-	Context("bgpPeers and addressPools are set", func() {
+	Context("bgpPeers and addressPools in BGP mode are set", func() {
 		BeforeEach(func() {
 			f.ValuesSetFromYaml("metallb.bgpPeers", bgpPeers)
-			f.ValuesSetFromYaml("metallb.addressPools", addressPools)
+			f.ValuesSetFromYaml("metallb.addressPools", addressPoolsBGP)
 			f.HelmRender()
 		})
 
-		It("Should create a ConfigMap `config` with our values", func() {
+		It("Should create a resources with our values", func() {
 			Expect(f.RenderError).ShouldNot(HaveOccurred())
 
-			crb := f.KubernetesResource("ConfigMap", "d8-metallb", "config")
-			Expect(crb.Exists()).To(BeTrue())
+			ipAddressPool := f.KubernetesResource("IPAddressPool", "d8-metallb", "mypool")
+			Expect(ipAddressPool.Exists()).To(BeTrue())
 
-			Expect(crb.Field("data.config").String()).To(MatchYAML(`
-peers:
-- hold-time: 3s
-  my-asn: 64000
-  peer-address: 1.1.1.1
-  peer-asn: 65000
-- hold-time: 3s
-  my-asn: 64000
-  peer-address: 1.1.1.2
-  peer-asn: 65000
-address-pools:
-- addresses:
-  - 192.168.0.0/24
-  name: mypool
-  protocol: bgp
+			Expect(ipAddressPool.Field("spec.addresses").String()).To(MatchYAML(`
+- 192.168.0.0/24
 `))
+
+			bgpPeer0 := f.KubernetesResource("BGPPeer", "d8-metallb", "bgp-peer-0")
+			Expect(bgpPeer0.Exists()).To(BeTrue())
+			Expect(bgpPeer0.Field("spec").String()).To(MatchYAML(`
+holdTime: 3s
+myASN: 64000
+peerASN: 65000
+peerAddress: 1.1.1.1
+`))
+
+			bgpPeer1 := f.KubernetesResource("BGPPeer", "d8-metallb", "bgp-peer-1")
+			Expect(bgpPeer1.Exists()).To(BeTrue())
+			Expect(bgpPeer1.Field("spec").String()).To(MatchYAML(`
+holdTime: 3s
+myASN: 64000
+nodeSelectors:
+- matchLabels:
+  - node: metallb
+peerASN: 65000
+peerAddress: 1.1.1.2
+`))
+			bgpAdvertisement := f.KubernetesResource("BGPAdvertisement", "d8-metallb", "mypool")
+			Expect(bgpAdvertisement.Exists()).To(BeTrue())
+
+			l2Advertisement := f.KubernetesResource("L2Advertisement", "d8-metallb", "mypool")
+			Expect(l2Advertisement.Exists()).To(BeFalse())
+
+		})
+	})
+	Context("addressPools in L2 mode are set", func() {
+		BeforeEach(func() {
+			f.ValuesSetFromYaml("metallb.addressPools", addressPoolsL2)
+			f.HelmRender()
 		})
 
-	})
+		It("Should create a resources with our values", func() {
+			Expect(f.RenderError).ShouldNot(HaveOccurred())
 
+			ipAddressPool := f.KubernetesResource("IPAddressPool", "d8-metallb", "mypool")
+			Expect(ipAddressPool.Exists()).To(BeTrue())
+
+			Expect(ipAddressPool.Field("spec.addresses").String()).To(MatchYAML(`
+- 192.168.0.0/24
+`))
+
+			bgpAdvertisement := f.KubernetesResource("BGPAdvertisement", "d8-metallb", "mypool")
+			Expect(bgpAdvertisement.Exists()).To(BeFalse())
+
+			l2Advertisement := f.KubernetesResource("L2Advertisement", "d8-metallb", "mypool")
+			Expect(l2Advertisement.Exists()).To(BeTrue())
+
+		})
+	})
 })
