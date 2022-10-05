abb4b08b3519ba72b810230f70c81df7cd543b68
deckhouse-BOaTswain <89150800+deckhouse-BOaTswain@users.noreply.github.com>
2022-10-05 16:58:45 +0300

    [node-manager] Preserve old control-plane label for compatibility (#2635) (#2669)
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>
    
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>
    Co-authored-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- modules/040-control-plane-manager/hooks/get_pki_checksum_test.go
+++ modules/040-control-plane-manager/hooks/get_pki_checksum_test.go
@@ -23,7 +23,7 @@ import (
 	. "github.com/deckhouse/deckhouse/testing/hooks"
 )
 
-var _ = Describe("Modules :: controler-plane-manager :: hooks :: get_pki_checksum ::", func() {
+var _ = Describe("Modules :: control-plane-manager :: hooks :: get_pki_checksum ::", func() {
 	const (
 		stateSecret = `
 ---
