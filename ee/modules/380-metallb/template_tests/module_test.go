55aed94cd46a5bba9600a07e0f8502ab07942eca
Denis Romanenko <denis.romanenko@flant.com>
2022-10-19 20:08:19 +0300

    ++
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- ee/modules/380-metallb/template_tests/module_test.go
+++ ee/modules/380-metallb/template_tests/module_test.go
@@ -21,29 +21,62 @@ func Test(t *testing.T) {
 
 const (
 	bgpPeers = `
-    - peer-address: 1.1.1.1
-      peer-asn: 65000
-      my-asn: 64000
-      hold-time: 3s
-    - peer-address: 1.1.1.2
-      peer-asn: 65000
-      my-asn: 64000
-      hold-time: 3s
-      node-selector:
-        matchLabels:
-          node: "metallb"`
+- peer-address: 1.1.1.1
+  peer-asn: 65000
+  my-asn: 64000
+  hold-time: 3s
+- peer-address: 1.1.1.2
+  peer-asn: 65000
+  my-asn: 64000
+  hold-time: 3s
+  node-selector:
+    matchLabels:
+      node: "metallb"
+`
 
 	addressPoolsBGP = `
-    - name: mypool
-      protocol: bgp
-      addresses:
-      - 192.168.0.0/24`
+- name: mypool1
+  protocol: bgp
+  auto-assign: true
+  avoid-buggy-ips: false
+  addresses:
+  - 192.168.0.0/24
+- name: mypool2
+  protocol: bgp
+  addresses:
+  - 192.68.1.1-192.168.1.255
+  auto-assign: false
+  avoid-buggy-ips: true
+  bgp-advertisements:
+  - aggregation-length: 32
+    localpref: 100
+    communities:
+    - comm1
+    - comm2
+  - aggregation-length: 32
+    localpref: 150
+`
 
 	addressPoolsL2 = `
-    - name: mypool
-      protocol: layer2
-      addresses:
-      - 192.168.0.0/24`
+- name: mypool1
+  auto-assign: false
+  avoid-buggy-ips: true
+  protocol: layer2
+  addresses:
+  - 192.168.0.0/24
+- name: mypool2
+  auto-assign: true
+  avoid-buggy-ips: false
+  protocol: layer2
+  addresses:
+  - 192.168.1.0-192.168.1.255
+`
+
+	bgpCommunities = `
+comm1: 65535:65282
+comm2: 1111:1111
+unusable: 2222:2222
+`
 )
 
 var _ = Describe("Module :: metallb :: helm template ::", func() {
@@ -61,17 +94,29 @@ var _ = Describe("Module :: metallb :: helm template ::", func() {
 		BeforeEach(func() {
 			f.ValuesSetFromYaml("metallb.bgpPeers", bgpPeers)
 			f.ValuesSetFromYaml("metallb.addressPools", addressPoolsBGP)
+			f.ValuesSetFromYaml("metallb.bgpCommunities", bgpCommunities)
 			f.HelmRender()
 		})
 
 		It("Should create a resources with our values", func() {
 			Expect(f.RenderError).ShouldNot(HaveOccurred())
 
-			ipAddressPool := f.KubernetesResource("IPAddressPool", "d8-metallb", "mypool")
-			Expect(ipAddressPool.Exists()).To(BeTrue())
-
-			Expect(ipAddressPool.Field("spec.addresses").String()).To(MatchYAML(`
+			ipAddressPool1 := f.KubernetesResource("IPAddressPool", "d8-metallb", "mypool1")
+			Expect(ipAddressPool1.Exists()).To(BeTrue())
+			Expect(ipAddressPool1.Field("spec").String()).To(MatchYAML(`
+addresses:
 - 192.168.0.0/24
+autoAssign: true
+avoidBuggyIPs: false
+`))
+
+			ipAddressPool2 := f.KubernetesResource("IPAddressPool", "d8-metallb", "mypool2")
+			Expect(ipAddressPool2.Exists()).To(BeTrue())
+			Expect(ipAddressPool2.Field("spec").String()).To(MatchYAML(`
+addresses:
+- 192.68.1.1-192.168.1.255
+autoAssign: false
+avoidBuggyIPs: true
 `))
 
 			bgpPeer0 := f.KubernetesResource("BGPPeer", "d8-metallb", "bgp-peer-0")
@@ -94,11 +139,39 @@ nodeSelectors:
 peerASN: 65000
 peerAddress: 1.1.1.2
 `))
-			bgpAdvertisement := f.KubernetesResource("BGPAdvertisement", "d8-metallb", "mypool")
-			Expect(bgpAdvertisement.Exists()).To(BeTrue())
+			bgpAdvertisement1 := f.KubernetesResource("BGPAdvertisement", "d8-metallb", "mypool1-0")
+			Expect(bgpAdvertisement1.Exists()).To(BeTrue())
+			Expect(bgpAdvertisement1.Field("spec").String()).To(MatchYAML(`
+ipAddressPools:
+- mypool1
+`))
 
-			l2Advertisement := f.KubernetesResource("L2Advertisement", "d8-metallb", "mypool")
-			Expect(l2Advertisement.Exists()).To(BeFalse())
+			bgpAdvertisement2 := f.KubernetesResource("BGPAdvertisement", "d8-metallb", "mypool2-0")
+			Expect(bgpAdvertisement2.Exists()).To(BeTrue())
+			Expect(bgpAdvertisement2.Field("spec").String()).To(MatchYAML(`
+aggregationLength: 32
+communities:
+- 65535:65282
+- 1111:1111
+ipAddressPools:
+- mypool2
+localPref: 100
+`))
+
+			bgpAdvertisement3 := f.KubernetesResource("BGPAdvertisement", "d8-metallb", "mypool2-1")
+			Expect(bgpAdvertisement3.Exists()).To(BeTrue())
+			Expect(bgpAdvertisement3.Field("spec").String()).To(MatchYAML(`
+aggregationLength: 32
+ipAddressPools:
+- mypool2
+localPref: 150
+`))
+
+			l2Advertisement1 := f.KubernetesResource("L2Advertisement", "d8-metallb", "mypool1")
+			Expect(l2Advertisement1.Exists()).To(BeFalse())
+
+			l2Advertisement2 := f.KubernetesResource("L2Advertisement", "d8-metallb", "mypool2")
+			Expect(l2Advertisement2.Exists()).To(BeFalse())
 
 		})
 	})
@@ -111,18 +184,43 @@ peerAddress: 1.1.1.2
 		It("Should create a resources with our values", func() {
 			Expect(f.RenderError).ShouldNot(HaveOccurred())
 
-			ipAddressPool := f.KubernetesResource("IPAddressPool", "d8-metallb", "mypool")
-			Expect(ipAddressPool.Exists()).To(BeTrue())
-
-			Expect(ipAddressPool.Field("spec.addresses").String()).To(MatchYAML(`
+			ipAddressPool1 := f.KubernetesResource("IPAddressPool", "d8-metallb", "mypool1")
+			Expect(ipAddressPool1.Exists()).To(BeTrue())
+			Expect(ipAddressPool1.Field("spec").String()).To(MatchYAML(`
+addresses:
 - 192.168.0.0/24
+autoAssign: false
+avoidBuggyIPs: true
 `))
 
-			bgpAdvertisement := f.KubernetesResource("BGPAdvertisement", "d8-metallb", "mypool")
-			Expect(bgpAdvertisement.Exists()).To(BeFalse())
+			ipAddressPool2 := f.KubernetesResource("IPAddressPool", "d8-metallb", "mypool2")
+			Expect(ipAddressPool2.Exists()).To(BeTrue())
+			Expect(ipAddressPool2.Field("spec").String()).To(MatchYAML(`
+addresses:
+- 192.168.1.0-192.168.1.255
+autoAssign: true
+avoidBuggyIPs: false
+`))
 
-			l2Advertisement := f.KubernetesResource("L2Advertisement", "d8-metallb", "mypool")
-			Expect(l2Advertisement.Exists()).To(BeTrue())
+			bgpAdvertisement1 := f.KubernetesResource("BGPAdvertisement", "d8-metallb", "mypool1-0")
+			Expect(bgpAdvertisement1.Exists()).To(BeFalse())
+
+			bgpAdvertisement2 := f.KubernetesResource("BGPAdvertisement", "d8-metallb", "mypool2-0")
+			Expect(bgpAdvertisement2.Exists()).To(BeFalse())
+
+			l2Advertisement1 := f.KubernetesResource("L2Advertisement", "d8-metallb", "mypool1")
+			Expect(l2Advertisement1.Exists()).To(BeTrue())
+			Expect(l2Advertisement1.Field("spec").String()).To(MatchYAML(`
+ipAddressPools:
+- mypool1
+`))
+
+			l2Advertisement2 := f.KubernetesResource("L2Advertisement", "d8-metallb", "mypool2")
+			Expect(l2Advertisement2.Exists()).To(BeTrue())
+			Expect(l2Advertisement2.Field("spec").String()).To(MatchYAML(`
+ipAddressPools:
+- mypool2
+`))
 
 		})
 	})
