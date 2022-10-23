2c8249687347a686f177b13ce32f6abd5beab01f
Yuriy Losev <yuriy.losev@flant.com>
2022-10-24 00:22:11 +0400

    [node-group] Fix remove taints patch from master NG (#2809)
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>
    Co-authored-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

--- modules/040-node-manager/hooks/handle_node_templates_test.go
+++ modules/040-node-manager/hooks/handle_node_templates_test.go
@@ -697,4 +697,101 @@ spec:
 
 		})
 	})
+
+	Context("Single node cluster: master with control-plane taint", func() {
+		BeforeEach(func() {
+			state := `
+---
+apiVersion: deckhouse.io/v1
+kind: NodeGroup
+metadata:
+  name: master
+spec:
+  nodeTemplate:
+    labels:
+      node-role.kubernetes.io/control-plane: ""
+      node-role.kubernetes.io/master: ""
+  nodeType: CloudPermanent
+---
+apiVersion: v1
+kind: Node
+metadata:
+  annotations:
+    node-manager.deckhouse.io/last-applied-node-template: '{"annotations":{},"labels":{"node-role.kubernetes.io/control-plane":"","node-role.kubernetes.io/master":""},"taints":[{"key":"node-role.kubernetes.io/control-plane","effect":"NoSchedule"}]}'
+    node.deckhouse.io/configuration-checksum: 3ef180a2b2cce73299012a049437bfef447b031ccfbb6c7d26913124a9ac1c1e
+  labels:
+    kubernetes.io/hostname: kube-master-0
+    node-role.kubernetes.io/control-plane: ""
+    node-role.kubernetes.io/master: ""
+    node.deckhouse.io/group: master
+    node.deckhouse.io/type: CloudPermanent
+  name: kube-master-0
+spec:
+  podCIDR: 10.111.0.0/24
+  podCIDRs:
+  - 10.111.0.0/24
+  providerID: aws:///eu-central-1a/i-05724e80e8b61b339
+  taints:
+  - effect: NoSchedule
+    key: node-role.kubernetes.io/control-plane
+`
+			f.BindingContexts.Set(f.KubeStateSet(state))
+			f.RunHook()
+		})
+
+		It("After patching ng (removing taints), node should not have taints", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			taints := f.KubernetesGlobalResource("Node", "kube-master-0").Parse().Get("spec.taints")
+			Expect(taints.Array()).To(HaveLen(0))
+		})
+	})
+
+	Context("Single node cluster: master with both taints", func() {
+		BeforeEach(func() {
+			state := `
+---
+apiVersion: deckhouse.io/v1
+kind: NodeGroup
+metadata:
+  name: master
+spec:
+  nodeTemplate:
+    labels:
+      node-role.kubernetes.io/control-plane: ""
+      node-role.kubernetes.io/master: ""
+  nodeType: CloudPermanent
+---
+apiVersion: v1
+kind: Node
+metadata:
+  annotations:
+    node-manager.deckhouse.io/last-applied-node-template: '{"annotations":{},"labels":{"node-role.kubernetes.io/control-plane":"","node-role.kubernetes.io/master":""},"taints":[{"key":"node-role.kubernetes.io/control-plane","effect":"NoSchedule"}]}'
+    node.deckhouse.io/configuration-checksum: 3ef180a2b2cce73299012a049437bfef447b031ccfbb6c7d26913124a9ac1c1e
+  labels:
+    kubernetes.io/hostname: kube-master-0
+    node-role.kubernetes.io/control-plane: ""
+    node-role.kubernetes.io/master: ""
+    node.deckhouse.io/group: master
+    node.deckhouse.io/type: CloudPermanent
+  name: kube-master-0
+spec:
+  podCIDR: 10.111.0.0/24
+  podCIDRs:
+  - 10.111.0.0/24
+  providerID: aws:///eu-central-1a/i-05724e80e8b61b339
+  taints:
+  - effect: NoSchedule
+    key: node-role.kubernetes.io/control-plane
+  - effect: NoSchedule
+    key: node-role.kubernetes.io/master
+`
+			f.BindingContexts.Set(f.KubeStateSet(state))
+			f.RunHook()
+		})
+
+		It("Node should not have taints", func() {
+			taints := f.KubernetesGlobalResource("Node", "kube-master-0").Parse().Get("spec.taints")
+			Expect(taints.Array()).To(HaveLen(0))
+		})
+	})
 })
