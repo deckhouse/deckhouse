42c770f9ef51b0d8acf21ee0f75e90009ac5928f
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2022-10-31 13:14:01 +0300

    go
    Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

--- modules/300-prometheus/hooks/calculate_storage_capacity.go
+++ modules/300-prometheus/hooks/calculate_storage_capacity.go
@@ -18,13 +18,14 @@ package hooks
 
 import (
 	"fmt"
+	"math"
+	"strings"
+
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
 	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"math"
-	"strings"
 )
 
 const defaultDiskSizeGiB = 40
