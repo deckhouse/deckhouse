57b6edd1d6cda16e1f2c7d6e3372260f54280851
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2022-11-08 13:07:22 +0300

    go
    Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

--- modules/099-ceph-csi/tools/rbd-in-tree-to-ceph-csi-migration-helper.sh
+++ modules/099-ceph-csi/tools/rbd-in-tree-to-ceph-csi-migration-helper.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-sample_pvc_name="$1"
-target_pvc_name="$2"
+if [ "$#" != "2" ] || ! grep -qs "/" <<< "$1" || ! grep -qs "/" <<< "$2"; then
+  echo "Not enough arguments passed or namespace is not specified."
+  echo "Usage: ./rbd-in-tree-to-ceph-csi-migration-helper.sh <namespace>/<sample-pvc-name> <namespace>/<target-pvc-name>"
+  exit 1
+fi
 
-sample_pvc="$(kubectl get pvc "$sample_pvc_name" -o json)"
-target_pvc="$(kubectl get pvc "$target_pvc_name" -o json)"
+sample_pvc_namespace="$(echo -n "$1" | awk '{gsub("/"," ");print $1}')"
+target_pvc_namespace="$(echo -n "$2" | awk '{gsub("/"," ");print $1}')"
+
+sample_pvc_name="$(echo -n "$1" | awk '{gsub("/"," ");print $2}')"
+target_pvc_name="$(echo -n "$2" | awk '{gsub("/"," ");print $2}')"
+
+
+sample_pvc="$(kubectl -n "$sample_pvc_namespace" get pvc "$sample_pvc_name" -o json)"
+target_pvc="$(kubectl -n "$target_pvc_namespace" get pvc "$target_pvc_name" -o json)"
 
 sample_pv_name="$(jq -r '.spec.volumeName' <<< "$sample_pvc")"
 target_pv_name="$(jq -r '.spec.volumeName' <<< "$target_pvc")"
@@ -34,7 +44,7 @@ new_target_pvc="$(jq --argjson a "$new_annotations_for_target_pvc" --arg sc "$ne
 
 while true; do
   msg="rbd mv $pool_name/$original_rbd_image_name $pool_name/$new_rbd_image_name
-Rename the rbd image in the ceph cluster and type \"yes\": "
+Rename rbd image in ceph cluster and type yes to confirm: "
   read -p "$msg" confirm
   if [ "$confirm" == "yes" ]; then
     break
@@ -42,14 +52,14 @@ Rename the rbd image in the ceph cluster and type \"yes\": "
 done
 
 
-echo "kubectl delete pvc $target_pvc_name
+echo "kubectl -n $target_pvc_namespace delete pvc $target_pvc_name
 kubectl delete pv $target_pv_name"
 
 while true; do
   read -p "PVC $target_pvc_name and PV $target_pv_name will be removed (Type yes to confirm): " confirm
   if [ "$confirm" == "yes" ]; then
-    kubectl delete pvc $target_pvc_name
-    kubectl delete pv $target_pv_name
+    kubectl -n "$target_pvc_namespace" delete pvc "$target_pvc_name"
+    kubectl delete pv "$target_pv_name"
     break
   fi
 done
@@ -69,7 +79,7 @@ END
   fi
 done
 
-new_target_pvc="$(kubectl get pvc "$target_pvc_name" -o json)"
+new_target_pvc="$(kubectl -n "$target_pvc_namespace" get pvc "$target_pvc_name" -o json)"
 new_target_pvc_metadata="$(jq -r '.metadata' <<< "$new_target_pvc")"
 
 new_target_pv="$(jq --argjson m "$new_target_pvc_metadata" --argjson a "$new_annotations_for_target_pv" --argjson csi "$csi_section_for_target_pv" --arg sc "$new_storage_class_for_target" --arg sampleRbdImageUid "$sample_rbd_image_uid" --arg newRbdImageUid "$new_rbd_image_uid" '
