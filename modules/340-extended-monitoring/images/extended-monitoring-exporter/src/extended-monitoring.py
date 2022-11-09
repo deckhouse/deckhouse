9d0be90e06bed5ff0e71c61699112883c0ebd050
Maksim Nabokikh <maksim.nabokikh@flant.com>
2022-11-09 08:33:17 +0100

    Update modules/340-extended-monitoring/images/extended-monitoring-exporter/src/extended-monitoring.py
    Co-authored-by: Andrey Klimentyev <andrey.klimentyev@flant.com>
    Signed-off-by: Maksim Nabokikh <max.nabokih@gmail.com>

--- modules/340-extended-monitoring/images/extended-monitoring-exporter/src/extended-monitoring.py
+++ modules/340-extended-monitoring/images/extended-monitoring-exporter/src/extended-monitoring.py
@@ -305,7 +305,7 @@ class GetHandler(BaseHTTPRequestHandler):
 
         if self.path == "/healthz":
             # Fail if metrics were last collected more than 15 minutes ago
-            self.send_response(200 if self.__class__._last_observe > (datetime.now() - timedelta(minutes=15)) else 400)
+            self.send_response(200 if self.__class__._last_observe > (datetime.now() - timedelta(minutes=15)) else 500)
             self.end_headers()
             logging.info('Last observed time: {}'.format(self.__class__._last_observe.strftime("%m/%d/%Y, %H:%M:%S")))
             return
