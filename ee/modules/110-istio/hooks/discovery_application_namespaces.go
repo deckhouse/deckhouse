48c3ac2f53627574c3df2379ce301c02bd61c659
Pavel Tishkov <pavel.tishkov@flant.com>
2022-11-16 10:50:42 +0300

    [istio] lint fix
    Signed-off-by: Pavel Tishkov <pavel.tishkov@flant.com>

--- ee/modules/110-istio/hooks/discovery_application_namespaces.go
+++ ee/modules/110-istio/hooks/discovery_application_namespaces.go
@@ -7,12 +7,12 @@ package hooks
 
 import (
 	"fmt"
-	v1 "k8s.io/api/core/v1"
 	"sort"
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
 	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
