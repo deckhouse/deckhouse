085ab84c0d6f9a3f566af1e9efc1378fa8a99e44
v.oleynikov <vasily.oleynikov@flant.com>
2022-11-29 18:23:05 +0300

    [flant-integration] Added regex to parse os image for rocky and astra
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- ee/modules/600-flant-integration/images/flant-pricing/hooks/prometheus_metrics.sh
+++ ee/modules/600-flant-integration/images/flant-pricing/hooks/prometheus_metrics.sh
@@ -324,7 +324,9 @@ function node_os_image_metrics() {
                 "(?:(r)ed (h)at (e)nterprise (l)inux.*(7|8))",
                 "(?:(centos).*(7|8))",
                 "(?:(debian).*(9|10|11))",
-                "(?:(ubuntu).*(16.04|18.04|20.04|22.04))"
+                "(?:(ubuntu).*(16.04|18.04|20.04|22.04))",
+                "(?:(rocky).*(8|9))",
+                "(?:(astra))"
               ] | join("|")
             ).captures // [{string: "unknown"}] |
             .[] | select(.string) | .string
