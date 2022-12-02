d99e1b0e0750245c3db9a5baed69e3ad0dcc2470
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2022-12-02 21:33:08 +0300

    upgrade hack scripts to unite rbac-for-us
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- ee/modules/502-delivery/hack/get_manifests.sh
+++ ee/modules/502-delivery/hack/get_manifests.sh
@@ -16,7 +16,7 @@
 #   perl
 
 set -euo pipefail
-# set -x
+set -x
 
 # TODO check for the presence of yq
 
@@ -37,7 +37,8 @@ popd
 
 # target dirs
 CRD_ROOT=crds
-ARGOCD_MANIFESTS_ROOT=templates/argocd
+TEMPLATES_ROOT=templates
+ARGOCD_MANIFESTS_ROOT=$TEMPLATES_ROOT/argocd
 
 # clean existing manifests
 mkdir -p $CRD_ROOT
@@ -61,6 +62,7 @@ split_manifests "${VENDOR_ROOT}/${ARGO_MANIFESTS}"
 split_manifests "${VENDOR_ROOT}/${IMAGE_UPDATER_MANIFESTS}"
 
 # remove network policies as we don't need them in Deckhouse
+# TODO why?
 rm *-networkpolicy.yaml
 
 # Move CRDs
@@ -72,37 +74,63 @@ mv crd-*.yaml ${CRD_ROOT} &&
 # Add module namespace
 xargs -n 1 -- yq -i '.metadata.namespace="d8-{{ .Chart.Name }}"' <<<$(egrep --files-without-match '^kind: Cluster' argocd-*.yaml)
 
-# Fix default "argocd" namespace where it is specified (ClusterRoleBindings).
+# Fix default "argocd" namespace where it is specified (ClusterRoleBindings referring Roles).
 # https://argo-cd.readthedocs.io/en/stable/getting_started/#1-install-argo-cd
 #   - `sed -i` does not work on both MacOS and Linux consistently, so using Perl.
 #   - not using `yq` to avoid coupling with manifests paths, we don't know where we can meet the
 #     namespace.
 xargs -n 1 -- perl -pi -e 's/namespace: argocd/namespace: d8-{{ .Chart.Name }}/' <<<$(egrep --files-with-matches '^\s+namespace: argocd$' argocd-*.yaml)
 
+# Fix manifests
+# xargs -n 1 -- perl -pi -e 's/image: .+updater.+/image: {{ include "helm_lib_module_image" (list . "argocdImageUpdater") }}/' <<<$(egrep --files-with-matches '^\s+image:\s+[^{{]+updater.+$' argocd-*.yaml)
+# xargs -n 1 -- perl -pi -e 's/image: .+argocd.+/image: {{ include "helm_lib_module_image" (list . "argocd") }}/'              <<<$(egrep --files-with-matches '^\s+image:\s+[^{{]+argocd.+$' argocd-*.yaml)
+# xargs -n 1 -- perl -pi -e 's/image: .+redis.+/image: {{ include "helm_lib_module_image" (list . "redis") }}/'                <<<$(egrep --files-with-matches '^\s+image:\s+[^{{]+redis.+$' argocd-*.yaml)
+
 # Sort manifests
-mkdir -p ${ARGOCD_MANIFESTS_ROOT}/application-controller
-mv argocd-application-controller*.yaml ${ARGOCD_MANIFESTS_ROOT}/application-controller
-mv argocd-metrics-*.yaml ${ARGOCD_MANIFESTS_ROOT}/application-controller
+COMPONENT_ROOT=${ARGOCD_MANIFESTS_ROOT}/application-controller
+mkdir -p $COMPONENT_ROOT
+mv argocd-application-controller*.yaml $COMPONENT_ROOT
+mv argocd-metrics-*.yaml $COMPONENT_ROOT
+yq e $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account* > $COMPONENT_ROOT/rbac-for-us.yaml
+rm $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account*
 
-mkdir -p ${ARGOCD_MANIFESTS_ROOT}/applicationset-controller
-mv argocd-applicationset-controller*.yaml ${ARGOCD_MANIFESTS_ROOT}/applicationset-controller
+COMPONENT_ROOT=${ARGOCD_MANIFESTS_ROOT}/applicationset-controller
+mkdir -p $COMPONENT_ROOT
+mv argocd-applicationset-controller*.yaml $COMPONENT_ROOT
+yq e $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account* > $COMPONENT_ROOT/rbac-for-us.yaml
+rm $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account*
 
-mkdir -p ${ARGOCD_MANIFESTS_ROOT}/notifications-controller
-mv argocd-notifications*.yaml ${ARGOCD_MANIFESTS_ROOT}/notifications-controller
+COMPONENT_ROOT=${ARGOCD_MANIFESTS_ROOT}/notifications-controller
+mkdir -p $COMPONENT_ROOT
+mv argocd-notifications*.yaml $COMPONENT_ROOT
+yq e $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account* > $COMPONENT_ROOT/rbac-for-us.yaml
+rm $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account*
 
-mkdir -p ${ARGOCD_MANIFESTS_ROOT}/repo-server
-mv argocd-repo-server*.yaml ${ARGOCD_MANIFESTS_ROOT}/repo-server
+COMPONENT_ROOT=${ARGOCD_MANIFESTS_ROOT}/repo-server
+mkdir -p $COMPONENT_ROOT
+mv argocd-repo-server*.yaml $COMPONENT_ROOT
+yq e $COMPONENT_ROOT/*account* > $COMPONENT_ROOT/rbac-for-us.yaml
+rm $COMPONENT_ROOT/*account*
 
-mkdir -p ${ARGOCD_MANIFESTS_ROOT}/server
-mv argocd-server*.yaml ${ARGOCD_MANIFESTS_ROOT}/server
+COMPONENT_ROOT=${ARGOCD_MANIFESTS_ROOT}/server
+mkdir -p $COMPONENT_ROOT
+mv argocd-server*.yaml $COMPONENT_ROOT
+yq e $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account* > $COMPONENT_ROOT/rbac-for-us.yaml
+rm $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account*
 
-mkdir -p ${ARGOCD_MANIFESTS_ROOT}/redis
-mv argocd-redis*.yaml ${ARGOCD_MANIFESTS_ROOT}/redis
-pushd ${ARGOCD_MANIFESTS_ROOT}/redis && rename 's/^(.*)$/ha-$1/g' *-ha* && rename 's/-ha//' *-ha* || true && popd
+COMPONENT_ROOT=${ARGOCD_MANIFESTS_ROOT}/redis
+mkdir -p $COMPONENT_ROOT
+mv argocd-redis*.yaml $COMPONENT_ROOT
+pushd  $COMPONENT_ROOT && rename 's/^(.*)$/ha-$1/g' *-ha* && rename 's/-ha//' *-ha* || true && popd
+yq e $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account* > $COMPONENT_ROOT/rbac-for-us.yaml
+rm $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account*
 
-mkdir -p ${ARGOCD_MANIFESTS_ROOT}/image-updater
-mv argocd-image-updater*.yaml ${ARGOCD_MANIFESTS_ROOT}/image-updater
-pushd ${ARGOCD_MANIFESTS_ROOT}/image-updater && rename 's/^(.*)$/ha-$1/g' *-ha* && rename 's/-ha//' *-ha* || true && popd
+COMPONENT_ROOT=${ARGOCD_MANIFESTS_ROOT}/image-updater
+mkdir -p $COMPONENT_ROOT
+mv argocd-image-updater*.yaml $COMPONENT_ROOT
+pushd $COMPONENT_ROOT && rename 's/^(.*)$/ha-$1/g' *-ha* && rename 's/-ha//' *-ha* || true && popd
+yq e $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account* > $COMPONENT_ROOT/rbac-for-us.yaml
+rm $COMPONENT_ROOT/*role* $COMPONENT_ROOT/*account*
 
 # We use our own dex
 rm -rf mv argocd-dex*.yaml
