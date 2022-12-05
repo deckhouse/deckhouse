f28dec0a8e2d41cb78ddd08e5d78f1f0a58892c5
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2022-12-05 16:23:32 +0300

    hack: substitute security context
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- ee/modules/502-delivery/hack/retemplate.py
+++ ee/modules/502-delivery/hack/retemplate.py
@@ -8,12 +8,15 @@ def re_template(filepath):
 
     print(f"File {filepath}")
     lines = list(open(filepath).readlines())
-    print(f"Processing {len(lines)} lines")
-    labels = {}
+
+    overrides, erasures, insertions = [], [], []
+
     in_labels = False
+    labels = {}
     i_labels_start = 0
-    i_labels_end = 0
-    insertions = []
+
+    in_seccontext = False
+    i_seccontext_start = 0
 
     for i, l in enumerate(lines):
         if l.strip() == "labels:":
@@ -50,20 +53,46 @@ def re_template(filepath):
                         + " }}\n"
                     )
                     print(f"Labels end at {i}")
-                    lines[i_labels_start] = labels_template
-                    i_labels_end = i
-                    for j in range(i_labels_start + 1, i_labels_end):
-                        lines[j] = ""
+                    overrides.append((i_labels_start, labels_template))
+                    erasures += range(i_labels_start + 1, i)
                 labels = {}
                 in_labels = False
             continue
 
+        if l.strip() == "securityContext:":
+            print(f"Found securityContext at {i}")
+            in_seccontext = True
+            i_seccontext_start = i
+            sc_indent = l.count(" ", 0, l.find("s"))
+            helm_template = " ".join(
+                (
+                    " " * (sc_indent - 1),  # Helm template indentation
+                    "{{-",
+                    'include "helm_lib_module_container_security_context_read_only_root_filesystem_capabilities_drop_all"',
+                    f"| nindent {sc_indent}",
+                    "}}\n",
+                )
+            )
+            overrides.append((i, helm_template))
+            continue
+
+        if in_seccontext:
+            if l.startswith((2 + sc_indent) * " "):
+                erasures.append(i)
+            else:
+                in_seccontext = False
+            continue
+
         if l.strip().startswith("image: "):
             image_parts = lines[i].split(": ")
-            print(f"Line {i}: '{l}'")
-            print(f"Image parts {len(image_parts)}: {image_parts}")
             print(f"Found image '{image_parts[1]}'")
             lines[i] = image_parts[0] + ": " + map_image(image_parts[1].strip()) + "\n"
+
+    for i, l in overrides:
+        lines[i] = l
+
+    for i in erasures:
+        lines[i] = ""
 
     shift = 0
     for i, l in insertions:
@@ -85,7 +114,8 @@ def map_image(image):
 
 
 if __name__ == "__main__":
-    for root, dirs, files in os.walk(os.path.join(os.getcwd(), "templates", "argocd")):
+    templates_root = os.path.join(os.getcwd(), "templates", "argocd")
+    for root, dirs, files in os.walk(templates_root):
         print(root)
         # print("\n".join(["  ./" + d for d in dirs]))
 
