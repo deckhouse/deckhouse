e5c1da08314a72b8cac9a3eccb2b8a2a9d662118
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2022-12-06 20:04:47 +0300

    chore(hack): add copyright
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- ee/modules/502-delivery/hack/retemplate.py
+++ ee/modules/502-delivery/hack/retemplate.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+#
+# Copyright 2022 Flant JSC
+# Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+#
 import os
 
 
