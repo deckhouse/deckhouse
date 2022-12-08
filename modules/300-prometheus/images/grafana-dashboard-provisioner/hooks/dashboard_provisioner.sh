d10183d8027043359ccb0080d789fc3530c16297
Yuriy Losev <yuriy.losev@flant.com>
2022-12-08 12:35:41 +0400

    ++
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>

--- modules/300-prometheus/images/grafana-dashboard-provisioner/hooks/dashboard_provisioner.sh
+++ modules/300-prometheus/images/grafana-dashboard-provisioner/hooks/dashboard_provisioner.sh
@@ -46,6 +46,7 @@ function __main__() {
     fi
 
     title=$(slugify <<<${title})
+    genUid=$(jq -rc '.name' <<<${dashboard} | md5sum | awk '{print $1}')
 
     folder=$(jq -rc '.folder' <<<${dashboard})
     file="${folder}/${title}.json"
@@ -57,7 +58,7 @@ function __main__() {
     fi
 
     mkdir -p "${tmpDir}/${folder}"
-    jq -rc '.definition' <<<${dashboard} > "${tmpDir}/${file}"
+    echo ${dashboard} | jq -rc '.definition' | jq --arg newUid ${genUid} '.uid=$newUid' > "${tmpDir}/${file}"
   done
 
   if [[ "x${malformed_dashboards}" != "x" ]]; then
