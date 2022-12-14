6ec9ad99262d5d250414cc68440a103d0ce9d229
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2022-12-14 14:42:54 +0300

    metrics for alerts Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

--- modules/002-deckhouse/hooks/metrics.go
+++ modules/002-deckhouse/hooks/metrics.go
@@ -1,5 +1,9 @@
 /*
+<<<<<<< HEAD
 Copyright 2021 Flant JSC
+=======
+Copyright 2022 Flant JSC
+>>>>>>> bdaad86fb (metrics for alerts)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,6 +21,7 @@ limitations under the License.
 package hooks
 
 import (
+<<<<<<< HEAD
 	"strings"
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
@@ -54,5 +59,79 @@ func collectMetrics(input *go_hook.HookInput) error {
 			})
 		}
 	}
+=======
+	"fmt"
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook/metrics"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	storagev1 "k8s.io/api/storage/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Queue:        "/modules/deckhouse/metrics_for_alerts",
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 10},
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "storageclasses",
+			ApiVersion: "storage.k8s.io/v1",
+			Kind:       "Storageclass",
+			FilterFunc: applyStorageClasssesFilter,
+		},
+	},
+}, storageRetentionMetricHandler)
+
+func applyStorageClasssesFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	storageClass := &storagev1.StorageClass{}
+
+	err := sdk.FromUnstructured(obj, storageClass)
+	if err != nil {
+		return nil, fmt.Errorf("cannot convert kubernetes object: %v", err)
+	}
+
+	var isDefault bool
+
+	if storageClass.Annotations["storageclass.beta.kubernetes.io/is-default-class"] == "true" {
+		isDefault = true
+	}
+
+	if storageClass.Annotations["storageclass.kubernetes.io/is-default-class"] == "true" {
+		isDefault = true
+	}
+
+	return DefaultStorageClass{
+		Name:      storageClass.Name,
+		IsDefault: isDefault,
+	}, nil
+}
+
+
+func storageRetentionMetricHandler(input *go_hook.HookInput) error {
+	retentionDaysMain := input.Values.Get("prometheus.retentionDays")
+	retentionDaysLongterm := input.Values.Get("prometheus.longtermRetentionDays")
+
+	input.MetricsCollector.Expire("prometheus_disk_hook")
+
+	input.MetricsCollector.Set(
+		"d8_prometheus_storage_retention_days",
+		retentionDaysMain.Float(),
+		map[string]string{
+			"prometheus": "main",
+		},
+		metrics.WithGroup("prometheus_disk_hook"),
+	)
+
+	input.MetricsCollector.Set(
+		"d8_prometheus_storage_retention_days",
+		retentionDaysLongterm.Float(),
+		map[string]string{
+			"prometheus": "longterm",
+		},
+		metrics.WithGroup("prometheus_disk_hook"),
+	)
+
+>>>>>>> bdaad86fb (metrics for alerts)
 	return nil
 }
