4a67cc451f257e01a41639c0ff53dab8837a2e7c
Pavel Tishkov <pavel.tishkov@flant.com>
2022-12-26 21:44:49 +0300

    [cni-cilium] v1.12.5
    Signed-off-by: Pavel Tishkov <pavel.tishkov@flant.com>

--- modules/021-cni-cilium/images/cilium/build-gops.sh
+++ modules/021-cni-cilium/images/cilium/build-gops.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2017-2021 Authors of Cilium
+# Copyright Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
 set -o xtrace
@@ -20,7 +20,7 @@ git checkout -b "${gops_version}" "${gops_version}"
 git --no-pager remote -v
 git --no-pager log -1
 
-for arch in amd64 ; do
+for arch in amd64 arm64 ; do
   mkdir -p "/out/linux/${arch}/bin"
   GOARCH="${arch}" CGO_ENABLED=0 go build -ldflags "-s -w" -o "/out/linux/${arch}/bin/gops" github.com/google/gops
 done
