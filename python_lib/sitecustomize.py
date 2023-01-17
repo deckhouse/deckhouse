cde24d7541f2461b4c2f66dd4d6cf5134e44db0b
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2023-01-17 19:12:59 +0300

    add python runtime
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

---
+++ python_lib/sitecustomize.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+#
+# Copyright 2023 Flant JSC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import sys
+
+
+def find_module_root(path):
+    """Returns module root path and module name for hook being executed.
+
+    E.g. for hook path /deckhouse/modules/999-zz-python/hooks/detect_array.py
+    module root path is /deckhouse/modules/999-zz-python and module name is 999-zz-python
+
+    Args:
+        path (str): hook absolute path
+
+    Returns:
+        (str, str): module root and module dir name, last segment of the root
+    """
+    while True:
+        parent, name = os.path.split(path)
+        # Don't stick absolute paths to keep it portable on dev machines
+        if os.path.split(parent)[1] == "modules":
+            break
+        path = parent
+    return path, name
+
+
+hook_path = os.path.abspath(sys.argv[0])
+mod_root, mod_name = find_module_root(hook_path)
+
+# Add module python dependencies to PYTHONPATH
+if mod_root:
+    lib_path = os.path.join(mod_root, "lib", "python", "dist")
+    sys.path.append(lib_path)
+
+# Add module name to env for simpler discovery, consumed by deckhouse python lib
+if mod_name:
+    os.environ["D8_MODULE_NAME"] = mod_name
