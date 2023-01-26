26b8a83d71092cbb0fbf4abd72c973e62f558ca4
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2023-01-26 14:07:15 +0300

    actualize sitecustomize
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- python_lib/sitecustomize.py
+++ python_lib/sitecustomize.py
@@ -20,32 +20,30 @@ import sys
 def find_module_root(path):
     """Returns module root path and module name for hook being executed.
 
-    E.g. for hook path /deckhouse/modules/999-zz-python/hooks/detect_array.py
-    module root path is /deckhouse/modules/999-zz-python and module name is 999-zz-python
+    E.g. for hook
+        /deckhouse/modules/999-my-module/hooks/my_hook.py
+    module root is
+        /deckhouse/modules/999-my-module
 
     Args:
         path (str): hook absolute path
-
     Returns:
-        (str, str): module root and module dir name, last segment of the root
+        (str): module root
     """
     while True:
-        parent, name = os.path.split(path)
+        parent, _ = os.path.split(path)
         # Don't stick to absolute path (/deckhouse/modules) to keep it portable
         if os.path.split(parent)[1] == "modules":
-            break
+            return path  # we are in the module root
         path = parent
-    return path, name
 
 
 hook_path = os.path.abspath(sys.argv[0])
-mod_root, mod_dirname = find_module_root(hook_path)
+mod_root = find_module_root(hook_path)
 
-# Add Python packages discovery for module hooks
+# Add Python packages discovery for module hooks, $D8_MODULE_ROOT/lib/python/dist
 if mod_root:
     lib_path = os.path.join(mod_root, "lib", "python", "dist")
     sys.path.append(lib_path)
 
-# Add module name to env for simpler discovery, consumed by Deckhouse hook lib for Python
-if mod_dirname:
-    os.environ["D8_MODULE_DIRNAME"] = mod_dirname
+    os.environ["D8_MODULE_ROOT"] = mod_root
