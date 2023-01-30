a5b639b99a7c8e42ce0a5ad580513cd1b146d66d
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2023-01-30 21:30:21 +0300

    fix missing dependency
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- modules/999-zz-python/webhooks/conversion/python.py
+++ modules/999-zz-python/webhooks/conversion/python.py
@@ -49,15 +49,10 @@ def main(ctx: hook.Context):
     try:
         # DotMap is a dict with dot notation
         bctx = DotMap(ctx.binding_context)
-
         # print(bctx.pprint(pformat="json"))  # debug printing
-
         for obj in bctx.review.request.objects:
-            converted = conv.convert(bctx.fromVersion, bctx.toVersion, obj)
-
+            converted = conv.convert(bctx.fromVersion, bctx.toVersion, obj.toDict())
             # print(converted.pprint(pformat="json"))  # debug printing
-
-            # DotMap is not JSON serializable, we need raw dict
             ctx.output.conversions.collect(converted)
     except Exception as e:
         print("conversion error", str(e))  # debug printing
