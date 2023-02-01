5dfd4c83bc372e56efcb93121925e52fe8a97169
Pavel Tishkov <pavel.tishkov@flant.com>
2023-02-01 12:07:46 +0300

    [istio] add webhook checks
    Signed-off-by: Pavel Tishkov <pavel.tishkov@flant.com>

--- ee/modules/110-istio/hooks/discovery_istiod_health.go
+++ ee/modules/110-istio/hooks/discovery_istiod_health.go
@@ -9,6 +9,7 @@ import (
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
 	"github.com/flant/shell-operator/pkg/kube/object_patch"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -48,6 +49,38 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 			},
 			FilterFunc: applyIstiodPodFilter,
 		},
+		{
+			Name:       "istio_sidecar_injector_global_webhook",
+			ApiVersion: "admissionregistration.k8s.io/v1",
+			Kind:       "MutatingWebhookConfiguration",
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{"d8-istio-sidecar-injector-global"},
+			},
+			LabelSelector: &metav1.LabelSelector{
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "module",
+						Operator: metav1.LabelSelectorOpIn,
+						Values:   []string{"istio"},
+					},
+					{
+						Key:      "istio-full-version",
+						Operator: metav1.LabelSelectorOpExists,
+					},
+				},
+			},
+			FilterFunc: applyIstiodInjectorWebhook,
+		},
+		// we need to subscribe to the istio module configuration to update the istiod isReady status in time
+		{
+			Name:       "istio_module_config",
+			ApiVersion: "deckhouse.io/v1alpha1",
+			Kind:       "ModuleConfig",
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{"istio"},
+			},
+			FilterFunc: applyIstioModuleConfig,
+		},
 	},
 }, discoveryIstiodHealthHook)
 
@@ -64,27 +97,59 @@ func applyIstiodPodFilter(obj *unstructured.Unstructured) (go_hook.FilterResult,
 	}, nil
 }
 
+func applyIstiodInjectorWebhook(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	labels := obj.GetLabels()
+	fullVer := labels["istio-full-version"]
+	return fullVer, nil
+}
+
+func applyIstioModuleConfig(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	return obj.GetName(), nil
+}
+
 func discoveryIstiodHealthHook(input *go_hook.HookInput) error {
 	var isGlobalVersionIstiodReady bool
+	var injectorGlobalFullVer string
 	if !input.Values.Get("istio.internal.globalVersion").Exists() {
 		return nil
 	}
 
-	versionMap := istio_versions.VersionMapJSONToVersionMap(input.Values.Get("istio.internal.versionMap").String())
+	if len(input.Snapshots["istio_sidecar_injector_global_webhook"]) == 1 {
+		injectorGlobalFullVer = input.Snapshots["istio_sidecar_injector_global_webhook"][0].(string)
+	}
 
+	versionMap := istio_versions.VersionMapJSONToVersionMap(input.Values.Get("istio.internal.versionMap").String())
 	globalVersion := input.Values.Get("istio.internal.globalVersion").String()
+	globalFullVersion := versionMap[globalVersion].FullVersion
+
+	// reset istiod ready statuses
+	for ver, istioInfo := range versionMap {
+		istioInfo.IsReady = false
+		versionMap[ver] = istioInfo
+	}
+
 	for _, podRaw := range input.Snapshots["istiod_pods"] {
 		pod := podRaw.(istiodPod)
+		podVer := versionMap.GetVersionByRevision(pod.Revision)
+		podFullVer := versionMap.GetFullVersionByRevision(pod.Revision)
 		if pod.Phase == v1.PodRunning {
-			versionMap.SetRevisionStatus(pod.Revision, true)
+			if podVer != globalVersion {
+				versionMap.SetRevisionStatus(pod.Revision, true)
+			} else {
+				if podFullVer == globalFullVersion && injectorGlobalFullVer == globalFullVersion {
+					versionMap.SetRevisionStatus(pod.Revision, true)
+				} else {
+					versionMap.SetRevisionStatus(pod.Revision, false)
+				}
+			}
 			if versionMap.GetVersionByRevision(pod.Revision) == globalVersion {
 				isGlobalVersionIstiodReady = true
 			}
 		} else {
 			versionMap.SetRevisionStatus(pod.Revision, false)
 		}
-
 	}
+
 	input.Values.Set(isGlobalVersionIstiodReadyPath, isGlobalVersionIstiodReady)
 	input.Values.Set(versionMapPath, versionMap)
 	if !isGlobalVersionIstiodReady {
