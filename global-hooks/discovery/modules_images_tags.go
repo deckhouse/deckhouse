a54f71c26d0085b792c3251bf1301fe457463306
Denys Romanenko <65756796+RomanenkoDenys@users.noreply.github.com>
2023-02-02 15:08:27 +0300

    [global] modify modules_images_tags to support external modules (#3568)
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>
    Co-authored-by: Yuriy Losev <yuriy.losev@flant.com>

--- global-hooks/discovery/modules_images_tags.go
+++ global-hooks/discovery/modules_images_tags.go
@@ -18,32 +18,93 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
+	"regexp"
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
+	"github.com/iancoleman/strcase"
 )
+
+var re = regexp.MustCompile(`^([0-9]+)-([a-zA-Z-]+)$`)
 
 var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 	OnStartup: &go_hook.OrderedConfig{Order: 10},
 }, discoveryModulesImagesTags)
 
 func discoveryModulesImagesTags(input *go_hook.HookInput) error {
+	// Discover main deckhouse images tags file
 	tagsFile := "/deckhouse/modules/images_tags.json"
+	externalModulesDir := filepath.Join(os.Getenv("EXTERNAL_MODULES_DIR"), "modules")
+
 	if os.Getenv("D8_IS_TESTS_ENVIRONMENT") != "" {
 		tagsFile = os.Getenv("D8_TAGS_TMP_FILE")
+		externalModulesDir = "testdata/modules-images-tags/external-modules"
 	}
 
-	tagsContent, err := os.ReadFile(tagsFile)
+	tagsObj, err := parseImagesTagsFile(tagsFile)
 	if err != nil {
-		return fmt.Errorf("cannot read images tags files: %w", err)
+		return err
 	}
 
-	var tagsObj map[string]interface{}
-	if err := json.Unmarshal(tagsContent, &tagsObj); err != nil {
-		return fmt.Errorf("invalid images tags json: %w", err)
+	modulesTagsObj := readModulesImagesTags(input, externalModulesDir)
+	for k, v := range modulesTagsObj {
+		tagsObj[k] = v
 	}
 
 	input.Values.Set("global.modulesImages.tags", tagsObj)
 
 	return nil
+}
+
+func parseImagesTagsFile(filePath string) (map[string]interface{}, error) {
+	tagsContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read images tags files: %w", err)
+	}
+
+	var tagsObj map[string]interface{}
+	if err := json.Unmarshal(tagsContent, &tagsObj); err != nil {
+		return nil, fmt.Errorf("invalid images tags json: %w", err)
+	}
+
+	return tagsObj, nil
+}
+
+func readModulesImagesTags(input *go_hook.HookInput, modulesDir string) map[string]interface{} {
+	tagsObj := make(map[string]interface{})
+
+	dirItems, err := os.ReadDir(modulesDir)
+	if err != nil {
+		input.LogEntry.Warning(err)
+		return nil
+	}
+
+	for _, dirItem := range dirItems {
+		evalPath := filepath.Join(modulesDir, dirItem.Name())
+		evalPath, err = filepath.EvalSymlinks(evalPath)
+		if err != nil {
+			input.LogEntry.Warning(err)
+			continue
+		}
+
+		fi, err := os.Stat(evalPath)
+		if err != nil {
+			input.LogEntry.Warning(err)
+			continue
+		}
+		if !fi.Mode().IsDir() {
+			continue
+		}
+
+		moduleTagsObj, err := parseImagesTagsFile(filepath.Join(evalPath, "images_tags.json"))
+		if err != nil {
+			input.LogEntry.Warning(err)
+			continue
+		}
+
+		moduleNameLowerCamel := strcase.ToLowerCamel(re.ReplaceAllString(dirItem.Name(), "$2"))
+		tagsObj[moduleNameLowerCamel] = moduleTagsObj
+	}
+	return tagsObj
 }
