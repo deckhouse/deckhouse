c290516ff9f5a3fad82a308fc630af62eb68c7b3
Andrey Klimentyev <andrey.klimentyev@flant.com>
2023-02-07 11:12:26 +0300

    [node-manager] Fixed Yandex preemptible hook (#3589)
    * [cloud-provider-yandex] Fixed Yandex preemptible hook
    
    Signed-off-by: Andrey Klimentyev <andrey.klimentyev@flant.com>

--- modules/040-node-manager/hooks/yc_delete_preemptible_instances_test.go
+++ modules/040-node-manager/hooks/yc_delete_preemptible_instances_test.go
@@ -29,12 +29,13 @@ import (
 
 var _ = Describe("Modules :: cloud-provider-yandex :: hooks :: preemptibly_delete_preemtible_instances ::", func() {
 	f := HookExecutionConfigInit(`{}`, `{}`)
+	f.RegisterCRD("deckhouse.io", "v1", "NodeGroup", false)
 	f.RegisterCRD("machine.sapcloud.io", "v1alpha1", "YandexMachineClass", true)
 	f.RegisterCRD("machine.sapcloud.io", "v1alpha1", "Machine", true)
 
 	Context("With no proper Machines", func() {
 		BeforeEach(func() {
-			f.BindingContexts.Set(f.KubeStateSet(generateNGsAndMCs()))
+			f.BindingContexts.Set(f.KubeStateSet(generateNGsAndMCs(10, 10, "")))
 			f.RunHook()
 		})
 
@@ -48,29 +49,12 @@ var _ = Describe("Modules :: cloud-provider-yandex :: hooks :: preemptibly_delet
 	Context("With proper machines", func() {
 		BeforeEach(func() {
 			f.BindingContexts.Set(f.KubeStateSet(generateNGsAndMCs(
-				"23h10m", "23h30m", "23h40m", "22h",
+				7, 7, "", "28h", "22h10m", "22h5m", "22h2m", "21h", "20h", "2h",
 			)))
 			f.RunHook()
 		})
 
-		It("All machines after 23h mark should be deleted", func() {
-			Expect(f).To(ExecuteSuccessfully())
-			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-0").Exists()).To(BeFalse())
-			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-1").Exists()).To(BeFalse())
-			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-2").Exists()).To(BeFalse())
-			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-3").Exists()).To(BeTrue())
-		})
-	})
-
-	Context("With proper machines", func() {
-		BeforeEach(func() {
-			f.BindingContexts.Set(f.KubeStateSet(generateNGsAndMCs(
-				"22h10m", "22h5m", "22h2m", "21h", "20h", "2h",
-			)))
-			f.RunHook()
-		})
-
-		It("Oldest 2 machines should be deleted", func() {
+		It("One oldest Machine will be deleted", func() {
 			Expect(f).To(ExecuteSuccessfully())
 			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-0").Exists()).To(BeFalse())
 			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-1").Exists()).To(BeTrue())
@@ -78,64 +62,144 @@ var _ = Describe("Modules :: cloud-provider-yandex :: hooks :: preemptibly_delet
 			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-3").Exists()).To(BeTrue())
 			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-4").Exists()).To(BeTrue())
 			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-5").Exists()).To(BeTrue())
+			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-6").Exists()).To(BeTrue())
 		})
 	})
 
 	Context("With proper machines", func() {
 		BeforeEach(func() {
 			f.BindingContexts.Set(f.KubeStateSet(generateNGsAndMCs(
-				"22h10m", "22h5m", "22h2m", "21h",
+				4, 4, "", "22h10m", "22h5m", "22h2m", "21h",
 			)))
 			f.RunHook()
 		})
 
-		It("Oldest machine should be deleted", func() {
+		It("Oldest Machine should be deleted", func() {
 			Expect(f).To(ExecuteSuccessfully())
 			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-0").Exists()).To(BeFalse())
 			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-1").Exists()).To(BeTrue())
 			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-2").Exists()).To(BeTrue())
 			Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", "test-3").Exists()).To(BeTrue())
 		})
+
+	})
+
+	Context("With 60 Machines older than 24h, one Machine machines younger than 20 hours, and one between 20 and 24 hours", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(generateNMachines(
+				60, 60, "test", durationWithCount{count: 60, duration: "30h"},
+			)))
+			f.RunHook()
+		})
+
+		It("15 older than 24 hours Machines and one machine between 20 and 24 hours old should be deleted", func() {
+			Expect(f).To(ExecuteSuccessfully())
+
+			for i := 0; i < 54; i++ {
+				machineName := fmt.Sprintf("test-0-test-%d", i)
+				Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", machineName).Exists()).To(BeTrue())
+			}
+		})
+	})
+
+	Context("With proper machines, but node readiness ratio is below 0.9", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(generateNMachines(
+				50, 40, "old", durationWithCount{count: 50, duration: "22h10m"},
+			)))
+			f.RunHook()
+		})
+
+		It("No Machine should be deleted", func() {
+			Expect(f).To(ExecuteSuccessfully())
+
+			for i := 0; i < 50; i++ {
+				machineName := fmt.Sprintf("old-0-test-%d", i)
+				By(fmt.Sprintf("Checking that Machines with name %q still exists", machineName))
+				Expect(f.KubernetesResource("Machine", "d8-cloud-instance-manager", machineName).Exists()).To(BeTrue())
+			}
+		})
 	})
 })
 
-func generateNGsAndMCs(durationStrings ...string) string {
+type durationWithCount struct {
+	duration string
+	count    int
+}
+
+func generateNMachines(ngNodes, ngReady int, prefix string, machinesCount ...durationWithCount) string {
+	var builder strings.Builder
+
+	for i, mc := range machinesCount {
+		var durations []string
+
+		for j := 0; j < mc.count; j++ {
+			durations = append(durations, mc.duration)
+		}
+
+		builder.WriteString(generateNGsAndMCs(ngNodes, ngReady, fmt.Sprintf("%s-%d-", prefix, i), durations...))
+	}
+
+	return builder.String()
+}
+
+func generateNGsAndMCs(ngNodes, ngReady int, prefix string, durationStrings ...string) string {
 	timeNow := time.Now().UTC()
 
 	var offsets []time.Duration
-	for _, d := range durationStrings {
+	for i, d := range durationStrings {
 		duration, err := time.ParseDuration(d)
 		if err != nil {
 			panic(err)
 		}
 
+		duration += time.Duration(i) * time.Millisecond
+
 		offsets = append(offsets, duration)
 	}
 
 	var builder strings.Builder
-	builder.WriteString(`---
+	builder.WriteString(fmt.Sprintf(strings.ReplaceAll(`---
+apiVersion: deckhouse.io/v1
+kind: NodeGroup
+metadata:
+  name: %stest
+spec:
+  cloudInstances:
+    classReference:
+      kind: YandexInstanceClass
+status:
+  nodes: %d
+  ready: %d
+---
 apiVersion: v1
 kind: Node
 metadata:
-  name: test
+  name: %stest
+  labels:
+    node.deckhouse.io/group: %stest
   creationTimestamp: "1970-01-01T00:00:00Z"
 ---
 apiVersion: v1
 kind: Node
 metadata:
-  name: not-preemptible
+  name: %snot-preemptible
+  labels:
+    node.deckhouse.io/group: %stest
   creationTimestamp: "1970-01-01T00:00:00Z"
 ---
 apiVersion: v1
 kind: Node
 metadata:
-  name: wrong-instance-class
+  name: %swrong-instance-class
+  labels:
+    node.deckhouse.io/group: %stest
   creationTimestamp: "1970-01-01T00:00:00Z"
 ---
 apiVersion: machine.sapcloud.io/v1alpha1
 kind: YandexMachineClass
 metadata:
-  name: test
+  name: %stest
   namespace: d8-cloud-instance-manager
 spec:
   schedulingPolicy:
@@ -144,7 +208,7 @@ spec:
 apiVersion: machine.sapcloud.io/v1alpha1
 kind: YandexMachineClass
 metadata:
-  name: not-preemptible
+  name: %snot-preemptible
   namespace: d8-cloud-instance-manager
 spec:
   schedulingPolicy:
@@ -153,24 +217,24 @@ spec:
 apiVersion: machine.sapcloud.io/v1alpha1
 kind: Machine
 metadata:
-  name: wrong-instance-class
+  name: %swrong-instance-class
   namespace: d8-cloud-instance-manager
 spec:
   class:
     kind: AWSMachineClass
-    name: test
+    name: %stest
 ---
 apiVersion: machine.sapcloud.io/v1alpha1
 kind: Machine
 metadata:
-  name: terminating
+  name: %sterminating
   namespace: d8-cloud-instance-manager
   deletionTimestamp: "1970-01-01T00:00:00Z"
 spec:
   class:
     kind: YandexMachineClass
-    name: test
-`)
+    name: %stest
+`, "%s", prefix), ngNodes, ngReady))
 
 	for i, offset := range offsets {
 		ts, err := timeNow.Add(-offset).MarshalJSON()
@@ -178,23 +242,25 @@ spec:
 			panic(err)
 		}
 
-		_, _ = builder.WriteString(fmt.Sprintf(`---
+		_, _ = builder.WriteString(fmt.Sprintf(strings.ReplaceAll(`---
 apiVersion: v1
 kind: Node
 metadata:
-  name: test-%d
+  name: %s%test-%d
+  labels:
+    node.deckhouse.io/group: %s%test
   creationTimestamp: %s
 ---
 apiVersion: machine.sapcloud.io/v1alpha1
 kind: Machine
 metadata:
-  name: test-%d
+  name: %s%test-%d
   namespace: d8-cloud-instance-manager
 spec:
   class:
     kind: YandexMachineClass
-    name: test
-`, i, string(ts), i))
+    name: %s%test
+`, "%s%", prefix), i, string(ts), i))
 	}
 
 	return builder.String()
