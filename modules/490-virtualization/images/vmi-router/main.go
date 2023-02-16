19f6c1494d2a505cc85c33fa3602257438a6f02d
deckhouse-BOaTswain <89150800+deckhouse-BOaTswain@users.noreply.github.com>
2023-02-16 13:31:23 +0300

    [virtualization] vmi-router: disable metrics by default (#3876) (#3885)
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>
    Co-authored-by: Andrei Kvapil <kvapss@gmail.com>

--- modules/490-virtualization/images/vmi-router/main.go
+++ modules/490-virtualization/images/vmi-router/main.go
@@ -66,8 +66,8 @@ func main() {
 	var probeAddr string
 	flag.Var(&cidrs, "cidr", "CIDRs enabled to route (multiple flags allowed)")
 	flag.BoolVar(&dryRun, "dry-run", false, "Don't perform any changes on the node.")
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":0", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":0", "The address the probe endpoint binds to.")
 	opts := zap.Options{
 		Development: true,
 	}
