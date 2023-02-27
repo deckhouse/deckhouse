4ae346b0bdae04bb6c7c94122b1987928eab5b20
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2023-02-27 11:00:36 +0300

    go generate
    Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -321,7 +321,7 @@ var DefaultImagesTags = map[string]interface{}{
 		"conntrackDebian1462":                 "imageHash-registrypackages-conntrackDebian1462",
 		"containerdAlteros14631El7X8664":      "imageHash-registrypackages-containerdAlteros14631El7X8664",
 		"containerdAlteros161431El7X8664":     "imageHash-registrypackages-containerdAlteros161431El7X8664",
-		"containerdAltlinux169Alt1":           "imageHash-registrypackages-containerdAltlinux169Alt1",
+		"containerdAltlinux169Alt1X8664":      "imageHash-registrypackages-containerdAltlinux169Alt1X8664",
 		"containerdAstra1461Buster":           "imageHash-registrypackages-containerdAstra1461Buster",
 		"containerdAstra16181Buster":          "imageHash-registrypackages-containerdAstra16181Buster",
 		"containerdCentos14631El7X8664":       "imageHash-registrypackages-containerdCentos14631El7X8664",
