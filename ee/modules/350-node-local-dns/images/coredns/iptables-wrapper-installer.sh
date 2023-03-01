238864f094d2c3a8ad781d89b452da76455091c5
Andrey Klimentyev <andrey.klimentyev@flant.com>
2023-03-01 10:33:35 +0300

    distroless
    Signed-off-by: Andrey Klimentyev <andrey.klimentyev@flant.com>

--- ee/modules/350-node-local-dns/images/coredns/iptables-wrapper-installer.sh
+++ ee/modules/350-node-local-dns/images/coredns/iptables-wrapper-installer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Copyright 2020 The Kubernetes Authors.
 #
@@ -14,71 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Usage:
-#
-#   iptables-wrapper-installer.sh [--no-sanity-check]
-#
-# Installs a wrapper iptables script in a container that will figure out
-# whether iptables-legacy or iptables-nft is in use on the host and then
-# replaces itself with the correct underlying iptables version.
-#
-# Unless "--no-sanity-check" is passed, it will first verify that the
-# container already contains a suitable version of iptables.
-
-# NOTE: This can only use POSIX /bin/sh features; the build container
-# might not contain bash.
-
 set -eu
 
-# Find iptables binary location
-if [ -d /usr/sbin -a -e /usr/sbin/iptables ]; then
-    sbin="/usr/sbin"
-elif [ -d /sbin -a -e /sbin/iptables ]; then
-    sbin="/sbin"
-else
-    echo "ERROR: iptables is not present in either /usr/sbin or /sbin" 1>&2
-    exit 1
-fi
-
-# Determine how the system selects between iptables-legacy and iptables-nft
-if [ -x /usr/sbin/alternatives ]; then
-    # Fedora/SUSE style alternatives
-    altstyle="fedora"
-elif [ -x /usr/sbin/update-alternatives ]; then
-    # Debian style alternatives
-    altstyle="debian"
-else
-    # No alternatives system
-    altstyle="none"
-fi
-
-if [ "${1:-}" != "--no-sanity-check" ]; then
-    # Ensure dependencies are installed
-    if ! version=$("${sbin}/iptables-nft" --version 2> /dev/null); then
-        echo "ERROR: iptables-nft is not installed" 1>&2
-        exit 1
-    fi
-    if ! "${sbin}/iptables-legacy" --version > /dev/null 2>&1; then
-        echo "ERROR: iptables-legacy is not installed" 1>&2
-        exit 1
-    fi
-
-    case "${version}" in
-    *v1.8.[0123]\ *)
-        echo "ERROR: iptables 1.8.0 - 1.8.3 have compatibility bugs." 1>&2
-        echo "       Upgrade to 1.8.4 or newer." 1>&2
-        exit 1
-        ;;
-    *)
-        # 1.8.4+ are OK
-        ;;
-    esac
-fi
+chroot_sbin="/dpkg/usr/sbin"
+sbin="/usr/sbin"
 
 # Start creating the wrapper...
-rm -f "${sbin}/iptables-wrapper"
-cat > "${sbin}/iptables-wrapper" <<EOF
-#!/bin/sh
+rm -f "${chroot_sbin}/iptables-wrapper"
+cat > "${chroot_sbin}/iptables-wrapper" <<EOF
+#!/bin/bash
 
 # Copyright 2020 The Kubernetes Authors.
 #
@@ -128,39 +72,19 @@ else
         fi
     fi
 fi
-
 EOF
 
 # Write out the appropriate alternatives-selection commands
-case "${altstyle}" in
-    fedora)
-cat >> "${sbin}/iptables-wrapper" <<EOF
-# Update links to point to the selected binaries
-alternatives --set iptables "/usr/sbin/iptables-\${mode}" > /dev/null || failed=1
-EOF
-    ;;
-
-    debian)
-cat >> "${sbin}/iptables-wrapper" <<EOF
-# Update links to point to the selected binaries
-update-alternatives --set iptables "/usr/sbin/iptables-\${mode}" > /dev/null || failed=1
-update-alternatives --set ip6tables "/usr/sbin/ip6tables-\${mode}" > /dev/null || failed=1
-EOF
-    ;;
-
-    *)
-cat >> "${sbin}/iptables-wrapper" <<EOF
+cat >> "${chroot_sbin}/iptables-wrapper" <<EOF
 # Update links to point to the selected binaries
 for cmd in iptables iptables-save iptables-restore ip6tables ip6tables-save ip6tables-restore; do
     rm -f "${sbin}/\${cmd}"
     ln -s "${sbin}/xtables-\${mode}-multi" "${sbin}/\${cmd}"
 done 2>/dev/null || failed=1
 EOF
-    ;;
-esac
 
 # Write out the post-alternatives-selection error checking and final wrap-up
-cat >> "${sbin}/iptables-wrapper" <<EOF
+cat >> "${chroot_sbin}/iptables-wrapper" <<EOF
 if [ "\${failed:-0}" = 1 ]; then
     echo "Unable to redirect iptables binaries. (Are you running in an unprivileged pod?)" 1>&2
     # fake it, though this will probably also fail if they aren't root
@@ -170,39 +94,14 @@ fi
 # Now re-exec the original command with the newly-selected alternative
 exec "\$0" "\$@"
 EOF
-chmod +x "${sbin}/iptables-wrapper"
+chmod +x "${chroot_sbin}/iptables-wrapper"
 
 # Now back in the installer script, point the iptables binaries at our
 # wrapper
-case "${altstyle}" in
-    fedora)
-	alternatives \
-            --install /usr/sbin/iptables iptables /usr/sbin/iptables-wrapper 100 \
-            --slave /usr/sbin/iptables-restore iptables-restore /usr/sbin/iptables-wrapper \
-            --slave /usr/sbin/iptables-save iptables-save /usr/sbin/iptables-wrapper \
-            --slave /usr/sbin/ip6tables iptables /usr/sbin/iptables-wrapper \
-            --slave /usr/sbin/ip6tables-restore iptables-restore /usr/sbin/iptables-wrapper \
-            --slave /usr/sbin/ip6tables-save iptables-save /usr/sbin/iptables-wrapper
-	;;
-
-    debian)
-	update-alternatives \
-            --install /usr/sbin/iptables iptables /usr/sbin/iptables-wrapper 100 \
-            --slave /usr/sbin/iptables-restore iptables-restore /usr/sbin/iptables-wrapper \
-            --slave /usr/sbin/iptables-save iptables-save /usr/sbin/iptables-wrapper
-	update-alternatives \
-            --install /usr/sbin/ip6tables ip6tables /usr/sbin/iptables-wrapper 100 \
-            --slave /usr/sbin/ip6tables-restore ip6tables-restore /usr/sbin/iptables-wrapper \
-            --slave /usr/sbin/ip6tables-save ip6tables-save /usr/sbin/iptables-wrapper
-	;;
-
-    *)
-	for cmd in iptables iptables-save iptables-restore ip6tables ip6tables-save ip6tables-restore; do
-            rm -f "${sbin}/${cmd}"
-            ln -s "${sbin}/iptables-wrapper" "${sbin}/${cmd}"
-	done
-	;;
-esac
+for cmd in iptables iptables-save iptables-restore ip6tables ip6tables-save ip6tables-restore; do
+        rm -f "${chroot_sbin}/${cmd}"
+        ln -s "${sbin}/iptables-wrapper" "${chroot_sbin}/${cmd}"
+done
 
 # Cleanup
 rm -f "$0"
