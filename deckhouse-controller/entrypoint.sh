982ba4801bc0e65468794ba2415eefe96c229902
Yuriy Losev <yuriy.losev@flant.com>
2023-03-02 20:10:41 +0400

    [deckhouse] Handle pending release (#3976)
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>

--- deckhouse-controller/entrypoint.sh
+++ deckhouse-controller/entrypoint.sh
@@ -28,11 +28,6 @@ signal_handler() {
     wait "${PID}"
     run_deckhouse
     ;;
-  "SIGTERM")
-    # always sleep 10 seconds on modules converge to avoid Deckhouse self-upgrade helm race (release is stuck into pending-upgrade)
-    /usr/bin/deckhouse-controller queue main | grep ConvergeModules && echo "{\"level\":\"info\", \"msg\": \"Deckhouse is restarting during the modules converge. Sleeping 10 seconds.\"}" && sleep 10
-    kill -"${1}" "${PID}"
-    ;;
   *)
     kill -"${1}" "${PID}"
     ;;
