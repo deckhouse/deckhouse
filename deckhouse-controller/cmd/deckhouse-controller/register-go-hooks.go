03c147ac90434515285199f0b660533ecc521f97
Andrei Kvapil <kvapss@gmail.com>
2023-03-03 16:46:12 +0100

    [containerized-data-importer] fix customCertificate
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

--- deckhouse-controller/cmd/deckhouse-controller/register-go-hooks.go
+++ deckhouse-controller/cmd/deckhouse-controller/register-go-hooks.go
@@ -73,6 +73,7 @@ import (
 	_ "github.com/deckhouse/deckhouse/modules/460-log-shipper/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/490-virtualization/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/491-containerized-data-importer/hooks"
+	_ "github.com/deckhouse/deckhouse/modules/491-containerized-data-importer/hooks/https"
 	_ "github.com/deckhouse/deckhouse/modules/500-cilium-hubble/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/500-cilium-hubble/hooks/https"
 	_ "github.com/deckhouse/deckhouse/modules/500-dashboard/hooks"
