4f11085842fb01344cc719cf449b821db20882c0
Konstantin Maltsev <114921649+Diaderius@users.noreply.github.com>
2023-03-06 13:32:47 +0300

    [user-authn] discover_dex_ca loops the list of secrets (#3842)
    Signed-off-by: Konstantin Maltsev <konstantin.maltsev@flant.com>
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>
    Co-authored-by: Konstantin Maltsev <konstantin.maltsev@flant.com>
    Co-authored-by: Yuriy Losev <yuriy.losev@flant.com>

--- modules/150-user-authn/hooks/discover_dex_ca_test.go
+++ modules/150-user-authn/hooks/discover_dex_ca_test.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2021 Flant JSC
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +14,8 @@ limitations under the License.
 package hooks
 
 import (
+	"fmt"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -32,11 +31,12 @@ var _ = Describe("User Authn hooks :: discover dex ca ::", func() {
 			f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts("", 0))
 			f.RunHook()
 		})
+
 		It("Should run", func() {
 			Expect(f).To(ExecuteSuccessfully())
 		})
 
-		Context("Adding secret", func() {
+		Context("Adding ingress-tls secret", func() {
 			BeforeEach(func() {
 				f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(`
 apiVersion: v1
@@ -56,7 +56,7 @@ data:
 			})
 		})
 
-		Context("Adding secret with empty ca.crt", func() {
+		Context("Adding ingress-tls secret with empty ca.crt", func() {
 			BeforeEach(func() {
 				f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(`
 apiVersion: v1
@@ -84,6 +84,7 @@ data:
 			f.ValuesSet("userAuthn.controlPlaneConfigurator.dexCAMode", "DoNotNeed")
 			f.RunHook()
 		})
+
 		It("Should add no ca for OIDC provider", func() {
 			Expect(f).To(ExecuteSuccessfully())
 			Expect(f.ValuesGet("userAuthn.internal.discoveredDexCA").String()).To(Equal(""))
@@ -97,9 +98,126 @@ data:
 			f.ValuesSet("userAuthn.controlPlaneConfigurator.dexCustomCA", "testca")
 			f.RunHook()
 		})
-		It("Should add no ca for OIDC provide from config", func() {
+
+		It("Should add no ca for OIDC provider from config", func() {
 			Expect(f).To(ExecuteSuccessfully())
 			Expect(f.ValuesGet("userAuthn.internal.discoveredDexCA").String()).To(Equal("testca"))
+		})
+	})
+
+	Context("Checking for CertManager and CustomcCertificate cases with secrets", func() {
+		BeforeEach(func() {
+			f.ValuesSet("userAuthn.controlPlaneConfigurator.dexCAMode", "FromIngressSecret")
+			f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts("", 0))
+			f.RunHook()
+		})
+
+		It("Should run", func() {
+			Expect(f).To(ExecuteSuccessfully())
+		})
+
+		Context("Matching with CertManager and ingress-tls", func() {
+			BeforeEach(func() {
+				f.ValuesSet("userAuthn.https.mode", "CertManager")
+				f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(`
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ingress-tls
+  namespace: d8-user-authn
+data:
+  tls.crt: dGVzdA==
+`, 2))
+				f.RunHook()
+			})
+
+			It("Should add tls.crt for OIDC provider", func() {
+				Expect(f).To(ExecuteSuccessfully())
+				Expect(f.ValuesGet("userAuthn.internal.discoveredDexCA").String()).To(Equal("test"))
+			})
+
+			It("Should check non-existing ingress-tls-customcertificate secret", func() {
+				Expect(f).To(ExecuteSuccessfully())
+				Expect(f.KubernetesResource("Secret", "d8-user-authn", "ingress-tls-customcertificate").Exists()).To(BeFalse())
+			})
+		})
+
+		Context("Matching with CustomCertificate and ingress-tls-customcertificate", func() {
+			BeforeEach(func() {
+				f.ValuesSet("userAuthn.https.mode", "CustomCertificate")
+				f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(`
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ingress-tls-customcertificate
+  namespace: d8-user-authn
+data:
+  tls.crt: dGVzdGNh
+`, 2))
+				f.RunHook()
+			})
+
+			It("Should add tls.crt for OIDC provider", func() {
+				Expect(f).To(ExecuteSuccessfully())
+				Expect(f.ValuesGet("userAuthn.internal.discoveredDexCA").String()).To(Equal("testca"))
+			})
+
+			It("Should check non-existing ingress-tls secret", func() {
+				Expect(f).To(ExecuteSuccessfully())
+				Expect(f.KubernetesResource("Secret", "d8-user-authn", "ingress-tls").Exists()).To(BeFalse())
+			})
+		})
+
+		Context("Incorrect matching between mode and secret", func() {
+			BeforeEach(func() {
+				f.ValuesSet("userAuthn.https.mode", "CustomCertificate")
+				f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(`
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ingress-tls
+  namespace: d8-user-authn
+data:
+  tls.crt: dGVzdA==
+`, 2))
+				f.RunHook()
+			})
+
+			It("Should generate an error", func() {
+				Expect(f).To(ExecuteSuccessfully())
+				Expect(fmt.Errorf("cannot convert dex ca certificate from snaphots"))
+			})
+		})
+
+		Context("Proper matching between mode and one of existing secrets", func() {
+			BeforeEach(func() {
+				f.ValuesSet("userAuthn.https.mode", "CustomCertificate")
+				f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(`
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ingress-tls-customcertificate
+  namespace: d8-user-authn
+data:
+  tls.crt: dGVzdGNh
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ingress-tls
+  namespace: d8-user-authn
+data:
+  tls.crt: dGVzdA==
+`, 2))
+				f.RunHook()
+			})
+
+			It("Should use only ingress-tls-customcertificate secret", func() {
+				Expect(f).To(ExecuteSuccessfully())
+				Expect(f.ValuesGet("userAuthn.https.mode").String()).Should(BeEquivalentTo("CustomCertificate"))
+				Expect(f.KubernetesResource("Secret", "d8-user-authn", "ingress-tls-customcertificate").Exists()).To(BeTrue())
+				Expect(f.ValuesGet("userAuthn.internal.discoveredDexCA").String()).To(Equal("testca"))
+			})
 		})
 	})
 })
