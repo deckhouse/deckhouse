bb0f6637f2eae78df29a502c5bc42ffdd68e8317
Makhonin Alexey <60808275+alex123012@users.noreply.github.com>
2023-03-23 21:00:43 +0300

    Add zones to provider cloud discovery data (#4136)
    Signed-off-by: alexey.makhonin <alexey.makhonin@flant.com>
    Co-authored-by: alexey.makhonin <alexey.makhonin@flant.com>

--- ee/modules/030-cloud-provider-vsphere/hooks/internal/v1/cloud_discovery_data.go
+++ ee/modules/030-cloud-provider-vsphere/hooks/internal/v1/cloud_discovery_data.go
@@ -6,8 +6,9 @@ Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https
 package v1
 
 type VsphereCloudDiscoveryData struct {
-	APIVersion       *string `json:"apiVersion,omitempty" yaml:"apiVersion,omitempty"`
-	Kind             *string `json:"kind,omitempty" yaml:"kind,omitempty"`
-	VMFolderPath     *string `json:"vmFolderPath,omitempty" yaml:"vmFolderPath,omitempty"`
-	ResourcePoolPath *string `json:"resourcePoolPath,omitempty" yaml:"resourcePoolPath,omitempty"`
+	APIVersion       *string  `json:"apiVersion,omitempty" yaml:"apiVersion,omitempty"`
+	Kind             *string  `json:"kind,omitempty" yaml:"kind,omitempty"`
+	VMFolderPath     *string  `json:"vmFolderPath,omitempty" yaml:"vmFolderPath,omitempty"`
+	ResourcePoolPath *string  `json:"resourcePoolPath,omitempty" yaml:"resourcePoolPath,omitempty"`
+	Zones            []string `json:"zones,omitempty" yaml:"zones,omitempty"`
 }
