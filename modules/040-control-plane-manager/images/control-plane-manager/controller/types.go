9171269f5e9a9d0990bb8fc2d00f6949b6ae5b74
Denis Romanenko <denis.romanenko@flant.com>
2023-03-31 22:17:15 +0300

    [control-plane-manager] refactor to go
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- modules/040-control-plane-manager/images/control-plane-manager/controller/types.go
+++ modules/040-control-plane-manager/images/control-plane-manager/controller/types.go
@@ -19,56 +19,26 @@ package main
 // Copied from https://github.com/giantswarm/kubeconfig/blob/master/types.go
 // KubeConfigValue is a struct used to create a kubectl configuration YAML file.
 type KubeConfigValue struct {
-	APIVersion     string                   `yaml:"apiVersion"`
-	Kind           string                   `yaml:"kind"`
-	Clusters       []KubeconfigNamedCluster `yaml:"clusters"`
-	Users          []KubeconfigUser         `yaml:"users"`
-	Contexts       []KubeconfigNamedContext `yaml:"contexts"`
-	CurrentContext string                   `yaml:"current-context"`
-	Preferences    struct{}                 `yaml:"preferences"`
+	Clusters []KubeconfigNamedCluster `yaml:"clusters"`
+	Users    []KubeconfigUser         `yaml:"users"`
 }
 
 // KubeconfigUser is a struct used to create a kubectl configuration YAML file
 type KubeconfigUser struct {
-	Name string                `yaml:"name"`
 	User KubeconfigUserKeyPair `yaml:"user"`
 }
 
 // KubeconfigUserKeyPair is a struct used to create a kubectl configuration YAML file
 type KubeconfigUserKeyPair struct {
-	ClientCertificateData string                 `yaml:"client-certificate-data"`
-	ClientKeyData         string                 `yaml:"client-key-data"`
-	AuthProvider          KubeconfigAuthProvider `yaml:"auth-provider,omitempty"`
-}
-
-// KubeconfigAuthProvider is a struct used to create a kubectl authentication provider
-type KubeconfigAuthProvider struct {
-	Name   string            `yaml:"name"`
-	Config map[string]string `yaml:"config"`
+	ClientCertificateData string `yaml:"client-certificate-data"`
 }
 
 // KubeconfigNamedCluster is a struct used to create a kubectl configuration YAML file
 type KubeconfigNamedCluster struct {
-	Name    string            `yaml:"name"`
 	Cluster KubeconfigCluster `yaml:"cluster"`
 }
 
 // KubeconfigCluster is a struct used to create a kubectl configuration YAML file
 type KubeconfigCluster struct {
-	Server                   string `yaml:"server"`
-	CertificateAuthorityData string `yaml:"certificate-authority-data"`
-	CertificateAuthority     string `yaml:"certificate-authority"`
-}
-
-// KubeconfigNamedContext is a struct used to create a kubectl configuration YAML file
-type KubeconfigNamedContext struct {
-	Name    string            `yaml:"name"`
-	Context KubeconfigContext `yaml:"context"`
-}
-
-// KubeconfigContext is a struct used to create a kubectl configuration YAML file
-type KubeconfigContext struct {
-	Cluster   string `yaml:"cluster"`
-	Namespace string `yaml:"namespace,omitempty"`
-	User      string `yaml:"user"`
+	Server string `yaml:"server"`
 }
