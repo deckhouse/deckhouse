33e3b05860bb49d0c7310e4d40af69ca4d279314
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2023-04-03 14:59:47 +0300

    backend: fix dexauthenticators -> dexproviders
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- ee/modules/810-ui/images/backend/handler.go
+++ ee/modules/810-ui/images/backend/handler.go
@@ -106,7 +106,7 @@ func initHandlers(
 		},
 		{gvr: schema.GroupVersionResource{Group: "deckhouse.io", Resource: "deckhousereleases", Version: "v1alpha1"}},
 		{gvr: schema.GroupVersionResource{Group: "deckhouse.io", Resource: "moduleconfigs", Version: "v1alpha1"}},
-		{gvr: schema.GroupVersionResource{Group: "deckhouse.io", Resource: "dexauthenticators", Version: "v1alpha1"}},
+		{gvr: schema.GroupVersionResource{Group: "deckhouse.io", Resource: "dexproviders", Version: "v1alpha1"}},
 		{gvr: schema.GroupVersionResource{Group: "deckhouse.io", Resource: "users", Version: "v1"}},
 
 		{
