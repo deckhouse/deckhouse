2a49c41fd8646eb559ef702a4c09306d185485ef
Konstantin Maltsev <114921649+Diaderius@users.noreply.github.com>
2023-04-07 11:28:08 +0300

    [candi] Remove early-oom (#3821)
    Signed-off-by: Konstantin Maltsev <konstantin.maltsev@flant.com>
    Signed-off-by: Andrey Klimentyev <andrey.klimentyev@flant.com>
    Co-authored-by: Denys Romanenko <65756796+RomanenkoDenys@users.noreply.github.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -288,7 +288,6 @@ var DefaultImagesDigests = map[string]interface{}{
 	"nodeManager": map[string]interface{}{
 		"bashibleApiserver":        "imageHash-nodeManager-bashibleApiserver",
 		"clusterAutoscaler":        "imageHash-nodeManager-clusterAutoscaler",
-		"earlyOom":                 "imageHash-nodeManager-earlyOom",
 		"machineControllerManager": "imageHash-nodeManager-machineControllerManager",
 	},
 	"openvpn": map[string]interface{}{
