6caad89e86f31fc1874cde58876879ac7f51ebec
Denis Romanenko <denis.romanenko@flant.com>
2023-04-11 12:11:22 +0300

    [cni-cilium] refactor build process
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

---
+++ modules/021-cni-cilium/images/builder/scripts/hubble/download-hubble.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# https://github.com/cilium/cilium/blob/v1.12.8/images/cilium/download-hubble.sh
+# Copyright Authors of Cilium
+# SPDX-License-Identifier: Apache-2.0
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# renovate: datasource=github-releases depName=cilium/hubble
+hubble_version="v0.11.2"
+
+declare -A hubble_sha256
+# renovate: datasource=github-releases depName=cilium/hubble digestVersion=v0.11.2
+hubble_sha256[amd64]="fa2b5a1468f17957899063ad2ff1b51c68aae955a2b5d09390174a7b36a80bdb"
+# renovate: datasource=github-releases depName=cilium/hubble digestVersion=v0.11.2
+hubble_sha256[arm64]="7e1496bdc937dcf34de2f282edf62f91edc7ed1435ad78401035dc04e6b66f90"
+
+for arch in amd64 arm64 ; do
+  curl --fail --show-error --silent --location "https://github.com/cilium/hubble/releases/download/${hubble_version}/hubble-linux-${arch}.tar.gz" --output "/tmp/hubble-${arch}.tgz"
+  printf "%s %s" "${hubble_sha256[${arch}]}" "/tmp/hubble-${arch}.tgz" | sha256sum -c
+  mkdir -p "/out/linux/${arch}/bin"
+  tar -C "/out/linux/${arch}/bin" -xf "/tmp/hubble-${arch}.tgz" hubble
+done
+
+x86_64-linux-gnu-strip /out/linux/amd64/bin/hubble
+aarch64-linux-gnu-strip /out/linux/arm64/bin/hubble
