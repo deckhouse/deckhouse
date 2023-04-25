721a39e8d874b192a8bbca5c5d3775841a77069d
Artem Kladov <artem.kladov@flant.com>
2023-04-25 19:57:13 +0300

    Update sidebar
    Signed-off-by: Artem Kladov <artem.kladov@flant.com>

--- docs/documentation/generate_k8s_sidebar.sh
+++ docs/documentation/generate_k8s_sidebar.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# the first argument is URL prefix for items in the YAML structure
+# the second argument is a starting indent for YAML structure
+
 function print_item {
   local DIR=${1:+$1/}
   local INDENT=$2
   local list=$(find $DIR -mindepth 1 -maxdepth 1 -type d -print | sed "s|^./||; s|^$DIR||" | sort)
 
   for item in $list ; do
+    echo $item | grep -Eq '^[./]?images$' && continue
     #echo "${DIR},${item}, ${list}"
     local title=$(grep -s 'title:' $DIR$item/index.html | head -n 1 | sed 's/^title: //')
     if [[ -z "${title}" ]] ; then
