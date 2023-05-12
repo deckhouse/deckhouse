efd62fdd5ff8727074a2ae60dd18918fad479f1c
Andrey Klimentyev <andrey.klimentyev@flant.com>
2023-05-12 18:57:23 +0300

    [descheduler] Provided a proper migration (#4606)
    Signed-off-by: Andrey Klimentyev <andrey.klimentyev@flant.com>

--- global-hooks/migrate/migrate_descheduler.go
+++ global-hooks/migrate/migrate_descheduler.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube/object_patch"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -30,6 +31,17 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	"github.com/deckhouse/deckhouse/go_lib/dependency"
+)
+
+const (
+	defaultLegacyDeschedulerConfigJSON = `{
+"removePodsViolatingInterPodAntiAffinity": true,
+"removePodsViolatingNodeAffinity": true
+}
+`
+
+	migrationCM = "descheduler-config-migration"
+	migrationNS = "d8-system"
 )
 
 var _ = sdk.RegisterFunc(&go_hook.HookConfig{
@@ -44,66 +56,71 @@ func deschedulerConfigMigration(input *go_hook.HookInput, dc dependency.Containe
 
 	mcGVR := schema.ParseGroupResource("moduleconfigs.deckhouse.io").WithVersion("v1alpha1")
 
+	_, err = kubeCl.CoreV1().ConfigMaps(migrationNS).Get(context.TODO(), migrationCM, metav1.GetOptions{})
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+	if !errors.IsNotFound(err) {
+		input.LogEntry.Infof("Migration cm %s already exists, skipping migration", migrationCM)
+		return nil
+	}
+
 	moduleConfig, err := kubeCl.Dynamic().Resource(mcGVR).Get(context.TODO(), "descheduler", metav1.GetOptions{})
 	if errors.IsNotFound(err) {
-		input.LogEntry.Info("ModuleConfig for descheduler does not exists, nothing to migrate")
-		return nil
+		input.LogEntry.Info("ModuleConfig for descheduler does not exists, migrating with default config")
 	} else if err != nil {
 		return err
 	}
 
-	mcVersion, exists, err := unstructured.NestedInt64(moduleConfig.UnstructuredContent(), "spec", "version")
-	if err != nil {
-		return err
-	}
-	if !exists {
-		return fmt.Errorf("moduleConfig does not exists, that should not be happening")
-	}
-	if mcVersion != 1 {
-		input.LogEntry.Infof("moduleConfig is not version 1, skipping migration")
-		return nil
-	}
-
-	moduleEnabled, exists, err := unstructured.NestedBool(moduleConfig.UnstructuredContent(), "spec", "enabled")
-	if err != nil {
-		return err
-	}
-	if exists && !moduleEnabled {
-		input.LogEntry.Infof("module explicitly disabled, skipping migration")
-		return nil
-	}
-
-	deschedulerSettings, exists, err := unstructured.NestedMap(moduleConfig.UnstructuredContent(), "spec", "settings")
+	deschedulerSettings, exists, err := extractDeschedulerSettingsFromMC(moduleConfig)
 	if err != nil {
 		return err
 	}
 
-	deschedulerConfigJSON := []byte("{}")
+	var deschedulerConfigJSON []byte
 	if exists && len(deschedulerSettings) > 0 {
 		deschedulerConfigJSON, err = json.Marshal(deschedulerSettings)
 		if err != nil {
 			return err
 		}
 	} else {
-		input.LogEntry.Info("Config for descheduler is empty, but module is enabled, migrating without config")
+		deschedulerConfigJSON = []byte(defaultLegacyDeschedulerConfigJSON)
 	}
 
-	_, err = kubeCl.CoreV1().ConfigMaps("d8-system").Create(context.TODO(), &corev1.ConfigMap{
+	cm := &corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "v1",
 			Kind:       "ConfigMap",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "descheduler-config-migration",
-			Namespace: "d8-system",
+			Name:      migrationCM,
+			Namespace: migrationNS,
 		},
 		Data: map[string]string{"config": string(deschedulerConfigJSON)},
-	}, metav1.CreateOptions{})
-	if errors.IsAlreadyExists(err) {
-		input.LogEntry.Infof("CM already existis, skipping migration: %s", err)
-	} else if err != nil {
-		return err
 	}
 
+	input.PatchCollector.Create(cm, object_patch.UpdateIfExists())
+
 	return nil
+}
+
+func extractDeschedulerSettingsFromMC(mc *unstructured.Unstructured) (map[string]interface{}, bool, error) {
+	if mc == nil {
+		return nil, false, nil
+	}
+
+	moduleEnabled, exists, err := unstructured.NestedBool(mc.UnstructuredContent(), "spec", "enabled")
+	if err != nil {
+		return nil, false, err
+	}
+	if exists && !moduleEnabled {
+		return nil, false, nil
+	}
+
+	deschedulerSettings, _, err := unstructured.NestedMap(mc.UnstructuredContent(), "spec", "settings")
+	if err != nil {
+		return deschedulerSettings, false, err
+	}
+
+	return deschedulerSettings, true, nil
 }
