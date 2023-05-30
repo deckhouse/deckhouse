7caf7690233765244defb0f45488ebafa4fa699d
Makhonin Alexey <60808275+alex123012@users.noreply.github.com>
2023-05-30 17:37:08 +0300

    [operator-trivy] Add configuration for `tolerations` and `nodeSelector` for module (#4721)
    Signed-off-by: alexey.makhonin <alexey.makhonin@flant.com>

--- ee/modules/500-operator-trivy/template_tests/template_test.go
+++ ee/modules/500-operator-trivy/template_tests/template_test.go
@@ -10,6 +10,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
 
 	. "github.com/deckhouse/deckhouse/testing/helm"
 )
@@ -19,7 +20,8 @@ func Test(t *testing.T) {
 	RunSpecs(t, "")
 }
 
-const globalValues = `
+const (
+	globalValues = `
 enabledModules: ["vertical-pod-autoscaler-crd", "operator-trivy"]
 modulesImages:
   registry:
@@ -34,18 +36,65 @@ discovery:
   clusterDomain: cluster.local
 `
 
+	scanJobsValues = `
+tolerations:
+  - key: "key1"
+    operator: "Equal"
+    value: "value1"
+    effect: "NoSchedule"
+nodeSelector:
+  test-label: test-value
+`
+)
+
 var _ = Describe("Module :: operator-trivy :: helm template :: custom-certificate", func() {
 	f := SetupHelmConfig(``)
 
+	checkTivyOperatorCM := func(f *Config, tolerations, nodeSelector types.GomegaMatcher) {
+		cm := f.KubernetesResource("ConfigMap", "d8-operator-trivy", "trivy-operator")
+		Expect(cm.Exists()).To(BeTrue())
+
+		cmdData := cm.Field(`data`).Map()
+
+		Expect(cmdData["scanJob.tolerations"].String()).To(tolerations)
+		Expect(cmdData["scanJob.nodeSelector"].String()).To(nodeSelector)
+
+	}
+
+	BeforeEach(func() {
+		f.ValuesSetFromYaml("global", globalValues)
+		f.ValuesSet("global.modulesImages", GetModulesImages())
+		f.HelmRender()
+	})
+
 	Context("Default", func() {
 		BeforeEach(func() {
-			f.ValuesSetFromYaml("global", globalValues)
-			f.ValuesSet("global.modulesImages", GetModulesImages())
 			f.HelmRender()
 		})
 
 		It("Everything must render properly for default cluster", func() {
 			Expect(f.RenderError).ShouldNot(HaveOccurred())
+		})
+
+		It("Operator trivy configmap has proper tolerations and nodeSelector", func() {
+			checkTivyOperatorCM(f, Equal(""), Equal(""))
+		})
+	})
+
+	Context("Operator trivy with custom tolerations and nodeSelector", func() {
+		BeforeEach(func() {
+			f.ValuesSetFromYaml("operatorTrivy", scanJobsValues)
+			f.HelmRender()
+		})
+
+		It("Everything must render properly for cluster", func() {
+			Expect(f.RenderError).ShouldNot(HaveOccurred())
+		})
+
+		It("Operator trivy configmap has proper tolerations and nodeSelector", func() {
+			tolerations := `[{"effect":"NoSchedule","key":"key1","operator":"Equal","value":"value1"}]`
+			nodeSelector := `{"test-label":"test-value"}`
+			checkTivyOperatorCM(f, MatchJSON(tolerations), MatchJSON(nodeSelector))
 		})
 	})
 
