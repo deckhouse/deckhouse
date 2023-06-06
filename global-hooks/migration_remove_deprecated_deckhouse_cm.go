42980ab05000f89f78db023908142a26b24f78f2
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2023-06-06 15:22:28 +0300

    [global] Remove deprecated cm deckhouse
    Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

---
+++ global-hooks/migration_remove_deprecated_deckhouse_cm.go
@@ -0,0 +1,62 @@
+// Copyright 2023 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hooks
+
+import (
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	v1core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/utils/pointer"
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "deckhouse_cm",
+			ApiVersion: "v1",
+			Kind:       "ConfigMap",
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{"deckhouse"},
+			},
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"d8-system"},
+				},
+			},
+			ExecuteHookOnEvents:          pointer.Bool(false),
+			ExecuteHookOnSynchronization: pointer.Bool(true),
+			FilterFunc:                   applyDeckhouseConfigmapFilter,
+		},
+	},
+}, migrationRemoveDeprecatedConfigmapDeckhouse)
+
+func applyDeckhouseConfigmapFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	var cm v1core.ConfigMap
+	err := sdk.FromUnstructured(obj, &cm)
+	if err != nil {
+		return "", err
+	}
+	return "", nil
+}
+
+func migrationRemoveDeprecatedConfigmapDeckhouse(input *go_hook.HookInput) error {
+	deckhouseConfigSnap := input.Snapshots["deckhouse_cm"]
+	if len(deckhouseConfigSnap) > 0 {
+		input.PatchCollector.Delete("v1", "Configmap", "d8-system", "deckhouse")
+	}
+	return nil
+}
---
+++ global-hooks/migration_remove_deprecated_deckhouse_cm_test.go
@@ -0,0 +1,49 @@
+// Copyright 2023 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hooks
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/deckhouse/deckhouse/testing/hooks"
+)
+
+const (
+	deckhouseConfigmap = `
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: deckhouse
+  namespace: d8-system
+`
+)
+
+var _ = Describe("Global hooks :: migration_remove_deprecated_deckhouse_cm ", func() {
+	f := HookExecutionConfigInit(`{"global": {"discovery": {}}}`, `{}`)
+
+	Context("Cluster with old deckhouse Configmap", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(deckhouseConfigmap))
+			f.RunHook()
+		})
+
+		It("ExecuteSuccessfully", func() {
+			Expect(f.KubernetesResource("Configmap", "d8-system", "deckhouse").Exists()).Should(BeFalse())
+		})
+	})
+
+})
