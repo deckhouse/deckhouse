62f543123fa621555272545ed4e728af2b683dca
Alexey Gavrilov <alexey.gavrilov@flant.com>
2023-06-26 19:28:23 +0300

    [node-manager] added tests spec.properties.networkProfile.acceleratedNetworking in AzureMachineClass
    Signed-off-by: Alexey Gavrilov <alexey.gavrilov@flant.com>

--- modules/040-node-manager/template_tests/module_test.go
+++ modules/040-node-manager/template_tests/module_test.go
@@ -173,13 +173,41 @@ internal:
       type: "Docker"
     cloudInstances:
       classReference:
-        kind: GCPInstanceClass
+        kind: AzureInstanceClass
         name: worker
       maxPerZone: 5
       minPerZone: 2
       zones:
       - zonea
       - zoneb
+  - name: aaa
+    instanceClass:
+      acceleratedNetworking: false
+      machineSize: test
+      urn: test
+    nodeType: CloudEphemeral
+    cloudInstances:
+      classReference:
+        kind: AzureInstanceClass
+        name: test
+      maxPerZone: 1
+      minPerZone: 1
+      zones:
+      - zonea
+  - name: bbb
+    instanceClass:
+      acceleratedNetworking: true
+      machineSize: bbb
+      urn: zzz
+    nodeType: CloudEphemeral
+    cloudInstances:
+      classReference:
+        kind: AzureInstanceClass
+        name: worker
+      maxPerZone: 1
+      minPerZone: 1
+      zones:
+      - zonea
   machineControllerManagerEnabled: true
 `
 
@@ -1354,6 +1382,33 @@ ccc: ddd
 
 				Expect(mcls.Exists()).To(BeTrue())
 				assertValues(mcls, "spec.tags")
+			})
+
+			It("spec.properties.networkProfile.acceleratedNetworking default true", func() {
+				Expect(f.RenderError).ShouldNot(HaveOccurred())
+
+				t := f.KubernetesResource("AzureMachineClass", "d8-cloud-instance-manager", "worker-02320933")
+				Expect(t.Exists()).To(BeTrue())
+				//checksum := mcdt.Field("spec.properties.networkProfile.acceleratedNetworking").String()
+				Expect(t.Field("spec.properties.networkProfile.acceleratedNetworking").String()).To(Equal("true"))
+			})
+
+			It("spec.properties.networkProfile.acceleratedNetworking true", func() {
+				Expect(f.RenderError).ShouldNot(HaveOccurred())
+
+				t := f.KubernetesResource("AzureMachineClass", "d8-cloud-instance-manager", "aaa-02320933")
+				Expect(t.Exists()).To(BeTrue())
+				//checksum := mc.Field("spec.properties.networkProfile.acceleratedNetworking").String()
+				Expect(t.Field("spec.properties.networkProfile.acceleratedNetworking").String()).To(Equal("true"))
+			})
+
+			It("spec.properties.networkProfile.acceleratedNetworking false", func() {
+				Expect(f.RenderError).ShouldNot(HaveOccurred())
+
+				t := f.KubernetesResource("AzureMachineClass", "d8-cloud-instance-manager", "bbb-02320933")
+				Expect(t.Exists()).To(BeTrue())
+				//checksum := mc.Field("spec.properties.networkProfile.acceleratedNetworking").String()
+				Expect(t.Field("spec.properties.networkProfile.acceleratedNetworking").String()).To(Equal("false"))
 			})
 
 			// Important! If checksum changes, the MachineDeployments will re-deploy!
