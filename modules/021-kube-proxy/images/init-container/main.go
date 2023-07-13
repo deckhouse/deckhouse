40b579a8ba6b0dd985da7e730d72f6452e853080
Artem Kladov <artem.kladov@flant.com>
2023-07-13 22:03:01 +0300

    [kube-proxy] Update dependencies
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>
    Signed-off-by: Artem Kladov <artem.kladov@flant.com>

--- modules/021-kube-proxy/images/init-container/main.go
+++ modules/021-kube-proxy/images/init-container/main.go
@@ -23,7 +23,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/Masterminds/semver"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
