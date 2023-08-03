25e29528dbc085f75aeb223073c36b5d9688c2fb
Alexey Sulimenko <alexey.sulimenko@flant.com>
2023-08-03 15:56:06 +0300

    [distroless] prometheus
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- modules/300-prometheus/images/trickster/expand-conf/main.go
+++ modules/300-prometheus/images/trickster/expand-conf/main.go
@@ -10,6 +10,7 @@ func main() {
 	content, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		os.Exit(1)
 	}
 	fmt.Fprint(os.Stdout, os.ExpandEnv(string(content)))
 }
