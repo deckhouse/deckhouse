0d4a9da431735d9a46d58cdf3fd6d62b08a46362
Artem Kladov <artem.kladov@flant.com>
2023-08-09 09:46:44 +0300

    [testing] Use K8s 1.25 as the default version
    Signed-off-by: Artem Kladov <artem.kladov@flant.com>

--- .github/scripts/js/ci.js
+++ .github/scripts/js/ci.js
@@ -1195,7 +1195,7 @@ You can trigger release related actions by commenting on this issue:
 
 
 **Note 1:**
-A single command \`/e2e/run/<provider>\` will run e2e with default CRI 'containerd' and Kubernetes version '1.23'.
+A single command \`/e2e/run/<provider>\` will run e2e with default CRI 'containerd' and Kubernetes version '1.25'.
 Put \`/e2e/use\` options below \`/e2e/run\` command to set specific CRI and Kubernetes version. E.g.:
 
 \`\`\`
