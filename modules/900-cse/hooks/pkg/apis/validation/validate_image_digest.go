546c88b18fbf425ad46441583379a306a7e4bf72
Alexey Sulimenko <alexey.sulimenko@flant.com>
2023-08-22 16:25:25 +0300

    [webhook] Image digest check webhook
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

---
+++ modules/900-cse/hooks/pkg/apis/validation/validate_image_digest.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2023 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validation
+
+import (
+	"context"
+	"net/http"
+
+	kwhhttp "github.com/slok/kubewebhook/v2/pkg/http"
+	"github.com/slok/kubewebhook/v2/pkg/model"
+	kwhvalidating "github.com/slok/kubewebhook/v2/pkg/webhook/validating"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func imageDigestValidationHandler() http.Handler {
+	vf := kwhvalidating.ValidatorFunc(func(ctx context.Context, review *model.AdmissionReview, obj metav1.Object) (result *kwhvalidating.ValidatorResult, err error) {
+		return allowResult("")
+	})
+
+	// Create webhook.
+	wh, _ := kwhvalidating.NewWebhook(kwhvalidating.WebhookConfig{
+		ID:        "image-digest-validation",
+		Validator: vf,
+		Logger:    validationLogger,
+		Obj:       &corev1.Pod{},
+	})
+
+	return kwhhttp.MustHandlerFor(kwhhttp.HandlerConfig{Webhook: wh, Logger: validationLogger})
+}
