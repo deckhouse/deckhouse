0ab2a1616c299b976daa7703abf0da985539e518
Yuriy Losev <yuriy.losev@flant.com>
2023-08-23 00:42:44 +0400

    Handle also Endpoints
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>

--- global-hooks/create_endpoints.go
+++ global-hooks/create_endpoints.go
@@ -17,7 +17,11 @@ limitations under the License.
 package hooks
 
 import (
+	"context"
 	"os"
+
+	"github.com/deckhouse/deckhouse/go_lib/dependency"
+	"github.com/deckhouse/deckhouse/go_lib/dependency/k8s"
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
@@ -32,24 +36,84 @@ import (
 // But Deckhouse itself has ValidationWebhooks that should be executed even when pod is not ready.
 // Endpoints created via service do not go to ready state in this case and we cannot use validation.
 
+const (
+	d8Namespace = "d8-system"
+	d8Name      = "deckhouse"
+)
+
 var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 	OnStartup: &go_hook.OrderedConfig{Order: 1},
-}, generateDeckhouseEndpoints)
+}, dependency.WithExternalDependencies(generateDeckhouseEndpoints))
 
-func generateDeckhouseEndpoints(input *go_hook.HookInput) error {
+func generateDeckhouseEndpoints(input *go_hook.HookInput, dc dependency.Container) error {
+	client, err := dc.GetK8sClient()
+	if err != nil {
+		return err
+	}
+	err = cleanupOldEndpointSlices(client)
+	if err != nil {
+		return err
+	}
+
+	ep := &v1.Endpoints{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Endpoints",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      d8Name,
+			Namespace: d8Namespace,
+			Labels: map[string]string{
+				"app":                        d8Name,
+				"module":                     d8Name,
+				"heritage":                   d8Name,
+				"kubernetes.io/service-name": d8Name,
+			},
+		},
+		Subsets: []v1.EndpointSubset{
+			{
+				Addresses: []v1.EndpointAddress{
+					{
+						IP:       os.Getenv("ADDON_OPERATOR_LISTEN_ADDRESS"),
+						Hostname: os.Getenv("DECKHOUSE_NODE_NAME"),
+						NodeName: pointer.String(os.Getenv("DECKHOUSE_NODE_NAME")),
+						TargetRef: &v1.ObjectReference{
+							Kind:       "Pod",
+							Namespace:  d8Namespace,
+							Name:       os.Getenv("DECKHOUSE_POD"),
+							APIVersion: "v1",
+						},
+					},
+				},
+				Ports: []v1.EndpointPort{
+					{
+						Name:     "self",
+						Port:     9650,
+						Protocol: v1.ProtocolTCP,
+					},
+					{
+						Name:     "webhook",
+						Port:     9651,
+						Protocol: v1.ProtocolTCP,
+					},
+				},
+			},
+		},
+	}
+
 	es := &discv1.EndpointSlice{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "EndpointSlice",
 			APIVersion: "discovery.k8s.io/v1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "deckhouse",
-			Namespace: "d8-system",
+			Name:      d8Name,
+			Namespace: d8Namespace,
 			Labels: map[string]string{
-				"app":                        "deckhouse",
-				"module":                     "deckhouse",
-				"heritage":                   "deckhouse",
-				"kubernetes.io/service-name": "deckhouse",
+				"app":                        d8Name,
+				"module":                     d8Name,
+				"heritage":                   d8Name,
+				"kubernetes.io/service-name": d8Name,
 			},
 		},
 		AddressType: "IPv4",
@@ -64,7 +128,7 @@ func generateDeckhouseEndpoints(input *go_hook.HookInput) error {
 				Hostname: pointer.String(os.Getenv("DECKHOUSE_NODE_NAME")),
 				TargetRef: &v1.ObjectReference{
 					Kind:      "Pod",
-					Namespace: "d8-system",
+					Namespace: d8Namespace,
 					Name:      os.Getenv("DECKHOUSE_POD"),
 				},
 				NodeName: pointer.String(os.Getenv("DECKHOUSE_NODE_NAME")),
@@ -84,7 +148,26 @@ func generateDeckhouseEndpoints(input *go_hook.HookInput) error {
 		},
 	}
 
+	input.PatchCollector.Create(ep, object_patch.UpdateIfExists())
 	input.PatchCollector.Create(es, object_patch.UpdateIfExists())
+
+	return nil
+}
+
+// we have to clean old EndpointSlices, otherwise in a multi-master cluster it can lead to the Request Timeout error
+func cleanupOldEndpointSlices(client k8s.Client) error {
+	list, err := client.DiscoveryV1().EndpointSlices(d8Namespace).List(context.Background(), metav1.ListOptions{LabelSelector: "app=deckhouse,heritage=deckhouse,endpointslice.kubernetes.io/managed-by=endpointslice-controller.k8s.io"})
+	if err != nil {
+		return err
+	}
+
+	policy := metav1.DeletePropagationBackground
+	for _, item := range list.Items {
+		err = client.DiscoveryV1().EndpointSlices(d8Namespace).Delete(context.Background(), item.Name, metav1.DeleteOptions{PropagationPolicy: &policy})
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
