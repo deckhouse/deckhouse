0744c52c94da17dd67b70a1f28870e553f3c574b
Vasily Oleynikov <vasily.oleynikov@flant.com>
2023-08-22 21:40:38 +0500

    [e2e] linstor tests in e2e static is temporary disabled (#5605)
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- testing/cloud_layouts/script.sh
+++ testing/cloud_layouts/script.sh
@@ -778,15 +778,15 @@ function wait_cluster_ready() {
   infoScript=$(cat "$(pwd)/testing/cloud_layouts/script.d/wait_cluster_ready/info_script.sh")
   $ssh_command -i "$ssh_private_key_path" $ssh_bastion "$ssh_user@$master_ip" sudo su -c /bin/bash <<<"${infoScript}";
 
-  if [[ "$PROVIDER" == "Static" ]]; then
-    if ! run_linstor_tests; then
-      >&2 echo -n "Linstor tests failed"
-      >&2 echo -n "Fetch Deckhouse logs after test ..."
-      $ssh_command -i "$ssh_private_key_path" $ssh_bastion "$ssh_user@$master_ip" sudo su -c /bin/bash > "$logs/deckhouse.json.log" <<<"${testLog}"
-      return 1
-    fi
-  fi
-  echo "Linstor test suite: success"
+#  if [[ "$PROVIDER" == "Static" ]]; then
+#    if ! run_linstor_tests; then
+#      >&2 echo -n "Linstor tests failed"
+#      >&2 echo -n "Fetch Deckhouse logs after test ..."
+#      $ssh_command -i "$ssh_private_key_path" $ssh_bastion "$ssh_user@$master_ip" sudo su -c /bin/bash > "$logs/deckhouse.json.log" <<<"${testLog}"
+#      return 1
+#    fi
+#  fi
+#  echo "Linstor test suite: success"
 
   test_failed=
 
