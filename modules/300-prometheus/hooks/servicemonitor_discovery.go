35e7c7cbd580b66e423ccbb325e51ec6de7635a6
Yuriy Losev <yuriy.losev@flant.com>
2023-09-22 01:37:12 +0400

    ++
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>

---
+++ modules/300-prometheus/hooks/servicemonitor_discovery.go
@@ -0,0 +1,141 @@
+package hooks
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook/metrics"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Queue: "/modules/prometheus/servicemonitors",
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "servicemonitors",
+			ApiVersion: "monitoring.coreos.com/v1",
+			Kind:       "ServiceMonitor",
+			LabelSelector: &v1.LabelSelector{
+				MatchLabels: map[string]string{
+					"prometheus": "main",
+				},
+			},
+			FilterFunc: filterServiceMonitor,
+		},
+	},
+}, serviceMonitorHandler)
+
+func filterServiceMonitor(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	var sm serviceMonitor
+	err := sdk.FromUnstructured(obj, &sm)
+	if err != nil {
+		return nil, err
+	}
+
+	return sm, nil
+}
+
+func serviceMonitorHandler(input *go_hook.HookInput) error {
+	input.MetricsCollector.Expire("d8_servicemonitors")
+
+	snap := input.Snapshots["servicemonitors"]
+
+	for _, sn := range snap {
+		serviceMon := sn.(serviceMonitor)
+
+	serviceMonitorLoop:
+		for _, endpoint := range serviceMon.Spec.Endpoints {
+			for _, relabel := range endpoint.Relabelings {
+				tmpMap := make(map[string]struct{}, len(relabel.SourceLabels))
+				for _, sourceLabel := range relabel.SourceLabels {
+					if strings.HasPrefix(sourceLabel, "__meta_kubernetes_endpoint") {
+						sourceLabel = mutateLabelOrAnnotationSource(sourceLabel)
+						fmt.Println("<SDD", sourceLabel)
+						tmpMap[sourceLabel] = struct{}{}
+					}
+				}
+
+				if len(tmpMap) > 0 {
+					// check if sourceLabel has `__endpoint` label but does not have `__endpoint_slice` label
+					for k, v := range endpointsMap {
+						_, ok1 := tmpMap[k]
+						_, ok2 := tmpMap[v]
+						if ok1 && !ok2 {
+							input.MetricsCollector.Set("d8_prometheus_deprecated_servicemonitor", 1, map[string]string{"name": serviceMon.Metadata.Name, "namespace": serviceMon.Metadata.Namespace}, metrics.WithGroup("d8_servicemonitors"))
+							break serviceMonitorLoop
+						}
+					}
+				}
+
+			}
+		}
+	}
+	return nil
+}
+
+func mutateLabelOrAnnotationSource(label string) string {
+	if strings.HasPrefix(label, "__meta_kubernetes_endpoints_label_") {
+		return "__meta_kubernetes_endpoints_label_"
+	}
+	if strings.HasPrefix(label, "__meta_kubernetes_endpoints_labelpresent_") {
+		return "__meta_kubernetes_endpoints_labelpresent_"
+	}
+	if strings.HasPrefix(label, "__meta_kubernetes_endpoints_annotation_") {
+		return "__meta_kubernetes_endpoints_annotation_"
+	}
+	if strings.HasPrefix(label, "__meta_kubernetes_endpoints_annotationpresent_") {
+		return "__meta_kubernetes_endpoints_annotationpresent_"
+	}
+	if strings.HasPrefix(label, "__meta_kubernetes_endpointslice_label_") {
+		return "__meta_kubernetes_endpointslice_label_"
+	}
+	if strings.HasPrefix(label, "__meta_kubernetes_endpointslice_labelpresent_") {
+		return "__meta_kubernetes_endpointslice_labelpresent_"
+	}
+	if strings.HasPrefix(label, "__meta_kubernetes_endpointslice_annotation_") {
+		return "__meta_kubernetes_endpointslice_annotation_"
+	}
+	if strings.HasPrefix(label, "__meta_kubernetes_endpointslice_annotationpresent_") {
+		return "__meta_kubernetes_endpointslice_annotationpresent_"
+	}
+
+	return label
+}
+
+// cutted ServiceMonitor
+type serviceMonitor struct {
+	Metadata struct {
+		Name      string `json:"name"`
+		Namespace string `json:"namespace"`
+	} `json:"metadata"`
+	Spec struct {
+		Endpoints []struct {
+			Relabelings []struct {
+				SourceLabels []string `json:"sourceLabels"`
+			} `json:"relabelings"`
+		} `json:"endpoints"`
+	} `json:"spec"`
+}
+
+var (
+	endpointsMap = map[string]string{
+		"__meta_kubernetes_endpoints_name":               "__meta_kubernetes_endpointslice_name",
+		"__meta_kubernetes_endpoint_node_name":           "__meta_kubernetes_endpointslice_endpoint_topology_kubernetes_io_hostname",
+		"__meta_kubernetes_endpoint_ready":               "__meta_kubernetes_endpointslice_endpoint_conditions_ready",
+		"__meta_kubernetes_endpoint_port_name":           "__meta_kubernetes_endpointslice_port_name",
+		"__meta_kubernetes_endpoint_port_protocol":       "__meta_kubernetes_endpointslice_port_protocol",
+		"__meta_kubernetes_endpoint_address_target_kind": "__meta_kubernetes_endpointslice_address_target_kind",
+		"__meta_kubernetes_endpoint_address_target_name": "__meta_kubernetes_endpointslice_address_target_name",
+
+		// prefix labels
+		"__meta_kubernetes_endpoints_label_":             "__meta_kubernetes_endpointslice_label_",
+		"__meta_kubernetes_endpoints_labelpresent_":      "__meta_kubernetes_endpointslice_labelpresent_",
+		"__meta_kubernetes_endpoints_annotation_":        "__meta_kubernetes_endpointslice_annotation_",
+		"__meta_kubernetes_endpoints_annotationpresent_": "__meta_kubernetes_endpointslice_annotationpresent_",
+	}
+)
