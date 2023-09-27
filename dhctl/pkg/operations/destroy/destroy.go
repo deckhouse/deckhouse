35de29e8fd776495dd303771726d22c101ab8cc7
Timofey Kirillov <timofey.kirillov@flant.com>
2023-09-27 12:46:02 +0300

    [dhctl] fix(dhctl-for-commander): correct stop-operation condition handling in the phased-execution-context
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

--- dhctl/pkg/operations/destroy/destroy.go
+++ dhctl/pkg/operations/destroy/destroy.go
@@ -110,7 +110,7 @@ func (d *ClusterDestroyer) DestroyCluster(autoApprove bool) error {
 	}
 
 	if !d.skipResources {
-		if shouldStop, err := d.PhasedExecutionContext.StartPhase(phases.DeleteResourcesPhase, false); err != nil {
+		if shouldStop, err := d.PhasedExecutionContext.StartPhase(phases.DeleteResourcesPhase, false, d.stateCache); err != nil {
 			return err
 		} else if shouldStop {
 			return nil
@@ -149,7 +149,7 @@ func (d *ClusterDestroyer) DestroyCluster(autoApprove bool) error {
 	}
 
 	d.state.Clean()
-	return d.PhasedExecutionContext.Complete()
+	return d.PhasedExecutionContext.Complete(d.stateCache)
 }
 
 type StaticMastersDestroyer struct {
