60eea0e08417a619f251604add496c792441dff8
Mikhail Scherba <41360396+miklezzzz@users.noreply.github.com>
2023-09-29 21:28:25 +0300

    [ingress-nginx] apply postpone update to main controller of HostWithFailover inlet (#5988)
    Signed-off-by: Mikhail Scherba <mikhail.scherba@flant.com>
    Co-authored-by: Mikhail Scherba <mikhail.scherba@flant.com>

--- modules/402-ingress-nginx/hooks/safe_daemonset_update_test.go
+++ modules/402-ingress-nginx/hooks/safe_daemonset_update_test.go
@@ -27,12 +27,86 @@ var _ = Describe("ingress-nginx :: hooks :: safe_daemonset_update ::", func() {
 	f := HookExecutionConfigInit(`{"ingressNginx":{"defaultControllerVersion": "1.1", "internal": {}}}`, "")
 	f.RegisterCRD("apps.kruise.io", "v1alpha1", "DaemonSet", true)
 
-	Context("Failover pods are ready", func() {
+	Context("Failover pods are ready, update postponed", func() {
 		BeforeEach(func() {
 			st := f.KubeStateSet(`
 apiVersion: v1
 kind: Pod
 metadata:
+  annotations:
+    lifecycle.apps.kruise.io/timestamp: "2023-09-28T14:44:21Z"
+  labels:
+    app: controller
+    ingress.deckhouse.io/block-deleting: "true"
+    lifecycle.apps.kruise.io/state: "PreparingDelete"
+    name: test
+  name: controller-test-bw8sc
+  namespace: d8-ingress-nginx
+spec:
+  containers:
+  - name: controller
+  nodeName: ndev-worker-5e11c78a-5f688-kw6c5
+status:
+  conditions:
+  - lastProbeTime: null
+    lastTransitionTime: "2023-03-24T15:02:56Z"
+    status: "True"
+    type: Ready
+---
+apiVersion: apps.kruise.io/v1alpha1
+kind: DaemonSet
+metadata:
+  annotations:
+    ingress-nginx-controller.deckhouse.io/checksum: a6fca03cfb274eaa9d1def32dde2bd730ac204baa941ddd88685b47e9b487787
+  labels:
+    app: controller
+    ingress-nginx-failover: ""
+    name: test-failover
+  name: controller-test-failover
+  namespace: d8-ingress-nginx
+spec: {}
+status:
+  desiredNumberScheduled: 3
+  numberAvailable: 3
+  updatedNumberScheduled: 3
+---
+apiVersion: apps.kruise.io/v1alpha1
+kind: DaemonSet
+metadata:
+  annotations:
+    ingress-nginx-controller.deckhouse.io/checksum: a6fca03cfb274eaa9d1def32dde2bd730ac204baa941ddd88685b47e9b487787
+  labels:
+    app: proxy-failover
+    name: test
+  name: proxy-test-failover
+  namespace: d8-ingress-nginx
+spec: {}
+status:
+  desiredNumberScheduled: 3
+  numberAvailable: 3
+  updatedNumberScheduled: 3
+`)
+
+			f.BindingContexts.Set(st)
+			f.RunHook()
+		})
+
+		It("Shouldn't remove blocking label", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			pod := f.KubernetesResource("Pod", "d8-ingress-nginx", "controller-test-bw8sc")
+			Expect(pod.Field("metadata.labels.ingress\\.deckhouse\\.io/block-deleting").Exists()).To(BeTrue())
+			Expect(pod.Field("metadata.annotations.ingress\\.deckhouse\\.io/update-postponed-at").Exists()).To(BeTrue())
+		})
+	})
+
+	Context("Failover pods are ready, pod update was postponed", func() {
+		BeforeEach(func() {
+			st := f.KubeStateSet(`
+apiVersion: v1
+kind: Pod
+metadata:
+  annotations:
+    ingress.deckhouse.io/update-postponed-at: "2023-03-24T15:02:56Z"
   labels:
     app: controller
     ingress.deckhouse.io/block-deleting: "true"
@@ -102,6 +176,8 @@ status:
 apiVersion: v1
 kind: Pod
 metadata:
+  annotations:
+    lifecycle.apps.kruise.io/timestamp: "2023-09-28T14:44:21Z"
   labels:
     app: controller
     ingress.deckhouse.io/block-deleting: "true"
@@ -171,6 +247,8 @@ status:
 apiVersion: v1
 kind: Pod
 metadata:
+  annotations:
+    lifecycle.apps.kruise.io/timestamp: "2023-09-28T14:44:21Z"
   labels:
     app: controller
     ingress.deckhouse.io/block-deleting: "true"
@@ -240,6 +318,8 @@ status:
 apiVersion: v1
 kind: Pod
 metadata:
+  annotations:
+    lifecycle.apps.kruise.io/timestamp: "2023-09-28T14:44:21Z"
   labels:
     app: controller
     ingress.deckhouse.io/block-deleting: "true"
