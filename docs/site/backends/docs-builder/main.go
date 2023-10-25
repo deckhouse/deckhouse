f70d525099cecc6459ef1022f830b28686458205
Yuriy Losev <yuriy.losev@flant.com>
2023-10-25 17:17:20 +0400

    Ports
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>
    Signed-off-by: Artem Kladov <artem.kladov@flant.com>

--- docs/site/backends/docs-builder/main.go
+++ docs/site/backends/docs-builder/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) { _, _ = w.Write([]byte("ok")) })
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { _, _ = w.Write([]byte("hello world")) })
-	_ = http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
