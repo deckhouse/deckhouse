909103f942bb9d32962d2a5d4bc9c11b153b21e0
Daniil <daniil.stepanenko@flant.com>
2023-10-31 13:21:38 +0300

    fix path
    Signed-off-by: Daniil <daniil.stepanenko@flant.com>

---
+++ docs/site/backends/docs-builder/upload_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func TestLoadHandlerGetLocalPath(t *testing.T) {
+	tests := []struct {
+		fileName string
+		want     string
+		wantOK   bool
+	}{
+		{
+			"docs/install.md",
+			"/app/hugo/content/moduleName/stable/install.md",
+			true,
+		},
+		{
+			"docs",
+			"/app/hugo/content/moduleName/stable",
+			true,
+		},
+		{
+			"not-docs/file.ext",
+			"",
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.fileName, func(t *testing.T) {
+			u := newLoadHandler("/app/hugo/")
+
+			got, ok := u.getLocalPath("moduleName", "stable", tt.fileName)
+			if got != tt.want || ok != tt.wantOK {
+				t.Errorf("getLocalPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
