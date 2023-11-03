75d2686f608552c9e7af1784e9311a61b541c3d9
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2023-11-03 16:40:39 +0300

    [dhctl] Fix tests
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- dhctl/pkg/util/maputil/maputil_test.go
+++ dhctl/pkg/util/maputil/maputil_test.go
@@ -15,7 +15,6 @@
 package maputil
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -185,9 +184,8 @@ func TestFilter(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Filter(tt.args.in, tt.args.filterFunc); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Filter() = %v, want %v", got, tt.want)
-			}
+			got := Filter(tt.args.in, tt.args.filterFunc)
+			require.Equal(t, got, tt.want)
 		})
 	}
 }
