9367f063f40fd0e0a1fa1b6eb98aa4f1a4092e7c
Daniil <daniil.stepanenko@flant.com>
2023-11-11 15:42:35 +0300

    WIP
    Signed-off-by: Daniil <daniil.stepanenko@flant.com>
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>

--- deckhouse-controller/controller/module-docs-sync.go
+++ deckhouse-controller/controller/module-docs-sync.go
@@ -15,19 +15,35 @@
 package controller
 
 import (
+	"archive/tar"
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/Masterminds/semver/v3"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/iancoleman/strcase"
+	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/rest"
-	"time"
-
-	log "github.com/sirupsen/logrus"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
+
+	"github.com/deckhouse/deckhouse/go_lib/dependency/cr"
+	d8http "github.com/deckhouse/deckhouse/go_lib/dependency/http"
 )
 
 const (
@@ -73,13 +89,13 @@ type ModuleDocsSyncer struct {
 }
 
 func (s *ModuleDocsSyncer) Run(ctx context.Context) {
-	log.Error(s.onLease(ctx)) // TODO: remove this line
+	log.Error("TMP", s.onLease(ctx)) // TODO: remove this line
 
 	s.informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			err := s.onLease(ctx)
 			if err != nil {
-				log.Error("on lease: ", err)
+				log.Error("module docs syncer: on lease:", err)
 			}
 		},
 	})
@@ -93,33 +109,202 @@ func (s *ModuleDocsSyncer) onLease(ctx context.Context) error {
 		return fmt.Errorf("list: %w", err)
 	}
 
+	httpClient := newHTTPClient(d8http.WithTimeout(3 * time.Minute))
 	for _, item := range list.Items {
-		log.Printf("TMP: %+v\n", item)
-		log.Println(unstructured.NestedString(item.UnstructuredContent(), "spec", "registry", "repo"))
-		log.Println(unstructured.NestedString(item.UnstructuredContent(), "spec", "registry", "dockerCfg"))
-		log.Println(unstructured.NestedString(item.UnstructuredContent(), "spec", "registry", "ca"))
+		repo, _, _ := unstructured.NestedString(item.UnstructuredContent(), "spec", "registry", "repo")
+		dockerCfg, _, _ := unstructured.NestedString(item.UnstructuredContent(), "spec", "registry", "dockerCfg")
+		ca, _, _ := unstructured.NestedString(item.UnstructuredContent(), "spec", "registry", "ca")
+		releaseChannel, _, _ := unstructured.NestedString(item.UnstructuredContent(), "spec", "releaseChannel")
+
+		log.Println("TMP", repo, releaseChannel, dockerCfg, ca)
+
+		opts := make([]cr.Option, 0)
+		if dockerCfg != "" {
+			opts = append(opts, cr.WithAuth(dockerCfg))
+		} else {
+			opts = append(opts, cr.WithDisabledAuth())
+		}
+
+		if ca != "" {
+			opts = append(opts, cr.WithCA(ca))
+		}
+
+		regCli, err := cr.NewClient(repo, opts...)
+		if err != nil {
+			return fmt.Errorf("get regestry client: %w", err)
+		}
+
+		tags, err := regCli.ListTags()
+		if err != nil {
+			return fmt.Errorf("list tags: %w", err)
+		}
+
+		sort.Strings(tags)
+		for _, moduleName := range tags {
+			regCli, err := cr.NewClient(path.Join(repo, moduleName))
+			if err != nil {
+				return fmt.Errorf("fetch module %s: %v", moduleName, err)
+			}
+
+			moduleVersion, err := fetchModuleVersion(releaseChannel, repo, moduleName, opts)
+			if err != nil {
+				return fmt.Errorf("fetch module version: %w", err)
+			}
+
+			img, err := regCli.Image(moduleVersion)
+			if err != nil {
+				return fmt.Errorf("fetch module %s %s image: %v", moduleName, moduleVersion, err)
+			}
+
+			err = buildDocumentation(httpClient, img, moduleName, moduleVersion)
+			if err != nil {
+				return fmt.Errorf("build documentation for %s %s: %w", moduleName, moduleVersion, err)
+			}
+		}
 	}
 
-	//opts := make([]cr.Option, 0)
-	//if ex.Spec.Registry.DockerCFG != "" {
-	//	opts = append(opts, cr.WithAuth(ex.Spec.Registry.DockerCFG))
-	//} else {
-	//	opts = append(opts, cr.WithDisabledAuth())
-	//}
-	//
-	//if ex.Spec.Registry.CA != "" {
-	//	opts = append(opts, cr.WithCA(ex.Spec.Registry.CA))
-	//}
+	return nil
+}
 
-	//regCli, err := cr.NewClient(path.Join(moduleSource.Spec.Registry.Repo, moduleName))
-	//if err != nil {
-	//	return fmt.Errorf("fetch module error: %v", err)
-	//}
-	//
-	//img, err := regCli.Image(moduleVersion)
-	//if err != nil {
-	//	return fmt.Errorf("fetch module version error: %v", err)
-	//}
+func fetchModuleVersion(releaseChannel, repo, moduleName string, registryOptions []cr.Option) (moduleVersion string, err error) {
+	regCli, err := cr.NewClient(path.Join(repo, moduleName, "release"), registryOptions...)
+	if err != nil {
+		return "", fmt.Errorf("fetch release image error: %v", err)
+	}
+
+	img, err := regCli.Image(strcase.ToKebab(releaseChannel))
+	if err != nil {
+		return "", fmt.Errorf("fetch image error: %v", err)
+	}
+
+	moduleMetadata, err := fetchModuleReleaseMetadata(img)
+	if err != nil {
+		return "", fmt.Errorf("fetch release metadata error: %v", err)
+	}
+
+	return "v" + moduleMetadata.Version.String(), nil
+}
+
+type moduleReleaseMetadata struct {
+	Version *semver.Version `json:"version"`
+}
+
+func fetchModuleReleaseMetadata(img v1.Image) (moduleReleaseMetadata, error) {
+	buf := bytes.NewBuffer(nil)
+	var meta moduleReleaseMetadata
+
+	layers, err := img.Layers()
+	if err != nil {
+		return meta, err
+	}
+
+	for _, layer := range layers {
+		size, err := layer.Size()
+		if err != nil {
+			// dcr.logger.Warnf("couldn't calculate layer size")
+			return meta, err
+		}
+		if size == 0 {
+			// skip some empty werf layers
+			continue
+		}
+		rc, err := layer.Uncompressed()
+		if err != nil {
+			return meta, err
+		}
+
+		err = untarMetadata(rc, buf)
+		if err != nil {
+			return meta, err
+		}
+
+		rc.Close()
+	}
+
+	err = json.Unmarshal(buf.Bytes(), &meta)
+
+	return meta, err
+}
+
+func untarMetadata(rc io.ReadCloser, rw io.Writer) error {
+	tr := tar.NewReader(rc)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			// end of archive
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if strings.HasPrefix(hdr.Name, ".werf") {
+			continue
+		}
+
+		switch hdr.Name {
+		case "version.json":
+			_, err = io.Copy(rw, tr)
+			if err != nil {
+				return err
+			}
+			return nil
+
+		default:
+			continue
+		}
+	}
+}
+
+func buildDocumentation(client d8http.Client, img v1.Image, moduleName, moduleVersion string) error {
+	rc := mutate.Extract(img)
+	defer rc.Close()
+
+	const docsBuilderBasePath = "http://documentation-builder.d8-system.svc.cluster.local:8081"
+
+	url := fmt.Sprintf("%s/loadDocArchive/%s/%s", docsBuilderBasePath, moduleName, moduleVersion)
+	response, statusCode, err := httpPost(client, url, rc)
+	if err != nil {
+		return fmt.Errorf("POST %q return %d %q: %w", url, statusCode, response, err)
+	}
+
+	url = fmt.Sprintf("%s/build", docsBuilderBasePath)
+	response, statusCode, err = httpPost(client, url, nil)
+	if err != nil {
+		return fmt.Errorf("POST %q return %d %q: %w", url, statusCode, response, err)
+	}
 
 	return nil
+}
+
+func newHTTPClient(options ...d8http.Option) d8http.Client {
+	var opts []d8http.Option
+	opts = append(opts, options...)
+
+	//TODO: try to remove this
+	contentCA, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+	if err == nil {
+		opts = append(opts, d8http.WithAdditionalCACerts([][]byte{contentCA}))
+	}
+
+	return d8http.NewClient(opts...)
+}
+
+func httpPost(httpClient d8http.Client, url string, body io.Reader) ([]byte, int, error) {
+	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer res.Body.Close()
+
+	dataBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return dataBytes, res.StatusCode, nil
 }
