fa409238a3e19f4335b1d974f9cc5eb7f6d452e4
Denis Romanenko <denis.romanenko@flant.com>
2023-11-15 12:52:42 +0300

    [global] add cloud-provider-vdirector module
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- ee/candi/cloud-providers/vdirector/layouts/standard/base-infrastructure/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Copyright 2021 Flant JSC
-# Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
-
-output "cloud_discovery_data" {
-  value = {
-    "apiVersion"       = "deckhouse.io/v1"
-    "kind"             = "VdirectorCloudDiscoveryData"
-  }
-}
