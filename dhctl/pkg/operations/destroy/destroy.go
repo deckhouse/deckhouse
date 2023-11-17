e25b0dfac292699538ada2bbb2a24a1b1ac34c40
Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>
2023-11-17 11:16:18 +0300

    [dhctl] Support destroy command for static clusters (#6557)
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- dhctl/pkg/operations/destroy/destroy.go
+++ dhctl/pkg/operations/destroy/destroy.go
@@ -15,12 +15,26 @@
 package destroy
 
 import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"time"
+
+	"github.com/deckhouse/deckhouse/dhctl/pkg/config"
 	infra "github.com/deckhouse/deckhouse/dhctl/pkg/infrastructure"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/log"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/operations/phases"
 	dhctlstate "github.com/deckhouse/deckhouse/dhctl/pkg/state"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/state/terraform"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/system/ssh"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/system/ssh/frontend"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/util/input"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/util/retry"
 )
+
+type Destroyer interface {
+	DestroyCluster(autoApprove bool) error
+}
 
 type Params struct {
 	SSHClient   *ssh.Client
@@ -35,10 +49,12 @@ type ClusterDestroyer struct {
 	stateCache      dhctlstate.Cache
 	terrStateLoader infra.StateLoader
 
-	d8Destroyer  *DeckhouseDestroyer
-	clusterInfra *infra.ClusterInfra
+	d8Destroyer       *DeckhouseDestroyer
+	cloudClusterInfra *infra.ClusterInfra
 
 	skipResources bool
+
+	staticDestroyer *staticMastersDestroyer
 
 	*phases.PhasedExecutionContext
 }
@@ -50,23 +66,25 @@ func NewClusterDestroyer(params *Params) *ClusterDestroyer {
 	terraStateLoader := terraform.NewLazyTerraStateLoader(terraform.NewCachedTerraStateLoader(d8Destroyer, state.cache))
 	clusterInfra := infra.NewClusterInfraWithOptions(terraStateLoader, state.cache, infra.ClusterInfraOptions{PhasedExecutionContext: pec})
 
+	staticDestroyer := newStaticMastersDestroyer(params.SSHClient)
+
 	return &ClusterDestroyer{
 		state:           state,
 		stateCache:      params.StateCache,
 		terrStateLoader: terraStateLoader,
 
-		d8Destroyer:  d8Destroyer,
-		clusterInfra: clusterInfra,
+		d8Destroyer:       d8Destroyer,
+		cloudClusterInfra: clusterInfra,
 
 		skipResources: params.SkipResources,
 
 		PhasedExecutionContext: pec,
+
+		staticDestroyer: staticDestroyer,
 	}
 }
 
 func (d *ClusterDestroyer) DestroyCluster(autoApprove bool) error {
-	var err error
-
 	defer d.d8Destroyer.UnlockConverge(true)
 
 	if err := d.PhasedExecutionContext.Init(d.stateCache); err != nil {
@@ -74,13 +92,30 @@ func (d *ClusterDestroyer) DestroyCluster(autoApprove bool) error {
 	}
 	defer d.PhasedExecutionContext.Finalize(d.stateCache)
 
+	// populate cluster state in cache
+	metaConfig, err := d.terrStateLoader.PopulateMetaConfig()
+	if err != nil {
+		return err
+	}
+
+	clusterType := metaConfig.ClusterType
+	var infraDestroyer Destroyer
+	switch clusterType {
+	case config.CloudClusterType:
+		infraDestroyer = d.cloudClusterInfra
+	case config.StaticClusterType:
+		infraDestroyer = d.staticDestroyer
+	default:
+		return fmt.Errorf("Unknown cluster type '%s'", clusterType)
+	}
+
 	if !d.skipResources {
 		if shouldStop, err := d.PhasedExecutionContext.StartPhase(phases.DeleteResourcesPhase, false); err != nil {
 			return err
 		} else if shouldStop {
 			return nil
 		}
-		if err := d.d8Destroyer.DeleteResources(); err != nil {
+		if err := d.d8Destroyer.DeleteResources(clusterType); err != nil {
 			return err
 		}
 		if err := d.PhasedExecutionContext.CommitState(d.stateCache); err != nil {
@@ -88,15 +123,11 @@ func (d *ClusterDestroyer) DestroyCluster(autoApprove bool) error {
 		}
 	}
 
-	// populate cluster state in cache
-	_, err = d.terrStateLoader.PopulateMetaConfig()
-	if err != nil {
-		return err
-	}
-
-	_, _, err = d.terrStateLoader.PopulateClusterState()
-	if err != nil {
-		return err
+	if clusterType == config.CloudClusterType {
+		_, _, err = d.terrStateLoader.PopulateClusterState()
+		if err != nil {
+			return err
+		}
 	}
 
 	// only after load and save all states into cache
@@ -113,10 +144,67 @@ func (d *ClusterDestroyer) DestroyCluster(autoApprove bool) error {
 	// Stop proxy because we have already got all info from kubernetes-api
 	d.d8Destroyer.StopProxy()
 
-	if err := d.clusterInfra.DestroyCluster(autoApprove); err != nil {
+	if err := infraDestroyer.DestroyCluster(autoApprove); err != nil {
 		return err
 	}
 
 	d.state.Clean()
 	return d.PhasedExecutionContext.Complete()
+}
+
+type staticMastersDestroyer struct {
+	SSHClient *ssh.Client
+}
+
+func newStaticMastersDestroyer(c *ssh.Client) *staticMastersDestroyer {
+	return &staticMastersDestroyer{
+		SSHClient: c,
+	}
+}
+
+func (d *staticMastersDestroyer) DestroyCluster(autoApprove bool) error {
+	if !autoApprove {
+		if !input.NewConfirmation().WithMessage("Do you really want to cleanup control-plane nodes?").Ask() {
+			return fmt.Errorf("Cleanup master nodes disallow")
+		}
+	}
+
+	mastersHosts := d.SSHClient.Settings.AvailableHosts()
+	stdOutErrHandler := func(l string) {
+		log.WarnLn(l)
+	}
+
+	cmd := "test -f /var/lib/bashible/cleanup_static_node.sh || exit 0 && bash /var/lib/bashible/cleanup_static_node.sh --yes-i-am-sane-and-i-understand-what-i-am-doing"
+	for _, host := range mastersHosts {
+		settings := d.SSHClient.Settings.Copy()
+		settings.SetAvailableHosts([]string{host})
+		err := retry.NewLoop(fmt.Sprintf("Clear master %s", host), 5, 10*time.Second).Run(func() error {
+			err := frontend.NewCommand(settings, cmd).
+				Sudo().
+				WithTimeout(5 * time.Minute).
+				WithStdoutHandler(stdOutErrHandler).
+				WithStderrHandler(stdOutErrHandler).
+				Run()
+
+			if err != nil {
+				var ee *exec.ExitError
+				if errors.As(err, &ee) {
+					// script reboot node
+					if ee.ExitCode() == 255 {
+						return nil
+					}
+				}
+
+				return err
+			}
+
+			return err
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
