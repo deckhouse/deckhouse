a0ebea4007daa5c46686fe1c779bd09d4ced8696
Artem <mail@artbin.me>
2023-11-30 20:23:15 +0800

    [node-manager] add DeckhouseControlPlane (#6664)
    Signed-off-by: Artem Kulyabin <artem.kulyabin@flant.com>
    Co-authored-by: Artem Kulyabin <artem.kulyabin@flant.com>

--- modules/040-node-manager/images/caps-controller-manager/src/cmd/main.go
+++ modules/040-node-manager/images/caps-controller-manager/src/cmd/main.go
@@ -151,14 +151,6 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "StaticMachineTemplate")
 		os.Exit(1)
 	}
-	if err = (&infrastructurecontroller.StaticControlPlaneReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-		Config: mgr.GetConfig(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "StaticControlPlane")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
