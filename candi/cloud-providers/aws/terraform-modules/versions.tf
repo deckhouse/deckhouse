db755403f4e82a293a69907f5ae1adea9d163b14
Maxim Vasilenko <5184586+mvasl@users.noreply.github.com>
2023-12-02 21:39:29 +0300

    [terraform-manager] Distroless build (#6639)
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    Signed-off-by: Maxim Vasilenko <5184586+mvasl@users.noreply.github.com>
    Co-authored-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    Co-authored-by: Denys Romanenko <65756796+RomanenkoDenys@users.noreply.github.com>

--- candi/cloud-providers/aws/terraform-modules/versions.tf
+++ candi/cloud-providers/aws/terraform-modules/versions.tf
@@ -16,6 +16,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "4.50.0"
     }
   }
   required_version = ">= 0.13"
--- ee/candi/cloud-providers/vsphere/terraform-modules/versions.tf
+++ ee/candi/cloud-providers/vsphere/terraform-modules/versions.tf
@@ -5,6 +5,7 @@ terraform {
   required_providers {
     vsphere = {
       source = "hashicorp/vsphere"
+      version = "2.0.2"
     }
   }
   required_version = ">= 0.13"
