b7ceb32b026ff216d84034ba21304e3f72db717e
Maxim Vasilenko <5184586+mvasl@users.noreply.github.com>
2023-12-05 08:24:26 +0000

    force use of specific openstack provider that we built
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>

--- testing/cloud_layouts/Static/infra.tpl.tf
+++ testing/cloud_layouts/Static/infra.tpl.tf
@@ -22,6 +22,7 @@ terraform {
   required_providers {
     openstack = {
       source = "terraform-provider-openstack/openstack"
+      version = "1.32.0"
     }
   }
   required_version = ">= 0.13"
