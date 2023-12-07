f528951c2adbd23c11e9a7db5f9882c8ef2284a0
Denis Romanenko <denis.romanenko@flant.com>
2023-12-07 13:14:46 +0300

    ++
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

---
+++ ee/modules/030-cloud-provider-vcd/images/cloud-data-discoverer/discoverer.go
@@ -0,0 +1,170 @@
+/*
+Copyright 2023 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/vmware/go-vcloud-director/v2/govcd"
+
+	"github.com/deckhouse/deckhouse/go_lib/cloud-data/apis/v1alpha1"
+)
+
+type Discoverer struct {
+	logger       *log.Entry
+	config       *Config
+	moduleConfig []byte
+}
+
+type Config struct {
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Org      string `json:"org"`
+	Href     string `json:"href"`
+	VDC      string `json:"vdc"`
+	Insecure bool   `json:"insecure"`
+	Token    string `json:"token"`
+}
+
+func parseEnvToConfig() (*Config, error) {
+	c := &Config{}
+	user := os.Getenv("VCD_USER")
+	if user == "" {
+		return nil, fmt.Errorf("VCD_USER env should be set")
+	}
+	c.User = user
+
+	password := os.Getenv("VCD_PASSWORD")
+	token := os.Getenv("VCD_TOKEN")
+	if password == "" && token == "" {
+		return nil, fmt.Errorf("VCD_PASSWORD or VCD_TOKEN env should be set")
+	}
+	c.Password = password
+	c.Token = token
+
+	org := os.Getenv("VCD_ORG")
+	if org == "" {
+		return nil, fmt.Errorf("VCD_ORG env should be set")
+	}
+	c.Org = org
+
+	vdc := os.Getenv("VCD_VDC")
+	if vdc == "" {
+		return nil, fmt.Errorf("VCD_VDC env should be set")
+	}
+	c.VDC = vdc
+
+	insecure := os.Getenv("VCD_INSECURE")
+	if insecure == "true" {
+		c.Insecure = true
+	}
+
+	href := os.Getenv("VCD_HREF")
+	if href == "" {
+		return nil, fmt.Errorf("VCD_HREF env should be set")
+	}
+	c.Href = href
+
+	return c, nil
+}
+
+// Client Creates a vCD client
+func (c *Config) client() (*govcd.VCDClient, error) {
+	u, err := url.ParseRequestURI(c.Href)
+	if err != nil {
+		return nil, fmt.Errorf("unable to pass url: %s", err)
+	}
+
+	vcdClient := govcd.NewVCDClient(*u, c.Insecure)
+	if c.Token != "" {
+		_ = vcdClient.SetToken(c.Org, govcd.AuthorizationHeader, c.Token)
+	} else {
+		resp, err := vcdClient.GetAuthResponse(c.User, c.Password, c.Org)
+		if err != nil {
+			return nil, fmt.Errorf("unable to authenticate: %s", err)
+		}
+		fmt.Printf("Token: %s\n", resp.Header[govcd.AuthorizationHeader])
+	}
+	return vcdClient, nil
+}
+
+func NewDiscoverer(logger *log.Entry) *Discoverer {
+	config, err := parseEnvToConfig()
+	if err != nil {
+		logger.Fatalf("Cannnot get opts from env: %v", err)
+	}
+
+	moduleConfig := os.Getenv("MODULE_CONFIG")
+
+	return &Discoverer{
+		logger:       logger,
+		config:       config,
+		moduleConfig: []byte(moduleConfig),
+	}
+}
+
+func (d *Discoverer) DiscoveryData(_ context.Context, cloudProviderDiscoveryData []byte) ([]byte, error) {
+	var discoveryData VCDCloudDiscoveryData
+
+	if len(cloudProviderDiscoveryData) == 0 {
+		cloudProviderDiscoveryData = d.moduleConfig
+	}
+
+	if len(cloudProviderDiscoveryData) > 0 {
+		err := json.Unmarshal(cloudProviderDiscoveryData, &discoveryData)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal cloud provider discovery data: %v", err)
+		}
+	}
+
+	vcdClient, err := d.config.client()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create vcd client: %v", err)
+	}
+
+	sizingPolicies, err := d.getSizingPolicies(vcdClient)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get sizing policies: %v", err)
+	}
+
+	discoveryDataJson, err := json.Marshal(v1alpha1.VCDCloudProviderDiscoveryData{
+		APIVersion:     "deckhouse.io/v1alpha1",
+		Kind:           "VCDCloudProviderDiscoveryData",
+		SizingPolicies: sizingPolicies,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal discovery data: %v", err)
+	}
+
+	return discoveryDataJson, nil
+}
+
+func (d *Discoverer) getSizingPolicies(vcdClient *govcd.VCDClient) ([]string, error) {
+	sizingPolicies, err := vcdClient.GetAllVdcComputePoliciesV2(url.Values{})
+	if err != nil {
+		return nil, err
+	}
+
+	policies := make([]string, len(sizingPolicies))
+
+	for _, s := range sizingPolicies {
+		policies = append(policies, s.VdcComputePolicyV2.Name)
+	}
+	return policies, nil
+}
+
+func (d *Discoverer) InstanceTypes(_ context.Context) ([]v1alpha1.InstanceType, error) {
+	return nil, nil
+}
+
+type VCDCloudDiscoveryData struct {
+	SizingPolicies []string `json:"sizingPolicies,omitempty" yaml:"sizingPolicies,omitempty"`
+}
---
+++ ee/modules/030-cloud-provider-vcd/images/cloud-data-discoverer/main.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2023 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package main
+
+import (
+	"os"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+
+	cloud_data "github.com/deckhouse/deckhouse/go_lib/cloud-data"
+	"github.com/deckhouse/deckhouse/go_lib/cloud-data/app"
+)
+
+func main() {
+	kpApp := kingpin.New("vcd cloud data discoverer", "A tool for discovery data from cloud provider")
+	kpApp.HelpFlag.Short('h')
+
+	app.InitFlags(kpApp)
+
+	kpApp.Action(func(context *kingpin.ParseContext) error {
+		logger := app.InitLogger()
+		client := app.InitClient(logger)
+		dynamicClient := app.InitDynamicClient(logger)
+		discoverer := NewDiscoverer(logger)
+
+		r := cloud_data.NewReconciler(discoverer, app.ListenAddress, app.DiscoveryPeriod, logger, client, dynamicClient)
+		r.Start()
+
+		return nil
+	})
+
+	_, err := kpApp.Parse(os.Args[1:])
+	if err != nil {
+		logrus.Error(err)
+		os.Exit(1)
+	}
+}
