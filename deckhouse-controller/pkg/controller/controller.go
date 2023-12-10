dc55aeea76a40a9cf5014b4875f07614f994cefc
Daniil Stepanenko <daniil.stepanenko@flant.com>
2023-12-10 22:59:26 +0300

    WIP
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>

--- deckhouse-controller/pkg/controller/controller.go
+++ deckhouse-controller/pkg/controller/controller.go
@@ -35,6 +35,7 @@ import (
 
 	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/client/clientset/versioned"
 	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/client/informers/externalversions"
+	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/controller/coordination-controllers/lease"
 	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/controller/models"
 	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/controller/module-controllers/release"
 	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/controller/module-controllers/source"
@@ -64,6 +65,7 @@ type DeckhouseController struct {
 	moduleSourceController       *source.Controller
 	moduleReleaseController      *release.Controller
 	modulePullOverrideController *release.ModulePullOverrideController
+	leaseController              *lease.Controller
 }
 
 func NewDeckhouseController(ctx context.Context, config *rest.Config, mm *module_manager.ModuleManager) (*DeckhouseController, error) {
@@ -96,6 +98,7 @@ func NewDeckhouseController(ctx context.Context, config *rest.Config, mm *module
 		moduleSourceController:       source.NewController(mcClient, moduleSourceInformer, moduleReleaseInformer, moduleUpdatePolicyInformer, modulePullOverrideInformer),
 		moduleReleaseController:      release.NewController(cs, mcClient, moduleReleaseInformer, moduleSourceInformer, moduleUpdatePolicyInformer, modulePullOverrideInformer, mm),
 		modulePullOverrideController: release.NewModulePullOverrideController(cs, mcClient, moduleSourceInformer, modulePullOverrideInformer, mm),
+		leaseController:              lease.NewController(cs, mcClient),
 	}, nil
 }
 
@@ -119,6 +122,7 @@ func (dml *DeckhouseController) Start(ec chan events.ModuleEvent) error {
 	go dml.moduleSourceController.Run(dml.ctx, 3)
 	go dml.moduleReleaseController.Run(dml.ctx, 3)
 	go dml.modulePullOverrideController.Run(dml.ctx, 1)
+	go dml.leaseController.Run(dml.ctx, 3)
 
 	return nil
 }
---
+++ deckhouse-controller/pkg/controller/coordination-controllers/lease/controller.go
@@ -0,0 +1,423 @@
+// Copyright 2023 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lease
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/iancoleman/strcase"
+	"io"
+	"net/http"
+	"path"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/flant/addon-operator/pkg/utils/logger"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/time/rate"
+	coordination "k8s.io/api/coordination/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	coordinationv1 "k8s.io/client-go/listers/coordination/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/client/clientset/versioned"
+	controllerUtils "github.com/deckhouse/deckhouse/deckhouse-controller/pkg/controller/module-controllers/utils"
+	"github.com/deckhouse/deckhouse/go_lib/dependency/cr"
+	d8http "github.com/deckhouse/deckhouse/go_lib/dependency/http"
+	"github.com/deckhouse/deckhouse/go_lib/module"
+)
+
+const (
+	leaseLabel    = "deckhouse.io/documentation-builder-sync"
+	namespace     = "d8-system"
+	resyncTimeout = 15 * time.Minute
+)
+
+type Controller struct {
+	kubeclientset kubernetes.Interface
+	d8ClientSet   versioned.Interface
+	workqueue     workqueue.RateLimitingInterface
+	lister        coordinationv1.LeaseLister
+	hasSync       cache.InformerSynced
+	httpClient    d8http.Client
+	logger        logger.Logger
+}
+
+// NewController returns a new sample controller
+func NewController(ks kubernetes.Interface, d8ClientSet versioned.Interface) *Controller {
+	ratelimiter := workqueue.NewMaxOfRateLimiter(
+		workqueue.NewItemExponentialFailureRateLimiter(500*time.Millisecond, 1000*time.Second),
+		&workqueue.BucketRateLimiter{Limiter: rate.NewLimiter(rate.Limit(50), 300)},
+	)
+
+	lg := log.WithField("component", "LeaseController")
+
+	factory := informers.NewSharedInformerFactoryWithOptions(
+		ks,
+		resyncTimeout,
+		informers.WithNamespace(namespace),
+		informers.WithTweakListOptions(func(options *metav1.ListOptions) {
+			options.LabelSelector = leaseLabel
+		}),
+	)
+	informer := factory.Coordination().V1().Leases()
+
+	httpClient := d8http.NewClient(d8http.WithTimeout(3 * time.Minute))
+
+	controller := &Controller{
+		kubeclientset: ks,
+		d8ClientSet:   d8ClientSet,
+		workqueue:     workqueue.NewRateLimitingQueue(ratelimiter),
+		lister:        informer.Lister(),
+		hasSync:       informer.Informer().HasSynced,
+		httpClient:    httpClient,
+		logger:        lg,
+	}
+
+	_, _ = informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: controller.enqueueLease,
+	})
+
+	return controller
+}
+
+func (c *Controller) enqueueLease(obj interface{}) {
+	var key string
+	var err error
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		utilruntime.HandleError(err)
+		return
+	}
+	c.logger.Debugf("enqueue Lease: %s", key)
+	c.workqueue.Add(key)
+}
+
+func (c *Controller) Run(ctx context.Context, workers int) {
+	defer utilruntime.HandleCrash()
+	defer c.workqueue.ShutDown()
+
+	c.logger.Info("Starting Lease controller")
+
+	c.logger.Debug("Waiting for lease caches to sync")
+	if ok := cache.WaitForCacheSync(ctx.Done(), c.hasSync); !ok {
+		c.logger.Fatal("failed to wait for caches to sync")
+	}
+
+	c.logger.Infof("Starting workers count: %d", workers)
+	for i := 0; i < workers; i++ {
+		go wait.UntilWithContext(ctx, c.runWorker, time.Second)
+	}
+
+	<-ctx.Done()
+	c.logger.Info("Shutting down workers")
+}
+
+func (c *Controller) runWorker(ctx context.Context) {
+	for c.processNextWorkItem(ctx) {
+	}
+}
+
+func (c *Controller) processNextWorkItem(ctx context.Context) bool {
+	obj, shutdown := c.workqueue.Get()
+	if shutdown {
+		return false
+	}
+
+	err := func(obj interface{}) error {
+		defer c.workqueue.Done(obj)
+		var key string
+		var ok bool
+
+		if key, ok = obj.(string); !ok {
+			c.workqueue.Forget(obj)
+			c.logger.Errorf("expected string in workqueue but got %#v", obj)
+			return nil
+		}
+
+		// run reconcile loop
+		result, err := c.Reconcile(ctx, key)
+		switch {
+		case result.RequeueAfter != 0:
+			c.workqueue.AddAfter(key, result.RequeueAfter)
+
+		case result.Requeue:
+			// Put the item back on the workqueue to handle any transient errors.
+			c.workqueue.AddRateLimited(key)
+
+		default:
+			c.workqueue.Forget(key)
+		}
+
+		return err
+	}(obj)
+
+	if err != nil {
+		c.logger.Errorf("Lease reconcile error: %s", err.Error())
+		return true
+	}
+
+	return true
+}
+
+func (c *Controller) Reconcile(ctx context.Context, name string) (ctrl.Result, error) {
+	nameList := strings.SplitN(name, "/", 1)
+	lease, err := c.lister.Leases(nameList[0]).Get(nameList[1])
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+
+		return ctrl.Result{Requeue: true}, err
+	}
+
+	return c.createReconcile(ctx, lease)
+}
+
+func (c *Controller) createReconcile(ctx context.Context, _ *coordination.Lease) (ctrl.Result, error) {
+	list, err := c.d8ClientSet.DeckhouseV1alpha1().ModuleSources().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return ctrl.Result{Requeue: true}, fmt.Errorf("list: %w", err)
+	}
+
+	addrs, err := c.getDocsBuilderAddresses(ctx)
+	if err != nil {
+		return ctrl.Result{Requeue: true}, fmt.Errorf("get builder addresses: %w", err)
+	}
+
+	if len(addrs) == 0 {
+		return ctrl.Result{}, nil
+	}
+
+	for _, item := range list.Items {
+		//TODO: use downloader
+		opts := controllerUtils.GenerateRegistryOptions(&item)
+
+		regCli, err := cr.NewClient(item.Spec.Registry.Repo, opts...)
+		if err != nil {
+			return ctrl.Result{Requeue: true}, fmt.Errorf("get regestry client: %w", err)
+		}
+
+		tags, err := regCli.ListTags()
+		if err != nil {
+			return ctrl.Result{Requeue: true}, fmt.Errorf("list tags: %w", err)
+		}
+
+		sort.Strings(tags)
+		for _, moduleName := range tags {
+			regCli, err := cr.NewClient(path.Join(item.Spec.Registry.Repo, moduleName), opts...)
+			if err != nil {
+				return ctrl.Result{Requeue: true}, fmt.Errorf("fetch module %s: %v", moduleName, err)
+			}
+
+			moduleVersion, err := fetchModuleVersion(item.Spec.ReleaseChannel, item.Spec.Registry.Repo, moduleName, opts)
+			if err != nil {
+				return ctrl.Result{Requeue: true}, fmt.Errorf("fetch module version: %w", err)
+			}
+
+			for _, addr := range addrs {
+				img, err := regCli.Image(moduleVersion)
+				if err != nil {
+					return ctrl.Result{Requeue: true}, fmt.Errorf("fetch module %s %s image: %v", moduleName, moduleVersion, err)
+				}
+
+				err = c.sendDocumentation(addr, img, moduleName, moduleVersion)
+				if err != nil {
+					return ctrl.Result{Requeue: true}, fmt.Errorf("send documentation for %s %s: %w", moduleName, moduleVersion, err)
+				}
+			}
+		}
+	}
+
+	for _, addr := range addrs {
+		err = c.buildDocumentation(addr)
+		if err != nil {
+			return ctrl.Result{Requeue: true}, fmt.Errorf("build documentation %w", err)
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func fetchModuleVersion(releaseChannel, repo, moduleName string, registryOptions []cr.Option) (moduleVersion string, err error) {
+	regCli, err := cr.NewClient(path.Join(repo, moduleName, "release"), registryOptions...)
+	if err != nil {
+		return "", fmt.Errorf("fetch release image error: %v", err)
+	}
+
+	img, err := regCli.Image(strcase.ToKebab(releaseChannel))
+	if err != nil {
+		return "", fmt.Errorf("fetch image error: %v", err)
+	}
+
+	moduleMetadata, err := fetchModuleReleaseMetadata(img)
+	if err != nil {
+		return "", fmt.Errorf("fetch release metadata error: %v", err)
+	}
+
+	return "v" + moduleMetadata.Version.String(), nil
+}
+
+type moduleReleaseMetadata struct {
+	Version *semver.Version `json:"version"`
+}
+
+func fetchModuleReleaseMetadata(img v1.Image) (moduleReleaseMetadata, error) {
+	buf := bytes.NewBuffer(nil)
+	var meta moduleReleaseMetadata
+
+	layers, err := img.Layers()
+	if err != nil {
+		return meta, err
+	}
+
+	for _, layer := range layers {
+		size, err := layer.Size()
+		if err != nil {
+			// dcr.logger.Warnf("couldn't calculate layer size")
+			return meta, err
+		}
+		if size == 0 {
+			// skip some empty werf layers
+			continue
+		}
+		rc, err := layer.Uncompressed()
+		if err != nil {
+			return meta, err
+		}
+
+		err = untarMetadata(rc, buf)
+		if err != nil {
+			return meta, err
+		}
+
+		rc.Close()
+	}
+
+	err = json.Unmarshal(buf.Bytes(), &meta)
+
+	return meta, err
+}
+
+func untarMetadata(rc io.ReadCloser, rw io.Writer) error {
+	tr := tar.NewReader(rc)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			// end of archive
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if strings.HasPrefix(hdr.Name, ".werf") {
+			continue
+		}
+
+		switch hdr.Name {
+		case "version.json":
+			_, err = io.Copy(rw, tr)
+			if err != nil {
+				return err
+			}
+			return nil
+
+		default:
+			continue
+		}
+	}
+}
+
+func (c *Controller) getDocsBuilderAddresses(ctx context.Context) (addresses []string, err error) {
+	list, err := c.kubeclientset.DiscoveryV1().EndpointSlices("d8-system").List(ctx, metav1.ListOptions{LabelSelector: "app=documentation"})
+	if err != nil {
+		return nil, fmt.Errorf("list endpoint slices: %w", err)
+	}
+
+	for _, eps := range list.Items {
+		var port int32
+		for _, p := range eps.Ports {
+			if p.Name != nil && *p.Name == "builder-http" {
+				port = *p.Port
+			}
+		}
+
+		if port == 0 {
+			continue
+		}
+		for _, ep := range eps.Endpoints {
+			for _, addr := range ep.Addresses {
+				addresses = append(addresses, fmt.Sprintf("http://%s:%d/???", addr, port))
+			}
+		}
+	}
+
+	return
+}
+
+func (c *Controller) sendDocumentation(docsBuilderBasePath string, img v1.Image, moduleName, moduleVersion string) error {
+	rc := module.ExtractDocs(img)
+	defer rc.Close()
+
+	url := fmt.Sprintf("%s/loadDocArchive/%s/%s", docsBuilderBasePath, moduleName, moduleVersion)
+	response, statusCode, err := c.httpPost(url, rc)
+	if err != nil {
+		return fmt.Errorf("POST %q return %d %q: %w", url, statusCode, response, err)
+	}
+
+	return nil
+}
+
+func (c *Controller) buildDocumentation(docsBuilderBasePath string) error {
+	url := fmt.Sprintf("%s/build", docsBuilderBasePath)
+	response, statusCode, err := c.httpPost(url, nil)
+	if err != nil {
+		return fmt.Errorf("POST %q return %d %q: %w", url, statusCode, response, err)
+	}
+
+	return nil
+}
+
+func (c *Controller) httpPost(url string, body io.Reader) ([]byte, int, error) {
+	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer res.Body.Close()
+
+	dataBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return dataBytes, res.StatusCode, nil
+}
