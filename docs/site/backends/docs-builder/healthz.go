894dbc72b9a727bd13f831202c6d724d5ad8ac86
Daniil Stepanenko <daniil.stepanenko@flant.com>
2023-12-13 22:06:51 +0300

    WIP
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>

--- docs/site/backends/docs-builder/healthz.go
+++ docs/site/backends/docs-builder/healthz.go
@@ -1,3 +1,17 @@
+// Copyright 2023 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -6,17 +20,17 @@ import (
 	"sync/atomic"
 )
 
-func newHealthzHandler(isReady *atomic.Bool) *healthzHandler {
-	return &healthzHandler{
+func newReadinessHandler(isReady *atomic.Bool) *readinessHandler {
+	return &readinessHandler{
 		isReady: isReady,
 	}
 }
 
-type healthzHandler struct {
+type readinessHandler struct {
 	isReady *atomic.Bool
 }
 
-func (h *healthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *readinessHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if h.isReady.Load() {
 		_, _ = io.WriteString(w, "ok")
 	} else {
