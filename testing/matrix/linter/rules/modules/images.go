dbeafeefe0933e5fbee0dc12554d39f297de991b
Vadim Lazovsky <vadim.lazovsky@flant.com>
2023-12-25 17:08:34 +0200

    Add grafana v10
    Signed-off-by: Vadim Lazovsky <vadim.lazovsky@flant.com>
    Signed-off-by: m.nabokikh <maksim.nabokikh@flant.com>

--- testing/matrix/linter/rules/modules/images.go
+++ testing/matrix/linter/rules/modules/images.go
@@ -104,7 +104,8 @@ func skipDistrolessImageCheckIfNeeded(image string) bool {
 		"pmacct/Dockerfile",
 		"argocd/Dockerfile",
 		"argocd-image-updater/Dockerfile",
-		"werf-argocd-cmp-sidecar/Dockerfile":
+		"werf-argocd-cmp-sidecar/Dockerfile",
+		"memcached/werf.inc.yaml":
 		return true
 	}
 	return false
