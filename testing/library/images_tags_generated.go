3000aed1ef492762a0cd8c6c182585ed4e9a3be9
Andrey Pavlov <andrey.pavlov@flant.com>
2024-01-16 18:00:15 +0500

    some fix
    Signed-off-by: Andrey Pavlov <andrey.pavlov@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -24,9 +24,11 @@ var DefaultImagesDigests = map[string]interface{}{
 		"chrony": "imageHash-chrony-chrony",
 	},
 	"ciliumHubble": map[string]interface{}{
-		"relay":      "imageHash-ciliumHubble-relay",
-		"uiBackend":  "imageHash-ciliumHubble-uiBackend",
-		"uiFrontend": "imageHash-ciliumHubble-uiFrontend",
+		"relay":               "imageHash-ciliumHubble-relay",
+		"relayDistroless":     "imageHash-ciliumHubble-relayDistroless",
+		"uiBackend":           "imageHash-ciliumHubble-uiBackend",
+		"uiBackendDistroless": "imageHash-ciliumHubble-uiBackendDistroless",
+		"uiFrontend":          "imageHash-ciliumHubble-uiFrontend",
 	},
 	"cloudProviderAws": map[string]interface{}{
 		"cloudControllerManager124": "imageHash-cloudProviderAws-cloudControllerManager124",
