1f13f80c249f29b89365432fe6c4f1f15dc1658b
Maxim Vasilenko <maksim.vasilenko@flant.com>
2024-02-09 12:13:50 +0300

    zvirt standard layout
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>

--- ee/candi/cloud-providers/zvirt/terraform-modules/variables.tf
+++ ee/candi/cloud-providers/zvirt/terraform-modules/variables.tf
@@ -1,3 +1,60 @@
 # Copyright 2024 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
+variable "url" {
+  description = "oVirt API URL"
+}
+variable "username" {
+  description = "oVirt Admin user"
+}
+variable "password" {
+  description = "oVirt Admin password"
+}
+variable "insecure_mode" {
+  description = "TLS validation"
+}
+
+variable "cluster_id" {
+  description = "Cluster id"
+}
+
+variable "storage_domain_id" {
+  description = "UUID of the storage domain used to store VM disks"
+}
+
+variable "vnic_profile_id" {
+  description = "vNIC profile ID for VMs"
+}
+
+variable "nic_name" {
+  description = "Network interface name for VM"
+  default = "eth0"
+}
+
+variable "template_name" {
+  description = "VM template name"
+}
+
+variable "instance_os" {
+  description = "OS type of the VM"
+}
+
+variable "instance_cpu_cores" {
+  description = "VM instance CPU cores count"
+  default     = 4
+}
+
+variable "instance_memory" {
+  description = "VM instance RAM amount in GB"
+  default     = 8 # Gb
+}
+
+variable "instance_vm_type" {
+  description = "VM type"
+  default = "high_performance"
+}
+
+variable "node_name_prefix" {
+  description = "Prefix for Node naming"
+  default = "d8"
+}
