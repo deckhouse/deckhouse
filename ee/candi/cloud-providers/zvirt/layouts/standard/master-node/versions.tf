f84a47f4f0910781f8b54bde11b50e37a19a66d0
Maxim Vasilenko <5184586+mvasl@users.noreply.github.com>
2024-02-09 17:14:36 +0300

    zvirt standard layout (#7449)
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    Co-authored-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/zvirt/layouts/standard/master-node/versions.tf
+++ ee/candi/cloud-providers/zvirt/layouts/standard/master-node/versions.tf
@@ -1,9 +1,12 @@
 # Copyright 2024 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
-provider "ovirt" {
-  url = var.url
-  username = var.username
-  password = var.password
-  tls_insecure = var.insecure_mode
+terraform {
+  required_providers {
+    vsphere = {
+      source = "oVirt/ovirt"
+      version = "2.1.5"
+    }
+  }
+  required_version = ">= 0.13"
 }
