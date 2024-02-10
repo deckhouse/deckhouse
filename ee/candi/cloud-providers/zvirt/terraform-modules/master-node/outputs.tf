128c515978a001da4047215b4effbbf1e2d4f7dc
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-02-10 23:39:56 +0300

    fix terraform
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/zvirt/terraform-modules/master-node/outputs.tf
+++ ee/candi/cloud-providers/zvirt/terraform-modules/master-node/outputs.tf
@@ -1,12 +1,21 @@
 # Copyright 2024 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
+data "ovirt_wait_for_ip" "master_vm" {
+  vm_id = ovirt_vm_start.master_vm.vm_id
+}
+
+locals {
+  master_vm_interface = tolist(data.ovirt_wait_for_ip.master_vm.interfaces)[0]
+  master_vm_ip = tolist(local.master_vm_interface.ipv4_addresses)[0]
+}
+
 output "master_ip_address_for_ssh" {
-  value = ovirt_wait_for_ip.master_wm.interfaces.ipv4_addresses
+  value = local.master_vm_ip
 }
 
 output "node_internal_ip_address" {
-  value = ovirt_wait_for_ip.master_wm.interfaces.ipv4_addresses
+  value = local.master_vm_ip
 }
 
 output "kubernetes_data_device_path" {
