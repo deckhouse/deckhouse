9430278b22911957adf430bdbd26819639a9745a
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-02-10 22:39:56 +0300

    add cluster configuration
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/zvirt/terraform-modules/master-node/outputs.tf
+++ ee/candi/cloud-providers/zvirt/terraform-modules/master-node/outputs.tf
@@ -1,13 +1,13 @@
 # Copyright 2024 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
-# output "master_ip_address_for_ssh" {
-#   value = vcd_vapp_vm.master.network[0].ip
-# }
-#
-# output "node_internal_ip_address" {
-#   value = vcd_vapp_vm.master.network[0].ip
-# }
+output "master_ip_address_for_ssh" {
+  value = ovirt_wait_for_ip.master_wm.interfaces.ipv4_addresses
+}
+
+output "node_internal_ip_address" {
+  value = ovirt_wait_for_ip.master_wm.interfaces.ipv4_addresses
+}
 
 output "kubernetes_data_device_path" {
   value = "/dev/sdb"
