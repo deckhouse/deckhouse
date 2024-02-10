a9b68a577b4e0946944013e56b4b834f96af6b13
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-02-10 22:39:56 +0300

    add cluster configuration
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/zvirt/terraform-modules/providers.tf
+++ ee/candi/cloud-providers/zvirt/terraform-modules/providers.tf
@@ -2,8 +2,8 @@
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
 provider "ovirt" {
-  url = var.url
-  username = var.username
-  password = var.password
-  tls_insecure = var.insecure
+  url = var.providerClusterConfiguration.provider.server
+  username = var.providerClusterConfiguration.provider.username
+  password = var.providerClusterConfiguration.provider.password
+  tls_insecure = var.providerClusterConfiguration.provider.insecure
 }
