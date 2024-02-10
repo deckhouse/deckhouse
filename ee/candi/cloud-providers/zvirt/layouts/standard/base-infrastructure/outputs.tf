3ae385e4a4f96c42887881132efe05d07827db28
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-02-10 20:29:31 +0300

    add cluster configuration
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/zvirt/layouts/standard/base-infrastructure/outputs.tf
+++ ee/candi/cloud-providers/zvirt/layouts/standard/base-infrastructure/outputs.tf
@@ -1,6 +1,9 @@
-output "cloud_discovery_data" {
-  value = {
-    "apiVersion"       = "deckhouse.io/v1"
-    "kind"             = "ZvirtCloudProviderDiscoveryData"
-  }
-}
+# Copyright 2024 Flant JSC
+# Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+
+# output "cloud_discovery_data" {
+#   value = {
+#     "apiVersion"       = "deckhouse.io/v1"
+#     "kind"             = "ZvirtCloudProviderDiscoveryData"
+#   }
+# }
