ce50de11184af2b3de80cdb9aa47edf69d9f03d6
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-02-10 20:02:57 +0300

    add cluster configuration
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/zvirt/layouts/standard/base-infrastructure/variables.tf
+++ ee/candi/cloud-providers/zvirt/layouts/standard/base-infrastructure/variables.tf
@@ -9,23 +9,3 @@ variable "providerClusterConfiguration" {
   type = any
 }
 
-variable "url" {
-  description = "oVirt API URL"
-}
-
-variable "username" {
-  description = "oVirt Admin user"
-}
-
-variable "password" {
-  description = "oVirt Admin password"
-}
-
-variable "insecure_mode" {
-  description = "TLS validation"
-}
-
-variable "node_name_prefix" {
-  description = "Prefix for Node naming"
-  default = "d8"
-}
