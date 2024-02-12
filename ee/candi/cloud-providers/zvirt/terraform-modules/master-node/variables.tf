0505833c05e7bfaa40490b8dd136dd211df2f7b8
Maxim Vasilenko <maksim.vasilenko@flant.com>
2024-02-12 15:50:55 +0300

    cloud-init for first master on zvirt
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>

--- ee/candi/cloud-providers/zvirt/terraform-modules/master-node/variables.tf
+++ ee/candi/cloud-providers/zvirt/terraform-modules/master-node/variables.tf
@@ -18,3 +18,23 @@ variable "nodeIndex" {
   type    = number
   default = 0
 }
+
+locals {
+  resource_name_prefix = var.clusterConfiguration.cloud.prefix
+  vnic_profile_id = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "vnicProfileId", [])
+  storage_domain_id = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "storageDomainId", [])
+  cluster_id = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "clusterId", [])
+  template_name = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "template", [])
+  master_cpus = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "numCPUs", [])
+  master_ram_mb = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "memory", [])
+  master_os_type = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "os", [])
+  master_vm_type = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "vmType", [])
+  master_nic_name = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "nicName", [])
+  ssh_pubkey = lookup(var.providerClusterConfiguration, "sshPublicKey", null)
+
+  master_cloud_init_script = yamlencode({
+    "ssh_keys": {
+      "ssh_authorized_keys": local.ssh_pubkey,
+    }
+  })
+}
