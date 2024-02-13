984bcc7b97110df3bcb17cb1abf97d1a792b86de
Maxim Vasilenko <5184586+mvasl@users.noreply.github.com>
2024-02-13 18:29:30 +0300

    cloud-init for first master on zvirt (#7463)
    * cloud-init for first master on zvirt
    
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    
    * cloud-init for first master on zvirt
    
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    
    * resize boot disk
    
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    
    ---------
    
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    Co-authored-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/zvirt/terraform-modules/master-node/variables.tf
+++ ee/candi/cloud-providers/zvirt/terraform-modules/master-node/variables.tf
@@ -47,3 +47,26 @@ variable "nodeIndex" {
   type    = number
   default = 0
 }
+
+locals {
+  resource_name_prefix = var.clusterConfiguration.cloud.prefix
+  vnic_profile_id = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "vnicProfileId", [])
+  storage_domain_id = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "storageDomainId", [])
+  cluster_id = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "clusterId", [])
+  template_name = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "template", [])
+  master_node_name = join("-", [local.resource_name_prefix, "master", var.nodeIndex])
+  master_cpus = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "numCPUs", [])
+  master_ram_mb = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "memory", [])
+  master_os_type = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "os", [])
+  master_vm_type = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "vmType", [])
+  master_nic_name = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "nicName", [])
+  ssh_pubkey = lookup(var.providerClusterConfiguration, "sshPublicKey", null)
+
+  master_cloud_init_script = yamlencode({
+    "hostname": local.master_node_name,
+    "create_hostname_file": true,
+    "ssh_deletekeys": true,
+    "ssh_genkeytypes": ["rsa", "ecdsa", "ed25519"],
+    "ssh_authorized_keys" : [local.ssh_pubkey]
+  })
+}
