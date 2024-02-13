c0ce845517157051eddd2e1f0ef8e0f1b389ce51
Alexander Zverev <beastlex@gmail.com>
2024-02-13 21:21:15 +0700

    Add cni dockerfile for v1.12.6
    Signed-off-by: Alexander Zverev <beastlex@gmail.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -229,6 +229,7 @@ var DefaultImagesDigests = map[string]interface{}{
 	},
 	"istio": map[string]interface{}{
 		"apiProxy":          "imageHash-istio-apiProxy",
+		"cniV1x12x6":        "imageHash-istio-cniV1x12x6",
 		"cniV1x13x7":        "imageHash-istio-cniV1x13x7",
 		"cniV1x16x2":        "imageHash-istio-cniV1x16x2",
 		"cniV1x19x4":        "imageHash-istio-cniV1x19x4",
