6f17c58a44325b1e4eec4f752275c4cb756056ba
Denys Romanenko <65756796+RomanenkoDenys@users.noreply.github.com>
2024-02-19 19:18:13 +0300

    [shell_lib] Fix an error with password generation in shell hooks (#7548)
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- shell_lib/hook.sh
+++ shell_lib/hook.sh
@@ -25,7 +25,6 @@ function hook::run() {
   for i in `seq 0 $((CONTEXT_LENGTH - 1))`; do
     export BINDING_CONTEXT_CURRENT_INDEX="${i}"
     export BINDING_CONTEXT_CURRENT_BINDING=$(context::jq -r '.binding // "unknown"')
-
     case "${BINDING_CONTEXT_CURRENT_BINDING}" in
     "beforeAll")
       HANDLERS="__on_before_all"
@@ -43,7 +42,7 @@ function hook::run() {
       HANDLERS="__on_after_delete_helm"
     ;;
     *)
-      HANDLERS=$(hook::_determine_kubernetes_and_scheduler_handlers)
+      HANDLERS=$(hook::_get_possible_handler_names)
     esac
     HANDLERS="${HANDLERS} __main__"
 
