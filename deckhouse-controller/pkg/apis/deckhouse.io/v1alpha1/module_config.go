db3026295568e18f56c19da39ea6d8b4a421d952
Mikhail Scherba <41360396+miklezzzz@users.noreply.github.com>
2024-02-21 16:41:06 +0400

    [External module manager] add module status (#7446)
    Signed-off-by: Mikhail Scherba <mikhail.scherba@flant.com>

--- deckhouse-controller/pkg/apis/deckhouse.io/v1alpha1/module_config.go
+++ deckhouse-controller/pkg/apis/deckhouse.io/v1alpha1/module_config.go
@@ -87,10 +87,8 @@ type ModuleConfigSpec struct {
 }
 
 type ModuleConfigStatus struct {
-	State   string `json:"state"`
 	Version string `json:"version"`
-	Type    string `json:"type"`
-	Status  string `json:"status"`
+	Message string `json:"message"`
 }
 
 // +k8s:deepcopy-gen=true
