1dc6eb7ffc8b6b6720cc5ad1a7d2e2024617d112
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-02-22 15:40:55 +0300

    [cloud-providers] add ccm
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/modules/030-cloud-provider-zvirt/hooks/zvirt_cluster_configuration.go
+++ ee/modules/030-cloud-provider-zvirt/hooks/zvirt_cluster_configuration.go
@@ -1,17 +1,6 @@
 /*
 Copyright 2024 Flant JSC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 */
 
 package hooks
@@ -31,6 +20,5 @@ var _ = cluster_configuration.RegisterHook(func(input *go_hook.HookInput, metaCf
 		return fmt.Errorf("kube-system/d8-provider-cluster-configuration secret not found")
 	}
 	input.Values.Set("cloudProviderZvirt.internal.providerClusterConfiguration", metaCfg.ProviderClusterConfig)
-	// input.Values.Set("cloudProviderZvirt.internal.providerDiscoveryData", providerDiscoveryData.Object)
 	return nil
 })
