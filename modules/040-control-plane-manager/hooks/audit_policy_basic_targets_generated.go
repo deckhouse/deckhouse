013cf88517d33176bfe1adffb46747974fb5bc71
Yan Zarytovsky <yan.zarytovsky@flant.com>
2024-02-26 14:58:33 +0300

    Fix go generated files
    Signed-off-by: Yan Zarytovsky <yan.zarytovsky@flant.com>

--- modules/040-control-plane-manager/hooks/audit_policy_basic_targets_generated.go
+++ modules/040-control-plane-manager/hooks/audit_policy_basic_targets_generated.go
@@ -66,6 +66,7 @@ var auditPolicyBasicServiceAccounts = []string{
 	"system:serviceaccount:d8-cloud-provider-vcd:cloud-data-discoverer",
 	"system:serviceaccount:d8-cloud-provider-vsphere:cloud-controller-manager",
 	"system:serviceaccount:d8-cloud-provider-yandex:cloud-controller-manager",
+	"system:serviceaccount:d8-cloud-provider-yandex:cloud-data-discoverer",
 	"system:serviceaccount:d8-cloud-provider-yandex:cloud-metrics-exporter",
 	"system:serviceaccount:d8-cni-cilium:agent",
 	"system:serviceaccount:d8-cni-cilium:dashboard",
