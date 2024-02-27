61cd0656d8cf5999df71e6398227315416ea5a54
Alexander Zverev <beastlex@gmail.com>
2024-02-27 18:25:35 +0700

    change hook to discovery type
    Signed-off-by: Alexander Zverev <beastlex@gmail.com>

--- modules/021-cni-cilium/hooks/discovery_is_istio_cni_enabled.go
+++ modules/021-cni-cilium/hooks/discovery_is_istio_cni_enabled.go
@@ -19,17 +19,16 @@ package hooks
 import (
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
-	"github.com/flant/shell-operator/pkg/kube/object_patch"
 	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/utils/pointer"
 )
 
 // If istio-cni is enabled and DaemonSet istio-cni is created.
 // We need to set cni-exclusive: "false" to avoid a conflict writing to /etc/cni/net.d/*.conflist.
 
 var _ = sdk.RegisterFunc(&go_hook.HookConfig{
-	Queue: "/modules/cni-cilium/set-exclusive",
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 10},
+	Queue:        "/modules/cni-cilium/set-exclusive",
 	Kubernetes: []go_hook.KubernetesConfig{
 		{
 			Name:       "istio-cni-daemonset",
@@ -43,28 +42,21 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 					MatchNames: []string{"d8-istio"},
 				},
 			},
-			ExecuteHookOnEvents:          pointer.Bool(false),
-			ExecuteHookOnSynchronization: pointer.Bool(false),
-			FilterFunc:                   nameFilter,
+			FilterFunc: daemonsetFilter,
 		},
 	},
 }, setExclusiveMode)
 
-func nameFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+func daemonsetFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
 	return obj.GetName(), nil
 }
 
 func setExclusiveMode(input *go_hook.HookInput) error {
 	istioCniDaemonSet := input.Snapshots["istio-cni-daemonset"]
 	if len(istioCniDaemonSet) < 1 {
-		return nil // No need to change default value?
+		input.Values.Set("cniCilium.internal.isIstioCNIEnabled", false)
+	} else {
+		input.Values.Set("cniCilium.internal.isIstioCNIEnabled", true)
 	}
-
-	cm := map[string]interface{}{
-		"data": map[string]string{
-			"cni-exclusive": "false",
-		},
-	}
-	input.PatchCollector.MergePatch(cm, "v1", "ConfigMap", "d8-cni-cilium", "cilium-config", object_patch.IgnoreMissingObject())
 	return nil
 }
