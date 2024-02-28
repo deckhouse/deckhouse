a7a84bcee50f862adcaca66d1ce792555e113cc7
Alexander Zverev <beastlex@gmail.com>
2024-02-28 20:24:18 +0700

    add kube-rbac-proxy container and podmonitor
    Signed-off-by: Alexander Zverev <beastlex@gmail.com>

--- modules/110-istio/template_tests/module_test.go
+++ modules/110-istio/template_tests/module_test.go
@@ -95,6 +95,8 @@ const istioValues = `
     auth:
       externalAuthentication: {}
     outboundTrafficPolicyMode: AllowAny
+    dataPlane:
+      trafficRedirectionInSetupMode: CNIPlugin
     sidecar:
       includeOutboundIPRanges: ["10.0.0.0/24"]
       excludeOutboundIPRanges: ["1.2.3.4/32"]
