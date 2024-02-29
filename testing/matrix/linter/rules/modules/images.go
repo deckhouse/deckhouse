39a4893c53dc1f74e785d88ad6964a4f26d4fe0f
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-02-29 11:44:18 +0300

    [matrix-tests] distroless linter
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- testing/matrix/linter/rules/modules/images.go
+++ testing/matrix/linter/rules/modules/images.go
@@ -213,7 +213,7 @@ func lintOneDockerfileOrWerfYAML(name, filePath, imagesPath string) errors.LintR
 				// "from:" right after "image:"
 				if linePos-lastWerfImagePos == 1 {
 					if skipDistrolessImageCheckIfNeeded(relativeFilePath) {
-						log.Printf("SKIP DISTROLESS CHECK!!!\nmodule = %s, image = %s\nvalue - %s\n", name, relativeFilePath, fromTrimmed)
+						log.Printf("\nSKIP DISTROLESS CHECK!!!\nmodule = %s, image = %s\nvalue - %s\n", name, relativeFilePath, fromTrimmed)
 						continue
 					}
 
@@ -239,7 +239,7 @@ func lintOneDockerfileOrWerfYAML(name, filePath, imagesPath string) errors.LintR
 	for i, fromInstruction := range dockerfileFromInstructions {
 		lastInstruction := i == len(dockerfileFromInstructions)-1
 		if skipDistrolessImageCheckIfNeeded(relativeFilePath) {
-			log.Printf("SKIP DISTROLESS CHECK!!!\nmodule = %s, image = %s\nvalue - %s\n", name, relativeFilePath, fromInstruction)
+			log.Printf("\nSKIP DISTROLESS CHECK!!!\nmodule = %s, image = %s\nvalue - %s\n", name, relativeFilePath, fromInstruction)
 			continue
 		}
 
