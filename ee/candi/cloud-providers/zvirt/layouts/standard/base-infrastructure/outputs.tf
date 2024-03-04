a666bffd7e40617320ca0973525a17521e1660f2
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-03-04 17:58:00 +0300

    [zvirt] csi
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/zvirt/layouts/standard/base-infrastructure/outputs.tf
+++ ee/candi/cloud-providers/zvirt/layouts/standard/base-infrastructure/outputs.tf
@@ -5,6 +5,6 @@ output "cloud_discovery_data" {
   value = {
     "apiVersion"       = "deckhouse.io/v1"
     "kind"             = "ZvirtCloudProviderDiscoveryData"
-    "storageProfiles"  = []
+    "storageDomains"  = []
   }
 }
