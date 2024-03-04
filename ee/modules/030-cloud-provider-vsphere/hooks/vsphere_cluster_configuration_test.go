aa371089b3b778fbd6080013e3cc19f550af39b9
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2024-03-04 12:22:41 +0300

    gogo
    Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

--- ee/modules/030-cloud-provider-vsphere/hooks/vsphere_cluster_configuration_test.go
+++ ee/modules/030-cloud-provider-vsphere/hooks/vsphere_cluster_configuration_test.go
@@ -93,7 +93,7 @@ provider:
   password: test
   insecure: true
 vmFolderPath: test
-vmFolderExists: true
+vmFolderExists: false
 regionTagCategory: test
 zoneTagCategory: test
 region: test
@@ -141,6 +141,7 @@ region: override
 regionTagCategory: override
 sshPublicKey: override1
 vmFolderPath: override
+vmFolderExists: false
 zoneTagCategory: override
 zones:
 - override1
@@ -188,6 +189,7 @@ region: override
 regionTagCategory: override
 sshPublicKey: override1
 vmFolderPath: override
+vmFolderExists: false
 zoneTagCategory: override
 zones:
 - override1
@@ -210,6 +212,7 @@ region: override
 regionTagCategory: k8s-region
 sshPublicKey: override1
 vmFolderPath: override
+vmFolderExists: false
 zoneTagCategory: k8s-zone
 zones:
 - test
