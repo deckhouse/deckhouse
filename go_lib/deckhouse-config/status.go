68a72c69a459ecbab51fb09c9e0052ff0b1194ed
Daniil Stepanenko <daniil.stepanenko@flant.com>
2024-03-12 17:25:09 +0300

    WIP
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>

--- go_lib/deckhouse-config/status.go
+++ go_lib/deckhouse-config/status.go
@@ -147,12 +147,7 @@ func (s *StatusReporter) ForConfig(cfg *v1alpha1.ModuleConfig) ModuleConfigStatu
 	if chain.IsKnownVersion(cfg.Spec.Version) && hasVersionedSettings(cfg) {
 		res := Service().ConfigValidator().Validate(cfg)
 		if res.HasError() {
-			var prefix = "Ignored"
-			if s.moduleManager.IsModuleEnabled(cfg.GetName()) {
-				prefix = "Error"
-			}
-
-			invalidMsg := fmt.Sprintf("%s: %s", prefix, res.Error)
+			invalidMsg := fmt.Sprintf("Ignored: %s", res.Error)
 			return ModuleConfigStatus{
 				Version: "",
 				Message: invalidMsg,
