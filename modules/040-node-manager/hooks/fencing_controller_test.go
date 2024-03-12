a7097ba969de5e8e0c89bda322b5212453f9e5d7
Pavel Tishkov <pavel.tishkov@flant.com>
2024-03-12 12:08:09 +0300

    [node-manager] fix lint
    Signed-off-by: Pavel Tishkov <pavel.tishkov@flant.com>

--- modules/040-node-manager/hooks/fencing_controller_test.go
+++ modules/040-node-manager/hooks/fencing_controller_test.go
@@ -22,12 +22,11 @@ import (
 	"text/template"
 	"time"
 
-	v1core "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	v1core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/deckhouse/deckhouse/testing/hooks"
 )
