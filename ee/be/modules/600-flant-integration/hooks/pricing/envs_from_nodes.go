e245c9e58844311c65bde39961b6b1637f18accc
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2024-03-20 13:57:13 +0300

    go
    Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

--- ee/be/modules/600-flant-integration/hooks/pricing/envs_from_nodes.go
+++ ee/be/modules/600-flant-integration/hooks/pricing/envs_from_nodes.go
@@ -17,11 +17,9 @@ import (
 )
 
 type Node struct {
-	Version        string          `json:"version"`
-	Type           string          `json:"type"`
-	MasterNodeInfo MasterNodeInfo  `json:"masterNodeInfo"`
-	Capacity       v1.ResourceList `json:"capacity"`
-	NodeGroup      string          `json:"nodeGroup"`
+	Version        string         `json:"version"`
+	Type           string         `json:"type"`
+	MasterNodeInfo MasterNodeInfo `json:"masterNodeInfo"`
 }
 
 type MasterNodeInfo struct {
@@ -37,16 +35,6 @@ type NodeStats struct {
 	MasterMinCPU          int64  `json:"masterMinCPU"`
 	MasterMinMemory       int64  `json:"masterMinMemory"`
 	MinimalKubeletVersion string `json:"minimalKubeletVersion"`
-}
-
-type NodeGroupCapacity struct {
-	CPU    *resource.Quantity `json:"CPU"`
-	Memory *resource.Quantity `json:"memory"`
-}
-
-type NodeGroupCapacityInt64 struct {
-	CPU    int64 `json:"CPU"`
-	Memory int64 `json:"memory"`
 }
 
 func ApplyPricingNodeFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
@@ -78,9 +66,6 @@ func ApplyPricingNodeFilter(obj *unstructured.Unstructured) (go_hook.FilterResul
 	n.MasterNodeInfo.CPU = node.Status.Allocatable.Cpu()
 	n.MasterNodeInfo.Memory = node.Status.Allocatable.Memory()
 
-	n.Capacity = node.Status.Capacity
-	n.NodeGroup = node.Labels["node.deckhouse.io/group"]
-
 	return n, nil
 }
 
@@ -108,13 +93,8 @@ func nodeHandler(input *go_hook.HookInput) error {
 	var minNodeVersion *semver.Version
 	stats := NodeStats{}
 
-	ngc := map[string]NodeGroupCapacity{}
-	nodeGroupsCapacity := map[string]NodeGroupCapacityInt64{}
-
 	for _, s := range snaps {
 		node := s.(*Node)
-
-		input.LogEntry.Printf("Node: %s, cpu: %v, mem: %s", node.NodeGroup, node.Capacity.Cpu().String(), node.Capacity.Memory().String())
 
 		nodeVersion, err := semver.NewVersion(node.Version)
 		if err != nil {
@@ -122,18 +102,6 @@ func nodeHandler(input *go_hook.HookInput) error {
 		}
 		if minNodeVersion == nil || nodeVersion.LessThan(minNodeVersion) {
 			minNodeVersion = nodeVersion
-		}
-
-		if node.NodeGroup != "" {
-			if _, ok := ngc[node.NodeGroup]; !ok {
-				ngc[node.NodeGroup] = NodeGroupCapacity{
-					CPU:    node.Capacity.Cpu(),
-					Memory: node.Capacity.Memory(),
-				}
-			} else {
-				ngc[node.NodeGroup].CPU.Add(*node.Capacity.Cpu())
-				ngc[node.NodeGroup].Memory.Add(*node.Capacity.Memory())
-			}
 		}
 
 		if node.Type == "Static" {
@@ -163,15 +131,6 @@ func nodeHandler(input *go_hook.HookInput) error {
 
 	stats.MinimalKubeletVersion = fmt.Sprintf("%d.%d", minNodeVersion.Major(), minNodeVersion.Minor())
 
-	for k, v := range ngc {
-		nodeGroupsCapacity[k] = NodeGroupCapacityInt64{
-			CPU:    v.CPU.Value(),
-			Memory: v.Memory.Value(),
-		}
-	}
-
 	input.Values.Set("flantIntegration.internal.nodeStats", stats)
-	input.Values.Set("flantIntegration.internal.nodeGroupsCapacity", nodeGroupsCapacity)
-
 	return nil
 }
