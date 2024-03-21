06dec03d7cc8a413bce828c739f8ef264ccd05fc
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-03-21 15:26:15 +0300

    [zvirt] static node
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -103,14 +103,9 @@ var DefaultImagesDigests = map[string]interface{}{
 		"yandexCsiPlugin":           "imageHash-cloudProviderYandex-yandexCsiPlugin",
 	},
 	"cloudProviderZvirt": map[string]interface{}{
-		"capzControllerManager":     "imageHash-cloudProviderZvirt-capzControllerManager",
-		"cloudControllerManager125": "imageHash-cloudProviderZvirt-cloudControllerManager125",
-		"cloudControllerManager126": "imageHash-cloudProviderZvirt-cloudControllerManager126",
-		"cloudControllerManager127": "imageHash-cloudProviderZvirt-cloudControllerManager127",
-		"cloudControllerManager128": "imageHash-cloudProviderZvirt-cloudControllerManager128",
-		"cloudControllerManager129": "imageHash-cloudProviderZvirt-cloudControllerManager129",
-		"cloudDataDiscoverer":       "imageHash-cloudProviderZvirt-cloudDataDiscoverer",
-		"zvirtCsiDriver":            "imageHash-cloudProviderZvirt-zvirtCsiDriver",
+		"capzControllerManager": "imageHash-cloudProviderZvirt-capzControllerManager",
+		"cloudDataDiscoverer":   "imageHash-cloudProviderZvirt-cloudDataDiscoverer",
+		"zvirtCsiDriver":        "imageHash-cloudProviderZvirt-zvirtCsiDriver",
 	},
 	"cniCilium": map[string]interface{}{
 		"builderArtifact":        "imageHash-cniCilium-builderArtifact",
