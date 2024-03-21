c9861308aa69bdab34a177c221bc81da1b703aec
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2024-03-21 10:50:09 +0300

    go
    Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

--- ee/be/modules/600-flant-integration/images/flant-pricing/hooks/node_group_resources_metrics.sh
+++ ee/be/modules/600-flant-integration/images/flant-pricing/hooks/node_group_resources_metrics.sh
@@ -49,15 +49,6 @@ function __main__() {
     memory="$(jq -cr '.memory' <<< "$ng_capacity")"
     labels="$(context::jq -cr --arg ng_name "$node_group_name" '.snapshots.ngs[].filterResult | select(.name == $ng_name) | ((.taints // [] | .[] | {("taint-"+.effect): .key}) // {}) as $t | (.labels // {}) as $l | $t * $l * {"name": .name}')"
 
-    context::jq -c --arg memory "$memory" --argjson labels "$labels" --arg group "$group" '
-      {
-        "name": "flant_pricing_node_group_memory_bytes",
-        "group": $group,
-        "set": ($memory | tonumber),
-        "labels": $labels
-      }
-      '
-
     context::jq -c --arg cpu "$cpu" --argjson labels "$labels" --arg group "$group" '
       {
         "name": "flant_pricing_node_group_cpu_cores",
