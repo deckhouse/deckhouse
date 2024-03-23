74ee25f93d6f67237b16fd343012f0469e8d9b70
Pavel Tishkov <pavel.tishkov@flant.com>
2024-03-23 13:08:15 +0300

    [cloud-provider-dvp] add module tests
    Signed-off-by: Pavel Tishkov <pavel.tishkov@flant.com>

---
+++ modules/030-cloud-provider-dvp/template_tests/module_test.go
@@ -0,0 +1,39 @@
+/*
+Copyright 2021 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+
+User-stories:
+1. There are module settings. They must be exported via Secret d8-node-manager-cloud-provider.
+2. There are applications which must be deployed — cloud-controller-manager, azure-csi-driver, simple-bridge.
+
+*/
+
+package template_tests
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/deckhouse/deckhouse/testing/helm"
+)
+
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "")
+}
