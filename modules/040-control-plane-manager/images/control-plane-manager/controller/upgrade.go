a15376712daa393d649e195674e35f2cfcb3318b
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2024-03-28 02:22:05 +0300

    go
    Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

--- modules/040-control-plane-manager/images/control-plane-manager/controller/upgrade.go
+++ modules/040-control-plane-manager/images/control-plane-manager/controller/upgrade.go
@@ -19,6 +19,7 @@ package main
 import (
 	"context"
 	"fmt"
+
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/mod/semver"
 	rbac "k8s.io/api/rbac/v1"
@@ -31,37 +32,38 @@ import (
 const clusterAdminsGroupAndClusterRoleBinding = "kubeadm:cluster-admins"
 
 func upgradeToK8s129() error {
-	log.Println("I'm fine 1")
 	config, _ := rest.InClusterConfig()
 	kubeClient, _ := kubernetes.NewForConfig(config)
 
 	_, err := kubeClient.RbacV1().ClusterRoleBindings().Get(context.TODO(), clusterAdminsGroupAndClusterRoleBinding, metav1.GetOptions{})
 
-	log.Println("I'm fine 2", err)
-	if apierrors.IsNotFound(err) {
-		log.Println("I'm fine 3")
-		log.Print("create ClusterRoleBinding \"kubeadm:cluster-admins\"")
-		clusterRoleBinding := &rbac.ClusterRoleBinding{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: clusterAdminsGroupAndClusterRoleBinding,
-			},
-			RoleRef: rbac.RoleRef{
-				APIGroup: rbac.GroupName,
-				Kind:     "ClusterRole",
-				Name:     "cluster-admin",
-			},
-			Subjects: []rbac.Subject{
-				{
-					Kind: rbac.GroupKind,
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Print("create ClusterRoleBinding \"kubeadm:cluster-admins\"")
+
+			clusterRoleBinding := &rbac.ClusterRoleBinding{
+				ObjectMeta: metav1.ObjectMeta{
 					Name: clusterAdminsGroupAndClusterRoleBinding,
 				},
-			},
-		}
+				RoleRef: rbac.RoleRef{
+					APIGroup: rbac.GroupName,
+					Kind:     "ClusterRole",
+					Name:     "cluster-admin",
+				},
+				Subjects: []rbac.Subject{
+					{
+						Kind: rbac.GroupKind,
+						Name: clusterAdminsGroupAndClusterRoleBinding,
+					},
+				},
+			}
 
-		_, err = kubeClient.RbacV1().ClusterRoleBindings().Create(context.TODO(), clusterRoleBinding, metav1.CreateOptions{})
-		if err != nil {
-			return fmt.Errorf("create ClusterRoleBinding \"kubeadm:cluster-admins\" error: %v", err)
+			_, err = kubeClient.RbacV1().ClusterRoleBindings().Create(context.TODO(), clusterRoleBinding, metav1.CreateOptions{})
+			if err != nil {
+				return fmt.Errorf("create ClusterRoleBinding \"kubeadm:cluster-admins\" error: %v", err)
+			}
 		}
+		return err
 	}
 
 	return nil
