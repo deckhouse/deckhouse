88a11f9b11f6b22ec233adfd758bae5320a47079
Ilya Mayorov <37873799+faroyam@users.noreply.github.com>
2024-03-28 20:57:33 +0700

    Support stop message (#7467)
    Signed-off-by: Ilya Mayorov <ilya.mayorov@flant.com>
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

--- dhctl/pkg/server/rpc/dhctl/dhctl.go
+++ dhctl/pkg/server/rpc/dhctl/dhctl.go
@@ -15,13 +15,120 @@
 package dhctl
 
 import (
+	"fmt"
+	"log/slog"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/deckhouse/deckhouse/dhctl/pkg/app"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/config"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/operations/bootstrap"
 	pb "github.com/deckhouse/deckhouse/dhctl/pkg/server/pb/dhctl"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/system/ssh"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/system/ssh/session"
 )
 
-func New() *Service {
-	return &Service{}
+func New(podName string, log *slog.Logger) *Service {
+	return &Service{
+		podName: podName,
+		log:     log,
+	}
 }
 
 type Service struct {
 	pb.UnimplementedDHCTLServer
+
+	podName string
+	log     *slog.Logger
+}
+
+func prepareSSHClient(config *config.ConnectionConfig) (*ssh.Client, error) {
+	keysPaths := make([]string, 0, len(config.SSHConfig.SSHAgentPrivateKeys))
+	for _, key := range config.SSHConfig.SSHAgentPrivateKeys {
+		keyPath, err := writeTempFile([]byte(strings.TrimSpace(key.Key) + "\n"))
+		if err != nil {
+			return nil, fmt.Errorf("failed to write ssh private key: %w", err)
+		}
+		keysPaths = append(keysPaths, keyPath)
+	}
+	normalizedKeysPaths, err := app.ParseSSHPrivateKeyPaths(keysPaths)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing ssh agent private keys %v: %w", normalizedKeysPaths, err)
+	}
+	keys := make([]session.AgentPrivateKey, 0, len(normalizedKeysPaths))
+	for i, key := range normalizedKeysPaths {
+		keys = append(keys, session.AgentPrivateKey{
+			Key:        key,
+			Passphrase: config.SSHConfig.SSHAgentPrivateKeys[i].Passphrase,
+		})
+	}
+
+	var sshHosts []string
+	if len(config.SSHHosts) > 0 {
+		for _, h := range config.SSHHosts {
+			sshHosts = append(sshHosts, h.Host)
+		}
+	} else {
+		mastersIPs, err := bootstrap.GetMasterHostsIPs()
+		if err != nil {
+			return nil, err
+		}
+		sshHosts = mastersIPs
+	}
+
+	sess := session.NewSession(session.Input{
+		User:           config.SSHConfig.SSHUser,
+		Port:           portToString(config.SSHConfig.SSHPort),
+		BastionHost:    config.SSHConfig.SSHBastionHost,
+		BastionPort:    portToString(config.SSHConfig.SSHBastionPort),
+		BastionUser:    config.SSHConfig.SSHBastionUser,
+		ExtraArgs:      config.SSHConfig.SSHExtraArgs,
+		AvailableHosts: sshHosts,
+	})
+
+	sshClient, err := ssh.NewClient(sess, keys).Start()
+	if err != nil {
+		return nil, err
+	}
+
+	return sshClient, nil
+}
+
+func writeTempFile(data []byte) (string, error) {
+	f, err := os.CreateTemp("", "*")
+	if err != nil {
+		return "", fmt.Errorf("creating temp file: %w", err)
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return "", fmt.Errorf("writing temp file: %w", err)
+	}
+
+	return f.Name(), nil
+}
+
+func portToString(p *int32) string {
+	if p == nil {
+		return ""
+	}
+	return strconv.Itoa(int(*p))
+}
+
+func combineYAMLs(yamls ...string) string {
+	var res string
+	for _, yaml := range yamls {
+		if yaml == "" {
+			continue
+		}
+
+		if res != "" {
+			res += "---\n"
+		}
+
+		res = res + strings.TrimSpace(yaml) + "\n"
+	}
+
+	return res
 }
