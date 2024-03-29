29d3fa9bf88efa09120e8c559776bccc851c8391
Stepan Paksashvili <stepan.paksashvili@flant.com>
2024-03-29 18:09:13 +0300

    [admission-policy-engine] fix test
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- modules/015-admission-policy-engine/hooks/storage_class_change_test.go
+++ modules/015-admission-policy-engine/hooks/storage_class_change_test.go
@@ -41,6 +41,7 @@ var _ = Describe("Modules :: admission-policy-engine :: hooks :: storage_class_c
 	Context("Global storage class is set but denyVulnerableImages is disabled", func() {
 		BeforeEach(func() {
 			f.BindingContexts.Set(f.KubeStateSet(""))
+			f.ConfigValuesSet("global.storageClass", "test")
 			f.RunHook()
 		})
 		It("Should not set effectiveClass", func() {
@@ -66,6 +67,7 @@ var _ = Describe("Modules :: admission-policy-engine :: hooks :: storage_class_c
 		BeforeEach(func() {
 			f.BindingContexts.Set(f.KubeStateSet(""))
 			f.ValuesSet("admissionPolicyEngine.denyVulnerableImages.enabled", true)
+			f.ConfigValuesSet("global.storageClass", "global")
 			f.ConfigValuesSet("admissionPolicyEngine.denyVulnerableImages.storageClass", "test1")
 			f.RunHook()
 		})
