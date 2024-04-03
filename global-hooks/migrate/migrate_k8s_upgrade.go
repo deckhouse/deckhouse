a867b462a5860c7a7d7fb153b86190765b3faa00
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2024-04-03 12:49:44 +0300

    go
    Signed-off-by: Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>

--- global-hooks/migrate/migrate_k8s_upgrade.go
+++ global-hooks/migrate/migrate_k8s_upgrade.go
@@ -70,25 +70,18 @@ func k8sPostUpgrade(input *go_hook.HookInput, dc dependency.Container) error {
 
 	input.LogEntry.Printf("kubernetesVersion: %s", kubernetesVersion)
 
-	input.LogEntry.Printf("debug kubernetesVersion: %s (%v)", kubernetesVersion, semver.Compare("v1.29.0", kubernetesVersion))
-	input.LogEntry.Println("debug 1")
 	// if kubernetesVersion < v1.29.0
 	if semver.Compare("v1.29.0", kubernetesVersion) == 1 {
 		return nil
 	}
-	input.LogEntry.Println("debug 2")
-
-	input.LogEntry.Println("debug 3")
 
 	_, err = kubeCl.RbacV1().ClusterRoleBindings().Get(context.TODO(), clusterAdminsGroupAndClusterRoleBinding, metav1.GetOptions{})
 	if err == nil {
 		return nil
 	}
-	input.LogEntry.Println("debug 4")
 	if !apierrors.IsNotFound(err) {
 		return fmt.Errorf("error: %v", err)
 	}
-	input.LogEntry.Println("debug 5")
 	clusterRoleBinding := &rbac.ClusterRoleBinding{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "rbac.authorization.k8s.io/v1",
