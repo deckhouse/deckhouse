fc29d462aa2ca1a6698a1d9637a33bacda872a05
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-04-03 19:53:03 +0300

    [zvirt] ca bundle
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/zvirt/terraform-modules/providers.tf
+++ ee/candi/cloud-providers/zvirt/terraform-modules/providers.tf
@@ -1,9 +1,14 @@
 # Copyright 2024 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
+locals {
+  tls_ca_bundle = base64decode(var.providerClusterConfiguration.provider.caBundle)
+}
+
 provider "ovirt" {
   url = var.providerClusterConfiguration.provider.server
   username = var.providerClusterConfiguration.provider.username
   password = var.providerClusterConfiguration.provider.password
   tls_insecure = var.providerClusterConfiguration.provider.insecure
+  tls_ca_bundle = local.tls_ca_bundle
 }
