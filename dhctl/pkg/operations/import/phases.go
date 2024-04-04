75a996e18952e702d62a9cc8afaa7518fcd34d64
Timofey Kirillov <timofey.kirillov@flant.com>
2024-04-04 19:43:22 +0300

    [dhctl] feat(dhctl-for-commander): add import operation skeleton without real implementation, rework on-phase hook
    On-phase-hook in phased-execution-context reworked so that it can accept additional completed phase data argument.
    
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

---
+++ dhctl/pkg/operations/import/phases.go
@@ -0,0 +1,20 @@
+package _import
+
+import (
+	"github.com/deckhouse/deckhouse/dhctl/pkg/operations/check"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/operations/phases"
+)
+
+type (
+	PhaseData struct {
+		ScanResult  *ScanResult
+		CheckResult *check.CheckResult
+	}
+	OnPhaseFunc phases.OnPhaseFunc[PhaseData]
+)
+
+const (
+	ScanPhase    phases.OperationPhase = "Scan"
+	CapturePhase phases.OperationPhase = "Capture"
+	CheckPhase   phases.OperationPhase = "Check"
+)
