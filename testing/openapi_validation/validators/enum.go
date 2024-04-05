f170743424867cfa56c9803d494046e3cfc28354
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-04-05 13:01:20 +0300

    [zvirt] fix
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- testing/openapi_validation/validators/enum.go
+++ testing/openapi_validation/validators/enum.go
@@ -139,6 +139,9 @@ var (
 		"modules/030-cloud-provider-zvirt/openapi/values.yaml": {
 			// ignore internal values
 			"properties.internal.properties.providerClusterConfiguration.properties.apiVersion",
+		},
+		"ee/modules/030-cloud-provider-zvirt/openapi/values.yaml": {
+			// ignore internal values
 			"properties.internal.properties.providerDiscoveryData.properties.apiVersion.enum",
 		},
 		"ee/modules/030-cloud-provider-vcd/openapi/values.yaml": {
