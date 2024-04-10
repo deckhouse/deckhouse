e176c1bbacd5dd5738f1df3ebc7a6e0b8fc5b444
Denis Romanenko <denis.romanenko@flant.com>
2024-04-10 22:16:20 +0300

    ++
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- candi/bashible/bashbooster/50_registrypackages.sh
+++ candi/bashible/bashbooster/50_registrypackages.sh
@@ -68,7 +68,7 @@ try:
 except ImportError as e:
     from urllib2 import urlretrieve, build_opener, install_opener
 
-socket.setdefaulttimeout(60)
+socket.setdefaulttimeout(180)
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
