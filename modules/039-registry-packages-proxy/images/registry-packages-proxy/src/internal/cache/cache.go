28775eb64499d62e33a6ea04f00f8d232cb3c32e
Denis Romanenko <denis.romanenko@flant.com>
2024-04-10 21:34:14 +0300

    ++
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- modules/039-registry-packages-proxy/images/registry-packages-proxy/src/internal/cache/cache.go
+++ modules/039-registry-packages-proxy/images/registry-packages-proxy/src/internal/cache/cache.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"io"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/pkg/errors"
@@ -26,7 +27,14 @@ import (
 	"github.com/deckhouse/deckhouse/go_lib/registry-packages-proxy/cache"
 )
 
+type CacheEntry struct {
+	lastAccessTime time.Time
+	size           int64
+}
+
 type Cache struct {
+	storage map[string]*CacheEntry
+	sync.RWMutex
 	logger        *log.Entry
 	root          string
 	retentionSize uint64
@@ -35,7 +43,9 @@ type Cache struct {
 }
 
 func New(logger *log.Entry, root string, retentionSize uint64, metrics *Metrics) *Cache {
+	storage := make(map[string]*CacheEntry)
 	return &Cache{
+		storage:       storage,
 		logger:        logger,
 		root:          root,
 		retentionSize: retentionSize,
@@ -62,14 +72,27 @@ func (c *Cache) Get(digest string) (int64, io.ReadCloser, error) {
 		return 0, nil, err
 	}
 
+	c.Lock()
+	defer c.Unlock()
+	c.storage[digest].lastAccessTime = time.Now()
+
 	return stat.Size(), file, nil
 }
 
 func (c *Cache) Set(digest string, size int64, reader io.Reader) error {
+	c.logger.Infof("write file with digest %s with size %d to the cache dir", digest, size)
 	err := c.copyPackage(digest, reader)
 	if err != nil {
 		return err
 	}
+
+	c.Lock()
+	defer c.Unlock()
+	c.storage[digest] = &CacheEntry{
+		lastAccessTime: time.Now(),
+		size:           size,
+	}
+
 	c.metrics.CacheSize.Add(float64(size))
 	return nil
 }
