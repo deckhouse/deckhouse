b5198ebc7b75b5f0a37754a40b61bbf3325f5cb3
Maxim Vasilenko <maksim.vasilenko@flant.com>
2024-04-15 12:44:52 +0300

    do not generate deckhousereleases.yaml and skip release channels if the are not in registry with `--release` option
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>

--- dhctl/pkg/operations/mirror.go
+++ dhctl/pkg/operations/mirror.go
@@ -96,12 +96,15 @@ func MirrorDeckhouseToLocalFS(
 		return fmt.Errorf("pull release channels: %w", err)
 	}
 
-	log.InfoF("Generating DeckhouseRelease manifests...\t")
-	deckhouseReleasesManifestFile := filepath.Join(filepath.Dir(mirrorCtx.BundlePath), "deckhousereleases.yaml")
-	if err = mirror.GenerateDeckhouseReleaseManifests(versions, deckhouseReleasesManifestFile, layouts.ReleaseChannel); err != nil {
-		return fmt.Errorf("Generate DeckhouseRelease manifests: %w", err)
+	// We should not generate deckhousereleases.yaml manifest for single-release bundles
+	if mirrorCtx.SpecificVersion == nil {
+		log.InfoF("Generating DeckhouseRelease manifests...\t")
+		deckhouseReleasesManifestFile := filepath.Join(filepath.Dir(mirrorCtx.BundlePath), "deckhousereleases.yaml")
+		if err = mirror.GenerateDeckhouseReleaseManifests(versions, deckhouseReleasesManifestFile, layouts.ReleaseChannel); err != nil {
+			return fmt.Errorf("Generate DeckhouseRelease manifests: %w", err)
+		}
+		log.InfoLn("✅")
 	}
-	log.InfoLn("✅")
 
 	if err = mirror.PullDeckhouseImages(mirrorCtx, layouts); err != nil {
 		return fmt.Errorf("pull Deckhouse: %w", err)
