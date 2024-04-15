bd1908fd587c22eb3fc54596a8669c53487518b3
Ilya Mayorov <37873799+faroyam@users.noreply.github.com>
2024-04-15 13:17:50 +0700

    [dhctl] feat(cliuster-import): Improve import logs (#8134)
    Signed-off-by: Ilya Mayorov <ilya.mayorov@flant.com>
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

--- dhctl/pkg/log/logger.go
+++ dhctl/pkg/log/logger.go
@@ -140,6 +140,7 @@ func NewPrettyLogger(opts LoggerOptions) *PrettyLogger {
 			"converge":  {"🛸 ~ Converge: %s", ConvergeOptions},
 			"bootstrap": {"⛵ ~ Bootstrap: %s", BootstrapOptions},
 			"mirror":    {"🪞 ~ Mirror: %s", MirrorOptions},
+			"import":    {"📦 ~ Import: %s", ImportOptions},
 			"default":   {"%s", BoldOptions},
 		},
 		isDebug: opts.IsDebug,
