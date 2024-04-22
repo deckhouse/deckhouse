fa101272bbc0691daa85732ec701bdf148786444
Denis Romanenko <denis.romanenko@flant.com>
2024-04-22 22:09:28 +0300

    add migrator and fix discoverer
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- modules/030-cloud-provider-yandex/images/cloud-migrator/src/disks.go
+++ modules/030-cloud-provider-yandex/images/cloud-migrator/src/disks.go
@@ -19,39 +19,25 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/yandex-cloud/go-genproto/yandex/cloud/compute/v1"
 	ycsdk "github.com/yandex-cloud/go-sdk"
 	"github.com/yandex-cloud/go-sdk/iamkey"
-
-	"github.com/deckhouse/deckhouse/go_lib/cloud-data/apis/v1alpha1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
-type Discoverer struct {
+type DiskMigrator struct {
 	logger      *log.Entry
 	folderID    string
-	clusterName string
 	sdk         *ycsdk.SDK
+	clusterName string
+	client      *kubernetes.Clientset
 }
 
-func NewDiscoverer(logger *log.Entry) *Discoverer {
-	folderID := os.Getenv("YC_FOLDER_ID")
-	if folderID == "" {
-		logger.Fatal("Cannot get YC_FOLDER_ID env")
-	}
-
-	saKeyJSON := os.Getenv("YC_SA_KEY_JSON")
-	if saKeyJSON == "" {
-		logger.Fatal("Cannot get YC_SA_KEY_JSON env")
-	}
-
-	clusterName := os.Getenv("CLUSTER_NAME")
-	if clusterName == "" {
-		logger.Fatal("Cannot get CLUSTER_NAME env")
-	}
-
+func NewDiskMigrator(logger *log.Entry, kubeClient *kubernetes.Clientset, folderID, saKeyJSON, clusterName string) *DiskMigrator {
 	saKeyJSONBytes := []byte(saKeyJSON)
 	key, err := iamkey.ReadFromJSONBytes(saKeyJSONBytes)
 	if err != nil {
@@ -70,45 +56,54 @@ func NewDiscoverer(logger *log.Entry) *Discoverer {
 		log.Fatalf("Failed to build YC SDK: %v", err)
 	}
 
-	return &Discoverer{
+	return &DiskMigrator{
 		logger:      logger,
 		folderID:    folderID,
-		clusterName: clusterName,
 		sdk:         sdk,
+		client:      kubeClient,
+		clusterName: clusterName,
 	}
 }
 
-// NotImplemented
-func (d *Discoverer) InstanceTypes(ctx context.Context) ([]v1alpha1.InstanceType, error) {
-	return nil, nil
-}
+func (d *DiskMigrator) MigrateDisks(ctx context.Context) error {
 
-// NotImplemented
-func (d *Discoverer) DiscoveryData(ctx context.Context, cloudProviderDiscoveryData []byte) ([]byte, error) {
-	return nil, nil
-}
-
-func (d *Discoverer) DisksMeta(ctx context.Context) ([]v1alpha1.DiskMeta, error) {
 	disks, err := d.getDisksCreatedByCSIDriver(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get disks: %v", err)
+		return fmt.Errorf("failed to get disks: %v", err)
 	}
 
-	disksMeta := make([]v1alpha1.DiskMeta, 0, len(disks))
+	pvs, err := d.getPVFromCluster(ctx)
 
-	for _, disk := range disks {
-		// skip disks created from another clusters
-		clusterName, ok := disk.Labels["cluster"]
-		if ok && clusterName != d.clusterName {
+	pvMap := make(map[string]struct{}, len(pvs))
+	for _, pv := range pvs {
+		// if not yandex csi PV, skip it
+		if pv.Annotations[" pv.kubernetes.io/provisioned-by"] != "yandex.csi.flant.com" {
 			continue
 		}
-		disksMeta = append(disksMeta, v1alpha1.DiskMeta{ID: disk.Id, Name: disk.Name})
+		pvMap[pv.Name] = struct{}{}
 	}
 
-	return disksMeta, nil
+	for _, disk := range disks {
+		if _, ok := disk.Labels["cluster"]; ok {
+			d.logger.Warnf("disk %s already has 'cluster' label, skipping", disk.Name)
+			continue
+		}
+
+		if _, ok := pvMap[disk.Name]; !ok {
+			d.logger.Warnf("disk %s is not present in the cluster, skipping", disk.Name)
+			continue
+		}
+
+		err := d.migrateDisk(ctx, disk)
+		if err != nil {
+			d.logger.Errorf("failed to migrate disk: %v", err)
+		}
+	}
+
+	return nil
 }
 
-func (d *Discoverer) getDisksCreatedByCSIDriver(ctx context.Context) ([]*compute.Disk, error) {
+func (d *DiskMigrator) getDisksCreatedByCSIDriver(ctx context.Context) ([]*compute.Disk, error) {
 	diskList, err := d.sdk.Compute().Disk().List(ctx, &compute.ListDisksRequest{
 		FolderId: d.folderID,
 	})
@@ -130,4 +125,19 @@ func (d *Discoverer) getDisksCreatedByCSIDriver(ctx context.Context) ([]*compute
 	}
 
 	return disksCreatedByCSIDriver, nil
+}
+
+func (d *DiskMigrator) migrateDisk(ctx context.Context, disk *compute.Disk) error {
+	disk.Labels["cluster"] = d.clusterName
+	_, err := d.sdk.Compute().Disk().Update(ctx, &compute.UpdateDiskRequest{DiskId: disk.Id, Labels: disk.Labels})
+	return err
+}
+
+func (d *DiskMigrator) getPVFromCluster(ctx context.Context) ([]v1.PersistentVolume, error) {
+	pvs, err := d.client.CoreV1().PersistentVolumes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return pvs.Items, nil
 }
---
+++ modules/030-cloud-provider-yandex/images/cloud-migrator/src/main.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2024 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	var (
+		kubeConfig  string
+		folderID    string
+		saKeyJSON   string
+		clusterName string
+	)
+
+	folderID = os.Getenv("YC_FOLDER_ID")
+	kubeConfig = os.Getenv("KUBE_CONFIG")
+	saKeyJSON = os.Getenv("YC_SA_KEY_JSON")
+	clusterName = os.Getenv("CLUSTER_NAME")
+
+	flag.StringVar(&kubeConfig, "kube-config", kubeConfig, "Path to kube-config")
+	flag.StringVar(&folderID, "folder-id", folderID, "Yandex folder id")
+	flag.StringVar(&saKeyJSON, "sa-key-json", saKeyJSON, "Yandex SA key in JSON format")
+	flag.StringVar(&saKeyJSON, "cluster-name", clusterName, "Cluster name")
+
+	flag.Parse()
+
+	var formatter log.Formatter = &log.TextFormatter{
+		DisableColors:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+	}
+	formatter = &log.JSONFormatter{}
+
+	logger := log.New()
+	logger.SetLevel(log.InfoLevel)
+	logger.SetFormatter(formatter)
+
+	if folderID == "" {
+		logger.Fatal("Cannot get YC_FOLDER_ID")
+	}
+
+	if saKeyJSON == "" {
+		logger.Fatal("Cannot get YC_SA_KEY_JSON")
+	}
+
+	if clusterName == "" {
+		logger.Fatal("Cannot get CLUSTER_NAME")
+	}
+
+	// init kube clients
+	client, err := InitClient(kubeConfig)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	d := NewDiskMigrator(log.NewEntry(logger), client, folderID, saKeyJSON, clusterName)
+	err = d.MigrateDisks(context.Background())
+	if err != nil {
+		logger.Fatal(err)
+	}
+}
