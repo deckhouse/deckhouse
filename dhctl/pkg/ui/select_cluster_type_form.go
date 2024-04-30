ca7fea570b650da7f856e6f94f8db2127708305f
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2024-04-30 15:56:44 +0300

    [dhctl] UI bootstrap
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- dhctl/pkg/ui/select_cluster_type_form.go
+++ dhctl/pkg/ui/select_cluster_type_form.go
@@ -7,21 +7,35 @@ import (
 )
 
 func SelectClusterForm(b *configBuilder, onNext func()) (tview.Primitive, []tview.Primitive) {
-	lbl1 := tview.NewTextView().
+	optionsGrid := tview.NewGrid().
+		SetColumns(30, 0).SetRows(2, 2)
+
+	lblType := tview.NewTextView().
 		SetText("Choice cluster type:")
 
-	tview.NewList()
+	lblProvider := tview.NewTextView().
+		SetText("Provider:")
 
-	types := tview.NewDropDown().SetOptions([]string{cloudCluster, staticCluster}, func(text string, _ int) {
+	providers := tview.NewDropDown().SetOptions([]string{"Yandex", "Openstack", "GCP"}, func(text string, _ int) {
+	}).SetFieldWidth(20).SetCurrentOption(0)
+
+	types := tview.NewDropDown().SetOptions([]string{staticCluster, cloudCluster}, func(text string, _ int) {
+		switch text {
+		case staticCluster:
+			providers.SetCurrentOption(0)
+			providers.SetDisabled(true)
+		case cloudCluster:
+			providers.SetDisabled(false)
+		}
 		b.setClusterType(text)
 	}).SetFieldWidth(20).SetCurrentOption(0)
 
-	optionsGrid := tview.NewGrid().
-		SetColumns(30, 0).
-		AddItem(lbl1, 0, 0, 1, 1, 0, 0, false).
-		AddItem(types, 0, 1, 1, 1, 0, 0, false)
+	optionsGrid.AddItem(lblType, 0, 0, 1, 1, 0, 0, false).
+		AddItem(types, 0, 1, 1, 1, 0, 0, false).
+		AddItem(lblProvider, 1, 0, 1, 1, 0, 0, false).
+		AddItem(providers, 1, 1, 1, 1, 0, 0, false)
 
 	p, focusable := widget.OptionsPage(optionsGrid, onNext, nil)
 
-	return p, append([]tview.Primitive{types}, focusable...)
+	return p, append([]tview.Primitive{types, providers}, focusable...)
 }
