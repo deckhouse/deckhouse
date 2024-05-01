2a5847a886e3f866c3b06417d2f4f6be2d17cef7
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2024-05-01 16:45:17 +0300

    [dhctl] UI bootstrap
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- dhctl/pkg/ui/cluster.go
+++ dhctl/pkg/ui/cluster.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 
 	"github.com/deckhouse/deckhouse/dhctl/pkg/ui/state"
@@ -20,65 +21,81 @@ type clusterTypesSchema interface {
 }
 
 func clusterPage(st clusterTypeState, schema clusterTypesSchema, onNext func()) (tview.Primitive, []tview.Primitive) {
-	const constInputsWidth = 30
+	const (
+		constInputsWidth = 50
+		k8sVersionIndex  = 0
+		typeIndex        = 1
+		providerIndex    = 2
+		prefixIndex      = 3
+	)
 
-	lblType := tview.NewTextView().
-		SetText("Type")
+	form := tview.NewForm()
 
-	lblK8sVer := tview.NewTextView().
-		SetText("Kubernetes version")
+	initAddProvider := false
 
-	lblProvider := tview.NewTextView().
-		SetText("Provider")
+	addProviderPrefixItems := func() {
+		form.AddDropDown("Provider", schema.CloudProviders(), 0, func(option string, optionIndex int) {
+			st.SetProvider(option)
+		})
 
-	lblPrefix := tview.NewTextView().
-		SetText("Prefix")
-
-	providers := tview.NewDropDown().SetOptions(schema.CloudProviders(), func(text string, _ int) {
-		st.SetProvider(text)
-	}).SetFieldWidth(constInputsWidth).SetCurrentOption(0)
+		form.AddInputField("Prefix", "", constInputsWidth, nil, func(text string) {
+			st.SetClusterPrefix(text)
+		})
+	}
 
 	versions := schema.K8sVersions()
-	k8sVersions := tview.NewDropDown().SetOptions(versions, func(text string, _ int) {
-		st.SetK8sVersion(text)
-	}).SetFieldWidth(constInputsWidth).SetCurrentOption(len(versions) - 1)
+	form.AddDropDown("Kubernetes version", versions, len(versions)-1, func(option string, optionIndex int) {
+		st.SetK8sVersion(option)
+	})
 
-	prefix := tview.NewInputField().SetFieldWidth(constInputsWidth)
-
-	types := tview.NewDropDown().
-		SetOptions([]string{state.CloudCluster, state.StaticCluster}, func(text string, _ int) {
-			switch text {
-			case state.StaticCluster:
-				providers.SetCurrentOption(0)
-				providers.SetDisabled(true)
-				prefix.SetDisabled(true)
-				prefix.SetText("")
-			case state.CloudCluster:
-				providers.SetDisabled(false)
-				prefix.SetDisabled(false)
+	form.AddDropDown("Type", []string{state.CloudCluster, state.StaticCluster}, 0, func(option string, optionIndex int) {
+		switch option {
+		case state.StaticCluster:
+			if form.GetFormItemCount() > 2 {
+				form.RemoveFormItem(2)
+				form.RemoveFormItem(2)
 			}
-			st.SetClusterType(text)
-		}).SetFieldWidth(30).SetCurrentOption(0)
+		case state.CloudCluster:
+			if initAddProvider && form.GetFormItemCount() < 4 {
+				addProviderPrefixItems()
+			}
+
+			initAddProvider = true
+		}
+
+		st.SetClusterType(option)
+	})
+
+	addProviderPrefixItems()
+
+	errorLbl := tview.NewTextView().SetTextColor(tcell.ColorRed)
 
 	optionsGrid := tview.NewGrid().
-		SetColumns(constInputsWidth, 0).SetRows(2, 2, 2, 2).
-		//----------------------------------------------------------------------------------------------
-		AddItem(lblType, 0, 0, 1, 1, 0, 0, false).
-		AddItem(types, 0, 1, 1, 1, 0, 0, false).
-		//----------------------------------------------------------------------------------------------
-		AddItem(lblK8sVer, 1, 0, 1, 1, 0, 0, false).
-		AddItem(k8sVersions, 1, 1, 1, 1, 0, 0, false).
-		//----------------------------------------------------------------------------------------------
-		AddItem(lblProvider, 2, 0, 1, 1, 0, 0, false).
-		AddItem(providers, 2, 1, 1, 1, 0, 0, false).
-		//----------------------------------------------------------------------------------------------
-		AddItem(lblPrefix, 3, 0, 1, 1, 0, 0, false).
-		AddItem(prefix, 3, 1, 1, 1, 0, 0, false)
+		SetColumns(0).SetRows(0, 5).
+		AddItem(form, 0, 0, 1, 1, 0, 0, true).
+		AddItem(errorLbl, 1, 0, 1, 1, 0, 0, false)
 
 	p, focusable := widget.OptionsPage("Cluster settings", optionsGrid, func() {
-		st.SetClusterPrefix(prefix.GetText())
+		_, clType := form.GetFormItem(typeIndex).(*tview.DropDown).GetCurrentOption()
+		st.SetClusterType(clType)
+
+		_, k8sVersion := form.GetFormItem(k8sVersionIndex).(*tview.DropDown).GetCurrentOption()
+		st.SetK8sVersion(k8sVersion)
+
+		if form.GetFormItemCount() > 2 {
+			_, provider := form.GetFormItem(providerIndex).(*tview.DropDown).GetCurrentOption()
+			st.SetProvider(provider)
+
+			prefix := form.GetFormItem(prefixIndex).(*tview.InputField).GetText()
+			if len(prefix) < 1 {
+				errorLbl.SetText("Prefix is required")
+				return
+			}
+			st.SetClusterPrefix(prefix)
+		}
+
 		onNext()
 	}, nil)
 
-	return p, append([]tview.Primitive{types, providers, prefix}, focusable...)
+	return p, append([]tview.Primitive{optionsGrid}, focusable...)
 }
