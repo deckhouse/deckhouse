088fb432ad04d96124d3b61f6c217d0112de14d1
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2024-05-14 13:39:35 +0300

    item 18670
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- modules/340-extended-monitoring/images/extended-monitoring-exporter/src/main.go
+++ modules/340-extended-monitoring/images/extended-monitoring-exporter/src/main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(10 * 60 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
 func main() {
+	recordMetrics()
+
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe("127.0.0.1:8081", nil))
 }
