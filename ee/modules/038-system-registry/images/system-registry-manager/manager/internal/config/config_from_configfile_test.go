7d89a97300411ac481eadd126d039cd109a73c91
vadimartynov <vadim.martynov@flant.com>
2024-05-16 18:11:07 +0300

    Updated template rendering
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

--- ee/modules/038-system-registry/images/system-registry-manager/manager/internal/config/config_from_configfile_test.go
+++ ee/modules/038-system-registry/images/system-registry-manager/manager/internal/config/config_from_configfile_test.go
@@ -21,33 +21,81 @@ func TestNewFileConfig_WithEnv(t *testing.T) {
 	SetConfigFilePath(tmpFile.Name())
 
 	// Write test data to the temp config file
-	_, err = io.WriteString(tmpFile, ``)
+	_, err = io.WriteString(tmpFile, `
+leaderElection:
+  namespace: filenamespace
+  leaseDurationSeconds: 7200
+  renewDeadlineSeconds: 20
+  retryPeriodSeconds: 8
+etcd:
+  addresses: ["etcd1.example.com", "etcd2.example.com"]
+distribution:
+  image: distribution_image
+auth:
+  image: auth_image
+seaweedfs:
+  image: seaweedfs_image
+`)
 	assert.NoError(t, err)
 	tmpFile.Close()
 
 	// Set up test environment variables
-	os.Setenv("HOSTNAME", "testhostname")
-	os.Setenv("MY_IP", "testmyip")
-	os.Setenv("MY_POD_NAME", "testpodname")
-	os.Setenv("LEADER_ELECTION_NAMESPACE", "testnamespace")
-	os.Setenv("LEADER_ELECTION_LEASE_DURATION_SECONDS", "3600")
-	os.Setenv("LEADER_ELECTION_RENEW_DEADLINE_SECONDS", "10")
-	os.Setenv("LEADER_ELECTION_RETRY_PERIOD_SECONDS", "4")
+	os.Setenv("HOSTNAME", "filehostname")
+	os.Setenv("HOST_IP", "filemyip") // Correct the environment variable name
+	os.Setenv("POD_NAME", "filepodname")
+
+	// Restore the original environment variables after the test
+	defer func() {
+		os.Unsetenv("HOSTNAME")
+		os.Unsetenv("HOST_IP")
+		os.Unsetenv("POD_NAME")
+	}()
 
 	// Run the function under test
+	expectedCfg := &FileConfig{
+		HostName: "filehostname",
+		HostIP:   "filemyip",
+		PodName:  "filepodname",
+		LeaderElection: struct {
+			Namespace            string "mapstructure:\"namespace\""
+			LeaseDurationSeconds int    "mapstructure:\"leaseDurationSeconds\""
+			RenewDeadlineSeconds int    "mapstructure:\"renewDeadlineSeconds\""
+			RetryPeriodSeconds   int    "mapstructure:\"retryPeriodSeconds\""
+		}{
+			Namespace:            "filenamespace",
+			LeaseDurationSeconds: 7200,
+			RenewDeadlineSeconds: 20,
+			RetryPeriodSeconds:   8,
+		},
+		// Add new fields
+		Etcd: struct {
+			Addresses []string `mapstructure:"addresses"`
+		}{
+			Addresses: []string{"etcd1.example.com", "etcd2.example.com"},
+		},
+		Distribution: struct {
+			Image string `mapstructure:"image"`
+		}{
+			Image: "distribution_image",
+		},
+		Auth: struct {
+			Image string `mapstructure:"image"`
+		}{
+			Image: "auth_image",
+		},
+		Seaweedfs: struct {
+			Image string `mapstructure:"image"`
+		}{
+			Image: "seaweedfs_image",
+		},
+	}
 	cfg, err := NewFileConfig()
 
 	// Assert that no error occurred
 	assert.NoError(t, err)
 
-	// Assert the values in the config
-	assert.Equal(t, "testhostname", cfg.HostName)
-	assert.Equal(t, "testmyip", cfg.MyIP)
-	assert.Equal(t, "testpodname", cfg.MyPodName)
-	assert.Equal(t, "testnamespace", cfg.LeaderElection.Namespace)
-	assert.Equal(t, 3600, cfg.LeaderElection.LeaseDurationSeconds)
-	assert.Equal(t, 10, cfg.LeaderElection.RenewDeadlineSeconds)
-	assert.Equal(t, 4, cfg.LeaderElection.RetryPeriodSeconds)
+	// Assert the entire config structure
+	assert.Equal(t, expectedCfg, cfg)
 }
 
 func TestNewFileConfig_WithFile(t *testing.T) {
@@ -61,29 +109,68 @@ func TestNewFileConfig_WithFile(t *testing.T) {
 	// Write test data to the temp config file
 	_, err = io.WriteString(tmpFile, `
 hostName: filehostname
-myIP: filemyip
-myPodName: filepodname
+hostIP: filemyip
+podName: filepodname
 leaderElection:
   namespace: filenamespace
   leaseDurationSeconds: 7200
   renewDeadlineSeconds: 20
   retryPeriodSeconds: 8
+etcd:
+  addresses: ["etcd1.example.com", "etcd2.example.com"]
+distribution:
+  image: distribution_image
+auth:
+  image: auth_image
+seaweedfs:
+  image: seaweedfs_image
 `)
 	assert.NoError(t, err)
 	tmpFile.Close()
 
 	// Run the function under test
+	expectedCfg := &FileConfig{
+		HostName: "filehostname",
+		HostIP:   "filemyip",
+		PodName:  "filepodname",
+		LeaderElection: struct {
+			Namespace            string "mapstructure:\"namespace\""
+			LeaseDurationSeconds int    "mapstructure:\"leaseDurationSeconds\""
+			RenewDeadlineSeconds int    "mapstructure:\"renewDeadlineSeconds\""
+			RetryPeriodSeconds   int    "mapstructure:\"retryPeriodSeconds\""
+		}{
+			Namespace:            "filenamespace",
+			LeaseDurationSeconds: 7200,
+			RenewDeadlineSeconds: 20,
+			RetryPeriodSeconds:   8,
+		},
+		// Add new fields
+		Etcd: struct {
+			Addresses []string `mapstructure:"addresses"`
+		}{
+			Addresses: []string{"etcd1.example.com", "etcd2.example.com"},
+		},
+		Distribution: struct {
+			Image string `mapstructure:"image"`
+		}{
+			Image: "distribution_image",
+		},
+		Auth: struct {
+			Image string `mapstructure:"image"`
+		}{
+			Image: "auth_image",
+		},
+		Seaweedfs: struct {
+			Image string `mapstructure:"image"`
+		}{
+			Image: "seaweedfs_image",
+		},
+	}
 	cfg, err := NewFileConfig()
 
 	// Assert that no error occurred
 	assert.NoError(t, err)
 
-	// Assert the values in the config
-	assert.Equal(t, "filehostname", cfg.HostName)
-	assert.Equal(t, "filemyip", cfg.MyIP)
-	assert.Equal(t, "filepodname", cfg.MyPodName)
-	assert.Equal(t, "filenamespace", cfg.LeaderElection.Namespace)
-	assert.Equal(t, 7200, cfg.LeaderElection.LeaseDurationSeconds)
-	assert.Equal(t, 20, cfg.LeaderElection.RenewDeadlineSeconds)
-	assert.Equal(t, 8, cfg.LeaderElection.RetryPeriodSeconds)
+	// Assert the entire config structure
+	assert.Equal(t, expectedCfg, cfg)
 }
