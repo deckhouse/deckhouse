af5e650404c6e8d6a0fa178d59f353cd47b942bf
vadimartynov <vadim.martynov@flant.com>
2024-05-21 16:46:47 +0300

    Make generate and fixed matrix tests
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

--- modules/040-control-plane-manager/hooks/audit_policy_basic_targets_generated.go
+++ modules/040-control-plane-manager/hooks/audit_policy_basic_targets_generated.go
@@ -133,6 +133,7 @@ var auditPolicyBasicServiceAccounts = []string{
 	"system:serviceaccount:d8-system:deckhouse-tools",
 	"system:serviceaccount:d8-system:documentation",
 	"system:serviceaccount:d8-system:network-policy-engine",
+	"system:serviceaccount:d8-system:system-registry",
 	"system:serviceaccount:d8-system:terraform-auto-converger",
 	"system:serviceaccount:d8-system:terraform-state-exporter",
 	"system:serviceaccount:d8-system:webhook-handler",
