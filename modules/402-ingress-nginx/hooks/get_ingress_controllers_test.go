734a001a6c5620ae88bb6ec4b49ed61e332c1c20
Andrey Polovov <andrey.polovov@flant.com>
2024-05-21 16:28:50 +0300

    [l2-load-balancer] Cancelling the feature in favor of the renewed approach coming (#8429)
    Signed-off-by: Andrey Polovov <andrey.polovov@flant.com>

--- modules/402-ingress-nginx/hooks/get_ingress_controllers_test.go
+++ modules/402-ingress-nginx/hooks/get_ingress_controllers_test.go
@@ -67,7 +67,6 @@ spec:
   "ingressClass": "nginx",
   "controllerVersion": "1.6",
   "inlet": "LoadBalancer",
-  "l2LoadBalancer": {},
   "loadBalancer": {},
   "hstsOptions": {},
   "geoIP2": {},
@@ -177,7 +176,6 @@ spec:
   "static": {},
   "vpa": {"cpu": {}, "memory": {}}
 },
-"l2LoadBalancer": {},
 "loadBalancer": {},
 "loadBalancerWithProxyProtocol": {},
 "hostPortWithProxyProtocol": {},
@@ -198,7 +196,6 @@ spec:
   "vpa": {"cpu": {"max": "100m"}, "memory": {"max": "200Mi"}, "mode": "Auto"}
 },
 "loadBalancer": {},
-"l2LoadBalancer": {},
 "loadBalancerWithProxyProtocol": {},
 "hostPortWithProxyProtocol": {"httpPort": 80, "httpsPort": 443},
 "hostWithFailover": {},
@@ -218,7 +215,6 @@ spec:
   "vpa": {"cpu": {}, "memory": {}}
 },
 "loadBalancer": {},
-"l2LoadBalancer": {},
 "loadBalancerWithProxyProtocol": {},
 "hostPortWithProxyProtocol": {},
 "hostWithFailover": {},
