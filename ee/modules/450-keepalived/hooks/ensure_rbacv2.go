7298fff0c1d295f8c927cc1377e272fcb48fe225
Stepan Paksashvili <stepan.paksashvili@flant.com>
2024-06-03 16:22:44 +0300

    [deckhouse] add multitenancy-manager, keepalived, extended-monitoring, loki, chrony, okmeter, upmeter
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

---
+++ ee/modules/450-keepalived/hooks/ensure_rbacv2.go
@@ -0,0 +1,10 @@
+/*
+Copyright 2024 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package hooks
+
+import "github.com/deckhouse/deckhouse/go_lib/hooks/ensure_rbacv2"
+
+var _ = ensure_rbacv2.RegisterHook("keepalived", "others", []string{"/deckhouse/modules/450-keepalived/crds/*.yaml"})
