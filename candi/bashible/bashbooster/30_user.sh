3112496c0fe905487a8167acceb7388295b5af7e
borg-z <me@nikolay-z.top>
2024-06-03 17:24:29 +0300

    fix
    Signed-off-by: borg-z <me@nikolay-z.top>

--- candi/bashible/bashbooster/30_user.sh
+++ candi/bashible/bashbooster/30_user.sh
@@ -1,22 +1,3 @@
-# Bash Booster 0.6 <http://www.bashbooster.net>
-# =============================================
-#
-# Copyright (c) 2014, Dmitry Vakhrushev <self@kr41.net> and Contributors
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 bb-create_user_and_group() {
     local username="$1"
     local userid="$2"
