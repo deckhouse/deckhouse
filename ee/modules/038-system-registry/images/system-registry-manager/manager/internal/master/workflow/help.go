bba9eddb7c58bf32a897eab5e49db092a8e3e60e
vadimartynov <vadim.martynov@flant.com>
2024-06-04 12:42:21 +0300

    Splitted files

--- ee/modules/038-system-registry/images/system-registry-manager/manager/internal/master/workflow/help.go
+++ ee/modules/038-system-registry/images/system-registry-manager/manager/internal/master/workflow/help.go
@@ -5,50 +5,6 @@ Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https
 
 package workflow
 
-type SeaweedfsNodeManager interface {
-	// Info
-	GetNodeClusterStatus() (*SeaweedfsNodeClusterStatus, error)
-	GetNodeRunningStatus() (*SeaweedfsNodeRunningStatus, error)
-	GetNodeIP() string
-
-	// Cluster actions
-	AddNodeToCluster(newNodeIP string) error
-	RemoveNodeFromCluster(removeNodeIP string) error
-
-	// Runtime actions
-	CreateNodeManifests(request *SeaweedfsCreateNodeRequest) error
-	UpdateNodeManifests(request *SeaweedfsUpdateNodeRequest) error
-	DeleteNodeManifests() error
-}
-
-type SeaweedfsNodeClusterStatus struct {
-	IsMaster        bool
-	ClusterNodesIPs []string
-}
-
-type SeaweedfsNodeRunningStatus struct {
-	IsExist            bool
-	IsRunning          bool
-	NeedUpdateManifest bool
-	NeedUpdateCerts    bool
-	NeedUpdateCaCerts  bool
-}
-
-type SeaweedfsCreateNodeRequest struct {
-	CreateManifestsData struct {
-		MasterPeers []string
-	}
-}
-
-type SeaweedfsUpdateNodeRequest struct {
-	UpdateCert          bool
-	UpdateCaCerts       bool
-	UpdateManifests     bool
-	UpdateManifestsData struct {
-		MasterPeers []string
-	}
-}
-
 func CmpSelectIsNeedUpdateCaCerts(status *SeaweedfsNodeRunningStatus) bool {
 	if status == nil {
 		return false
