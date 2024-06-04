6ac187bb12ea6a1f1aabdfe922f62539f27d54ab
Stepan Paksashvili <stepan.paksashvili@flant.com>
2024-06-04 15:59:51 +0300

    [deckhouse] add metallb and fix user-authn
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- modules/150-user-authn/hooks/ensure_rbacv2.go
+++ modules/150-user-authn/hooks/ensure_rbacv2.go
@@ -18,4 +18,5 @@ package hooks
 
 import "github.com/deckhouse/deckhouse/go_lib/hooks/ensure_rbacv2"
 
-var _ = ensure_rbacv2.RegisterHook("user-authn", []string{"security"}, nil)
+var _ = ensure_rbacv2.RegisterHook("user-authn", []string{"security"}, []string{
+	"/deckhouse/modules/010-user-authn-crd/crds/*.yaml"})
