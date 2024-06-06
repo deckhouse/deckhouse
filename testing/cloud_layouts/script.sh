764d3bfa103272e371447573fbed0e801ac7a80c
Denis Romanenko <denis.romanenko@flant.com>
2024-06-06 10:05:14 +0300

    ++
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- testing/cloud_layouts/script.sh
+++ testing/cloud_layouts/script.sh
@@ -411,7 +411,7 @@ function run-test() {
 
 function bootstrap_static() {
   >&2 echo "Run terraform to create nodes for Static cluster ..."
-  pushd $cwd
+  pushd "$cwd"
   terraform init -input=false -plugin-dir=/plugins || return $?
   terraform apply -state="${terraform_state_file}" -auto-approve -no-color | tee "$cwd/terraform.log" || return $?
   popd
