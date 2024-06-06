3d34d757891bb8fefc7b879a902dfc9e7261e79d
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-06-06 16:20:08 +0300

    fix
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- dhctl/pkg/preflight/preflight.go
+++ dhctl/pkg/preflight/preflight.go
@@ -82,11 +82,6 @@ func (pc *Checker) Static() error {
 			successMessage: "resolve the localhost domain",
 			skipFlag:       app.RegistryCredentialsCheckArgName,
 		},
-		{
-			fun:            pc.CheckMasterHostname,
-			successMessage: "master hostname unique",
-			skipFlag:       app.MasterHostnameCheckArgName,
-		},
 	})
 }
 
