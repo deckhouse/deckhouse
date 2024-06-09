b5f55fe34833978f5e16ce9f439ccb337098afee
Denis Romanenko <denis.romanenko@flant.com>
2024-06-09 22:19:17 +0300

    ++
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- testing/cloud_layouts/script.sh
+++ testing/cloud_layouts/script.sh
@@ -159,6 +159,9 @@ function destroy_static_infra() {
   >&2 echo "Run destroy_static_infra from ${terraform_state_file}"
 
   pushd "$cwd"
+  ls -la .
+  echo ${terraform_state_file}
+  cat ${terraform_state_file}
   terraform init -input=false -plugin-dir=/plugins || return $?
   terraform destroy -state="${terraform_state_file}" -input=false -auto-approve || exitCode=$?
   popd
