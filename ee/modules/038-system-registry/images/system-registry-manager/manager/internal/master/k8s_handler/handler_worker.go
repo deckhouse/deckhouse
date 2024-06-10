671c3ccfd73c30b00920c65d94bc49871ff0705b
vadimartynov <vadim.martynov@flant.com>
2024-06-10 11:37:34 +0300

    Removed k&s handlers, added k&s info

--- ee/modules/038-system-registry/images/system-registry-manager/manager/internal/master/k8s_handler/handler_worker.go
+++ ee/modules/038-system-registry/images/system-registry-manager/manager/internal/master/k8s_handler/handler_worker.go
@@ -5,150 +5,150 @@ Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https
 
 package k8shandler
 
-import (
-	"context"
-	"github.com/sirupsen/logrus"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/kubernetes"
-	"sync"
-)
+// import (
+// 	"context"
+// 	"github.com/sirupsen/logrus"
+// 	appsv1 "k8s.io/api/apps/v1"
+// 	corev1 "k8s.io/api/core/v1"
+// 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+// 	"k8s.io/apimachinery/pkg/runtime/schema"
+// 	"k8s.io/client-go/kubernetes"
+// 	"sync"
+// )
 
-type WorkerDaemonsetResource struct {
-	log           *logrus.Entry
-	data          *appsv1.DaemonSet
-	daemonsetName string
-	mu            sync.Mutex
-}
+// type WorkerDaemonsetResource struct {
+// 	log           *logrus.Entry
+// 	data          *appsv1.DaemonSet
+// 	daemonsetName string
+// 	mu            sync.Mutex
+// }
 
-func NewWorkerDaemonsetResource(log *logrus.Entry, daemonsetName string) *WorkerDaemonsetResource {
-	return &WorkerDaemonsetResource{
-		log:           log,
-		daemonsetName: daemonsetName,
-	}
-}
+// func NewWorkerDaemonsetResource(log *logrus.Entry, daemonsetName string) *WorkerDaemonsetResource {
+// 	return &WorkerDaemonsetResource{
+// 		log:           log,
+// 		daemonsetName: daemonsetName,
+// 	}
+// }
 
-func (r *WorkerDaemonsetResource) Filter(obj interface{}) bool {
-	if daemonSet, ok := obj.(*appsv1.DaemonSet); ok {
-		return daemonSet.Name == r.daemonsetName
-	}
-	return false
-}
+// func (r *WorkerDaemonsetResource) Filter(obj interface{}) bool {
+// 	if daemonSet, ok := obj.(*appsv1.DaemonSet); ok {
+// 		return daemonSet.Name == r.daemonsetName
+// 	}
+// 	return false
+// }
 
-func (r *WorkerDaemonsetResource) OnAdd(obj interface{}, isInInitialList bool) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	if daemonset, ok := obj.(*appsv1.DaemonSet); ok {
-		r.data = daemonset
-	} else {
-		r.log.Error("Pars daemonSet error")
-	}
-}
-func (r *WorkerDaemonsetResource) OnUpdate(oldObj, newObj interface{}) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	if daemonset, ok := newObj.(*appsv1.DaemonSet); ok {
-		r.data = daemonset
-	} else {
-		r.log.Error("Pars daemonSet error")
-	}
-}
-func (r *WorkerDaemonsetResource) OnDelete(obj interface{}) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	if _, ok := obj.(*appsv1.DaemonSet); ok {
-		r.data = nil
-	} else {
-		r.log.Error("Pars daemonSet error")
-	}
-}
+// func (r *WorkerDaemonsetResource) OnAdd(obj interface{}, isInInitialList bool) {
+// 	r.mu.Lock()
+// 	defer r.mu.Unlock()
+// 	if daemonset, ok := obj.(*appsv1.DaemonSet); ok {
+// 		r.data = daemonset
+// 	} else {
+// 		r.log.Error("Pars daemonSet error")
+// 	}
+// }
+// func (r *WorkerDaemonsetResource) OnUpdate(oldObj, newObj interface{}) {
+// 	r.mu.Lock()
+// 	defer r.mu.Unlock()
+// 	if daemonset, ok := newObj.(*appsv1.DaemonSet); ok {
+// 		r.data = daemonset
+// 	} else {
+// 		r.log.Error("Pars daemonSet error")
+// 	}
+// }
+// func (r *WorkerDaemonsetResource) OnDelete(obj interface{}) {
+// 	r.mu.Lock()
+// 	defer r.mu.Unlock()
+// 	if _, ok := obj.(*appsv1.DaemonSet); ok {
+// 		r.data = nil
+// 	} else {
+// 		r.log.Error("Pars daemonSet error")
+// 	}
+// }
 
-func (r *WorkerDaemonsetResource) GetGroupVersionResourse() schema.GroupVersionResource {
-	return schema.GroupVersionResource{
-		Group:    "apps",
-		Version:  "v1",
-		Resource: "daemonsets",
-	}
-}
+// func (r *WorkerDaemonsetResource) GetGroupVersionResourse() schema.GroupVersionResource {
+// 	return schema.GroupVersionResource{
+// 		Group:    "apps",
+// 		Version:  "v1",
+// 		Resource: "daemonsets",
+// 	}
+// }
 
-func (r *WorkerDaemonsetResource) GetData() *appsv1.DaemonSet {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	return r.data
-}
+// func (r *WorkerDaemonsetResource) GetData() *appsv1.DaemonSet {
+// 	r.mu.Lock()
+// 	defer r.mu.Unlock()
+// 	return r.data
+// }
 
-type WorkerEndpointResource struct {
-	log          *logrus.Entry
-	data         *corev1.Endpoints
-	endpointName string
-	mu           sync.Mutex
-}
+// type WorkerEndpointResource struct {
+// 	log          *logrus.Entry
+// 	data         *corev1.Endpoints
+// 	endpointName string
+// 	mu           sync.Mutex
+// }
 
-func NewWorkerEndpointResource(log *logrus.Entry, endpointName string) *WorkerEndpointResource {
-	return &WorkerEndpointResource{
-		log:          log,
-		endpointName: endpointName,
-	}
-}
+// func NewWorkerEndpointResource(log *logrus.Entry, endpointName string) *WorkerEndpointResource {
+// 	return &WorkerEndpointResource{
+// 		log:          log,
+// 		endpointName: endpointName,
+// 	}
+// }
 
-func (r *WorkerEndpointResource) Filter(obj interface{}) bool {
-	if endpoint, ok := obj.(*corev1.Endpoints); ok {
-		return endpoint.Name == r.endpointName
-	}
-	return false
-}
+// func (r *WorkerEndpointResource) Filter(obj interface{}) bool {
+// 	if endpoint, ok := obj.(*corev1.Endpoints); ok {
+// 		return endpoint.Name == r.endpointName
+// 	}
+// 	return false
+// }
 
-func (r *WorkerEndpointResource) OnAdd(obj interface{}, isInInitialList bool) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	if endpoints, ok := obj.(*corev1.Endpoints); ok {
-		r.data = endpoints
-	} else {
-		r.log.Error("Pars endpoints error")
-	}
-}
-func (r *WorkerEndpointResource) OnUpdate(oldObj, newObj interface{}) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	if endpoints, ok := newObj.(*corev1.Endpoints); ok {
-		r.data = endpoints
-	} else {
-		r.log.Error("Pars endpoints error")
-	}
-}
-func (r *WorkerEndpointResource) OnDelete(obj interface{}) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	if _, ok := obj.(*corev1.Endpoints); ok {
-		r.data = nil
-	} else {
-		r.log.Error("Pars endpoints error")
-	}
-}
+// func (r *WorkerEndpointResource) OnAdd(obj interface{}, isInInitialList bool) {
+// 	r.mu.Lock()
+// 	defer r.mu.Unlock()
+// 	if endpoints, ok := obj.(*corev1.Endpoints); ok {
+// 		r.data = endpoints
+// 	} else {
+// 		r.log.Error("Pars endpoints error")
+// 	}
+// }
+// func (r *WorkerEndpointResource) OnUpdate(oldObj, newObj interface{}) {
+// 	r.mu.Lock()
+// 	defer r.mu.Unlock()
+// 	if endpoints, ok := newObj.(*corev1.Endpoints); ok {
+// 		r.data = endpoints
+// 	} else {
+// 		r.log.Error("Pars endpoints error")
+// 	}
+// }
+// func (r *WorkerEndpointResource) OnDelete(obj interface{}) {
+// 	r.mu.Lock()
+// 	defer r.mu.Unlock()
+// 	if _, ok := obj.(*corev1.Endpoints); ok {
+// 		r.data = nil
+// 	} else {
+// 		r.log.Error("Pars endpoints error")
+// 	}
+// }
 
-func (r *WorkerEndpointResource) Update(clientSet *kubernetes.Clientset, namespace string) {
-	r.log.Info("Update WorkerEndpointResource")
-	data, err := clientSet.CoreV1().Endpoints(namespace).Get(context.TODO(), r.endpointName, metav1.GetOptions{})
-	if err != nil {
-		r.log.Errorf("error getting Endpoint: %s", err)
-	}
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	r.data = data
-}
+// func (r *WorkerEndpointResource) Update(clientSet *kubernetes.Clientset, namespace string) {
+// 	r.log.Info("Update WorkerEndpointResource")
+// 	data, err := clientSet.CoreV1().Endpoints(namespace).Get(context.TODO(), r.endpointName, metav1.GetOptions{})
+// 	if err != nil {
+// 		r.log.Errorf("error getting Endpoint: %s", err)
+// 	}
+// 	r.mu.Lock()
+// 	defer r.mu.Unlock()
+// 	r.data = data
+// }
 
-func (r *WorkerEndpointResource) GetGroupVersionResourse() schema.GroupVersionResource {
-	return schema.GroupVersionResource{
-		Group:    "",
-		Version:  "v1",
-		Resource: "endpoints",
-	}
-}
+// func (r *WorkerEndpointResource) GetGroupVersionResourse() schema.GroupVersionResource {
+// 	return schema.GroupVersionResource{
+// 		Group:    "",
+// 		Version:  "v1",
+// 		Resource: "endpoints",
+// 	}
+// }
 
-func (r *WorkerEndpointResource) GetData() *corev1.Endpoints {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	return r.data
-}
+// func (r *WorkerEndpointResource) GetData() *corev1.Endpoints {
+// 	r.mu.Lock()
+// 	defer r.mu.Unlock()
+// 	return r.data
+// }
--- ee/modules/038-system-registry/images/system-registry-manager/manager/internal/master/k8s_handler/root.go
+++ ee/modules/038-system-registry/images/system-registry-manager/manager/internal/master/k8s_handler/root.go
@@ -5,201 +5,201 @@ Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https
 
 package k8shandler
 
-import (
-	"context"
-	"fmt"
-	pkg_cfg "system-registry-manager/pkg/cfg"
-	"time"
+// import (
+// 	"context"
+// 	"fmt"
+// 	pkg_cfg "system-registry-manager/pkg/cfg"
+// 	"time"
 
-	"github.com/sirupsen/logrus"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	pkg_logs "system-registry-manager/pkg/logs"
-)
+// 	"github.com/sirupsen/logrus"
+// 	appsv1 "k8s.io/api/apps/v1"
+// 	corev1 "k8s.io/api/core/v1"
+// 	pkg_logs "system-registry-manager/pkg/logs"
+// )
 
-type CommonHandler struct {
-	ctx                     context.Context
-	log                     *logrus.Entry
-	MasterNodesResource     *MasterNodesResource
-	SeaweedfsPodsResource   *SeaweedfsPodsResource
-	WorkerDaemonsetResource *WorkerDaemonsetResource
-	WorkerEndpointResource  *WorkerEndpointResource
+// type CommonHandler struct {
+// 	ctx                     context.Context
+// 	log                     *logrus.Entry
+// 	MasterNodesResource     *MasterNodesResource
+// 	SeaweedfsPodsResource   *SeaweedfsPodsResource
+// 	WorkerDaemonsetResource *WorkerDaemonsetResource
+// 	WorkerEndpointResource  *WorkerEndpointResource
 
-	KubernetesResourcesHandler *KubernetesResourcesHandler
-}
+// 	KubernetesResourcesHandler *KubernetesResourcesHandler
+// }
 
-func NewCommonHandler(ctx context.Context) (*CommonHandler, error) {
-	log := pkg_logs.GetLoggerFromContext(ctx)
+// func NewCommonHandler(ctx context.Context) (*CommonHandler, error) {
+// 	log := pkg_logs.GetLoggerFromContext(ctx)
 
-	cfg := pkg_cfg.GetConfig()
+// 	cfg := pkg_cfg.GetConfig()
 
-	commonHandler := CommonHandler{ctx: ctx, log: log}
+// 	commonHandler := CommonHandler{ctx: ctx, log: log}
 
-	commonHandler.KubernetesResourcesHandler = NewKubernetesResourcesHandler(
-		ctx,
-		cfg.K8sClient,
-		time.Duration(0)*time.Minute,
-		cfg.Manager.Namespace,
-	)
+// 	commonHandler.KubernetesResourcesHandler = NewKubernetesResourcesHandler(
+// 		ctx,
+// 		cfg.K8sClient,
+// 		time.Duration(0)*time.Minute,
+// 		cfg.Manager.Namespace,
+// 	)
 
-	var err error
-	commonHandler.MasterNodesResource, err = NewMasterNodesResource(log)
-	if err != nil {
-		return nil, err
-	}
-	commonHandler.SeaweedfsPodsResource, err = NewSeaweedfsPodsResource(log, pkg_cfg.SeaweedfsStaticPodLabelsSelector)
-	if err != nil {
-		return nil, err
-	}
-	commonHandler.WorkerDaemonsetResource = NewWorkerDaemonsetResource(log, cfg.Manager.DaemonsetName)
-	commonHandler.WorkerEndpointResource = NewWorkerEndpointResource(log, cfg.Manager.ServiceName)
+// 	var err error
+// 	commonHandler.MasterNodesResource, err = NewMasterNodesResource(log)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	commonHandler.SeaweedfsPodsResource, err = NewSeaweedfsPodsResource(log, pkg_cfg.SeaweedfsStaticPodLabelsSelector)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	commonHandler.WorkerDaemonsetResource = NewWorkerDaemonsetResource(log, cfg.Manager.DaemonsetName)
+// 	commonHandler.WorkerEndpointResource = NewWorkerEndpointResource(log, cfg.Manager.ServiceName)
 
-	commonHandler.KubernetesResourcesHandler.Subscribe(commonHandler.MasterNodesResource)
-	commonHandler.KubernetesResourcesHandler.Subscribe(commonHandler.SeaweedfsPodsResource)
-	commonHandler.KubernetesResourcesHandler.Subscribe(commonHandler.WorkerDaemonsetResource)
-	commonHandler.KubernetesResourcesHandler.Subscribe(commonHandler.WorkerEndpointResource)
+// 	commonHandler.KubernetesResourcesHandler.Subscribe(commonHandler.MasterNodesResource)
+// 	commonHandler.KubernetesResourcesHandler.Subscribe(commonHandler.SeaweedfsPodsResource)
+// 	commonHandler.KubernetesResourcesHandler.Subscribe(commonHandler.WorkerDaemonsetResource)
+// 	commonHandler.KubernetesResourcesHandler.Subscribe(commonHandler.WorkerEndpointResource)
 
-	return &commonHandler, nil
-}
+// 	return &commonHandler, nil
+// }
 
-func (c *CommonHandler) Start() {
-	c.KubernetesResourcesHandler.Start()
-}
+// func (c *CommonHandler) Start() {
+// 	c.KubernetesResourcesHandler.Start()
+// }
 
-func (c *CommonHandler) Stop() {
-	c.KubernetesResourcesHandler.Stop()
-}
+// func (c *CommonHandler) Stop() {
+// 	c.KubernetesResourcesHandler.Stop()
+// }
 
-func (c *CommonHandler) GetMasterNodeNameList() []string {
-	data := c.MasterNodesResource.GetData()
-	if data == nil {
-		return nil
-	}
+// func (c *CommonHandler) GetMasterNodeNameList() []string {
+// 	data := c.MasterNodesResource.GetData()
+// 	if data == nil {
+// 		return nil
+// 	}
 
-	nodes := make([]string, 0, len(data))
-	for nodeName := range data {
-		nodes = append(nodes, nodeName)
-	}
-	return nodes
-}
+// 	nodes := make([]string, 0, len(data))
+// 	for nodeName := range data {
+// 		nodes = append(nodes, nodeName)
+// 	}
+// 	return nodes
+// }
 
-func (c *CommonHandler) GetWorkerEndpointByNodeName(nodeNmae string) *corev1.EndpointAddress {
-	endpointsData := c.WorkerEndpointResource.GetData()
+// func (c *CommonHandler) GetWorkerEndpointByNodeName(nodeNmae string) *corev1.EndpointAddress {
+// 	endpointsData := c.WorkerEndpointResource.GetData()
 
-	if endpointsData == nil {
-		return nil
-	}
-	for _, subsets := range endpointsData.Subsets {
-		for _, address := range subsets.Addresses {
-			if address.NodeName != nil && *address.NodeName == nodeNmae {
-				return &address
-			}
-		}
-	}
-	return nil
-}
+// 	if endpointsData == nil {
+// 		return nil
+// 	}
+// 	for _, subsets := range endpointsData.Subsets {
+// 		for _, address := range subsets.Addresses {
+// 			if address.NodeName != nil && *address.NodeName == nodeNmae {
+// 				return &address
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
 
-func (c *CommonHandler) GetSeaweedfsPodByNodeName(nodeNmae string) *corev1.Pod {
-	seaweedfsPods := c.SeaweedfsPodsResource.GetData()
-	if seaweedfsPods == nil {
-		return nil
-	}
-	for _, pod := range seaweedfsPods {
-		if pod.Spec.NodeName == nodeNmae {
-			return &pod
-		}
-	}
-	return nil
-}
+// func (c *CommonHandler) GetSeaweedfsPodByNodeName(nodeNmae string) *corev1.Pod {
+// 	seaweedfsPods := c.SeaweedfsPodsResource.GetData()
+// 	if seaweedfsPods == nil {
+// 		return nil
+// 	}
+// 	for _, pod := range seaweedfsPods {
+// 		if pod.Spec.NodeName == nodeNmae {
+// 			return &pod
+// 		}
+// 	}
+// 	return nil
+// }
 
-func (c *CommonHandler) GetMasterNodeByNodeName(nodeNmae string) *corev1.Node {
-	masterNodedata := c.MasterNodesResource.GetData()
-	if masterNodedata == nil {
-		return nil
-	}
-	if masterNode, ok := masterNodedata[nodeNmae]; ok {
-		return &masterNode
-	}
-	return nil
-}
+// func (c *CommonHandler) GetMasterNodeByNodeName(nodeNmae string) *corev1.Node {
+// 	masterNodedata := c.MasterNodesResource.GetData()
+// 	if masterNodedata == nil {
+// 		return nil
+// 	}
+// 	if masterNode, ok := masterNodedata[nodeNmae]; ok {
+// 		return &masterNode
+// 	}
+// 	return nil
+// }
 
-func (c *CommonHandler) WaitWorkerDaemonset() (*appsv1.DaemonSet, error) {
-	for i := 0; i < pkg_cfg.MaxRetries; i++ {
-		defer time.Sleep(1 * time.Second)
+// func (c *CommonHandler) WaitWorkerDaemonset() (*appsv1.DaemonSet, error) {
+// 	for i := 0; i < pkg_cfg.MaxRetries; i++ {
+// 		defer time.Sleep(1 * time.Second)
 
-		dsInfo := c.WorkerDaemonsetResource.GetData()
-		if dsInfo == nil {
-			continue
-		}
-		if dsInfo.Status.DesiredNumberScheduled == dsInfo.Status.NumberAvailable {
-			return dsInfo, nil
-		}
-	}
-	return nil, fmt.Errorf("error WaitDaemonsetPods")
-}
+// 		dsInfo := c.WorkerDaemonsetResource.GetData()
+// 		if dsInfo == nil {
+// 			continue
+// 		}
+// 		if dsInfo.Status.DesiredNumberScheduled == dsInfo.Status.NumberAvailable {
+// 			return dsInfo, nil
+// 		}
+// 	}
+// 	return nil, fmt.Errorf("error WaitDaemonsetPods")
+// }
 
-func (c *CommonHandler) WaitWorkerEndpoints() (*corev1.Endpoints, error) {
-	for i := 0; i < pkg_cfg.MaxRetries; i++ {
-		defer time.Sleep(1 * time.Second)
+// func (c *CommonHandler) WaitWorkerEndpoints() (*corev1.Endpoints, error) {
+// 	for i := 0; i < pkg_cfg.MaxRetries; i++ {
+// 		defer time.Sleep(1 * time.Second)
 
-		ep := c.WorkerEndpointResource.GetData()
+// 		ep := c.WorkerEndpointResource.GetData()
 
-		if ep == nil {
-			continue
-		}
+// 		if ep == nil {
+// 			continue
+// 		}
 
-		for _, subset := range ep.Subsets {
-			if len(subset.NotReadyAddresses) == 0 {
-				return ep, nil
-			}
-		}
-	}
-	return nil, fmt.Errorf("error WaitWorkerEndpoints")
-}
+// 		for _, subset := range ep.Subsets {
+// 			if len(subset.NotReadyAddresses) == 0 {
+// 				return ep, nil
+// 			}
+// 		}
+// 	}
+// 	return nil, fmt.Errorf("error WaitWorkerEndpoints")
+// }
 
-func (c *CommonHandler) WaitAllWorkers() (int, error) {
-	dsInfo, err := c.WaitWorkerDaemonset()
-	if err != nil {
-		return 0, err
-	}
+// func (c *CommonHandler) WaitAllWorkers() (int, error) {
+// 	dsInfo, err := c.WaitWorkerDaemonset()
+// 	if err != nil {
+// 		return 0, err
+// 	}
 
-	ep, err := c.WaitWorkerEndpoints()
-	if err != nil {
-		return 0, err
-	}
+// 	ep, err := c.WaitWorkerEndpoints()
+// 	if err != nil {
+// 		return 0, err
+// 	}
 
-	numberOfNode := dsInfo.Status.DesiredNumberScheduled
+// 	numberOfNode := dsInfo.Status.DesiredNumberScheduled
 
-	if len(ep.Subsets) == 0 {
-		return 0, fmt.Errorf("error len(ep.Subsets) == 0")
-	}
-	if len(ep.Subsets[0].Addresses) != int(numberOfNode) {
-		return 0, fmt.Errorf("error len(ep.Subsets[0].Addresses) != numberOfNode")
-	}
+// 	if len(ep.Subsets) == 0 {
+// 		return 0, fmt.Errorf("error len(ep.Subsets) == 0")
+// 	}
+// 	if len(ep.Subsets[0].Addresses) != int(numberOfNode) {
+// 		return 0, fmt.Errorf("error len(ep.Subsets[0].Addresses) != numberOfNode")
+// 	}
 
-	return int(numberOfNode), nil
-}
+// 	return int(numberOfNode), nil
+// }
 
-func (c *CommonHandler) GetAllDataByNodeName(nodeNmae string) *MergeInfo {
-	masterNode := c.GetMasterNodeByNodeName(nodeNmae)
-	if masterNode == nil {
-		return nil
-	}
-	workerEndpoint := c.GetWorkerEndpointByNodeName(nodeNmae)
-	if workerEndpoint == nil {
-		return nil
-	}
-	seaweedfsPod := c.GetSeaweedfsPodByNodeName(nodeNmae)
+// func (c *CommonHandler) GetAllDataByNodeName(nodeNmae string) *MergeInfo {
+// 	masterNode := c.GetMasterNodeByNodeName(nodeNmae)
+// 	if masterNode == nil {
+// 		return nil
+// 	}
+// 	workerEndpoint := c.GetWorkerEndpointByNodeName(nodeNmae)
+// 	if workerEndpoint == nil {
+// 		return nil
+// 	}
+// 	seaweedfsPod := c.GetSeaweedfsPodByNodeName(nodeNmae)
 
-	return &MergeInfo{
-		MasterNode:   *masterNode,
-		Worker:       *workerEndpoint,
-		SeaweedfsPod: seaweedfsPod,
-	}
-}
+// 	return &MergeInfo{
+// 		MasterNode:   *masterNode,
+// 		Worker:       *workerEndpoint,
+// 		SeaweedfsPod: seaweedfsPod,
+// 	}
+// }
 
-type MergeInfo struct {
-	MasterNode   corev1.Node
-	Worker       corev1.EndpointAddress
-	SeaweedfsPod *corev1.Pod
-}
+// type MergeInfo struct {
+// 	MasterNode   corev1.Node
+// 	Worker       corev1.EndpointAddress
+// 	SeaweedfsPod *corev1.Pod
+// }
