6ad22dac2560cc5c36d20afdf37cfea03acdfd1e
vadimartynov <vadim.martynov@flant.com>
2024-06-17 23:39:03 +0300

    Added hook for init secret overrides

---
+++ ee/modules/038-system-registry/hooks/internal/resources/v1/module_config_settings.go
@@ -0,0 +1,206 @@
+/*
+Copyright 2024 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package resources
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/apis/deckhouse.io/v1alpha1"
+	"github.com/flant/addon-operator/sdk"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/utils/pointer"
+	"strings"
+)
+
+// Init secret
+type InitSecretData struct {
+	Data *struct {
+		RegistryMode            *[]byte `json:"registryMode,omitempty" yaml:"registryMode,omitempty"`
+		UpstreamRegistryAddress *[]byte `json:"upstreamRegistryAddress,omitempty" yaml:"upstreamRegistryAddress,omitempty"`
+		UpstreamRegistryAuth    *[]byte `json:"upstreamRegistryAuth,omitempty" yaml:"upstreamRegistryAuth,omitempty"`
+		UpstreamRegistryCA      *[]byte `json:"upstreamRegistryCA,omitempty" yaml:"upstreamRegistryCA,omitempty"`
+		UpstreamRegistryPath    *[]byte `json:"upstreamRegistryPath,omitempty" yaml:"upstreamRegistryPath,omitempty"`
+		UpstreamRegistryScheme  *[]byte `json:"upstreamRegistryScheme,omitempty" yaml:"upstreamRegistryScheme,omitempty"`
+	} `json:"data,omitempty" yaml:"data,omitempty"`
+}
+
+// Module Config Settings
+type ModuleConfigSettings struct {
+	RegistryMode     *string                               `json:"registryMode,omitempty" yaml:"registryMode,omitempty"`
+	UpstreamRegistry *ModuleConfigSettingsUpstreamRegistry `json:"upstreamRegistry,omitempty" yaml:"upstreamRegistry,omitempty"`
+}
+
+type ModuleConfigSettingsUpstreamRegistry struct {
+	UpstreamRegistryHost     *string `json:"upstreamRegistryHost,omitempty" yaml:"upstreamRegistryHost,omitempty"`
+	UpstreamRegistryScheme   *string `json:"upstreamRegistryScheme,omitempty" yaml:"upstreamRegistryScheme,omitempty"`
+	UpstreamRegistryCa       *string `json:"upstreamRegistryCa,omitempty" yaml:"upstreamRegistryCa,omitempty"`
+	UpstreamRegistryPath     *string `json:"upstreamRegistryPath,omitempty" yaml:"upstreamRegistryPath,omitempty"`
+	UpstreamRegistryUser     *string `json:"upstreamRegistryUser,omitempty" yaml:"upstreamRegistryUser,omitempty"`
+	UpstreamRegistryPassword *string `json:"upstreamRegistryPassword,omitempty" yaml:"upstreamRegistryPassword,omitempty"`
+}
+
+// Module Config funcs
+func NewModuleConfigByInitSecret(secretData *InitSecretData) (*v1alpha1.ModuleConfig, error) {
+	settings, err := fromInitSecretDataToModuleConfigSettings(secretData)
+	if err != nil {
+		return nil, err
+	}
+	mapStructSettings, err := FromModuleConfigSettingsToUnstructured(settings)
+	if err != nil {
+		return nil, err
+	}
+
+	newModuleConfig := v1alpha1.ModuleConfig{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ModuleConfig",
+			APIVersion: "deckhouse.io/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "system-registry",
+		},
+		Spec: v1alpha1.ModuleConfigSpec{
+			Version:  1,
+			Settings: mapStructSettings,
+			Enabled:  pointer.Bool(true),
+		},
+	}
+	return &newModuleConfig, nil
+}
+
+func fromInitSecretDataToModuleConfigSettings(initData *InitSecretData) (*ModuleConfigSettings, error) {
+	if initData == nil || initData.Data == nil {
+		return nil, fmt.Errorf("initData or initData.Data is nil")
+	}
+
+	decodeBase64 := func(data *[]byte) (*[]byte, error) {
+		if data == nil {
+			return nil, nil
+		}
+		if len(*data) == 0 {
+			return nil, nil
+		}
+		decoded, err := base64.StdEncoding.DecodeString(string(*data))
+		if err != nil {
+			return nil, err
+		}
+		return &decoded, nil
+	}
+
+	toString := func(data *[]byte) *string {
+		if data == nil {
+			return nil
+		}
+		strData := string(*data)
+		strData = strings.TrimSpace(strData)
+		return &strData
+	}
+
+	var upstreamRegistryUser, upstreamRegistryPassword *string
+
+	if initData.Data.UpstreamRegistryAuth != nil {
+		// Double decode
+		authData, err := decodeBase64(initData.Data.UpstreamRegistryAuth)
+		if err != nil {
+			return nil, err
+		}
+
+		authStr := toString(authData)
+		if authStr != nil {
+			parts := strings.Split(*authStr, ":")
+			if len(parts) == 2 {
+				upstreamRegistryUser = &parts[0]
+				upstreamRegistryPassword = &parts[1]
+			}
+		}
+	}
+
+	return &ModuleConfigSettings{
+		RegistryMode: toString(initData.Data.RegistryMode),
+		UpstreamRegistry: &ModuleConfigSettingsUpstreamRegistry{
+			UpstreamRegistryHost:     toString(initData.Data.UpstreamRegistryAddress),
+			UpstreamRegistryScheme:   toString(initData.Data.UpstreamRegistryScheme),
+			UpstreamRegistryCa:       toString(initData.Data.UpstreamRegistryCA),
+			UpstreamRegistryPath:     toString(initData.Data.UpstreamRegistryPath),
+			UpstreamRegistryUser:     upstreamRegistryUser,
+			UpstreamRegistryPassword: upstreamRegistryPassword,
+		},
+	}, nil
+}
+
+func FromModuleConfigSettingsToUnstructured(mcSettings *ModuleConfigSettings) (map[string]interface{}, error) {
+	if mcSettings == nil {
+		return nil, fmt.Errorf("mcSettings is nil")
+	}
+	unstruct, err := sdk.ToUnstructured(mcSettings)
+	if err != nil {
+		return nil, err
+	}
+	if unstruct == nil {
+		return map[string]interface{}{}, nil
+	}
+	return unstruct.Object, nil
+}
+
+func FromUnstructuredToModuleConfigSettings(mcSettings map[string]interface{}) (*ModuleConfigSettings, error) {
+	if mcSettings == nil {
+		return nil, fmt.Errorf("mcSettings is nil")
+	}
+	var mcSettingsStruct ModuleConfigSettings
+	err := sdk.FromUnstructured(&unstructured.Unstructured{Object: mcSettings}, &mcSettingsStruct)
+	return &mcSettingsStruct, err
+}
+
+func PrepareModuleConfigByInitSettings(moduleConfig *v1alpha1.ModuleConfig, secretData *InitSecretData) error {
+	if moduleConfig == nil {
+		return fmt.Errorf("moduleConfig is nil")
+	}
+	if secretData == nil {
+		return fmt.Errorf("secretData is nil")
+	}
+	settingsData, err := fromInitSecretDataToModuleConfigSettings(secretData)
+	if err != nil {
+		return err
+	}
+	moduleConfigData, err := FromUnstructuredToModuleConfigSettings(moduleConfig.Spec.Settings)
+	if err != nil {
+		return err
+	}
+
+	if moduleConfigData.RegistryMode == nil {
+		moduleConfigData.RegistryMode = settingsData.RegistryMode
+	}
+	if moduleConfigData.UpstreamRegistry == nil {
+		moduleConfigData.UpstreamRegistry = settingsData.UpstreamRegistry
+	} else {
+		if moduleConfigData.UpstreamRegistry.UpstreamRegistryHost == nil {
+			moduleConfigData.UpstreamRegistry.UpstreamRegistryHost = settingsData.UpstreamRegistry.UpstreamRegistryHost
+		}
+		if moduleConfigData.UpstreamRegistry.UpstreamRegistryScheme == nil {
+			moduleConfigData.UpstreamRegistry.UpstreamRegistryScheme = settingsData.UpstreamRegistry.UpstreamRegistryScheme
+		}
+		if moduleConfigData.UpstreamRegistry.UpstreamRegistryCa == nil {
+			moduleConfigData.UpstreamRegistry.UpstreamRegistryCa = settingsData.UpstreamRegistry.UpstreamRegistryCa
+		}
+		if moduleConfigData.UpstreamRegistry.UpstreamRegistryPath == nil {
+			moduleConfigData.UpstreamRegistry.UpstreamRegistryPath = settingsData.UpstreamRegistry.UpstreamRegistryPath
+		}
+		if moduleConfigData.UpstreamRegistry.UpstreamRegistryUser == nil {
+			moduleConfigData.UpstreamRegistry.UpstreamRegistryUser = settingsData.UpstreamRegistry.UpstreamRegistryUser
+		}
+		if moduleConfigData.UpstreamRegistry.UpstreamRegistryPassword == nil {
+			moduleConfigData.UpstreamRegistry.UpstreamRegistryPassword = settingsData.UpstreamRegistry.UpstreamRegistryPassword
+		}
+	}
+	newModuleConfigData, err := FromModuleConfigSettingsToUnstructured(moduleConfigData)
+	if err != nil {
+		return err
+	}
+
+	moduleConfig.Spec.Settings = newModuleConfigData
+	return nil
+}
---
+++ ee/modules/038-system-registry/hooks/set_init_configuration.go
@@ -0,0 +1,170 @@
+/*
+Copyright 2024 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package hooks
+
+import (
+	"fmt"
+
+	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/apis/deckhouse.io/v1alpha1"
+	resources_v1 "github.com/deckhouse/deckhouse/ee/modules/038-system-registry/hooks/internal/resources/v1"
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube/object_patch"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"reflect"
+	// "k8s.io/utils/pointer"
+)
+
+const (
+	initSecretName          = "system-registry-init-configuration"
+	initSecretNamespace     = "d8-system"
+	initSecretSnapshotsName = "init_secret_config"
+
+	registryModuleConfigName          = "system-registry"
+	registryModuleConfigSnapshotsName = "registry_module_config"
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Queue:        "/modules/system-registry/bootstrap_init_cfg",
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 10},
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       initSecretSnapshotsName,
+			ApiVersion: "v1",
+			Kind:       "Secret",
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{initSecretName},
+			},
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{initSecretNamespace},
+				},
+			},
+
+			FilterFunc: applyInitSecretDataFilter,
+		},
+		{
+			Name:       registryModuleConfigSnapshotsName,
+			ApiVersion: "deckhouse.io/v1alpha1",
+			Kind:       "ModuleConfig",
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{registryModuleConfigName},
+			},
+			// ExecuteHookOnEvents:          pointer.Bool(false),
+			// ExecuteHookOnSynchronization: pointer.Bool(false),
+			FilterFunc: applyMCSettingsFilter,
+		},
+	},
+}, setInitConfigurationToMC)
+
+func applyMCSettingsFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	if obj == nil {
+		return nil, nil
+	}
+
+	var moduleConfig v1alpha1.ModuleConfig
+	err := sdk.FromUnstructured(obj, &moduleConfig)
+	if err != nil {
+		return nil, err
+	}
+	return moduleConfig, nil
+}
+
+func applyInitSecretDataFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	if obj == nil {
+		return nil, nil
+	}
+
+	var initSecretData resources_v1.InitSecretData
+	err := sdk.FromUnstructured(obj, &initSecretData)
+	if err != nil {
+		return nil, err
+	}
+	return initSecretData, nil
+}
+
+func setInitConfigurationToMC(input *go_hook.HookInput) error {
+	initSecretData, err := getInitSecretDataFromSnapshots(input)
+	if err != nil {
+		return err
+	}
+	moduleConfig, err := getMCSettingsFromSnapshots(input)
+	if err != nil {
+		return err
+	}
+
+	// If secret not exist or empty - nothing to do
+	if initSecretData == nil || initSecretData.Data == nil {
+		return nil
+	}
+
+	// If secret exist - create/update Module Config
+	if moduleConfig != nil {
+		err = updateMCWithInitSecretData(input, initSecretData, moduleConfig)
+	} else {
+		err = createMCWithInitSecretData(input, initSecretData)
+	}
+	if err != nil {
+		return err
+	}
+
+	// Delete secret after
+	deleteInitSecretData(input)
+	return nil
+}
+
+func getInitSecretDataFromSnapshots(input *go_hook.HookInput) (*resources_v1.InitSecretData, error) {
+	snap := input.Snapshots[initSecretSnapshotsName]
+	if len(snap) == 0 {
+		return nil, nil
+	}
+	if snap[0] == nil {
+		return nil, nil
+	}
+	secretData, ok := snap[0].(resources_v1.InitSecretData)
+	if !ok {
+		return nil, fmt.Errorf("error converting secret '%s' to structure '%s'", initSecretName, reflect.TypeOf(secretData).Name())
+	}
+	return &secretData, nil
+}
+
+func getMCSettingsFromSnapshots(input *go_hook.HookInput) (*v1alpha1.ModuleConfig, error) {
+	snap := input.Snapshots[registryModuleConfigSnapshotsName]
+	if len(snap) == 0 {
+		return nil, nil
+	}
+	if snap[0] == nil {
+		return nil, nil
+	}
+	mcSettings, ok := snap[0].(v1alpha1.ModuleConfig)
+	if !ok {
+		return nil, fmt.Errorf("error converting module config '%s' to structure '%s'", initSecretName, reflect.TypeOf(mcSettings).Name())
+	}
+	return &mcSettings, nil
+}
+
+func createMCWithInitSecretData(input *go_hook.HookInput, initSecretData *resources_v1.InitSecretData) error {
+	newModuleConfig, err := resources_v1.NewModuleConfigByInitSecret(initSecretData)
+	if err != nil {
+		return err
+	}
+	input.PatchCollector.Create(newModuleConfig, object_patch.UpdateIfExists())
+	return nil
+}
+
+func updateMCWithInitSecretData(input *go_hook.HookInput, initSecretData *resources_v1.InitSecretData, moduleConfig *v1alpha1.ModuleConfig) error {
+	err := resources_v1.PrepareModuleConfigByInitSettings(moduleConfig, initSecretData)
+	if err != nil {
+		return err
+	}
+	input.PatchCollector.Create(moduleConfig, object_patch.UpdateIfExists())
+	return nil
+}
+
+func deleteInitSecretData(input *go_hook.HookInput) {
+	input.PatchCollector.Delete("v1", "Secret", initSecretNamespace, initSecretName, object_patch.InBackground())
+}
