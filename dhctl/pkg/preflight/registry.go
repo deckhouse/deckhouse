841ddfb413077c0f717c55845b66b414b27c7b72
Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>
2024-07-08 11:46:38 +0300

    [dhctl] Fix registry preflight check for anonymous docker registries (#8940)
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- dhctl/pkg/preflight/registry.go
+++ dhctl/pkg/preflight/registry.go
@@ -244,13 +244,6 @@ func (pc *Checker) CheckRegistryCredentials() error {
 		return err
 	}
 
-	if authData == "" {
-		return fmt.Errorf(
-			"%w, credentials are not specified. If you are using CE edition in a closed environment, this check can be skipped by specifying the --preflight-skip-registry-credential flag",
-			ErrAuthRegistryFailed,
-		)
-	}
-
 	return checkRegistryAuth(ctx, pc.metaConfig, authData)
 }
 
