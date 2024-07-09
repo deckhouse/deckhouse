66c8d781f0e6f96449d61dd197e907f64d9d3dfd
vadimartynov <vadim.martynov@flant.com>
2024-07-09 17:42:57 +0400

    FIxed Copyright

--- ee/modules/038-system-registry/images/system-registry-manager/help_scripts/ansible-delete-registry.sh
+++ ee/modules/038-system-registry/images/system-registry-manager/help_scripts/ansible-delete-registry.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Copyright 2024 Flant JSC
+# Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
 # Function to find and switch to the directory where the script is located
 cd_script_dir() {
--- ee/modules/038-system-registry/images/system-registry-manager/help_scripts/manager-docker-build-and-apply.sh
+++ ee/modules/038-system-registry/images/system-registry-manager/help_scripts/manager-docker-build-and-apply.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+#
+# Copyright 2024 Flant JSC
+# Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
 set -e
 
