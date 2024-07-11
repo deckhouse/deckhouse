32415e022f4ef025d40a4e888444f816c518ed2a
Alexander Zverev <beastlex@gmail.com>
2024-07-11 17:03:40 +0700

    fix skipCheckWildcards
    Signed-off-by: Alexander Zverev <beastlex@gmail.com>

--- testing/matrix/linter/rules/roles/wildcards.go
+++ testing/matrix/linter/rules/roles/wildcards.go
@@ -70,9 +70,6 @@ var skipCheckWildcards = map[string][]string{
 	"ingress-nginx/templates/kruise/rbac-for-us.yaml": {
 		"d8:ingress-nginx:kruise-role",
 	},
-	"cilium-hubble/templates/ui/rbac-for-us.yaml": {
-		"d8:cilium-hubble:ui:reader",
-	},
 	"okmeter/templates/rbac-for-us.yaml": {
 		"d8:okmeter",
 	},
