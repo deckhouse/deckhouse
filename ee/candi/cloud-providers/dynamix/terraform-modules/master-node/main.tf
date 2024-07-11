c27892cfff122377c9459e260ab43bead6751a73
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-07-11 15:14:30 +0300

    [cloud-providers] dynamix
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

---
+++ ee/candi/cloud-providers/dynamix/terraform-modules/master-node/main.tf
@@ -0,0 +1,47 @@
+# Copyright 2024 Flant JSC
+# Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+
+data "decort_account" "account" {
+   account_id = local.account_id
+}
+
+data "decort_image" "os_image" {
+  image_id = local.os_image_id
+}
+
+resource "decort_resgroup" "resource_group" {
+  name = local.resource_group_name
+  account_id = data.decort_account.account.account_id
+  gid = local.grid
+}
+
+resource "decort_disk" "kubernetes_data_disk" {
+   disk_name = local.kubernetes_data_disk_name
+   account_id = data.decort_account.account.account_id
+   gid = local.grid
+   size_max = local.master_etcd_disk_size
+   type = "D"    # disk type, always use "D" for extra disks
+   pool = local.pool
+}
+
+resource "decort_cb_kvmvm" "master_vm" {
+  name = local.master_node_name
+  driver = local.driver
+  rg_id = decort_resgroup.resource_group.id
+  cpu = local.master_cpus
+  ram = local.master_ram_mb
+  boot_disk_size = local.master_root_disk_size
+  image_id = data.decort_image.os_image.image_id
+  pool = local.pool
+  extra_disks = [ decort_disk.kubernetes_data_disk.id ]
+
+  network {
+    net_type = local.net_type_extnet
+    net_id = local.extnet_id
+  }
+  network {
+    net_type = local.net_type_vins
+    net_id = local.vins_id
+  }
+}
+
---
+++ ee/candi/cloud-providers/dynamix/terraform-modules/master-node/variables.tf
@@ -0,0 +1,49 @@
+# Copyright 2024 Flant JSC
+# Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+
+variable "clusterConfiguration" {
+  type = any
+}
+
+variable "providerClusterConfiguration" {
+  type = any
+}
+
+variable "nodeIndex" {
+  type    = number
+  default = 0
+}
+
+variable "cloudConfig" {
+  type = string
+  default = ""
+}
+
+locals{
+  resource_name_prefix = var.clusterConfiguration.cloud.prefix
+  master_node_name = join("-", [local.resource_name_prefix, "master", var.nodeIndex])
+  master_cpus = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "numCPUs", [])
+  master_ram_mb = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "memory", [])
+  ssh_pubkey = lookup(var.providerClusterConfiguration, "sshPublicKey", null)
+  master_root_disk_size = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "rootDiskSizeGb", 50)
+  master_etcd_disk_size = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "etcdDiskSizeGb", 15)
+  account_id = lookup(var.providerClusterConfiguration, "accountId", null)
+  os_image_id = lookup(var.providerClusterConfiguration, "osImageId", null)
+  resource_group_name = join("-", [local.resource_name_prefix, "rg"]
+  kubernetes_data_disk_name = join("-", [local.master_node_name, "kubernetes-data"])
+  grid = lookup(var.providerClusterConfiguration, "grid", null)
+  pool = lookup(var.providerClusterConfiguration, "pool", null)
+  extnet_id = lookup(var.providerClusterConfiguration, "externalNetworkId", null)
+  vins_id = lookup(var.providerClusterConfiguration, "vinsNetworkId", null)
+  driver = "KVM_X86"
+  net_type_vins = "VINS"
+  net_type_extnet = "EXTNET"
+
+  master_cloud_init_script = merge({
+    "hostname": local.master_node_name,
+    "create_hostname_file": true,
+    "ssh_deletekeys": true,
+    "ssh_genkeytypes": ["rsa", "ecdsa", "ed25519"],
+    "ssh_authorized_keys" : [local.ssh_pubkey]
+  }, length(var.cloudConfig) > 0 ? base64decode(var.cloudConfig) : tomap({}))
+}
