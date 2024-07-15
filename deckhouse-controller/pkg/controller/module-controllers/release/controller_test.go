8ce1a51ebc70da0d1116474c8d37e304b21c3388
Stepan Paksashvili <stepan.paksashvili@flant.com>
2024-07-15 13:08:47 +0300

    [deckhouse] fix extenders
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/release/controller_test.go
+++ deckhouse-controller/pkg/controller/module-controllers/release/controller_test.go
@@ -100,6 +100,8 @@ func (suite *ReleaseControllerTestSuite) TearDownSubTest() {
 func (suite *ReleaseControllerTestSuite) TestCreateReconcile() {
 	entries, err := os.ReadDir("./testdata/releaseController")
 	require.NoError(suite.T(), err)
+	os.Setenv("TEST_EXTENDER_DECKHOUSE_VERSION", "v1.0.0")
+	os.Setenv("TEST_EXTENDER_KUBERNETES_VERSION", "1.28.0")
 	suite.Run("testdata cases", func() {
 		dependency.TestDC.CRClient.ImageMock.Return(&crfake.FakeImage{LayersStub: func() ([]v1.Layer, error) {
 			return []v1.Layer{&utils.FakeLayer{}, &utils.FakeLayer{FilesContent: map[string]string{"openapi/values.yaml": "{}}"}}}, nil
