ea25da5c637da979b28911494bf29ddc8d20467d
Maxim Vasilenko <maksim.vasilenko@flant.com>
2024-07-15 17:05:20 +0300

    ++
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>

--- dhctl/pkg/preflight/specs_cloud.go
+++ dhctl/pkg/preflight/specs_cloud.go
@@ -46,21 +46,21 @@ func (pc *Checker) CheckCloudMasterNodeSystemRequirements() error {
 	}
 
 	var coreCountPropertyPath, ramAmountPropertyPath, rootDiskPropertyPath []string
-	externalDiskSizeDefault := 0
+	// externalDiskSizeDefault := 0
 	switch configKind {
 	case "AWSClusterConfiguration", "GCPClusterConfiguration":
 		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "diskSizeGb"}
-		externalDiskSizeDefault = 20
+		// externalDiskSizeDefault = 20
 
 	case "AzureClusterConfiguration":
 		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "diskSizeGb"}
-		externalDiskSizeDefault = 50
+		// externalDiskSizeDefault = 50
 
 	case "YandexClusterConfiguration":
 		coreCountPropertyPath = []string{"masterNodeGroup", "instanceClass", "cores"}
 		ramAmountPropertyPath = []string{"masterNodeGroup", "instanceClass", "memory"}
 		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "diskSizeGB"}
-		externalDiskSizeDefault = 50
+		// externalDiskSizeDefault = 50
 
 	case "OpenStackClusterConfiguration":
 		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "rootDiskSize"}
@@ -69,36 +69,36 @@ func (pc *Checker) CheckCloudMasterNodeSystemRequirements() error {
 		coreCountPropertyPath = []string{"masterNodeGroup", "instanceClass", "numCPUs"}
 		ramAmountPropertyPath = []string{"masterNodeGroup", "instanceClass", "memory"}
 		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "rootDiskSize"}
-		externalDiskSizeDefault = 30
+		// externalDiskSizeDefault = 30
 
 	case "VCDClusterConfiguration":
 		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "rootDiskSizeGb"}
-		externalDiskSizeDefault = 20
+		// externalDiskSizeDefault = 20
 
 	case "ZvirtClusterConfiguration":
 		coreCountPropertyPath = []string{"masterNodeGroup", "instanceClass", "numCPUs"}
 		ramAmountPropertyPath = []string{"masterNodeGroup", "instanceClass", "memory"}
 		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "rootDiskSizeGb"}
-		externalDiskSizeDefault = 30
+		// externalDiskSizeDefault = 30
 
 	default:
 		return fmt.Errorf("unknown provider cluster configuration kind: %s", configKind)
 	}
 
-	if err = validateIntegerPropertyAtPath(configObject, rootDiskPropertyPath, minimumRequiredRootDiskSizeGB, externalDiskSizeDefault); err != nil {
+	if err = validateIntegerPropertyAtPath(configObject, rootDiskPropertyPath, minimumRequiredRootDiskSizeGB); err != nil {
 		return fmt.Errorf("Root disk capacity: %v", err)
 	}
-	if err = validateIntegerPropertyAtPath(configObject, ramAmountPropertyPath, minimumRequiredMemoryMB, 0); err != nil {
+	if err = validateIntegerPropertyAtPath(configObject, ramAmountPropertyPath, minimumRequiredMemoryMB); err != nil {
 		return fmt.Errorf("RAM amount: %v", err)
 	}
-	if err = validateIntegerPropertyAtPath(configObject, coreCountPropertyPath, minimumRequiredCPUCores, 0); err != nil {
+	if err = validateIntegerPropertyAtPath(configObject, coreCountPropertyPath, minimumRequiredCPUCores); err != nil {
 		return fmt.Errorf("CPU cores count: %v", err)
 	}
 
 	return nil
 }
 
-func validateIntegerPropertyAtPath(configObject map[string]any, propertyPath []string, minimalValue, externalDefault int) error {
+func validateIntegerPropertyAtPath(configObject map[string]any, propertyPath []string, minimalValue int) error {
 	if len(propertyPath) == 0 {
 		return nil
 	}
@@ -107,10 +107,10 @@ func validateIntegerPropertyAtPath(configObject map[string]any, propertyPath []s
 	if err != nil {
 		return fmt.Errorf("malformed provider cluster configuration: reading .%s: %w", strings.Join(propertyPath, "."), err)
 	}
-	if !found && externalDefault == 0 {
+	if !found {
 		return fmt.Errorf("malformed provider cluster configuration: reading reading .%s: no such property", strings.Join(propertyPath, "."))
 	}
-	if propertyValue != nil && propertyValue.(int) < minimalValue {
+	if propertyValue.(int) < minimalValue {
 		return fmt.Errorf("expected at least %d, but %d is configured", minimalValue, propertyValue)
 	}
 
