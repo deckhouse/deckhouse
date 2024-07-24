af73802bcf06b65eceb87cfa43c6ce400fbc73bc
vadimartynov <vadim.martynov@flant.com>
2024-07-24 10:54:16 +0300

    Fixed system registry ca in dhctl
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

--- dhctl/pkg/config/system_registry.go
+++ dhctl/pkg/config/system_registry.go
@@ -1,13 +1,27 @@
+// Copyright 2024 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package config
 
 import (
 	"path/filepath"
 	"sync"
 
-	// "github.com/cloudflare/cfssl/csr"
+	"github.com/cloudflare/cfssl/csr"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/app"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/util/cache"
-	// "github.com/deckhouse/deckhouse/go_lib/certificate"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/util/certificate"
 )
 
 var (
@@ -55,30 +69,28 @@ func getRegistryPkiData() (*RegistryPkiData, error) {
 		err := registryCache.LoadStruct(registryPkiDataCacheName, &registryPkiData)
 		return &registryPkiData, err
 	} else {
-		// authority, err := newRegistryAuthority()
-		// if err != nil {
-		// 	return nil, err
-		// }
+		authority, err := newRegistryAuthority()
+		if err != nil {
+			return nil, err
+		}
 
-		// registryPkiData := RegistryPkiData{CaCert: authority.Cert, CaKey: authority.Key}
-		registryPkiData := RegistryPkiData{CaCert: "", CaKey: ""}
+		registryPkiData := RegistryPkiData{CaCert: authority.Cert, CaKey: authority.Key}
 		err = registryCache.SaveStruct(registryPkiDataCacheName, registryPkiData)
 		return &registryPkiData, err
 	}
 }
 
-// func newRegistryAuthority() (certificate.Authority, error) {
-// 	return certificate.GenerateCA(
-// 		nil,
-// 		"registry-selfsigned-ca",
-// 		certificate.WithNames(
-// 			csr.Name{
-// 				C:  "RU",
-// 				ST: "MO",
-// 				L:  "Moscow",
-// 				O:  "Flant",
-// 				OU: "Deckhouse Registry",
-// 			},
-// 		),
-// 	)
-// }
+func newRegistryAuthority() (certificate.Authority, error) {
+	return certificate.GenerateCA(
+		"registry-selfsigned-ca",
+		certificate.WithNames(
+			csr.Name{
+				C:  "RU",
+				ST: "MO",
+				L:  "Moscow",
+				O:  "Flant",
+				OU: "Deckhouse Registry",
+			},
+		),
+	)
+}
