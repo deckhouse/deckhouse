927197bac1906cad5333122586e7cb7e2e43dfd3
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-07-29 09:13:53 +0300

    [cloud-providers] dynamix create vins
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/dynamix/layouts/standard/base-infrastructure/main.tf
+++ ee/candi/cloud-providers/dynamix/layouts/standard/base-infrastructure/main.tf
@@ -1,2 +1,21 @@
 # Copyright 2024 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+
+data "decort_account" "account" {
+   account_id = local.account_id
+}
+
+resource "decort_resgroup" "resource_group" {
+  name = local.resource_group_name
+  account_id = data.decort_account.account.account_id
+  gid = local.grid
+}
+
+resource "decort_vins" "vins" {
+  name = local.vins_name
+  rg_id = decort_resgroup.resource_group.id
+  ipcidr = local.node_network_cidr
+  ip {
+    type = "DHCP"
+  }
+}
