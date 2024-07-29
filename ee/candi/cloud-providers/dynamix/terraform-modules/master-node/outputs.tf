13f09872bf938f0774e9f40d7b17b1275b82e9f2
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-07-29 11:01:31 +0300

    [cloud-providers] dynamix create vins
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/dynamix/terraform-modules/master-node/outputs.tf
+++ ee/candi/cloud-providers/dynamix/terraform-modules/master-node/outputs.tf
@@ -2,14 +2,15 @@
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
 locals {
-  master_vm_network = tolist(decort_cb_kvmvm.master_vm.network)
+  master_vm_network_extnet = [for net in tolist(decort_cb_kvmvm.master_vm.network): net if net.type == local.net_type_extnet]
+  master_vm_network_vins = [for net in tolist(decort_cb_kvmvm.master_vm.network): net if net.type == local.net_type_vins]
 }
 
 output "master_ip_address_for_ssh" {
-  value = local.master_vm_network[0].ip_address
+  value = local.master_vm_network_extnet[0].ip_address
 }
 output "node_internal_ip_address" {
-  value = local.master_vm_network[0].ip_address
+  value = local.master_vm_network_vins[0].ip_address
 }
 output "kubernetes_data_device_path" {
   value = "/dev/vdb"
