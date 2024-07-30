3faac8ab98f3e176d97e69167a805b8e9e4c85df
Denis Romanenko <denis.romanenko@flant.com>
2024-07-30 11:42:39 +0300

    [runtime-audit-engine] update falco to 0.38.1 version
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- ee/modules/650-runtime-audit-engine/images/falco-driver-loader/docker-entrypoint.sh
+++ ee/modules/650-runtime-audit-engine/images/falco-driver-loader/docker-entrypoint.sh
@@ -3,5 +3,5 @@
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
 set -Eeuo pipefail
-/usr/bin/falcoctl driver config --type modern_ebpf
-/usr/bin/falcoctl driver install --compile=false --download=false modern_ebpf
+#/usr/bin/falcoctl driver config --type modern_ebpf
+/usr/bin/falcoctl driver install --compile=false --download=false --version=deckhouse modern_ebpf
