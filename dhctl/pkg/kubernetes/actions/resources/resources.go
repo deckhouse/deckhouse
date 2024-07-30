622b8843a38558f7ea3a36d469ab5bd7a11f4fa5
Maxim Vasilenko <5184586+mvasl@users.noreply.github.com>
2024-07-30 19:00:33 +0300

    [dhctl] Wait for resources required by manifest being created (#9157)
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    Signed-off-by: Maxim Vasilenko <5184586+mvasl@users.noreply.github.com>
    Co-authored-by: Maxim Vasilenko <maksim.vasilenko@flant.com>
    Co-authored-by: Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>

--- dhctl/pkg/kubernetes/actions/resources/resources.go
+++ dhctl/pkg/kubernetes/actions/resources/resources.go
@@ -105,6 +105,10 @@ func (c *Creator) createAll() error {
 		c.resources = remainResources
 	}()
 
+	if err := c.ensureRequiredNamespacesExist(); err != nil {
+		return err
+	}
+
 	for indx, resource := range c.resources {
 		resourcesList, err := apiResourceGetter.Get(&resource.GVK)
 		if err != nil {
@@ -125,6 +129,37 @@ func (c *Creator) createAll() error {
 		}
 	}
 
+	return nil
+}
+
+func (c *Creator) ensureRequiredNamespacesExist() error {
+	knownNamespaces := make(map[string]struct{})
+
+	for _, res := range c.resources {
+		nsName := res.Object.GetNamespace()
+		if _, nsWasSeenBefore := knownNamespaces[nsName]; nsName == "" || nsWasSeenBefore {
+			continue // If this resource is not namespaces, or we saw this namespace already, there is no need to check
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		if _, err := c.kubeCl.CoreV1().Namespaces().Get(ctx, nsName, metav1.GetOptions{}); err != nil {
+			cancel()
+
+			if !apierrors.IsNotFound(err) {
+				return fmt.Errorf("can't get namespace %q: %w", nsName, err)
+			}
+
+			return fmt.Errorf(
+				"%w: waiting for namespace %q is to create %q (%s)",
+				ErrNotAllResourcesCreated,
+				nsName,
+				res.Object.GetName(),
+				res.GVK.String(),
+			)
+		}
+		cancel()
+		knownNamespaces[nsName] = struct{}{}
+	}
 	return nil
 }
 
