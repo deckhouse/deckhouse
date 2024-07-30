68ce3ed0a51bac87fbb82731e59962f870280e4e
Denis Romanenko <denis.romanenko@flant.com>
2024-07-30 10:33:05 +0300

    [runtime-audit-engine] update falco to 0.38.1 version
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- ee/modules/650-runtime-audit-engine/images/falco-driver-loader/docker-entrypoint.sh
+++ ee/modules/650-runtime-audit-engine/images/falco-driver-loader/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -Eeuo pipefail
-/usr/bin/falcoctl driver config --type modern_bpf
-/usr/bin/falcoctl driver install --compile=false --download=false modern_bpf
+/usr/bin/falcoctl driver config --type modern_ebpf
+/usr/bin/falcoctl driver install --compile=false --download=false modern_ebpf
