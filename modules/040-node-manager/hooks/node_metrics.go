9d58a7899e46da09b23a512d94bc76f27cd63510
borg-z <me@nikolay-z.top>
2024-08-01 13:36:34 +0300

    del d8_node_status metric
    Signed-off-by: borg-z <me@nikolay-z.top>

--- modules/040-node-manager/hooks/node_metrics.go
+++ modules/040-node-manager/hooks/node_metrics.go
@@ -20,15 +20,12 @@ import (
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook/metrics"
 	"github.com/flant/addon-operator/sdk"
-	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	d8v1alpha1 "github.com/deckhouse/deckhouse/modules/040-node-manager/hooks/internal/v1alpha1"
 )
 
 const (
-	nodeMetricName     = "d8_node_status"
 	instanceMetricName = "d8_instance_status"
 	metricsGroup       = "node_instance_metrics"
 )
@@ -37,51 +34,15 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 	Queue: "/modules/node-manager/node_instance_metrics",
 	Kubernetes: []go_hook.KubernetesConfig{
 		{
-			Name:       "nodes",
-			ApiVersion: "v1",
-			Kind:       "Node",
-			FilterFunc: nodeMetricsNodeFilter,
-			LabelSelector: &v1.LabelSelector{
-				MatchExpressions: []v1.LabelSelectorRequirement{
-					{
-						Key:      "node.deckhouse.io/group",
-						Operator: v1.LabelSelectorOpExists,
-					},
-				},
-			},
-		},
-		{
 			Name:       "instances",
 			ApiVersion: "deckhouse.io/v1alpha1",
 			Kind:       "Instance",
-			FilterFunc: nodeMetricsinstanceFilter,
+			FilterFunc: instanceMetricsFilter,
 		},
 	},
-}, handleNodeInstanceMetrics)
+}, handleInstanceMetrics)
 
-func nodeMetricsNodeFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
-	var node corev1.Node
-	err := sdk.FromUnstructured(obj, &node)
-	if err != nil {
-		return nil, err
-	}
-
-	isReady := "NotReady"
-
-	for _, cond := range node.Status.Conditions {
-		if cond.Type == corev1.NodeReady && cond.Status == corev1.ConditionTrue {
-			isReady = "Ready"
-			break
-		}
-	}
-
-	return nodeStatus{
-		Name:   node.Name,
-		Status: isReady,
-	}, nil
-}
-
-func nodeMetricsinstanceFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+func instanceMetricsFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
 	var instance d8v1alpha1.Instance
 	err := sdk.FromUnstructured(obj, &instance)
 	if err != nil {
@@ -94,19 +55,14 @@ func nodeMetricsinstanceFilter(obj *unstructured.Unstructured) (go_hook.FilterRe
 	}, nil
 }
 
-type nodeStatus struct {
-	Name   string
-	Status string
-}
-
 type instanceStatus struct {
 	Name    string
 	Status  string
 	NodeRef string
 }
 
-func handleNodeInstanceMetrics(input *go_hook.HookInput) error {
-	nodeSnapshots := input.Snapshots["nodes"]
+func handleInstanceMetrics(input *go_hook.HookInput) error {
+
 	instanceSnapshots := input.Snapshots["instances"]
 
 	input.MetricsCollector.Expire(metricsGroup)
@@ -115,15 +71,9 @@ func handleNodeInstanceMetrics(input *go_hook.HookInput) error {
 		metrics.WithGroup(metricsGroup),
 	}
 
-	for _, snap := range nodeSnapshots {
-		node := snap.(nodeStatus)
-		labels := map[string]string{"node_name": node.Name, "status": node.Status}
-		input.MetricsCollector.Set(nodeMetricName, 1, labels, options...)
-	}
-
 	for _, snap := range instanceSnapshots {
 		instance := snap.(instanceStatus)
-		labels := map[string]string{"instance_name": instance.Name, "status": instance.Status, "node_name": instance.NodeRef}
+		labels := map[string]string{"instance_name": instance.Name, "status": instance.Status, "node": instance.NodeRef}
 		input.MetricsCollector.Set(instanceMetricName, 1, labels, options...)
 	}
 
--- modules/040-node-manager/hooks/node_metrics_test.go
+++ modules/040-node-manager/hooks/node_metrics_test.go
@@ -23,34 +23,6 @@ import (
 	. "github.com/deckhouse/deckhouse/testing/hooks"
 )
 
-const NodeMetricsSample = `
----
-apiVersion: v1
-kind: Node
-metadata:
-  name: node-1
-  labels:
-    node.deckhouse.io/group: "true"
-status:
-  conditions:
-  - type: Ready
-    status: "True"
-`
-
-const NodeNotReadyMetricsSample = `
----
-apiVersion: v1
-kind: Node
-metadata:
-  name: node-2
-  labels:
-    node.deckhouse.io/group: "true"
-status:
-  conditions:
-  - type: Ready
-    status: "False"
-`
-
 const InstanceMetricsSample = `
 ---
 apiVersion: deckhouse.io/v1alpha1
@@ -68,12 +40,13 @@ var _ = Describe("Node Manager :: hooks :: node_instance_metrics ::", func() {
 	f := HookExecutionConfigInit(`{"nodeManager":{"internal":{}}}`, `{}`)
 	f.RegisterCRD("deckhouse.io", "v1alpha1", "Instance", true)
 
-	assertMetric := func(f *HookExecutionConfig, metricName, nodeName, expectedStatus string) {
+	assertMetric := func(f *HookExecutionConfig, metricName, instanceName, expectedStatus, expectedNodeName string) {
 		metrics := f.MetricsCollector.CollectedMetrics()
 		ok := false
 		for _, m := range metrics {
-			if m.Name == metricName && m.Labels["node_name"] == nodeName {
+			if m.Name == metricName && m.Labels["instance_name"] == instanceName {
 				Expect(m.Labels["status"]).To(Equal(expectedStatus))
+				Expect(m.Labels["node"]).To(Equal(expectedNodeName))
 				ok = true
 				break
 			}
@@ -81,30 +54,16 @@ var _ = Describe("Node Manager :: hooks :: node_instance_metrics ::", func() {
 		Expect(ok).To(BeTrue(), "Expected metric not found")
 	}
 
-	Context("Node and Instance metrics", func() {
+	Context("Instance metrics", func() {
 		BeforeEach(func() {
-			f.BindingContexts.Set(f.KubeStateSet(NodeMetricsSample + NodeNotReadyMetricsSample + InstanceMetricsSample))
+			f.BindingContexts.Set(f.KubeStateSet(InstanceMetricsSample))
 			f.RunHook()
 		})
 
-		It("Should set correct metrics for nodes and instances", func() {
+		It("Should set correct metrics for instances", func() {
 			Expect(f).To(ExecuteSuccessfully())
 
-			assertMetric(f, "d8_node_status", "node-1", "Ready")
-			assertMetric(f, "d8_node_status", "node-2", "NotReady")
-
-			metrics := f.MetricsCollector.CollectedMetrics()
-			ok := false
-			for _, m := range metrics {
-				if m.Name == "d8_instance_status" {
-					Expect(m.Labels["instance_name"]).To(Equal("instance-1"))
-					Expect(m.Labels["node_name"]).To(Equal("node-1"))
-					Expect(m.Labels["status"]).To(Equal("Running"))
-					ok = true
-					break
-				}
-			}
-			Expect(ok).To(BeTrue(), "Expected instance metric not found")
+			assertMetric(f, "d8_instance_status", "instance-1", "Running", "node-1")
 		})
 	})
 })
