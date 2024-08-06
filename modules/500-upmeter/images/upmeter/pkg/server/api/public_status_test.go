d05abb1c4399cbbe93a7fed5b416efdeef96926f
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2024-08-06 23:05:58 +0300

    chore(upmeter): add tests for new30SecondsStepRange function (it is fine btw)
    Signed-off-by: Eugene Shevchenko <evgeny.shevchenko@flant.com>

--- modules/500-upmeter/images/upmeter/pkg/server/api/public_status_test.go
+++ modules/500-upmeter/images/upmeter/pkg/server/api/public_status_test.go
@@ -21,6 +21,7 @@ import (
 	"time"
 
 	"d8.io/upmeter/pkg/server/entity"
+	"d8.io/upmeter/pkg/server/ranges"
 )
 
 func Test_currentRange(t *testing.T) {
@@ -364,4 +365,68 @@ func Test_calculateAvailability(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_new30SecondsStepRange(t *testing.T) {
+	now := time.Date(2024, time.November, 13, 0o4, 42, 17, 647, time.UTC)
+	step := 30 * time.Second
+	aligned := now.Truncate(step)
+	// fmt.Println(aligned)
+	misaligned := aligned.Add(1 * time.Second).Add(1 * time.Millisecond)
+	// fmt.Println(aligned, misaligned)
+	misalignedAlittleBit := aligned.Add(1 * time.Nanosecond)
+	// fmt.Println(aligned, misaligned, misalignedAlittleBit)
+
+	// The range we expect is from the recent 30s episode. All arguments are in seconds
+	recent30sRange := ranges.NewStepRange(aligned.Add(-step).Unix(), aligned.Unix(), int64(step.Seconds()))
+
+	type args struct {
+		now time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+		want ranges.StepRange
+	}{
+		{
+			name: "aligned time",
+			args: args{now: aligned},
+			want: recent30sRange,
+		},
+		{
+			name: "misaligned time by 1.001s",
+			args: args{now: misaligned},
+			want: recent30sRange,
+		},
+		{
+			name: "misaligned time by 1ns",
+			args: args{now: misalignedAlittleBit},
+			want: recent30sRange,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := new30SecondsStepRange(tt.args.now); !stepRangeEqual(got, tt.want) {
+				t.Errorf("new30SecondsStepRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func stepRangeEqual(a, b ranges.StepRange) bool {
+	if !(a.From == b.From && a.To == b.To && a.Step == b.Step) {
+		return false
+	}
+	if len(a.Subranges) != len(b.Subranges) {
+		return false
+	}
+
+	for i := range a.Subranges {
+		if !(a.Subranges[i].From == b.Subranges[i].From && a.Subranges[i].To == b.Subranges[i].To) {
+			return false
+		}
+	}
+
+	return true
 }
