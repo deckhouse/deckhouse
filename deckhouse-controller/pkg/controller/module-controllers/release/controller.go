659b43fbd45c0843de3be41684b38818f12def20
Stepan Paksashvili <stepan.paksashvili@flant.com>
2024-08-07 14:10:30 +0300

    [deckhouse] fix extenders
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/release/controller.go
+++ deckhouse-controller/pkg/controller/module-controllers/release/controller.go
@@ -341,14 +341,12 @@ func (c *moduleReleaseReconciler) reconcileDeployedRelease(ctx context.Context, 
 func (c *moduleReleaseReconciler) reconcilePendingRelease(ctx context.Context, mr *v1alpha1.ModuleRelease) (ctrl.Result, error) {
 	moduleName := mr.Spec.ModuleName
 
-	if c.moduleManager.IsModuleEnabled(mr.GetModuleName()) {
-		c.logger.Debugf("checking requirements of %s for module %s by extenders", mr.GetName(), mr.GetModuleName())
-		if err := extenders.CheckModuleReleaseRequirements(mr.GetName(), mr.Spec.Requirements); err != nil {
-			if err = c.updateModuleReleaseStatusMessage(ctx, mr, err.Error()); err != nil {
-				return ctrl.Result{Requeue: true}, err
-			}
-			return ctrl.Result{RequeueAfter: defaultCheckInterval}, nil
+	c.logger.Debugf("checking requirements of %s for module %s by extenders", mr.GetName(), mr.GetModuleName())
+	if err := extenders.CheckModuleReleaseRequirements(mr.GetName(), mr.Spec.Requirements); err != nil {
+		if err = c.updateModuleReleaseStatusMessage(ctx, mr, err.Error()); err != nil {
+			return ctrl.Result{Requeue: true}, err
 		}
+		return ctrl.Result{RequeueAfter: defaultCheckInterval}, nil
 	}
 
 	otherReleases := new(v1alpha1.ModuleReleaseList)
