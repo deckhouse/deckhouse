2eb7e4b95b0781addf4e6a8aee4169678ad48070
Evsyukov Denis <denis.evsyukov@flant.com>
2024-08-08 12:53:32 +0300

    fix: skip error on required config
    Signed-off-by: Evsyukov Denis <denis.evsyukov@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/release/controller_test.go
+++ deckhouse-controller/pkg/controller/module-controllers/release/controller_test.go
@@ -311,8 +311,12 @@ func Test_validateModule(t *testing.T) {
 				nil,
 			)
 
-			if err != nil && !failed {
-				t.Fatalf("%s: unexpected error: %v", name, err)
+			if !failed {
+				require.NoError(t, err, "%s: unexpected error: %v", name, err)
+			}
+
+			if failed {
+				require.Error(t, err, "%s: got nil error", name)
 			}
 		})
 	}
@@ -321,4 +325,5 @@ func Test_validateModule(t *testing.T) {
 	check("module-not-valid", true)
 	check("module-failed", true)
 	check("module-values-failed", true)
+	check("virtualization", false)
 }
