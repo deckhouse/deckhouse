6fe12291ae30811a80019fa6ca65976f559aea1a
vadimartynov <vadim.martynov@flant.com>
2024-08-14 11:07:48 +0400

    Fixed 04-remove-flags.sh task name in dhctl
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

--- dhctl/pkg/operations/bootstrap/steps.go
+++ dhctl/pkg/operations/bootstrap/steps.go
@@ -69,7 +69,7 @@ const (
 
 func BootstrapMaster(nodeInterface node.Interface, controller *template.Controller) error {
 	return log.Process("bootstrap", "Initial bootstrap", func() error {
-		for _, bootstrapScript := range []string{"01-network-scripts.sh", "02-base-pkgs.sh", "03-remove-flags.sh"} {
+		for _, bootstrapScript := range []string{"01-network-scripts.sh", "02-base-pkgs.sh", "04-remove-flags.sh"} {
 			scriptPath := filepath.Join(controller.TmpDir, "bootstrap", bootstrapScript)
 
 			err := retry.NewLoop(fmt.Sprintf("Execute %s", bootstrapScript), 30, 5*time.Second).
