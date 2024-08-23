972add0329591fa20d5d95c2d16992e513b51b8b
Denis Tarabrin <denis.tarabrin@flant.com>
2024-08-23 13:05:10 +0400

    some fixes
    Signed-off-by: Denis Tarabrin <denis.tarabrin@flant.com>

--- global-hooks/migrate/delete-metallb-deprecated-crd_test.go
+++ global-hooks/migrate/delete-metallb-deprecated-crd_test.go
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// This hook should be deleted in DH version 1.65.
-
 package hooks
 
 import (
