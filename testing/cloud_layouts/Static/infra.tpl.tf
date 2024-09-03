015202125ccfd38814a5af16409c0a05016a5377
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2024-09-03 16:33:35 +0300

    4
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- testing/cloud_layouts/Static/infra.tpl.tf
+++ testing/cloud_layouts/Static/infra.tpl.tf
@@ -132,6 +132,45 @@ data "openstack_images_image_v2" "opensuse_image" {
   name        = "openSUSE-Leap-15.6"
 }
 
+resource "openstack_blockstorage_volume_v3" "bastion" {
+  name                 = "candi-${PREFIX}-bastion-0"
+  size                 = "30"
+  image_id             = data.openstack_images_image_v2.astra_image.id
+  volume_type          = var.volume_type
+  availability_zone    = var.az_zone
+  enable_online_resize = true
+  lifecycle {
+    ignore_changes = [image_id]
+  }
+}
+
+resource "openstack_compute_instance_v2" "bastion" {
+  name = "candi-${PREFIX}-bastion"
+  flavor_name = var.flavor_name_medium
+  key_pair = "candi-${PREFIX}-key"
+  availability_zone = var.az_zone
+
+  network {
+    port = openstack_networking_port_v2.bastion_internal_without_security.id
+  }
+  block_device {
+    uuid             = openstack_blockstorage_volume_v3.bastion.id
+    source_type      = "volume"
+    destination_type = "volume"
+    boot_index       = 0
+    delete_on_termination = true
+  }
+}
+
+resource "openstack_compute_floatingip_v2" "bastion" {
+  pool = data.openstack_networking_network_v2.external.name
+}
+
+resource "openstack_compute_floatingip_associate_v2" "bastion" {
+  floating_ip = openstack_compute_floatingip_v2.bastion.address
+  instance_id = openstack_compute_instance_v2.bastion.id
+}
+
 resource "openstack_blockstorage_volume_v3" "master" {
   name                 = "candi-${PREFIX}-master-0"
   size                 = "30"
@@ -162,38 +201,6 @@ resource "openstack_compute_instance_v2" "master" {
     boot_index       = 0
     delete_on_termination = true
   }
-
-}
-
-resource "openstack_blockstorage_volume_v3" "bastion" {
-  name                 = "candi-${PREFIX}-bastion-0"
-  size                 = "30"
-  image_id             = data.openstack_images_image_v2.astra_image.id
-  volume_type          = var.volume_type
-  availability_zone    = var.az_zone
-  enable_online_resize = true
-  lifecycle {
-    ignore_changes = [image_id]
-  }
-}
-
-resource "openstack_compute_instance_v2" "bastion" {
-  name = "candi-${PREFIX}-bastion"
-  flavor_name = var.flavor_name_medium
-  key_pair = "candi-${PREFIX}-key"
-  availability_zone = var.az_zone
-
-  network {
-    port = openstack_networking_port_v2.bastion_internal_without_security.id
-  }
-  block_device {
-    uuid             = openstack_blockstorage_volume_v3.bastion.id
-    source_type      = "volume"
-    destination_type = "volume"
-    boot_index       = 0
-    delete_on_termination = true
-  }
-
 }
 
 resource "openstack_blockstorage_volume_v3" "system" {
@@ -226,7 +233,6 @@ resource "openstack_compute_instance_v2" "system" {
     boot_index       = 0
     delete_on_termination = true
   }
-
 }
 
 resource "openstack_blockstorage_volume_v3" "worker_0" {
@@ -259,7 +265,6 @@ resource "openstack_compute_instance_v2" "worker_0" {
     boot_index       = 0
     delete_on_termination = true
   }
-
 }
 
 resource "openstack_blockstorage_volume_v3" "worker_1" {
@@ -292,16 +297,6 @@ resource "openstack_compute_instance_v2" "worker_1" {
     boot_index       = 0
     delete_on_termination = true
   }
-
-}
-
-resource "openstack_compute_floatingip_v2" "bastion" {
-  pool = data.openstack_networking_network_v2.external.name
-}
-
-resource "openstack_compute_floatingip_associate_v2" "bastion" {
-  floating_ip = openstack_compute_floatingip_v2.bastion.address
-  instance_id = openstack_compute_instance_v2.bastion.id
 }
 
 output "master_ip_address_for_ssh" {
