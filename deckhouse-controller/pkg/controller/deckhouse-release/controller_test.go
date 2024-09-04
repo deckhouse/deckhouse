64ec95049d7e92f965d55ca51220ec3bb86b8646
Daniil Stepanenko <39344422+libmonsoon-dev@users.noreply.github.com>
2024-09-04 10:13:53 +0300

    [deckhouse-controller] Restore absent releases from a registry (#9263)
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>
    Signed-off-by: Daniil Stepanenko    <39344422+libmonsoon-dev@users.noreply.github.com>
    Co-authored-by: Daniil Stepanenko <daniil.stepanenko@flant.com>
    Co-authored-by: Mikhail Scherba <41360396+miklezzzz@users.noreply.github.com>

--- deckhouse-controller/pkg/controller/deckhouse-release/controller_test.go
+++ deckhouse-controller/pkg/controller/deckhouse-release/controller_test.go
@@ -26,6 +26,7 @@ import (
 	"net/http/httptest"
 	"os"
 	"path/filepath"
+	"regexp"
 	"testing"
 	"time"
 
@@ -50,10 +51,14 @@ import (
 	"github.com/deckhouse/deckhouse/go_lib/updater"
 )
 
-var golden bool
+var (
+	golden     bool
+	mDelimiter *regexp.Regexp
+)
 
 func init() {
 	flag.BoolVar(&golden, "golden", false, "generate golden files")
+	mDelimiter = regexp.MustCompile("(?m)^---$")
 }
 
 var embeddedMUP = &v1alpha1.ModuleUpdatePolicySpec{
@@ -94,11 +99,14 @@ type ControllerTestSuite struct {
 	ctr        *deckhouseReleaseReconciler
 
 	testDataFileName string
+	backupTZ         *time.Location
 }
 
 func (suite *ControllerTestSuite) SetupSuite() {
 	flag.Parse()
 	suite.T().Setenv("D8_IS_TESTS_ENVIRONMENT", "true")
+	suite.backupTZ = time.Local
+	time.Local = dependency.TestTimeZone
 }
 
 func (suite *ControllerTestSuite) SetupSubTest() {
@@ -110,17 +118,26 @@ func (suite *ControllerTestSuite) SetupSubTest() {
 		}, nil)
 }
 
+func (suite *ControllerTestSuite) TearDownSuite() {
+	time.Local = suite.backupTZ
+}
+
 func (suite *ControllerTestSuite) TearDownSubTest() {
 	goldenFile := filepath.Join("./testdata", "golden", suite.testDataFileName)
-	got := suite.fetchResults()
+	gotB := suite.fetchResults()
 
 	if golden {
-		err := os.WriteFile(goldenFile, got, 0666)
+		err := os.WriteFile(goldenFile, gotB, 0666)
 		require.NoError(suite.T(), err)
 	} else {
-		exp, err := os.ReadFile(goldenFile)
+		got := singleDocToManifests(gotB)
+		expB, err := os.ReadFile(goldenFile)
 		require.NoError(suite.T(), err)
-		assert.YAMLEq(suite.T(), string(exp), string(got))
+		exp := singleDocToManifests(expB)
+		assert.Equal(suite.T(), len(got), len(exp), "The number of `got` manifests must be equal to the number of `exp` manifests")
+		for i := range got {
+			assert.YAMLEq(suite.T(), exp[i], got[i], "Got and exp manifests must match")
+		}
 	}
 }
 func (suite *ControllerTestSuite) setupController(
@@ -689,6 +706,17 @@ func (suite *ControllerTestSuite) fetchResults() []byte {
 	}
 
 	return result.Bytes()
+}
+
+func singleDocToManifests(doc []byte) (result []string) {
+	split := mDelimiter.Split(string(doc), -1)
+
+	for i := range split {
+		if split[i] != "" {
+			result = append(result, split[i])
+		}
+	}
+	return
 }
 
 func (suite *ControllerTestSuite) getDeckhouseRelease(name string) *v1alpha1.DeckhouseRelease {
