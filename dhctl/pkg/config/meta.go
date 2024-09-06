69e0a186bf08b1d7f7796c4103528336fe8072b1
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2024-09-06 11:21:36 +0300

    ++
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- dhctl/pkg/config/meta.go
+++ dhctl/pkg/config/meta.go
@@ -134,7 +134,7 @@ func (m *MetaConfig) Prepare() (*MetaConfig, error) {
 
 		if masterNodeGroup.Replicas > 0 &&
 			len(masterNodeGroup.InstanceClass.ExternalIPAddresses) > 0 &&
-			masterNodeGroup.Replicas != len(masterNodeGroup.InstanceClass.ExternalIPAddresses) {
+			masterNodeGroup.Replicas < len(masterNodeGroup.InstanceClass.ExternalIPAddresses) {
 			return nil, fmt.Errorf("number of masterNodeGroup.replicas should be equal to the length of masterNodeGroup.instanceClass.externalIPAddresses")
 		}
 
@@ -148,7 +148,7 @@ func (m *MetaConfig) Prepare() (*MetaConfig, error) {
 			for _, nodeGroup := range yandexNodeGroups {
 				if nodeGroup.Replicas > 0 &&
 					len(nodeGroup.InstanceClass.ExternalIPAddresses) > 0 &&
-					nodeGroup.Replicas != len(nodeGroup.InstanceClass.ExternalIPAddresses) {
+					nodeGroup.Replicas < len(nodeGroup.InstanceClass.ExternalIPAddresses) {
 					return nil, fmt.Errorf(`number of nodeGroups["%s"].replicas should be equal to the length of nodeGroups["%s"].instanceClass.externalIPAddresses`, nodeGroup.Name, nodeGroup.Name)
 				}
 			}
