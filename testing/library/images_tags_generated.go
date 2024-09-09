deckhouse/deckhouse-test-1

[candi] add d8-ca-updater
Signed-off-by: Nikolay Gorbatov <nikolay.gorbatov@flant.com>

testing/library/images_tags_generated.go
@@ -379,6 +379,7 @@ var DefaultImagesDigests = map[string]interface{}{
 		"crictl129":                 "imageHash-registrypackages-crictl129",
 		"crictl130":                 "imageHash-registrypackages-crictl130",
 		"d8":                        "imageHash-registrypackages-d8",
+		"d8CaUpdater001":            "imageHash-registrypackages-d8CaUpdater001",
 		"d8Curl821":                 "imageHash-registrypackages-d8Curl821",
 		"drbd":                      "imageHash-registrypackages-drbd",
 		"e2fsprogs1470":             "imageHash-registrypackages-e2fsprogs1470",

deckhouse/deckhouse

testing/library/images_tags_generated.go
@@ -378,13 +378,14 @@ var DefaultImagesDigests = map[string]interface{}{
 	},
 	"registrypackages": map[string]interface{}{
 		"amazonEc2Utils220":         "imageHash-registrypackages-amazonEc2Utils220",
-		"containerd1713":            "imageHash-registrypackages-containerd1713",
+		"containerd1720":            "imageHash-registrypackages-containerd1720",
 		"crictl126":                 "imageHash-registrypackages-crictl126",
 		"crictl127":                 "imageHash-registrypackages-crictl127",
 		"crictl128":                 "imageHash-registrypackages-crictl128",
 		"crictl129":                 "imageHash-registrypackages-crictl129",
 		"crictl130":                 "imageHash-registrypackages-crictl130",
 		"d8":                        "imageHash-registrypackages-d8",
+		"d8CaUpdater060824":         "imageHash-registrypackages-d8CaUpdater060824",
 		"d8Curl821":                 "imageHash-registrypackages-d8Curl821",
 		"dockerRegistry283":         "imageHash-registrypackages-dockerRegistry283",
 		"drbd":                      "imageHash-registrypackages-drbd",