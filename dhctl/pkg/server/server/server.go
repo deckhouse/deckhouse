e9d42b1c5c3ed509f76b236f127da74cc9d19fc7
Ilya Mayorov <ilya.mayorov@flant.com>
2024-09-11 18:41:13 +0700

    +

--- dhctl/pkg/server/server/server.go
+++ dhctl/pkg/server/server/server.go
@@ -44,7 +44,7 @@ func Serve(network, address string, parallelTasksLimit int) error {
 	dhctllog.InitLoggerWithOptions("silent", dhctllog.LoggerOptions{})
 	lvl := &slog.LevelVar{}
 	lvl.Set(slog.LevelDebug)
-	log := logger.NewLogger(lvl).With(slog.String("component", "DEV-server"))
+	log := logger.NewLogger(lvl).With(slog.String("component", "DEV-server-1"))
 
 	ctx, cancel := context.WithCancel(context.Background())
 	done := make(chan struct{})
