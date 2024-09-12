ba0d598a80b606d90b17d70f95ebf07f1e2f86e0
Denisenko Dmitry <dmitry.denisenko@flant.com>
2024-09-12 12:41:51 +0400

    add new version & del deprecated versions

--- .github/scripts/js/constants.js
+++ .github/scripts/js/constants.js
@@ -39,11 +39,10 @@ const labels = {
   'e2e/run/static': { type: 'e2e-run', provider: 'static' },
 
   // E2E: use Kubernetes version
-  'e2e/use/k8s/1.26': { type: 'e2e-use', ver: '1.26' },
-  'e2e/use/k8s/1.27': { type: 'e2e-use', ver: '1.27' },
   'e2e/use/k8s/1.28': { type: 'e2e-use', ver: '1.28' },
   'e2e/use/k8s/1.29': { type: 'e2e-use', ver: '1.29' },
   'e2e/use/k8s/1.30': { type: 'e2e-use', ver: '1.30' },
+  'e2e/use/k8s/1.31': { type: 'e2e-use', ver: '1.31' },
   'e2e/use/k8s/automatic': { type: 'e2e-use', ver: 'Automatic' },
 
   // Allow running workflows for external PRs.
