46f66b44f904442bcbb020c50964910249007784
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2024-09-19 13:24:05 +0300

    item 4594
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -414,6 +414,7 @@ var DefaultImagesDigests = map[string]interface{}{
 		"kubelet1298":               "imageHash-registrypackages-kubelet1298",
 		"kubelet1304":               "imageHash-registrypackages-kubelet1304",
 		"kubernetesCni140":          "imageHash-registrypackages-kubernetesCni140",
+		"lsblk2402":                 "imageHash-registrypackages-lsblk2402",
 		"netcat110481":              "imageHash-registrypackages-netcat110481",
 		"socat1734":                 "imageHash-registrypackages-socat1734",
 		"tomlMerge01":               "imageHash-registrypackages-tomlMerge01",
