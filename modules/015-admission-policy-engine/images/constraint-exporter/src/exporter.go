450059199f7f9cd1be770cf177e2f2eb76e45ea5
Denis Romanenko <denis.romanenko@flant.com>
2024-09-24 15:08:50 +0300

    ++
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- modules/015-admission-policy-engine/images/constraint-exporter/src/exporter.go
+++ modules/015-admission-policy-engine/images/constraint-exporter/src/exporter.go
@@ -14,18 +14,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package src
+package main
 
 import (
 	"sync"
 	"time"
 
-	"github.com/flant/constraint_exporter/pkg/gatekeeper"
-	"github.com/flant/constraint_exporter/pkg/kinds"
 	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
 	controllerClient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/flant/constraint_exporter/pkg/gatekeeper"
+	"github.com/flant/constraint_exporter/pkg/kinds"
 )
 
 type Exporter struct {
