40f7c47c73a63d6b8cb14a16a97a84056921075f
mortelumina98 <ekaterina.matseevska@flant.com>
2024-09-26 16:29:26 +0300

    update_17
    Signed-off-by: mortelumina98 <ekaterina.matseevska@flant.com>

--- docs/site/_plugins/gs_generator.rb
+++ docs/site/_plugins/gs_generator.rb
@@ -13,7 +13,7 @@ module GSGenerator
           if installTypeData['languages']
              installTypeData['languages'].each do |lang|
               site.pages << GSPage.new(site, site.data['getting_started']['data']['global'], installTypeKey, installTypeData, stepName, lang )
-          else  
+             else  
             site.pages << GSPage.new(site, site.data['getting_started']['data']['global'], installTypeKey, installTypeData, stepName, 'ru' )
             site.pages << GSPage.new(site, site.data['getting_started']['data']['global'], installTypeKey, installTypeData, stepName, 'en' )
           end
