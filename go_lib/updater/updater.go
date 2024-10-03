81ebef7d0eedcbfca0c17454221d00440582e9ea
Daniil Stepanenko <daniil.stepanenko@flant.com>
2024-10-04 00:58:11 +0300

    WIP
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>

--- go_lib/updater/updater.go
+++ go_lib/updater/updater.go
@@ -398,6 +398,8 @@ func (du *Updater[R]) calculatePatchResultDeployTime(release R) (releaseApplyTim
 		if err != nil {
 			return time.Time{}, 0, fmt.Errorf("patch release %s apply after: %w", release.GetName(), err)
 		}
+
+		return releaseApplyTime, notificationDelayReason, nil
 	}
 
 	return releaseApplyTime, reason, nil
