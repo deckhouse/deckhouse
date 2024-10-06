2cfe36bc3a8aa6533cf4c130c501042483ddf51c
Daniil Stepanenko <daniil.stepanenko@flant.com>
2024-10-06 18:28:51 +0300

    WIP
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>

--- go_lib/updater/updater_test.go
+++ go_lib/updater/updater_test.go
@@ -1,11 +1,27 @@
+/*
+Copyright 2024 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package updater
 
 import (
 	"testing"
 
-	"github.com/deckhouse/deckhouse/go_lib/dependency"
-
 	"github.com/stretchr/testify/require"
+
+	"github.com/deckhouse/deckhouse/go_lib/dependency"
 )
 
 func TestDeployDelayReason(t *testing.T) {
@@ -18,8 +34,8 @@ func TestDeployDelayReason(t *testing.T) {
 
 	reason = reason.add(outOfWindowReason)
 
-	require.False(t, reason.contains(manualApprovalRequiredReason))
 	require.False(t, reason.contains(noDelay))
+	require.False(t, reason.contains(manualApprovalRequiredReason))
 	require.True(t, reason.contains(outOfWindowReason))
 	require.Equal(t, "Release is waiting for the update window", reason.String())
 	require.Equal(t, "Release is waiting for the update window until 17 Oct 19 15:33 UTC", reason.string(now))
