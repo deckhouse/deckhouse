596241a989831e9201ad1d8d98d5f42ce088afc6
Timofey Kirillov <timofey.kirillov@flant.com>
2024-10-08 16:57:38 +0300

    feat(dhctl-server): options to skip preflight checks for dhctl-server operations
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

--- dhctl/pkg/server/rpc/dhctl/abort.go
+++ dhctl/pkg/server/rpc/dhctl/abort.go
@@ -199,6 +199,7 @@ func (s *Service) abort(
 	app.ResourcesTimeout = request.Options.ResourcesTimeout.AsDuration()
 	app.DeckhouseTimeout = request.Options.DeckhouseTimeout.AsDuration()
 	app.CacheDir = s.cacheDir
+	app.ApplyPreflightSkips(request.Options.CommonOptions.SkipPreflightChecks)
 
 	log.InfoF("Task is running by DHCTL Server pod/%s\n", s.podName)
 	defer func() {
