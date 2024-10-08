e4af5f2b5ebd5bbf0fae5b92a85239a5bc3847eb
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2024-10-08 16:45:44 +0300

    [documentation] changed the images to distroless
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -220,11 +220,9 @@ var DefaultImagesDigests = map[string]interface{}{
 	"descheduler": map[string]interface{}{
 		"descheduler": "imageHash-descheduler-descheduler",
 	},
-	"docsBuilder": map[string]interface{}{
-		"docsBuilder": "imageHash-docsBuilder-docsBuilder",
-	},
 	"documentation": map[string]interface{}{
-		"web": "imageHash-documentation-web",
+		"docsBuilder": "imageHash-documentation-docsBuilder",
+		"web":         "imageHash-documentation-web",
 	},
 	"extendedMonitoring": map[string]interface{}{
 		"certExporter":               "imageHash-extendedMonitoring-certExporter",
