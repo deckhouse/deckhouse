6334d90445537d5ac3cda1d5d7ac74e833aa4ccb
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-10-16 12:23:45 +0300

    [HuaweiCloud] HuaweiCloud
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- dhctl/pkg/preflight/specs_cloud.go
+++ dhctl/pkg/preflight/specs_cloud.go
@@ -80,6 +80,11 @@ func (pc *Checker) CheckCloudMasterNodeSystemRequirements() error {
 		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "rootDiskSizeGb"}
 		// externalDiskSizeDefault = 30
 
+	case "HuaweiCloudClusterConfiguration":
+		coreCountPropertyPath = []string{"masterNodeGroup", "instanceClass", "numCPUs"}
+		ramAmountPropertyPath = []string{"masterNodeGroup", "instanceClass", "memory"}
+		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "rootDiskSizeGb"}
+
 	default:
 		return fmt.Errorf("unknown provider cluster configuration kind: %s", configKind)
 	}
