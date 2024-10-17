73fda5bf7c2cbf86d49bd568a5aa01ebed6a2a72
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-10-17 12:14:57 +0300

    [dynamix] layout fix
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/dynamix/terraform-modules/static-node/main.tf
+++ ee/candi/cloud-providers/dynamix/terraform-modules/static-node/main.tf
@@ -18,38 +18,32 @@ data "decort_extnet_list" "extnets" {
 }
 
 locals {
-  image_id = data.decort_image_list.images.items[0].image_id
-  rg_id = data.decort_rg_list.resource_group.items[0].rg_id
+  image_id  = data.decort_image_list.images.items[0].image_id
+  rg_id     = data.decort_rg_list.resource_group.items[0].rg_id
   extnet_id = data.decort_extnet_list.extnets.items[0].net_id
 }
 
 resource "decort_kvmvm" "node_vm" {
-  name = local.node_name
-  driver = local.driver
-  rg_id = local.rg_id
-  cpu = local.cpus
-  ram = local.ram_mb
+  name           = local.node_name
+  driver         = local.driver
+  rg_id          = local.rg_id
+  cpu            = local.cpus
+  ram            = local.ram_mb
   boot_disk_size = local.root_disk_size
-  image_id = local.image_id
-  pool = local.pool
-  cloud_init = local.cloud_init_script
+  image_id       = local.image_id
+  pool           = local.pool
+  cloud_init     = local.cloud_init_script
 
   dynamic "network" {
     for_each = length(data.decort_vins_list.vins.items) > 0 ? [data.decort_vins_list.vins.items[0].vins_id] : []
     content {
       net_type = local.net_type_vins
-      net_id = network.value
+      net_id   = network.value
     }
   }
   network {
     net_type = local.net_type_extnet
-    net_id = local.extnet_id
-  }
-
-  lifecycle {
-    ignore_changes = [
-      cloud_init,
-    ]
+    net_id   = local.extnet_id
   }
 
   lifecycle {
