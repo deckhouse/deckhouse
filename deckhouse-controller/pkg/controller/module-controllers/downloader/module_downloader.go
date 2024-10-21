42eee6152223770a966dd8b2cf9366f9ed098015
Yuriy Losev <yuriy.losev@flant.com>
2024-10-21 14:28:58 +0400

    Fill values dockercfg if anonymous access
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/downloader/module_downloader.go
+++ deckhouse-controller/pkg/controller/module-controllers/downloader/module_downloader.go
@@ -18,6 +18,7 @@ import (
 	"archive/tar"
 	"bytes"
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -551,7 +552,18 @@ func (rsv *registrySchemaForValues) SetBase(registryBase string) {
 
 func (rsv *registrySchemaForValues) SetDockercfg(dockercfg string) {
 	if len(dockercfg) == 0 {
-		return
+		// according to the deckhouse docs, for anonymous registry access we must have the value:
+		// {"auths": { "<PROXY_REGISTRY>": {}}}
+		// but it could be empty for a ModuleSource.
+		// modules are not ready to catch empty string, so we have to fill it with the default value
+		index := strings.Index(rsv.Properties.Base.Default, "/")
+		var registry string
+		if index != -1 {
+			registry = rsv.Properties.Base.Default[:index]
+		} else {
+			registry = rsv.Properties.Base.Default
+		}
+		dockercfg = base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf(`{"auths": {"%s": {}}}`, registry)))
 	}
 
 	rsv.Properties.Dockercfg.Default = dockercfg
