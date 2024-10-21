7c6ab8b8cdad2f424b193eff76fe7936f8315cf0
Pavel Okhlopkov <36456348+ldmonster@users.noreply.github.com>
2024-10-21 10:15:56 +0300

    [deckhouse-controller] Add disable control for important modules (#10098)
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>
    Co-authored-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>
    Co-authored-by: Yuriy Losev <yuriy.losev@flant.com>

--- deckhouse-controller/pkg/apis/deckhouse.io/validation/validate_module_config.go
+++ deckhouse-controller/pkg/apis/deckhouse.io/validation/validate_module_config.go
@@ -17,12 +17,15 @@ limitations under the License.
 package validation
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
+	"github.com/flant/shell-operator/pkg/metric_storage"
 	kwhhttp "github.com/slok/kubewebhook/v2/pkg/http"
-	"github.com/slok/kubewebhook/v2/pkg/model"
 	kwhmodel "github.com/slok/kubewebhook/v2/pkg/model"
 	kwhvalidating "github.com/slok/kubewebhook/v2/pkg/webhook/validating"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -31,25 +34,114 @@ import (
 	d8config "github.com/deckhouse/deckhouse/go_lib/deckhouse-config"
 )
 
+type AnnotationsOnly struct {
+	ObjectMeta `json:"metadata,omitempty"`
+}
+
+type ObjectMeta struct {
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+const disableReasonSuffix = "Please annotate ModuleConfig with `modules.deckhouse.io/allow-disable=true` if you're sure that you want to disable the module."
+
 // moduleConfigValidationHandler validations for ModuleConfig creation
-func moduleConfigValidationHandler() http.Handler {
-	vf := kwhvalidating.ValidatorFunc(func(_ context.Context, review *model.AdmissionReview, obj metav1.Object) (result *kwhvalidating.ValidatorResult, err error) {
+func moduleConfigValidationHandler(moduleStorage ModuleStorage, metricStorage *metric_storage.MetricStorage) http.Handler {
+	vf := kwhvalidating.ValidatorFunc(func(_ context.Context, review *kwhmodel.AdmissionReview, obj metav1.Object) (result *kwhvalidating.ValidatorResult, err error) {
+		var (
+			cfg = new(v1alpha1.ModuleConfig)
+			ok  bool
+		)
+
 		switch review.Operation {
 		case kwhmodel.OperationDelete:
-			// Always allow deletion.
-			return allowResult("")
+			{
+				cfg, ok = obj.(*v1alpha1.ModuleConfig)
+				if !ok {
+					return nil, fmt.Errorf("expect ModuleConfig as unstructured, got %T", obj)
+				}
 
+				// if we have no annotation and module enabled
+				//
+				// we check module
+				// we check confirmation restriction and confirmation message
+				_, ok = cfg.Annotations[v1alpha1.AllowDisableAnnotation]
+				if !ok && *cfg.Spec.Enabled {
+					// we can delete unknown module without any further check
+					module, err := moduleStorage.GetModuleByName(obj.GetName())
+					if err == nil {
+						reason, needConfirm := module.GetConfirmationReason()
+						if needConfirm {
+							if !strings.HasSuffix(reason, ".") {
+								reason += "."
+							}
+							reason += disableReasonSuffix
+
+							return rejectResult(reason)
+						}
+					}
+				}
+
+				metricStorage.GaugeSet("d8_moduleconfig_allowed_to_disable", 0, map[string]string{"module": cfg.GetName()})
+
+				// if module is already disabled - we don't need to warn user about disabling module
+				return allowResult("")
+			}
 		case kwhmodel.OperationConnect, kwhmodel.OperationUnknown:
 			return rejectResult(fmt.Sprintf("operation '%s' is not applicable", review.Operation))
+		case kwhmodel.OperationCreate:
+			cfg, ok = obj.(*v1alpha1.ModuleConfig)
+			if !ok {
+				return nil, fmt.Errorf("expect ModuleConfig as unstructured, got %T", obj)
+			}
+		case kwhmodel.OperationUpdate:
+			oldModuleMeta := new(AnnotationsOnly)
+
+			buf := bytes.NewBuffer(review.OldObjectRaw)
+			err = json.NewDecoder(buf).Decode(oldModuleMeta)
+			if err != nil {
+				return nil, fmt.Errorf("can not parse old module config: %w", err)
+			}
+
+			cfg, ok = obj.(*v1alpha1.ModuleConfig)
+			if !ok {
+				return nil, fmt.Errorf("expect ModuleConfig as unstructured, got %T", obj)
+			}
+
+			// if we have no annotation on current ModuleConfig
+			// and we have no annotation on previous ModuleConfig (if we want to take off annotation, while disabled)
+			// and module is disabled
+			//
+			// we check module
+			// we check confirmation restriction and confirmation message
+			_, ok = cfg.Annotations[v1alpha1.AllowDisableAnnotation]
+			_, oldOk := oldModuleMeta.Annotations[v1alpha1.AllowDisableAnnotation]
+			if !ok && !oldOk && !*cfg.Spec.Enabled {
+				// we can disable unknown module without any further check
+				module, err := moduleStorage.GetModuleByName(obj.GetName())
+				if err == nil {
+					reason, needConfirm := module.GetConfirmationReason()
+					if needConfirm {
+						if !strings.HasSuffix(reason, ".") {
+							reason += "."
+						}
+						reason += disableReasonSuffix
+
+						return rejectResult(reason)
+					}
+				}
+			}
 		}
 
-		cfg, ok := obj.(*v1alpha1.ModuleConfig)
-		if !ok {
-			return nil, fmt.Errorf("expect ModuleConfig as unstructured, got %T", obj)
+		var allowedToDisableMetric float64
+		_, ok = cfg.Annotations[v1alpha1.AllowDisableAnnotation]
+		if ok && *cfg.Spec.Enabled {
+			allowedToDisableMetric = 1
 		}
 
 		// Allow changing configuration for unknown modules.
 		if !d8config.Service().PossibleNames().Has(cfg.Name) {
+			metricStorage.GaugeSet("d8_moduleconfig_allowed_to_disable", allowedToDisableMetric, map[string]string{"module": cfg.GetName()})
+
 			return allowResult(fmt.Sprintf("module name '%s' is unknown for deckhouse", cfg.Name))
 		}
 
@@ -59,6 +151,8 @@ func moduleConfigValidationHandler() http.Handler {
 		if res.HasError() {
 			return rejectResult(res.Error)
 		}
+
+		metricStorage.GaugeSet("d8_moduleconfig_allowed_to_disable", allowedToDisableMetric, map[string]string{"module": cfg.GetName()})
 
 		// Return allow with warning.
 		return allowResult(res.Warning)
