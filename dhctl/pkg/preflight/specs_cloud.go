c29ab14ba120a4c815c9c647fda2006f4178b165
Artem <mail@artbin.me>
2024-10-30 20:13:54 +0800

    Add terraform layout (#10258)
    Signed-off-by: Artem Kulyabin <artem.kulyabin@flant.com>
    Co-authored-by: Artem Kulyabin <artem.kulyabin@flant.com>
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- dhctl/pkg/preflight/specs_cloud.go
+++ dhctl/pkg/preflight/specs_cloud.go
@@ -81,9 +81,7 @@ func (pc *Checker) CheckCloudMasterNodeSystemRequirements() error {
 		// externalDiskSizeDefault = 30
 
 	case "HuaweiCloudClusterConfiguration":
-		coreCountPropertyPath = []string{"masterNodeGroup", "instanceClass", "numCPUs"}
-		ramAmountPropertyPath = []string{"masterNodeGroup", "instanceClass", "memory"}
-		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "rootDiskSizeGb"}
+		rootDiskPropertyPath = []string{"masterNodeGroup", "instanceClass", "rootDiskSize"}
 
 	default:
 		return fmt.Errorf("unknown provider cluster configuration kind: %s", configKind)
