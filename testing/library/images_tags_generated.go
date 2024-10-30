840df12d3daabead6bfe57645a553f83262cc929
Alexey Sulimenko <alexey.sulimenko@flant.com>
2024-10-30 17:04:48 +0300

    [huaweicloud] ccm
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -58,6 +58,7 @@ var DefaultImagesDigests = map[string]interface{}{
 	},
 	"cloudProviderHuaweicloud": map[string]interface{}{
 		"cloudControllerManager": "imageHash-cloudProviderHuaweicloud-cloudControllerManager",
+		"cloudDataDiscoverer":    "imageHash-cloudProviderHuaweicloud-cloudDataDiscoverer",
 		"huaweicloudCsiDriver":   "imageHash-cloudProviderHuaweicloud-huaweicloudCsiDriver",
 	},
 	"cloudProviderOpenstack": map[string]interface{}{
