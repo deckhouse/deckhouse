adc21caa1b1e8bbae8b9ad92b4be07474273fc97
Daniil Stepanenko <daniil.stepanenko@flant.com>
2024-10-31 19:51:07 +0300

    WIP
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>

--- deckhouse-controller/pkg/controller/deckhouse-release/controller_test.go
+++ deckhouse-controller/pkg/controller/deckhouse-release/controller_test.go
@@ -21,7 +21,6 @@ import (
 	"context"
 	"errors"
 	"flag"
-	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -30,6 +29,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"github.com/tidwall/sjson"
