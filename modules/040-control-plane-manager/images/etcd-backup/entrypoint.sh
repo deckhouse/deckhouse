36e529966326845371abe6fea6d2f4d5c90a7814
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2024-11-01 21:17:28 +0300

    item 15274
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- modules/040-control-plane-manager/images/etcd-backup/entrypoint.sh
+++ modules/040-control-plane-manager/images/etcd-backup/entrypoint.sh
@@ -28,6 +28,8 @@ tar -czvf "${archive}" "${etcd}"
 # Check that there will be 25% free space left after adding the file
 if [ $(df /var/lib/etcd/ | tail -1 | awk '{printf "%.0f\n", $4 - ($2 * 0.25)}') -ge $(du -k "${archive}" | awk '{print $1}') ]; then
     cp "${archive}" "/var/lib/etcd/${archive}"
+    # remove after 1.66 Old version.
+    rm -f /var/lib/etcd/etcd-backup.snapshot
 else
     echo "Free space in /var/lib/etcd/ is too small for backup should be more than 25%"
     exit 1
