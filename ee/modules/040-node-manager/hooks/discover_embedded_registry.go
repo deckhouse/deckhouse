6f8f025dbd81e7b7171f6911c4c53b3b53d27d6b
Nikita Borzykh <nikita.borzykh@flant.com>
2024-11-04 18:09:01 +0300

    Rename discover_system_registry_addresses to /discover_embedded_registry, add Values from registry-pki and deckhouse-user-ro secrets, move hook to EE, add openapi spec
    Signed-off-by: Nikita Borzykh <nikita.borzykh@flant.com>

---
+++ ee/modules/040-node-manager/hooks/discover_embedded_registry.go
@@ -0,0 +1,176 @@
+/*
+Copyright 2024 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package ee
+
+import (
+	"fmt"
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"github.com/deckhouse/deckhouse/go_lib/set"
+)
+
+const (
+	embeddedRegistryPort = "5001"
+	embeddedRegistry     = "embedded-registry.d8-system.svc"
+)
+
+type registryCredentials struct {
+	name     string
+	password string
+}
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 5},
+	Queue:        "/modules/node-manager/discover-embedded-registry",
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "system_registry",
+			ApiVersion: "v1",
+			Kind:       "Pod",
+			LabelSelector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"component": "system-registry", // TODO change to embedded-registry
+					"tier":      "control-plane",
+				},
+			},
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"d8-system"},
+				},
+			},
+			FilterFunc: embeddedRegistryPodFilter,
+		},
+		{
+			Name:       "registry_pki_secret",
+			ApiVersion: "v1",
+			Kind:       "Secret",
+			NamespaceSelector: &types.NamespaceSelector{NameSelector: &types.NameSelector{
+				MatchNames: []string{"d8-system"},
+			},
+			},
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{"registry-pki"},
+			},
+			FilterFunc: filterRegistryPkiSecret,
+		},
+		{
+			Name:       "registry_user_ro_secret",
+			ApiVersion: "v1",
+			Kind:       "Secret",
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"d8-system"},
+				},
+			},
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{"registry-user-ro"},
+			},
+			FilterFunc: filterRegistryUserRoSecret,
+		},
+	},
+}, handleEmbeddedRegistryData)
+
+func embeddedRegistryPodFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	pod := &corev1.Pod{}
+	err := sdk.FromUnstructured(obj, pod)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse pod object from unstructured: %v", err)
+	}
+	return fmt.Sprintf("%s:%s", pod.Status.HostIP, embeddedRegistryPort), nil
+}
+
+func filterRegistryPkiSecret(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	secret := &corev1.Secret{}
+
+	err := sdk.FromUnstructured(obj, secret)
+	if err != nil {
+		return nil, err
+	}
+
+	caCertBytes, exists := secret.Data["registry-ca.crt"]
+
+	if !exists {
+		return nil, nil
+	}
+	return string(caCertBytes), nil
+}
+
+func filterRegistryUserRoSecret(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	secret := &corev1.Secret{}
+
+	err := sdk.FromUnstructured(obj, &secret)
+	if err != nil {
+		return nil, err
+	}
+
+	nameBytes, nameExists := secret.Data["name"]
+	passwordBytes, passwordExists := secret.Data["password"]
+	if !nameExists || !passwordExists {
+		return nil, nil
+	}
+
+	return registryCredentials{
+		name:     string(nameBytes),
+		password: string(passwordBytes),
+	}, nil
+}
+
+func handleEmbeddedRegistryData(input *go_hook.HookInput) error {
+	endpointsSet := set.NewFromSnapshot(input.Snapshots["system_registry"])
+	endpointsList := endpointsSet.Slice() // sorted
+
+	if len(endpointsList) == 0 {
+		return nil
+	}
+
+	// Set embedded registry endpoints
+	input.LogEntry.Infof("found embedded registry endpoints: %v", endpointsList)
+	input.Values.Set("nodeManager.internal.systemRegistry.addresses", endpointsList) // TODO systemRegistry to embeddedRegistry here and in code below
+
+	// Get embedded registry CA from snapshot
+	caCertSnap := input.Snapshots["registry_pki_secret"]
+	if len(caCertSnap) == 0 {
+		input.LogEntry.Warn("Secret registry-pki not found or empty")
+		return nil
+	}
+	// Set embedded registry CA value
+	caCert := caCertSnap[0].(string)
+	input.LogEntry.Infof("found embedded registry CA")
+	input.Values.Set("nodeManager.internal.systemRegistry.registryCA", caCert)
+
+	// Get embedded registry credentials from snapshot
+	credsSnap := input.Snapshots["registry_user_ro_secret"]
+
+	if len(credsSnap) == 0 {
+		input.LogEntry.Warn("Secret registry-user-ro not found or empty")
+		return nil
+	}
+
+	registryCreds, exists := credsSnap[1].(registryCredentials)
+	if !exists {
+		input.LogEntry.Warn("Failed to parse registry-user-ro secret")
+		return nil
+	}
+
+	// If credentials are present, set them
+	if registryCreds.name != "" && registryCreds.password != "" {
+		input.LogEntry.Infof("found embedded registry credentials")
+		input.Values.Set("nodeManager.internal.systemRegistry.auth", map[string]string{
+			"username": registryCreds.name,
+			"password": registryCreds.password,
+		})
+		// Set embedded registry embeddedRegistry only if credentials are present
+		input.LogEntry.Infof("setting embedded registry embeddedRegistry to %s", embeddedRegistry)
+		input.Values.Set("nodeManager.internal.systemRegistry.address", embeddedRegistry+":"+embeddedRegistryPort)
+	}
+
+	return nil
+}
---
+++ ee/modules/040-node-manager/hooks/discover_embedded_registry_test.go
@@ -0,0 +1,260 @@
+/*
+Copyright 2024 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package ee
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/deckhouse/deckhouse/testing/hooks"
+)
+
+// #TODO change to all system-registry to embedded-registry
+var _ = Describe("Modules :: node-manager :: hooks :: discover_embedded_registry ::", func() {
+	const (
+		stateDeckhouseEmbeddedRegistryPod1 = `
+apiVersion: v1
+kind: Pod
+metadata:
+  name: system-registry-0
+  namespace: d8-system
+  labels:
+    component: "system-registry"
+    tier: "control-plane"
+status:
+  hostIP: 192.168.199.233
+  conditions:
+  - status: "True"
+    type: Ready
+`
+		stateDeckhouseEmbeddedRegistryPod2 = `
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: system-registry-1
+  namespace: d8-system
+  labels:
+    component: "system-registry"
+    tier: "control-plane"
+status:
+  hostIP: 192.168.199.234
+  conditions:
+  - status: "True"
+    type: Ready
+`
+		stateDeckhouseEmbeddedRegistryPod3 = `
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: system-registry-2
+  namespace: d8-system
+  labels:
+    component: "system-registry"
+    tier: "control-plane"
+status:
+  hostIP: 192.168.199.235
+  conditions:
+  - status: "False"
+    type: Ready
+`
+		stateRegistryPkiSecret = `
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  labels:
+    heritage: deckhouse
+    module: embedded-registry
+    type: ca-secret
+  name: registry-pki
+  namespace: d8-system
+type: Opaque
+data:
+  registry-ca.crt: Y2FfY2VydA==  # base64("ca_cert")
+  registry-ca.key: Y2Ffa2V5      # base64("ca_key")
+`
+		stateRegistryUserRoSecret = `
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  labels:
+    heritage: deckhouse
+    module: embedded-registry
+  name: registry-user-ro
+  namespace: d8-system
+type: Opaque
+data:
+  name: dXNlcg==          # base64("user")
+  password: cGFzc3dvcmQ=  # base64("password")
+`
+	)
+
+	f := HookExecutionConfigInit(`{"nodeManager":{"internal":{"systemRegistry":{}}}}`, `{}`)
+
+	Context("embedded registry pods are not found", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(""))
+			f.RunHook()
+		})
+
+		It("Hook should execute successfully", func() {
+			Expect(f).To(ExecuteSuccessfully())
+		})
+		It("`nodeManager.internal.systemRegistry.registryCA` should not be set", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.registryCA").Exists()).To(BeFalse())
+		})
+
+		It("`nodeManager.internal.systemRegistry.auth.username` should not be set", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.auth.username").Exists()).To(BeFalse())
+		})
+
+		It("`nodeManager.internal.systemRegistry.auth.password` should not be set", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.auth.password").Exists()).To(BeFalse())
+		})
+		It("`nodeManager.internal.systemRegistry.address` should not be set", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.address").Exists()).To(BeFalse())
+		})
+	})
+
+	Context("One embedded registry pod, both secrets are present", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(stateDeckhouseEmbeddedRegistryPod1 + stateRegistryPkiSecret + stateRegistryUserRoSecret))
+			f.RunHook()
+		})
+
+		It("Hook should execute successfully", func() {
+			Expect(f).To(ExecuteSuccessfully())
+		})
+
+		It("`nodeManager.internal.systemRegistry.addresses` must be ['192.168.199.233:5001']", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.addresses").String()).To(MatchJSON(`["192.168.199.233:5001"]`))
+		})
+
+		It("`nodeManager.internal.systemRegistry.address` must be 'embedded-registry.d8-system.svc:5001'", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.address").String()).To(Equal("embedded-registry.d8-system.svc:5001"))
+		})
+
+		Context("Add second embedded registry pod", func() {
+			BeforeEach(func() {
+				f.BindingContexts.Set(f.KubeStateSet(stateDeckhouseEmbeddedRegistryPod1 + stateDeckhouseEmbeddedRegistryPod2 + stateRegistryPkiSecret + stateRegistryUserRoSecret))
+				f.RunHook()
+			})
+
+			It("`nodeManager.internal.systemRegistry.addresses` must be ['192.168.199.233:5001','192.168.199.234:5001']", func() {
+				Expect(f).To(ExecuteSuccessfully())
+				Expect(f.ValuesGet("nodeManager.internal.systemRegistry.addresses").String()).To(MatchJSON(`["192.168.199.233:5001","192.168.199.234:5001"]`))
+			})
+
+			It("`nodeManager.internal.systemRegistry.address` must be 'embedded-registry.d8-system.svc:5001'", func() {
+				Expect(f.ValuesGet("nodeManager.internal.systemRegistry.address").String()).To(Equal("embedded-registry.d8-system.svc:5001"))
+			})
+
+			Context("Add third embedded registry pod", func() {
+				BeforeEach(func() {
+					f.BindingContexts.Set(f.KubeStateSet(stateDeckhouseEmbeddedRegistryPod1 + stateDeckhouseEmbeddedRegistryPod2 + stateDeckhouseEmbeddedRegistryPod3 + stateRegistryPkiSecret + stateRegistryUserRoSecret))
+					f.RunHook()
+				})
+
+				It("`nodeManager.internal.systemRegistry.addresses` must be ['192.168.199.233:5001','192.168.199.234:5001','192.168.199.235:5001']", func() {
+					Expect(f).To(ExecuteSuccessfully())
+					Expect(f.ValuesGet("nodeManager.internal.systemRegistry.addresses").String()).To(MatchJSON(`["192.168.199.233:5001","192.168.199.234:5001","192.168.199.235:5001"]`))
+				})
+
+				It("`nodeManager.internal.systemRegistry.address` must be 'embedded-registry.d8-system.svc:5001'", func() {
+					Expect(f.ValuesGet("nodeManager.internal.systemRegistry.address").String()).To(Equal("embedded-registry.d8-system.svc:5001"))
+				})
+			})
+		})
+	})
+
+	Context("One embedded registry pod, secrets are missing", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(stateDeckhouseEmbeddedRegistryPod1))
+			f.RunHook()
+		})
+
+		It("Hook should execute successfully", func() {
+			Expect(f).To(ExecuteSuccessfully())
+		})
+
+		It("`nodeManager.internal.systemRegistry.addresses` must be ['192.168.199.233:5001']", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.addresses").String()).To(MatchJSON(`["192.168.199.233:5001"]`))
+		})
+
+		It("`nodeManager.internal.systemRegistry.registryCA` should not be set", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.registryCA").Exists()).To(BeFalse())
+		})
+
+		It("`nodeManager.internal.systemRegistry.auth.username` should not be set", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.auth.username").Exists()).To(BeFalse())
+		})
+
+		It("`nodeManager.internal.systemRegistry.auth.password` should not be set", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.auth.password").Exists()).To(BeFalse())
+		})
+
+		It("`nodeManager.internal.systemRegistry.address` should not be set", func() {
+			Expect(f.ValuesGet("nodeManager.internal.systemRegistry.address").Exists()).To(BeFalse())
+		})
+
+		Context("Add second embedded registry pod", func() {
+			BeforeEach(func() {
+				f.BindingContexts.Set(f.KubeStateSet(stateDeckhouseEmbeddedRegistryPod1 + stateDeckhouseEmbeddedRegistryPod2))
+				f.RunHook()
+			})
+
+			It("`nodeManager.internal.systemRegistry.addresses` must be ['192.168.199.233:5001','192.168.199.234:5001']", func() {
+				Expect(f).To(ExecuteSuccessfully())
+				Expect(f.ValuesGet("nodeManager.internal.systemRegistry.addresses").String()).To(MatchJSON(`["192.168.199.233:5001","192.168.199.234:5001"]`))
+			})
+			It("`nodeManager.internal.systemRegistry.registryCA` should not be set", func() {
+				Expect(f.ValuesGet("nodeManager.internal.systemRegistry.registryCA").Exists()).To(BeFalse())
+			})
+
+			It("`nodeManager.internal.systemRegistry.auth.username` should not be set", func() {
+				Expect(f.ValuesGet("nodeManager.internal.systemRegistry.auth.username").Exists()).To(BeFalse())
+			})
+
+			It("`nodeManager.internal.systemRegistry.auth.password` should not be set", func() {
+				Expect(f.ValuesGet("nodeManager.internal.systemRegistry.auth.password").Exists()).To(BeFalse())
+			})
+			It("`nodeManager.internal.systemRegistry.address` should not be set", func() {
+				Expect(f.ValuesGet("nodeManager.internal.systemRegistry.address").Exists()).To(BeFalse())
+			})
+
+			Context("Add secrets", func() {
+				BeforeEach(func() {
+					f.BindingContexts.Set(f.KubeStateSet(stateDeckhouseEmbeddedRegistryPod1 + stateDeckhouseEmbeddedRegistryPod2 + stateRegistryPkiSecret + stateRegistryUserRoSecret))
+					f.RunHook()
+				})
+
+				It("`nodeManager.internal.systemRegistry.addresses` must be ['192.168.199.233:5001','192.168.199.234:5001']", func() {
+					Expect(f).To(ExecuteSuccessfully())
+					Expect(f.ValuesGet("nodeManager.internal.systemRegistry.addresses").String()).To(MatchJSON(`["192.168.199.233:5001","192.168.199.234:5001"]`))
+				})
+
+				It("`nodeManager.internal.systemRegistry.registryCA` must be 'ca_cert'", func() {
+					Expect(f.ValuesGet("nodeManager.internal.systemRegistry.registryCA").String()).To(Equal("ca_cert"))
+				})
+
+				It("`nodeManager.internal.systemRegistry.auth.username` must be 'user'", func() {
+					Expect(f.ValuesGet("nodeManager.internal.systemRegistry.auth.username").String()).To(Equal("user"))
+				})
+
+				It("`nodeManager.internal.systemRegistry.auth.password` must be 'password'", func() {
+					Expect(f.ValuesGet("nodeManager.internal.systemRegistry.auth.password").String()).To(Equal("password"))
+				})
+
+				It("`nodeManager.internal.systemRegistry.address` must be 'embedded-registry.d8-system.svc:5001'", func() {
+					Expect(f.ValuesGet("nodeManager.internal.systemRegistry.address").String()).To(Equal("embedded-registry.d8-system.svc:5001"))
+				})
+			})
+		})
+	})
+})
