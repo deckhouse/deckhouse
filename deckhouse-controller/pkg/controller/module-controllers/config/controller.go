654b0bb933ce8b9314a33dde1491c7df9b8eeb8f
Stepan Paksashvili <stepan.paksashvili@flant.com>
2024-11-19 14:28:51 +0300

    [deckhouse] update phases
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/config/controller.go
+++ deckhouse-controller/pkg/controller/module-controllers/config/controller.go
@@ -172,10 +172,11 @@ func (r *reconciler) processModule(ctx context.Context, moduleConfig *v1alpha1.M
 		if enabled {
 			r.log.Debugf("disable the '%s' module", moduleConfig.Name)
 			err := utils.UpdateStatus[*v1alpha1.Module](ctx, r.client, module, func(module *v1alpha1.Module) bool {
-				// modules in Conflict should not be installed, and they cannot receive events, so set NotInstalled phase manually
+				// modules in Conflict should not be installed, and they cannot receive events, so set Available phase manually
 				if module.Status.Phase == v1alpha1.ModulePhaseConflict {
 					module.Status.Phase = v1alpha1.ModulePhaseAvailable
-					module.SetConditionFalse(v1alpha1.ModuleConditionEnabledByModuleManager, v1alpha1.ModuleReasonNotInstalled, v1alpha1.ModuleMessageNotInstalled)
+					module.SetConditionFalse(v1alpha1.ModuleConditionEnabledByModuleManager, "", "")
+					module.SetConditionFalse(v1alpha1.ModuleConditionIsReady, v1alpha1.ModuleReasonNotInstalled, v1alpha1.ModuleMessageNotInstalled)
 				}
 				module.SetConditionFalse(v1alpha1.ModuleConditionEnabledByModuleConfig, v1alpha1.ModuleReasonDisabled, v1alpha1.ModuleMessageDisabled)
 				module.SetConditionFalse(v1alpha1.ModuleConditionIsReady, v1alpha1.ModuleReasonDisabled, v1alpha1.ModuleMessageDisabled)
