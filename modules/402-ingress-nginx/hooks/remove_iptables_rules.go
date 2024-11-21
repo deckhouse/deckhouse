14693bb691eb04c4fd317150f48218be0e1ef8dc
Evsyukov Denis <denis.evsyukov@flant.com>
2024-11-21 20:56:42 +0300

    fix: HostNetwork true
    Signed-off-by: Evsyukov Denis <denis.evsyukov@flant.com>

--- modules/402-ingress-nginx/hooks/remove_iptables_rules.go
+++ modules/402-ingress-nginx/hooks/remove_iptables_rules.go
@@ -54,8 +54,8 @@ func removeIptablesRules(input *go_hook.HookInput, dc dependency.Container) (err
 		input.Logger.Error("Failed to run job for removing iptables rules", "error", err)
 		return err
 	}
-	//input.Logger.Info("Remove iptables remove rules job from cluster...")
-	//input.PatchCollector.Delete("batch/v1", "Job", systemNamespace, iptablesRemoveJobName)
+	// input.Logger.Info("Remove iptables remove rules job from cluster...")
+	// input.PatchCollector.Delete("batch/v1", "Job", systemNamespace, iptablesRemoveJobName)
 
 	return nil
 }
@@ -80,6 +80,7 @@ func generateJob(registry, digest string) *batchv1.Job {
 			Template: corev1.PodTemplateSpec{
 				Spec: corev1.PodSpec{
 					ImagePullSecrets: []corev1.LocalObjectReference{{Name: "deckhouse-registry"}},
+					HostNetwork:      true,
 					Containers: []corev1.Container{
 						{
 							Name:  "iptables-remove-rules",
