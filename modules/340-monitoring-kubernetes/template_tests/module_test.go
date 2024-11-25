48176c06872b8c08d729d507ea1323b12c16cff0
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2024-11-25 10:33:27 +0300

    item 18282
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

---
+++ modules/340-monitoring-kubernetes/template_tests/module_test.go
@@ -0,0 +1,87 @@
+/*
+Copyright 2021 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package template_tests
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/deckhouse/deckhouse/testing/helm"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "")
+}
+
+const globalValues = `
+enabledModules: ["vertical-pod-autoscaler"]
+modules:
+  publicDomainTemplate: "%s.example.com"
+  placement: {}
+discovery:
+  clusterUUID: f49dd1c3-a63a-4565-a06c-625e35587eab
+  kubernetesVersion: 1.15.4
+  d8SpecificNodeCountByRole:
+    system: 1
+    master: 1
+`
+
+const module = `
+oomKillsExporterEnabled: true
+internal:
+  clusterDNSImplementation: coredns
+  vpa:
+    kubeStateMetricsMaxCPU: 460m
+    kubeStateMetricsMaxMemory: 870Mi
+vpa: {}
+`
+
+var _ = Describe("Module :: monitoring-kubernetes :: helm template ::", func() {
+	f := SetupHelmConfig(``)
+
+	Context("Default", func() {
+		BeforeEach(func() {
+			f.ValuesSetFromYaml("global", globalValues)
+			f.ValuesSet("global.modulesImages", GetModulesImages())
+			f.ValuesSetFromYaml("monitoringKubernetes", module)
+			f.HelmRender()
+		})
+
+		It("Everything must render properly", func() {
+			Expect(f.RenderError).ShouldNot(HaveOccurred())
+		})
+
+		var deploy []string = []string{"kube-state-metrics"}
+		for _, i := range deploy {
+			It(fmt.Sprintf("Deployment %s Exists", i), func() {
+				test := f.KubernetesResource("Deployment", "d8-monitoring", i)
+				Expect(test.Exists()).To(BeTrue())
+			})
+		}
+
+		var ds []string = []string{"node-exporter", "oom-kills-exporter"}
+		for _, i := range ds {
+			It(fmt.Sprintf("DaemonSet %s Exists", i), func() {
+				test := f.KubernetesResource("DaemonSet", "d8-monitoring", i)
+				Expect(test.Exists()).To(BeTrue())
+			})
+		}
+	})
+})
