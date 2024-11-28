ad53f9ef537ec6d65a34cebf77c95a474e80e11d
Yuriy Losev <yuriy.losev@flant.com>
2024-11-28 17:29:24 +0400

    fix provider bootstrap
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>

--- ee/modules/015-admission-policy-engine/hooks/update_trivy_provider_config.go
+++ ee/modules/015-admission-policy-engine/hooks/update_trivy_provider_config.go
@@ -7,16 +7,11 @@ package hooks
 
 import (
 	"fmt"
-	"maps"
 	"strings"
-	"time"
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
-	"github.com/flant/shell-operator/pkg/kube/object_patch"
 	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/utils/ptr"
 
@@ -26,7 +21,7 @@ import (
 const (
 	insecureRegistryPrefix = "trivy.insecureRegistry."
 	trivyProvider          = "trivy-provider"
-	trivyProviderNs        = "d8-admission-policy-engine"
+	apeNS                  = "d8-admission-policy-engine"
 	registryCAKey          = "TRIVY_REGISTRY_CA"
 	insecureKey            = "TRIVY_INSECURE"
 )
@@ -57,22 +52,6 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 			FilterFunc:                   filterCM,
 			ExecuteHookOnSynchronization: ptr.To(true),
 			ExecuteHookOnEvents:          ptr.To(true),
-		},
-		{
-			Name:       "trivy_provider_config",
-			ApiVersion: "v1",
-			Kind:       "ConfigMap",
-			NameSelector: &types.NameSelector{
-				MatchNames: []string{trivyProvider},
-			},
-			NamespaceSelector: &types.NamespaceSelector{
-				NameSelector: &types.NameSelector{
-					MatchNames: []string{trivyProviderNs},
-				},
-			},
-			FilterFunc:                   filterCM,
-			ExecuteHookOnSynchronization: ptr.To(false),
-			ExecuteHookOnEvents:          ptr.To(false),
 		},
 	},
 }, updateConfig)
@@ -109,7 +88,6 @@ func filterCM(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
 func updateConfig(input *go_hook.HookInput) error {
 	if set.NewFromValues(input.Values, "global.enabledModules").Has("operator-trivy") && input.ConfigValues.Get("admissionPolicyEngine.denyVulnerableImages.enabled").Bool() {
 		var (
-			restartRequired bool
 			// default settings
 			trivyConfig = trivySettings{
 				InsecureDbRegistry: "false",
@@ -119,71 +97,36 @@ func updateConfig(input *go_hook.HookInput) error {
 			trivyConfig = input.Snapshots["trivy_config"][0].(trivySettings)
 		}
 
-		resultingCm := &corev1.ConfigMap{
-			TypeMeta: metav1.TypeMeta{
-				APIVersion: "v1",
-				Kind:       "ConfigMap",
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      trivyProvider,
-				Namespace: trivyProviderNs,
-				Labels: map[string]string{
-					"heritage": "deckhouse",
-					"module":   "admission-policy-engine",
-				},
-			},
-			Data: make(map[string]string, 0),
-		}
+		trivyData := make(map[string]string, 0)
 		customCA := input.Values.Get("global.modulesImages.registry.CA").String()
 
 		if len(input.Snapshots["trivy_provider_config"]) == 0 {
-			restartRequired = true
 			if len(customCA) != 0 {
-				resultingCm.Data[registryCAKey] = customCA
+				trivyData[registryCAKey] = customCA
 			}
 
-			resultingCm.Data[insecureKey] = trivyConfig.InsecureDbRegistry
+			trivyData[insecureKey] = trivyConfig.InsecureDbRegistry
 
 			for k, v := range trivyConfig.InsecureRegistries {
-				resultingCm.Data[k] = v
+				trivyData[k] = v
 			}
 		} else {
-			providerConfig := input.Snapshots["trivy_provider_config"][0].(trivySettings)
-			if (customCA != providerConfig.CustomCA) || (trivyConfig.InsecureDbRegistry != providerConfig.InsecureDbRegistry) || !maps.Equal(trivyConfig.InsecureRegistries, providerConfig.InsecureRegistries) {
-				restartRequired = true
-			}
-
 			if len(customCA) != 0 {
-				resultingCm.Data[registryCAKey] = customCA
+				trivyData[registryCAKey] = customCA
 			}
-			resultingCm.Data[insecureKey] = trivyConfig.InsecureDbRegistry
+			trivyData[insecureKey] = trivyConfig.InsecureDbRegistry
 			for k, v := range trivyConfig.InsecureRegistries {
-				resultingCm.Data[k] = v
+				trivyData[k] = v
 			}
 		}
 
-		if restartRequired {
-			input.PatchCollector.Create(resultingCm, object_patch.UpdateIfExists())
-
-			templatePatch := map[string]interface{}{
-				"spec": map[string]interface{}{
-					"template": map[string]interface{}{
-						"metadata": map[string]interface{}{
-							"annotations": map[string]string{
-								"restartedAt": time.Now().Format(time.RFC3339),
-							},
-						},
-					},
-				},
-			}
-			input.PatchCollector.MergePatch(templatePatch, "apps/v1", "StatefulSet", trivyProviderNs, trivyProvider, object_patch.IgnoreMissingObject())
-		}
+		input.Values.Set("admissionPolicyEngine.internal.trivyConfigData", trivyData)
 
 		return nil
 	}
 
-	// either operator-trivy or trivy-provider is disabled, clean up the configmap
-	input.PatchCollector.Delete("v1", "ConfigMap", "d8-admission-policy-engine", trivyProvider)
+	// either operator-trivy or trivy-provider is disabled, cclean up the configmap
+	input.Values.Remove("admissionPolicyEngine.internal.trivyConfigData")
 
 	return nil
 }
