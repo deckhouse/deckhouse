3f15bf3ea0c017eae3604390686897bbe396fcf6
Sergey Kurbatov <sergey.kurbatov@flant.com>
2024-11-28 16:46:45 +0300

    [istio] Deleting the previous configuration and adding a check to see if the admission-policy-engine module is enabled
    Signed-off-by: Sergey Kurbatov <sergey.kurbatov@flant.com>

--- modules/110-istio/template_tests/module_test.go
+++ modules/110-istio/template_tests/module_test.go
@@ -943,41 +943,4 @@ updatePolicy:
 			Expect(podMonitor.Field("spec.namespaceSelector.matchNames")).To(MatchJSON(`["myns","review-123"]`))
 		})
 	})
-
-	Context("istiod with sidecar enabled livenessProbe", func() {
-		BeforeEach(func() {
-			f.ValuesSetFromYaml("global", globalValues)
-			f.ValuesSet("global.modulesImages", GetModulesImages())
-			f.ValuesSetFromYaml("istio", istioValues)
-			f.ValuesSetFromYaml("istio.internal.versionsToInstall", `["1.19.7"]`)
-			f.ValuesSet("istio.sidecar.livenessProbeEnabled", true)
-			f.HelmRender()
-		})
-
-		It("", func() {
-			Expect(f.RenderError).ShouldNot(HaveOccurred())
-			iopV19 := f.KubernetesResource("IstioOperator", "d8-istio", "v1x19x7")
-			Expect(iopV19.Field("spec.values.sidecarInjectorWebhook.templates.d8-seccomp").String()).To(MatchYAML(`
-spec:
-  initContainers:
-  - name: istio-validation
-    securityContext:
-      seccompProfile:
-        type: RuntimeDefault
-  containers:
-  - name: istio-proxy
-    securityContext:
-      seccompProfile:
-        type: RuntimeDefault
-    livenessProbe:
-      httpGet:
-        path: /healthz/ready
-        port: 15021
-      initialDelaySeconds: 10
-      periodSeconds: 2
-      timeoutSeconds: 3
-      failureThreshold: 30
-`))
-		})
-	})
 })
