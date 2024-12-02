6b7325754825727eb1c732f5eda5043e3a3eca6a
Vadim Lazovsky <vadim.lazovsky@flant.com>
2024-12-02 14:44:12 +0200

    Initial
    Signed-off-by: Vadim Lazovsky <vadim.lazovsky@flant.com>

---
+++ modules/304-observability/hooks/convert_dashboards.go
@@ -0,0 +1,174 @@
+/*
+Copyright 2021 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hooks
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube/object_patch"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+type LegacyDashboard struct {
+	Name       string
+	Definition string
+}
+
+func filterLegacyDashboard(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	cr := &LegacyDashboard{}
+	cr.Name = obj.GetName()
+	definition, ok, err := unstructured.NestedString(obj.Object, "spec", "definition")
+	if err != nil {
+		return nil, err
+	}
+
+	if !ok {
+		return nil, errors.New("no spec.definition field")
+	}
+
+	cr.Definition = definition
+	return cr, nil
+}
+
+func filterDashboardName(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	return obj.GetName(), nil
+}
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Queue: "/modules/observability/convert_dashboards",
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "cluster_observability_dashboards",
+			ApiVersion: "observability.deckhouse.io/v1alpha1",
+			Kind:       "ClusterObservabilityDashboard",
+			FilterFunc: filterDashboardName,
+		},
+		{
+			Name:       "propagated_observability_dashboards",
+			ApiVersion: "observability.deckhouse.io/v1alpha1",
+			Kind:       "PropagatedObservabilityDashboard",
+			FilterFunc: filterDashboardName,
+		},
+		{
+			Name:       "legacy_dashboards",
+			ApiVersion: "deckhouse.io/v1",
+			Kind:       "GrafanaDashboardDefinition",
+			LabelSelector: &v1.LabelSelector{
+				MatchLabels: map[string]string{
+					"module": "prometheus",
+				},
+			},
+			FilterFunc: filterLegacyDashboard,
+		},
+	},
+}, convertDashboards)
+
+func convertDashboards(input *go_hook.HookInput) error {
+	dashboards := make(map[string]bool)
+
+	legacyDashboardsSnap := input.Snapshots["legacy_dashboards"]
+
+	for _, snap := range legacyDashboardsSnap {
+		legacyDasboard := snap.(*LegacyDashboard)
+		newDashboard := createDashboard(legacyDasboard.Name, dashboardKindByName(legacyDasboard.Name), legacyDasboard.Definition)
+		input.PatchCollector.Create(&newDashboard, object_patch.UpdateIfExists())
+
+		dashboards[legacyDasboard.Name] = true
+	}
+
+	clusterObservabilityDashboardsSnap := input.Snapshots["cluster_observability_dashboards"]
+	propagatedObservabilityDashboardsSnap := input.Snapshots["propagated_observability_dashboards"]
+
+	// delete ClusterObservabilityDashboard and PropagatedObservabilityDashboard if no corresponding GrafanaDashboardDefinition found
+	for _, sn := range clusterObservabilityDashboardsSnap {
+		resourceName := sn.(string)
+		if _, ok := dashboards[resourceName]; !ok {
+			input.PatchCollector.Delete("observability.deckhouse.io/v1alpha1", "ClusterObservabilityDashboard", "", resourceName)
+		}
+	}
+
+	for _, sn := range propagatedObservabilityDashboardsSnap {
+		resourceName := sn.(string)
+		if _, ok := dashboards[resourceName]; !ok {
+			input.PatchCollector.Delete("observability.deckhouse.io/v1alpha1", "PropagatedObservabilityDashboard", "", resourceName)
+		}
+	}
+
+	return nil
+}
+
+func dashboardKindByName(name string) string {
+	propagatedDashboards := map[string]bool{
+		"d8-admission-policy-engine-security-admission-policy-engine": true,
+		"d8-applications-elasticsearch":                               true,
+		"d8-applications-etcd3":                                       true,
+		"d8-applications-loki":                                        true,
+		"d8-applications-memcached":                                   true,
+		"d8-applications-mongodb":                                     true,
+		"d8-applications-nats":                                        true,
+		"d8-applications-nats-legacy":                                 true,
+		"d8-applications-pgbouncer":                                   true,
+		"d8-applications-php-fpm":                                     true,
+		"d8-applications-prometheus":                                  true,
+		"d8-applications-rabbitmq":                                    true,
+		"d8-applications-rabbitmq-legacy":                             true,
+		"d8-applications-redis":                                       true,
+		"d8-applications-sidekiq":                                     true,
+		"d8-applications-uwsgi":                                       true,
+		"d8-monitoring-kubernetes-main-controller":                    true,
+		"d8-monitoring-kubernetes-main-namespace-namespace":           true,
+		"d8-monitoring-kubernetes-main-namespace-namespaces":          true,
+		"d8-monitoring-kubernetes-main-pod":                           true,
+		"d8-ingress-nginx-ingress-nginx-namespace-namespace-detail":   true,
+		"d8-ingress-nginx-ingress-nginx-namespace-namespaces":         true,
+		"d8-ingress-nginx-ingress-nginx-vhost-vhost-detail":           true,
+		"d8-ingress-nginx-ingress-nginx-vhost-vhosts":                 true,
+		"d8-loki-applications-loki-search":                            true,
+	}
+
+	if _, ok := propagatedDashboards[name]; ok {
+		return "PropagatedObservabilityDashboard"
+	}
+
+	return "ClusterObservabilityDashboard"
+}
+
+func createDashboard(name string, kind string, definition string) unstructured.Unstructured {
+	customName := fmt.Sprintf("d8-custom-%s", name)
+
+	un := unstructured.Unstructured{Object: map[string]interface{}{
+		"apiVersion": "observability.deckhouse.io/v1alpha1",
+		"kind":       kind,
+		"metadata": map[string]interface{}{
+			"name":      customName,
+			"namespace": "d8-monitoring",
+			"labels": map[string]interface{}{
+				"module":   "observability",
+				"heritage": "deckhouse",
+			},
+		},
+		"spec": map[string]interface{}{
+			"definition": definition,
+		},
+	}}
+
+	return un
+}
