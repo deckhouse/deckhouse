fe4ee779b797f39eb24322c3a0e038b621f3c46b
Denis Tarabrin <denis.tarabrin@flant.com>
2024-12-04 16:24:55 +0400

    go
    Signed-off-by: Denis Tarabrin <denis.tarabrin@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -292,7 +292,6 @@ var DefaultImagesDigests = map[string]interface{}{
 	},
 	"kubeDns": map[string]interface{}{
 		"coredns":                           "imageHash-kubeDns-coredns",
-		"resolvWatcher":                     "imageHash-kubeDns-resolvWatcher",
 		"stsPodsHostsAppenderInitContainer": "imageHash-kubeDns-stsPodsHostsAppenderInitContainer",
 		"stsPodsHostsAppenderWebhook":       "imageHash-kubeDns-stsPodsHostsAppenderWebhook",
 	},
