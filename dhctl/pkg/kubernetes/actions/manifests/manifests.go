c66ea3df46607ccce00728c65719126804e59fee
Stepan Paksashvili <81509933+ipaqsa@users.noreply.github.com>
2024-12-09 14:05:15 +0300

    [dhctl] fix cluster configuration secret name label (#10994)
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>
    Co-authored-by: Yuriy Losev <yuriy.losev@flant.com>

--- dhctl/pkg/kubernetes/actions/manifests/manifests.go
+++ dhctl/pkg/kubernetes/actions/manifests/manifests.go
@@ -584,7 +584,7 @@ func SecretWithClusterConfig(data []byte) *apiv1.Secret {
 		"d8-cluster-configuration",
 		"kube-system",
 		map[string][]byte{"cluster-configuration.yaml": data},
-		nil,
+		map[string]string{"name": "d8-cluster-configuration"},
 	)
 }
 
