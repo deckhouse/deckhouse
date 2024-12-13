9840b57d3c3192b7735b10711537f93301fbc555
Timur Kamaev <timur.kamaev@flant.com>
2024-12-13 14:20:22 +0600

    webhooks
    Signed-off-by: Timur Kamaev <timur.kamaev@flant.com>

--- modules/304-observability/webhooks/clusterobservabilitypropagateddashboards.py
+++ modules/304-observability/webhooks/clusterobservabilitypropagateddashboards.py
@@ -53,11 +53,12 @@ def main(ctx: hook.Context):
         # DotMap is a dict with dot notation
         binding_context = DotMap(ctx.binding_context)
         errmsg, warnings = validate(binding_context)
+        ctx.output.validations.deny("not allowed")
+        return
         if errmsg is None:
             ctx.output.validations.allow(*warnings)
         else:
-            ctx.output.validations.deny("not allowed")
-            #ctx.output.validations.deny(errmsg)
+            ctx.output.validations.deny(errmsg)
     except Exception as e:
         ctx.output.validations.error(str(e))
 
