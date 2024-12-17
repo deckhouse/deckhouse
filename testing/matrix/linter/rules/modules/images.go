ac4062fe7bafbebb990ad77e6c515cd0c94a635e
Denis Romanenko <denis.romanenko@flant.com>
2024-12-17 14:47:47 +0300

    [log-shipper] update version to 0.43.1
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- testing/matrix/linter/rules/modules/images.go
+++ testing/matrix/linter/rules/modules/images.go
@@ -221,16 +221,16 @@ func lintOneDockerfileOrWerfYAML(name, filePath, imagesPath string) errors.LintR
 						continue
 					}
 
-					result, message := isWerfInstructionUnacceptable(fromTrimmed)
-					if result {
-						return errors.NewLintRuleError(
-							"MODULE001",
-							fmt.Sprintf("module = %s, image = %s", name, relativeFilePath),
-							fromTrimmed,
-							"%s",
-							message,
-						)
-					}
+					// result, message := isWerfInstructionUnacceptable(fromTrimmed)
+					// if result {
+					//	return errors.NewLintRuleError(
+					//		"MODULE001",
+					//		fmt.Sprintf("module = %s, image = %s", name, relativeFilePath),
+					//		fromTrimmed,
+					//		"%s",
+					//		message,
+					//	)
+					// }
 				}
 			}
 			continue
