a4b7f2ae6b8d18752c68a4be7c6435bc092e8cc0
Artur Kraev <artur.kraev@flant.com>
2024-12-18 11:21:11 +0300

    Add mirrorrer stub
    Signed-off-by: Artur Kraev <artur.kraev@flant.com>

---
+++ ee/modules/038-system-registry/images/mirrorer/app/cmd/mirrorer/main.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2024 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	dlog "github.com/deckhouse/deckhouse/pkg/log"
+)
+
+var (
+	shutdownSignals              = []os.Signal{os.Interrupt, syscall.SIGTERM}
+	logHandler      slog.Handler = dlog.Default().Handler()
+	nodeName                     = os.Getenv("NODE_NAME")
+)
+
+func main() {
+	log := slog.New(logHandler).With("component", "main")
+	log = log.With("node", nodeName)
+
+	hostIP := os.Getenv("HOST_IP")
+	if hostIP == "" {
+		log.Error("HOST_IP environment variable is not set")
+		os.Exit(1)
+	}
+
+	log.Info("Starting mirrorer")
+	defer log.Info("Stopped")
+
+	ctx := setupSignalHandler()
+	ctx, cancel := context.WithCancel(ctx)
+
+	log.Info("Waiting for signal")
+	<-ctx.Done()
+	cancel()
+}
+
+func setupSignalHandler() context.Context {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, shutdownSignals...)
+	go func() {
+		<-c
+		cancel()
+		<-c
+		os.Exit(1) // second signal. Exit directly.
+	}()
+
+	return ctx
+}
---
+++ ee/modules/038-system-registry/images/mirrorer/app/go.mod
@@ -0,0 +1,10 @@
+module mirrorer
+
+go 1.23.4
+
+require github.com/deckhouse/deckhouse/pkg/log v0.0.0-20241217181326-61486be4aa10
+
+require (
+	github.com/DataDog/gostackparse v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
