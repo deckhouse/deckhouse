122682e59bc0ce6a2bfa523862c5cdbe881efaa5
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2024-12-19 17:01:23 +0300

    add copyright
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- deckhouse-controller/pkg/controller/deckhouse-release/updater/requirements_checker.go
+++ deckhouse-controller/pkg/controller/deckhouse-release/updater/requirements_checker.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package d8updater
 
 import (
@@ -35,8 +51,6 @@ type NotMetReason struct {
 var _ RequirementsChecker[v1alpha1.DeckhouseRelease] = (*Checker[v1alpha1.DeckhouseRelease])(nil)
 
 type Checker[T any] struct {
-	k8sclient client.Client
-
 	fns []Check[T]
 
 	logger *log.Logger
@@ -65,7 +79,6 @@ func NewDeckhouseReleaseChecker(k8sclient client.Client, enabledModules []string
 	}
 
 	return &Checker[v1alpha1.DeckhouseRelease]{
-		k8sclient: k8sclient,
 		fns: []Check[v1alpha1.DeckhouseRelease]{
 			newDeckhouseVersionCheck(enabledModules),
 			newDeckhouseRequirementsCheck(enabledModules),
