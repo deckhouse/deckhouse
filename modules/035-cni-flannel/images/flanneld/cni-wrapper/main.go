a54610e6d3c78f17d0cbc405d728fb7942ffae96
Denis Tarabrin <denis.tarabrin@flant.com>
2024-12-20 16:39:44 +0400

    +
    Signed-off-by: Denis Tarabrin <denis.tarabrin@flant.com>

--- modules/035-cni-flannel/images/flanneld/cni-wrapper/main.go
+++ modules/035-cni-flannel/images/flanneld/cni-wrapper/main.go
@@ -17,9 +17,7 @@ limitations under the License.
 package main
 
 import (
-	"context"
 	"log"
-	"net"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -29,16 +27,15 @@ import (
 	"time"
 
 	"github.com/vishvananda/netlink"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+)
+
+const (
+	wrapperAim          = "/entrypoint"
+	iptablesFullCommand = "/sbin/iptables"
 )
 
 func runCNI(done chan<- error, pidChan chan<- int) {
-	cmd := exec.Command("/entrypoint", os.Args[1:]...)
+	cmd := exec.Command(wrapperAim, os.Args[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Env = os.Environ()
@@ -84,7 +81,7 @@ func deleteConfigFiles(configsDir string, cni string) {
 
 func execIptables(args ...string) (string, error) {
 	args = append(args, "--wait")
-	cmd := exec.Command("/sbin/iptables", args...)
+	cmd := exec.Command(iptablesFullCommand, args...)
 	output, err := cmd.CombinedOutput()
 	return string(output), err
 }
@@ -195,98 +192,98 @@ func main() {
 		}
 	}
 
-	// Get CNI ModuleConfig
-	moduleConfigName := "cni-flannel"
+	// // Get CNI ModuleConfig
+	// moduleConfigName := "cni-flannel"
 
-	nodeName, err := os.Hostname()
-	if err != nil {
-		log.Fatal(err) // FIXME: ???
-	}
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		log.Fatalf("failed to get in-cluster config: %v", err)
-	}
-	dynamicClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		log.Fatalf("Failed to create dynamic client: %v", err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 25*time.Second)
-	defer cancel()
+	// nodeName, err := os.Hostname()
+	// if err != nil {
+	// 	log.Fatal(err) // FIXME: ???
+	// }
+	// config, err := rest.InClusterConfig()
+	// if err != nil {
+	// 	log.Fatalf("failed to get in-cluster config: %v", err)
+	// }
+	// dynamicClient, err := dynamic.NewForConfig(config)
+	// if err != nil {
+	// 	log.Fatalf("Failed to create dynamic client: %v", err)
+	// }
+	// ctx, cancel := context.WithTimeout(context.Background(), 25*time.Second)
+	// defer cancel()
 
-	moduleConfigGVR := schema.GroupVersionResource{
-		Group:    "deckhouse.io",
-		Version:  "v1alpha1",
-		Resource: "moduleconfigs",
-	}
-	moduleConfig, err := dynamicClient.Resource(moduleConfigGVR).Get(ctx, moduleConfigName, metav1.GetOptions{})
-	if err != nil {
-		log.Fatalf("failed to get ModuleConfig %s: %v", moduleConfigName, err)
-	}
-	moduleStatus, found, err := unstructured.NestedBool(moduleConfig.Object, "spec", "enabled")
-	if err != nil {
-		log.Fatalf("Failed to get moduleStatus from ModuleConfig: %v", err)
-	}
-	if !found {
-		log.Fatalf("moduleStatus not found in ModuleConfig")
-	}
-	log.Printf("ModuleStatus: %t", moduleStatus)
+	// moduleConfigGVR := schema.GroupVersionResource{
+	// 	Group:    "deckhouse.io",
+	// 	Version:  "v1alpha1",
+	// 	Resource: "moduleconfigs",
+	// }
+	// moduleConfig, err := dynamicClient.Resource(moduleConfigGVR).Get(ctx, moduleConfigName, metav1.GetOptions{})
+	// if err != nil {
+	// 	log.Fatalf("failed to get ModuleConfig %s: %v", moduleConfigName, err)
+	// }
+	// moduleStatus, found, err := unstructured.NestedBool(moduleConfig.Object, "spec", "enabled")
+	// if err != nil {
+	// 	log.Fatalf("Failed to get moduleStatus from ModuleConfig: %v", err)
+	// }
+	// if !found {
+	// 	log.Fatalf("moduleStatus not found in ModuleConfig")
+	// }
+	// log.Printf("ModuleStatus: %t", moduleStatus)
 
-	// Get pods within podCIDR
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatalf("failed to create clientset: %v", err)
-	}
-	nodeObj, err := clientset.CoreV1().Nodes().Get(ctx, nodeName, metav1.GetOptions{})
-	if err != nil {
-		log.Fatalf("failed to get node %s: %v", nodeName, err)
-	}
-	podCIDR := nodeObj.Spec.PodCIDR
-	if podCIDR == "" {
-		log.Fatalf("PodCIDR is empty for node %s", nodeName)
-	}
-	log.Printf("node: %s, PodCIDR: %s\n", nodeName, podCIDR)
+	// // Get pods within podCIDR
+	// clientset, err := kubernetes.NewForConfig(config)
+	// if err != nil {
+	// 	log.Fatalf("failed to create clientset: %v", err)
+	// }
+	// nodeObj, err := clientset.CoreV1().Nodes().Get(ctx, nodeName, metav1.GetOptions{})
+	// if err != nil {
+	// 	log.Fatalf("failed to get node %s: %v", nodeName, err)
+	// }
+	// podCIDR := nodeObj.Spec.PodCIDR
+	// if podCIDR == "" {
+	// 	log.Fatalf("PodCIDR is empty for node %s", nodeName)
+	// }
+	// log.Printf("node: %s, PodCIDR: %s\n", nodeName, podCIDR)
 
-	_, ipNet, err := net.ParseCIDR(podCIDR)
-	if err != nil {
-		log.Fatalf("failed to parse PodCIDR %s: %v", podCIDR, err)
-	}
+	// _, ipNet, err := net.ParseCIDR(podCIDR)
+	// if err != nil {
+	// 	log.Fatalf("failed to parse PodCIDR %s: %v", podCIDR, err)
+	// }
 
-	pods, err := clientset.CoreV1().Pods("").List(ctx, metav1.ListOptions{
-		FieldSelector: "spec.nodeName=" + nodeName,
-	})
-	if err != nil {
-		log.Fatalf("failed to list pods on node %s: %v", nodeName, err)
-	}
+	// pods, err := clientset.CoreV1().Pods("").List(ctx, metav1.ListOptions{
+	// 	FieldSelector: "spec.nodeName=" + nodeName,
+	// })
+	// if err != nil {
+	// 	log.Fatalf("failed to list pods on node %s: %v", nodeName, err)
+	// }
 
-	for _, pod := range pods.Items {
-		podIP := net.ParseIP(pod.Status.PodIP)
-		if podIP == nil {
-			log.Printf("pod %s/%s has no IP yet, skipping", pod.Namespace, pod.Name)
-			continue
-		}
+	// for _, pod := range pods.Items {
+	// 	podIP := net.ParseIP(pod.Status.PodIP)
+	// 	if podIP == nil {
+	// 		log.Printf("pod %s/%s has no IP yet, skipping", pod.Namespace, pod.Name)
+	// 		continue
+	// 	}
 
-		if ipNet.Contains(podIP) {
-			log.Printf(
-				"deleting pod %s/%s (IP: %s) because it belongs to PodCIDR %s\n",
-				pod.Namespace,
-				pod.Name,
-				pod.Status.PodIP,
-				podCIDR,
-			)
+	// 	if ipNet.Contains(podIP) {
+	// 		log.Printf(
+	// 			"deleting pod %s/%s (IP: %s) because it belongs to PodCIDR %s\n",
+	// 			pod.Namespace,
+	// 			pod.Name,
+	// 			pod.Status.PodIP,
+	// 			podCIDR,
+	// 		)
 
-			// gracePeriodSeconds := int64(30)
-			// deleteOptions := metav1.DeleteOptions{
-			// 	GracePeriodSeconds: &gracePeriodSeconds,
-			// }
+	// 		// gracePeriodSeconds := int64(30)
+	// 		// deleteOptions := metav1.DeleteOptions{
+	// 		// 	GracePeriodSeconds: &gracePeriodSeconds,
+	// 		// }
 
-			// err := clientset.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, deleteOptions)
-			// if err != nil {
-			// 	log.Printf("failed to delete pod %s/%s: %v", pod.Namespace, pod.Name, err)
-			// } else {
-			// 	log.Printf("pod %s/%s deleted successfully", pod.Namespace, pod.Name)
-			// }
-		}
-	}
+	// 		// err := clientset.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, deleteOptions)
+	// 		// if err != nil {
+	// 		// 	log.Printf("failed to delete pod %s/%s: %v", pod.Namespace, pod.Name, err)
+	// 		// } else {
+	// 		// 	log.Printf("pod %s/%s deleted successfully", pod.Namespace, pod.Name)
+	// 		// }
+	// 	}
+	// }
 
 	log.Println("start system cleaning")
 
