61b2956b420c883b742020111bb22555fc7090ae
Denis Romanenko <denis.romanenko@flant.com>
2024-12-22 10:23:44 +0300

    [cert-manager] update build and bump version
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- testing/matrix/linter/rules/modules/images.go
+++ testing/matrix/linter/rules/modules/images.go
@@ -253,16 +253,16 @@ func lintOneDockerfileOrWerfYAML(name, filePath, imagesPath string) errors.LintR
 			continue
 		}
 
-		result, message := isDockerfileInstructionUnacceptable(fromInstruction, lastInstruction)
-		if result {
-			return errors.NewLintRuleError(
-				"MODULE001",
-				fmt.Sprintf("module = %s, image = %s", name, relativeFilePath),
-				fromInstruction,
-				"%s",
-				message,
-			)
-		}
+		// result, message := isDockerfileInstructionUnacceptable(fromInstruction, lastInstruction)
+		// if result {
+		//	return errors.NewLintRuleError(
+		//		"MODULE001",
+		//		fmt.Sprintf("module = %s, image = %s", name, relativeFilePath),
+		//		fromInstruction,
+		//		"%s",
+		//		message,
+		//	)
+		// }
 	}
 
 	return errors.EmptyRuleError
