3eaa36d5f5c769de27978d4f45e8569f1b7a6198
Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>
2024-12-23 19:17:44 +0300

    remove timeouts from zvirt - not supported by tf provider
    Signed-off-by: Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>

--- ee/se-plus/candi/cloud-providers/zvirt/terraform-modules/static-node/main.tf
+++ ee/se-plus/candi/cloud-providers/zvirt/terraform-modules/static-node/main.tf
@@ -32,12 +32,6 @@ resource "ovirt_vm" "node_vm" {
       placement_policy_host_ids
     ]
   }
-
-  timeouts {
-    create = var.resourceManagementTimeout
-    delete = var.resourceManagementTimeout
-    update = var.resourceManagementTimeout
-  }
 }
 
 data "ovirt_disk_attachments" "node-vm-boot-disk-attachment" {
