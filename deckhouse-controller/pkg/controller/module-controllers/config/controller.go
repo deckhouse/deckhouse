661d0d245cff48ad081c59b1e29812bdafb394a9
Stepan Paksashvili <81509933+ipaqsa@users.noreply.github.com>
2024-12-27 13:10:55 +0300

    Update deckhouse-controller/pkg/controller/module-controllers/config/controller.go
    Co-authored-by: Yuriy Losev <yuriy.losev@flant.com>
    Signed-off-by: Stepan Paksashvili <81509933+ipaqsa@users.noreply.github.com>

--- deckhouse-controller/pkg/controller/module-controllers/config/controller.go
+++ deckhouse-controller/pkg/controller/module-controllers/config/controller.go
@@ -329,7 +329,7 @@ func (r *reconciler) deleteModuleConfig(ctx context.Context, moduleConfig *v1alp
 	}
 
 	if err := r.removeFinalizer(ctx, moduleConfig); err != nil {
-		r.log.Errorf("failed to remove finalizer for the '%s' module config: %v", moduleConfig.Name, err)
+		r.log.Error("failed to remove finalizer from ModuleConfig", slog.String("module", moduleConfig.Name), log.Err(err))
 		return ctrl.Result{Requeue: true}, nil
 	}
 
