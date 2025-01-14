88d241bc1ffa35f4b36374b6892843e6add6c529
YuryLysov <yuriy.lysov@flant.com>
2025-01-14 16:28:35 +0400

    Add k8s 1.32 to CI templates
    Signed-off-by: YuryLysov <yuriy.lysov@flant.com>

--- .github/scripts/js/constants.js
+++ .github/scripts/js/constants.js
@@ -45,6 +45,7 @@ const labels = {
   'e2e/use/k8s/1.29': { type: 'e2e-use', ver: '1.29' },
   'e2e/use/k8s/1.30': { type: 'e2e-use', ver: '1.30' },
   'e2e/use/k8s/1.31': { type: 'e2e-use', ver: '1.31' },
+  'e2e/use/k8s/1.32': { type: 'e2e-use', ver: '1.32' },
   'e2e/use/k8s/automatic': { type: 'e2e-use', ver: 'Automatic' },
 
   // E2E: use multimaster configuration
