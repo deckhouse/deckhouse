06071879c55ba7c73507fc93a3bacea56be1a88f
Kanatbek Saribaev <kanatbek.saribaev@flant.com>
2025-01-14 20:14:00 +0300

    fix

--- tools/cve/check-non-root.sh
+++ tools/cve/check-non-root.sh
@@ -2,77 +2,107 @@
 
 set -e
 source tools/cve/trivy-wrapper.sh
-# Функция для генерации HTML тела
+
+declare -A allowed_users=(
+  ["istio operatorV1x21x6"]="1337:1337"
+  ["istio operatorV1x16x2"]="1337:1337"
+  ["istio operatorV1x19x7"]="1337:1337"
+  ["istio pilotV1x16x2"]="1337:1337"
+  ["istio pilotV1x19x7"]="1337:1337"
+  ["istio pilotV1x21x6"]="1337:1337"
+)
+declare -A allowed_components=(
+  ["64535"]="all_components"
+  ["64535:64535"]="all_components"
+  ["deckhouse:deckhouse"]="all_components"
+  ["deckhouse"]="all_components"
+)
+
+# Function to generate HTML body
 generate_html_body() {
-  # Проверяем, что передан файл
+  # Check if a file is provided
   if [ -z "$1" ]; then
     echo "Usage: $0 <input.jsonl>"
     exit 1
   fi
 
   input_file="$1"
-
-  # Начало тела HTML
+  # Start of HTML body
   cat <<EOF
     <h1>Non-Root User Check Report</h1>
     <table>
         <thead>
             <tr>
+                <th>Status</th>
                 <th>ImageReportName</th>
                 <th>Image</th>
                 <th>User</th>
-                <th>Status</th>
             </tr>
         </thead>
         <tbody>
 EOF
 
-  # Генерируем строки таблицы из JSONL файла
+  # Generate table rows from JSONL file
   jq -r '. | @json' "$input_file" | while read -r line; do
       imageReportName=$(echo "$line" | jq -r '.ImageReportName')
       image=$(echo "$line" | jq -r '.Image')
       user=$(echo "$line" | jq -r '.User')
       status=$(echo "$line" | jq -r '.Status')
 
-      # Выбираем класс строки в зависимости от статуса
+      # Select row class based on status
       if [ "$status" == "FAIL" ]; then
           row_class="fail"
       else
           row_class="pass"
       fi
 
-      # Добавляем строку в таблицу
+      # Add a row to the table
       cat <<ROW
           <tr class="$row_class">
+              <td>$status</td>
               <td>$imageReportName</td>
               <td>$image</td>
               <td>$user</td>
-              <td>$status</td>
           </tr>
 ROW
   done
 
-  # Закрытие тела HTML
+  # Close HTML body
   cat <<EOF
         </tbody>
     </table>
 EOF
 }
-# Проверка запуска образа от пользователя root
+
+function get_allowed_components() {
+  local component=$1
+  echo "${allowed_components[$component]:-"none"}"
+}
+function get_allowed_users() {
+  local user_name=$1
+  echo "${allowed_users[$user_name]:-"none"}"
+}
+
+# Function to check if the image is run as the root user
 function check_user() {
   local image=$2
   local user
   local result
   local image_report_name=$1
   local workdir=$3
-  # Извлекаем информацию о пользователе из конфигурации образа
-  user=$(crane config "$image" | jq '.config.User')
+  # Extract user information from the image configuration
+  user=$(crane config "$image" | jq -r '.config.User')
+  allowed_component=$(get_allowed_components "$user")
+  allowed_user=$(get_allowed_users "$image_report_name")
   if [ $user == "null" ] || [ "$user" == "root" ] || [ "$user" == "0:0" ]; then
-    result="FAIL"
-    if [ $user == "null" ];then
+    result="ERROR"
+    if [ $user == "null" ]; then
       user="root"
     fi 
-    echo "{\"ImageReportName\":\"$image_report_name\",\"Image\":\"$image\",\"User\":\"$user\",\"Status\":\"$result\"}" >> $workdir/report.jsonl
+    echo "{\"Status\":\"$result\",\"ImageReportName\":\"$image_report_name\",\"Image\":\"$image\",\"User\":\"$user\"}" >> $workdir/report.jsonl
+  elif [ "$allowed_component" != "all_components" ] && [ "$user" != "$allowed_user" ]; then
+    result="WARNING"
+    echo "{\"Status\":\"$result\",\"ImageReportName\":\"$image_report_name\",\"Image\":\"$image\",\"User\":\"$user\"}" >> $workdir/report.jsonl
   fi
 }
 
@@ -84,12 +114,12 @@ function __main__() {
 
   docker pull "$IMAGE:$TAG"
   digests=$(docker run --rm "$IMAGE:$TAG" cat /deckhouse/modules/images_digests.json)
-
   WORKDIR=$(mktemp -d)
-  IMAGE_REPORT_NAME="deckhouse::$(echo "$IMAGE:$TAG" | sed 's/^.*\/\(.*\)/\1/')"
+  IMAGE_REPORT_NAME="deckhouse $(echo "$IMAGE:$TAG" | sed 's/^.*\/\(.*\)/\1/')"
   mkdir -p out/
+  touch $WORKDIR/report.jsonl
   htmlReportHeader > out/non-root-images.html
-  check_user $IMAGE_REPORT_NAME $IMAGE:$TAG $WORKDIR
+  check_user "$IMAGE_REPORT_NAME" "$IMAGE:$TAG" $WORKDIR
   for module in $(jq -rc 'to_entries[]' <<< "$digests"); do
     MODULE_NAME=$(jq -rc '.key' <<< "$module")
     echo "=============================================="
@@ -105,11 +135,11 @@ function __main__() {
       echo ""
 
       IMAGE_HASH="$(jq -rc '.value' <<< "$module_image")"
-      IMAGE_REPORT_NAME="$MODULE_NAME::$IMAGE_NAME"
-      check_user $IMAGE_REPORT_NAME "$IMAGE@$IMAGE_HASH" $WORKDIR
+      IMAGE_REPORT_NAME="$MODULE_NAME $IMAGE_NAME"
+      check_user "$IMAGE_REPORT_NAME" "$IMAGE@$IMAGE_HASH" $WORKDIR
     done
   done
-  # Генерация HTML-отчёта
+  # Generate HTML report
   generate_html_body $WORKDIR/report.jsonl >> out/non-root-images.html
   rm -r "$WORKDIR"
   htmlReportFooter >> out/non-root-images.html
