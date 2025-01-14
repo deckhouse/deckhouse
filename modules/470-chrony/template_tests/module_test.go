fc2238135879456809fc6076f0a5d6d83be1ac6a
YuryLysov <yuriy.lysov@flant.com>
2025-01-14 19:07:20 +0400

    Fix tests
    Signed-off-by: YuryLysov <yuriy.lysov@flant.com>

--- modules/470-chrony/template_tests/module_test.go
+++ modules/470-chrony/template_tests/module_test.go
@@ -38,7 +38,7 @@ const (
     kind: ClusterConfiguration
     clusterType: Static
     clusterDomain: "cluster.local"
-    kubernetesVersion: "1.27"
+    kubernetesVersion: "1.28"
     serviceSubnetCIDR: "10.222.0.0/16"
     podSubnetCIDR: "10.111.0.0/16"
   modules:
