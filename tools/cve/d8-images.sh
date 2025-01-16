7bdaf336c0c1edb3ff82850ca2ff49adc4132c27
Nikolay Mordvintsev <nikolay.mordvintsev@flant.com>
2025-01-16 15:35:29 +0300

    cve report issues
    Signed-off-by: Nikolay Mordvintsev <nikolay.mordvintsev@flant.com>

--- tools/cve/d8-images.sh
+++ tools/cve/d8-images.sh
@@ -56,6 +56,7 @@ function __main__() {
 
   for module in $(jq -rc 'to_entries[]' <<< "$digests"); do
     MODULE_NAME=$(jq -rc '.key' <<< "$module")
+    touch out/${MODULE_NAME}_report
     echo "=============================================="
     echo "🛰 Module: $MODULE_NAME"
 
@@ -70,7 +71,27 @@ function __main__() {
 
       IMAGE_HASH="$(jq -rc '.value' <<< "$module_image")"
       IMAGE_REPORT_NAME="$MODULE_NAME::$IMAGE_NAME"
-      trivyGetHTMLReportPartForImage  -l "$IMAGE_REPORT_NAME" -i "$IMAGE@$IMAGE_HASH" -s "$SEVERITY" --ignore out/.trivyignore >> out/d8-images.html
+      # Output reports to common_report file and appropriate module_report file
+      trivyGetHTMLReportPartForImage  -l "$IMAGE_REPORT_NAME" -i "$IMAGE@$IMAGE_HASH" -s "$SEVERITY" --ignore out/.trivyignore | tee -a out/${MODULE_NAME}_report >> out/d8-images.html
+
+    done
+    # Create an issue with found vulnerabilities
+    CODEOWNERS_MODULE_NAME=$(echo $MODULE_NAME|sed -s 's/[A-Z]/-&/g')
+    owners="[\"RomanenkoDenys\"]" # Default assignee in case if not found in CODEOWNERS file
+    for line in $(cat /deckhouse/.github/CODEOWNERS); do
+      if echo $line| grep -i "$CODEOWNERS_MODULE_NAME"; then
+        owners=$(echo $line | cut -d "@" -f 2-|jq --raw-input 'split(" @")')
+      fi
+      echo " Creating GitHub issue for module $MODULE_NAME with assignees $owners"
+      echo ""
+
+      curl -L \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/deckhouse/deckhouse/issues \
+        -d '{"title":"'$module' CVE Issue","body":"'$(cat out/${MODULE_NAME}_report)'","assignees":'$owners',"labels":["cve"]}'
     done
   done
 
