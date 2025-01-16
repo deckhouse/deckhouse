9d0f3d1e689af0f0eb685a3456dfb34372dcf861
Nikolay Mordvintsev <nikolay.mordvintsev@flant.com>
2025-01-16 16:59:14 +0300

    quite grep
    Signed-off-by: Nikolay Mordvintsev <nikolay.mordvintsev@flant.com>

--- tools/cve/d8-images.sh
+++ tools/cve/d8-images.sh
@@ -79,7 +79,7 @@ function __main__() {
     CODEOWNERS_MODULE_NAME=$(echo $MODULE_NAME|sed -s 's/[A-Z]/-&/g')
     owners="[\"Nikolay1224\"]" # Default assignee in case if not found in CODEOWNERS file
     for line in $(cat ./.github/CODEOWNERS); do
-      if echo $line| grep -i "$CODEOWNERS_MODULE_NAME"; then
+      if echo $line| grep -i -q "$CODEOWNERS_MODULE_NAME"; then
         owners=$(echo $line | cut -d "@" -f 2-|jq --raw-input 'split(" @")')
       fi
       echo " Creating GitHub issue for module $MODULE_NAME with assignees $owners"
