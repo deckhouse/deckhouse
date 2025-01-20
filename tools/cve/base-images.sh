e0b8caf9ba9ab9918093b24e2f5c5d15294a9a55
Nikolay Mordvintsev <nikolay.mordvintsev@flant.com>
2025-01-20 17:28:01 +0300

    fix wf and curl
    Signed-off-by: Nikolay Mordvintsev <nikolay.mordvintsev@flant.com>

--- tools/cve/base-images.sh
+++ tools/cve/base-images.sh
@@ -104,7 +104,9 @@ function __main__() {
       -F "product_name=Base Images" \
       -F "scan_date=${date_iso}" \
       -F "engagement_name=${image} CVE Report ${date_iso}" \
+      -F "engagement_end_date=${date_iso}" \
       -F "group_by=component_name+component_version" \
+      -F "deduplication_on_engagement=true" \
     > /dev/null
   done
 
