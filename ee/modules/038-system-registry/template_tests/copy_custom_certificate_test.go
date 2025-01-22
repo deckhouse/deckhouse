0de07d3d68b5404be5456666a6dc36815731e082
vadimartynov <vadim.martynov@flant.com>
2025-01-22 17:16:33 +0300

    Fixed template_tests
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

--- ee/modules/038-system-registry/template_tests/copy_custom_certificate_test.go
+++ ee/modules/038-system-registry/template_tests/copy_custom_certificate_test.go
@@ -25,7 +25,17 @@ discovery:
     system: 1
     master: 1
 `
-const customCertificatePresent = `
+const customCertificateModeWithDetachedMode = `
+mode: Detached
+https:
+  mode: CustomCertificate
+internal:
+  customCertificateData:
+    tls.crt: CRTCRTCRT
+    tls.key: KEYKEYKEY
+`
+
+const customCertificateModeWithNonDetachedMode = `
 https:
   mode: CustomCertificate
 internal:
@@ -37,19 +47,35 @@ internal:
 var _ = Describe("Module :: system-registry :: helm template :: custom-certificate", func() {
 	f := SetupHelmConfig(``)
 
-	Context("Default", func() {
+	Context("Detached mode", func() {
 		BeforeEach(func() {
 			f.ValuesSetFromYaml("global", globalValues)
 			f.ValuesSet("global.modulesImages", GetModulesImages())
-			f.ValuesSetFromYaml("systemRegistry", customCertificatePresent)
+			f.ValuesSetFromYaml("systemRegistry", customCertificateModeWithDetachedMode)
 			f.HelmRender()
 		})
 
-		It("Everything must render properly for default cluster", func() {
+		It("Non-empty customcertificate for detached mode", func() {
 			Expect(f.RenderError).ShouldNot(HaveOccurred())
 			createdSecret := f.KubernetesResource("Secret", "d8-system", "embedded-registry-ingress-tls-customcertificate")
 			Expect(createdSecret.Exists()).To(BeTrue())
 			Expect(createdSecret.Field("data").String()).To(Equal(`{"tls.crt":"CRTCRTCRT","tls.key":"KEYKEYKEY"}`))
+		})
+
+	})
+
+	Context("Non-detached mode", func() {
+		BeforeEach(func() {
+			f.ValuesSetFromYaml("global", globalValues)
+			f.ValuesSet("global.modulesImages", GetModulesImages())
+			f.ValuesSetFromYaml("systemRegistry", customCertificateModeWithNonDetachedMode)
+			f.HelmRender()
+		})
+
+		It("Empty customcertificate for non-detached mode", func() {
+			Expect(f.RenderError).ShouldNot(HaveOccurred())
+			createdSecret := f.KubernetesResource("Secret", "d8-system", "embedded-registry-ingress-tls-customcertificate")
+			Expect(createdSecret.Exists()).To(BeFalse())
 		})
 
 	})
