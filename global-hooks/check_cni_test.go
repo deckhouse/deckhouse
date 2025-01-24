bcc04faba4fe125d26d6035e0397de06b573dea3
AndreyPavlovFlant <139264830+AndreyPavlovFlant@users.noreply.github.com>
2025-01-24 14:18:20 +0500

    [core] Another attempt to replace the d8-cni configuration. (#10517)
    Signed-off-by: Andrey Pavlov <andrey.pavlov@flant.com>
    Signed-off-by: AndreyPavlovFlant <139264830+AndreyPavlovFlant@users.noreply.github.com>
    Signed-off-by: Nikolay Gorbatov <nikolay.gorbatov@flant.com>
    Co-authored-by: Andrey Polovov <andrey.polovov@flant.com>
    Co-authored-by: Nikolay Gorbatov <nikolay.gorbatov@flant.com>

---
+++ global-hooks/check_cni_test.go
@@ -0,0 +1,97 @@
+// Copyright 2021 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hooks
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/deckhouse/deckhouse/go_lib/dependency/requirements"
+	. "github.com/deckhouse/deckhouse/testing/hooks"
+)
+
+var _ = Describe("Global hooks :: check_cni ::", func() {
+
+	cniMC := func(name string) string {
+		return fmt.Sprintf(`
+---
+apiVersion: deckhouse.io/v1alpha1
+kind: ModuleConfig
+metadata:
+  name: %s
+spec:
+  enabled: true
+`, name)
+	}
+
+	f := HookExecutionConfigInit(`{"global": {"discovery": {}}}`, `{}`)
+	f.RegisterCRD("deckhouse.io", "v1alpha1", "ModuleConfig", false)
+
+	Context("Cluster has no deckhouse MC and cluster managed by other", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(""))
+			f.RunHook()
+		})
+
+		It("ExecuteSuccessfully", func() {
+			Expect(f).To(ExecuteSuccessfully())
+		})
+	})
+
+	Context("Cluster has no deckhouse MC and cluster managed by D8", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(""))
+			f.ValuesSetFromYaml("global.enabledModules", []byte(`["control-plane-manager"]`))
+			f.RunHook()
+		})
+
+		It("Throw the error", func() {
+			Expect(f).ToNot(ExecuteSuccessfully())
+			Expect(f.GoHookError.Error()).Should(ContainSubstring("the cluster is managed by D8, but there are no explicitly enabled CNI-modules"))
+			value, exists := requirements.GetValue(cniConfigurationSettledKey)
+			Expect(exists).To(BeTrue())
+			Expect(value).To(BeEquivalentTo("false"))
+		})
+	})
+
+	Context("Cluster has deckhouse MC with cni module enabled", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(cniMC("cni-cilium")))
+			f.RunHook()
+		})
+
+		It("ExecuteSuccessfully", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(string(f.LoggerOutput.Contents())).To(ContainSubstring("Enabled CNI from Deckhouse ModuleConfig: cni-cilium"))
+		})
+	})
+
+	Context("Cluster has a few CNI ModuleConfigs", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(cniMC("cni-cilium") + cniMC("cni-flannel")))
+			f.RunHook()
+		})
+
+		It("Throw the error", func() {
+			Expect(f).ToNot(ExecuteSuccessfully())
+			Expect(f.GoHookError.Error()).Should(ContainSubstring("more then one CNI enabled"))
+			value, exists := requirements.GetValue(cniConfigurationSettledKey)
+			Expect(exists).To(BeTrue())
+			Expect(value).To(BeEquivalentTo("false"))
+		})
+	})
+})
