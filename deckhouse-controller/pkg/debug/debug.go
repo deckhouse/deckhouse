64330da6e80a1979f8769442fc111110f03480ff
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2025-01-28 14:14:22 +0300

    item 27000
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- deckhouse-controller/pkg/debug/debug.go
+++ deckhouse-controller/pkg/debug/debug.go
@@ -135,38 +135,38 @@ func createTarball() *bytes.Buffer {
 		},
 		{
 			File: "mcm-logs.txt",
-			Cmd:  "kubectl",
-			Args: []string{"-n", "d8-cloud-instance-manager", "logs", "-l", "app=machine-controller-manager", "--tail", "3000", "-c", "controller"},
+			Cmd:  "bash",
+			Args: []string{"-c", `kubectl get modules node-manager -o json | jq -r '.items[] | select(.status.phase == "Ready") | "kubectl -n d8-cloud-instance-manager logs -l app=machine-controller-manager --tail=3000 -c controller"' | bash`},
 		},
 		{
 			File: "ccm-logs.txt",
 			Cmd:  "bash",
-			Args: []string{"-c", `kubectl get modules -o json | jq -r '.items[] | select(.properties.state == "Enabled" and (.metadata.name | test("^cloud-provider"))) | "kubectl -n d8-"+.metadata.name+" logs -l app=cloud-controller-manager --tail=3000"' | bash`},
+			Args: []string{"-c", `kubectl get modules -o json | jq -r '.items[] | select(.status.phase == "Ready" and (.metadata.name | test("^cloud-provider"))) | "kubectl -n d8-"+.metadata.name+" logs -l app=cloud-controller-manager --tail=3000"' | bash`},
 		},
 		{
 			File: "cluster-autoscaler-logs.txt",
 			Cmd:  "bash",
-			Args: []string{"-c", `kubectl get modules -o json | jq -r '.items[] | select(.properties.state == "Enabled" and .metadata.name == "node-manager") | "kubectl -n d8-cloud-instance-manager logs -l app=cluster-autoscaler --tail=3000 -c cluster-autoscaler"' | bash`},
+			Args: []string{"-c", `kubectl get modules node-manager -o json | jq -r '.items[] | select(.status.phase == "Ready") | "kubectl -n d8-cloud-instance-manager logs -l app=cluster-autoscaler --tail=3000 -c cluster-autoscaler"' | bash`},
 		},
 		{
 			File: "vpa-admission-controller-logs.txt",
 			Cmd:  "bash",
-			Args: []string{"-c", `kubectl get modules -o json | jq -r '.items[] | select(.properties.state == "Enabled" and .metadata.name == "vertical-pod-autoscaler") | "kubectl -n kube-system logs -l app=vpa-admission-controller --tail=3000 -c admission-controller"' | bash`},
+			Args: []string{"-c", `kubectl get modules vertical-pod-autoscaler -o json | jq -r '.items[] | select(.status.phase == "Ready") | "kubectl -n kube-system logs -l app=vpa-admission-controller --tail=3000 -c admission-controller"' | bash`},
 		},
 		{
 			File: "vpa-recommender-logs.txt",
 			Cmd:  "bash",
-			Args: []string{"-c", `kubectl get modules -o json | jq -r '.items[] | select(.properties.state == "Enabled" and .metadata.name == "vertical-pod-autoscaler") | "kubectl -n kube-system logs -l app=vpa-recommender --tail=3000 -c recommender"' | bash`},
+			Args: []string{"-c", `kubectl get modules vertical-pod-autoscaler -o json | jq -r '.items[] | select(.status.phase == "Ready") | "kubectl -n kube-system logs -l app=vpa-recommender --tail=3000 -c recommender"' | bash`},
 		},
 		{
 			File: "vpa-updater-logs.txt",
 			Cmd:  "bash",
-			Args: []string{"-c", `kubectl get modules -o json | jq -r '.items[] | select(.properties.state == "Enabled" and .metadata.name == "vertical-pod-autoscaler") | "kubectl -n kube-system logs -l app=vpa-updater --tail=3000 -c updater"' | bash`},
+			Args: []string{"-c", `kubectl get modules vertical-pod-autoscaler -o json | jq -r '.items[] | select(.status.phase == "Ready") | "kubectl -n kube-system logs -l app=vpa-updater --tail=3000 -c updater"' | bash`},
 		},
 		{
 			File: "prometheus-logs.txt",
 			Cmd:  "bash",
-			Args: []string{"-c", `kubectl get modules -o json | jq -r '.items[] | select(.properties.state == "Enabled" and .metadata.name == "prometheus") | "kubectl -n d8-monitoring logs -l prometheus=main --tail=3000 -c prometheus"' | bash`},
+			Args: []string{"-c", `kubectl get modules prometheus -o json | jq -r '.items[] | select(.status.phase == "Ready") | "kubectl -n d8-monitoring logs -l prometheus=main --tail=3000 -c prometheus"' | bash`},
 		},
 		{
 			File: "terraform-check.json",
