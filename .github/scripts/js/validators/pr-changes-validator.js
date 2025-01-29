f28926e6b17c28e5600cfa643949e274f7dcbf5a
Alexey Paschenko <alexey.paschenko@flant.com>
2025-01-29 18:42:54 +0300

    chore: Refactoring
    Signed-off-by: Alexey Paschenko <alexey.paschenko@flant.com>

--- .github/scripts/js/validators/pr-changes-validator.js
+++ .github/scripts/js/validators/pr-changes-validator.js
@@ -9,6 +9,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+// required yaml https://www.npmjs.com/package/yaml
 const YAML = require('yaml');
 
 /**
