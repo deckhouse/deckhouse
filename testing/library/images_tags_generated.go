54cb1ad8bc0d0a18e64aa458df8e9430f9afd204
vadimartynov <vadim.martynov@flant.com>
2025-02-04 17:52:05 +0300

    Make generate
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -462,6 +462,7 @@ var DefaultImagesDigests = map[string]interface{}{
 		"tomlMerge01":               "imageHash-registrypackages-tomlMerge01",
 		"virtWhat125":               "imageHash-registrypackages-virtWhat125",
 		"xfsprogs670":               "imageHash-registrypackages-xfsprogs670",
+		"yq4451":                    "imageHash-registrypackages-yq4451",
 	},
 	"runtimeAuditEngine": map[string]interface{}{
 		"falco":            "imageHash-runtimeAuditEngine-falco",
