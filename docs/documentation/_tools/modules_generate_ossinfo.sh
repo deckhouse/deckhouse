827316f952a5f2e5b843b9b255f6c1547d167237
mortelumina98 <ekaterina.matseevska@flant.com>
2025-02-04 17:07:27 +0300

    fix duplicate modules
    Signed-off-by: mortelumina98 <ekaterina.matseevska@flant.com>
    
    update
    
    Signed-off-by: mortelumina98 <ekaterina.matseevska@flant.com>
    
    update
    
    Signed-off-by: mortelumina98 <ekaterina.matseevska@flant.com>
    
    update
    
    Signed-off-by: mortelumina98 <ekaterina.matseevska@flant.com>

--- docs/documentation/_tools/modules_generate_ossinfo.sh
+++ docs/documentation/_tools/modules_generate_ossinfo.sh
@@ -17,8 +17,6 @@
 #
 # Copy files with information about the licenses used in modules to _data/ossinfo folder (jekyll will construct an array with this data)
 
-set -euo pipefail
-
 mkdir -p _data/ossinfo/
 
 > _data/ossinfo-cumulative.yaml
@@ -27,6 +25,7 @@ declare -A seen_names=()
 
 for path in $(find "$MODULES_DIR" -iname oss.yaml -print); do
   module_short_name=$(basename "$(dirname "$path")" | cut -d- -f2-)
+  module_full_name=$(echo "$path" | sed -E 's#.+/(.+/[^/]+)$#\1#' | cut -d\/ -f-1)
   cp -f "$path" "_data/ossinfo/${module_short_name}.yaml"
 
   while IFS= read -r line; do
