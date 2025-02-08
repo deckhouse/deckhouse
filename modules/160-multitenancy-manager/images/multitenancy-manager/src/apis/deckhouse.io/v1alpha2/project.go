aeaa2c081e97032536cf14e24caa0927e85e9d8a
Stepan Paksashvili <stepan.paksashvili@flant.com>
2025-02-08 15:20:34 +0300

    [multitenancy-manager] resource labels/annotations
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- modules/160-multitenancy-manager/images/multitenancy-manager/src/apis/deckhouse.io/v1alpha2/project.go
+++ modules/160-multitenancy-manager/images/multitenancy-manager/src/apis/deckhouse.io/v1alpha2/project.go
@@ -116,6 +116,12 @@ type ProjectSpec struct {
 	// Description of the Project
 	Description string `json:"description,omitempty"`
 
+	// Labels that will be set for all project resources
+	ResourceLabels map[string]string `json:"resourceLabels,omitempty"`
+
+	// Annotations that will be set for all project resources
+	ResourceAnnotations map[string]string `json:"resourceAnnotations,omitempty"`
+
 	// Name of ProjectTemplate to use to create Project
 	ProjectTemplateName string `json:"projectTemplateName,omitempty"`
 
@@ -135,11 +141,23 @@ func (p *ProjectSpec) DeepCopy() *ProjectSpec {
 }
 func (p *ProjectSpec) DeepCopyInto(newObj *ProjectSpec) {
 	*newObj = *p
-	newObj.Description = p.Description
-	newObj.ProjectTemplateName = p.ProjectTemplateName
 	newObj.Parameters = make(map[string]interface{})
 	for key, value := range p.Parameters {
 		newObj.Parameters[key] = value
+	}
+	if p.ResourceLabels != nil {
+		in, out := &p.ResourceLabels, &newObj.ResourceLabels
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	if p.ResourceAnnotations != nil {
+		in, out := &p.ResourceAnnotations, &newObj.ResourceAnnotations
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
 	}
 }
 
