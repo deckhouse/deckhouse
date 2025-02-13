44bddd3c13525eeffd0d240363fe167f66fe2825
Stepan Paksashvili <stepan.paksashvili@flant.com>
2025-02-13 15:43:31 +0300

    [deckhouse] trigger the module config when a module created
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/source/helpers.go
+++ deckhouse-controller/pkg/controller/module-controllers/source/helpers.go
@@ -275,10 +275,12 @@ func (r *reconciler) ensureModule(ctx context.Context, sourceName, moduleName, r
 
 	err := ctrlutils.UpdateStatusWithRetry(ctx, r.client, module, func() error {
 		// init just created downloaded modules
-		module.Status.Phase = v1alpha1.ModulePhaseAvailable
-		module.SetConditionFalse(v1alpha1.ModuleConditionEnabledByModuleConfig, v1alpha1.ModuleReasonDisabled, v1alpha1.ModuleMessageDisabled)
-		module.SetConditionFalse(v1alpha1.ModuleConditionEnabledByModuleManager, "", "")
-		module.SetConditionFalse(v1alpha1.ModuleConditionIsReady, v1alpha1.ModuleReasonNotInstalled, v1alpha1.ModuleMessageNotInstalled)
+		if module.Status.Phase == "" {
+			module.Status.Phase = v1alpha1.ModulePhaseAvailable
+			module.SetConditionFalse(v1alpha1.ModuleConditionEnabledByModuleConfig, v1alpha1.ModuleReasonDisabled, v1alpha1.ModuleMessageDisabled)
+			module.SetConditionFalse(v1alpha1.ModuleConditionEnabledByModuleManager, "", "")
+			module.SetConditionFalse(v1alpha1.ModuleConditionIsReady, v1alpha1.ModuleReasonNotInstalled, v1alpha1.ModuleMessageNotInstalled)
+		}
 
 		return nil
 	})
