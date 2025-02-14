84ee13dd8040925a556ea926291af607039d1285
Polina Polyntseva <pvsskk@mail.ru>
2025-02-14 10:25:39 +0300

    add a modal window to the contact button
    Signed-off-by: Polina Polyntseva <pvsskk@mail.ru>

--- docs/site/_assets/js/popup-form.js
+++ docs/site/_assets/js/popup-form.js
@@ -14,6 +14,41 @@ document.addEventListener("DOMContentLoaded", function() {
       this.form.addEventListener('submit', this.submitForm.bind(this));
       this.closeBtn.addEventListener('click', this.closeModal.bind(this));
       this.closeBg.addEventListener('click', this.closeModal.bind(this));
+
+      this.preferredContact = document.querySelector('input[name="preferred_contact"]');
+      this.telegramInput = document.querySelector('input[name="telegram_id"]');
+      this.telegramCheckbox = document.querySelector('input[value="telegram"]');
+      this.updateContactValue();
+      this.initializeCheckbox();
+      this.toggleTelegramInput();
+      this.telegramCheckbox.addEventListener('change', this.toggleTelegramInput.bind(this));
+    }
+
+    initializeCheckbox() {
+      const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+      checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', this.updateContactValue.bind(this));
+      });
+    }
+
+    updateContactValue() {
+      let selectedContacts = [];
+      const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+      checkboxes.forEach(checkbox => {
+        if(checkbox.checked) {
+          selectedContacts.push(checkbox.value);
+        }
+      });
+      this.preferredContact.value = selectedContacts.join(',');
+    }
+
+    toggleTelegramInput() {
+      if(this.telegramCheckbox.checked) {
+        this.telegramInput.style.display = 'block';
+      } else {
+        this.telegramInput.style.display = 'none';
+        this.telegramInput.value = '';
+      }
     }
 
     initializeOpenModalButton() {
@@ -26,14 +61,37 @@ document.addEventListener("DOMContentLoaded", function() {
     submitForm(e) {
       e.preventDefault();
 
-      PostData(this.url, this.serializeData()).then(res => {
-        if (res.ok) {
-          this.downloadFile();
-          this.successSubmit();
-        } else {
-          this.errorSubmit();
+      const FormData = this.serializeData();
+      console.log(FormData)
+
+      const bitrixFields = {
+        fields: {
+          'TITLE': 'с сайта документации Deckhouse',
+          'NAME': FormData.name,
+          'EMAIL': FormData.email,
+          'PHONE': FormData.phone,
+          'COMPANY': FormData.company,
+          'POST': FormData.position,
+          'COMMENTS': 'Предпочтительный вид связи: ' + FormData.preferred_contact,
         }
-      });
+      }
+
+      if(this.telegramCheckbox.checked && this.telegramInput.value) {
+        bitrixFields.fields.COMMENTS += '. Telegram ID: ' + this.telegramInput.value;
+      }
+
+      BX24.callMethod(
+        "crm.lead.add",
+        bitrixFields,
+        res => {
+          if (res.ok) {
+            this.downloadFile();
+            this.successSubmit();
+          } else {
+            this.errorSubmit();
+          }
+        }
+      )
     }
 
     serializeData() {
@@ -98,16 +156,4 @@ document.addEventListener("DOMContentLoaded", function() {
   wrapper.forEach(item => {
     new PopupForm(item);
   })
-
-  async function PostData(url, data) {
-    const res = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        Accept: "application/json",
-      },
-      body: JSON.stringify(data)
-    })
-    return res
-  }
 })
