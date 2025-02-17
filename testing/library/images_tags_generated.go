c5b44db8abac1c7e93985cf53da16fa198db119e
Dmitriy Andreychenko <dmitriy.andreychenko@flant.com>
2025-02-17 14:40:37 +0300

    fix: add multus-controller to image tags
    Signed-off-by: Dmitriy Andreychenko <dmitriy.andreychenko@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -137,6 +137,7 @@ var DefaultImagesDigests = map[string]interface{}{
 		"egressGatewayAgent": "imageHash-cniCilium-egressGatewayAgent",
 		"kubeRbacProxy":      "imageHash-cniCilium-kubeRbacProxy",
 		"multusCni":          "imageHash-cniCilium-multusCni",
+		"multusController":   "imageHash-cniCilium-multusController",
 		"operator":           "imageHash-cniCilium-operator",
 		"safeAgentUpdater":   "imageHash-cniCilium-safeAgentUpdater",
 	},
