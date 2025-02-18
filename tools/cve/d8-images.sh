7109d9740e346b72076e2d67a643ddd6b95cf87b
Nikolay Mordvintsev <nikolay.mordvintsev@flant.com>
2025-02-18 19:23:20 +0300

    fix
    Signed-off-by: Nikolay Mordvintsev <nikolay.mordvintsev@flant.com>

--- tools/cve/d8-images.sh
+++ tools/cve/d8-images.sh
@@ -53,10 +53,11 @@ function __main__() {
                 "dev-registry.deckhouse.io/sys/deckhouse-oss/install"
                 "dev-registry.deckhouse.io/sys/deckhouse-oss/install-standalone"
                 )
+  digests=$(cat digest.json)
   for additional_image in "${additional_images[@]}"; do
     additional_image_name=$(echo "$additional_image" | grep -o '[^/]*$')
-    additional_image_sha=$(docker manifest inspect $additional_image:$TAG | jq -r .config.digest)
-    digests=$(echo "$digests"|jq --arg i "$additional_image_name" --arg s "$additional_image_sha" '.deckhouse += {$i: $s}')
+    additional_image_sha=$(docker manifest inspect $additional_image:release-1.68 | jq -r .config.digest)
+    digests=$(echo "$digests"|jq --arg i "$additional_image_name" --arg s "$additional_image_sha" '.deckhouse += {($i): ($s)}')
   done
 
   IMAGE_REPORT_NAME="deckhouse::$(echo "$IMAGE:$TAG" | sed 's/^.*\/\(.*\)/\1/')"
