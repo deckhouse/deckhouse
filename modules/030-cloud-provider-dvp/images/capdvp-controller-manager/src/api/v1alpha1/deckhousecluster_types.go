11ea3a04c322df17f1f8b6c31526514bfa43deac
Maxim Vasilenko <maksim.vasilenko@flant.com>
2025-02-20 13:04:56 +0300

    [cloud-provider-dvp] Scaffold basic provider
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>

---
+++ modules/030-cloud-provider-dvp/images/capdvp-controller-manager/src/api/v1alpha1/deckhousecluster_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DeckhouseClusterSpec defines the desired state of DeckhouseCluster.
+type DeckhouseClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of DeckhouseCluster. Edit deckhousecluster_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// DeckhouseClusterStatus defines the observed state of DeckhouseCluster.
+type DeckhouseClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// DeckhouseCluster is the Schema for the deckhouseclusters API.
+type DeckhouseCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DeckhouseClusterSpec   `json:"spec,omitempty"`
+	Status DeckhouseClusterStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// DeckhouseClusterList contains a list of DeckhouseCluster.
+type DeckhouseClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DeckhouseCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DeckhouseCluster{}, &DeckhouseClusterList{})
+}
