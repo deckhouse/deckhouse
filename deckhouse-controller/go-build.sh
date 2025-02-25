d0e7a89fa56fee8d47fc961c8d62649fd0604409
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2025-02-25 12:13:50 +0300

    item 15823
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- deckhouse-controller/go-build.sh
+++ deckhouse-controller/go-build.sh
@@ -24,6 +24,6 @@ addonOpVer=$(go list -m all | grep addon-operator | cut -d' ' -f 2-)
 GOOS=linux CGO_LDFLAGS=-v \
     go build \
      -gcflags "all=-N -l" \
-     -ldflags="-s -w -X 'main.DeckhouseVersion=$deckhouseVer' -X 'main.AddonOperatorVersion=$addonOpVer' -X 'main.ShellOperatorVersion=$shellOpVer'" \
+     -ldflags="-X 'main.DeckhouseVersion=$deckhouseVer' -X 'main.AddonOperatorVersion=$addonOpVer' -X 'main.ShellOperatorVersion=$shellOpVer'" \
      -o ./deckhouse-controller \
      ./cmd/deckhouse-controller
