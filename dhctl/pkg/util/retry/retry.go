a54ca0d947f2843d8277c39540954cc1f1bdb67a
alexey-gavrilov-flant <53515419+alexey-gavrilov-flant@users.noreply.github.com>
2025-02-26 12:58:26 +0300

    [dhctl] Improved logging and operation when performing converge (#11926)
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- dhctl/pkg/util/retry/retry.go
+++ dhctl/pkg/util/retry/retry.go
@@ -15,9 +15,9 @@
 package retry
 
 import (
-	"context"
 	"errors"
 	"fmt"
+	"math/rand"
 	"time"
 
 	"github.com/deckhouse/deckhouse/dhctl/pkg/log"
@@ -54,7 +54,7 @@ type Loop struct {
 	logger           log.Logger
 	interruptable    bool
 	showError        bool
-	ctx              context.Context
+	prefix           string
 }
 
 // NewLoop create Loop with features:
@@ -83,6 +83,7 @@ func NewSilentLoop(name string, attemptsQuantity int, wait time.Duration) *Loop 
 		// - this loop is not interruptable by the signal watcher in tomb package.
 		interruptable: false,
 		showError:     true,
+		prefix:        fmt.Sprintf("[%s][%d] ", name, rand.Int()),
 	}
 }
 
@@ -93,11 +94,6 @@ func (l *Loop) BreakIf(pred BreakPredicate) *Loop {
 
 func (l *Loop) WithInterruptable(flag bool) *Loop {
 	l.interruptable = flag
-	return l
-}
-
-func (l *Loop) WithContext(ctx context.Context) *Loop {
-	l.ctx = ctx
 	return l
 }
 
@@ -126,33 +122,25 @@ func (l *Loop) Run(task func() error) error {
 			// Run task and return if everything is ok.
 			err = task()
 			if err == nil {
-				l.logger.LogSuccess("Succeeded!\n")
+				l.logger.LogSuccess(l.prefix + "Succeeded!\n")
 				return nil
 			}
 
 			if l.breakPredicate != nil && l.breakPredicate(err) {
-				l.logger.LogDebugF("Client break loop with %v\n", err)
+				l.logger.LogDebugF(l.prefix+"Client break loop with %v\n", err)
 				return err
 			}
 
-			l.logger.LogFailRetry(fmt.Sprintf(attemptMessage, i, l.attemptsQuantity, l.name, l.waitTime))
+			l.logger.LogFailRetry(fmt.Sprintf(l.prefix+attemptMessage, i, l.attemptsQuantity, l.name, l.waitTime))
 			errorMsg := "\t%v\n\n"
 			if l.showError {
 				errorMsg = "\tError: %v\n\n"
 			}
-			l.logger.LogInfoF(errorMsg, err)
+			l.logger.LogInfoF(l.prefix+errorMsg, err)
 
 			// Do not waitTime after the last iteration.
 			if i < l.attemptsQuantity {
-				if l.ctx != nil {
-					select {
-					case <-l.ctx.Done():
-						return fmt.Errorf("timeout while %q: last error: %v", l.name, l.ctx.Err())
-					case <-time.After(l.waitTime):
-					}
-				} else {
-					time.Sleep(l.waitTime)
-				}
+				time.Sleep(l.waitTime)
 			}
 		}
 
