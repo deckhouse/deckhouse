9aad6d9b12f76eeef4ba018a5cae51ffa42bfa12
Maxim Vasilenko <maksim.vasilenko@flant.com>
2025-02-26 14:11:40 +0300

    [cloud-provider-dvp] DeckhouseMachine & DeckhouseCluster CRD's, updated DVPInstanceClass
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>

--- modules/030-cloud-provider-dvp/images/capdvp-controller-manager/src/api/v1alpha1/deckhousemachine_types.go
+++ modules/030-cloud-provider-dvp/images/capdvp-controller-manager/src/api/v1alpha1/deckhousemachine_types.go
@@ -18,24 +18,84 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clusterv1b1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// CPU defines the VM CPU, made of variable number of cores, each getting the Fraction amount of processing time on a physical core.
+type CPU struct {
+	// Cores is the number of cores per socket.
+	// +kubebuilder:default=4
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=1024
+	Cores int `json:"cores"`
+
+	// Fraction is a guaranteed share of CPU time that will be allocated to the VM.
+	// Expressed as percentage (0-100]
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=100
+	// +kubebuilder:default=100
+	Fraction int `json:"cpuFraction"`
+}
+
+type VMAddress struct {
+	Type    clusterv1b1.MachineAddressType `json:"type"`
+	Address string                         `json:"address"`
+}
 
 // DeckhouseMachineSpec defines the desired state of DeckhouseMachine.
 type DeckhouseMachineSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of DeckhouseMachine. Edit deckhousemachine_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Name is the hostname and unique identifier of this VM within DVP.
+	Name string `json:"name"`
+
+	// ProviderID is the UUID of the VM, prefixed with 'dvp://' proto.
+	// +optional
+	ProviderID string `json:"providerID,omitempty"`
+
+	// VMClassName defines the name of the VirtualMachineClass resource describing requirements
+	// for a virtual CPU, memory, and the resource allocation policy of this machine.
+	VMClassName string `json:"vmClassName"`
+
+	// CPU holds number of cores and processing time allocated to them.
+	CPU CPU `json:"cpu"`
+
+	// Memory is this machine's RAM amount in megabytes.
+	// +kubebuilder:validation:Minimum=8
+	// +kubebuilder:default=8192
+	Memory int `json:"memory"`
+
+	// RootDiskSize size of the bootable disk in GiB.
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:default=30
+	RootDiskSize int64 `json:"rootDiskSize"`
 }
 
 // DeckhouseMachineStatus defines the observed state of DeckhouseMachine.
 type DeckhouseMachineStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Ready indicates the VM has been provisioned and is ready.
+	// +optional
+	Ready bool `json:"ready"`
+
+	// Addresses holds a list of the host names, external IP addresses, internal IP addresses, external DNS names, and/or internal DNS names for the VM.
+	// +optional
+	Addresses []VMAddress `json:"addresses,omitempty"`
+
+	// FailureReason will contain an error type if something goes wrong during Machine lifecycle.
+	// +optional
+	FailureReason *string `json:"failureReason,omitempty"`
+
+	// FailureMessage will describe an error if something goes wrong during Machine lifecycle.
+	// +optional
+	FailureMessage *string `json:"failureMessage,omitempty"`
+
+	// Conditions defines current service state of the ZvirtMachine.
+	// +optional
+	Conditions clusterv1b1.Conditions `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
--- modules/030-cloud-provider-dvp/images/capdvp-controller-manager/src/go.mod
+++ modules/030-cloud-provider-dvp/images/capdvp-controller-manager/src/go.mod
@@ -2,11 +2,14 @@ module cluster-api-provider-dvp
 
 go 1.23.0
 
+replace dvp-common => ../../dvp-common
+
 require (
-	github.com/onsi/ginkgo/v2 v2.21.0
-	github.com/onsi/gomega v1.35.1
+	github.com/onsi/ginkgo/v2 v2.22.0
+	github.com/onsi/gomega v1.36.0
 	k8s.io/apimachinery v0.32.0
 	k8s.io/client-go v0.32.0
+	sigs.k8s.io/cluster-api v1.9.5
 	sigs.k8s.io/controller-runtime v0.20.0
 )
 
@@ -19,7 +22,7 @@ require (
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/emicklei/go-restful/v3 v3.12.1 // indirect
 	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
@@ -68,18 +71,18 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
-	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/oauth2 v0.23.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/term v0.25.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/oauth2 v0.24.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/term v0.27.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.7.0 // indirect
 	golang.org/x/tools v0.26.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240826202546-f6391c0de4c7 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240826202546-f6391c0de4c7 // indirect
-	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/grpc v1.65.1 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
