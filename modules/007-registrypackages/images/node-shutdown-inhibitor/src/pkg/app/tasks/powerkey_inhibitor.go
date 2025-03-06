5d941a76ef93ff2d3e4c0c156c902b28f66ed937
Ivan Mikheykin <ivan.mikheykin@flant.com>
2025-03-06 19:57:11 +0300

    ++ add node-shutdown-inhibitor component, fix wrapper to run shutdown command.
    Signed-off-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

---
+++ modules/007-registrypackages/images/node-shutdown-inhibitor/src/pkg/app/tasks/powerkey_inhibitor.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tasks
+
+import (
+	"context"
+	"fmt"
+
+	"graceful_shutdown/pkg/systemd"
+)
+
+type PowerKeyInhibitor struct {
+	UnlockInhibitorsCh <-chan struct{}
+	dbusCon            *systemd.DBusCon
+	inhibitLock        systemd.InhibitLock
+}
+
+func (p *PowerKeyInhibitor) Run(ctx context.Context, errCh chan error) {
+	err := p.acquireLock()
+	if err != nil {
+		errCh <- fmt.Errorf("powerKeyInhibitor acquireLock: %w", err)
+		return
+	}
+
+	if p.inhibitLock == 0 {
+		errCh <- fmt.Errorf("powerKeyInhibitor: lock not acquired")
+		return
+	}
+	fmt.Printf("powerKeyInhibitor: got lock\n")
+
+	select {
+	case <-ctx.Done():
+		fmt.Printf("powerKeyInhibitor: unlock on global exit\n")
+	case <-p.UnlockInhibitorsCh:
+		fmt.Printf("powerKeyInhibitor: unlock on meeting shutdown requirements.\n")
+	}
+
+	err = p.dbusCon.ReleaseInhibitLock(p.inhibitLock)
+	if err != nil {
+		fmt.Printf("powerKeyInhibitor: unlock error: %v\n", err)
+		return
+	}
+	fmt.Printf("powerKeyInhibitor: unlocked\n")
+}
+
+func (p *PowerKeyInhibitor) acquireLock() error {
+	systemBus, err := systemd.NewDBusCon()
+	if err != nil {
+		return fmt.Errorf("initiate DBus connection: %v", err)
+	}
+	p.dbusCon = systemBus
+
+	lock, err := p.dbusCon.InhibitPowerKey()
+	if err != nil {
+		return err
+	}
+	if p.inhibitLock != 0 {
+		p.dbusCon.ReleaseInhibitLock(p.inhibitLock)
+	}
+	p.inhibitLock = lock
+	return nil
+}
--- modules/007-registrypackages/images/systemctl-power-commands-wrapper/src/wrapper.h
+++ modules/007-registrypackages/images/systemctl-power-commands-wrapper/src/wrapper.h
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #ifndef WRAPPER_H
 #define WRAPPER_H
 
@@ -21,24 +37,24 @@ static const char DRY_RUN[] = "--dry-run";
 
 static const char SYSTEMCTL[] = "systemctl";
 
-static const char REBOOT[] = "reboot";
-static const char POWEROFF[] = "poweroff";
-static const char SHUTDOWN[] = "shutdown";
-static const char HALT[] = "halt";
+static const char ACTION_HALT[] = "halt";
+static const char ACTION_REBOOT[] = "reboot";
+static const char ACTION_POWEROFF[] = "poweroff";
+static const char ACTION_SHUTDOWN[] = "shutdown";
 
-static const char* KNOWN_COMMANDS[] = {
-    REBOOT,
-    POWEROFF,
-    SHUTDOWN,
-    HALT,
+static const char* KNOWN_ACTIONS[] = {
+    ACTION_HALT,
+    ACTION_REBOOT,
+    ACTION_POWEROFF,
+    ACTION_SHUTDOWN,
 };
-static const int KNOWN_COMMANDS_COUNT = (int)(sizeof(KNOWN_COMMANDS)/sizeof(KNOWN_COMMANDS[0]));
+static const int KNOWN_ACTIONS_COUNT = (int)(sizeof(KNOWN_ACTIONS)/sizeof(KNOWN_ACTIONS[0]));
 
 static const char CHECK_INHIBITORS_FLAG[] = "--check-inhibitors=yes";
 
 
 
-int detect_command(char const* argv0, char const **command_name);
+int detect_action(char const* argv0, char const **action_name);
 
 int detect_dry_run(int argc, char ** argv);
 
