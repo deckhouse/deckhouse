1d5859841b4b4951fe675a2d1bd342f99661da19
Ivan Mikheykin <ivan.mikheykin@flant.com>
2025-03-06 19:57:11 +0300

    ++ add node-shutdown-inhibitor component, fix wrapper to run shutdown command.
    Signed-off-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

---
+++ modules/007-registrypackages/images/node-shutdown-inhibitor/src/pkg/app/start.go
@@ -0,0 +1,252 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"time"
+
+	"graceful_shutdown/pkg/app/containerd"
+	"graceful_shutdown/pkg/app/tasks"
+	"graceful_shutdown/pkg/systemd"
+	"graceful_shutdown/pkg/taskstarter"
+)
+
+type App struct {
+	InhibitDelayMax time.Duration
+	PodLabel        string
+	taskStarter     *taskstarter.Starter
+}
+
+func NewApp(maxDelay time.Duration, podLabel string) *App {
+	return &App{
+		InhibitDelayMax: maxDelay,
+		PodLabel:        podLabel,
+	}
+}
+
+func (a *App) Start() error {
+	err := a.overrideInhibitDelayMax()
+	if err != nil {
+		return err
+	}
+
+	tasks := a.wireAppTasks()
+	a.taskStarter = taskstarter.NewStarter(tasks...)
+
+	go a.taskStarter.Start(context.Background())
+
+	go func() {
+		<-a.taskStarter.Done()
+		a.Stop()
+	}()
+
+	return nil
+}
+
+func (a *App) Stop() {
+	fmt.Printf("Stop app...\n")
+	a.taskStarter.Stop()
+}
+
+func (a *App) Done() <-chan struct{} {
+	return a.taskStarter.Done()
+}
+
+func (a *App) Err() error {
+	return a.taskStarter.Err()
+}
+
+// CheckPods list all pods for testing purposes.
+func (a *App) CheckPods() {
+	//podMatcher := containerd.ByLabel(a.PodLabel)
+	podList, err := containerd.ListPods(context.Background())
+	if err != nil {
+		fmt.Printf("List pods error: %v\n", err)
+		return
+	}
+
+	sort.SliceStable(podList.Items, func(i, j int) bool {
+		return podList.Items[i].Metadata.Name < podList.Items[j].Metadata.Name
+	})
+
+	fmt.Printf("Pods with label %s:\n", a.PodLabel)
+	matched := containerd.FilterPods(podList.Items, containerd.WithLabel(a.PodLabel), containerd.WithReadyState())
+	for _, pod := range matched {
+		fmt.Printf("  %s\n", pod.Metadata.Name)
+	}
+
+	//fmt.Printf("Other pods:\n")
+	//for _, pod := range podList.Items {
+	//	if !podMatcher(pod) {
+	//		fmt.Printf("  %s\n", pod.Metadata.Name)
+	//	}
+	//}
+}
+
+func (a *App) overrideInhibitDelayMax() error {
+	dbusCon, err := systemd.NewDBusCon()
+	if err != nil {
+		return fmt.Errorf("initiate DBus connection: %v", err)
+	}
+
+	currentInhibitDelay, err := dbusCon.CurrentInhibitDelay()
+	if err != nil {
+		return fmt.Errorf("get current inihibit delay: %v", err)
+	}
+
+	/**
+
+	currentInhibitDelay, err := m.dbusCon.CurrentInhibitDelay()
+	if err != nil {
+		return nil, err
+	}
+
+	// If the logind's InhibitDelayMaxUSec as configured in (logind.conf) is less than periodRequested, attempt to update the value to periodRequested.
+	if periodRequested := m.periodRequested(); periodRequested > currentInhibitDelay {
+		err := m.dbusCon.OverrideInhibitDelay(periodRequested)
+		if err != nil {
+			return nil, fmt.Errorf("unable to override inhibit delay by shutdown manager: %v", err)
+		}
+
+		err = m.dbusCon.ReloadLogindConf()
+		if err != nil {
+			return nil, err
+		}
+
+		// Read the current inhibitDelay again, if the override was successful, currentInhibitDelay will be equal to shutdownGracePeriodRequested.
+		updatedInhibitDelay, err := m.dbusCon.CurrentInhibitDelay()
+		if err != nil {
+			return nil, err
+		}
+
+		if periodRequested > updatedInhibitDelay {
+			return nil, fmt.Errorf("node shutdown manager was unable to update logind InhibitDelayMaxSec to %v (ShutdownGracePeriod), current value of InhibitDelayMaxSec (%v) is less than requested ShutdownGracePeriod", periodRequested, updatedInhibitDelay)
+		}
+	}
+
+	*/
+
+	if currentInhibitDelay >= a.InhibitDelayMax {
+		fmt.Printf("overrideInhibitDelayMax: current inhibit delay is already greater or equal to requested: %s >= %s\n", currentInhibitDelay.Truncate(time.Second).String(), a.InhibitDelayMax.Truncate(time.Second).String())
+		return nil
+	}
+
+	fmt.Printf("overrideInhibitDelayMax: current inhibit delay: %s, override to %s\n", currentInhibitDelay.Truncate(time.Second).String(), a.InhibitDelayMax.Truncate(time.Second).String())
+
+	err = dbusCon.OverrideInhibitDelay(a.InhibitDelayMax)
+	if err != nil {
+		return fmt.Errorf("overrideInhibitDelayMax: unable to override: %v", err)
+	}
+
+	err = dbusCon.ReloadLogindConf()
+	if err != nil {
+		return fmt.Errorf("overrideInhibitDelayMax: unable to reload systemd conf: %v", err)
+	}
+
+	// Getting current delay without waiting for reload is not reliable and gives old value.
+	// Comment it for now until a better solution.
+	//
+	currentInhibitDelay, err = dbusCon.CurrentInhibitDelay()
+	if err != nil {
+		return fmt.Errorf("get current inhibit delay after override: %v", err)
+	}
+
+	if currentInhibitDelay < a.InhibitDelayMax {
+		return fmt.Errorf("overrideInhibitDelayMax: unable to override inhibit delay to %s, current value of InhibitDelayMaxSec (%v) is less than requested", a.InhibitDelayMax.Truncate(time.Second).String(), currentInhibitDelay.Truncate(time.Second).String())
+	}
+
+	fmt.Printf("overrideInhibitDelayMax: overridden inhibit delay: %s\n", currentInhibitDelay.Truncate(time.Second).String())
+	return nil
+}
+
+func (a *App) wireAppTasks() []taskstarter.Task {
+	// Create channels for events.
+	// Event on receiving ShutdownPrepareSignal.
+	shutdownSignalCh := make(chan struct{})
+	// Event from PowerKey observer.
+	//powerKeyPressCh := make(chan struct{})
+	// Event to unlock all inhibitors when shutdown requirements are met.
+	unlockInhibitorsCh := make(chan struct{})
+
+	return []taskstarter.Task{
+		//&tasks.ShutdownBlockInhibitor{
+		//	UnlockInhibitorsCh: unlockInhibitorsCh,
+		//},
+		&tasks.ShutdownInhibitor{
+			ShutdownSignalCh:   shutdownSignalCh,
+			UnlockInhibitorsCh: unlockInhibitorsCh,
+		},
+		&tasks.PowerKeyInhibitor{
+			UnlockInhibitorsCh: unlockInhibitorsCh,
+		},
+		&tasks.PowerKeyEvent{
+			//PowerKeyPressedCh:  powerKeyPressCh,
+			UnlockInhibitorsCh: unlockInhibitorsCh,
+		},
+		&tasks.PodObserver{
+			CheckInterval:    5 * time.Second,
+			ShutdownSignalCh: shutdownSignalCh,
+			//PowerKeyPressedCh: powerKeyPressCh,
+			StopInhibitorsCh: unlockInhibitorsCh,
+			PodMatchers: []containerd.PodMatcher{
+				containerd.WithLabel(a.PodLabel),
+				containerd.WithReadyState(),
+			},
+		},
+		&tasks.StatusReporter{
+			UnlockInhibitorsCh: unlockInhibitorsCh,
+		},
+	}
+}
+
+func someTask(ctx context.Context, id string) {
+	tt := time.NewTicker(1 * time.Second)
+	ttt := 120
+	for {
+		select {
+		case <-tt.C:
+			fmt.Printf("ticker %s: %d\n", id, ttt)
+			ttt--
+			if ttt == 0 {
+				fmt.Printf("ticker %s done: %d\n", id, ttt)
+				return
+			}
+		case <-ctx.Done():
+			tt.Stop()
+			fmt.Printf("Got cancel. Ticker %s stopped. Will exit in 5 sec.\n", id)
+			time.Sleep(5 * time.Second)
+			return
+		}
+	}
+}
+
+//dbusConn, err := systemd.NewDBusCon()
+//if err != nil {
+//	fmt.Printf("open Dbus connection: %v\n", err)
+//	os.Exit(1)
+//}
+//inhibitDelay, err := dbusConn.CurrentInhibitDelay()
+//if err != nil {
+//	fmt.Printf("get inhibit delay: %v\n", err)
+//	os.Exit(1)
+//}
+//
+//fmt.Printf("Inhibit delay: %s\n", inhibitDelay.Truncate(time.Second).String())
+//
---
+++ modules/007-registrypackages/images/node-shutdown-inhibitor/src/pkg/inputdev/watcher.go
@@ -0,0 +1,162 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package inputdev
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+type Watcher struct {
+	devs      []Device
+	buttons   []Button
+	stopCh    chan struct{}
+	pressedCh chan struct{}
+}
+
+func NewWatcher(devs []Device, buttons ...Button) *Watcher {
+	return &Watcher{
+		devs:      devs,
+		buttons:   buttons,
+		stopCh:    make(chan struct{}),
+		pressedCh: make(chan struct{}),
+	}
+}
+
+func (w *Watcher) Start() {
+	go w.watch()
+}
+
+func (w *Watcher) Pressed() <-chan struct{} {
+	return w.pressedCh
+}
+
+func (w *Watcher) Stop() {
+	close(w.stopCh)
+}
+
+func (w *Watcher) watch() {
+	evCh := make(chan *inputEvent)
+	go w.readEvents(evCh)
+	for {
+		select {
+		case event := <-evCh:
+			if event.isAnyButtonPressed(w.buttons) {
+				w.pressedCh <- struct{}{}
+			}
+		case <-w.stopCh:
+			return
+		}
+	}
+}
+
+func (w *Watcher) readEvents(evCh chan *inputEvent) {
+	fds := []int{}
+
+	// Open each device.
+	for _, dev := range w.devs {
+		fd, err := syscall.Open(dev.DevPath, syscall.O_RDONLY, 0)
+		if err != nil {
+			continue
+		}
+		defer syscall.Close(fd)
+		fds = append(fds, fd)
+		//devices[fd] = devicePath
+	}
+
+	// Create FdSet bitmask
+	fdSet := syscall.FdSet{}
+	// Max fd to check for Select.
+	fdMax := fds[len(fds)-1] + 1
+
+	// Бесконечный цикл чтения событий
+	for {
+		// Return if watcher was stopped.
+		select {
+		case <-w.stopCh:
+			return
+		default:
+		}
+
+		InitFdSet(&fdSet, fds...)
+		// Wait when read is available on any of the fds.
+		// TODO add timeout to check stopCh more frequently?
+		_, err := syscall.Select(fdMax, &fdSet, nil, nil, nil)
+		if err != nil {
+			fmt.Printf("select: %v\n", err)
+			continue
+		}
+
+		// Check if fd is set and read event.
+		for _, fd := range fds {
+			if !FD_ISSET(fd, &fdSet) {
+				continue
+			}
+
+			event, err := readEvent(fd)
+			if err != nil {
+				fmt.Printf("readEvent: %v\n", err)
+				continue
+			}
+
+			evCh <- event
+		}
+	}
+}
+
+type inputEvent struct {
+	Time  [16]byte
+	Type  uint16
+	Code  uint16
+	Value int32
+}
+
+func (ev *inputEvent) isAnyButtonPressed(buttons []Button) bool {
+	if ev.Type == uint16(EV_KEY) && ev.Value == 1 {
+		for _, button := range buttons {
+			if ev.Code == uint16(button) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func readEvent(fd int) (*inputEvent, error) {
+	var event inputEvent
+	err := binaryRead(fd, unsafe.Pointer(&event), unsafe.Sizeof(event))
+	if err != nil {
+		return nil, fmt.Errorf("read event error: %v\n", err)
+	}
+
+	return &event, nil
+}
+
+func binaryRead(fd int, data unsafe.Pointer, size uintptr) error {
+	buf := make([]byte, size)
+	n, err := syscall.Read(fd, buf)
+	if err != nil {
+		return err
+	}
+	if n != int(size) {
+		return fmt.Errorf("got %d bytes, expected %d", n, size)
+	}
+	copy(unsafe.Slice((*byte)(data), size), buf)
+	return nil
+}
