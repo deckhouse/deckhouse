b466818ef75acdc5b6027ea2a04e793318893016
Artur Kraev <artur.kraev@flant.com>
2025-03-13 02:02:51 +0300

    add static pods status hook
    Signed-off-by: Artur Kraev <artur.kraev@flant.com>

---
+++ ee/modules/038-system-registry/hooks/staticpod_status.go
@@ -0,0 +1,87 @@
+/*
+Copyright 2024 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package hooks
+
+import (
+	"fmt"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	v1core "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+type registryStaticPod struct {
+	PodName  string
+	PodIP    string
+	NodeName string
+	NodeIP   string
+}
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 5},
+	Queue:        "/modules/system-registry/staticpod-status",
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "static_pods",
+			ApiVersion: "v1",
+			Kind:       "Pod",
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"d8-system"},
+				},
+			},
+			LabelSelector: &v1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app":       "system-registry",
+					"module":    "system-registry",
+					"component": "system-registry",
+				},
+			},
+			FilterFunc: filterRegistryStaticPods,
+		},
+	},
+}, handleRegistryStaticPods)
+
+func filterRegistryStaticPods(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	var pod v1core.Pod
+
+	err := sdk.FromUnstructured(obj, &pod)
+	if err != nil {
+		return "", fmt.Errorf("failed to convert pod to struct: %v", err)
+	}
+
+	nodeFound := false
+	for _, ref := range pod.OwnerReferences {
+		if ref.Kind == "Node" {
+			nodeFound = true
+			break
+		}
+	}
+
+	if !nodeFound {
+		return "", nil
+	}
+
+	ret := registryStaticPod{
+		PodName:  pod.Name,
+		PodIP:    pod.Status.PodIP,
+		NodeName: pod.Spec.NodeName,
+		NodeIP:   pod.Status.HostIP,
+	}
+
+	return ret, nil
+}
+
+func handleRegistryStaticPods(input *go_hook.HookInput) error {
+	pods := input.Snapshots["static_pods"]
+
+	input.Values.Set("systemRegistry.state.staticPods", pods)
+
+	return nil
+}
