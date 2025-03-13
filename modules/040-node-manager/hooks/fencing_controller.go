1889a5a0634d39975f6288d5c453e72235d3bf70
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2025-03-13 18:10:58 +0300

    rebase
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- modules/040-node-manager/hooks/fencing_controller.go
+++ modules/040-node-manager/hooks/fencing_controller.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
-	"github.com/flant/shell-operator/pkg/kube/object_patch"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/fields"
@@ -159,7 +158,7 @@ func fencingControllerHandler(input *go_hook.HookInput, dc dependency.Container)
 		}
 
 		input.Logger.Warnf("Delete node %s", node)
-		input.PatchCollector.Delete("v1", "Node", "", node, object_patch.InBackground())
+		input.PatchCollector.DeleteInBackground("v1", "Node", "", node)
 	}
 
 	return nil
