dd6ff791061c1f6c5bca677c18cc8449747f35f3
Stepan Paksashvili <stepan.paksashvili@flant.com>
2025-03-14 13:03:24 +0300

    [deckhouse] fix
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- deckhouse-controller/pkg/controller/moduleloader/loader.go
+++ deckhouse-controller/pkg/controller/moduleloader/loader.go
@@ -344,6 +344,10 @@ func (l *Loader) ensureModule(ctx context.Context, def *moduletypes.Definition, 
 				module.Properties.Version = l.version
 			}
 
+			if !module.HasCondition(v1alpha1.ModuleConditionEnabledByModuleConfig) {
+				module.SetConditionFalse(v1alpha1.ModuleConditionEnabledByModuleManager, v1alpha1.ModuleReasonDisabled, v1alpha1.ModuleMessageDisabled)
+			}
+
 			if !reflect.DeepEqual(moduleCopy.Properties, module.Properties) ||
 				!reflect.DeepEqual(moduleCopy.Labels, module.Labels) ||
 				!reflect.DeepEqual(moduleCopy.Annotations, module.Annotations) {
