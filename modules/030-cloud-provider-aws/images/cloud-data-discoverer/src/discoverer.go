e7da906b4637efee33e7eec7e26f124cc470c0e4
Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>
2025-03-17 14:47:05 +0300

    AWS CheckCloudConditions implementation
    Signed-off-by: Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>

--- modules/030-cloud-provider-aws/images/cloud-data-discoverer/src/discoverer.go
+++ modules/030-cloud-provider-aws/images/cloud-data-discoverer/src/discoverer.go
@@ -18,11 +18,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	log "github.com/sirupsen/logrus"
@@ -50,8 +52,51 @@ func NewDiscoverer(logger *log.Entry) *Discoverer {
 }
 
 func (d *Discoverer) CheckCloudConditions(ctx context.Context) ([]v1alpha1.CloudCondition, error) {
-	// TODO implement me
-	return nil, nil
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String(d.region),
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize new session: %v", err)
+	}
+
+	ec2Client := ec2.New(sess)
+
+	var res []v1alpha1.CloudCondition
+	var awsErr awserr.Error
+
+	// check role for DescribeAddressesAttribute
+	_, err = ec2Client.DescribeAddressesAttribute(&ec2.DescribeAddressesAttributeInput{
+		DryRun: aws.Bool(true),
+	})
+	if ok := errors.As(err, &awsErr); !ok {
+		return nil, fmt.Errorf("DescribeAddressesAttribute AWS IAM role check error: %v", err)
+	}
+	if awsErr.Code() != "DryRunOperation" {
+		res = append(res, v1alpha1.CloudCondition{
+			Name:    "insufficient AWS service account roles",
+			Message: "DescribeAddressesAttribute is not allowed",
+			Ok:      false,
+		})
+	}
+
+	// check role for DescribeInstanceTopology
+	_, err = ec2Client.DescribeInstanceTopology(&ec2.DescribeInstanceTopologyInput{
+		DryRun: aws.Bool(true),
+	})
+	if ok := errors.As(err, &awsErr); !ok {
+		return nil, fmt.Errorf("DescribeInstanceTopology AWS IAM role check error: %v", err)
+	}
+	if awsErr.Code() != "DryRunOperation" {
+		res = append(res, v1alpha1.CloudCondition{
+			Name:    "insufficient AWS service account roles",
+			Message: "DescribeInstanceTopology is not allowed",
+			Ok:      false,
+		})
+	}
+
+	return res, nil
 }
 
 func (d *Discoverer) InstanceTypes(_ context.Context) ([]v1alpha1.InstanceType, error) {
