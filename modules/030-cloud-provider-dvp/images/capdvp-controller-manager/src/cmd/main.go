01919625c8d49aa4a361fa550e747e17c01caa15
Maxim Vasilenko <maksim.vasilenko@flant.com>
2025-03-18 10:59:33 +0300

    [cloud-provider-dvp] ++
    Signed-off-by: Maxim Vasilenko <maksim.vasilenko@flant.com>

--- modules/030-cloud-provider-dvp/images/capdvp-controller-manager/src/cmd/main.go
+++ modules/030-cloud-provider-dvp/images/capdvp-controller-manager/src/cmd/main.go
@@ -25,6 +25,7 @@ import (
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 
 	dvpapi "dvp-common/api"
 	"dvp-common/config"
@@ -52,7 +53,7 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-
+	utilruntime.Must(clusterv1.AddToScheme(scheme))
 	utilruntime.Must(infrastructurev1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
