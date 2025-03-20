650f1c4f85a2644554630d4f2ebfe8e61aa8183e
Alexey Sulimenko <alexey.sulimenko@flant.com>
2025-03-20 12:41:32 +0300

    Cloud provider dvp ccm lb
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

---
+++ modules/030-cloud-provider-dvp/images/cloud-controller-manager/src/pkg/cloudprovider/dvp/loadbalancer.go
@@ -0,0 +1,140 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dvp
+
+import (
+	"context"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	cloudprovider "k8s.io/cloud-provider"
+	"k8s.io/klog/v2"
+)
+
+const (
+	LBCreationPollInterval = 5 * time.Second
+	LBCreationPollTimeout  = 5 * time.Minute
+)
+
+func (c *Cloud) GetLoadBalancer(ctx context.Context, clusterName string, service *corev1.Service) (status *corev1.LoadBalancerStatus, exists bool, err error) {
+	name := c.GetLoadBalancerName(ctx, clusterName, service)
+	svc, err := c.dvpService.LoadBalancerService.GetLoadBalancerByName(ctx, name)
+	if err != nil {
+		klog.Errorf("Failed to get LoadBalancer service %q in namespace %q: %v", name, c.config.Namespace, err)
+		return nil, false, err
+	}
+	if svc == nil {
+		return nil, false, nil
+	}
+	status = &svc.Status.LoadBalancer
+	return status, true, nil
+}
+
+func (c *Cloud) GetLoadBalancerName(_ context.Context, _ string, service *corev1.Service) string {
+	// TODO: replace DefaultLoadBalancerName to generate more meaningful loadbalancer names.
+	return cloudprovider.DefaultLoadBalancerName(service)
+}
+
+func (c *Cloud) EnsureLoadBalancer(ctx context.Context, clusterName string, service *corev1.Service, nodes []*corev1.Node) (*corev1.LoadBalancerStatus, error) {
+	name := c.GetLoadBalancerName(ctx, clusterName, service)
+	svc, err := c.dvpService.LoadBalancerService.GetLoadBalancerByName(ctx, name)
+	if err != nil {
+		klog.Errorf("Failed to get LoadBalancer service %q in namespace %q: %v", name, c.config.Namespace, err)
+		return nil, err
+	}
+	ports := c.dvpService.LoadBalancerService.CreateLoadBalancerPorts(service)
+
+	if svc != nil {
+		return &svc.Status.LoadBalancer, c.dvpService.LoadBalancerService.UpdateLoadBalancerPorts(ctx, svc, ports)
+	}
+
+	// TODO: fix labels.
+	vmLabels := map[string]string{
+		"cluster.x-k8s.io/cluster-name": clusterName,
+	}
+	// TODO: fix labels.
+	svcLabels := map[string]string{
+		"cluster.x-k8s.io/tenant-service-name":      service.Name,
+		"cluster.x-k8s.io/tenant-service-namespace": service.Namespace,
+		"cluster.x-k8s.io/cluster-name":             clusterName,
+	}
+
+	// for k, v := range lb.infraLabels {
+	// 	svcLabels[k] = v
+	// }
+
+	svc, err = c.dvpService.LoadBalancerService.CreateLoadBalancer(
+		ctx,
+		name,
+		service,
+		vmLabels,
+		true,
+		svcLabels,
+		ports,
+	)
+	if err != nil {
+		klog.Errorf("Failed to create LoadBalancer service %q in namespace %q: %v", name, c.config.Namespace, err)
+		return nil, err
+	}
+
+	err = wait.PollUntilContextTimeout(ctx,
+		LBCreationPollInterval,
+		LBCreationPollTimeout,
+		true,
+		func(context.Context) (done bool, err error) {
+			if len(svc.Status.LoadBalancer.Ingress) > 0 {
+				return true, nil
+			}
+			s, err := c.dvpService.LoadBalancerService.GetLoadBalancerByName(ctx, name)
+			if err != nil {
+				klog.Errorf("Failed to get LoadBalancer service %q in namespace %q: %v", name, c.config.Namespace, err)
+				return false, err
+			}
+			if s != nil && len(s.Status.LoadBalancer.Ingress) > 0 {
+				svc = s
+				return true, nil
+			}
+			return false, nil
+		})
+	if err != nil {
+		klog.Errorf("Failed to poll LoadBalancer service %q in namespace %q: %v", name, c.config.Namespace, err)
+		return nil, err
+	}
+
+	return &svc.Status.LoadBalancer, nil
+}
+
+func (c *Cloud) UpdateLoadBalancer(ctx context.Context, clusterName string, service *corev1.Service, _ []*corev1.Node) error {
+	name := c.GetLoadBalancerName(ctx, clusterName, service)
+
+	svc, err := c.dvpService.LoadBalancerService.GetLoadBalancerByName(ctx, name)
+	if err != nil {
+		klog.Errorf("Failed to get LoadBalancer service %q in namespace %q: %v", name, c.config.Namespace, err)
+		return err
+	}
+
+	ports := c.dvpService.LoadBalancerService.CreateLoadBalancerPorts(service)
+	// LoadBalancer already exist, update the ports if changed
+	return c.dvpService.LoadBalancerService.UpdateLoadBalancerPorts(ctx, svc, ports)
+}
+
+func (c *Cloud) EnsureLoadBalancerDeleted(ctx context.Context, clusterName string, service *corev1.Service) error {
+	name := c.GetLoadBalancerName(ctx, clusterName, service)
+
+	return c.dvpService.LoadBalancerService.DeleteLoadBalancerByName(ctx, name)
+}
---
+++ modules/030-cloud-provider-dvp/images/dvp-common/api/loadbalancer.go
@@ -0,0 +1,131 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package api
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/klog/v2"
+	"k8s.io/utils/ptr"
+)
+
+type LoadBalancerService struct {
+	*Service
+}
+
+func NewLoadBalancerService(service *Service) *LoadBalancerService {
+	return &LoadBalancerService{service}
+}
+
+func (lb *LoadBalancerService) GetLoadBalancerByName(ctx context.Context, name string) (*corev1.Service, error) {
+	var svc corev1.Service
+	if err := lb.client.Get(ctx, types.NamespacedName{Name: name, Namespace: lb.namespace}, &svc); err != nil {
+		if k8serrors.IsNotFound(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &svc, nil
+}
+
+func (lb *LoadBalancerService) CreateLoadBalancerPorts(service *corev1.Service) []corev1.ServicePort {
+	ports := make([]corev1.ServicePort, len(service.Spec.Ports))
+	for i, port := range service.Spec.Ports {
+		ports[i].Name = port.Name
+		ports[i].Protocol = port.Protocol
+		ports[i].Port = port.Port
+		ports[i].TargetPort = intstr.IntOrString{
+			Type:   intstr.Int,
+			IntVal: port.NodePort,
+		}
+	}
+	return ports
+}
+
+func (lb *LoadBalancerService) UpdateLoadBalancerPorts(ctx context.Context, service *corev1.Service, ports []corev1.ServicePort) error {
+	if !equality.Semantic.DeepEqual(ports, service.Spec.Ports) {
+		service.Spec.Ports = ports
+		if err := lb.client.Update(ctx, service); err != nil {
+			klog.Errorf("Failed to update LoadBalancer service %q in namespace %q: %v", service.GetName(), lb.namespace, err)
+			return err
+		}
+		return nil
+	}
+	return nil
+}
+
+func (lb *LoadBalancerService) CreateLoadBalancer(
+	ctx context.Context,
+	name string,
+	service *corev1.Service,
+	vmLabels map[string]string,
+	selectorLess bool,
+	serviceLabels map[string]string,
+	ports []corev1.ServicePort,
+) (*corev1.Service, error) {
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Namespace:   lb.namespace,
+			Annotations: service.Annotations,
+			Labels:      serviceLabels,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports:                 ports,
+			Type:                  corev1.ServiceTypeLoadBalancer,
+			ExternalTrafficPolicy: service.Spec.ExternalTrafficPolicy,
+		},
+	}
+	if !selectorLess {
+		svc.Spec.Selector = vmLabels
+	}
+	if len(service.Spec.ExternalIPs) > 0 {
+		svc.Spec.ExternalIPs = service.Spec.ExternalIPs
+	}
+	if service.Spec.LoadBalancerClass != nil {
+		svc.Spec.LoadBalancerClass = ptr.To(*service.Spec.LoadBalancerClass)
+	}
+	if service.Spec.LoadBalancerIP != "" {
+		svc.Spec.LoadBalancerIP = service.Spec.LoadBalancerIP
+	}
+	if service.Spec.HealthCheckNodePort > 0 {
+		svc.Spec.HealthCheckNodePort = service.Spec.HealthCheckNodePort
+	}
+	return svc, lb.client.Create(ctx, svc)
+}
+
+func (lb *LoadBalancerService) DeleteLoadBalancerByName(ctx context.Context, name string) error {
+	svc, err := lb.GetLoadBalancerByName(ctx, name)
+	if err != nil {
+		klog.Errorf("Failed to get LoadBalancer service %q in namespace %q: %v", name, lb.namespace, err)
+		return err
+	}
+	if svc == nil {
+		return nil
+	}
+	if err = lb.client.Delete(ctx, svc); err != nil {
+		klog.Errorf("Failed to delete LoadBalancer service %q in namespace %q: %v", name, lb.namespace, err)
+		return err
+	}
+	return nil
+}
