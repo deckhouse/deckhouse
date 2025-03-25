07d92ea0b14a45c12900ecd0877dd9c4f72b7c21
vadimartynov <vadim.martynov@flant.com>
2025-03-25 20:59:33 +0300

    Added img override for deckhouse module
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

---
+++ modules/002-deckhouse/hack/deckhouse-docker-build-and-apply.sh
@@ -0,0 +1,128 @@
+#! /bin/bash
+#
+# Copyright 2024 Flant JSC
+# Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+
+set -e
+
+# Input parameters:
+DECKHOUSE_PATH="/deckhouse/"
+IMG_PREFIX="$USER"
+DOCKER_REGISTRY="cr.yandex/crp8n201pre28pm81udl/sys/deckhouse-oss"
+PATCH_MODULE_CONFIG_BY_IMAGE_AMD64_NAME_DIGEST=true
+
+IMG_NAME="$DOCKER_REGISTRY/$IMG_PREFIX-deckhouse"
+IMG_NAME_LATEST="$IMG_NAME:latest"
+
+log_stage() {
+    local stage_name="$1"
+    local line_length=90  # Line length for the frame
+    local dash_line="-"   # Character for the line
+    local green="\033[1;32m"  # Green color
+    local reset="\033[0m"     # Reset color
+
+    # Create a string of dashes of the required length
+    local dashes=$(printf "%-${line_length}s" "" | tr " " "-")
+
+    # Print the dash line at the top
+    echo "$dashes"
+
+    # Calculate padding to center the stage name
+    local padding=$(( (line_length - ${#stage_name} - 4) / 2 ))
+
+    # Print the stage name in green color
+    printf "|%*s ${green}%s${reset} %*s|\n" $padding "" "$stage_name" $padding ""
+
+    # Print the bottom border of the frame
+    echo "$dashes"
+}
+
+# Function to find and switch to the directory where the script is located
+cd_script_dir() {
+  # Determine the path to the script directory
+  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+  # Switch to this directory
+  cd "$script_dir" || exit
+
+  # Inform the user which directory we have switched to
+  echo "Switched to script directory: $script_dir"
+}
+
+# Function to apply a patch
+kubectl_patch_module_config() {
+  local args="$1"
+  local patch="$2"
+
+  # Apply the patch using kubectl with Strategic Merge Patch
+  kubectl patch $args --type='merge' -p "$patch" --output=name
+
+  # Check the result of the execution
+  if [ $? -eq 0 ]; then
+    echo "Patch applied successfully!"
+  else
+    echo "Error applying patch!"
+    exit 1
+  fi
+}
+
+cd_script_dir
+
+log_stage "Create buildx"
+if ! docker buildx inspect mybuilder > /dev/null 2>&1; then
+    # If it does not exist, create it
+    docker buildx create --name mybuilder --driver docker-container --use
+else
+    # If it exists, just switch to it
+    docker buildx use mybuilder
+fi
+
+log_stage "Build and push docker img"
+docker buildx build $DECKHOUSE_PATH \
+    -f deckhouse.Dockerfile \
+    --platform linux/amd64 \
+    -t $IMG_NAME_LATEST \
+    --pull\
+    --push
+
+log_stage "Show img with tag"
+echo "$IMG_NAME_LATEST"
+
+IMAGE_AMD64_DIGEST=$(docker buildx imagetools inspect $IMG_NAME_LATEST --raw | jq -r '.manifests[] | select(.platform.architecture == "amd64").digest')
+IMAGE_AMD64_NAME_DIGEST="$IMG_NAME@$IMAGE_AMD64_DIGEST"
+
+
+log_stage "Show img with digest"
+echo $IMAGE_AMD64_NAME_DIGEST
+
+if [ "$PATCH_MODULE_CONFIG_BY_IMAGE_AMD64_NAME_DIGEST" = "true" ]; then
+
+    # Define the patch that will add or replace deckhouse
+    PATCH=$(cat <<EOF
+{
+  "spec": {
+    "settings": {
+      "imagesOverride": {
+        "deckhouse": "$IMAGE_AMD64_NAME_DIGEST"
+      }
+    }
+  }
+}
+EOF
+)
+
+
+    before_patch=$(kubectl get ModuleConfig deckhouse -o yaml)
+
+    # Apply the patch without conditions
+
+    log_stage "Applying patch to ModuleConfig deckhouse"
+    kubectl_patch_module_config "ModuleConfig/deckhouse" "$PATCH"
+
+
+    after_patch=$(kubectl get ModuleConfig deckhouse -o yaml)
+    log_stage "Show Diff"
+    if diff --color=auto <(echo "$before_patch") <(echo "$after_patch"); then
+        echo "No changes were made."
+    fi
+fi
---
+++ modules/002-deckhouse/hack/deckhouse.Dockerfile
@@ -0,0 +1,45 @@
+# syntax=docker/dockerfile:1.7-labs
+
+FROM --platform=linux/amd64 golang:1.24 AS build
+
+# Set workdir
+WORKDIR /deckhouse
+
+# Install setcap
+RUN apt-get update && \
+    apt-get install -y libcap2-bin
+
+# Download pkg
+COPY --parents **/*.mod **/*sum ./
+RUN --mount=type=cache,target=/root/.cache/go-build \
+        go mod download
+
+# Build controller
+COPY --parents **/*.go **/*.sh ./
+RUN --mount=type=cache,target=/root/.cache/go-build \
+        cd ./deckhouse-controller/ && \
+        chmod +x *.sh && \
+        ./go-build.sh && \
+        cp deckhouse-controller /usr/bin/deckhouse-controller && \
+        cp deckhouse-controller /usr/bin/caps-deckhouse-controller && \
+        setcap "cap_sys_chroot=ep cap_sys_admin=ep cap_mknod=ep" /usr/bin/caps-deckhouse-controller
+
+# Replace controller
+FROM --platform=linux/amd64 dev-registry.deckhouse.io/sys/deckhouse-oss:pr8229-ee
+COPY --from=build /usr/bin/deckhouse-controller /usr/bin/deckhouse-controller
+COPY --from=build /usr/bin/caps-deckhouse-controller /usr/bin/caps-deckhouse-controller
+
+# Replace module helm chart
+COPY --exclude=docs \
+     --exclude=charts/helm_lib \
+     --exclude=README.md \
+     --exclude=images \
+     --exclude=hooks/**/*.go \
+     --exclude=template_tests \
+     --exclude=.namespace \
+     --exclude=values_matrix_test.yaml \
+     --exclude=apis/**/*.go \
+     --exclude=requirements/**/*.go \
+     --exclude=settings-conversion/**/*.go \
+     --exclude=hack/**/*.go \
+     ee/modules/038-system-registry/ /deckhouse/modules/038-system-registry/
