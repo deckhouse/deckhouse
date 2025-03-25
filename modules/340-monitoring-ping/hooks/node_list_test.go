4a9667b8d76ded1d39cd8e831bcc5d47e1cfe2a9
Paramoshka <parfenov_ivan_42a@mail.ru>
2025-03-25 15:17:10 +0300

    [monitoring-ping] refactoring
    Signed-off-by: Paramoshka <parfenov_ivan_42a@mail.ru>
    Signed-off-by: Mikhail Scherba <mikhail.scherba@flant.com>
    
    Co-authored-by: Mikhail Scherba <mikhail.scherba@flant.com>

---
+++ modules/340-monitoring-ping/hooks/node_list_test.go
@@ -0,0 +1,143 @@
+// Package ping Copyright 2025 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hooks
+
+import (
+	"encoding/json"
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/deckhouse/deckhouse/pkg/log"
+	. "github.com/deckhouse/deckhouse/testing/hooks"
+)
+
+type Target struct {
+	Name      string `json:"name"`
+	IPAddress string `json:"ipAddress"`
+}
+
+type Targets struct {
+	Cluster []Target `json:"cluster_targets"`
+}
+
+var ctxNode1 = `
+---
+apiVersion: v1
+kind: Node
+metadata:
+  name: node1
+spec: {}
+status:
+  addresses:
+    - type: InternalIP
+      address: 10.0.0.1
+`
+
+var ctxNode2 = `
+---
+apiVersion: v1
+kind: Node
+metadata:
+  name: node2
+spec: {}
+status:
+  addresses:
+    - type: InternalIP
+      address: 10.0.0.2
+`
+
+var ctxNode3NoAddress = `
+---
+apiVersion: v1
+kind: Node
+metadata:
+  name: node3
+spec: {}
+status:
+  addresses:
+    - type: InternalIP
+      address: ""
+`
+
+var ctxNode4Unschedulable = `
+---
+apiVersion: v1
+kind: Node
+metadata:
+  name: node4
+spec:
+  unschedulable: true
+status:
+  addresses:
+    - type: InternalIP
+      address: 10.0.0.4
+`
+
+var ctxConfigMap = `
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: monitoring-ping-config
+  namespace: d8-monitoring
+data:
+  targets.json: ""
+`
+
+var ctxNamespace = `
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: d8-monitoring
+`
+
+var _ = Describe("Modules :: monitoring-ping :: hooks :: node_list", func() {
+	f := HookExecutionConfigInit(`{}`, `{}`)
+
+	Context("List nodes", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.GenerateScheduleContext("* * * * *"))
+			f.BindingContexts.Set(f.KubeStateSet(ctxNode1 + ctxNode2 + ctxNode3NoAddress + ctxNode4Unschedulable + ctxNamespace + ctxConfigMap))
+			f.RunGoHook()
+		})
+
+		It("Targets should exist", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(f.KubernetesResource("ConfigMap", "d8-monitoring", "monitoring-ping-config").Exists()).To(BeTrue())
+			obj := f.KubernetesResource("ConfigMap", "d8-monitoring", "monitoring-ping-config")
+			data, ok := obj["data"].(map[string]interface{})
+			if !ok {
+				fmt.Printf("data is not a map")
+			}
+
+			targetsJSON, ok := data["targets.json"].(string)
+			if !ok {
+				fmt.Printf("targets is not a map")
+			}
+
+			var targets Targets
+			err := json.Unmarshal([]byte(targetsJSON), &targets)
+			if err != nil {
+				log.Fatal("failed to unmarshal targets.json: %v", err)
+			}
+
+			Expect(len(targets.Cluster)).To(Equal(2))
+
+		})
+	})
+})
---
+++ modules/340-monitoring-ping/images/monitoring-ping/src/nodeTarget.go
@@ -0,0 +1,145 @@
+// Package ping Copyright 2025 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/deckhouse/deckhouse/pkg/log"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"sync"
+)
+
+type NodeTracker struct {
+	sync.RWMutex
+	nodes []NodeTarget
+	externalTargets []ExternalTarget
+}
+
+func NewNodeTracker() *NodeTracker {
+	return &NodeTracker{
+		nodes: make([]NodeTarget, 0),
+		externalTargets: make([]ExternalTarget, 0),
+	}
+}
+
+func (nt *NodeTracker) Start(ctx context.Context, internalCM, externalCM, namespace string) error {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	internalInformer := createInformer(internalCM, namespace, nt.onUpdate, true, clientset)
+	externalInformer := createInformer(externalCM, namespace, nt.onUpdate, false, clientset)
+
+	go internalInformer.Run(ctx.Done())
+	go externalInformer.Run(ctx.Done())
+
+	if !cache.WaitForCacheSync(ctx.Done(), internalInformer.HasSynced, externalInformer.HasSynced) {
+		return fmt.Errorf("failed to sync caches")
+	}
+
+	return nil
+}
+
+func (t *InternalTargets) Update(nt *NodeTracker) {
+	nt.nodes = t.Cluster
+	log.Info("Updated internal targets: %d nodes", len(t.Cluster))
+}
+
+func (t *ExternalTargets) Update(nt *NodeTracker) {
+	nt.externalTargets = t.Targets
+	log.Info("Updated external targets: %d hosts", len(t.Targets))
+}
+
+func (nt *NodeTracker) updateTargets(jsonData string, target Targets) {
+	if err := json.Unmarshal([]byte(jsonData), target); err != nil {
+		log.Error("Failed to unmarshal targets.json: %v", err)
+		return
+	}
+
+	nt.Lock()
+	defer nt.Unlock()
+	target.Update(nt)
+}
+
+func (nt *NodeTracker) onUpdate(obj interface{}, internal bool) {
+	cfgMap, ok := obj.(*v1.ConfigMap)
+	if !ok {
+		log.Warn("unexpected object type in onUpdate")
+		return
+	}
+
+	jsonData, exists := cfgMap.Data["targets.json"]
+	if !exists {
+		log.Warn("targets.json not found in ConfigMap")
+		return
+	}
+
+	if internal {
+		nt.updateTargets(jsonData, &InternalTargets{})
+	} else {
+		nt.updateTargets(jsonData, &ExternalTargets{})
+	}
+}
+
+func (nt *NodeTracker) ListClusterTargets() []NodeTarget {
+	nt.RLock()
+	defer nt.RUnlock()
+	list := make([]NodeTarget, len(nt.nodes))
+	copy(list, nt.nodes)
+	return list
+}
+
+func (nt *NodeTracker) ListExternalTargets() []ExternalTarget {
+	nt.RLock()
+	defer nt.RUnlock()
+	list := make([]ExternalTarget, len(nt.externalTargets))
+	copy(list, nt.externalTargets)
+	return list
+}
+
+func createInformer(cfgMapName, namespace string, handler func(interface{}, bool), internal bool, clientset *kubernetes.Clientset) cache.SharedInformer {
+	lw := cache.NewFilteredListWatchFromClient(
+		clientset.CoreV1().RESTClient(),
+		"configmaps",
+		namespace,
+		func(options *metav1.ListOptions) {
+			options.FieldSelector = fmt.Sprintf("metadata.name=%s", cfgMapName)
+		},
+	)
+
+	informer := cache.NewSharedInformer(lw, &v1.ConfigMap{}, 0)
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    func(obj interface{}) { handler(obj, internal) },
+		UpdateFunc: func(_, obj interface{}) { handler(obj, internal) },
+		DeleteFunc: func(obj interface{}) {
+			log.Warn("%s deleted — list will not be updated until recreated", cfgMapName)
+		},
+	})
+
+	return informer
+}
+
