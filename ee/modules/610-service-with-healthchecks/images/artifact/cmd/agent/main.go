eb8c18dc05e6a5e7e8ca597e96769e6b83528e1d
Andrey Polovov <andrey.polovov@flant.com>
2025-04-03 10:18:44 +0300

    Update main.go
    Signed-off-by: Andrey Polovov <andrey.polovov@flant.com>

--- ee/modules/610-service-with-healthchecks/images/artifact/cmd/agent/main.go
+++ ee/modules/610-service-with-healthchecks/images/artifact/cmd/agent/main.go
@@ -2,6 +2,7 @@
 Copyright 2024 Flant JSC
 Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 */
+// fake modification
 
 package main
 
