630f2b32cc4c2e402f89c85c46da7ea8677c61aa
Artur Kraev <artur.kraev@flant.com>
2025-04-07 12:34:12 +0300

    Users hook start
    Signed-off-by: Artur Kraev <artur.kraev@flant.com>

--- ee/modules/038-system-registry/hooks/users/hook.go
+++ ee/modules/038-system-registry/hooks/users/hook.go
@@ -4,3 +4,59 @@ Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https
 */
 
 package users
+
+import (
+	"fmt"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	v1core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+const (
+	snapUsers = "users"
+)
+
+var (
+	namespaceSelector = &types.NamespaceSelector{
+		NameSelector: &types.NameSelector{
+			MatchNames: []string{"d8-system"},
+		},
+	}
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 5},
+	Queue:        "/modules/system-registry/users",
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:              snapUsers,
+			ApiVersion:        "v1",
+			Kind:              "Secret",
+			NamespaceSelector: namespaceSelector,
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{
+					"registry-user-ro",
+					"registry-user-rw",
+					"registry-user-mirror-puller",
+					"registry-user-mirror-pusher",
+				},
+			},
+			FilterFunc: func(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+				var secret v1core.Secret
+
+				err := sdk.FromUnstructured(obj, &secret)
+				if err != nil {
+					return "", fmt.Errorf("failed to convert secret \"%v\" to struct: %v", obj.GetName(), err)
+				}
+
+				return "", nil
+			},
+		},
+	},
+}, func(input *go_hook.HookInput) error {
+
+	return nil
+})
