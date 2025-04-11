fca514c91cca6943b6852141af012ff077d13c73
Stepan Paksashvili <stepan.paksashvili@flant.com>
2025-04-11 12:03:54 +0300

    [deckhouse] delete deckhouse epslices
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

---
+++ global-hooks/migrate/delete_deckhouse_epslices.go
@@ -0,0 +1,23 @@
+package hooks
+
+import (
+	"context"
+	"fmt"
+	"github.com/deckhouse/deckhouse/go_lib/dependency"
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	OnStartup: &go_hook.OrderedConfig{Order: 10},
+}, dependency.WithExternalDependencies(removeDeckhouseEpslices))
+
+func removeDeckhouseEpslices(_ *go_hook.HookInput, dc dependency.Container) error {
+	kubeClient, err := dc.GetK8sClient()
+	if err != nil {
+		return fmt.Errorf("get kubernetes client: %v", err)
+	}
+
+	return kubeClient.DiscoveryV1().EndpointSlices("d8-system").Delete(context.TODO(), "deckhouse", metav1.DeleteOptions{})
+}
