508bc6ffda86f0cd4f1ec15636696f9ca5ef65db
Sergey Kurbatov <sergey.kurbatov@flant.com>
2025-04-15 19:13:03 +0300

    [istio] disable search istio operator for new version
    Signed-off-by: Sergey Kurbatov <sergey.kurbatov@flant.com>

--- modules/110-istio/hooks/discovery_operator_versions_to_install.go
+++ modules/110-istio/hooks/discovery_operator_versions_to_install.go
@@ -76,18 +76,36 @@ func operatorRevisionsToInstallDiscovery(input *go_hook.HookInput) error {
 
 	var versionsToInstallResult = input.Values.Get("istio.internal.versionsToInstall").Array()
 	for _, versionResult := range versionsToInstallResult {
-		operatorVersionsToInstall = append(operatorVersionsToInstall, versionResult.String())
+		version := versionResult.String()
+		if version != "1.24" {
+			operatorVersionsToInstall = append(operatorVersionsToInstall, versionResult.String())
+		}
 	}
 
-	for _, iop := range input.Snapshots["istiooperators"] {
-		iopInfo := iop.(IstioOperatorCrdInfo)
-		iopVer := versionMap.GetVersionByRevision(iopInfo.Revision)
-		if !versionMap.IsRevisionSupported(iopInfo.Revision) {
-			unsupportedRevisions = append(unsupportedRevisions, iopInfo.Revision)
-			continue
+	nonOperatorVersion := false
+	for _, versionResult := range versionsToInstallResult {
+		if versionResult.String() == "1.24" {
+			nonOperatorVersion = true
+			break
 		}
-		if !lib.Contains(operatorVersionsToInstall, iopVer) {
-			operatorVersionsToInstall = append(operatorVersionsToInstall, iopVer)
+	}
+
+	if !nonOperatorVersion {
+		for _, iop := range input.Snapshots["istiooperators"] {
+			iopInfo := iop.(IstioOperatorCrdInfo)
+			if versionMap.GetVersionByRevision(iopInfo.Revision) == "1.24" {
+				continue
+			}
+
+			if !versionMap.IsRevisionSupported(iopInfo.Revision) {
+				unsupportedRevisions = append(unsupportedRevisions, iopInfo.Revision)
+				continue
+			}
+
+			iopVer := versionMap.GetVersionByRevision(iopInfo.Revision)
+			if !lib.Contains(operatorVersionsToInstall, iopVer) {
+				operatorVersionsToInstall = append(operatorVersionsToInstall, iopVer)
+			}
 		}
 	}
 
@@ -102,6 +120,9 @@ func operatorRevisionsToInstallDiscovery(input *go_hook.HookInput) error {
 	// Getting minVersion
 	var minVersion *semver.Version
 	for _, version := range operatorVersionsToInstall {
+		if version == "1.24" {
+			continue
+		}
 		versionSemver, err := semver.NewVersion(version)
 		if err != nil {
 			return err
