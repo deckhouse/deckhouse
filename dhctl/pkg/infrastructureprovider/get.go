0bb72c3490942e7592897d6c0f981c98e5b9929e
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2025-04-24 19:51:18 +0300

    ++
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- dhctl/pkg/infrastructureprovider/get.go
+++ dhctl/pkg/infrastructureprovider/get.go
@@ -15,7 +15,12 @@
 package infrastructureprovider
 
 import (
+	"fmt"
+	"os"
+	"slices"
 	"strings"
+
+	"sigs.k8s.io/yaml"
 
 	"github.com/deckhouse/deckhouse/dhctl/pkg/config"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/infrastructure"
@@ -23,6 +28,9 @@ import (
 	"github.com/deckhouse/deckhouse/dhctl/pkg/infrastructure/tofu"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/log"
 )
+
+// no in init function because config.InfrastructureVersions loaded dynamically
+var cloudNameToUseOpenTofu map[string]struct{}
 
 func ExecutorProvider(metaConfig *config.MetaConfig) infrastructure.ExecutorProvider {
 	if metaConfig == nil {
@@ -47,5 +55,77 @@ func ExecutorProvider(metaConfig *config.MetaConfig) infrastructure.ExecutorProv
 }
 
 func NeedToUseOpentofu(metaConfig *config.MetaConfig) bool {
-	return strings.ToLower(metaConfig.ProviderName) == "yandex"
+	useTofuMap, err := getCloudNameToUseOpentofuMap(config.InfrastructureVersions)
+	if err != nil {
+		panic(fmt.Errorf("Cannot get use tofu map: %v", err))
+	}
+
+	provider := strings.ToLower(metaConfig.ProviderName)
+
+	_, useTofu := useTofuMap[provider]
+
+	return useTofu
+}
+
+func getCloudNameToUseOpentofuMap(filename string) (map[string]struct{}, error) {
+	if cloudNameToUseOpenTofu != nil {
+		return cloudNameToUseOpenTofu, nil
+	}
+
+	infrastructureProviders := make(map[string]interface{})
+
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot read infrastructure versions file %s: %v", filename, err)
+	}
+
+	err = yaml.Unmarshal(file, &infrastructureProviders)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot unmarshal infrastructure versions file %s: %v", filename, err)
+	}
+
+	cloudNameToUseOpenTofu = make(map[string]struct{})
+
+	for name, rawSettings := range infrastructureProviders {
+		if slices.Contains([]string{"opentofu", "terraform"}, name) {
+			log.DebugF("Found not provider name key %s\n", name)
+			continue
+		}
+
+		settings, ok := rawSettings.(map[string]interface{})
+		if !ok {
+			log.DebugF("provider %s is not map\n", name)
+			continue
+		}
+
+		useTofuRaw, ok := settings["useOpentofu"]
+		if !ok {
+			log.DebugF("Provider %s does not have useOpentofu. Skip provider.\n", name)
+			continue
+		}
+
+		useTofu, ok := useTofuRaw.(bool)
+		if !ok {
+			return nil, fmt.Errorf("Provider %s none boolean have useOpentofu. Skip provider.\n", name)
+		}
+
+		if !useTofu {
+			log.DebugF("provider %s does not use OpenTofu. Skip provider.\n", name)
+			continue
+		}
+
+		cloudNameRaw, ok := settings["cloudName"]
+		if !ok {
+			return nil, fmt.Errorf("Provider %s does not have cloudName key", name)
+		}
+
+		cloudName, ok := cloudNameRaw.(string)
+		if !ok {
+			return nil, fmt.Errorf("Provider %s have none string cloudName key", name)
+		}
+
+		cloudNameToUseOpenTofu[strings.ToLower(cloudName)] = struct{}{}
+	}
+
+	return cloudNameToUseOpenTofu, nil
 }
