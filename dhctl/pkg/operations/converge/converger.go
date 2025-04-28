f9f0f74be1695ba241d617edea340a29297d5865
Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>
2025-04-28 09:22:34 +0300

    [dhctl] Disable converge deckhouse configuration for terraform autoconverger and converge from CLI (#13226)
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- dhctl/pkg/operations/converge/converger.go
+++ dhctl/pkg/operations/converge/converger.go
@@ -326,8 +326,14 @@ func (c *Converger) Converge(ctx context.Context) (*ConvergeResult, error) {
 
 	kubectlSwitcher := convergectx.NewKubeClientSwitcher(convergeCtx, inLockRunner)
 
+	phasesToSkip := make([]phases.OperationPhase, 0)
+	if !c.CommanderMode {
+		phasesToSkip = []phases.OperationPhase{phases.DeckhouseConfigurationPhase}
+	}
+
 	r := newRunner(inLockRunner, kubectlSwitcher).
-		WithCommanderUUID(c.CommanderUUID)
+		WithCommanderUUID(c.CommanderUUID).
+		WithSkipPhases(phasesToSkip)
 
 	if c.CommanderMode {
 		log.InfoF("Need automatic migration for commander: %v\n", needAutomaticTofuMigrationForCommander)
@@ -404,7 +410,7 @@ func (c *Converger) AutoConverge() error {
 	r := newRunner(inLockRunner, convergectx.NewKubeClientSwitcher(convergeCtx, inLockRunner)).
 		WithCommanderUUID(c.CommanderUUID).
 		WithExcludedNodes([]string{app.RunningNodeName}).
-		WithSkipPhases([]phases.OperationPhase{phases.AllNodesPhase})
+		WithSkipPhases([]phases.OperationPhase{phases.AllNodesPhase, phases.DeckhouseConfigurationPhase})
 
 	converger := NewAutoConverger(r, app.AutoConvergeListenAddress, app.ApplyInterval)
 	return converger.Start(convergeCtx)
