23e86ad7776550d2b7b0f58aabf739448fa6a815
vadimartynov <vadim.martynov@flant.com>
2025-04-28 13:32:33 +0300

    Fixed copy_custom_certificate_test
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

--- ee/modules/038-system-registry/template_tests/copy_custom_certificate_test.go
+++ ee/modules/038-system-registry/template_tests/copy_custom_certificate_test.go
@@ -25,20 +25,27 @@ discovery:
     system: 1
     master: 1
 `
-const customCertificateModeWithDetachedMode = `
-mode: Detached
+const customCertificateModeIngressEnable = `
 https:
   mode: CustomCertificate
 internal:
+  orchestrator:
+    hash: "123"
+    state:
+      ingress_enabled: true
+      conditions: []
+      mode: "Local"
+      target_mode: "Local"
   customCertificateData:
     tls.crt: CRTCRTCRT
     tls.key: KEYKEYKEY
 `
 
-const customCertificateModeWithNonDetachedMode = `
+const customCertificateModeIngressDisable = `
 https:
   mode: CustomCertificate
 internal:
+  orchestrator: {}
   customCertificateData:
     tls.crt: CRTCRTCRT
     tls.key: KEYKEYKEY
@@ -47,15 +54,15 @@ internal:
 var _ = Describe("Module :: system-registry :: helm template :: custom-certificate", func() {
 	f := SetupHelmConfig(``)
 
-	Context("Detached mode", func() {
+	Context("Ingress enable", func() {
 		BeforeEach(func() {
 			f.ValuesSetFromYaml("global", globalValues)
 			f.ValuesSet("global.modulesImages", GetModulesImages())
-			f.ValuesSetFromYaml("systemRegistry", customCertificateModeWithDetachedMode)
+			f.ValuesSetFromYaml("systemRegistry", customCertificateModeIngressEnable)
 			f.HelmRender()
 		})
 
-		It("Non-empty customcertificate for detached mode", func() {
+		It("Non-empty customcertificate if ingress enbale", func() {
 			Expect(f.RenderError).ShouldNot(HaveOccurred())
 			createdSecret := f.KubernetesResource("Secret", "d8-system", "embedded-registry-ingress-tls-customcertificate")
 			Expect(createdSecret.Exists()).To(BeTrue())
@@ -64,15 +71,15 @@ var _ = Describe("Module :: system-registry :: helm template :: custom-certifica
 
 	})
 
-	Context("Non-detached mode", func() {
+	Context("Ingress disable", func() {
 		BeforeEach(func() {
 			f.ValuesSetFromYaml("global", globalValues)
 			f.ValuesSet("global.modulesImages", GetModulesImages())
-			f.ValuesSetFromYaml("systemRegistry", customCertificateModeWithNonDetachedMode)
+			f.ValuesSetFromYaml("systemRegistry", customCertificateModeIngressDisable)
 			f.HelmRender()
 		})
 
-		It("Empty customcertificate for non-detached mode", func() {
+		It("Empty customcertificate if ingress disable", func() {
 			Expect(f.RenderError).ShouldNot(HaveOccurred())
 			createdSecret := f.KubernetesResource("Secret", "d8-system", "embedded-registry-ingress-tls-customcertificate")
 			Expect(createdSecret.Exists()).To(BeFalse())
