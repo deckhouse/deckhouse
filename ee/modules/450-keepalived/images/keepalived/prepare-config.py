70dc1d0aea5f4ef7ea0ac4bee0c0190696a029f7
Artem Kladov <6360800+z9r5@users.noreply.github.com>
2025-04-29 23:30:59 +0700

    Revert "[deckhouse-controller] use our python in deckhouse-controller image (…" (#13286)
    This reverts commit 594f87ba1efb10441edf585bd3b44da82b087766.

--- ee/modules/450-keepalived/images/keepalived/prepare-config.py
+++ ee/modules/450-keepalived/images/keepalived/prepare-config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Copyright 2021 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
