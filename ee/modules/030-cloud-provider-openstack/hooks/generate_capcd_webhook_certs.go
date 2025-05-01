97b9c6957ccbf03e7494e3d81776d0a1003845c4
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2025-05-01 13:17:32 +0300

    [cloud-provide-openstack][node-manager] Add parallel work MCM and CAPI. Add CAPI Openstack provider
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

---
+++ ee/modules/030-cloud-provider-openstack/hooks/generate_capcd_webhook_certs.go
@@ -0,0 +1,25 @@
+/*
+Copyright 2024 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
+package hooks
+
+import (
+	"github.com/deckhouse/deckhouse/go_lib/hooks/tls_certificate"
+)
+
+var _ = tls_certificate.RegisterInternalTLSHook(tls_certificate.GenSelfSignedTLSHookConf{
+	SANs: tls_certificate.DefaultSANs([]string{
+		"capo-controller-manager-webhook-service.d8-cloud-provider-vcd",
+		"capo-controller-manager-webhook-service.d8-cloud-provider-vcd.svc",
+		tls_certificate.ClusterDomainSAN("capo-controller-manager-webhook-service.d8-cloud-provider-vcd"),
+		tls_certificate.ClusterDomainSAN("capo-controller-manager-webhook-service.d8-cloud-provider-vcd.svc"),
+	}),
+
+	CN: "capo-controller-manager-webhook",
+
+	Namespace:            "d8-cloud-provider-openstack",
+	TLSSecretName:        "capo-controller-manager-webhook-tls",
+	FullValuesPathPrefix: "cloudProviderVcd.internal.capoControllerManagerWebhookCert",
+})
