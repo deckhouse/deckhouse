a000efd67b7a3fbb3e8a06c7d79f6a7fc64ec373
YuryLysov <yuriy.lysov@flant.com>
2025-05-05 18:58:33 +0400

    Add copyright and update docs
    Signed-off-by: YuryLysov <yuriy.lysov@flant.com>

--- modules/040-node-manager/images/caps-controller-manager/src/api/deckhouse.io/v1alpha2/groupversion_info.go
+++ modules/040-node-manager/images/caps-controller-manager/src/api/deckhouse.io/v1alpha2/groupversion_info.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Flant JSC
+Copyright 2025 Flant JSC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1alpha1 contains API Schema definitions for the deckhouse.io v1alpha1 API group
+// Package v1alpha1 contains API Schema definitions for the deckhouse.io v1alpha2 API group
 // +kubebuilder:object:generate=true
 // +groupName=deckhouse.io
 package v1alpha2
