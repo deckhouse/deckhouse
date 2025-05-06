9fed2716b6766727e42ce112b22ea4e237792ac9
borg-z <me@nikolay-z.top>
2025-05-06 13:16:39 +0300

    ++
    Signed-off-by: borg-z <me@nikolay-z.top>

--- .github/scripts/validation_bashible.sh
+++ .github/scripts/validation_bashible.sh
@@ -126,7 +126,7 @@ clusterBootstrap:
   clusterDNSAddress: 10.222.0.10
   clusterDomain: cluster.local
   nodeIP: 192.168.199.23
-kubernetesVersion: "1.28"
+kubernetesVersion: "1.29"
 cri: "Containerd"
 nodeGroup:
   cloudInstances:
