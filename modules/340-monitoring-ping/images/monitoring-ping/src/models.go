4291734eaf0e1e7579e86404effb5cf28325b64c
Paramoshka <parfenov_ivan_42a@mail.ru>
2025-05-10 09:31:24 +0300

    Applied from review
    Signed-off-by: Paramoshka <parfenov_ivan_42a@mail.ru>

--- modules/340-monitoring-ping/images/monitoring-ping/src/models.go
+++ modules/340-monitoring-ping/images/monitoring-ping/src/models.go
@@ -1,16 +1,18 @@
-// Package ping Copyright 2025 Flant JSC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package main
 
@@ -23,16 +25,3 @@ type NodeTarget struct {
 	Name string `json:"name,omitempty"`
 	IP   string `json:"ipAddress"`
 }
-
-type InternalTargets struct {
-	Cluster []NodeTarget `json:"cluster_targets"`
-}
-
-type ExternalTargets struct {
-	Targets []ExternalTarget `json:"targets"`
-}
-
-type Targets interface {
-	Update(*NodeTracker)
-}
-
