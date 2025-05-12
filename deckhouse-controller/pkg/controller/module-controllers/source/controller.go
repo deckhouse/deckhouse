e679a045968e8a1364b17045b5e8b18ede607db3
Evsyukov Denis <denis.evsyukov@flant.com>
2025-05-12 16:52:20 +0300

    refactor: simplify return statement in getIntermediateVersions and add test for module source handling
    Signed-off-by: Evsyukov Denis <denis.evsyukov@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/source/controller.go
+++ deckhouse-controller/pkg/controller/module-controllers/source/controller.go
@@ -493,8 +493,7 @@ func (r *reconciler) getIntermediateModuleVersions(
 	}
 
 	if currentVersionStr == "" {
-		result := []*semver.Version{targetVersion}
-		return result, nil
+		return []*semver.Version{targetVersion}, nil
 	}
 
 	currentVersion, err := semver.NewVersion(currentVersionStr)
