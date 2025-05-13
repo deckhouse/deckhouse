2077a9eb8add269e6a8bc246403b15fdbbc4b189
Kirill Kramorov <kramorov-k@mail.ru>
2025-05-13 20:04:40 +0700

    feat: e2e aws (#13272)
    Signed-off-by: Kirill Kramorov <kirill.kramorov@flant.com>
    Co-authored-by: Kirill Kramorov <kirill.kramorov@flant.com>

--- testing/cloud_layouts/script-commander.sh
+++ testing/cloud_layouts/script-commander.sh
@@ -134,6 +134,19 @@ function prepare_environment() {
     SERVICE_ACCOUNT_JSON=$LAYOUT_YANDEX_SERVICE_ACCOUNT_KEY_JSON
     ssh_user="redos"
     cluster_template_id="6a47d23a-e16f-4e7a-bf57-a65f7c05e8ae"
+    values="{
+      \"branch\": \"${DEV_BRANCH}\",
+      \"prefix\": \"a${PREFIX}\",
+      \"kubernetesVersion\": \"${KUBERNETES_VERSION}\",
+      \"defaultCRI\": \"${CRI}\",
+      \"masterCount\": \"${MASTERS_COUNT}\",
+      \"cloudId\": \"${CLOUD_ID}\",
+      \"folderId\": \"${FOLDER_ID}\",
+      \"serviceAccountJson\": \"${SERVICE_ACCOUNT_JSON}\",
+      \"sshPrivateKey\": \"${SSH_KEY}\",
+      \"sshUser\": \"${ssh_user}\",
+      \"deckhouseDockercfg\": \"${DECKHOUSE_DOCKERCFG}\"
+    }"
     ;;
 
   "GCP")
@@ -147,13 +160,20 @@ function prepare_environment() {
     ;;
 
   "AWS")
-    # shellcheck disable=SC2016
-    env AWS_ACCESS_KEY="$(base64 -d <<< "$LAYOUT_AWS_ACCESS_KEY")" AWS_SECRET_ACCESS_KEY="$(base64 -d <<< "$LAYOUT_AWS_SECRET_ACCESS_KEY")" \
-        KUBERNETES_VERSION="$KUBERNETES_VERSION" CRI="$CRI" DEV_BRANCH="$DEV_BRANCH" PREFIX="$PREFIX" DECKHOUSE_DOCKERCFG="$DECKHOUSE_DOCKERCFG" MASTERS_COUNT="$MASTERS_COUNT" \
-        envsubst '${DECKHOUSE_DOCKERCFG} ${PREFIX} ${DEV_BRANCH} ${KUBERNETES_VERSION} ${CRI} ${AWS_ACCESS_KEY} ${AWS_SECRET_ACCESS_KEY} ${MASTERS_COUNT}' \
-        <"$cwd/configuration.tpl.yaml" >"$cwd/configuration.yaml"
-
     ssh_user="ec2-user"
+    cluster_template_id="9b567623-91a9-4493-96de-f5c0b6acacfe"
+    values="{
+      \"branch\": \"${DEV_BRANCH}\",
+      \"prefix\": \"a${PREFIX}\",
+      \"kubernetesVersion\": \"${KUBERNETES_VERSION}\",
+      \"defaultCRI\": \"${CRI}\",
+      \"masterCount\": \"${MASTERS_COUNT}\",
+      \"awsAccessKey\": \"${LAYOUT_AWS_ACCESS_KEY}\",
+      \"awsSecretKey\": \"${LAYOUT_AWS_SECRET_ACCESS_KEY}\",
+      \"sshPrivateKey\": \"${SSH_KEY}\",
+      \"sshUser\": \"${ssh_user}\",
+      \"deckhouseDockercfg\": \"${DECKHOUSE_DOCKERCFG}\"
+    }"
     ;;
 
   "Azure")
@@ -457,19 +477,7 @@ function run-test() {
   payload="{
     \"name\": \"${PREFIX}\",
     \"cluster_template_version_id\": \"${cluster_template_version_id}\",
-    \"values\": {
-        \"branch\": \"${DEV_BRANCH}\",
-        \"prefix\": \"a${PREFIX}\",
-        \"kubernetesVersion\": \"${KUBERNETES_VERSION}\",
-        \"defaultCRI\": \"${CRI}\",
-        \"masterCount\": \"${MASTERS_COUNT}\",
-        \"cloudId\": \"${CLOUD_ID}\",
-        \"folderId\": \"${FOLDER_ID}\",
-        \"serviceAccountJson\": \"${SERVICE_ACCOUNT_JSON}\",
-        \"sshPrivateKey\": \"${SSH_KEY}\",
-        \"sshUser\": \"${ssh_user}\",
-        \"deckhouseDockercfg\": \"${DECKHOUSE_DOCKERCFG}\"
-    }
+    \"values\": ${values}
   }"
 
   echo "Bootstrap payload: ${payload}"
@@ -574,6 +582,21 @@ function run-test() {
     echo "Ingress and Istio test failure"
     return 1
   fi
+
+  testOpenvpnReady=$(cat "$(pwd)/testing/cloud_layouts/script.d/wait_cluster_ready/test_openvpn_ready.sh")
+
+  test_failed="true"
+    if $ssh_command $ssh_bastion "$ssh_user@$master_ip" \
+      sudo su -c /bin/bash <<<"${testOpenvpnReady}"; then
+      test_failed=""
+    else
+      >&2 echo "OpenVPN test failed for Static provider. Sleeping 30 seconds..."
+      sleep 30
+    fi
+
+    if [[ $test_failed == "true" ]]; then
+      return 1
+    fi
 
   if [[ -n ${SWITCH_TO_IMAGE_TAG} ]]; then
     echo "Starting switch deckhouse image"
