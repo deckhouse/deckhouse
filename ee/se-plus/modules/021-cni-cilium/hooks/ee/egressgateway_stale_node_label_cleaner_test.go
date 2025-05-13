f80a2519104470aec78d1d91d6fba15823970faf
Andrey Polovov <andrey.polovov@flant.com>
2025-05-13 12:35:58 +0300

    Update ee/se-plus/modules/021-cni-cilium/hooks/ee/egressgateway_stale_node_label_cleaner_test.go
    Signed-off-by: Andrey Polovov <andrey.polovov@flant.com>

--- ee/se-plus/modules/021-cni-cilium/hooks/ee/egressgateway_stale_node_label_cleaner_test.go
+++ ee/se-plus/modules/021-cni-cilium/hooks/ee/egressgateway_stale_node_label_cleaner_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Flant JSC
+Copyright 2025 Flant JSC
 Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 */
 
