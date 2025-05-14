3735ecb0b7d8d336fd85da6b2f6b67021744b5d2
Evsyukov Denis <denis.evsyukov@flant.com>
2025-05-14 16:26:14 +0300

    refactor: update version list in TestCreateReconcile for consistency
    Signed-off-by: Evsyukov Denis <denis.evsyukov@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/source/controller_test.go
+++ deckhouse-controller/pkg/controller/module-controllers/source/controller_test.go
@@ -324,7 +324,7 @@ func (suite *ControllerTestSuite) TestCreateReconcile() {
 		dc := newMockedContainerWithData(suite.T(),
 			"v1.7.1",
 			[]string{"parca"},
-			[]string{"v1.4.1", "v1.5.2", "v1.5.3", "v1.6.1", "v1.6.2", "v1.7.1", "v1.7.2"})
+			[]string{"v1.3.1", "v1.4.1", "v1.5.2", "v1.5.3", "v1.6.1", "v1.6.2", "v1.7.1", "v1.7.2"})
 		suite.setupTestController(string(suite.parseTestdata("modulewithmr-registry.yaml")), withDependencyContainer(dc))
 		_, err := suite.r.handleModuleSource(context.TODO(), suite.moduleSource(suite.source))
 		require.NoError(suite.T(), err)
