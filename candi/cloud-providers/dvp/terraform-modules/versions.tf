d6d78f255025affabe17bce987aa5ee94ff4b914
Alexey Sulimenko <alexey.sulimenko@flant.com>
2025-05-15 10:02:56 +0300

    tofu
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- candi/cloud-providers/dvp/terraform-modules/versions.tf
+++ candi/cloud-providers/dvp/terraform-modules/versions.tf
@@ -16,7 +16,7 @@ terraform {
   required_version = ">= 0.14.8"
   required_providers {
     kubernetes = {
-      source  = "hashicorp/kubernetes"
+      source  = "terraform-provider-kubernetes/kubernetes"
       version = ">= 2.35.1"
     }
   }
