752ad988687e1f05ab53b1f7d18fba8b449d8049
Alexey Voronov <alexey.voronov@flant.com>
2025-05-16 15:29:11 +0700

    [cloud-provider-vsphere] add module config conversions and its tests
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

---
+++ ee/se-plus/modules/030-cloud-provider-vsphere/openapi/conversions/conversions_test.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package conversions
+
+import (
+	"testing"
+
+	"github.com/deckhouse/deckhouse/go_lib/configtools/conversion"
+)
+
+func TestOpenVpnConversions(t *testing.T) {
+	conversions := "."
+	cases := []struct {
+		name            string
+		settings        string
+		expected        string
+		currentVersion  int
+		expectedVersion int
+	}{
+		{
+			name: "should convert from 1 to 2 version and capitalize the value of the storageClass.compatibilityFlag field",
+			settings: `
+storageClass:
+  exclude: qwerty123
+  compatibilityFlag: migration
+`,
+			expected: `
+storageClass:
+  exclude: qwerty123
+  compatibilityFlag: Migration
+`,
+			currentVersion:  1,
+			expectedVersion: 2,
+		},
+		{
+			name: "should convert from 1 to 2 version and do nothing if the storageClass.compatibilityFlag field does not exists",
+			settings: `
+storageClass:
+  exclude: qwerty123
+`,
+			expected: `
+storageClass:
+  exclude: qwerty123
+`,
+			currentVersion:  1,
+			expectedVersion: 2,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			err := conversion.TestConvert(c.settings, c.expected, conversions, c.currentVersion, c.expectedVersion)
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
