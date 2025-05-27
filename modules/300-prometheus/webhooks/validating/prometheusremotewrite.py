c03b7495d26ddf789f20de8f0de12df94a1478d1
Sergey Kurkin <sergey.kurkin@flant.com>
2025-05-27 15:06:12 +0300

    fix except error
    Signed-off-by: Sergey Kurkin <sergey.kurkin@flant.com>

--- modules/300-prometheus/webhooks/validating/prometheusremotewrite.py
+++ modules/300-prometheus/webhooks/validating/prometheusremotewrite.py
@@ -18,7 +18,7 @@ from typing import Optional
 from deckhouse import hook
 from dotmap import DotMap
 import cryptography
-from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidKey
 
 config = """
 configVersion: v1
@@ -97,7 +97,7 @@ def check_verify_ca_signatures(ctx: DotMap) -> Optional[str]:
     try:
         cryptography.x509.load_pem_x509_certificate(ca.encode(), cryptography.hazmat.backends())
         return None
-    except (ValueError, cryptography.exceptions.InvalidKey) as e:
+    except (ValueError, InvalidKey) as e:
         return f"Certificate verification failed: {e}"
     
 if __name__ == "__main__":
