639b2408be201c9fcdf26e22f111537a564e9144
Zhbert <konstantin.nezhbert@flant.com>
2025-05-28 17:30:29 +0300

    Updates
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- tools/docs/run_external_modules.sh
+++ tools/docs/run_external_modules.sh
@@ -18,10 +18,6 @@ EXT_MODULES_DIR=$PWD/external_modules
 BUILDER_TEMPLATE_PATH=$PWD/docs/site/backends/docs-builder-template
 CONTENT_PATH=$BUILDER_TEMPLATE_PATH/content/modules
 CRDS_PATH=$BUILDER_TEMPLATE_PATH/data/modules
-MODULES=(
-    "https://github.com/deckhouse/sds-replicated-volume"
-    "https://github.com/deckhouse/sds-node-configurator"
-)
 
 function create_ext_modules_path () {
     test -e $EXT_MODULES_DIR || mkdir $EXT_MODULES_DIR
@@ -31,75 +27,64 @@ function clean () {
     rm -rf $EXT_MODULES_DIR
 }
 
-function get_modules () {
-    for ix in ${!MODULES[*]}
-    do
-        unset MODULE_NAME
-        MODULE_NAME=$(echo ${MODULES[$ix]} | sed 's/.*\///')
-        echo "Getting module $MODULE_NAME"
-        module_path=$(echo $EXT_MODULES_DIR/$MODULE_NAME)
-        test -e $module_path || mkdir $CONTENT_PATH/$MODULE_NAME
-        test -e $module_path || git clone ${MODULES[$ix]} $module_path
-    done
-}
+function prepare_template () {
+    echo "Preparing docs of module $MODULE_NAME..."
+    module_path=$(echo $EXT_MODULES_DIR/$MODULE_NAME)
+    module_content_path=$(echo "$module_path/docs")
+    template_content_path=$(echo "$CONTENT_PATH/$MODULE_NAME/alpha")
+    test -e $template_content_path || mkdir -p $template_content_path
+    cp -r $module_content_path/*.md $template_content_path
+    find $template_content_path -name '*_RU.md' | sed 'p;s:_RU:.ru:g' | xargs -n2 mv
 
-function prepare_templates () {
-    for ix in ${!MODULES[*]}
-    do
-        unset MODULE_NAME
-        MODULE_NAME=$(echo ${MODULES[$ix]} | sed 's/.*\///')
-        echo "Preparing docs of module $MODULE_NAME..."
-        module_path=$(echo $EXT_MODULES_DIR/$MODULE_NAME)
-        module_content_path=$(echo "$module_path/docs")
-        template_content_path=$(echo "$CONTENT_PATH/$MODULE_NAME/alpha")
-        test -e $template_content_path || mkdir -p $template_content_path
-        cp -r $module_content_path/*.md $template_content_path
-        find $template_content_path -name '*_RU.md' | sed 'p;s:_RU:.ru:g' | xargs -n2 mv
+    echo "Preparing CRDs of module $MODULE_NAME..."
+    template_crds_path=$(echo "$CRDS_PATH/$MODULE_NAME/alpha/crds")
+    module_crds_path=$(echo "$module_path/crds")
+    test -e $template_crds_path || mkdir -p $template_crds_path
+    cp -r $module_crds_path/*.yaml $template_crds_path
 
-        echo "Preparing CRDs of module $MODULE_NAME..."
-        template_crds_path=$(echo "$CRDS_PATH/$MODULE_NAME/alpha/crds")
-        module_crds_path=$(echo "$module_path/crds")
-        test -e $template_crds_path || mkdir -p $template_crds_path
-        cp -r $module_crds_path/*.yaml $template_crds_path
-
-        echo "Preparing OpenAPI of module $MODULE_NAME..."
-        template_openapi_path=$(echo "$CRDS_PATH/$MODULE_NAME/alpha/openapi")
-        module_openapi_path=$(echo "$module_path/openapi")
-        test -e $template_openapi_path || mkdir -p $template_openapi_path
-        cp -r $module_openapi_path/*.yaml $template_openapi_path
-    done
+    echo "Preparing OpenAPI of module $MODULE_NAME..."
+    template_openapi_path=$(echo "$CRDS_PATH/$MODULE_NAME/alpha/openapi")
+    module_openapi_path=$(echo "$module_path/openapi")
+    test -e $template_openapi_path || mkdir -p $template_openapi_path
+    cp -r $module_openapi_path/*.yaml $template_openapi_path
 }
 
 function prepare_channels() {
     rm $CRDS_PATH/channels.yaml
     touch $CRDS_PATH/channels.yaml
-    for ix in ${!MODULES[*]}
-    do
-        unset MODULE_NAME
-        MODULE_NAME=$(echo ${MODULES[$ix]} | sed 's/.*\///')
-        echo "Preparing channels.yaml for $MODULE_NAME..."
-        echo "$MODULE_NAME:" >> $CRDS_PATH/channels.yaml
-        echo "  channels:" >> $CRDS_PATH/channels.yaml
-        echo "    alpha:" >> $CRDS_PATH/channels.yaml
-        echo "      version: v1.1" >> $CRDS_PATH/channels.yaml
-    done
+    echo "Preparing channels.yaml for $MODULE_NAME..."
+    echo "$MODULE_NAME:" >> $CRDS_PATH/channels.yaml
+    echo "  channels:" >> $CRDS_PATH/channels.yaml
+    echo "    alpha:" >> $CRDS_PATH/channels.yaml
+    echo "      version: v1.1" >> $CRDS_PATH/channels.yaml
 }
 
 function run_hugo() {
     docker run --rm -p 1313:1313 -v $BUILDER_TEMPLATE_PATH:/src klakegg/hugo:0.111.3-ext-alpine server --renderToDisk --disableFastRender --environment production
 }
 
-if [ "$#" -eq 0 ]; then
+if [ "$1" = "clean" ]; then
+    echo "Cleaning $EXT_MODULES_DIR..."
+    rm -rf $EXT_MODULES_DIR
+elif [ "$1" = "run" ]; then
+    run_hugo
+else
+    unset MODULE_NAME
+    repo_name=$(echo $1 | sed 's/.*\///')
+    MODULE_NAME="${repo_name%.*}"
+
     create_ext_modules_path
-    get_modules
-    prepare_templates
+
+    echo "Getting module $MODULE_NAME"
+    module_path=$(echo $EXT_MODULES_DIR/$MODULE_NAME)
+    test -e $module_path || mkdir $CONTENT_PATH/$MODULE_NAME
+    if [ $# -lt 2 ]; then
+      test -e $module_path || git clone $1 $module_path
+    else
+      test -e $module_path || git clone --branch $2 --single-branch $1 $module_path
+    fi
+
+    prepare_template
     prepare_channels
     clean
-else
-    if [ "$1" = "clean" ]; then
-        echo "Cleaning $EXT_MODULES_DIR..."
-        rm -rf $EXT_MODULES_DIR
-    elif [ "$1" = "run" ]; then
-        run_hugo
-    fi
 fi
