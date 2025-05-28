a1e8c926221b052d5c9da30e46b7313b95e3c769
Dmitry Tumanov <dmitry.tumanov@flant.com>
2025-05-28 18:05:27 +0300

    (vcd): fixes

--- go_lib/cloud-data/discovery/vcd/discoverer.go
+++ go_lib/cloud-data/discovery/vcd/discoverer.go
@@ -1,9 +1,4 @@
-/*
-Copyright 2023 Flant JSC
-Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
-*/
-
-package main
+package vcd
 
 import (
 	"context"
