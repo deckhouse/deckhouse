deeb23e189bd9e4673e67d183bb9bd5d16df0cdd
Alexey Voronov <alexey.voronov@flant.com>
2025-05-29 23:22:53 +0700

    fix hook formatting
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/modules/030-cloud-provider-vcd/hooks/legacy_mode.go
+++ ee/modules/030-cloud-provider-vcd/hooks/legacy_mode.go
@@ -12,7 +12,7 @@ import (
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
 	"github.com/flant/addon-operator/sdk"
 	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
-  v1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/util/json"
 
