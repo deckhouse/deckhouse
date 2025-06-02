f17509b56b2de6f6d9ae11471b91aa49765b1eb5
Beastlex <36466535+Beastlex@users.noreply.github.com>
2025-06-03 05:53:14 +0700

    [cni-cilium] tunnel-port discovering with regard to the virtualization nesting level (#9996)
    Signed-off-by: Alexander Zverev <beastlex@gmail.com>
    Signed-off-by: Mikhail Scherba <mikhail.scherba@flant.com>
    Signed-off-by: Andrey Polovov <andrey.polovov@flant.com>
    Co-authored-by: Mikhail Scherba <mikhail.scherba@flant.com>
    Co-authored-by: Andrey Polovov <andrey.polovov@flant.com>
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

---
+++ global-hooks/discovery/virtualization_level.go
@@ -0,0 +1,94 @@
+// Copyright 2024 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hooks
+
+import (
+	"math"
+	"strconv"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+type masterNodeInfo struct {
+	Name                string
+	VirtualizationLevel int
+}
+
+const (
+	nodeRole               = "master"
+	masterNodeGroup        = "node.deckhouse.io/group"
+	virtualizationLevelKey = "node.deckhouse.io/dvp-nesting-level"
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:          "master_nodes",
+			ApiVersion:    "v1",
+			Kind:          "Node",
+			LabelSelector: &metav1.LabelSelector{MatchLabels: map[string]string{masterNodeGroup: nodeRole}},
+			FilterFunc:    applyMasterNodesFilter,
+		},
+	},
+}, setGlobalVirtualizationLevel)
+
+func applyMasterNodesFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	node := new(corev1.Node)
+
+	err := sdk.FromUnstructured(obj, node)
+	if err != nil {
+		return nil, err
+	}
+
+	virtualizationLevel := 0
+	if value, exists := node.GetLabels()[virtualizationLevelKey]; exists {
+		if virtualizationLevel, err = strconv.Atoi(value); err != nil {
+			// exclude a node with a faulty label
+			return nil, nil
+		}
+	}
+
+	return masterNodeInfo{Name: node.GetName(), VirtualizationLevel: virtualizationLevel}, nil
+}
+
+func setGlobalVirtualizationLevel(input *go_hook.HookInput) error {
+	virtualizationLevel := getVirtualizationLevelFromMasterNodesLabels(input.Snapshots["master_nodes"])
+	input.Values.Set("global.discovery.dvpNestingLevel", virtualizationLevel)
+	input.Logger.Infof("set DVP nesting level to: %d", virtualizationLevel)
+
+	return nil
+}
+
+func getVirtualizationLevelFromMasterNodesLabels(masterNodeInfoSnaps []go_hook.FilterResult) int {
+	minimalVirtualizationLevel := math.MaxInt
+	for _, masterNodeInfoSnap := range masterNodeInfoSnaps {
+		masterNodeInfo, ok := masterNodeInfoSnap.(masterNodeInfo)
+		if ok {
+			if masterNodeInfo.VirtualizationLevel >= 0 && masterNodeInfo.VirtualizationLevel < minimalVirtualizationLevel {
+				minimalVirtualizationLevel = masterNodeInfo.VirtualizationLevel
+			}
+		}
+	}
+
+	if minimalVirtualizationLevel == math.MaxInt {
+		minimalVirtualizationLevel = 0
+	}
+
+	return minimalVirtualizationLevel
+}
