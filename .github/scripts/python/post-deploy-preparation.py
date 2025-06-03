1d2c715b909a1dc93acf0f3a9c26f42045b4bad2
Maksim Khimchenko <maksim.khimchenko@flant.com>
2025-06-04 00:55:27 +0300

    add mask to api host
    Signed-off-by: Maksim Khimchenko <maksim.khimchenko@flant.com>

--- .github/scripts/python/post-deploy-preparation.py
+++ .github/scripts/python/post-deploy-preparation.py
@@ -136,7 +136,7 @@ def determine_clusters_need_deploy (kubeconf_name,kubeconf64):
 
     try:
         kubeconf = yaml.safe_load(base64.b64decode(kubeconf64).decode('utf-8'))
-        config.load_kube_config_from_dict(kubeconf)
+        config.load_kube_config_from_dict(kubeconf,client_configuration=cfg)
     except Exception as e:
         print(f'::warning file=.github/scripts/python/{os.path.basename(__file__)},line={inspect.currentframe().f_lineno}::Unable to load "{kubeconf_name}". Unable to connect to "{kubeconf_name}". Skipping.')
         write_output(output_prefix+kubeconf_name,'false')
@@ -146,6 +146,7 @@ def determine_clusters_need_deploy (kubeconf_name,kubeconf64):
     namespace = os.getenv(f'NAMESPACE_{kubeconf_name}')
 
     cfg.ssl_ca_cert = SSL_CERT_FILE
+    print(f'::add-mask::{cfg.host}')
     api_client = client.ApiClient(configuration=cfg)
     v1 = client.CoreV1Api(api_client)
 
