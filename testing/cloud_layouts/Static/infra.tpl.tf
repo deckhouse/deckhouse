46acb9f09bfd30dca208eb2c06ac77e0ebec4b41
Vladimir Portnov <vladimir.portnov@flant.com>
2025-06-04 22:25:32 +0800

    [ci] Fix static e2e update in legacy e2e test script (#13785)
    * [ci] Fix static e2e update in legacy e2e test script
    
    Signed-off-by: Vladimir Portnov <vladimir.portnov@flant.com>
    
    * fix static config
    
    Signed-off-by: Vladimir Portnov <vladimir.portnov@flant.com>
    
    ---------
    
    Signed-off-by: Vladimir Portnov <vladimir.portnov@flant.com>

--- testing/cloud_layouts/Static/infra.tpl.tf
+++ testing/cloud_layouts/Static/infra.tpl.tf
@@ -52,7 +52,6 @@ data "openstack_networking_network_v2" "external" {
 resource "openstack_networking_network_v2" "internal" {
   name           = "candi-${PREFIX}"
   admin_state_up = "true"
-  port_security_enabled = "false"
 }
 
 resource "openstack_networking_subnet_v2" "internal" {
