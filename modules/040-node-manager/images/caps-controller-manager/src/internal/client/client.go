d8b9bda0b7905e3d34bf702f8a3955dc5403a36e
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2025-06-05 22:38:55 +0300

    [node-manager] Fix caps
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- modules/040-node-manager/images/caps-controller-manager/src/internal/client/client.go
+++ modules/040-node-manager/images/caps-controller-manager/src/internal/client/client.go
@@ -17,6 +17,7 @@ limitations under the License.
 package client
 
 import (
+	ctrl "sigs.k8s.io/controller-runtime"
 	"time"
 
 	"caps-controller-manager/internal/event"
@@ -40,11 +41,11 @@ type Client struct {
 // NewClient creates a new Client.
 func NewClient(recorder *event.Recorder) *Client {
 	return &Client{
-		checkTaskManager:     newTaskManager(),
-		bootstrapTaskManager: newTaskManager(),
-		cleanupTaskManager:   newTaskManager(),
-		adoptTaskManager:     newTaskManager(),
-		tcpCheckTaskManager:  newTaskManager(),
+		checkTaskManager:     newTaskManager(ctrl.Log.WithName("checkTaskManager")),
+		bootstrapTaskManager: newTaskManager(ctrl.Log.WithName("bootstrapTaskManager")),
+		cleanupTaskManager:   newTaskManager(ctrl.Log.WithName("cleanupTaskManager")),
+		adoptTaskManager:     newTaskManager(ctrl.Log.WithName("adoptTaskManager")),
+		tcpCheckTaskManager:  newTaskManager(ctrl.Log.WithName("tcpCheckTaskManager")),
 		tcpCheckRateLimiter:  workqueue.NewItemExponentialFailureRateLimiter(250*time.Millisecond, time.Minute),
 		recorder:             recorder,
 	}
