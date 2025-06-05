69f20862f49b04dadf8d45d1f2b604fd6d98bbd3
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2025-06-05 10:04:39 +0300

    item 2175
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- deckhouse-controller/files/kubectl_wrapper.sh
+++ deckhouse-controller/files/kubectl_wrapper.sh
@@ -27,8 +27,8 @@ case "$kubernetes_version" in
   1.29.* | 1.30.* | 1.31.* )
     kubectl_version="1.30"
     ;;
-  1.32.* | 1.33.* | 1.34.* )
-    kubectl_version="1.33"
+  1.32.* | 1.33.* )
+    kubectl_version="1.32"
     ;;
   *)
     >&2 echo "ERROR: unsupported kubernetes version $kubernetes_version"
