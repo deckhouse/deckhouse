9df2d91eaf72124902679cab66d6ccdb502f062a
Alexey Voronov <alexey.voronov@flant.com>
2025-06-11 17:09:26 +0700

    [cloud-provider-vcd] initial terraform layout standard-with-network
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/candi/cloud-providers/vcd/terraform-modules/vapp/main.tf
+++ ee/candi/cloud-providers/vcd/terraform-modules/vapp/main.tf
@@ -1,12 +1,7 @@
 # Copyright 2023 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
-terraform {
-  required_providers {
-    vcd = {
-      source  = "vmware/vcd"
-      version = "3.14.1"
-    }
-  }
-  required_version = ">= 0.13"
+resource "vcd_vapp" "vapp" {
+  name = var.prefix
+  org  = var.providerClusterConfiguration.organization
 }
