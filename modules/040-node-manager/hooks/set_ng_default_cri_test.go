53de0a202fded8966d63f76b65fdb46154548f2c
borg-z <me@nikolay-z.top>
2025-06-16 14:29:20 +0300

    ++
    Signed-off-by: borg-z <me@nikolay-z.top>

---
+++ modules/040-node-manager/hooks/set_ng_default_cri_test.go
@@ -0,0 +1,80 @@
+/*
+Copyright 2022 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hooks
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/deckhouse/deckhouse/testing/hooks"
+)
+
+var _ = Describe("Modules :: node-manager :: hooks :: set_cri ::", func() {
+	f := HookExecutionConfigInit(`{"nodeManager":{"internal":{}}}`, `{}`)
+
+	const (
+		stateEmpty = ``
+
+		stateNG1 = `
+---
+apiVersion: deckhouse.io/v1
+kind: NodeGroup
+metadata:
+  name: ng-1
+spec: {}
+`
+
+		stateNG2 = `
+---
+apiVersion: deckhouse.io/v1
+kind: NodeGroup
+metadata:
+  name: ng-2
+spec:
+  cri:
+    type: containerdV2
+`
+	)
+
+	Context("NodeGroup without CRI", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(stateNG1, 1))
+			f.RunHook()
+		})
+
+		It("should set spec.cri.type to Containerd", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(
+				f.KubernetesGlobalResource("NodeGroup", "ng-1").Field("spec.cri.type").String(),
+			).To(MatchJSON(`"Containerd"`))
+		})
+	})
+
+	Context("NodeGroup with existing CRI", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(stateNG2, 1))
+			f.RunHook()
+		})
+
+		It("should preserve the existing spec.cri.type", func() {
+			Expect(f).To(ExecuteSuccessfully())
+			Expect(
+				f.KubernetesGlobalResource("NodeGroup", "ng-2").Field("spec.cri.type").String(),
+			).To(MatchJSON(`"containerdV2"`))
+		})
+	})
+})
