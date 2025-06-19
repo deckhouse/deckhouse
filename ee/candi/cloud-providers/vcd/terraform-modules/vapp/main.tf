ef5e84bb1512828ac68429febeb833f59bd0bf19
Alexey Voronov <alexey.voronov@flant.com>
2025-06-19 12:48:16 +0700

    [cloud-provider-vcd] power_on vapp by default
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/candi/cloud-providers/vcd/terraform-modules/vapp/main.tf
+++ ee/candi/cloud-providers/vcd/terraform-modules/vapp/main.tf
@@ -2,7 +2,7 @@
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
 resource "vcd_vapp" "vapp" {
-  name = var.providerClusterConfiguration.virtualApplicationName
-  org  = var.providerClusterConfiguration.organization
-  
+  name     = var.providerClusterConfiguration.virtualApplicationName
+  org      = var.providerClusterConfiguration.organization
+  power_on = true
 }
