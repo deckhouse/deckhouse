fd63b8bf25da906dedd236d59bb89c85e3f2205d
Denis Romanenko <denis.romanenko@flant.com>
2025-06-23 11:01:40 +0300

    ++
    Signed-off-by: Denis Romanenko <denis.romanenko@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -390,7 +390,7 @@ var DefaultImagesDigests = map[string]interface{}{
 	"registrypackages": map[string]interface{}{
 		"amazonEc2Utils220":         "imageHash-registrypackages-amazonEc2Utils220",
 		"containerd1727":            "imageHash-registrypackages-containerd1727",
-		"containerdv2211":           "imageHash-registrypackages-containerdv2211",
+		"containerd213":             "imageHash-registrypackages-containerd213",
 		"crictl129":                 "imageHash-registrypackages-crictl129",
 		"crictl130":                 "imageHash-registrypackages-crictl130",
 		"crictl131":                 "imageHash-registrypackages-crictl131",
