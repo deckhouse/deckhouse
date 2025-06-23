61be9e35b51df7a826ffb76aa083f898a09dc099
borg-z <me@nikolay-z.top>
2025-06-23 11:11:40 +0300

    d8_node_cgroup_v2_support_status
    Signed-off-by: borg-z <me@nikolay-z.top>

---
+++ modules/040-node-manager/hooks/ctr_v2_support.go
@@ -0,0 +1,106 @@
+/*
+Copyright 2023 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hooks
+
+import (
+	"time"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+const (
+	containerdV2SupportLabel = "node.deckhouse.io/containerd-v2-support"
+	cgroupV2MetricName       = "d8_node_cgroup_v2_support_status"
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Settings: &go_hook.HookConfigSettings{
+		ExecutionMinInterval: 60 * time.Second,
+	},
+	Queue: "/modules/node-manager/cgroupv2_support_metrics",
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "nodes_with_group",
+			ApiVersion: "v1",
+			Kind:       "Node",
+			LabelSelector: &v1.LabelSelector{
+				MatchExpressions: []v1.LabelSelectorRequirement{
+					{
+						Key:      nodeGroupLabel,
+						Operator: v1.LabelSelectorOpExists,
+					},
+				},
+			},
+			FilterFunc: filterNodeForCgroupV2Support,
+		},
+	},
+}, handleCgroupV2SupportMetrics)
+
+type cgroupV2SupportNode struct {
+	Name           string
+	NodeGroup      string
+	V2SupportLabel string
+}
+
+func filterNodeForCgroupV2Support(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	var node corev1.Node
+	err := sdk.FromUnstructured(obj, &node)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeGroup := node.Labels[nodeGroupLabel]
+
+	v2Support := node.Labels[containerdV2SupportLabel]
+
+	return cgroupV2SupportNode{
+		Name:           node.Name,
+		NodeGroup:      nodeGroup,
+		V2SupportLabel: v2Support,
+	}, nil
+}
+
+func handleCgroupV2SupportMetrics(input *go_hook.HookInput) error {
+	snap := input.Snapshots["nodes_with_group"]
+
+	for _, s := range snap {
+		nodeInfo := s.(cgroupV2SupportNode)
+
+		var metricValue float64
+		switch nodeInfo.V2SupportLabel {
+		case "true":
+			metricValue = 1.0
+		case "false":
+			metricValue = 0.0
+		default:
+			metricValue = -1.0
+		}
+
+		labels := map[string]string{
+			"node":       nodeInfo.Name,
+			"node_group": nodeInfo.NodeGroup,
+		}
+
+		input.MetricsCollector.Set(cgroupV2MetricName, metricValue, labels)
+	}
+
+	return nil
+}
