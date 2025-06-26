bb5e657033fd648ea90d9f39a1dbb8ce32d2b441
Alexey Voronov <alexey.voronov@flant.com>
2025-06-26 13:33:04 +0700

    [cloud-provider-vcd] remove firewall module
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/candi/cloud-providers/vcd/layouts/standard-with-network/base-infrastructure/main.tf
+++ ee/candi/cloud-providers/vcd/layouts/standard-with-network/base-infrastructure/main.tf
@@ -1,10 +1,6 @@
 # Copyright 2025 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
-locals {
-  useNSXT = var.providerClusterConfiguration.edgeGatewayType == "NSX-T"
-}
-
 module "network" {
   source                       = "../../../terraform-modules/network"
   providerClusterConfiguration = var.providerClusterConfiguration
@@ -21,12 +17,4 @@ resource "vcd_vapp_org_network" "vapp_network" {
   vapp_name              = module.vapp.name
   org_network_name       = module.network.name
   reboot_vapp_on_removal = true
-}
-
-module "firewall-nsx-t" {
-  count = local.useNSXT ? 1 : 0
-  source = "../../../terraform-modules/firewall-nsx-t"
-  providerClusterConfiguration = var.providerClusterConfiguration
-  mainNetworkId = module.network.networkId
-  edgeGatewayId = module.network.edgeGatewayId
 }
