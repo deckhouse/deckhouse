a22b913ea063b72ab5b546018a65fcdb71eb8cb8
Alexey Voronov <alexey.voronov@flant.com>
2025-06-26 13:15:49 +0700

    Revert vApp network
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/candi/cloud-providers/vcd/terraform-modules/network/outputs.tf
+++ ee/candi/cloud-providers/vcd/terraform-modules/network/outputs.tf
@@ -2,9 +2,13 @@
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
 output "name" {
-  value = vcd_vapp_network.network.name
+  value = vcd_network_routed_v2.network.name  
 }
 
 output "networkId" {
-  value = vcd_vapp_network.network.id
+  value = vcd_network_routed_v2.network.id
+}
+
+output "edgeGatewayId" {
+  value = local.edgeGatewayId
 }
