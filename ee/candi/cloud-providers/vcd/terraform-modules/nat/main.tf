b2e473a0565386129943005f9edc630062264682
Alexey Voronov <alexey.voronov@flant.com>
2025-07-01 13:02:12 +0700

    [cloud-provider-vcd] serialize network and nat modules
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/candi/cloud-providers/vcd/terraform-modules/nat/main.tf
+++ ee/candi/cloud-providers/vcd/terraform-modules/nat/main.tf
@@ -3,7 +3,6 @@
 
 locals {
   useNSXT       = var.providerClusterConfiguration.edgeGatewayType == "NSX-T"
-  edgeGatewayId = local.useNSXT ? data.vcd_nsxt_edgegateway.gateway[0].id : data.vcd_edgegateway.gateway[0].id
   main_ip_addresses = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "mainNetworkIPAddresses", [])
 }
 
@@ -11,7 +10,7 @@ resource "vcd_nsxt_nat_rule" "snat" {
   count = local.useNSXT ? 1 : 0
   org = var.providerClusterConfiguration.organization
 
-  edge_gateway_id = local.edgeGatewayId
+  edge_gateway_id = var.edgeGatewayId
 
   name        = format("%-snat", var.providerClusterConfiguration.mainNetwork)
   rule_type   = "SNAT"
@@ -30,10 +29,10 @@ data "vcd_nsxt_app_port_profile" "ssh" {
 }
 
 resource "vcd_nsxt_nat_rule" "masters-dnat" {
-  count = local.useNSXT ? 1 : 0 && length(local.main_ip_addresses) > 0
+  count = (local.useNSXT && length(local.main_ip_addresses) > 0) ? 1 : 0
   org = var.providerClusterConfiguration.organization
 
-  edge_gateway_id = local.edgeGatewayId
+  edge_gateway_id = var.edgeGatewayId
 
   name        = format("%s-dnat-ssh", var.providerClusterConfiguration.mainNetwork)
   rule_type   = "DNAT"
@@ -43,5 +42,5 @@ resource "vcd_nsxt_nat_rule" "masters-dnat" {
   dnat_external_port  = var.providerClusterConfiguration.edgeGatewayExternalPort
   internal_address    = local.main_ip_addresses[count.index]
   logging             = false
-  app_port_profile_id = data.vcd_nsxt_app_port_profile.ssh.id
+  app_port_profile_id = data.vcd_nsxt_app_port_profile.ssh[0].id
 }
