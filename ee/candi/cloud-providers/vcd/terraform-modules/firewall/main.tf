784233ea4c60e5bb499923069a58ffd96037c0a5
Alexey Voronov <alexey.voronov@flant.com>
2025-07-04 14:07:36 +0700

    initial firewall module
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

---
+++ ee/candi/cloud-providers/vcd/terraform-modules/firewall/main.tf
@@ -0,0 +1,82 @@
+# Copyright 2025 Flant JSC
+# Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+
+resource "vcd_nsxt_ip_set" "internal_network" {
+  count           = var.useNSXV ? 0 : 1
+  org             = var.providerClusterConfiguration.organization
+  edge_gateway_id = var.edgeGatewayId
+
+  name         = var.providerClusterConfiguration.mainNetwork
+  description  = format("%s CIDR", var.providerClusterConfiguration.mainNetwork)
+  ip_addresses = [var.providerClusterConfiguration.internalNetworkCIDR]
+}
+
+data "vcd_nsxt_app_port_profile" "ssh" {
+  count = var.useNSXV ? 0 : 1
+  org   = var.providerClusterConfiguration.organization
+
+  name  = "SSH"
+  scope = "SYSTEM"
+}
+
+resource "vcd_nsxt_app_port_profile" "node_ports" {
+  count       = var.useNSXV ? 0 : 1
+  org         = var.providerClusterConfiguration.organization
+  description = "Node ports for Kubernetes"
+
+  name  = "NODE PORTS"
+  scope = "SYSTEM"
+
+  app_port {
+    protocol = "TCP"
+    port     = ["30000-32767"]
+  }
+}
+
+resource "vcd_nsxt_firewall" "firewall" {
+  count           = var.useNSXV ? 0 : 1
+  org             = var.providerClusterConfiguration.organization
+  edge_gateway_id = var.edgeGatewayId
+
+  rule {
+    enabled              = true
+    action               = "ALLOW"
+    name                 = format("%s-inbound-ssh", var.providerClusterConfiguration.mainNetwork)
+    direction            = "IN"
+    ip_protocol          = "IPV4"
+    source_ids           = ""
+    destination_ids      = vcd_nsxt_ip_set.internal_network.id
+    app_port_profile_ids = [data.vcd_nsxt_app_port_profile.ssh.id]
+  }
+
+  rule {
+    enabled         = true
+    action          = "ALLOW"
+    name            = format("%s-inbound-icmp", var.providerClusterConfiguration.mainNetwork)
+    direction       = "IN"
+    ip_protocol     = "ICMP"
+    source_ids      = ""
+    destination_ids = vcd_nsxt_ip_set.internal_network.id
+  }
+
+  rule {
+    enabled              = true
+    action               = "ALLOW"
+    name                 = format("%s-inbound-node-ports", var.providerClusterConfiguration.mainNetwork)
+    direction            = "IN"
+    ip_protocol          = "IPV4"
+    source_ids           = ""
+    destination_ids      = vcd_nsxt_ip_set.internal_network.id
+    app_port_profile_ids = [vcd_nsxt_app_port_profile.node_ports.id]
+  }
+
+  rule {
+    enabled         = true
+    action          = "ALLOW"
+    name            = format("%s-outbound-any", var.providerClusterConfiguration.mainNetwork)
+    direction       = "OUT"
+    ip_protocol     = "IPV4"
+    source_ids      = vcd_nsxt_ip_set.internal_network.id
+    destination_ids = ""
+  }
+}
