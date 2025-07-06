b43a719489846f1a3cfb6055528486d63fa008ae
Artem Kladov <artem.kladov@flant.com>
2025-07-06 16:26:06 +0300

    Updates
    Signed-off-by: Artem Kladov <artem.kladov@flant.com>

--- docs/documentation/_plugins/breadcrumbs_generator.rb
+++ docs/documentation/_plugins/breadcrumbs_generator.rb
@@ -7,7 +7,6 @@ Jekyll::Hooks.register :site, :pre_render do |site|
   puts "Generating breadcrumbs..."
 
   site.data['breadcrumbs'] = {} if site.data['breadcrumbs'].nil?
-  #breadcrumbsSite = site.data['breadcrumbs']
 
   site.data['sidebars'].each do |item|
     sidebarData = item[1]
@@ -16,18 +15,11 @@ Jekyll::Hooks.register :site, :pre_render do |site|
     if sidebarData['entries']
       sidebarData['entries'].each do |entry|
         if entry.is_a?(Hash) && entry['folders'].is_a?(Array)
-          #breadcrumbs = processSidebarItem([], entry)
-          #breadcrumbsSite.merge(breadcrumbs) if breadcrumbs && !breadcrumbs.empty?
           site.data['breadcrumbs'] = site.data['breadcrumbs'].merge(processSidebarItem([], entry))
         end
       end
     end
   end
-
-  #puts "[DEBUG]Breadcrumbs..."
-  #puts breadcrumbsSite
-  #File.open('breadcrumbs2.yaml', 'w') { |f| f.write(breadcrumbsSite.to_yaml) }
-  #raise
 end
 
 def processSidebarItem(parents, sidebarItem)
@@ -38,33 +30,13 @@ def processSidebarItem(parents, sidebarItem)
 
     if sidebarItem['folders'] && sidebarItem['folders'].is_a?(Array)
       sidebarItem['folders'].each do |folder|
-          #breadcrumbsFolders = processSidebarItem(parents + [{'title' => sidebarItem['title']}], folder)
-#          breadcrumbs + breadcrumbsFolders if breadcrumbsFolders && !breadcrumbsFolders.empty?
           breadcrumbs = breadcrumbs.merge(processSidebarItem(parents + [{'title' => sidebarItem['title']}], folder))
-          #puts '####################'
-          #puts breadcrumbsFolders
-          #puts 'breadcrumbs-----------'
-          #puts breadcrumbs
       end
     else
       if sidebarItem['url']
-        #section_url = sidebarItem['url'].split('/')[0...-1].join('/')
         section_url = sidebarItem['url']
         breadcrumbs[section_url] = parents
-#        breadcrumbs << {
-#          section_url => {
-#            'title' => {
-#              'en' => parents['en'],
-#              'ru' => parents['ru'],
-#            }
-#          }
-#        }
-#              'title' => {
-#                'en' => folder['title']['en'],
-#                'ru' => folder['title']['ru']
-#              }
       end
     end
-    #raise "DEBUG #{breadcrumbs}"
     breadcrumbs
 end
