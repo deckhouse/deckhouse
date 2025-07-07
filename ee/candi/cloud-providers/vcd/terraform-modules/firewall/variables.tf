e5db3221ff933a85af6e594a6fd444fc6d0927a3
Alexey Voronov <alexey.voronov@flant.com>
2025-07-07 12:53:50 +0700

    firewall and validation debug
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/candi/cloud-providers/vcd/terraform-modules/firewall/variables.tf
+++ ee/candi/cloud-providers/vcd/terraform-modules/firewall/variables.tf
@@ -5,13 +5,8 @@ variable "providerClusterConfiguration" {
   type = any
 
   validation {
-    condition     = cidrsubnet(var.providerClusterConfiguration.internalNetworkCIDR, 0, 0) == var.providerClusterConfiguration.internalNetworkCIDR
+    condition     = contains(keys(var.providerClusterConfiguration), "internalNetworkCIDR") ? cidrsubnet(var.providerClusterConfiguration.internalNetworkCIDR, 0, 0) == var.providerClusterConfiguration.internalNetworkCIDR : true
     error_message = "Invalid internalNetworkCIDR in VCDClusterConfiguration."
-  }
-
-  validation {
-    condition     = length(flatten([for ng in var.providerClusterConfiguration.nodeGroups : ng.instanceClass.mainNetworkIPAddresses if contains(keys(ng.instanceClass), "mainNetworkIPAddresses")])) == length(flatten([for ng in var.providerClusterConfiguration.nodeGroups : [for s in ng.instanceClass.mainNetworkIPAddresses : s if cidrsubnet(format("%s/%s", s, split("/", var.providerClusterConfiguration.internalNetworkCIDR)[1]), 0, 0) == var.providerClusterConfiguration.internalNetworkCIDR] if contains(keys(ng.instanceClass), "mainNetworkIPAddresses")]))
-    error_message = "Address in mainNetworkIPAddresses not in internalNetworkCIDR."
   }
 }
 
