8916369cd09d64f3000866d1107dfbce85ec9769
Denis Tarabrin <denis.tarabrin@flant.com>
2025-07-09 14:14:13 +0400

    [ingress-nginx] Distroless
    Signed-off-by: Denis Tarabrin <denis.tarabrin@flant.com>

--- modules/402-ingress-nginx/images/controller-1-12/curl-chroot-wrapper.sh
+++ modules/402-ingress-nginx/images/controller-1-12/curl-chroot-wrapper.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-unshare -S 101 -R /chroot /usr/bin/curl "$@"
+unshare -S 64535 -R /chroot /usr/bin/curl "$@"
