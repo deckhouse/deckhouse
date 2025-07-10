a2b73b203ae990b0b38578ecc69a4205051cdc4f
Sinelnikov Michail <mikhail.sinelnikov@flant.com>
2025-07-10 14:18:57 +0300

    fix
    Signed-off-by: Sinelnikov Michail <mikhail.sinelnikov@flant.com>

--- go_lib/dependency/extenders/experimental/experimental_test.go
+++ go_lib/dependency/extenders/experimental/experimental_test.go
@@ -65,7 +65,6 @@ func TestExtender_Filter(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			logger := log.NewNop()
 			e := NewExtender(tt.allow, logger)
