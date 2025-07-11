aa2b1cb84d3ad2a65e585432589b93f09c0bb677
Michail Sinelnikov <72449391+RottenRat@users.noreply.github.com>
2025-07-11 17:27:29 +0300

    [deckhouse-controller] New HookInput Snapshot logic [second-iteration] (#13941)
    Signed-off-by: Sinelnikov Michail <mikhail.sinelnikov@flant.com>
    Signed-off-by: Michail Sinelnikov <72449391+RottenRat@users.noreply.github.com>
    Co-authored-by: Sinelnikov Michail <mikhail.sinelnikov@flant.com>

--- modules/040-node-manager/hooks/convert_static_cluster_configuration.go
+++ modules/040-node-manager/hooks/convert_static_cluster_configuration.go
@@ -58,14 +58,16 @@ var _ = sdk.RegisterFunc(&go_hook.HookConfig{
 }, convertStaticClusterConfigurationHandler)
 
 func convertStaticClusterConfigurationHandler(input *go_hook.HookInput) error {
-	secret := input.Snapshots["static_cluster_configuration"]
+	secret := input.NewSnapshots.Get("static_cluster_configuration")
+
 	if len(secret) == 0 {
 		return nil
 	}
 
-	staticConfiguration, ok := secret[0].([]byte)
-	if !ok {
-		return fmt.Errorf("static_cluster_configuration filterFunc problem: expect []byte, got %T", staticConfiguration)
+	staticConfiguration := make([]byte, 0)
+	err := secret[0].UnmarshalTo(&staticConfiguration)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal first 'static_cluster_configuration' snapshot: %w", err)
 	}
 
 	internalNetwork, err := internalNetworkFromStaticConfiguration(staticConfiguration)
