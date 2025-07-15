29afcabca7757f877c77407541ffb9e113689e9f
Alexey Voronov <alexey.voronov@flant.com>
2025-07-15 18:10:24 +0700

    move dnat rule into master module
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/candi/cloud-providers/vcd/layouts/standard-with-network/master-node/main.tf
+++ ee/candi/cloud-providers/vcd/layouts/standard-with-network/master-node/main.tf
@@ -11,3 +11,30 @@ module "master_node" {
   cloudConfig                  = var.cloudConfig
   resourceManagementTimeout    = var.resourceManagementTimeout
 }
+
+locals {
+  useNSXV        = var.providerClusterConfiguration.edgeGateway.type == "NSX-V"
+  edgeGatewayId  = local.useNSXV ? data.vcd_edgegateway.gateway[0].id : data.vcd_nsxt_edgegateway.gateway[0].id
+}
+
+data "vcd_nsxt_edgegateway" "gateway" {
+  count = local.useNSXV ? 0 : 1
+  org   = var.providerClusterConfiguration.organization
+  name  = var.providerClusterConfiguration.edgeGateway.name
+}
+
+data "vcd_edgegateway" "gateway" {
+  count = local.useNSXV ? 1 : 0
+  org   = var.providerClusterConfiguration.organization
+  name  = var.providerClusterConfiguration.edgeGateway.name
+}
+
+module "dnat" {
+  source = "../../../terraform-modules/dnat"
+
+  providerClusterConfiguration = var.providerClusterConfiguration
+  edgeGatewayId                = local.edgeGatewayId
+  useNSXV                      = local.useNSXV
+  nodeIndex                    = var.nodeIndex
+  master_node_ip               = module.master_node.master_ip_address_for_ssh
+}
