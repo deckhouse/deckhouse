fa12d6fb3784e151641457faa085817220a75cb6
YuryLysov <yuriy.lysov@flant.com>
2025-07-19 03:03:55 +0400

    [node-manager] Remove duplicate fields from bashible context (#14371)
    Signed-off-by: YuryLysov <yuriy.lysov@flant.com>

--- modules/040-node-manager/images/bashible-apiserver/src/pkg/template/context_builder.go
+++ modules/040-node-manager/images/bashible-apiserver/src/pkg/template/context_builder.go
@@ -301,7 +301,6 @@ func (cb *ContextBuilder) newBundleNGContext(ng nodeGroup, freq interface{}, clo
 		tplContextCommon:          contextCommon,
 		KubernetesVersion:         ng.KubernetesVersion(),
 		CRI:                       ng.CRIType(),
-		GPU:                       ng.GPUSharingType(),
 		NodeGroup:                 ng,
 		CloudProvider:             cloudProvider,
 		NodeStatusUpdateFrequency: freq,
@@ -357,16 +356,6 @@ func (ng nodeGroup) CRIType() string {
 	if cri, ok := ng["cri"]; ok {
 		if typ, ok := cri.(map[string]interface{})["type"]; ok {
 			return typ.(string)
-		}
-	}
-
-	return ""
-}
-
-func (ng nodeGroup) GPUSharingType() string {
-	if gpu, ok := ng["gpu"]; ok {
-		if gputype, ok := gpu.(map[string]interface{})["sharing"]; ok {
-			return gputype.(string)
 		}
 	}
 
@@ -438,7 +427,6 @@ type bundleNGContext struct {
 
 	KubernetesVersion string      `json:"kubernetesVersion" yaml:"kubernetesVersion"`
 	CRI               string      `json:"cri" yaml:"cri"`
-	GPU               string      `json:"gpu,omitempty" yaml:"gpu,omitempty"`
 	NodeGroup         nodeGroup   `json:"nodeGroup" yaml:"nodeGroup"`
 	CloudProvider     interface{} `json:"cloudProvider,omitempty" yaml:"cloudProvider,omitempty"`
 
