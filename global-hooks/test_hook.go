0140ecc1e0adc3e9768a11f7e97c112d060b073b
Timur Tuktamyshev <timur.tuktamyshev@flant.com>
2025-07-22 16:19:35 +0300

    test
    Signed-off-by: Timur Tuktamyshev <timur.tuktamyshev@flant.com>

---
+++ global-hooks/test_hook.go
@@ -0,0 +1,50 @@
+package hooks
+
+import (
+	"fmt"
+
+	sdkobjectpatch "github.com/deckhouse/module-sdk/pkg/object-patch"
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Queue: "/test/read-pods",
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "pods",
+			ApiVersion: "v1",
+			Kind:       "Pod",
+			FilterFunc: func(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+				pod := v1.Pod{}
+				err := sdk.FromUnstructured(obj, &pod)
+				if err != nil {
+					return nil, err
+				}
+				return obj, nil
+			},
+		},
+	},
+}, func(input *go_hook.HookInput) error {
+	pods, err := sdkobjectpatch.UnmarshalToStruct[v1.Pod](input.NewSnapshots, "pods")
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal pods: %w", err)
+	}
+	fmt.Printf("[test_hook] Pods in cluster: %d\n", len(pods))
+
+	for _, pod := range pods {
+		patch := map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"annotations": map[string]interface{}{
+					"test-hook": "true",
+				},
+			},
+		}
+		fmt.Printf("[test_hook] Patching pod %s in namespace %s\n", pod.Name, pod.Namespace)
+		input.PatchCollector.PatchWithMerge(patch, "v1", "Pod", pod.Namespace, pod.Name)
+	}
+	fmt.Printf("[test_hook] Patched %d pods\n", len(pods))
+	return nil
+})
