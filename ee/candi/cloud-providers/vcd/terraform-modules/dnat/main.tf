a2b5e9a09193aa5bd41c382bf0f8f3a3ed0cc066
Alexey Sulimenko <alexey.sulimenko@flant.com>
2025-07-24 13:45:43 +0300

    dnat to all masters
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>

--- ee/candi/cloud-providers/vcd/terraform-modules/dnat/main.tf
+++ ee/candi/cloud-providers/vcd/terraform-modules/dnat/main.tf
@@ -2,8 +2,7 @@
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
 locals {
-  is_first_node = var.node_index == 0
-  use_nsxv      = var.edge_gateway_type == "NSX-V"
+  use_nsxv = var.edge_gateway_type == "NSX-V"
 }
 
 data "vcd_nsxt_edgegateway" "gateway" {
@@ -15,21 +14,21 @@ data "vcd_nsxt_edgegateway" "gateway" {
 # NSX-T DNAT rule only for the first master node
 
 data "vcd_nsxt_app_port_profile" "ssh" {
-  count = local.is_first_node ? (local.use_nsxv ? 0 : 1) : 0
+  count = local.use_nsxv ? 0 : 1
   org   = var.organization
   name  = "SSH"
   scope = "SYSTEM"
 }
 
 resource "vcd_nsxt_nat_rule" "master-dnat" {
-  count = local.is_first_node && local.use_nsxv ? 0 : 1
+  count = local.use_nsxv ? 0 : 1
   org   = var.organization
 
   edge_gateway_id = data.vcd_nsxt_edgegateway.gateway[0].id
 
-  name        = format("%s-dnat-ssh", var.internal_network_name)
+  name        = format("master-%d-%s-dnat-ssh", var.node_index, var.internal_network_name)
   rule_type   = "DNAT"
-  description = format("SSH DNAT rule for first master of %s", var.internal_network_name)
+  description = format("SSH DNAT rule for master-%d of %s", var.node_index, var.internal_network_name)
 
   external_address    = var.external_address
   dnat_external_port  = var.external_port
@@ -41,10 +40,10 @@ resource "vcd_nsxt_nat_rule" "master-dnat" {
 # NSX-V DNAT rule only for the first master node
 
 resource "vcd_nsxv_dnat" "master-dnat" {
-  count = local.is_first_node && local.use_nsxv ? 1 : 0
+  count = local.use_nsxv ? 1 : 0
 
   enabled     = true
-  description = format("SSH DNAT rule for first master of %s", var.internal_network_name)
+  description = format("SSH DNAT rule for master-%d of %s", var.node_index, var.internal_network_name)
   org         = var.organization
 
   edge_gateway = var.edge_gateway_name
