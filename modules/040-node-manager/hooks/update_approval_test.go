3493d64957f44f859aed2d4b2b03307e617cd7f3
Aleksey Gavrilov <alexey.gavrilov@flant.com>
2025-07-29 15:43:38 +0300

    Revert "item 6650"
    This reverts commit 609148d3ed8fa223b00f12fe56ef821678bcdf3e.
    
    Signed-off-by: Aleksey Gavrilov <alexey.gavrilov@flant.com>

--- modules/040-node-manager/hooks/update_approval_test.go
+++ modules/040-node-manager/hooks/update_approval_test.go
@@ -69,7 +69,7 @@ data:
 
 	f := HookExecutionConfigInit(`{"nodeManager":{"internal":{}}}`, `{}`)
 	f.RegisterCRD("deckhouse.io", "v1", "NodeGroup", false)
-	f.RegisterCRD("instances.deckhouse.io", "deckhouse.io/v1alpha1", "Instance", true)
+	f.RegisterCRD("machine.sapcloud.io", "v1alpha1", "Machine", true)
 
 	Context("Empty cluster", func() {
 		BeforeEach(func() {
