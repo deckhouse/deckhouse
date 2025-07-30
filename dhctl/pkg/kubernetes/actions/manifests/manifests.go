3d73a0171baf932913bea6036d49b01c2ed08d45
Sinelnikov Michail <mikhail.sinelnikov@flant.com>
2025-07-30 14:43:57 +0300

    fix
    Signed-off-by: Sinelnikov Michail <mikhail.sinelnikov@flant.com>

--- dhctl/pkg/kubernetes/actions/manifests/manifests.go
+++ dhctl/pkg/kubernetes/actions/manifests/manifests.go
@@ -47,10 +47,9 @@ const (
 )
 
 type DeckhouseDeploymentParams struct {
-	Bundle                   string
-	Registry                 string
-	LogLevel                 string
-	AllowExperimentalModules string
+	Bundle   string
+	Registry string
+	LogLevel string
 
 	DeployTime time.Time
 
@@ -399,10 +398,6 @@ func DeckhouseDeployment(params DeckhouseDeploymentParams) *appsv1.Deployment {
 		{
 			Name:  "DECKHOUSE_BUNDLE",
 			Value: params.Bundle,
-		},
-		{
-			Name:  "DECKHOUSE_ALLOW_EXPERIMENTAL_MODULES",
-			Value: params.AllowExperimentalModules,
 		},
 		{
 			Name:  "DEBUG_HTTP_SERVER_ADDR",
