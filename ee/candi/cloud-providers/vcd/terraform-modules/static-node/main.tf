8a0cd0bb0d1bd0bcb7f996db4f64447102434508
Alexey Voronov <alexey.voronov@flant.com>
2025-08-04 17:14:42 +0700

    resolve conflicts
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/candi/cloud-providers/vcd/terraform-modules/static-node/main.tf
+++ ee/candi/cloud-providers/vcd/terraform-modules/static-node/main.tf
@@ -95,11 +95,11 @@ resource "vcd_vapp_vm" "node" {
     for_each = local.additional_metadata
 
     content {
-      type        = "MetadataStringValue"
+      type        = format("Metadata%sValue", metadata_entry.type)
+      is_system   = metadata_entry.isSystem
+      user_access = metadata_entry.userAccess
       key         = metadata_entry.key
       value       = metadata_entry.value
-      is_system   = false
-      user_access = "READWRITE"
     }
 >>>>>>> 82fd6ae55c (add additionalMetadata to static instances)
   }
