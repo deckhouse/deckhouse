c130650766babbc627aa03c454d94de2136c76fb
Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>
2025-08-04 13:00:09 +0300

    bump vsphere tf provider version to 2.14.2
    Signed-off-by: Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>

--- ee/se-plus/candi/cloud-providers/vsphere/terraform-modules/versions.tf
+++ ee/se-plus/candi/cloud-providers/vsphere/terraform-modules/versions.tf
@@ -4,8 +4,8 @@
 terraform {
   required_providers {
     vsphere = {
-      source = "hashicorp/vsphere"
-      version = "2.0.2"
+      source = "vmware/vsphere"
+      version = "2.14.2"
     }
   }
   required_version = ">= 0.13"
