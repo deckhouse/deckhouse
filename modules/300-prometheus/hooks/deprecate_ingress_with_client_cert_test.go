0cd3d71ad401f8290e8762f62a9b777e0bcca467
Vadim Lazovsky <vadim.lazovsky@flant.com>
2025-08-04 09:46:52 +0300

    Fix hook and tests
    Signed-off-by: Vadim Lazovsky <vadim.lazovsky@flant.com>

--- modules/300-prometheus/hooks/deprecate_ingress_with_client_cert_test.go
+++ modules/300-prometheus/hooks/deprecate_ingress_with_client_cert_test.go
@@ -33,7 +33,56 @@ var _ = Describe("Prometheus hooks :: deprecate ingress with client cert ::", fu
 			f.RunHook()
 		})
 
-		Context("After adding ingress with the client certificate", func() {
+		Context("After adding some ingress with the client certificate", func() {
+			BeforeEach(func() {
+				f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(`
+---
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  annotations:
+    nginx.ingress.kubernetes.io/backend-protocol: HTTPS
+    nginx.ingress.kubernetes.io/configuration-snippet: |
+      proxy_ssl_certificate /etc/nginx/ssl/client.crt;
+      proxy_ssl_certificate_key /etc/nginx/ssl/client.key;
+      proxy_ssl_protocols TLSv1.2;
+      proxy_ssl_session_reuse on;
+    web.deckhouse.io/export-icon: /public/img/prometheus.ico
+    web.deckhouse.io/export-name: prometheus
+  name: grafana
+  namespace: d8-monitoring
+spec:
+  ingressClassName: nginx
+  rules:
+  - host: example.com
+    http:
+      paths:
+      - backend:
+          service:
+            name: grafana-v10
+            port:
+              name: https
+        path: /
+        pathType: ImplementationSpecific
+`, 1))
+				f.RunHook()
+			})
+
+			It("Should not start exposing metrics about deprecation", func() {
+				Expect(f).To(ExecuteSuccessfully())
+				m := f.MetricsCollector.CollectedMetrics()
+				Expect(m).To(HaveLen(0))
+			})
+		})
+	})
+
+	Context("Empty cluster", func() {
+		BeforeEach(func() {
+			f.BindingContexts.Set(f.KubeStateSet(``))
+			f.RunHook()
+		})
+
+		Context("After adding prometheus ingress with the client certificate", func() {
 			BeforeEach(func() {
 				f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(`
 ---
@@ -79,7 +128,7 @@ spec:
 				}))
 			})
 
-			Context("And after deleting the ingress should stop exposing metric", func() {
+			Context("And after deleting the prometheus ingress should stop exposing metric", func() {
 				BeforeEach(func() {
 					f.BindingContexts.Set(f.KubeStateSetAndWaitForBindingContexts(``, 1))
 					f.RunHook()
