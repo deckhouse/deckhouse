ad5a2e7e5159ca4dd69a6a79b9fd4a7abe4e00fc
dmitry.denisenko <dmitry.denisenko@flant.com>
2025-08-07 18:40:22 +0300

    ++ Signed-off-by: Dmitry Denisenko <dmitry.denisenko@flant.com>

---
+++ modules/040-node-manager/hooks/remove_bashible_labels_and_taints.go
@@ -0,0 +1,118 @@
+/*
+Copyright 2021 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hooks
+
+import (
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"github.com/deckhouse/deckhouse/go_lib/taints"
+)
+
+const (
+	BashibleFirstRunFinishedLabel = "node.deckhouse.io/bashible-first-run-finished"
+	BashibleUninitializedTaintKey = "node.deckhouse.io/bashible-uninitialized"
+)
+
+type NodesInfo struct {
+	Name   string
+	Labels map[string]string
+	Taints []v1.Taint
+}
+
+func nodeFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	node := &v1.Node{}
+	err := sdk.FromUnstructured(obj, node)
+	if err != nil {
+		return nil, err
+	}
+
+	return NodesInfo{
+		Name:   node.Name,
+		Labels: node.Labels,
+		Taints: node.Spec.Taints,
+	}, nil
+}
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Queue: "/modules/node-manager/cleanup_bashible_artifacts",
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "nodes",
+			ApiVersion: "v1",
+			Kind:       "Node",
+			FilterFunc: nodeFilter,
+		},
+	},
+}, cleanupBashibleArtifacts)
+
+func cleanupBashibleArtifacts(input *go_hook.HookInput) error {
+	snapshots := input.Snapshots["nodes"]
+	if len(snapshots) == 0 {
+		return nil
+	}
+
+	for _, snapshot := range snapshots {
+		node, ok := snapshot.(NodesInfo)
+		if !ok {
+			continue
+		}
+
+		hasTaint := false
+		for _, taint := range node.Taints {
+			if taint.Key == BashibleUninitializedTaintKey {
+				hasTaint = true
+				break
+			}
+		}
+
+		hasLabel := false
+		_, labelExists := node.Labels[BashibleFirstRunFinishedLabel]
+		if labelExists {
+			hasLabel = true
+		}
+
+		if hasLabel {
+			input.PatchCollector.PatchWithMutatingFunc(func(obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+				nodeObj := &v1.Node{}
+				err := sdk.FromUnstructured(obj, nodeObj)
+				if err != nil {
+					return nil, err
+				}
+
+				// Remove the label if hasLabel is true
+				delete(nodeObj.Labels, BashibleFirstRunFinishedLabel)
+
+				// Remove the taint if hasTaint is true
+				if hasTaint {
+					newTaints := taints.Slice(nodeObj.Spec.Taints).WithoutKey(BashibleUninitializedTaintKey)
+					if len(newTaints) == 0 {
+						nodeObj.Spec.Taints = nil
+					} else {
+						nodeObj.Spec.Taints = newTaints
+					}
+				}
+
+				return sdk.ToUnstructured(nodeObj)
+			}, "v1", "Node", "", node.Name)
+		}
+	}
+
+	return nil
+}
