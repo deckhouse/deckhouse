cd56ecfdf972fc0593091bf8caaf176e7d42e36e
Ilya Mayorov <ilya.mayorov@flant.com>
2025-08-07 17:44:41 +0700

    [webhook] split provider and static webhook into 2 different ones
    Signed-off-by: Ilya Mayorov <ilya.mayorov@flant.com>

--- deckhouse-controller/pkg/apis/deckhouse.io/validation/register.go
+++ deckhouse-controller/pkg/apis/deckhouse.io/validation/register.go
@@ -58,7 +58,8 @@ func RegisterAdmissionHandlers(
 	reg.RegisterHandler("/validate/v1alpha1/module-configs", moduleConfigValidationHandler(cli, storage, metricStorage, mm, validator, settings))
 	reg.RegisterHandler("/validate/v1alpha1/modules", moduleValidationHandler())
 	reg.RegisterHandler("/validate/v1/configuration-secret", clusterConfigurationHandler(mm, cli, schemaStore))
-	reg.RegisterHandler("/validate/v1/provider-and-static-configuration-secret", providerAndStaticConfigurationHandler(schemaStore))
+	reg.RegisterHandler("/validate/v1/provider-configuration-secret", providerConfigurationHandler(schemaStore))
+	reg.RegisterHandler("/validate/v1/static-configuration-secret", staticConfigurationHandler(schemaStore))
 	reg.RegisterHandler("/validate/v1alpha1/update-policies", updatePolicyHandler(cli))
 	reg.RegisterHandler("/validate/v1alpha1/deckhouse-releases", DeckhouseReleaseValidationHandler(cli, metricStorage, mm, exts))
 }
