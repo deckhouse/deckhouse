70c5dfbacd59212faec05fe4f6ab7017f625a599
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2025-08-19 21:17:38 +0300

    ++
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- dhctl/pkg/server/pkg/logger/writer.go
+++ dhctl/pkg/server/pkg/logger/writer.go
@@ -18,9 +18,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
-	"runtime"
 	"sync"
-	"time"
 )
 
 type LogWriter[T any] struct {
@@ -72,8 +70,9 @@ func (w *LogWriter[T]) Write(p []byte) (n int, err error) {
 type DebugLogWriter struct {
 	l *slog.Logger
 
-	m    sync.Mutex
-	prev []byte
+	m     sync.Mutex
+	prev  []byte
+	lines []string
 }
 
 func NewDebugLogWriter(l *slog.Logger) *DebugLogWriter {
@@ -83,47 +82,8 @@ func NewDebugLogWriter(l *slog.Logger) *DebugLogWriter {
 }
 
 func (w *DebugLogWriter) Write(p []byte) (n int, err error) {
-	fmt.Fprintln(os.Stderr, "---Gorutines in running---")
-	// 10 mb
-	buf := make([]byte, 10485760)  // Allocate a buffer for the stack trace
-	nn := runtime.Stack(buf, true) // Pass 'true' to get all goroutine stack traces
-	fmt.Fprintf(os.Stderr, "%s\n", string(buf[:nn]))
-	fmt.Fprintln(os.Stderr, "---")
-
-	buf = nil
-
-	ch := make(chan struct{})
-
-	go func() {
-		fmt.Fprintln(os.Stderr, "Starting profiling mutexes")
-		ticker := time.NewTicker(1 * time.Second)
-		select {
-		case <-ticker.C:
-			fmt.Fprintln(os.Stderr, "Next tick for profiling mutexes")
-			r := make([]runtime.BlockProfileRecord, 1000)
-			l, ok := runtime.MutexProfile(r)
-			fmt.Fprintf(os.Stderr, "MutexProfile got is = %v; len = %d\n", ok, l)
-			for i := 0; i < l; i++ {
-				r[i].Stack()
-				fmt.Fprintf(os.Stderr, "MutexProfile %d: count: %d; cycles: %d", i, r[i].Count, r[i].Cycles)
-			}
-			r = nil
-		case <-ch:
-			fmt.Fprintln(os.Stderr, "Finish profiling mutexes")
-			return
-		}
-	}()
-
 	fmt.Fprintln(os.Stderr, "Try to lock debug log writer")
 	w.m.Lock()
-
-	fmt.Fprintln(os.Stderr, "Locked debug log writer. Send signal to finish monitor")
-
-	ch <- struct{}{}
-
-	fmt.Fprintln(os.Stderr, "Locked debug log writer. Close channel to finish monitor")
-
-	close(ch)
 
 	fmt.Fprintln(os.Stderr, "Locked debug log writer. Closed channel to finish monitor. Defer to unlock")
 
@@ -133,10 +93,6 @@ func (w *DebugLogWriter) Write(p []byte) (n int, err error) {
 		fmt.Fprintln(os.Stderr, "Debug log writer unlocked")
 	}()
 
-	fmt.Fprintln(os.Stderr, "Locked debug log writer. Declare lines variable")
-
-	var lines []string
-
 	fmt.Fprintf(os.Stderr, "Split log %s by line\n", string(p))
 
 	for _, b := range p {
@@ -144,7 +100,7 @@ func (w *DebugLogWriter) Write(p []byte) (n int, err error) {
 		case '\n', '\r':
 			s := string(w.prev)
 			if s != "" {
-				lines = append(lines, s)
+				w.lines = append(w.lines, s)
 			}
 			w.prev = []byte{}
 		default:
@@ -152,10 +108,10 @@ func (w *DebugLogWriter) Write(p []byte) (n int, err error) {
 		}
 	}
 
-	fmt.Fprintf(os.Stderr, "Splited log %s by line; lines %d\n", string(p), len(lines))
+	fmt.Fprintf(os.Stderr, "Splited log %s by line; lines %d\n", string(p), len(w.lines))
 
-	if len(lines) > 0 {
-		for _, line := range lines {
+	if len(w.lines) > 0 {
+		for _, line := range w.lines {
 			fmt.Fprintf(os.Stderr, "debudlogwriter: write to sterr: %s\n", line)
 			//w.l.Debug(line)
 			//fmt.Fprintf(os.Stderr, "debudlogwriter: sent to logger: %s\n", line)
@@ -166,7 +122,8 @@ func (w *DebugLogWriter) Write(p []byte) (n int, err error) {
 
 	llen := len(p)
 
-	fmt.Fprintf(os.Stderr, "debudlogwriter: got len of buffer %d\n", llen)
+	fmt.Fprintf(os.Stderr, "debudlogwriter: got len of buffer. Set lines to nil %d\n", llen)
+	w.lines = nil
 
 	return llen, nil
 }
