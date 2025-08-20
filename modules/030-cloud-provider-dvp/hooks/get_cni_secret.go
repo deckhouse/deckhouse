4231ecdae2da393ce2e3cf4e875fe0d0d9d6b2ee
Vladislav Denisov <vlad144denisov@gmail.com>
2025-08-20 15:25:13 +0300

    add copyright
    add copyright
    
    Signed-off-by: Vladislav Denisov <vlad144denisov@gmail.com>

--- modules/030-cloud-provider-dvp/hooks/get_cni_secret.go
+++ modules/030-cloud-provider-dvp/hooks/get_cni_secret.go
@@ -1,3 +1,8 @@
+/*
+Copyright 2023 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
 package hooks
 
 import "github.com/deckhouse/deckhouse/go_lib/hooks/get_cni_secret"
