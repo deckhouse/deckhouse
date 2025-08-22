2105c728b1d48a70bc68cb57f7a956be15a72af4
Alexey Voronov <alexey.voronov@flant.com>
2025-08-22 16:19:45 +0700

    initial infra controller
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

---
+++ ee/modules/030-cloud-provider-vcd/images/infra-controller-manager/src/internal/controller/affinity_controller_test.go
@@ -0,0 +1,68 @@
+package controller
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	deckhouseiov1alpha1 "infra-controller-manager/api/v1alpha1"
+)
+
+var _ = Describe("Affinity Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		affinity := &deckhouseiov1alpha1.Affinity{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind Affinity")
+			err := k8sClient.Get(ctx, typeNamespacedName, affinity)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &deckhouseiov1alpha1.Affinity{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					// TODO(user): Specify other spec details if needed.
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &deckhouseiov1alpha1.Affinity{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance Affinity")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &AffinityReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})
