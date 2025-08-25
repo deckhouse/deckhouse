dd95a743940a397101f1bf1c81c3d65875a019a6
Smyslov Maxim <maksim.smyslov@flant.com>
2025-08-25 19:20:21 +0300

    todo
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- modules/002-deckhouse/images/webhook-handler/operator/internal/controller/validationwebhook_controller.go
+++ modules/002-deckhouse/images/webhook-handler/operator/internal/controller/validationwebhook_controller.go
@@ -18,10 +18,14 @@ package controller
 
 import (
 	"context"
+	"log/slog"
 
+	"github.com/deckhouse/deckhouse/pkg/log"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	deckhouseiov1alpha1 "deckhouse.io/webhook/api/v1alpha1"
@@ -29,13 +33,15 @@ import (
 
 // ValidationWebhookReconciler reconciles a ValidationWebhook object
 type ValidationWebhookReconciler struct {
-	client.Client
+	Client client.Client
 	Scheme *runtime.Scheme
+	// init logger as in docs builder (watcher)
+	Logger *log.Logger
 }
 
-// +kubebuilder:rbac:groups=deckhouse.io.deckhouse.io,resources=validationwebhooks,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=deckhouse.io.deckhouse.io,resources=validationwebhooks/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=deckhouse.io.deckhouse.io,resources=validationwebhooks/finalizers,verbs=update
+// +kubebuilder:rbac:groups=deckhouse.io,resources=validationwebhooks,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=deckhouse.io,resources=validationwebhooks/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=deckhouse.io,resources=validationwebhooks/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,11 +53,89 @@ type ValidationWebhookReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
 func (r *ValidationWebhookReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	// TODO: replace with deckhouse/pkg/log
 	_ = logf.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var res ctrl.Result
 
-	return ctrl.Result{}, nil
+	r.Logger.Debug("validating webhook processing started", slog.String("resource_name", req.Name))
+	defer func() {
+		r.Logger.Debug("validating webhook processing complete", slog.String("resource_name", req.Name), slog.Any("reconcile_result", res))
+	}()
+
+	webhook := new(deckhouseiov1alpha1.ValidationWebhook)
+	err := r.Client.Get(ctx, req.NamespacedName, webhook)
+	if err != nil {
+		// resource may no longer exist, in which case we stop
+		// processing.
+		if apierrors.IsNotFound(err) {
+			return res, nil
+		}
+
+		r.Logger.Debug("get validating webhook", log.Err(err))
+
+		return res, err
+	}
+
+	// resource marked as "to delete"
+	if !webhook.DeletionTimestamp.IsZero() {
+		// TODO: finalizer deletion logic
+		r.Logger.Debug("release deletion", slog.String("deletion_timestamp", webhook.DeletionTimestamp.String()))
+
+		res, err := r.handleDeleteValidatingWebhook(ctx, webhook)
+		if err != nil {
+			r.Logger.Warn("bla bla", log.Err(err))
+
+			return res, err
+		}
+		return res, nil
+	}
+
+	res, err = r.handleProcessValidatingWebhook(ctx, webhook)
+	if err != nil {
+		r.Logger.Warn("bla bla", log.Err(err))
+
+		return res, err
+	}
+
+	return res, nil
+}
+
+func (r *ValidationWebhookReconciler) handleProcessValidatingWebhook(ctx context.Context, vh *deckhouseiov1alpha1.ValidationWebhook) (ctrl.Result, error) {
+	var res ctrl.Result
+
+	_, _ = ctx, vh
+
+	// logic for processing validation webhook
+	// 1) MkdirAll to folder
+	// 2) Open template (maybe once at startup?)
+	// 3) Render template
+	// 4) Write to file (add finalizer)
+	// 5) write finalizer
+	// 6) kill shell-operator binary (we can start shell operator as library too?)
+
+	// add finalizer
+	if !controllerutil.ContainsFinalizer(vh, "some finalizer") {
+		controllerutil.AddFinalizer(vh, "some finalizer")
+	}
+
+	return res, nil
+}
+
+func (r *ValidationWebhookReconciler) handleDeleteValidatingWebhook(ctx context.Context, vh *deckhouseiov1alpha1.ValidationWebhook) (ctrl.Result, error) {
+	var res ctrl.Result
+
+	_, _ = ctx, vh
+
+	// logic for processing validation webhook deletion
+	// 1) delete file
+
+	// add finalizer
+	if controllerutil.ContainsFinalizer(vh, "some finalizer") {
+		controllerutil.RemoveFinalizer(vh, "some finalizer")
+	}
+
+	return res, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
