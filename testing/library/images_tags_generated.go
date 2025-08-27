25de3d5d02f9bed0c58567129f3234683faef24f
Nikolay Gorbatov <nikolay.gorbatov@flant.com>
2025-08-27 18:43:58 +0400

    +
    Signed-off-by: Nikolay Gorbatov <nikolay.gorbatov@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -408,7 +408,7 @@ var DefaultImagesDigests = map[string]interface{}{
 	},
 	"registrypackages": map[string]interface{}{
 		"amazonEc2Utils220":               "imageHash-registrypackages-amazonEc2Utils220",
-		"containerd1727":                  "imageHash-registrypackages-containerd1727",
+		"containerd1728":                  "imageHash-registrypackages-containerd1728",
 		"containerd214":                   "imageHash-registrypackages-containerd214",
 		"crictl129":                       "imageHash-registrypackages-crictl129",
 		"crictl130":                       "imageHash-registrypackages-crictl130",
