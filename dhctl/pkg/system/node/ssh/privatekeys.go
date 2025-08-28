8af04cc545f5866188706b00312e4cd93051feac
YuryLysov <yuriy.lysov@flant.com>
2025-08-28 18:04:03 +0400

    Remove retry on ssh key passphrase prompt
    Signed-off-by: YuryLysov <yuriy.lysov@flant.com>

--- dhctl/pkg/system/node/ssh/privatekeys.go
+++ dhctl/pkg/system/node/ssh/privatekeys.go
@@ -58,36 +58,21 @@ func GetPrivateKeys(keyPath string) (*session.AgentPrivateKey, error) {
 		var passphraseMissingError *ssh.PassphraseMissingError
 		switch {
 		case errors.As(err, &passphraseMissingError):
-			attempt := 0
-			for {
-				if attempt == 5 {
-					return nil, fmt.Errorf("too many attempts for ssh private key passphrase")
-				}
-				passphrase, err = askPassword(keyPath)
-				if err != nil {
-					return nil, err
-				}
-				_, err = ssh.ParseRawPrivateKeyWithPassphrase(keyData, passphrase)
-				if err == nil {
-					break
-				}
-				attempt++
+			var err error
+			passphrase, err = terminal.AskPassword(
+				fmt.Sprintf("Enter passphrase for ssh key %q: ", keyPath),
+			)
+			if err != nil {
+				return nil, fmt.Errorf("getting passphrase for ssh key %q: %w", keyPath, err)
 			}
-
+			_, err = ssh.ParseRawPrivateKeyWithPassphrase(keyData, passphrase)
+			if err != nil {
+				return nil, fmt.Errorf("wrong passphrase for ssh key")
+			}
 		default:
 			return nil, fmt.Errorf("parsing private key %q: %w", keyPath, err)
 		}
 	}
 
 	return &session.AgentPrivateKey{Key: keyPath, Passphrase: string(passphrase)}, nil
-}
-
-func askPassword(path string) ([]byte, error) {
-	passphraseFromStdin, err := terminal.AskPassword(
-		fmt.Sprintf("Enter passphrase for ssh key %q: ", path),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("getting passphrase for ssh key %q: %w", path, err)
-	}
-	return passphraseFromStdin, nil
 }
