68449a5fa12fbde7010f34c93128bbd9ea831a37
Smyslov Maxim <maksim.smyslov@flant.com>
2025-09-01 11:00:06 +0300

    KubernetesContext
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- modules/002-deckhouse/images/webhook-handler/operator/test.sh
+++ modules/002-deckhouse/images/webhook-handler/operator/test.sh
@@ -7,8 +7,13 @@ echo "--- manifests ---"
 make manifests
 
 echo ""
+echo "--- uninstall ---"
+make uninstall
+
+echo ""
 echo "--- install ---"
 make install
+kubectl apply -f config/samples/deckhouse.io_v1alpha1_validationwebhook.yaml
 
 echo ""
 echo "--- run ---"
