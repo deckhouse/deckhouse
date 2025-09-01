8842712a3242872869ad8c69ce740e4dd7201091
Smyslov Maxim <maksim.smyslov@flant.com>
2025-09-01 12:43:57 +0300

    template as file, enhance tests
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- modules/002-deckhouse/images/webhook-handler/operator/internal/controller/validationwebhook_controller_test.go
+++ modules/002-deckhouse/images/webhook-handler/operator/internal/controller/validationwebhook_controller_test.go
@@ -21,10 +21,17 @@ func setupTestReconciler() *ValidationWebhookReconciler {
 	deckhouseiov1alpha1.AddToScheme(sch)
 	k8sClient := fake.NewClientBuilder().WithScheme(sch).Build()
 
+	// init template file
+	tpl, err := os.ReadFile("templates/webhook.tpl")
+	if err != nil {
+		panic(err)
+	}
+
 	return &ValidationWebhookReconciler{
-		Client: k8sClient,
-		Scheme: sch,
-		Logger: log.NewLogger(log.WithLevel(slog.LevelDebug)),
+		Client:   k8sClient,
+		Scheme:   sch,
+		Logger:   log.NewLogger(log.WithLevel(slog.LevelDebug)),
+		Template: string(tpl),
 	}
 }
 
@@ -52,6 +59,10 @@ func getStructFromYamlFile(filename string) (*deckhouseiov1alpha1.ValidationWebh
 	return vh, nil
 }
 
+// ------------------
+// --- TEST-CASES ---
+// ------------------
+
 func TestTemplateNoError(t *testing.T) {
 	// hooks/002-deckhouse/webhooks/validating
 	// os.MkdirAll("/hooks/"+vh.Name+"/webhooks/validating/", 0777)
@@ -63,12 +74,16 @@ func TestTemplateNoError(t *testing.T) {
 
 	_, err = r.handleProcessValidatingWebhook(context.TODO(), vh)
 	assert.NoError(t, err)
+
+	// test equality
+	ref, err := os.ReadFile("testdata/golden/validationwebhook-sample.py")
+	assert.NoError(t, err)
+	res, err := os.ReadFile("hooks/validationwebhook-sample/webhooks/validating/validationwebhook-sample.py")
+	assert.NoError(t, err)
+	assert.Equal(t, string(ref), string(res))
 }
 
 func TestTemplateNoContext(t *testing.T) {
-	// hooks/002-deckhouse/webhooks/validating
-	// os.MkdirAll("/hooks/"+vh.Name+"/webhooks/validating/", 0777)
-
 	r := setupTestReconciler()
 
 	vh, err := getStructFromYamlFile("testdata/sample_without_context.yaml")
@@ -76,12 +91,16 @@ func TestTemplateNoContext(t *testing.T) {
 
 	_, err = r.handleProcessValidatingWebhook(context.TODO(), vh)
 	assert.NoError(t, err)
+
+	// test equality
+	ref, err := os.ReadFile("testdata/golden/sample_without_context.py")
+	assert.NoError(t, err)
+	res, err := os.ReadFile("hooks/validationwebhook-sample/webhooks/validating/validationwebhook-sample.py")
+	assert.NoError(t, err)
+	assert.Equal(t, string(ref), string(res))
 }
 
 func TestTemplateTwoContext(t *testing.T) {
-	// hooks/002-deckhouse/webhooks/validating
-	// os.MkdirAll("/hooks/"+vh.Name+"/webhooks/validating/", 0777)
-
 	r := setupTestReconciler()
 
 	vh, err := getStructFromYamlFile("testdata/sample_two_context.yaml")
@@ -89,18 +108,30 @@ func TestTemplateTwoContext(t *testing.T) {
 
 	_, err = r.handleProcessValidatingWebhook(context.TODO(), vh)
 	assert.NoError(t, err)
+
+	// test equality
+	ref, err := os.ReadFile("testdata/golden/sample_two_context.py")
+	assert.NoError(t, err)
+	res, err := os.ReadFile("hooks/validationwebhook-sample/webhooks/validating/validationwebhook-sample.py")
+	assert.NoError(t, err)
+	assert.Equal(t, string(ref), string(res))
 }
 
 // TODO: complete compare logic
 func TestTemplateEqual(t *testing.T) {
-	// hooks/002-deckhouse/webhooks/validating
-	// os.MkdirAll("/hooks/"+vh.Name+"/webhooks/validating/", 0777)
-
 	r := setupTestReconciler()
 
-	vh, err := getStructFromYamlFile("testdata/prometheusremotewrites.yaml")
+	vh, err := getStructFromYamlFile("testdata/prometheusremotewrite.yaml")
 	assert.NoError(t, err)
 
 	_, err = r.handleProcessValidatingWebhook(context.TODO(), vh)
 	assert.NoError(t, err)
+
+	ref, err := os.ReadFile("testdata/golden/prometheusremotewrite.py")
+	assert.NoError(t, err)
+
+	res, err := os.ReadFile("hooks/prometheusremotewrite/webhooks/validating/prometheusremotewrite.py")
+	assert.NoError(t, err)
+
+	assert.Equal(t, string(ref), string(res))
 }
