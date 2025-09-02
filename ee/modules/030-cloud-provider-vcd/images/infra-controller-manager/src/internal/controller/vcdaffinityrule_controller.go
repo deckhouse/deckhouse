d26246479d891bb4570c4cfe4e1f893b58a7ac7e
Alexey Voronov <alexey.voronov@flant.com>
2025-09-02 12:31:59 +0700

    vcd ar controller optimization
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/modules/030-cloud-provider-vcd/images/infra-controller-manager/src/internal/controller/vcdaffinityrule_controller.go
+++ ee/modules/030-cloud-provider-vcd/images/infra-controller-manager/src/internal/controller/vcdaffinityrule_controller.go
@@ -22,6 +22,7 @@ import (
 	"infra-controller-manager/internal/vcd"
 
 	"github.com/deckhouse/deckhouse/pkg/log"
+	"github.com/vmware/go-vcloud-director/v2/govcd"
 	"github.com/vmware/go-vcloud-director/v2/types/v56"
 )
 
@@ -64,8 +65,11 @@ func (r *VCDAffinityRuleReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 
 	// Add finalizer first if not exist to avoid the race condition between init and delete
 	if !controllerutil.ContainsFinalizer(vcdaffinityrule, finalizer) {
+		r.Logger.Info("adding finalizer")
+
 		controllerutil.AddFinalizer(vcdaffinityrule, finalizer)
-		return ctrl.Result{RequeueAfter: 0}, nil
+		r.Update(ctx, vcdaffinityrule)
+		return ctrl.Result{RequeueAfter: 1}, nil
 	}
 
 	var nodes corev1.NodeList
@@ -86,111 +90,85 @@ func (r *VCDAffinityRuleReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 	vdcClient, err := r.Config.NewVDCClient()
 	if err != nil {
 		r.Logger.Error("failed to create vdc client", slog.String("error", err.Error()))
-		return ctrl.Result{}, err
+		return ctrl.Result{}, fmt.Errorf("failed to create vdc client: %w", err)
 	}
 
 	vappClient, err := r.Config.NewVAppClientFromVDCClient(vdcClient)
 	if err != nil {
 		r.Logger.Error("failed to create vapp client", slog.String("error", err.Error()))
-		return ctrl.Result{}, err
+		return ctrl.Result{}, fmt.Errorf("failed to create vapp client: %w", err)
 	}
 
 	if !vcdaffinityrule.DeletionTimestamp.IsZero() {
-		if vcdaffinityrule.Status.RuleID != "" {
-			r.Logger.Info("deleting affinity rule in VCD", slog.String("ruleID", vcdaffinityrule.Status.RuleID))
-			vmAffinityRule, err := vdcClient.GetVmAffinityRuleById(vcdaffinityrule.Status.RuleID)
-			if err != nil {
-				r.Logger.Error("failed to get vm affinity rule by id", slog.String("error", err.Error()))
-			}
 
-			err = vmAffinityRule.Delete()
-			if err != nil {
-				r.Logger.Error("failed to delete vm affinity rule", slog.String("error", err.Error()))
-			}
-
-			controllerutil.RemoveFinalizer(vcdaffinityrule, finalizer)
-			return ctrl.Result{}, nil
-		} else {
-			r.Logger.Warn("no ruleID in status, trying to find rule by name and polarity")
-
-			vmAffinityRules, err := vdcClient.GetVmAffinityRulesByName(vcdaffinityrule.GetName(), vcdaffinityrule.Spec.Polarity)
-			if err != nil {
-				r.Logger.Error("failed to get vm affinity rule by name and polarity", slog.String("error", err.Error()))
-			}
-
-			if len(vmAffinityRules) == 0 {
-				r.Logger.Warn("no affinity rule found, nothing to delete")
-				return ctrl.Result{}, nil
-			} else if len(vmAffinityRules) > 1 {
-				r.Logger.Warn("multiple affinity rules found with same name and polarity, unable to determine which to delete")
-				return ctrl.Result{}, nil
-			} else {
-				err := vmAffinityRules[0].Delete()
-				if err != nil {
-					r.Logger.Error("failed to delete vm affinity rule", slog.String("error", err.Error()))
-					return ctrl.Result{}, err
-				}
-
-				controllerutil.RemoveFinalizer(vcdaffinityrule, finalizer)
-				return ctrl.Result{}, nil
-			}
-
+		err := r.deleteVmAffinityRule(vcdaffinityrule, vdcClient)
+		if err != nil {
+			r.Logger.Error("failed to delete vm affinity rule", slog.String("error", err.Error()))
+			return ctrl.Result{}, fmt.Errorf("failed to delete vm affinity rule: %w", err)
 		}
+
+		controllerutil.RemoveFinalizer(vcdaffinityrule, finalizer)
+		r.Update(ctx, vcdaffinityrule)
+
+		return ctrl.Result{}, nil
+	}
+
+	if len(nodes.Items) < 2 {
+		vcdaffinityrule.Status.Message = "Not enough nodes to build an affinity rule"
+
+		if vcdaffinityrule.Status.RuleID != "" {
+			r.Logger.Info("deleting affinity rule from VCD API due to insufficient VM count")
+
+			err = r.deleteVmAffinityRule(vcdaffinityrule, vdcClient)
+			if err != nil {
+				return ctrl.Result{}, fmt.Errorf("failed to delete vm affinity rule: %w", err)
+			}
+
+			vcdaffinityrule.Status.RuleID = ""
+		}
+
+		r.Status().Update(ctx, vcdaffinityrule)
+		return ctrl.Result{}, nil
+	}
+
+	vmAffinityRuleDefinition, err := r.buildVmAffinityRule(vcdaffinityrule, vappClient)
+	if err != nil {
+		r.Logger.Error("failed to build vm affinity rule", slog.String("error", err.Error()))
+		return ctrl.Result{}, fmt.Errorf("failed to build vm affinity rule: %w", err)
+	}
+
+	if vcdaffinityrule.Status.RuleID == "" {
+		vmAffinityRule, err := vdcClient.CreateVmAffinityRule(vmAffinityRuleDefinition)
+		if err != nil {
+			r.Logger.Error("failed to create vm affinity rule", slog.String("error", err.Error()))
+			vcdaffinityrule.Status.Message = fmt.Sprintf("Failed to create affinity rule in VCD: %s", err.Error())
+			r.Status().Update(ctx, vcdaffinityrule)
+			return ctrl.Result{}, fmt.Errorf("failed to create vm affinity rule: %w", err)
+		}
+
+		vcdaffinityrule.Status.RuleID = vmAffinityRule.VmAffinityRule.ID
+		vcdaffinityrule.Status.Message = "VM affinity rule is up to date"
+		r.Status().Update(ctx, vcdaffinityrule)
 
 	} else {
-		if len(nodes.Items) < 2 {
-			vcdaffinityrule.Status.Message = "Not enough nodes for building an affinity rule"
-			r.Status().Update(ctx, vcdaffinityrule)
-			return ctrl.Result{}, err
-		} else {
-			vmReference := make([]*types.Reference, len(nodes.Items))
+		vmAffinityRule, err := vdcClient.GetVmAffinityRuleById(vcdaffinityrule.Status.RuleID)
+		if err != nil {
+			r.Logger.Error("failed to get vm affinity rule by id", slog.String("error", err.Error()))
+			return ctrl.Result{}, fmt.Errorf("failed to get vm affinity rule by id: %w", err)
+		}
 
-			for _, node := range nodeStatus {
-				r.Logger.With(slog.String("node", node.Name), slog.String("providerID", node.ID))
+		vmAffinityRule.VmAffinityRule.Name = vmAffinityRuleDefinition.Name
+		vmAffinityRule.VmAffinityRule.Polarity = vmAffinityRuleDefinition.Polarity
+		vmAffinityRule.VmAffinityRule.IsEnabled = vmAffinityRuleDefinition.IsEnabled
+		vmAffinityRule.VmAffinityRule.IsMandatory = vmAffinityRuleDefinition.IsMandatory
+		vmAffinityRule.VmAffinityRule.VmReferences = vmAffinityRuleDefinition.VmReferences
 
-				vm, err := vappClient.GetVMById(node.ID, false)
-				if err != nil {
-					r.Logger.Error("failed to get vm by id", slog.String("error", err.Error()))
-					return ctrl.Result{}, err
-				}
-
-				vmReference = append(vmReference, &types.Reference{
-					HREF: vm.VM.HREF,
-					Name: vm.VM.Name,
-					ID:   vm.VM.ID,
-					Type: vm.VM.Type,
-				})
-			}
-
-			vmReferences := make([]*types.VMs, 1)
-			vmReferences[0] = &types.VMs{
-				VMReference: vmReference,
-			}
-
-			ruleEnabled := true
-			ruleMandatory := false
-
-			vmAffinityRuleDefinition := &types.VmAffinityRule{
-				Name:         vcdaffinityrule.GetName(),
-				Polarity:     vcdaffinityrule.Spec.Polarity,
-				IsEnabled:    &ruleEnabled,
-				IsMandatory:  &ruleMandatory,
-				VmReferences: vmReferences,
-			}
-
-			vmAffinityRule, err := vdcClient.CreateVmAffinityRule(vmAffinityRuleDefinition)
-			if err != nil {
-				r.Logger.Error("failed to create vm affinity rule", slog.String("error", err.Error()))
-				vcdaffinityrule.Status.Message = fmt.Sprintf("Failed to create affinity rule in VCD: %s", err.Error())
-				r.Status().Update(ctx, vcdaffinityrule)
-				return ctrl.Result{}, err
-			}
-
-			vcdaffinityrule.Status.RuleID = vmAffinityRule.VmAffinityRule.ID
-			vcdaffinityrule.Status.Message = "Ok"
+		err = vmAffinityRule.Update()
+		if err != nil {
+			r.Logger.Error("failed to update vm affinity rule", slog.String("error", err.Error()))
+			return ctrl.Result{}, fmt.Errorf("failed to update vm affinity rule: %w", err)
 		}
 	}
-	r.Status().Update(ctx, vcdaffinityrule)
 
 	return ctrl.Result{}, nil
 }
@@ -206,4 +184,87 @@ func (r *VCDAffinityRuleReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		)).
 		Named("vcdaffinityrule").
 		Complete(r)
+}
+
+func (r *VCDAffinityRuleReconciler) buildVmAffinityRule(resource *v1alpha1.VCDAffinityRule, vapp *govcd.VApp) (*types.VmAffinityRule, error) {
+	nodeStatus := resource.Status.Nodes
+
+	vmReference := make([]*types.Reference, len(nodeStatus))
+
+	for _, node := range nodeStatus {
+		r.Logger.With(slog.String("node", node.Name), slog.String("providerID", node.ID))
+
+		vm, err := vapp.GetVMById(node.ID, false)
+		if err != nil {
+			r.Logger.Error("failed to get vm by id", slog.String("error", err.Error()))
+			return nil, err
+		}
+
+		vmReference = append(vmReference, &types.Reference{
+			HREF: vm.VM.HREF,
+			Name: vm.VM.Name,
+			ID:   vm.VM.ID,
+			Type: vm.VM.Type,
+		})
+	}
+
+	vmReferences := make([]*types.VMs, 1)
+	vmReferences[0] = &types.VMs{
+		VMReference: vmReference,
+	}
+
+	return &types.VmAffinityRule{
+		Name:         resource.GetName(),
+		Polarity:     resource.Spec.Polarity,
+		IsEnabled:    &resource.Spec.Enabled,
+		IsMandatory:  &resource.Spec.Mandatory,
+		VmReferences: vmReferences,
+	}, nil
+}
+
+func (r *VCDAffinityRuleReconciler) deleteVmAffinityRule(resource *v1alpha1.VCDAffinityRule, vdc *govcd.Vdc) error {
+	if resource.Status.RuleID != "" {
+		r.Logger.Info("deleting affinity rule from VCD API by id", slog.String("ruleID", resource.Status.RuleID))
+		vmAffinityRule, err := vdc.GetVmAffinityRuleById(resource.Status.RuleID)
+
+		if err != nil {
+			r.Logger.Error("failed to get vm affinity rule by id", slog.String("error", err.Error()))
+			return fmt.Errorf("failed to get vm affinity rule by id: %w", err)
+		}
+
+		err = vmAffinityRule.Delete()
+		if err != nil {
+			r.Logger.Error("failed to delete vm affinity rule", slog.String("error", err.Error()))
+			return fmt.Errorf("failed to delete vm affinity rule: %w", err)
+		}
+
+		return nil
+
+	}
+	r.Logger.Warn("no ruleID in status, trying to find rule by name and polarity")
+
+	vmAffinityRules, err := vdc.GetVmAffinityRulesByName(resource.GetName(), resource.Spec.Polarity)
+	if err != nil {
+		r.Logger.Error("failed to get vm affinity rule by name and polarity", slog.String("error", err.Error()))
+		return fmt.Errorf("failed to get vm affinity rule by name and polarity: %w", err)
+	}
+
+	if len(vmAffinityRules) == 0 {
+		r.Logger.Warn("no affinity rule found, nothing to delete")
+		return nil
+
+	} else if len(vmAffinityRules) > 1 {
+		r.Logger.Warn("multiple affinity rules found with same name and polarity, unable to determine which to delete")
+		return fmt.Errorf("multiple affinity rules found with same name and polarity, unable to determine which to delete")
+
+	} else {
+
+		err := vmAffinityRules[0].Delete()
+		if err != nil {
+			r.Logger.Error("failed to delete vm affinity rule", slog.String("error", err.Error()))
+			return err
+		}
+
+		return nil
+	}
 }
