f6c087f7f324737f9554fb32a4f9e20f7eb74cfd
Alexey Voronov <alexey.voronov@flant.com>
2025-09-04 12:42:59 +0700

    fix dmtlint issues
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/modules/030-cloud-provider-vcd/images/infra-controller-manager/src/internal/logr/logr.go
+++ ee/modules/030-cloud-provider-vcd/images/infra-controller-manager/src/internal/logr/logr.go
@@ -1,3 +1,8 @@
+/*
+Copyright 2023 Flant JSC
+Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+*/
+
 package logr
 
 import (
