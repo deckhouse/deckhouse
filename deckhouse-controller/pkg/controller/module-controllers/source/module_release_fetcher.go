497c08122977fa00715bac54784ae13106c861c8
Sinelnikov Michail <mikhail.sinelnikov@flant.com>
2025-09-04 17:45:44 +0300

    fix
    Signed-off-by: Sinelnikov Michail <mikhail.sinelnikov@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/source/module_release_fetcher.go
+++ deckhouse-controller/pkg/controller/module-controllers/source/module_release_fetcher.go
@@ -306,6 +306,17 @@ func (f *ModuleReleaseFetcher) ensureReleases(
 		return nil
 	}
 
+	// For all non-LTS channels, check that minor version jumps are not allowed
+	// Minor jumps (1.0.0 -> 1.2.0) are not available
+	if f.releaseChannel != ltsReleaseChannel && !isUpdatingSequence(actual.GetVersion(), newSemver) {
+		logger.Warn("Non-LTS channel does not allow minor version jumps",
+			slog.String("current", "v"+actual.GetVersion().String()),
+			slog.String("target", "v"+newSemver.String()),
+			slog.String("channel", f.releaseChannel))
+		return fmt.Errorf("Non-LTS channel does not allow minor version jumps: current 'v%s' target 'v%s'",
+			actual.GetVersion().String(), newSemver.String())
+	}
+
 	vers, err := f.getNewVersions(ctx, actual.GetVersion(), newSemver)
 	if err != nil {
 		return fmt.Errorf("get new versions: %w", err)
