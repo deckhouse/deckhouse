835da4ed2b757bc1e5b330c05b2499c2ae195b3f
Smyslov Maxim <maksim.smyslov@flant.com>
2025-09-08 18:40:34 +0300

    reconciler init
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- modules/002-deckhouse/images/webhook-handler/operator/internal/controller/conversionwebhook_controller.go
+++ modules/002-deckhouse/images/webhook-handler/operator/internal/controller/conversionwebhook_controller.go
@@ -16,19 +16,28 @@ package controller
 
 import (
 	"context"
+	"log/slog"
+	"sync/atomic"
 
+	"github.com/deckhouse/deckhouse/pkg/log"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	deckhouseiov1alpha1 "deckhouse.io/webhook/api/v1alpha1"
 )
 
 // ConversionWebhookReconciler reconciles a ConversionWebhook object
 type ConversionWebhookReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
+	IsReloadShellNeed *atomic.Bool
+	Client            client.Client
+	Scheme            *runtime.Scheme
+	// init logger as in docs builder (watcher)
+	Logger *log.Logger
+	// Go template with python webhook
+	Template string
 }
 
 // +kubebuilder:rbac:groups=deckhouse.io,resources=conversionwebhooks,verbs=get;list;watch;create;update;patch;delete
@@ -45,11 +54,93 @@ type ConversionWebhookReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
 func (r *ConversionWebhookReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	var res ctrl.Result
 
-	// TODO(user): your logic here
+	r.Logger.Debug("conversion webhook processing started", slog.String("resource_name", req.Name))
+	defer func() {
+		r.Logger.Debug("conversion webhook processing complete", slog.String("resource_name", req.Name), slog.Any("reconcile_result", res))
+	}()
 
-	return ctrl.Result{}, nil
+	webhook := new(deckhouseiov1alpha1.ConversionWebhook)
+	err := r.Client.Get(ctx, req.NamespacedName, webhook)
+	if err != nil {
+		r.Logger.Warn("error get resource", slog.String("name", req.Name))
+		// resource may no longer exist, in which case we stop
+		// processing.
+		if apierrors.IsNotFound(err) {
+			return res, nil
+		}
+
+		r.Logger.Debug("get conversion webhook", log.Err(err))
+
+		return res, err
+	}
+
+	// resource marked as "to delete"
+	r.Logger.Debug("debug deletion timestamp", slog.Any("timestamp", webhook.DeletionTimestamp))
+	if !webhook.DeletionTimestamp.IsZero() {
+		// TODO: finalizer deletion logic
+		r.Logger.Debug("conversion webhook deletion", slog.String("deletion_timestamp", webhook.DeletionTimestamp.String()))
+
+		res, err := r.handleDeleteConversionWebhook(ctx, webhook)
+		if err != nil {
+			r.Logger.Warn("delete conversion webhook", log.Err(err))
+
+			return res, err
+		}
+		return res, nil
+	}
+
+	res, err = r.handleProcessConversionWebhook(ctx, webhook)
+	if err != nil {
+		r.Logger.Warn("bla bla", log.Err(err))
+
+		return res, err
+	}
+
+	return res, nil
+}
+
+func (r *ConversionWebhookReconciler) handleProcessConversionWebhook(ctx context.Context, ch *deckhouseiov1alpha1.ConversionWebhook) (ctrl.Result, error) {
+	var res ctrl.Result
+
+	_, _ = ctx, ch
+
+	// add finalizer
+	if !controllerutil.ContainsFinalizer(ch, deckhouseiov1alpha1.ValidationWebhookFinalizer) {
+		r.Logger.Debug("add finalizer")
+		controllerutil.AddFinalizer(ch, deckhouseiov1alpha1.ValidationWebhookFinalizer)
+
+		err := r.Client.Update(ctx, ch)
+		if err != nil {
+			log.Warn("add finalizer err", slog.String("err", err.Error()))
+		}
+	}
+
+	r.IsReloadShellNeed.Store(true)
+
+	return res, nil
+}
+
+func (r *ConversionWebhookReconciler) handleDeleteConversionWebhook(ctx context.Context, ch *deckhouseiov1alpha1.ConversionWebhook) (ctrl.Result, error) {
+	var res ctrl.Result
+
+	_, _ = ctx, ch
+
+	// remove finalizer
+	if controllerutil.ContainsFinalizer(ch, deckhouseiov1alpha1.ValidationWebhookFinalizer) {
+		r.Logger.Debug("remove finalizer")
+		controllerutil.RemoveFinalizer(ch, deckhouseiov1alpha1.ValidationWebhookFinalizer)
+
+		err := r.Client.Update(ctx, ch)
+		if err != nil {
+			log.Warn("remove finalizer err", slog.String("err", err.Error()))
+		}
+	}
+
+	r.IsReloadShellNeed.Store(true)
+
+	return res, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
