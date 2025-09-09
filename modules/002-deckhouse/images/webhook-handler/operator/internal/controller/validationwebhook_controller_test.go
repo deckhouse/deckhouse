00d0e16eb1a16c923aac0def8c742115f3211e8e
Smyslov Maxim <maksim.smyslov@flant.com>
2025-09-09 12:46:33 +0300

    resolve some TODOs
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- modules/002-deckhouse/images/webhook-handler/operator/internal/controller/validationwebhook_controller_test.go
+++ modules/002-deckhouse/images/webhook-handler/operator/internal/controller/validationwebhook_controller_test.go
@@ -11,8 +11,11 @@ import (
 	deckhouseiov1alpha1 "deckhouse.io/webhook/api/v1alpha1"
 	"github.com/deckhouse/deckhouse/pkg/log"
 	"github.com/stretchr/testify/assert"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/yaml"
 )
 
@@ -36,7 +39,7 @@ func setupTestReconciler() *ValidationWebhookReconciler {
 		Client:            k8sClient,
 		Scheme:            sch,
 		Logger:            log.NewLogger(log.WithLevel(slog.LevelDebug)),
-		Template:          string(tpl),
+		PythonTemplate:    string(tpl),
 	}
 }
 
@@ -72,26 +75,51 @@ func TestTemplateNoError(t *testing.T) {
 	// hooks/002-deckhouse/webhooks/validating
 	// os.MkdirAll("/hooks/"+vh.Name+"/webhooks/validating/", 0777)
 
+	// setup
 	r := setupTestReconciler()
+	ctx := context.TODO()
 
 	vh, err := getStructFromYamlFile("testdata/validating/validationwebhook-sample.yaml")
 	assert.NoError(t, err)
 
-	_, err = r.handleProcessValidatingWebhook(context.TODO(), vh)
+	err = r.Client.Create(ctx, vh)
+	assert.NoError(t, err)
+
+	_, err = r.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Namespace: vh.Namespace, Name: vh.Name}})
 	assert.NoError(t, err)
 
 	// test equality
 	ref, err := os.ReadFile("testdata/validating/golden/validationwebhook-sample.py")
 	assert.NoError(t, err)
+
 	res, err := os.ReadFile("hooks/validationwebhook-sample/webhooks/validating/validationwebhook-sample.py")
 	assert.NoError(t, err)
 	assert.Equal(t, string(ref), string(res))
+
+	// test delete
+	err = r.Client.Get(ctx, types.NamespacedName{Namespace: vh.Namespace, Name: vh.Name}, vh)
+	assert.NoError(t, err)
+
+	err = r.Client.Delete(ctx, vh)
+	assert.NoError(t, err)
+
+	_, err = r.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Namespace: vh.Namespace, Name: vh.Name}})
+	assert.NoError(t, err)
+
+	err = r.Client.Get(ctx, types.NamespacedName{Namespace: vh.Namespace, Name: vh.Name}, vh)
+	assert.True(t, apierrors.IsNotFound(err))
+
+	_, err = os.ReadFile("hooks/validationwebhook-sample/webhooks/validating/validationwebhook-sample.py")
+	assert.True(t, os.IsNotExist(err))
 }
 
 func TestTemplateNoContext(t *testing.T) {
 	r := setupTestReconciler()
 
 	vh, err := getStructFromYamlFile("testdata/validating/sample_without_context.yaml")
+	assert.NoError(t, err)
+
+	err = r.Client.Create(context.Background(), vh)
 	assert.NoError(t, err)
 
 	_, err = r.handleProcessValidatingWebhook(context.TODO(), vh)
@@ -111,6 +139,9 @@ func TestTemplateTwoContext(t *testing.T) {
 	vh, err := getStructFromYamlFile("testdata/validating/sample_two_context.yaml")
 	assert.NoError(t, err)
 
+	err = r.Client.Create(context.Background(), vh)
+	assert.NoError(t, err)
+
 	_, err = r.handleProcessValidatingWebhook(context.TODO(), vh)
 	assert.NoError(t, err)
 
@@ -127,6 +158,9 @@ func TestTemplateEqual(t *testing.T) {
 	r := setupTestReconciler()
 
 	vh, err := getStructFromYamlFile("testdata/validating/prometheusremotewrite.yaml")
+	assert.NoError(t, err)
+
+	err = r.Client.Create(context.Background(), vh)
 	assert.NoError(t, err)
 
 	_, err = r.handleProcessValidatingWebhook(context.TODO(), vh)
