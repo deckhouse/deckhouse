d296e339bb4814c84e6e30f7b728a54870784653
Stepan Paksashvili <stepan.paksashvili@flant.com>
2025-09-10 15:38:43 +0300

    [deckhouse] deckhouse is default source
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/source/helpers.go
+++ deckhouse-controller/pkg/controller/module-controllers/source/helpers.go
@@ -199,7 +199,7 @@ func (r *reconciler) needToEnsureRelease(
 			return false
 		}
 
-		if len(module.Properties.AvailableSources) > 1 && (source.Name != "deckhouse") {
+		if len(module.Properties.AvailableSources) > 1 && source.Name != "deckhouse" {
 			return false
 		}
 	} else if module.IsCondition(v1alpha1.ModuleConditionEnabledByModuleConfig, corev1.ConditionFalse) {
