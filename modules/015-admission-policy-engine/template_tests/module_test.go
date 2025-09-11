54f3b8113f2ca48cffa749f4d21e51ae58431950
dmitry.trofimov <dmitry.trofimov@falnt.com>
2025-09-11 16:38:25 +0300

    add 1.34 k8s version support
    Signed-off-by: dmitry.trofimov <dmitry.trofimov@falnt.com>

--- modules/015-admission-policy-engine/template_tests/module_test.go
+++ modules/015-admission-policy-engine/template_tests/module_test.go
@@ -51,7 +51,7 @@ clusterConfiguration:
 discovery:
   clusterMasterCount: 3
   prometheusScrapeInterval: 30
-  kubernetesVersion: "1.29.14"
+  kubernetesVersion: "1.30"
   d8SpecificNodeCountByRole:
     system: 1
 modules:
