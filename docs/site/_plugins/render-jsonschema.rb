7ae1f947d30900ba0f29aebc3467ced6184d9393
deckhouse-BOaTswain <89150800+deckhouse-BOaTswain@users.noreply.github.com>
2025-09-15 14:49:18 +0300

    [docs] Delete quotation for default values in OpenAPI schemas (#15362) (#15461)
    Signed-off-by: Denis.Rebenok <denis.rebenok@flant.ru>
    Co-authored-by: Denis Rebenok <60424108+denmaloyreb@users.noreply.github.com>
    Co-authored-by: Denis.Rebenok <denis.rebenok@flant.ru>

--- docs/site/_plugins/render-jsonschema.rb
+++ docs/site/_plugins/render-jsonschema.rb
@@ -310,7 +310,7 @@ module JSONSchemaRenderer
                 end
             else
                 if attributes['type'] == 'string'
-                    result.push(sprintf(%q(<p class="resources__attrs"><span class="resources__attrs_name">%s:</span> <span class="resources__attrs_content"><code>"%s"</code></span></p>), get_i18n_term("default_value").capitalize, attributes['x-doc-default']))
+                    result.push(sprintf(%q(<p class="resources__attrs"><span class="resources__attrs_name">%s:</span> <span class="resources__attrs_content"><code>%s</code></span></p>), get_i18n_term("default_value").capitalize, attributes['x-doc-default']))
                 else
                     result.push(sprintf(%q(<p class="resources__attrs"><span class="resources__attrs_name">%s:</span> <span class="resources__attrs_content"><code>%s</code></span></p>), get_i18n_term("default_value").capitalize, attributes['x-doc-default']))
                 end
@@ -322,7 +322,7 @@ module JSONSchemaRenderer
                 end
             else
                 if attributes['type'] == 'string'
-                    result.push(sprintf(%q(<p class="resources__attrs"><span class="resources__attrs_name">%s:</span> <span class="resources__attrs_content"><code>"%s"</code></span></p>), get_i18n_term("default_value").capitalize, attributes['default']))
+                    result.push(sprintf(%q(<p class="resources__attrs"><span class="resources__attrs_name">%s:</span> <span class="resources__attrs_content"><code>%s</code></span></p>), get_i18n_term("default_value").capitalize, attributes['default']))
                 else
                     result.push(sprintf(%q(<p class="resources__attrs"><span class="resources__attrs_name">%s:</span> <span class="resources__attrs_content"><code>%s</code></span></p>), get_i18n_term("default_value").capitalize, attributes['default']))
                 end
