9c05c664aa02c60770c51319bfef9da78757ee9f
Mikhail Scherba <mikhail.scherba@flant.com>
2025-09-22 19:48:55 +0300

    update wrapper
    Signed-off-by: Mikhail Scherba <mikhail.scherba@flant.com>

--- modules/402-ingress-nginx/images/controller-1-10/nginx-chroot-wrapper.sh
+++ modules/402-ingress-nginx/images/controller-1-10/nginx-chroot-wrapper.sh
@@ -18,26 +18,25 @@ cat /etc/resolv.conf > /chroot/etc/resolv.conf
 
 if [ "$NGINX_PROFILING_ENABLED" == "true" ]; then
   args="$@"
+  nginxWithCaps="/chroot/usr/local/nginx/sbin/nginx"
+  nginxWOCaps="/chroot/etc/ingress-controller/nginx/"
+
+  if [ ! -f $nginxWOCaps ]; then
+    # copy the nginx binary to drop capabilities (valgrind doesn't want to profile a privileged file)
+    cp -f $nginxWithCaps $nginxWOCaps
+  fi
+
   if [ "$args" == "-c /etc/nginx/nginx.conf" ]; then
     logDirInChroot="/var/log/valgrind"
     timestamp=$(date +%Y%m%d_%H%M%S)
     logfile="${logDirInChroot}/memcheck.${timestamp}.log"
 
-    # echo "Drop NGINX file capabilities as it prevent valgrind from running"
-    nginxWithCaps="/chroot/usr/local/nginx/sbin/nginx"
-    nginxWOCaps="/chroot/etc/ingress-controller/nginx/"
-    # copy the nginx binary to drop capabilities (valgrind doesn't want to profile a privileged file)
-    cp -f $nginxWithCaps $nginxWOCaps
-
     # echo "Mounting proc fs"
     # unshare --mount-proc -f -p don't work, need use mount -t proc for parent pid
     mount -t proc /proc /chroot/proc
-    # Set hack for www-data user
-    # echo 'www-data:x:64535:64535:www-data:/nonexistent:/usr/sbin/nologin' >> /chroot/etc/passwd
-    # echo 'www-data:x:64535:' >> /chroot/etc/group
 
-  # echo "Run profiling with Valgrind"
-  # echo "The log will be written to a file $logfile"
+    # echo "Run profiling with Valgrind"
+    # echo "The log will be written to a file $logfile"
     unshare -R /chroot /usr/local/valgrind \
       --trace-children=yes \
       --log-file="$logfile" \
