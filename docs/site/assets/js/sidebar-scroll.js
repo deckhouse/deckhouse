2f629c9a43437b805e82a44a4014381819c113a8
Polina Polyntseva <pvsskk@mail.ru>
2025-09-29 15:35:23 +0300

    correct
    Signed-off-by: Polina Polyntseva <pvsskk@mail.ru>

--- docs/site/assets/js/sidebar-scroll.js
+++ docs/site/assets/js/sidebar-scroll.js
@@ -19,13 +19,9 @@ $(window).on('load', function() {
     lastPathname = lastPathname.replace(/v1/g, '.');
   }
   const activeLink = document.querySelector(`ul.sidebar li.sidebar__item.active a[href$="${lastPathname}"]`);
-  
-  const hash = window.location.hash;
-  const activeLinkToc = document.querySelector(`li.toc-sidebar__item a[href="${hash}"]`);
 
   const sidebars = document.querySelectorAll('.sidebar__wrapper-inner');
   const sidebarLeft = document.querySelector('.sidebar');
-  const sidebarToc = document.querySelector('.toc-sidebar');
   
   function getTopActiveLink(element, sidebar) {
     let top = 0;
@@ -45,8 +41,9 @@ $(window).on('load', function() {
   }
 
   const activeLinkTop = getTopActiveLinkSidebar(activeLink);
+  const activeLinkTopForScroll = getTopActiveLink(activeLink, sidebarLeft);
 
-  if(activeLinkTop < sidebars[0].scrollTop || (activeLinkTop + activeLink.offsetHeight) > (sidebars[0].scrollTop + sidebars[0].scrollHeight)) {
+  if(activeLinkTopForScroll < sidebars[0].scrollTop || (activeLinkTopForScroll + activeLink.offsetHeight) > (sidebars[0].scrollTop + sidebars[0].scrollHeight)) {
     sidebars[0].scrollIntoView({
       block: 'nearest',
       behavior: 'smooth'
@@ -55,13 +52,6 @@ $(window).on('load', function() {
 
   sidebars[0].scrollTo({
     top: activeLinkTop,
-    behavior: 'smooth'
-  })
-
-  const tocActiveLinkTop = getTopActiveLink(activeLinkToc, sidebarToc);
-
-  sidebars[1].scrollTo({
-    top: tocActiveLinkTop,
     behavior: 'smooth'
   })
 });
