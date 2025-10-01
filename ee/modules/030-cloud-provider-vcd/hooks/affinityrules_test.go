298e7a0f203e604e57165285648854e565384ac6
Alexey Voronov <alexey.voronov@flant.com>
2025-10-01 12:26:14 +0700

    hook test wijp
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/modules/030-cloud-provider-vcd/hooks/affinityrules_test.go
+++ ee/modules/030-cloud-provider-vcd/hooks/affinityrules_test.go
@@ -12,48 +12,35 @@ import (
 	. "github.com/deckhouse/deckhouse/testing/hooks"
 )
 
-var _ = Describe("Modules :: cloud-provider-vcd :: hooks :: affinity_rules_from_vcdinstanceclass ::", func() {
-	initValues := `
-cloudProviderVcd:
-	internal:
-		providerClusterConfiguration:
-			masterNodeGroup:
-				affinityRule:
-       	  polarity: AntiAffinity
-       	  required: true
-			nodeGroups:
-			- name: front
-				affinityRule:
-					polarity: AntiAffinity
-					required: false
-			- name: worker
-				affinityRule:
-					polarity: Affinity
+var _ = Describe("Modules :: cloud-provider-vcd :: hooks :: affinity rules from vcdinstanceclass ::", func() {
+	initValues := `cloudProviderVcd:
+  internal:
+    providerClusterConfiguration:
+      masterNodeGroup:
+        affinityRule:
+          polarity: AntiAffinity
+          required: true
+      nodeGroups:
+      - name: front
+        affinityRule:
+          polarity: AntiAffinity
+          required: false
+      - name: worker
+        affinityRule:
+          polarity: Affinity
 `
-	instanceClassA := `
-apiVersion: deckhouse.io/v1
-kind: VCDInstanceClass
-metadata:
-	name: class-a
-spec:
-	affinityRule:
-		polarity: Affinity
-		required: true
-status:
-	nodeGroupConsumers:
-	- monitoring
-`	
-	a := HookExecutionConfigInit(initValues, `{}`)
+	abc := HookExecutionConfigInit(initValues, `{}`)
 	Context("Cluster has empty state", func() {
 		BeforeEach(func() {
-			a.BindingContexts.Set(a.KubeStateSet(instanceClassA))
-			a.RunHook()
+			abc.BindingContexts.Set(abc.KubeStateSet(""))
+			abc.RunHook()
 		})
+
 		It("Hook should not fail with errors", func() {
-			Expect(a).To(ExecuteSuccessfully())
-			Expect(a.GoHookError).Should(BeNil())
-			Expect(a.ValuesGet("cloudProviderVcd.internal.affinityRules").Exists()).To(BeTrue())
+			Expect(abc).To(ExecuteSuccessfully())
+			Expect(abc.GoHookError).Should(BeNil())
+			Expect(abc.ValuesGet("cloudProviderVcd.internal.affinityRules").Exists()).To(BeTrue())
 		})
-		
+
 	})
 })
