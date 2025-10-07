50c4c455719cdb20589de6bdce38ff0b9853da6d
Kanatbek Saribaev <kanatbek.saribaev@flant.com>
2025-10-07 22:55:44 +0300

    vsphere to commander
    Signed-off-by: Kanatbek Saribaev <kanatbek.saribaev@flant.com>

--- testing/cloud_layouts/script-commander.sh
+++ testing/cloud_layouts/script-commander.sh
@@ -236,7 +236,7 @@ function prepare_environment() {
     bastion_user="ubuntu"
     bastion_host="31.128.54.168"
     bastion_port="53359"
-    ssh_bastion="-o \"ProxyJump=${bastion_user}@${bastion_host}:${bastion_port}\""
+    ssh_bastion="-o ProxyJump=${bastion_user}@${bastion_host}:${bastion_port}"
     cluster_template_id="3e331a3d-8757-41b6-8c7e-4a8f5d2caea9"
     values="{
       \"branch\": \"${DEV_BRANCH}\",
