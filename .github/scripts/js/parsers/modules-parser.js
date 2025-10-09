0534c6376f6324faf640055c4f574adce9f05791
Alexey Paschenko <alexey.paschenko@flant.com>
2025-10-09 12:33:30 +0300

    Add common module as allowed in validation CI
    Signed-off-by: Alexey Paschenko <alexey.paschenko@flant.com>

--- .github/scripts/js/parsers/modules-parser.js
+++ .github/scripts/js/parsers/modules-parser.js
@@ -11,7 +11,7 @@
 
 const fs = require('fs');
 
-const STATIC_MODULES = ['ci', 'dependabot', 'testing', 'tools', 'docs', 'candi', 'deckhouse-controller'];
+const STATIC_MODULES = ['ci', 'dependabot', 'testing', 'tools', 'docs', 'candi', 'deckhouse-controller', 'common'];
 
 /**
  *
