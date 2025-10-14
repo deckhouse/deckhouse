917df957e71b04630787e44ca732e200da6970a3
Timur Tuktamyshev <timur.tuktamyshev@flant.com>
2025-10-14 12:06:56 +0300

    feat: add new package structures and controllers
    Signed-off-by: Timur Tuktamyshev <timur.tuktamyshev@flant.com>

---
+++ deckhouse-controller/pkg/apis/deckhouse.io/v1alpha1/application.go
@@ -0,0 +1,112 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+const (
+	ApplicationResource = "applications"
+	ApplicationKind     = "Application"
+
+	ApplicationStatusHealthy  = "Healthy"
+	ApplicationStatusDegraded = "Degraded"
+	ApplicationStatusError    = "Error"
+
+	ApplicationConditionRequirementsMet        = "RequirementsMet"
+	ApplicationConditionStartupHooksSuccessful = "StartupHooksSuccessful"
+	ApplicationConditionManifestsDeployed      = "ManifestsDeployed"
+	ApplicationConditionReplicasAvailable      = "ReplicasAvailable"
+)
+
+var (
+	ApplicationGVR = schema.GroupVersionResource{
+		Group:    SchemeGroupVersion.Group,
+		Version:  SchemeGroupVersion.Version,
+		Resource: ApplicationResource,
+	}
+	ApplicationGVK = schema.GroupVersionKind{
+		Group:   SchemeGroupVersion.Group,
+		Version: SchemeGroupVersion.Version,
+		Kind:    ApplicationKind,
+	}
+)
+
+var _ runtime.Object = (*Application)(nil)
+
+// +genclient
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Namespaced
+
+// Application represents a namespace-scoped application instance.
+type Application struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Spec defines the behavior of an Application.
+	Spec ApplicationSpec `json:"spec"`
+
+	// Status of an Application.
+	Status ApplicationStatus `json:"status,omitempty"`
+}
+
+type ApplicationSpec struct {
+	ApplicationPackageName string          `json:"applicationPackageName"`
+	Repository             string          `json:"repository,omitempty"`
+	Version                string          `json:"version"`
+	ReleaseChannel         string          `json:"releaseChannel,omitempty"`
+	Settings               *SettingsValues `json:"settings,omitempty"`
+}
+
+type ApplicationStatus struct {
+	Version    *ApplicationStatusVersion    `json:"version,omitempty"`
+	Repository string                       `json:"repository,omitempty"`
+	Status     string                       `json:"status,omitempty"`
+	Conditions []ApplicationStatusCondition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+type ApplicationStatusVersion struct {
+	Current string `json:"current,omitempty"`
+	Channel string `json:"channel,omitempty"`
+}
+
+type ApplicationStatusCondition struct {
+	Type               string                 `json:"type"`
+	Status             corev1.ConditionStatus `json:"status"`
+	Reason             string                 `json:"reason,omitempty"`
+	Message            string                 `json:"message,omitempty"`
+	LastProbeTime      metav1.Time            `json:"lastProbeTime,omitempty"`
+	LastTransitionTime metav1.Time            `json:"lastTransitionTime,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ApplicationList is a list of Application resources
+type ApplicationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []Application `json:"items"`
+}
---
+++ deckhouse-controller/pkg/controller/package-system/application/controller.go
@@ -0,0 +1,98 @@
+// Copyright 2025 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package application
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/apis/deckhouse.io/v1alpha1"
+	"github.com/deckhouse/deckhouse/pkg/log"
+)
+
+const (
+	controllerName = "d8-application-controller"
+
+	maxConcurrentReconciles = 3
+)
+
+type reconciler struct {
+	client client.Client
+	logger *log.Logger
+}
+
+func RegisterController(
+	runtimeManager manager.Manager,
+	logger *log.Logger,
+) error {
+	r := &reconciler{
+		client: runtimeManager.GetClient(),
+		logger: logger,
+	}
+
+	applicationController, err := controller.New(controllerName, runtimeManager, controller.Options{
+		MaxConcurrentReconciles: maxConcurrentReconciles,
+		Reconciler:              r,
+	})
+	if err != nil {
+		return fmt.Errorf("create controller: %w", err)
+	}
+
+	return ctrl.NewControllerManagedBy(runtimeManager).
+		For(&v1alpha1.Application{}).
+		Complete(applicationController)
+}
+
+func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	r.logger.Debug("reconciling Application", slog.String("name", req.Name), slog.String("namespace", req.Namespace))
+
+	application := new(v1alpha1.Application)
+	if err := r.client.Get(ctx, req.NamespacedName, application); err != nil {
+		if apierrors.IsNotFound(err) {
+			r.logger.Warn("application not found", slog.String("name", req.Name), slog.String("namespace", req.Namespace))
+			return ctrl.Result{}, nil
+		}
+		r.logger.Error("failed to get application", slog.String("name", req.Name), slog.String("namespace", req.Namespace), log.Err(err))
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	// handle delete event
+	if !application.DeletionTimestamp.IsZero() {
+		r.logger.Debug("deleting application", slog.String("name", req.Name), slog.String("namespace", req.Namespace))
+		return r.delete(ctx, application)
+	}
+
+	// handle create/update events
+	return r.handle(ctx, application)
+}
+
+func (r *reconciler) handle(_ context.Context, application *v1alpha1.Application) (ctrl.Result, error) {
+	// TODO: implement application reconciliation logic
+	r.logger.Info("handling Application", slog.String("name", application.Name), slog.String("namespace", application.Namespace))
+	return ctrl.Result{}, nil
+}
+
+func (r *reconciler) delete(_ context.Context, application *v1alpha1.Application) (ctrl.Result, error) {
+	// TODO: implement application deletion logic
+	r.logger.Info("deleting Application", slog.String("name", application.Name), slog.String("namespace", application.Namespace))
+	return ctrl.Result{}, nil
+}
