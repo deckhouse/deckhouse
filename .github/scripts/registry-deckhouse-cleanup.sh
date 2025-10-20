3e20fdb68904b38f8636c5c618b954072d278eaf
Kirill Kramorov <kirill.kramorov@flant.com>
2025-10-20 19:14:05 +0700

    feat: add stage registry
    Signed-off-by: Kirill Kramorov <kirill.kramorov@flant.com>

--- .github/scripts/registry-deckhouse-cleanup.sh
+++ .github/scripts/registry-deckhouse-cleanup.sh
@@ -15,4 +15,5 @@
 # limitations under the License.
 
 export WERF_PARALLEL_TASKS_LIMIT=21
-werf cleanup --config werf_cleanup.yaml --without-kube --disable-auto-host-cleanup=true --log-color-mode='off' --repo "registry-stage.deckhouse.io/sys/deckhouse-oss"
+export REGISTRY_URL="${DECKHOUSE_REGISTRY_STAGE_HOST}/${REGISTRY_PATH}"
+werf cleanup --config werf_cleanup.yaml --without-kube --disable-auto-host-cleanup=true --log-color-mode='off' --repo "${REGISTRY_URL}"
