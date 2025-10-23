8a0706f89cd8fe6fa75440e70a7e7b55d522b47c
Sinelnikov Michail <mikhail.sinelnikov@flant.com>
2025-10-23 13:18:15 +0300

    test fix
    Signed-off-by: Sinelnikov Michail <mikhail.sinelnikov@flant.com>

--- modules/150-user-authn/template_tests/publish_api_test.go
+++ modules/150-user-authn/template_tests/publish_api_test.go
@@ -82,6 +82,10 @@ var _ = Describe("Module :: user-authn :: helm template :: publish api", func() 
 		BeforeEach(func() {
 			hec.ValuesSet("userAuthn.publishAPI.https.mode", "Global")
 			hec.ValuesSet("global.modules.https.mode", "CustomCertificate")
+			hec.ValuesSetFromYaml("global.modules.https.customCertificateData", `
+			tls.crt: CRTCRTCRT
+			tls.key: KEYKEYKEY`)
+
 			hec.HelmRender()
 		})
 
