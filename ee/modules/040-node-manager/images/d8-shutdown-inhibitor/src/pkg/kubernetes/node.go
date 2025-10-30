a8c52b49765467aed51397111f8d02e49481c353
borg-z <me@zykov.tatar>
2025-10-30 10:13:03 +0300

    StrategicMergePatch
    Signed-off-by: borg-z <me@zykov.tatar>

--- ee/modules/040-node-manager/images/d8-shutdown-inhibitor/src/pkg/kubernetes/node.go
+++ ee/modules/040-node-manager/images/d8-shutdown-inhibitor/src/pkg/kubernetes/node.go
@@ -7,12 +7,12 @@ package kubernetes
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/util/retry"
 )
 
 const (
@@ -28,6 +28,20 @@ type Node struct {
 	err    error
 }
 
+type nodePatch struct {
+	Spec     *nodeSpecPatch     `json:"spec,omitempty"`
+	Metadata *nodeMetadataPatch `json:"metadata,omitempty"`
+}
+
+type nodeSpecPatch struct {
+	Unschedulable bool `json:"unschedulable,omitempty"`
+}
+
+type nodeMetadataPatch struct {
+	Annotations map[string]*string `json:"annotations,omitempty"`
+}
+
+
 func (c *Klient) GetNode(ctx context.Context, nodeName string) *Node {
 	node, err := c.clientset.CoreV1().Nodes().Get(ctx, nodeName, metav1.GetOptions{})
 	if err != nil {
@@ -36,74 +50,6 @@ func (c *Klient) GetNode(ctx context.Context, nodeName string) *Node {
 	return &Node{Node: node, client: c}
 }
 
-func (n *Node) update(ctx context.Context, mutate func(*corev1.Node)) *Node {
-    if n.err != nil {
-        return n
-    }
-    err := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
-        current, err := n.client.clientset.CoreV1().Nodes().Get(ctx, n.Name, metav1.GetOptions{})
-        if err != nil {
-            return err
-        }
-        mutate(current)
-        updated, err := n.client.clientset.CoreV1().Nodes().Update(ctx, current, metav1.UpdateOptions{})
-        if err != nil {
-            return err
-        }
-        n.Node = updated
-        return nil
-    })
-
-    if err != nil {
-        n.err = fmt.Errorf("update node %q: %w", n.Name, err)
-    }
-
-    return n
-}
-
-func (n *Node) Uncordon(ctx context.Context) *Node {
-    return n.update(ctx, func(node *corev1.Node) {
-        node.Spec.Unschedulable = false
-    })
-}
-
-func (n *Node) Cordon(ctx context.Context) *Node {
-    return n.update(ctx, func(node *corev1.Node) {
-        node.Spec.Unschedulable = true
-    })
-}
-
-func (n *Node) RemoveCordonAnnotation(ctx context.Context) *Node {
-    return n.update(ctx, func(node *corev1.Node) {
-        if node.Annotations != nil {
-            delete(node.Annotations, CordonAnnotationKey)
-        }
-    })
-}
-
-func (n *Node) SetCordonAnnotation(ctx context.Context) *Node {
-	if n.err != nil {
-		return n
-	}
-	patch := fmt.Sprintf(
-		`{"metadata":{"annotations":{"%s":"%s"}}}`,
-		CordonAnnotationKey,
-		CordonAnnotationValue,
-	)
-	node, err := n.client.clientset.CoreV1().Nodes().Patch(
-		ctx,
-		n.Name,
-		types.StrategicMergePatchType,
-		[]byte(patch),
-		metav1.PatchOptions{},
-	)
-	if err != nil {
-		n.err = fmt.Errorf("set cordon annotation on node %q: %w", n.Name, err)
-		return n
-	}
-	n.Node = node
-	return n
-}
 
 func (n *Node) GetAnnotationCordonedBy() (string, error) {
 	annotations := n.GetAnnotations()
@@ -120,6 +66,66 @@ func (n *Node) GetConditionByReason(reason string) (corev1.NodeCondition, error)
 		}
 	}
 	return corev1.NodeCondition{}, n.err
+}
+
+func (n *Node) patch(ctx context.Context, p nodePatch) *Node {
+	if n.err != nil {
+		return n
+	}
+
+	patchB, err := json.Marshal(p)
+	if err != nil {
+		n.err = fmt.Errorf("marshal patch: %w", err)
+		return n
+	}
+
+	node, err := n.client.clientset.CoreV1().Nodes().Patch(
+		ctx,
+		n.Name,
+		types.StrategicMergePatchType,
+		patchB,
+		metav1.PatchOptions{},
+	)
+	if err != nil {
+		n.err = fmt.Errorf("patch node %q: %w", n.Name, err)
+		return n
+	}
+
+	n.Node = node
+	return n
+}
+
+func (n *Node) Cordon(ctx context.Context) *Node {
+	return n.patch(ctx, nodePatch{
+		Spec: &nodeSpecPatch{Unschedulable: true},
+	})
+}
+
+func (n *Node) Uncordon(ctx context.Context) *Node {
+	return n.patch(ctx, nodePatch{
+		Spec: &nodeSpecPatch{Unschedulable: false},
+	})
+}
+
+func (n *Node) SetCordonAnnotation(ctx context.Context) *Node {
+	value := CordonAnnotationValue
+	return n.patch(ctx, nodePatch{
+		Metadata: &nodeMetadataPatch{
+			Annotations: map[string]*string{
+				CordonAnnotationKey: &value,
+			},
+		},
+	})
+}
+
+func (n *Node) RemoveCordonAnnotation(ctx context.Context) *Node {
+	return n.patch(ctx, nodePatch{
+		Metadata: &nodeMetadataPatch{
+			Annotations: map[string]*string{
+				CordonAnnotationKey: nil,
+			},
+		},
+	})
 }
 
 func (n *Node) PatchCondition(ctx context.Context, condType, status, reason, message string) *Node {
