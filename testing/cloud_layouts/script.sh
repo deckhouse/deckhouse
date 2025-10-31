f9486707e81844694520e60a8d047ec90df9c82f
deckhouse-BOaTswain <89150800+deckhouse-BOaTswain@users.noreply.github.com>
2025-10-31 09:06:03 +0300

    [ci] Fix e2e upgrade tests (#16255) (#16262)
    Signed-off-by: Vladimir Portnov <vladimir.portnov@flant.com>
    Co-authored-by: Vladimir Portnov <vladimir.portnov@flant.com>

--- testing/cloud_layouts/script.sh
+++ testing/cloud_layouts/script.sh
@@ -288,7 +288,7 @@ function prepare_environment() {
     if [[ "${DECKHOUSE_IMAGE_TAG}" =~ release-([0-9]+\.[0-9]+) ]]; then
       DEV_BRANCH="${INITIAL_IMAGE_TAG}"
       SWITCH_TO_IMAGE_TAG="v${BASH_REMATCH[1]}.0"
-      update_release_channel "${DEV_REGISTRY_PATH}" "${SWITCH_TO_IMAGE_TAG}"
+      update_release_channel "$(echo -n "${STAGE_DECKHOUSE_DOCKERCFG}" | base64 -d | awk -F'\"' '{print $4}')/${REGISTRY_PATH}" "${SWITCH_TO_IMAGE_TAG}"
       echo "Will install '${DEV_BRANCH}' first and then update to '${DECKHOUSE_IMAGE_TAG}' as '${SWITCH_TO_IMAGE_TAG}'"
     else
       echo "'${DECKHOUSE_IMAGE_TAG}' doesn't look like a release branch. Update command politely ignored."
