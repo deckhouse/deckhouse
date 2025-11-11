9fae1f961d68af4753d49d8af2ab3e95be643e9a
Dmitry Trofimov <161188690+trofimovdals@users.noreply.github.com>
2025-11-11 15:58:10 +0300

    [registrypackages] Fix containerd cve (#16335)
    Signed-off-by: dmitry.trofimov <dmitry.trofimov@falnt.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -470,8 +470,8 @@ var DefaultImagesDigests = map[string]interface{}{
 	},
 	"registrypackages": map[string]interface{}{
 		"amazonEc2Utils220":               "imageHash-registrypackages-amazonEc2Utils220",
-		"containerd1728":                  "imageHash-registrypackages-containerd1728",
-		"containerd214":                   "imageHash-registrypackages-containerd214",
+		"containerd1729":                  "imageHash-registrypackages-containerd1729",
+		"containerd215":                   "imageHash-registrypackages-containerd215",
 		"crictl129":                       "imageHash-registrypackages-crictl129",
 		"crictl129VexArtifact":            "imageHash-registrypackages-crictl129VexArtifact",
 		"crictl130":                       "imageHash-registrypackages-crictl130",
