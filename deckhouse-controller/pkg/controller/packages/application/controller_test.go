4ce61ce9fed1b8c26eb8c8599a58ee620ff70d90
Sinelnikov Michail <mikhail.sinelnikov@flant.com>
2025-11-14 17:41:31 +0300

    fix
    Signed-off-by: Sinelnikov Michail <mikhail.sinelnikov@flant.com>

--- deckhouse-controller/pkg/controller/packages/application/controller_test.go
+++ deckhouse-controller/pkg/controller/packages/application/controller_test.go
@@ -92,7 +92,7 @@ func (suite *ControllerTestSuite) TearDownSubTest() {
 	ctx := context.Background()
 	err := suite.ctr.statusService.WaitForIdle(ctx)
 	require.NoError(suite.T(), err)
-	time.Sleep(5 * time.Second)
+	time.Sleep(8 * time.Second)
 
 	goldenFile := filepath.Join("./testdata", "golden", suite.testDataFileName)
 	gotB := suite.fetchResults()
