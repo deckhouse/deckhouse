267141dded382c0ef58c4bab1b6de19c95b1f8e4
borg-z <me@zykov.tatar>
2025-11-24 18:54:58 +0300

    ++
    Signed-off-by: borg-z <me@zykov.tatar>

--- ee/modules/040-node-manager/images/d8-shutdown-inhibitor/src/pkg/inputdev/watcher.go
+++ ee/modules/040-node-manager/images/d8-shutdown-inhibitor/src/pkg/inputdev/watcher.go
@@ -6,11 +6,11 @@ Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https
 package inputdev
 
 import (
+	"errors"
 	"fmt"
+	"log/slog"
 	"syscall"
 	"unsafe"
-
-	"log/slog"
 
 	dlog "github.com/deckhouse/deckhouse/pkg/log"
 )
@@ -21,6 +21,8 @@ type Watcher struct {
 	stopCh    chan struct{}
 	pressedCh chan struct{}
 }
+
+var errDevicesNeedRefresh = errors.New("input devices need refresh")
 
 func NewWatcher(devs []Device, buttons ...Button) *Watcher {
 	return &Watcher{
@@ -59,17 +61,24 @@ func (w *Watcher) watch() {
 }
 
 func (w *Watcher) readEvents(evCh chan *inputEvent) {
-	fds := []int{}
+	var fds []int
+
+refreshDevices:
+	fds = fds[:0]
 
 	// Open each device.
 	for _, dev := range w.devs {
+		dlog.Info("power button watcher device:", slog.Any("DevPath", dev.DevPath))
 		fd, err := syscall.Open(dev.DevPath, syscall.O_RDONLY, 0)
 		if err != nil {
 			continue
 		}
-		defer syscall.Close(fd)
 		fds = append(fds, fd)
-		//devices[fd] = devicePath
+	}
+
+	if len(fds) == 0 {
+		dlog.Warn("power button watcher: no file descriptors to watch")
+		return
 	}
 
 	// Create FdSet bitmask
@@ -82,6 +91,7 @@ func (w *Watcher) readEvents(evCh chan *inputEvent) {
 		// Return if watcher was stopped.
 		select {
 		case <-w.stopCh:
+			closeFDs(fds)
 			return
 		default:
 		}
@@ -91,7 +101,7 @@ func (w *Watcher) readEvents(evCh chan *inputEvent) {
 		// TODO add timeout to check stopCh more frequently?
 		_, err := syscall.Select(fdMax, &fdSet, nil, nil, nil)
 		if err != nil {
-			dlog.Warn("input watcher: select failed", dlog.Err(err))
+			dlog.Warn("power button watcher: select failed", dlog.Err(err))
 			continue
 		}
 
@@ -101,14 +111,51 @@ func (w *Watcher) readEvents(evCh chan *inputEvent) {
 				continue
 			}
 
-			event, err := readEvent(fd)
+			event, err := w.readEvent(fd)
 			if err != nil {
-				dlog.Warn("input watcher: read event failed", slog.Int("fd", fd), dlog.Err(err))
+				if errors.Is(err, errDevicesNeedRefresh) {
+					closeFDs(fds)
+					goto refreshDevices
+				}
+				dlog.Warn("power button watcher: read event failed", slog.Int("fd", fd), dlog.Err(err))
 				continue
 			}
 
 			evCh <- event
 		}
+	}
+}
+
+
+func closeFDs(fds []int) {
+	for _, fd := range fds {
+		_ = syscall.Close(fd)
+	}
+}
+
+func (w *Watcher) readEvent(fd int) (*inputEvent, error) {
+	var event inputEvent
+	err := w.binaryRead(fd, unsafe.Pointer(&event), unsafe.Sizeof(event))
+	if err != nil {
+		if errors.Is(err, syscall.ENODEV) {
+			dlog.Error("power button watcher: device disappeared", slog.Int("fd", fd))
+			w.refreshDevsOnError()
+			return nil, fmt.Errorf("%w: %v", errDevicesNeedRefresh, err)
+		}
+
+		return nil, fmt.Errorf("read event error: %v\n", err)
+	}
+
+	return &event, nil
+}
+
+func (w *Watcher) refreshDevsOnError() {
+	dlog.Info("power button watcher: refresh devs list")
+	var err error
+
+	w.devs, err = ListInputDevicesWithAnyButton(KEY_POWER, KEY_POWER2)
+	if err != nil {
+		dlog.Error("power button watcher: refresh devs list", dlog.Err(err))
 	}
 }
 
@@ -131,17 +178,7 @@ func (ev *inputEvent) isAnyButtonPressed(buttons []Button) bool {
 	return false
 }
 
-func readEvent(fd int) (*inputEvent, error) {
-	var event inputEvent
-	err := binaryRead(fd, unsafe.Pointer(&event), unsafe.Sizeof(event))
-	if err != nil {
-		return nil, fmt.Errorf("read event error: %v\n", err)
-	}
-
-	return &event, nil
-}
-
-func binaryRead(fd int, data unsafe.Pointer, size uintptr) error {
+func (w *Watcher) binaryRead(fd int, data unsafe.Pointer, size uintptr) error {
 	buf := make([]byte, size)
 	n, err := syscall.Read(fd, buf)
 	if err != nil {
