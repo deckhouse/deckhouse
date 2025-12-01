842dc6ce2872bfadf44d06bf59b94894ee319906
alexey.komyakov <alexey.komyakov@flant.com>
2025-12-01 13:22:19 +0300

    add containerd watcher
    Signed-off-by: alexey.komyakov <alexey.komyakov@flant.com>

---
+++ modules/340-monitoring-kubernetes/images/oom-kills-exporter/src/go.mod
@@ -0,0 +1,96 @@
+module oom-kills-exporter
+
+go 1.24.7
+
+toolchain go1.24.10
+
+require (
+	github.com/containerd/containerd v1.7.29
+	github.com/containerd/containerd/api v1.8.0
+	github.com/containerd/typeurl/v2 v2.1.1
+	github.com/golang/glog v1.2.5
+	github.com/prometheus/client_golang v1.23.2
+	github.com/sapcc/go-api-declarations v1.18.0
+	k8s.io/node-problem-detector v1.34.0
+)
+
+require (
+	github.com/AdaLogics/go-fuzz-headers v0.0.0-20230811130428-ced1acdcaa24 // indirect
+	github.com/AdamKorcz/go-118-fuzz-build v0.0.0-20230306123547-8075edf89bb0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/Microsoft/hcsshim v0.11.7 // indirect
+	github.com/acobaugh/osrelease v0.1.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/containerd/cgroups v1.1.0 // indirect
+	github.com/containerd/continuity v0.4.4 // indirect
+	github.com/containerd/errdefs v0.3.0 // indirect
+	github.com/containerd/fifo v1.1.0 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/containerd/platforms v0.2.1 // indirect
+	github.com/containerd/ttrpc v1.2.7 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
+	github.com/ebitengine/purego v0.8.4 // indirect
+	github.com/euank/go-kmsg-parser v2.0.0+incompatible // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/moby/locker v1.0.1 // indirect
+	github.com/moby/sys/mountinfo v0.6.2 // indirect
+	github.com/moby/sys/sequential v0.5.0 // indirect
+	github.com/moby/sys/signal v0.7.0 // indirect
+	github.com/moby/sys/user v0.3.0 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.3-0.20250322232337-35a7c28c31ee // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/opencontainers/runtime-spec v1.1.0 // indirect
+	github.com/opencontainers/selinux v1.11.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.66.1 // indirect
+	github.com/prometheus/procfs v0.17.0 // indirect
+	github.com/shirou/gopsutil/v4 v4.25.8 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/tklauser/go-sysconf v0.3.15 // indirect
+	github.com/tklauser/numcpus v0.10.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
+	go.opentelemetry.io/otel v1.36.0 // indirect
+	go.opentelemetry.io/otel/metric v1.36.0 // indirect
+	go.opentelemetry.io/otel/trace v1.36.0 // indirect
+	go.yaml.in/yaml/v2 v2.4.2 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/genproto v0.0.0-20250603155806-513f23925822 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250728155136-f173205681a0 // indirect
+	google.golang.org/grpc v1.74.2 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.34.1 // indirect
+	k8s.io/apimachinery v0.34.1 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/utils v0.0.0-20250604170112-4c0f3b243397 // indirect
+	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
+	sigs.k8s.io/randfill v1.0.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v6 v6.3.0 // indirect
+)
---
+++ modules/340-monitoring-kubernetes/images/oom-kills-exporter/src/main.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/containerd/containerd"
+	containerdEvents "github.com/containerd/containerd/api/events"
+	"github.com/containerd/containerd/events"
+	"github.com/containerd/containerd/namespaces"
+	"github.com/golang/glog"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sapcc/go-api-declarations/bininfo"
+	"github.com/containerd/typeurl/v2"
+	"k8s.io/node-problem-detector/pkg/systemlogmonitor/logwatchers/kmsg"
+	"k8s.io/node-problem-detector/pkg/systemlogmonitor/logwatchers/types"
+)
+
+var (
+	defaultPattern            = `^oom-kill.+,task_memcg=\/kubepods(?:\.slice)?\/.+\/(?:kubepods-burstable-)?pod(\w+[-_]\w+[-_]\w+[-_]\w+[-_]\w+)(?:\.slice)?\/(?:cri-containerd-)?([a-f0-9]+)`
+	kmesgRE                   = regexp.MustCompile(defaultPattern)
+	kubernetesCounterVec      *prometheus.CounterVec
+	prometheusContainerLabels = map[string]string{
+		"io.kubernetes.container.name": "container_name",
+		"io.kubernetes.pod.namespace":  "namespace",
+		"io.kubernetes.pod.uid":        "pod_uid",
+		"io.kubernetes.pod.name":       "pod_name",
+	}
+	metricsAddr string
+	versionFlag bool
+	newPattern  string
+)
+
+func init() {
+	flag.StringVar(&metricsAddr, "listen-address", ":9102", "The address to listen on for HTTP requests.")
+	flag.StringVar(&newPattern, "regexp-pattern", defaultPattern, "Overwrites the default regexp pattern to match and extract Pod UID and Container ID.")
+	flag.BoolVar(&versionFlag, "version", false, "Print version info")
+}
+
+func main() {
+	flag.Parse()
+
+	if versionFlag {
+		fmt.Printf("Version: %s\n", bininfo.Version())
+		os.Exit(0)
+	}
+
+	if newPattern != "" {
+		kmesgRE = regexp.MustCompile(newPattern)
+	}
+
+	containerdClient, err := containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		glog.Fatal(err)
+	}
+	defer containerdClient.Close()
+
+	var labels []string
+	for _, label := range prometheusContainerLabels {
+		labels = append(labels, strings.ReplaceAll(label, ".", "_"))
+	}
+	kubernetesCounterVec = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "klog_pod_oomkill_total",
+		Help: "Extract metrics for OOMKilled pods from kernel log",
+	}, labels)
+
+	prometheus.MustRegister(kubernetesCounterVec)
+
+	go func() {
+		glog.Info("Starting prometheus metrics")
+		mux := http.NewServeMux()
+		mux.Handle("/metrics", promhttp.Handler())
+		server := &http.Server{
+			Addr:              metricsAddr,
+			ReadHeaderTimeout: 3 * time.Second,
+			Handler:           mux,
+		}
+		glog.Warning(server.ListenAndServe())
+	}()
+
+	ctx := namespaces.WithNamespace(context.Background(), "k8s.io")
+
+	go watchContainerd(ctx, containerdClient)
+
+	kmsgWatcher := kmsg.NewKmsgWatcher(types.WatcherConfig{Plugin: "kmsg"})
+	logCh, err := kmsgWatcher.Watch()
+
+	if err != nil {
+		glog.Fatal("Could not create log watcher")
+	}
+
+	for log := range logCh {
+		podUID, containerID := getContainerIDFromLog(log.Message)
+		if containerID != "" {
+			labels, err := getContainerLabels(containerID, containerdClient)
+			if err != nil || labels == nil {
+				glog.Warningf("Could not get labels for container id %s, pod %s: %v", containerID, podUID, err)
+			} else {
+				prometheusCount(labels)
+			}
+		}
+	}
+}
+
+func watchContainerd(ctx context.Context, cli *containerd.Client) {
+	eventCh, errCh := cli.EventService().Subscribe(
+		ctx,
+		"topic~=^/containers/",
+	)
+
+	for {
+		select {
+		case e := <-eventCh:
+			handleContainerdEvent(ctx, cli, e)
+		case err := <-errCh:
+			glog.Errorf("containerd event error: %v", err)
+			time.Sleep(5 * time.Second)
+		}
+	}
+}
+
+func handleContainerdEvent(ctx context.Context, cli *containerd.Client, e *events.Envelope) {
+	switch e.Topic {
+	case "/containers/create":
+		obj, err := typeurl.UnmarshalAny(e.Event)
+		if err != nil {
+			glog.Warningf("Failed to unmarshal containerd event: %v", err)
+			return
+		}
+
+		ev, ok := obj.(*containerdEvents.ContainerCreate)
+		if !ok {
+			glog.Warning("Unexpected event type")
+			return
+		}
+
+		container, err := cli.ContainerService().Get(ctx, ev.ID)
+		if err != nil {
+			glog.Warningf("Failed to get container %s: %v", ev.ID, err)
+			return
+		}
+
+		prometheusEnsureSeries(container.Labels)
+		glog.V(4).Infof("Registered zero series for container %s", ev.ID)
+	}
+}
+
+func prometheusEnsureSeries(containerLabels map[string]string) {
+	labels := make(map[string]string)
+	for key, label := range prometheusContainerLabels {
+		labels[label] = containerLabels[key]
+	}
+	kubernetesCounterVec.With(labels).Add(0)
+}
+
+func prometheusCount(containerLabels map[string]string) {
+	labels := make(map[string]string)
+	for key, label := range prometheusContainerLabels {
+		labels[label] = containerLabels[key]
+	}
+
+	counter, err := kubernetesCounterVec.GetMetricWith(labels)
+	if err != nil {
+		glog.Warning(err)
+		return
+	}
+
+	counter.Add(1)
+}
+
+func getContainerIDFromLog(log string) (podUID, containerID string) {
+	podUID = ""
+	containerID = ""
+
+	if matches := kmesgRE.FindStringSubmatch(log); matches != nil {
+		podUID = matches[1]
+		containerID = matches[2]
+	}
+
+	return
+}
+
+func getContainerLabels(containerID string, cli *containerd.Client) (map[string]string, error) {
+	ctx := namespaces.WithNamespace(context.Background(), "k8s.io")
+	container, err := cli.ContainerService().Get(ctx, containerID)
+	if err != nil {
+		return nil, err
+	}
+
+	return container.Labels, nil
+}
