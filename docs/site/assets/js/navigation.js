e268dc107cbc8337412ddb3c0a9e326d13f42d95
Polina Polyntseva <polina.polyntseva@flant.com>
2025-12-03 12:53:26 +0300

    [docs] Fix navigation scrolling errors (#16745)
    Signed-off-by: Polina Polyntseva <pvsskk@mail.ru>
    Co-authored-by: Polina Polyntseva <pvsskk@mail.ru>

--- docs/site/assets/js/navigation.js
+++ docs/site/assets/js/navigation.js
@@ -1,22 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navigationContainer = document.querySelector('.navigation__container');
-    let lastScroll = window.scrollY;
-    const range = 10;
 
     if(navigationContainer) {
+        const sidebarAndToc = document.querySelectorAll('.sidebar__wrapper-inner');
+        let lastScroll = window.scrollY;
+        const headerHeight = document.querySelector('.header-container').offsetHeight;
+        const navigationHeight = navigationContainer.offsetHeight;
+        const newTop = headerHeight + navigationHeight;
+        
         window.addEventListener('scroll', function() {
             const scrollWindowTop = window.scrollY;
-            const scrollDelta = scrollWindowTop - lastScroll;
-
-                if(scrollWindowTop > lastScroll) {
-                    navigationContainer.classList.add('hidden');
-                    lastScroll = scrollWindowTop;
-                } else {
-                    if(Math.abs(scrollDelta) > range) {
-                      navigationContainer.classList.remove('hidden'); 
-                      lastScroll = scrollWindowTop; 
-                    }
-                }
+            if(scrollWindowTop > lastScroll) {
+                navigationContainer.classList.add('hidden');
+                lastScroll = scrollWindowTop;
+                sidebarAndToc.forEach(e => {
+                    e.classList.remove('top');
+                    e.style.removeProperty('--scroll-top');
+                })
+            } else {
+                navigationContainer.classList.remove('hidden');
+                lastScroll = scrollWindowTop;
+                sidebarAndToc.forEach(e => {
+                    e.classList.add('top');
+                    e.style.setProperty('--scroll-top', `${newTop}px`);
+                })
+            }
         })
     }
 })
