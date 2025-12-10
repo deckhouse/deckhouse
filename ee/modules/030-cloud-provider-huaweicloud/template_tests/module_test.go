43832db912524e1942a24dee0571e57725c18a73
Alexey Voronov <alexey.voronov@flant.com>
2025-12-10 15:23:41 +0700

    fix huaweicloud template_tests
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- ee/modules/030-cloud-provider-huaweicloud/template_tests/module_test.go
+++ ee/modules/030-cloud-provider-huaweicloud/template_tests/module_test.go
@@ -51,7 +51,86 @@ const globalValues = `
 `
 
 const moduleValuesA = `
-`
+internal:
+  cniSecretData: "base64-encoded-string-or-placeholder"
+  providerClusterConfiguration:
+    apiVersion: deckhouse.io/v1
+    kind: HuaweiCloudClusterConfiguration
+    layout: Standard
+    sshPublicKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCu..."
+    zones:
+      - eu-3a
+    provider:
+      cloud: huaweicloud.example.com
+      region: eu-3
+      accessKey: "YOUR_ACCESS_KEY"
+      secretKey: "YOUR_SECRET_KEY"
+      domainName: "example.com"
+      insecure: false
+    standard:
+      internalNetworkCIDR: 192.168.200.0/24
+      internalNetworkDNSServers:
+        - 8.8.8.8
+        - 8.8.4.4
+      internalNetworkSecurity: true
+      enableEIP: true
+    masterNodeGroup:
+      replicas: 3
+      instanceClass:
+        flavorName: s3.xlarge.2
+        imageName: "debian-11-genericcloud-amd64-20220911-1135"
+        rootDiskSize: 50
+        etcdDiskSizeGb: 10
+      volumeTypeMap:
+        eu-3a: fast-eu-3a
+        eu-3b: fast-eu-3b
+      serverGroup:
+        policy: AntiAffinity
+    nodeGroups:
+      - name: front
+        replicas: 2
+        instanceClass:
+          flavorName: m1.large
+          imageName: "debian-11-genericcloud-amd64-20220911-1135"
+          rootDiskSize: 50
+          mainNetwork: "aaaff8f9-26af-43e3-9c49-c4d083e59c61"
+          additionalNetworks:
+            - "11111111-1111-1111-1111-111111111111"
+        zones:
+          - eu-1a
+          - eu-1b
+        volumeTypeMap:
+          eu-1a: fast-eu-1a
+          eu-1b: fast-eu-1b
+        nodeTemplate:
+          labels:
+            role: frontend
+            environment: production
+          annotations:
+            note: "frontend nodes"
+          taints:
+            - effect: NoSchedule
+              key: front-node
+              value: "true"
+  providerDiscoveryData:
+    apiVersion: deckhouse.io/v1
+    kind: HuaweiCloudDiscoveryData
+    layout: Standard
+    zones:
+      - eu-3a
+    instances:
+      vpcIPv4SubnetId: "00000000-0000-0000-0000-000000000000"
+    volumeTypes:
+      - id: "11111111-1111-1111-1111-111111111111"
+        name: "ssd"
+        isPublic: true
+  storageClasses:
+    - name: cinder-ssd
+      type: ssd
+      allowVolumeExpansion: true
+    - name: cinder-hdd
+      type: hdd
+      allowVolumeExpansion: false`
 
 const tolerationsAnyNodeWithUninitialized = `
 - key: node-role.kubernetes.io/master
@@ -114,19 +193,21 @@ var _ = Describe("Module :: cloud-provider-huaweicloud :: helm template ::", fun
 
 			regSecret := f.KubernetesResource("Secret", "kube-system", "d8-node-manager-cloud-provider")
 			Expect(regSecret.Exists()).To(BeTrue())
-			Expect(regSecret.Field("data.capiClusterName").String()).To(Equal(base64.StdEncoding.EncodeToString([]byte("v1rtual-app"))))
+			Expect(regSecret.Field("data.capiClusterName").String()).To(Equal(base64.StdEncoding.EncodeToString([]byte("huaweicloud"))))
 
 			ccmDeployment := f.KubernetesResource("Deployment", "d8-cloud-provider-huaweicloud", "cloud-controller-manager")
 			Expect(ccmDeployment.Exists()).To(BeTrue())
 			Expect(ccmDeployment.Field("spec.template.spec.containers.0.args").String()).To(MatchYAML(`
 - --leader-elect=true
+- --cluster-name=sandbox
+- --cluster-cidr=10.0.1.0/16
+- --allocate-node-cidrs=true
+- --configure-cloud-routes=true
 - --cloud-config=/etc/cloud/cloud-config
-- --cloud-provider=vmware-cloud-director
-- --allow-untagged-cloud=true
-- --configure-cloud-routes=false
-- --controllers=cloud-node,cloud-node-lifecycle
+- --cloud-provider=huaweicloud
 - --bind-address=127.0.0.1
 - --secure-port=10471
+- --tls-cipher-suites=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,TLS_RSA_WITH_AES_128_CBC_SHA,TLS_RSA_WITH_AES_256_CBC_SHA,TLS_RSA_WITH_AES_128_GCM_SHA256,TLS_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA
 - --v=4`))
 
 			csiControllerDeployment := f.KubernetesResource("Deployment", "d8-cloud-provider-huaweicloud", "csi-controller")
