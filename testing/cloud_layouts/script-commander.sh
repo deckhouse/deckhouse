bf8d8976dbc98e7a363acffcb5f1048d67370055
Kirill Kramorov <kirill.kramorov@flant.com>
2025-12-12 18:04:45 +0700

    feat: change openstack containerd version
    Signed-off-by: Kirill Kramorov <kirill.kramorov@flant.com>
    
    * fix: change e2e runner
    
    Signed-off-by: Kirill Kramorov <kirill.kramorov@flant.com>
    
    ---------
    
    Signed-off-by: Kirill Kramorov <kirill.kramorov@flant.com>
    Co-authored-by: Kirill Kramorov <kirill.kramorov@flant.com>
    (cherry picked from commit f3d4c372b80fa327cdeef6ff2b8f37aaa5af3f5f)
    Signed-off-by: Kirill Kramorov <kirill.kramorov@flant.com>
    
    # Conflicts:
    #	.github/ci_templates/e2e_tests.yml

--- testing/cloud_layouts/script-commander.sh
+++ testing/cloud_layouts/script-commander.sh
@@ -313,7 +313,7 @@ function prepare_environment() {
     ;;
 
   "OpenStack")
-    ssh_user="redos"
+    ssh_user="astra"
     values="{
       \"branch\": \"${DEV_BRANCH}\",
       \"prefix\": \"a${PREFIX}\",
