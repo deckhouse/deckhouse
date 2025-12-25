da5565dab6107096c10d811f3f603112a368d66f
vadimartynov <vadim.martynov@flant.com>
2025-12-25 16:28:53 +0300

    [yc_cloud] Fixed master node lifecycle
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

--- candi/cloud-providers/yandex/terraform-modules/master-node/main.tf
+++ candi/cloud-providers/yandex/terraform-modules/master-node/main.tf
@@ -174,8 +174,7 @@ resource "yandex_compute_instance" "master" {
   lifecycle {
     ignore_changes = [
       metadata,
-      secondary_disk[0], # ignore changes for kubernetes_data device
-      secondary_disk[1], # ignore changes for system_registry_data device
+      secondary_disk
     ]
   }
 
