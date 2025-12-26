09031277d356c7d043686f807a4e447f08692c5d
Vladislav Denisov <vlad144denisov@gmail.com>
2025-12-26 18:28:05 +0300

    +
    +
    
    Signed-off-by: Vladislav Denisov <vlad144denisov@gmail.com>

--- candi/cloud-providers/dvp/layouts/standard/base-infrastructure/main.tf
+++ candi/cloud-providers/dvp/layouts/standard/base-infrastructure/main.tf
@@ -1,40 +1,8 @@
-# Copyright 2025 Flant JSC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 locals {
-  project_name      = var.providerClusterConfiguration.provider.namespace
   project_namespace = var.providerClusterConfiguration.provider.namespace
   policy_name       = "isolated"
-}
-
-data "kubernetes_resources" "project" {
-  api_version    = "deckhouse.io/v1alpha2"
-  kind           = "Project"
-  field_selector = "metadata.name=${local.project_name}"
-}
-
-data "kubernetes_resources" "namespace" {
-  api_version    = "v1"
-  kind           = "Namespace"
-  field_selector = "metadata.name=${local.project_namespace}"
-}
-
-locals {
-  project_exists    = length(data.kubernetes_resources.project.objects) > 0
-  namespace_exists  = length(data.kubernetes_resources.namespace.objects) > 0
-  project_network_policy = local.project_exists ? try(data.kubernetes_resources.project.objects[0].object.spec.parameters.networkPolicy, "") : ""
-  should_manage     = local.project_exists && local.namespace_exists && local.project_network_policy == "Isolated"
+  network_policy    = try(var.providerClusterConfiguration.provider.networkPolicy, "")
+  should_manage     = local.network_policy == "Isolated"
 }
 
 data "kubernetes_resources" "isolated_np" {
