c6cf77d2341f0ab0d30bf312edfcd91aaac764a2
Vadim Timin <vadim.timin@flant.com>
2025-12-29 18:31:04 +0600

    HaveLen(3)-2
    Signed-off-by: Vadim Timin <vadim.timin@flant.com>

--- ee/modules/110-istio/hooks/ee/federation_discovery.go
+++ ee/modules/110-istio/hooks/ee/federation_discovery.go
@@ -232,7 +232,7 @@ func federationDiscovery(_ context.Context, input *go_hook.HookInput, dc depende
 		if privateMetadata.PublicServices != nil {
 			countServices = len(*privateMetadata.PublicServices)
 		}
-		input.Logger.Warn("Federation %s: connection to the cluster %s is successful, there are %d published services", federationInfo.Name, myTrustDomain, countServices)
+		input.Logger.Info("Federation %s: connection to the cluster %s is successful, there are %d published services", slog.String("name", federationInfo.Name), myTrustDomain, countServices)
 	}
 	return nil
 }
