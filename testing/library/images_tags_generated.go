e418188c552be8187dfd8db23ef1567366aaa678
Danila Bobkov <126116299+W1seKappa@users.noreply.github.com>
2025-12-30 13:34:07 +0300

    [descheduler] add vex insert into werf (#17360)
    Signed-off-by: Danila Bobkov <danila.bobkov@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -277,7 +277,8 @@ var DefaultImagesDigests = map[string]interface{}{
 		"webVexArtifact": "imageHash-deckhouseTools-webVexArtifact",
 	},
 	"descheduler": map[string]interface{}{
-		"descheduler": "imageHash-descheduler-descheduler",
+		"descheduler":            "imageHash-descheduler-descheduler",
+		"deschedulerVexArtifact": "imageHash-descheduler-deschedulerVexArtifact",
 	},
 	"documentation": map[string]interface{}{
 		"docsBuilder": "imageHash-documentation-docsBuilder",
