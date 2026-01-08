a8939f5c847a4f045d141fb284a96b9e9907fcc6
Vladislav Denisov <vlad144denisov@gmail.com>
2026-01-08 19:32:15 +0300

    +
    +
    
    Signed-off-by: Vladislav Denisov <vlad144denisov@gmail.com>

--- candi/cloud-providers/dvp/layouts/standard/base-infrastructure/main.tf
+++ candi/cloud-providers/dvp/layouts/standard/base-infrastructure/main.tf
@@ -1,15 +1,17 @@
-locals {
-  project_namespace   = try(var.providerClusterConfiguration.provider.namespace, "")
-  network_policy_mode = try(var.providerClusterConfiguration.provider.networkPolicy, "")
+# Copyright 2026 Flant JSC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-  should_check_project = local.project_namespace != "" && local.network_policy_mode == "Isolated"
-
-  # Берём prefix кластера из clusterConfiguration и используем его
-  # для селектора pod'ов виртуалок (virt-launcher) по метке dvp.deckhouse.io/cluster-prefix
-  cluster_prefix = try(var.clusterConfiguration.cloud.prefix, "")
-}
-
-# 1. Проверяем namespace
 data "kubernetes_resources" "project_namespace" {
   count       = local.should_check_project ? 1 : 0
   api_version = "v1"
@@ -18,208 +20,22 @@ data "kubernetes_resources" "project_namespace" {
   field_selector = "metadata.name=${local.project_namespace}"
 }
 
-locals {
-  ns_objects = local.should_check_project ? try(data.kubernetes_resources.project_namespace[0].objects, []) : []
-  ns_exists  = length(local.ns_objects) > 0
-
-  # Управляем NP только если Namespace существует и prefix известен
-  should_manage_np = local.should_check_project && local.ns_exists && local.cluster_prefix != ""
-}
-
-# 2. Проверяем существование NetworkPolicy isolated-extra
-data "kubernetes_resources" "isolated_extra_np" {
+data "kubernetes_resources" "isolated_cluster_prefix_np" {
   count       = local.should_manage_np ? 1 : 0
   api_version = "networking.k8s.io/v1"
   kind        = "NetworkPolicy"
   namespace   = local.project_namespace
 
-  field_selector = "metadata.name=isolated-extra"
+  field_selector = "metadata.name=isolated-${local.cluster_prefix}"
 }
 
-locals {
-  isolated_extra_objects = local.should_manage_np ? try(data.kubernetes_resources.isolated_extra_np[0].objects, []) : []
-  isolated_extra_exists  = length(local.isolated_extra_objects) > 0
-}
-
-# 3. Правила isolated-extra
-locals {
-  template_ingress = [
-    {
-      from  = [{ ipBlock = { cidr = "0.0.0.0/0" } }]
-      ports = [{ port = 22, protocol = "TCP" }]
-    },
-    {
-      from = [{ ipBlock = { cidr = "0.0.0.0/0" } }]
-      ports = [
-        { port = 80, protocol = "TCP" },
-        { port = 443, protocol = "TCP" },
-        { port = 30000, endPort = 32767, protocol = "TCP" }
-      ]
-    },
-    {
-      from = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = local.project_namespace }
-        }
-      }]
-    },
-    {
-      from = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-virtualization" }
-        }
-      }]
-    },
-    {
-      from = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-monitoring" }
-        }
-        podSelector = {
-          matchLabels = { app = "prometheus" }
-        }
-      }]
-    },
-    {
-      from = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-ingress-nginx" }
-        }
-        podSelector = {
-          matchLabels = { app = "controller" }
-        }
-      }]
-    },
-    {
-      from = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-commander" }
-        }
-      }]
-    },
-    {
-      from = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "bastion" }
-        }
-      }]
-    },
-    {
-      from = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-openvpn" }
-        }
-      }]
-    },
-    {
-      from = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-metallb" }
-        }
-      }]
-    },
-
-    # ДОПОЛНИТЕЛЬНОЕ ПРАВИЛО ДЛЯ ПРОВЕРКИ МОДИФИКАЦИИ:
-    {
-      from  = [{ ipBlock = { cidr = "0.0.0.0/0" } }]
-      ports = [{ port = 9100, protocol = "TCP" }]
-    }
-  ]
-
-  template_egress = [
-    { to = [{ ipBlock = { cidr = "82.202.254.194/32" } }] },
-    { to = [{ ipBlock = { cidr = "0.0.0.0/0" } }] },
-    {
-      to = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = local.project_namespace }
-        }
-      }]
-    },
-    {
-      to = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-virtualization" }
-        }
-      }]
-    },
-    {
-      to = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-ingress-nginx" }
-        }
-        podSelector = {
-          matchLabels = { app = "controller" }
-        }
-      }]
-    },
-    {
-      ports = [{ port = 53, protocol = "UDP" }]
-      to = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "kube-system" }
-        }
-      }]
-    },
-    {
-      to = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "bastion" }
-        }
-      }]
-    },
-    {
-      to = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-openvpn" }
-        }
-      }]
-    },
-    {
-      to = [{
-        namespaceSelector = {
-          matchLabels = { "kubernetes.io/metadata.name" = "d8-metallb" }
-        }
-      }]
-    },
-    { to = [{ ipBlock = { cidr = "1.1.1.1/32" } }] }
-  ]
-
-  desired_policy_fingerprint = sha256(jsonencode({
-    podSelector = {
-      matchLabels = {
-        "dvp.deckhouse.io/cluster-prefix" = local.cluster_prefix
-      }
-    }
-    ingress = local.template_ingress
-    egress  = local.template_egress
-  }))
-
-  # label value must be <= 63 chars
-  desired_policy_hash_short = substr(local.desired_policy_fingerprint, 0, 16)
-}
-
-# 4. Targets
-locals {
-  targets = local.should_manage_np ? {
-    isolated_extra = {
-      namespace = local.project_namespace
-      name      = "isolated-extra"
-    }
-  } : {}
-
-  import_targets = local.isolated_extra_exists ? local.targets : {}
-}
-
-# 5. Import если уже существует (для повторных запусков без backend state)
 import {
   for_each = local.import_targets
-  to       = kubernetes_manifest.isolated_extra_network_policy[each.key]
+  to       = kubernetes_manifest.isolated_cluster_prefix_network_policy[each.key]
   id       = "apiVersion=networking.k8s.io/v1,kind=NetworkPolicy,namespace=${each.value.namespace},name=${each.value.name}"
 }
 
-# 6. Создание/обновление isolated-extra
-resource "kubernetes_manifest" "isolated_extra_network_policy" {
+resource "kubernetes_manifest" "isolated_cluster_prefix_network_policy" {
   for_each = local.targets
 
   field_manager {
@@ -234,13 +50,12 @@ resource "kubernetes_manifest" "isolated_extra_network_policy" {
       name      = each.value.name
       namespace = each.value.namespace
       labels = {
-        "d8.tf/managed" = "isolated-extra"
+        "d8.tf/managed" = "isolated_cluster_prefix"
         "d8.tf/hash"    = local.desired_policy_hash_short
       }
     }
 
     spec = {
-      # Применяем политику только к pod'ам VM (virt-launcher) конкретного cluster-prefix
       podSelector = {
         matchLabels = {
           "dvp.deckhouse.io/cluster-prefix" = local.cluster_prefix
@@ -256,19 +71,19 @@ resource "kubernetes_manifest" "isolated_extra_network_policy" {
 
 output "dvp_np_status" {
   value = {
-    project_namespace        = local.project_namespace
-    network_policy_mode      = local.network_policy_mode
-    should_check_project     = local.should_check_project
-    namespace_exists         = local.ns_exists
-    cluster_prefix           = local.cluster_prefix
-    should_manage_np         = local.should_manage_np
+    project_namespace     = local.project_namespace
+    network_policy_mode   = local.network_policy_mode
+    should_check_project  = local.should_check_project
+    namespace_exists      = local.ns_exists
+    cluster_prefix        = local.cluster_prefix
+    should_manage_np      = local.should_manage_np
 
-    isolated_extra_exists    = local.isolated_extra_exists
-    will_import              = length(local.import_targets) > 0
-    will_create_or_update    = length(local.targets) > 0
+    isolated_np_exists    = local.isolated_np_exists
+    will_import           = length(local.import_targets) > 0
+    will_create_or_update = length(local.targets) > 0
 
-    policy_hash_short        = local.desired_policy_hash_short
-    ingress_rules_count      = length(local.template_ingress)
-    egress_rules_count       = length(local.template_egress)
+    policy_hash_short     = local.desired_policy_hash_short
+    ingress_rules_count   = length(local.template_ingress)
+    egress_rules_count    = length(local.template_egress)
   }
 }
--- candi/cloud-providers/dvp/layouts/standard/base-infrastructure/variables.tf
+++ candi/cloud-providers/dvp/layouts/standard/base-infrastructure/variables.tf
@@ -12,12 +12,196 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+variable "providerClusterConfiguration" {
+  type = any
+}
 
 variable "clusterConfiguration" {
   type = any
 }
 
-variable "providerClusterConfiguration" {
-  type = any
+locals {
+  project_namespace   = try(var.providerClusterConfiguration.provider.namespace, "")
+  network_policy_mode = try(var.providerClusterConfiguration.provider.networkPolicy, "")
+
+  should_check_project = local.project_namespace != "" && local.network_policy_mode == "Isolated"
+
+  cluster_prefix = try(var.clusterConfiguration.cloud.prefix, "")
 }
 
+locals {
+  ns_objects = local.should_check_project ? try(data.kubernetes_resources.project_namespace[0].objects, []) : []
+  ns_exists  = length(local.ns_objects) > 0
+
+  should_manage_np = local.should_check_project && local.ns_exists && local.cluster_prefix != ""
+}
+
+locals {
+  isolated_np_objects = local.should_manage_np ? try(data.kubernetes_resources.isolated_cluster_prefix_np[0].objects, []) : []
+  isolated_np_exists  = length(local.isolated_np_objects) > 0
+}
+
+locals {
+  template_ingress = [
+    {
+      from  = [{ ipBlock = { cidr = "0.0.0.0/0" } }]
+      ports = [{ port = 22, protocol = "TCP" }]
+    },
+    {
+      from = [{ ipBlock = { cidr = "0.0.0.0/0" } }]
+      ports = [
+        { port = 80, protocol = "TCP" },
+        { port = 443, protocol = "TCP" },
+        { port = 30000, endPort = 32767, protocol = "TCP" }
+      ]
+    },
+    {
+      from = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = local.project_namespace }
+        }
+      }]
+    },
+    {
+      from = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-virtualization" }
+        }
+      }]
+    },
+    {
+      from = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-monitoring" }
+        }
+        podSelector = {
+          matchLabels = { app = "prometheus" }
+        }
+      }]
+    },
+    {
+      from = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-ingress-nginx" }
+        }
+        podSelector = {
+          matchLabels = { app = "controller" }
+        }
+      }]
+    },
+    {
+      from = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-commander" }
+        }
+      }]
+    },
+    {
+      from = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "bastion" }
+        }
+      }]
+    },
+    {
+      from = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-openvpn" }
+        }
+      }]
+    },
+    {
+      from = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-metallb" }
+        }
+      }]
+    },
+    {
+      from  = [{ ipBlock = { cidr = "0.0.0.0/0" } }]
+      ports = [{ port = 9100, protocol = "TCP" }]
+    }
+  ]
+
+  template_egress = [
+    { to = [{ ipBlock = { cidr = "82.202.254.194/32" } }] },
+    { to = [{ ipBlock = { cidr = "0.0.0.0/0" } }] },
+    {
+      to = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = local.project_namespace }
+        }
+      }]
+    },
+    {
+      to = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-virtualization" }
+        }
+      }]
+    },
+    {
+      to = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-ingress-nginx" }
+        }
+        podSelector = {
+          matchLabels = { app = "controller" }
+        }
+      }]
+    },
+    {
+      ports = [{ port = 53, protocol = "UDP" }]
+      to = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "kube-system" }
+        }
+      }]
+    },
+    {
+      to = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "bastion" }
+        }
+      }]
+    },
+    {
+      to = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-openvpn" }
+        }
+      }]
+    },
+    {
+      to = [{
+        namespaceSelector = {
+          matchLabels = { "kubernetes.io/metadata.name" = "d8-metallb" }
+        }
+      }]
+    },
+    { to = [{ ipBlock = { cidr = "1.1.1.1/32" } }] }
+  ]
+
+  desired_policy_fingerprint = sha256(jsonencode({
+    podSelector = {
+      matchLabels = {
+        "dvp.deckhouse.io/cluster-prefix" = local.cluster_prefix
+      }
+    }
+    ingress = local.template_ingress
+    egress  = local.template_egress
+  }))
+
+  desired_policy_hash_short = substr(local.desired_policy_fingerprint, 0, 16)
+}
+
+locals {
+  targets = local.should_manage_np ? {
+    isolated_cluster_prefix = {
+      namespace = local.project_namespace
+      name      = "isolated-${local.cluster_prefix}"
+    }
+  } : {}
+
+  import_targets = local.isolated_np_exists ? local.targets : {}
+}
