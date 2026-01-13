b8656d8fca72208935a34768199988e7ca5bad7c
Danila Bobkov <danila.bobkov@flant.com>
2026-01-13 15:29:41 +0300

    add reflection to server
    Signed-off-by: Danila Bobkov <danila.bobkov@flant.com>

--- modules/040-node-manager/images/fencing-agent/src/internal/adapters/api/grpc/run.go
+++ modules/040-node-manager/images/fencing-agent/src/internal/adapters/api/grpc/run.go
@@ -4,21 +4,23 @@ import (
 	pb "fencing-agent/pkg/api/v1"
 	"fmt"
 	"net"
-	"os"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func Run(socketPath string, grpcSrv *Server) error {
-	if err := os.RemoveAll(socketPath); err != nil {
-		return fmt.Errorf("failed to remove socket: %w", err)
-	}
+	//if err := os.RemoveAll(socketPath); err != nil {
+	//	return fmt.Errorf("failed to remove socket: %w", err)
+	//}
 	lis, err := net.Listen("unix", socketPath)
 	if err != nil {
 		return fmt.Errorf("failed to listen: %w", err)
 	}
 
 	grpcServer := grpc.NewServer()
+
+	reflection.Register(grpcServer)
 
 	pb.RegisterFencingServer(grpcServer, grpcSrv)
 
