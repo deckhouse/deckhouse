cbf546cfe17f7941d0f0f0a2d16f35a2b080cfb2
Vladislav Denisov <vlad144denisov@gmail.com>
2026-01-15 13:29:13 +0300

    +
    +
    
    Signed-off-by: Vladislav Denisov <vlad144denisov@gmail.com>

--- candi/cloud-providers/dvp/layouts/standard/base-infrastructure/main.tf
+++ candi/cloud-providers/dvp/layouts/standard/base-infrastructure/main.tf
@@ -12,29 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-data "kubernetes_resources" "project_namespace" {
-  count       = local.should_check_project ? 1 : 0
-  api_version = "v1"
-  kind        = "Namespace"
-
-  field_selector = "metadata.name=${local.project_namespace}"
-}
-
-data "kubernetes_resources" "isolated_cluster_prefix_np" {
-  count       = local.should_manage_np ? 1 : 0
-  api_version = "networking.k8s.io/v1"
-  kind        = "NetworkPolicy"
-  namespace   = local.project_namespace
-
-  field_selector = "metadata.name=isolated-${local.cluster_prefix}"
-}
-
-import {
-  for_each = local.import_targets
-  to       = kubernetes_manifest.isolated_cluster_prefix_network_policy[each.key]
-  id       = "apiVersion=networking.k8s.io/v1,kind=NetworkPolicy,namespace=${each.value.namespace},name=${each.value.name}"
-}
-
 resource "kubernetes_manifest" "isolated_cluster_prefix_network_policy" {
   for_each = local.targets
 
@@ -42,48 +19,5 @@ resource "kubernetes_manifest" "isolated_cluster_prefix_network_policy" {
     force_conflicts = true
   }
 
-  manifest = {
-    apiVersion = "networking.k8s.io/v1"
-    kind       = "NetworkPolicy"
-
-    metadata = {
-      name      = each.value.name
-      namespace = each.value.namespace
-      labels = {
-        "d8.tf/managed" = "isolated_cluster_prefix"
-        "d8.tf/hash"    = local.desired_policy_hash_short
-      }
-    }
-
-    spec = {
-      podSelector = {
-        matchLabels = {
-          "dvp.deckhouse.io/cluster-prefix" = local.cluster_prefix
-        }
-      }
-
-      policyTypes = ["Ingress", "Egress"]
-      ingress     = local.template_ingress
-      egress      = local.template_egress
-    }
-  }
-}
-
-output "dvp_np_status" {
-  value = {
-    project_namespace     = local.project_namespace
-    network_policy_mode   = local.network_policy_mode
-    should_check_project  = local.should_check_project
-    namespace_exists      = local.ns_exists
-    cluster_prefix        = local.cluster_prefix
-    should_manage_np      = local.should_manage_np
-
-    isolated_np_exists    = local.isolated_np_exists
-    will_import           = length(local.import_targets) > 0
-    will_create_or_update = length(local.targets) > 0
-
-    policy_hash_short     = local.desired_policy_hash_short
-    ingress_rules_count   = length(local.template_ingress)
-    egress_rules_count    = length(local.template_egress)
-  }
+  manifest = each.value
 }
