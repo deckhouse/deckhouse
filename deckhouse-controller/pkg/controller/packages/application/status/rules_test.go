cc5149aad7f56c27e1a1f9a74e29cee9f5604003
Stepan Paksashvili <stepan.paksashvili@flant.com>
2026-01-17 01:30:02 +0300

    [deckhouse] condition mapper
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- deckhouse-controller/pkg/controller/packages/application/status/rules_test.go
+++ deckhouse-controller/pkg/controller/packages/application/status/rules_test.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package status
 
 import (
@@ -161,12 +175,13 @@ func TestInstalledRule(t *testing.T) {
 			},
 		},
 		{
-			name: "sticky - not updated when already true externally",
+			name: "sticky - not in result when already true externally",
 			opts: []mappingOption{
 				withExternalCondition(ConditionInstalled, metav1.ConditionTrue, "PreviouslyInstalled"),
 				withInternalCondition(string(intstatus.ConditionReadyInCluster), metav1.ConditionFalse, "ClusterNotReady"),
 			},
 			expected: map[string]*expectedCondition{
+				// Sticky rule skips evaluation - condition preserved in external state, not in result
 				ConditionInstalled: nil,
 			},
 		},
