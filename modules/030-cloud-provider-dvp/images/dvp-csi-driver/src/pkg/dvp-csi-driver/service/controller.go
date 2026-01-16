44174271258cfe8c6e785c9ba05183f02d4babc1
Alexey Voronov <alexey.voronov@flant.com>
2026-01-16 12:14:29 +0700

    revert previous
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- modules/030-cloud-provider-dvp/images/dvp-csi-driver/src/pkg/dvp-csi-driver/service/controller.go
+++ modules/030-cloud-provider-dvp/images/dvp-csi-driver/src/pkg/dvp-csi-driver/service/controller.go
@@ -504,6 +504,8 @@ func (c *ControllerService) CreateSnapshot(ctx context.Context, request *csi.Cre
 				return nil, status.Error(codes.Internal, msg.Error())
 			}
 
+			snapshotCreationTimestamp := timestamppb.New(virtualDiskSnapshot.CreationTimestamp.Time)
+
 			// we have to return response even if snapshot is not ready yet
 			// to let snapshotter delete it if it get Failed phase later
 			return &csi.CreateSnapshotResponse{
@@ -511,6 +513,7 @@ func (c *ControllerService) CreateSnapshot(ctx context.Context, request *csi.Cre
 					SnapshotId:     request.Name,
 					SourceVolumeId: request.SourceVolumeId,
 					ReadyToUse:     false,
+					CreationTime:   snapshotCreationTimestamp,
 				},
 			}, nil
 		} else {
@@ -541,14 +544,11 @@ func (c *ControllerService) CreateSnapshot(ctx context.Context, request *csi.Cre
 		return nil, status.Error(codes.Internal, msg.Error())
 	}
 
-	snapshotCreationTimestamp := timestamppb.New(volumeSnapshot.Status.CreationTime.Time)
-
 	response := &csi.CreateSnapshotResponse{
 		Snapshot: &csi.Snapshot{
 			SnapshotId:     request.Name,
 			SourceVolumeId: request.SourceVolumeId,
 			ReadyToUse:     true,
-			CreationTime:   snapshotCreationTimestamp,
 			SizeBytes:      volumeSnapshot.Status.RestoreSize.Value(),
 		},
 	}
