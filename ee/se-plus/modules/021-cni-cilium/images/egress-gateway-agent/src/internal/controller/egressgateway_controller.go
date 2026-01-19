016eaf650c33ba2310481c1f9ca0716e181f92dc
TDen <55845170+tarabrind@users.noreply.github.com>
2026-01-19 19:53:11 +0400

    [cni-cilium] Fix egress-gateway-agent controller logic for deleted resources and disable dev logging (#17378)
    Signed-off-by: Denis Tarabrin <denis.tarabrin@flant.com>

--- ee/se-plus/modules/021-cni-cilium/images/egress-gateway-agent/src/internal/controller/egressgateway_controller.go
+++ ee/se-plus/modules/021-cni-cilium/images/egress-gateway-agent/src/internal/controller/egressgateway_controller.go
@@ -46,7 +46,9 @@ func (r *EgressGatewayInstanceReconciler) Reconcile(ctx context.Context, req ctr
 	// Get resource
 	var egressGatewayInstance eeInternalCrd.SDNInternalEgressGatewayInstance
 	if err := r.Get(ctx, req.NamespacedName, &egressGatewayInstance); err != nil {
-		logger.Error(err, "unable to fetch egress gateway instance", "name", req.NamespacedName)
+		if client.IgnoreNotFound(err) != nil {
+			logger.Error(err, "unable to fetch egress gateway instance", "name", req.NamespacedName)
+		}
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
@@ -58,6 +60,7 @@ func (r *EgressGatewayInstanceReconciler) Reconcile(ctx context.Context, req ctr
 			logger.Error(err, "unable to cleanup egress gateway instance", "name", egressGatewayInstance.Name)
 			return ctrl.Result{}, err
 		}
+		return ctrl.Result{}, nil
 	}
 
 	desiredVirtualIPsToAnnounce := make(map[string][]string)
