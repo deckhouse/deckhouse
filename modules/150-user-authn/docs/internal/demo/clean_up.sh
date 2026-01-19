1a624104012e486222299f0654d62fa45df2d1cd
Ivan Zvyagintsev <ivan.zvyagintsev@flant.com>
2026-01-19 15:25:02 +0300

    [user-authn] improve Dex LDAP Kerberos logging and user-facing errors
    Signed-off-by: Ivan Zvyagintsev <ivan.zvyagintsev@flant.com>

--- modules/150-user-authn/docs/internal/demo/clean_up.sh
+++ modules/150-user-authn/docs/internal/demo/clean_up.sh
@@ -14,18 +14,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-kubectl delete dexprovider openldap-demo --ignore-not-found
-kubectl delete dexprovider corp-ldap --ignore-not-found
-kubectl delete user openldap-demo --ignore-not-found
-kubectl delete ns openldap-demo --ignore-not-found
+d8 k delete dexprovider openldap-demo --ignore-not-found
+d8 k delete dexprovider corp-ldap --ignore-not-found
+d8 k delete user openldap-demo --ignore-not-found
+d8 k delete ns openldap-demo --ignore-not-found
 
 # Kerberos demo resources in d8-user-authn
-kubectl -n d8-user-authn delete pod ldap-tools --ignore-not-found
-kubectl -n d8-user-authn delete deploy kdc --ignore-not-found
-kubectl -n d8-user-authn delete svc kdc --ignore-not-found
-kubectl -n d8-user-authn delete configmap kdc-config --ignore-not-found
-kubectl -n d8-user-authn delete secret dex-kerberos-test --ignore-not-found
-kubectl -n d8-user-authn delete deploy openldap --ignore-not-found
-kubectl -n d8-user-authn delete svc openldap --ignore-not-found
-kubectl -n d8-user-authn delete oauth2client spnego-test --ignore-not-found
-kubectl delete clusterauthorizationrule john-superadmin --ignore-not-found
+d8 k -n d8-user-authn delete pod ldap-tools --ignore-not-found
+d8 k -n d8-user-authn delete deploy kdc --ignore-not-found
+d8 k -n d8-user-authn delete svc kdc --ignore-not-found
+d8 k -n d8-user-authn delete configmap kdc-config --ignore-not-found
+d8 k -n d8-user-authn delete configmap ldap-bootstrap --ignore-not-found
+d8 k -n d8-user-authn delete secret dex-kerberos-test --ignore-not-found
+d8 k -n d8-user-authn delete deploy openldap --ignore-not-found
+d8 k -n d8-user-authn delete svc openldap --ignore-not-found
+d8 k -n d8-user-authn delete oauth2client spnego-test --ignore-not-found
+d8 k delete clusterauthorizationrule john-superadmin --ignore-not-found
