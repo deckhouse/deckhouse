0a34c75b3bafff78fae08c9bd6253bef16f2477f
Alexey Komyakov <alexey.komyakov@flant.com>
2026-01-19 19:33:19 +0300

    fix comments
    Signed-off-by: Alexey Komyakov <alexey.komyakov@flant.com>

--- modules/340-monitoring-kubernetes/images/oom-kills-exporter/src/main.go
+++ modules/340-monitoring-kubernetes/images/oom-kills-exporter/src/main.go
@@ -18,8 +18,10 @@ import (
 	"context"
 	"flag"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -35,37 +37,48 @@ import (
 	"k8s.io/node-problem-detector/pkg/systemlogmonitor/logwatchers/types"
 )
 
-var (
-	isReady       atomic.Bool
-	containerdOK  atomic.Bool
-	lastEventHB   atomic.Int64
-	lastKmsgHB    atomic.Int64
-)
+const defaultPattern = `^oom-kill.+,task_memcg=\/kubepods(?:\.slice)?\/.+\/(?:kubepods-burstable-)?pod(\w+[-_]\w+[-_]\w+[-_]\w+[-_]\w+)(?:\.slice)?\/(?:cri-containerd-)?([a-f0-9]+)`
 
-var (
-	defaultPattern            = `^oom-kill.+,task_memcg=\/kubepods(?:\.slice)?\/.+\/(?:kubepods-burstable-)?pod(\w+[-_]\w+[-_]\w+[-_]\w+[-_]\w+)(?:\.slice)?\/(?:cri-containerd-)?([a-f0-9]+)`
-	kmesgRE                   = regexp.MustCompile(defaultPattern)
-	kubernetesCounterVec      *prometheus.CounterVec
-	prometheusContainerLabels = map[string]string{
-		"io.kubernetes.container.name": "container_name",
-		"io.kubernetes.pod.namespace":  "namespace",
-		"io.kubernetes.pod.uid":        "pod_uid",
-		"io.kubernetes.pod.name":       "pod_name",
-	}
-	metricsAddr string
-	newPattern  string
-)
+type app struct {
+	isReady      atomic.Bool
+	containerdOK atomic.Bool
+	lastEventHB  atomic.Int64
+	lastKmsgHB   atomic.Int64
 
-func init() {
-	flag.StringVar(&metricsAddr, "listen-address", "127.0.0.1:4205", "The address to listen on for HTTP requests.")
-	flag.StringVar(&newPattern, "regexp-pattern", defaultPattern, "Overwrites the default regexp pattern to match and extract Pod UID and Container ID.")
+	kmesgRE              *regexp.Regexp
+	kubernetesCounterVec *prometheus.CounterVec
+	containerLabels      map[string]string
+	nodeName             string
+
+	labelsMu            sync.RWMutex
+	containerLabelsByID map[string]map[string]string
 }
 
 func main() {
 	flag.Parse()
 
+	var metricsAddr string
+	var newPattern string
+	flag.StringVar(&metricsAddr, "listen-address", "127.0.0.1:4205", "The address to listen on for HTTP requests.")
+	flag.StringVar(&newPattern, "regexp-pattern", defaultPattern, "Overwrites the default regexp pattern to match and extract Pod UID and Container ID.")
+
+	a := &app{
+		kmesgRE: regexp.MustCompile(defaultPattern),
+		containerLabels: map[string]string{
+			"io.kubernetes.container.name": "container_name",
+			"io.kubernetes.pod.namespace":  "namespace",
+			"io.kubernetes.pod.uid":        "pod_uid",
+			"io.kubernetes.pod.name":       "pod_name",
+		},
+	}
+
+	a.nodeName = os.Getenv("NODE_NAME")
+	if a.nodeName == "" {
+		a.nodeName = "unknown"
+	}
+
 	if newPattern != "" {
-		kmesgRE = regexp.MustCompile(newPattern)
+		a.kmesgRE = regexp.MustCompile(newPattern)
 	}
 
 	containerdClient, err := containerd.New("/run/containerd/containerd.sock")
@@ -75,56 +88,24 @@ func main() {
 	defer containerdClient.Close()
 
 	var labels []string
-	for _, label := range prometheusContainerLabels {
+	for _, label := range a.containerLabels {
 		labels = append(labels, strings.ReplaceAll(label, ".", "_"))
 	}
-	kubernetesCounterVec = prometheus.NewCounterVec(prometheus.CounterOpts{
+	labels = append(labels, "node_name")
+	a.kubernetesCounterVec = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "klog_pod_oomkill",
 		Help: "Extract metrics for OOMKilled pods from kernel log",
 	}, labels)
 
-	prometheus.MustRegister(kubernetesCounterVec)
+	prometheus.MustRegister(a.kubernetesCounterVec)
 
-	go func() {
-		glog.Info("Starting prometheus metrics")
-
-		mux := http.NewServeMux()
-
-		mux.Handle("/metrics", promhttp.Handler())
-
-		mux.HandleFunc("/ready", func(w http.ResponseWriter, _ *http.Request) {
-			if !isReady.Load() {
-				http.Error(w, "not ready", http.StatusServiceUnavailable)
-				return
-			}
-			w.WriteHeader(http.StatusOK)
-			_, _ = w.Write([]byte("ready"))
-		})
-
-		mux.HandleFunc("/live", func(w http.ResponseWriter, _ *http.Request) {
-			if !containerdOK.Load() {
-				http.Error(w, "containerd not reachable", http.StatusServiceUnavailable)
-				return
-			}
-
-			w.WriteHeader(http.StatusOK)
-			_, _ = w.Write([]byte("alive"))
-		})
-
-		server := &http.Server{
-			Addr:              metricsAddr,
-			ReadHeaderTimeout: 3 * time.Second,
-			Handler:           mux,
-		}
-
-		glog.Warning(server.ListenAndServe())
-	}()
+	go a.startMetricsServer(metricsAddr)
 
 	ctx := namespaces.WithNamespace(context.Background(), "k8s.io")
 
-	initialResync(ctx, containerdClient)
+	a.initialResync(ctx, containerdClient)
 
-	go watchContainerd(ctx, containerdClient)
+	go a.watchContainerd(ctx, containerdClient)
 
 	kmsgWatcher := kmsg.NewKmsgWatcher(types.WatcherConfig{Plugin: "kmsg"})
 	logCh, err := kmsgWatcher.Watch()
@@ -133,7 +114,7 @@ func main() {
 		ticker := time.NewTicker(30 * time.Second)
 		defer ticker.Stop()
 		for range ticker.C {
-			lastKmsgHB.Store(time.Now().Unix())
+			a.lastKmsgHB.Store(time.Now().Unix())
 		}
 	}()
 
@@ -141,7 +122,7 @@ func main() {
 		ticker := time.NewTicker(30 * time.Second)
 		defer ticker.Stop()
 		for range ticker.C {
-			lastEventHB.Store(time.Now().Unix())
+			a.lastEventHB.Store(time.Now().Unix())
 		}
 	}()
 
@@ -150,19 +131,20 @@ func main() {
 	}
 
 	for log := range logCh {
-		podUID, containerID := getContainerIDFromLog(log.Message)
+		podUID, containerID := a.getContainerIDFromLog(log.Message)
 		if containerID != "" {
 			labels, err := getContainerLabels(containerID, containerdClient)
 			if err != nil || labels == nil {
 				glog.Warningf("Could not get labels for container id %s, pod %s: %v", containerID, podUID, err)
 			} else {
-				prometheusCount(labels)
+				a.trackContainerLabels(containerID, labels)
+				a.prometheusCount(labels)
 			}
 		}
 	}
 }
 
-func watchContainerd(ctx context.Context, cli *containerd.Client) {
+func (a *app) watchContainerd(ctx context.Context, cli *containerd.Client) {
 	eventCh, errCh := cli.EventService().Subscribe(
 		ctx,
 		"topic~=^/containers/",
@@ -171,7 +153,7 @@ func watchContainerd(ctx context.Context, cli *containerd.Client) {
 	for {
 		select {
 		case e := <-eventCh:
-			handleContainerdEvent(ctx, cli, e)
+			a.handleContainerdEvent(ctx, cli, e)
 		case err := <-errCh:
 			glog.Errorf("containerd event error: %v", err)
 			time.Sleep(5 * time.Second)
@@ -179,8 +161,43 @@ func watchContainerd(ctx context.Context, cli *containerd.Client) {
 	}
 }
 
-func handleContainerdEvent(ctx context.Context, cli *containerd.Client, e *events.Envelope) {
-	containerdOK.Store(true)
+func (a *app) startMetricsServer(metricsAddr string) {
+	glog.Info("Starting prometheus metrics")
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/metrics", promhttp.Handler())
+
+	mux.HandleFunc("/ready", func(w http.ResponseWriter, _ *http.Request) {
+		if !a.isReady.Load() {
+			http.Error(w, "not ready", http.StatusServiceUnavailable)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ready"))
+	})
+
+	mux.HandleFunc("/live", func(w http.ResponseWriter, _ *http.Request) {
+		if !a.containerdOK.Load() {
+			http.Error(w, "containerd not reachable", http.StatusServiceUnavailable)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("alive"))
+	})
+
+	server := &http.Server{
+		Addr:              metricsAddr,
+		ReadHeaderTimeout: 3 * time.Second,
+		Handler:           mux,
+	}
+
+	glog.Warning(server.ListenAndServe())
+}
+
+func (a *app) handleContainerdEvent(ctx context.Context, cli *containerd.Client, e *events.Envelope) {
+	a.containerdOK.Store(true)
 
 	switch e.Topic {
 	case "/containers/create":
@@ -202,26 +219,38 @@ func handleContainerdEvent(ctx context.Context, cli *containerd.Client, e *event
 			return
 		}
 
-		prometheusEnsureSeries(container.Labels)
+		a.trackContainerLabels(ev.ID, container.Labels)
+		a.prometheusEnsureSeries(container.Labels)
 		glog.V(4).Infof("Registered zero series for container %s", ev.ID)
+	case "/containers/delete":
+		obj, err := typeurl.UnmarshalAny(e.Event)
+		if err != nil {
+			glog.Warningf("Failed to unmarshal containerd event: %v", err)
+			return
+		}
+
+		ev, ok := obj.(*containerdEvents.ContainerDelete)
+		if !ok {
+			glog.Warning("Unexpected event type")
+			return
+		}
+
+		if labels, ok := a.getTrackedLabels(ev.ID); ok {
+			a.kubernetesCounterVec.Delete(a.buildPrometheusLabels(labels))
+			a.deleteTrackedLabels(ev.ID)
+			glog.V(4).Infof("Removed series for container %s", ev.ID)
+		}
 	}
 }
 
-func prometheusEnsureSeries(containerLabels map[string]string) {
-	labels := make(map[string]string)
-	for key, label := range prometheusContainerLabels {
-		labels[label] = containerLabels[key]
-	}
-	kubernetesCounterVec.With(labels).Add(0)
+func (a *app) prometheusEnsureSeries(containerLabels map[string]string) {
+	a.kubernetesCounterVec.With(a.buildPrometheusLabels(containerLabels)).Add(0)
 }
 
-func prometheusCount(containerLabels map[string]string) {
-	labels := make(map[string]string)
-	for key, label := range prometheusContainerLabels {
-		labels[label] = containerLabels[key]
-	}
+func (a *app) prometheusCount(containerLabels map[string]string) {
+	labels := a.buildPrometheusLabels(containerLabels)
 
-	counter, err := kubernetesCounterVec.GetMetricWith(labels)
+	counter, err := a.kubernetesCounterVec.GetMetricWith(labels)
 	if err != nil {
 		glog.Warning(err)
 		return
@@ -230,16 +259,47 @@ func prometheusCount(containerLabels map[string]string) {
 	counter.Add(1)
 }
 
-func getContainerIDFromLog(log string) (podUID, containerID string) {
+func (a *app) getContainerIDFromLog(log string) (podUID, containerID string) {
 	podUID = ""
 	containerID = ""
 
-	if matches := kmesgRE.FindStringSubmatch(log); matches != nil {
+	if matches := a.kmesgRE.FindStringSubmatch(log); matches != nil {
 		podUID = matches[1]
 		containerID = matches[2]
 	}
 
 	return
+}
+
+func (a *app) buildPrometheusLabels(containerLabels map[string]string) prometheus.Labels {
+	labels := make(prometheus.Labels)
+	for key, label := range a.containerLabels {
+		labels[label] = containerLabels[key]
+	}
+	labels["node_name"] = a.nodeName
+	return labels
+}
+
+func (a *app) trackContainerLabels(containerID string, labels map[string]string) {
+	a.labelsMu.Lock()
+	defer a.labelsMu.Unlock()
+	if a.containerLabelsByID == nil {
+		a.containerLabelsByID = make(map[string]map[string]string)
+	}
+	a.containerLabelsByID[containerID] = copyLabels(labels)
+}
+
+func (a *app) getTrackedLabels(containerID string) (map[string]string, bool) {
+	a.labelsMu.RLock()
+	defer a.labelsMu.RUnlock()
+	labels, ok := a.containerLabelsByID[containerID]
+	return labels, ok
+}
+
+func (a *app) deleteTrackedLabels(containerID string) {
+	a.labelsMu.Lock()
+	defer a.labelsMu.Unlock()
+	delete(a.containerLabelsByID, containerID)
 }
 
 func getContainerLabels(containerID string, cli *containerd.Client) (map[string]string, error) {
@@ -252,7 +312,7 @@ func getContainerLabels(containerID string, cli *containerd.Client) (map[string]
 	return container.Labels, nil
 }
 
-func initialResync(ctx context.Context, cli *containerd.Client) {
+func (a *app) initialResync(ctx context.Context, cli *containerd.Client) {
 	containers, err := cli.ContainerService().List(ctx)
 	if err != nil {
 		glog.Errorf("Initial resync failed: %v", err)
@@ -266,7 +326,19 @@ func initialResync(ctx context.Context, cli *containerd.Client) {
 			continue
 		}
 
-		prometheusEnsureSeries(c.Labels)
+		a.trackContainerLabels(c.ID, c.Labels)
+		a.prometheusEnsureSeries(c.Labels)
 	}
-	isReady.Store(true)
+	a.isReady.Store(true)
+}
+
+func copyLabels(labels map[string]string) map[string]string {
+	if labels == nil {
+		return nil
+	}
+	copied := make(map[string]string, len(labels))
+	for key, value := range labels {
+		copied[key] = value
+	}
+	return copied
 }
