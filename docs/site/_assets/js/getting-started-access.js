b122edbfc5006f8187f93425065353da1145d9c6
Vladislav Panfilov <vladislav.panfilov@flant.com>
2026-01-23 18:24:11 +0400

    Move, add user fields
    Signed-off-by: Vladislav Panfilov <vladislav.panfilov@flant.com>

--- docs/site/_assets/js/getting-started-access.js
+++ docs/site/_assets/js/getting-started-access.js
@@ -1,31 +1,4 @@
-function prepare_parameters_update() {
-    // Helper function to update placeholders in code blocks
-    function updatePlaceholder(placeholder, storageKey, defaultValue) {
-        const value = sessionStorage.getItem(storageKey) || defaultValue;
-        if (!value) return;
-        
-        const placeholderRegex = new RegExp(placeholder.replace(/[<>]/g, '\\$&'), 'g');
-        
-        // Update code span elements (for syntax-highlighted code blocks)
-        $('code span').filter(function () {
-            return ((this.innerText.match(placeholderRegex) || []).length > 0);
-        }).each(function (index) {
-            let content = ($(this)[0]) ? $(this)[0].innerText : null;
-            if (content && content.length > 0) {
-                $(this)[0].innerText = content.replace(placeholderRegex, value);
-            }
-        });
-        
-        // Update full code blocks (textContent for full code blocks)
-        $('code').each(function (index) {
-            let content = ($(this)[0]) ? $(this)[0].textContent : null;
-            if (content && content.length > 0 && content.indexOf(placeholder) !== -1) {
-                $(this)[0].textContent = content.replace(placeholderRegex, value);
-            }
-        });
-    }
-
-    // Update domain parameters (complex regex replacements)
+function domain_update() {
     let domainPattern = sessionStorage.getItem('dhctl-domain');
     let domainSuffix = domainPattern ? domainPattern.replace('%s\.', '') : null;
 
@@ -52,27 +25,8 @@ function prepare_parameters_update() {
             }
         });
     }
-
-    // Update NFS parameters
-    updatePlaceholder('<NFS_SHARE>', 'dhctl-nfs-share', '/srv/nfs/dvp');
-    updatePlaceholder('<NFS_HOST>', 'dhctl-nfs-host', '192.168.1.100');
-
-    // Update subnet parameters
-    updatePlaceholder('<POD_SUBNET_CIDR>', 'dhctl-pod-subnet-cidr', '10.115.0.0/16');
-    updatePlaceholder('<SERVICE_SUBNET_CIDR>', 'dhctl-service-subnet-cidr', '10.225.0.0/16');
-
-    // Update optional subnet parameters (only if value exists)
-    const internalNetworkCIDRs = sessionStorage.getItem('dhctl-internal-network-cidrs');
-    if (internalNetworkCIDRs && internalNetworkCIDRs.length > 0) {
-        updatePlaceholder('<INTERNAL_NETWORK_CIDRS>', 'dhctl-internal-network-cidrs', null);
-    }
-
-    const virtualMachineCIDRs = sessionStorage.getItem('dhctl-virtual-machine-cidrs');
-    if (virtualMachineCIDRs && virtualMachineCIDRs.length > 0) {
-        updatePlaceholder('<VIRTUAL_MACHINE_CIDRS>', 'dhctl-virtual-machine-cidrs', null);
-    }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    prepare_parameters_update();
+    domain_update();
 });
--- docs/site/_includes/getting_started/dvp/global/STEP_FINALIZE.md
+++ docs/site/_includes/getting_started/dvp/global/STEP_FINALIZE.md
@@ -1,3 +1,3 @@
 <script type="text/javascript" src='{% javascript_asset_tag getting-started %}[_assets/js/getting-started.js]{% endjavascript_asset_tag %}'></script>
-<script type="text/javascript" src='{% javascript_asset_tag getting-started-access %}[_assets/js/getting-started-access.js]{% endjavascript_asset_tag %}'></script>
+<script type="text/javascript" src='{% javascript_asset_tag getting-started-access %}[_assets/js/dvp/getting-started-access.js]{% endjavascript_asset_tag %}'></script>
 <script type="text/javascript" src='{% javascript_asset_tag bcrypt %}[_assets/js/bcrypt.js]{% endjavascript_asset_tag %}'></script>
