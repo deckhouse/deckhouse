93a7797fe8875621c9df7dac22fdd19c5091a3df
Danila Bobkov <danila.bobkov@flant.com>
2026-01-23 12:40:53 +0300

    change kubeconfig from required:true to false
    Signed-off-by: Danila Bobkov <danila.bobkov@flant.com>

--- modules/040-node-manager/images/fencing-agent/src/internal/config/kube.go
+++ modules/040-node-manager/images/fencing-agent/src/internal/config/kube.go
@@ -1,25 +1,19 @@
 package fencingconfig
 
 import (
-	"errors"
 	"fencing-agent/internal/config/validators"
-	"strings"
 	"time"
 )
 
 type KubeConfig struct {
 	KubeAPIRPS                 int           `env:"KUBERNETES_API_RPS" env-default:"10"`
 	KubeAPIBurst               int           `env:"KUBERNETES_API_BURST" env-default:"100"`
-	KubeConfigPath             string        `env:"KUBECONFIG" env-required:"true"`
+	KubeConfigPath             string        `env:"KUBECONFIG" env-required:"false"`
 	KubernetesAPICheckInterval time.Duration `env:"KUBERNETES_API_CHECK_INTERVAL" env-default:"5s"`
 	KubernetesAPITimeout       time.Duration `env:"KUBERNETES_API_TIMEOUT" env-default:"10s"`
 }
 
 func (kc *KubeConfig) validate() error {
-	if strings.TrimSpace(kc.KubeConfigPath) == " " {
-		return errors.New("KUBECONFIG path is empty")
-	}
-
 	if unaryErr := validators.ValidateRateLimit(kc.KubeAPIRPS, kc.KubeAPIBurst, "kubernetesAPI"); unaryErr != nil {
 		return unaryErr
 	}
