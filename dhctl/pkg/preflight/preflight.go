6f89fe1150f05c0eeee4d3ef7ef689d29cecc0aa
Dmitry Denisenko <dmitry.denisenko@flant.com>
2026-01-28 17:04:32 +0300

    dhctl preflight check
    Signed-off-by: Dmitry Denisenko <dmitry.denisenko@flant.com>
    Signed-off-by: Denisenko Dmitry <dmitry.denisenko@flant.com>
    Signed-off-by: Dmitry Denisenko <dmitry.denisenko@flant.com>

--- dhctl/pkg/preflight/preflight.go
+++ dhctl/pkg/preflight/preflight.go
@@ -78,6 +78,11 @@ func (pc *Checker) Static(ctx context.Context) error {
 
 	err = pc.do(ctx, "Preflight checks for static-cluster", []checkStep{
 		{
+			fun:            pc.CheckStaticInstancesSSH,
+			successMessage: "SSHCredentials for StaticInstances are correct.",
+			skipFlag:       app.StaticInstancesWithSSHCredentials,
+		},
+		{
 			fun:            pc.CheckStaticInstancesIPDuplication,
 			successMessage: "IP of StaticInstances are unique",
 			skipFlag:       app.StaticInstancesIPDuplication,
