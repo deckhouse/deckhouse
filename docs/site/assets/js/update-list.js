e20b5648ac16d8c82a01b04c135915dbb54e31bf
Polina Polyntseva <pvsskk@mail.ru>
2026-01-28 14:28:28 +0300

    correct
    Signed-off-by: Polina Polyntseva <pvsskk@mail.ru>

---
+++ docs/site/assets/js/update-list.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const channelListTitle = document.querySelectorAll('.channel__list--title');
+
+    channelListTitle.forEach(title => {
+        title.addEventListener('click', function() {
+            if(title.classList.contains('show')) {
+                return;
+            }
+            const description = this.nextElementSibling;
+
+            channelListTitle.forEach(otherTitle => {
+                if(otherTitle !== title) {
+                    const otherDescription = otherTitle.nextElementSibling;
+                    otherDescription.classList.remove('active');
+                    otherTitle.classList.remove('show');
+                }
+            });
+
+            if(description.classList.contains('active')) {
+                description.classList.remove('active');
+                title.classList.remove('show');
+            } else {
+                description.classList.add('active');
+                title.classList.add('show');
+            }
+        })
+    })
+})
