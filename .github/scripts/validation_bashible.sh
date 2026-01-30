7fc1e67beced3e75c0f71ae02830512f9c2bd306
Alexey Voronov <alexey.voronov@flant.com>
2026-01-30 12:09:53 +0700

    fix bashible validation
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- .github/scripts/validation_bashible.sh
+++ .github/scripts/validation_bashible.sh
@@ -176,7 +176,7 @@ docker pull ${REGISTRY}/deckhouse/ee/install:stable
 cat <<'SCRIPT_END' | docker run -i --rm \
   -v ${volumesRoot}/candi/bashible:/deckhouse/candi/bashible \
   -v ${volumesRoot}/candi/cloud-providers:/deckhouse/candi/cloud-providers \
-  -v ${volumesRoot}/candi/cloud-providers/aws:/deckhouse/modules/030-cloud-provider-aws/candi \
+  -v ${volumesRoot}/modules/030-cloud-provider-aws/candi:/deckhouse/candi/cloud-providers/aws \
   -v ${volumesRoot}/${config}:/${config} \
   -e config=config.yaml \
   --entrypoint=bash \
