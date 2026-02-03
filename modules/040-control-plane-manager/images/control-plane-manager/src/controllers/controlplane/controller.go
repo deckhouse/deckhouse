f686122dfd1810ee172f8d763fb043c6dc56919d
dmitry.trofimov <dmitry.trofimov@flant.com>
2026-02-03 12:35:56 +0300

    control-plane-manager global refactor
    Signed-off-by: dmitry.trofimov <dmitry.trofimov@flant.com>

---
+++ modules/040-control-plane-manager/images/control-plane-manager/src/controllers/controlplane/controller.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2026 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controlplane
+
+import (
+	"context"
+	"time"
+	"control-plane-manager/pkg/constants"
+
+	"golang.org/x/time/rate"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	controlplanev1alpha1 "control-plane-manager/api/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+const (
+	maxConcurrentReconciles = 1
+	cacheSyncTimeout        = 3 * time.Minute
+	requeueInterval         = 1 * time.Minute
+)
+
+type Reconciler struct {
+	client client.Client
+}
+
+func Register(mgr manager.Manager) error {
+	r := &Reconciler{
+		client: mgr.GetClient(),
+	}
+
+	return ctrl.NewControllerManagedBy(mgr).
+		WithOptions(controller.TypedOptions[reconcile.Request]{
+			MaxConcurrentReconciles: maxConcurrentReconciles,
+			CacheSyncTimeout:        cacheSyncTimeout,
+			NeedLeaderElection:      ptr.To(true),
+			RateLimiter: workqueue.NewTypedMaxOfRateLimiter(
+				workqueue.NewTypedItemExponentialFailureRateLimiter[reconcile.Request](100*time.Millisecond, 3*time.Second),
+				&workqueue.TypedBucketRateLimiter[reconcile.Request]{
+					Limiter: rate.NewLimiter(rate.Limit(1), 1),
+				},
+			),
+		}).
+		Named(constants.ControllerName).
+		Watches(
+			&controlplanev1alpha1.ControlPlaneConfiguration{},
+			&handler.EnqueueRequestForObject{},
+			builder.WithPredicates(getControlPlaneConfigurationPredicate()),
+		).
+		Complete(r)
+}
+
+func getControlPlaneConfigurationPredicate() predicate.Predicate {
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			return true
+		},
+
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			return true
+		},
+
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			return true
+		},
+
+		GenericFunc: func(e event.GenericEvent) bool {
+			return false
+		},
+	}
+}
+
+func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+	klog.Infof("Reconcile started for request: %v", req)
+
+	// TODO: Implement control plane management logic here
+
+	klog.Info("Reconcile completed successfully")
+	return reconcile.Result{RequeueAfter: requeueInterval}, nil
+}
