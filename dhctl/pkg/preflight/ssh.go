a5dfe54f99a627c3abffc47ba5f296d780a3b22f
dmitry.denisenko <dmitry.denisenko@flant.com>
2026-02-03 13:59:54 +0400

    dhctl fix start http server
    Signed-off-by: Denisenko Dmitry <dmitry.denisenko@flant.com>

--- dhctl/pkg/preflight/ssh.go
+++ dhctl/pkg/preflight/ssh.go
@@ -180,8 +180,12 @@ func startHttpServer(ctx context.Context, port int) (shutdownServerFunc, error) 
 	})
 
 	address := fmt.Sprintf(":%d", port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.ErrorF("Cannot listen on %s: %v\n", address, err)
+		return nil, fmt.Errorf("Cannot start HTTP server for tunnel preflight check on %s: %w", address, err)
+	}
 	server := &http.Server{
-		Addr:         address,
 		Handler:      mux,
 		ReadTimeout:  20 * time.Second,
 		WriteTimeout: 20 * time.Second,
@@ -189,7 +193,7 @@ func startHttpServer(ctx context.Context, port int) (shutdownServerFunc, error) 
 
 	go func() {
 		log.DebugF("Starting HTTP server for tunnel preflight check on %s\n", address)
-		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		if err := server.Serve(listener); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			log.InfoF("Error starting HTTP server for tunnel preflight check on %s: %v\n", address, err)
 		}
 	}()
@@ -208,7 +212,7 @@ func startHttpServer(ctx context.Context, port int) (shutdownServerFunc, error) 
 
 	client := &http.Client{}
 
-	err := retry.NewSilentLoop("Check HTTP server running for tunnel preflight check", 5, 1*time.Millisecond).RunContext(ctx, func() error {
+	err = retry.NewSilentLoop("Check HTTP server running for tunnel preflight check", 50, 100*time.Millisecond).RunContext(ctx, func() error {
 		cctx, cancel := context.WithTimeout(ctx, 2*time.Second)
 		defer cancel() // Ensure the context is canceled to release resources
 
