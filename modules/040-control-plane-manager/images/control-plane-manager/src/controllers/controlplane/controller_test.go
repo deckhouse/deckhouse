743e73ee3a644e74619c44572aaf0d6c589fe661
dmitry.trofimov <dmitry.trofimov@flant.com>
2026-02-03 18:57:12 +0300

    control-plane-manager global refactor
    Signed-off-by: dmitry.trofimov <dmitry.trofimov@flant.com>

--- modules/040-control-plane-manager/images/control-plane-manager/src/controllers/controlplane/controller_test.go
+++ modules/040-control-plane-manager/images/control-plane-manager/src/controllers/controlplane/controller_test.go
@@ -34,6 +34,7 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/yaml"
 
 	controlplanev1alpha1 "control-plane-manager/api/v1alpha1"
@@ -67,19 +68,174 @@ type ControllerTestSuite struct {
 	time             metav1.Time
 }
 
-// TODO: Example test case
-// func (suite *ControllerTestSuite) TestReconcileBasic() {
-// 	suite.Run("When control plane configuration is created", func() {
-// 		suite.setupController(suite.fetchTestFileData("basic-config.yaml"))
-//
-// 		_, err := suite.controller.Reconcile(
-// 			suite.ctx,
-// 			reconcile.Request{},
-// 		)
-//
-// 		require.NoError(suite.T(), err)
-// 	})
-// }
+func (suite *ControllerTestSuite) TestReconcileCreateControlPlaneConfiguration() {
+	suite.Run("When secrets exist, ControlPlaneConfiguration should be created with correct checksums", func() {
+		suite.setupController(suite.fetchTestFileData("basic-config.yaml"))
+
+		_, err := suite.controller.Reconcile(
+			suite.ctx,
+			reconcile.Request{
+				NamespacedName: client.ObjectKey{
+					Name: "control-plane",
+				},
+			},
+		)
+
+		require.NoError(suite.T(), err)
+
+		// ControlPlaneConfiguration must be created/updated
+		cpc := &controlplanev1alpha1.ControlPlaneConfiguration{}
+		err = suite.client.Get(suite.ctx, client.ObjectKey{Name: "control-plane"}, cpc)
+		require.NoError(suite.T(), err, "ControlPlaneConfiguration should exist")
+
+		// Spec must be populated
+		require.NotNil(suite.T(), cpc.Spec.Components, "Components should not be empty")
+		require.NotEmpty(suite.T(), cpc.Spec.PKIChecksum, "PKIChecksum should not be empty")
+
+		// Component checksums must exist
+		require.NotNil(suite.T(), cpc.Spec.Components.Etcd, "Etcd component should not be empty")
+		require.NotEmpty(suite.T(), cpc.Spec.Components.Etcd.Checksum, "Etcd checksum should not be empty")
+
+		require.NotNil(suite.T(), cpc.Spec.Components.KubeAPIServer, "KubeAPIServer component should not be empty")
+		require.NotEmpty(suite.T(), cpc.Spec.Components.KubeAPIServer.Checksum, "KubeAPIServer checksum should not be empty")
+
+		require.NotNil(suite.T(), cpc.Spec.Components.KubeControllerManager, "KubeControllerManager component should not be empty")
+		require.NotEmpty(suite.T(), cpc.Spec.Components.KubeControllerManager.Checksum, "KubeControllerManager checksum should not be empty")
+
+		require.NotNil(suite.T(), cpc.Spec.Components.KubeScheduler, "KubeScheduler component should not be empty")
+		require.NotEmpty(suite.T(), cpc.Spec.Components.KubeScheduler.Checksum, "KubeScheduler checksum should not be empty")
+
+		suite.T().Logf("PKI Checksum: %s", cpc.Spec.PKIChecksum)
+		suite.T().Logf("Etcd Checksum: %s", cpc.Spec.Components.Etcd.Checksum)
+		suite.T().Logf("KubeAPIServer Checksum: %s", cpc.Spec.Components.KubeAPIServer.Checksum)
+		suite.T().Logf("KubeControllerManager Checksum: %s", cpc.Spec.Components.KubeControllerManager.Checksum)
+		suite.T().Logf("KubeScheduler Checksum: %s", cpc.Spec.Components.KubeScheduler.Checksum)
+	})
+}
+
+func (suite *ControllerTestSuite) TestReconcileUpdateControlPlaneConfiguration() {
+	suite.Run("When secrets are updated, ControlPlaneConfiguration checksums should change", func() {
+		suite.setupController(suite.fetchTestFileData("basic-config.yaml"))
+
+		// First reconcile
+		_, err := suite.controller.Reconcile(
+			suite.ctx,
+			reconcile.Request{
+				NamespacedName: client.ObjectKey{
+					Name: "control-plane",
+				},
+			},
+		)
+		require.NoError(suite.T(), err)
+
+		// Get initial checksums
+		cpc := &controlplanev1alpha1.ControlPlaneConfiguration{}
+		err = suite.client.Get(suite.ctx, client.ObjectKey{Name: "control-plane"}, cpc)
+		require.NoError(suite.T(), err)
+
+		oldPKIChecksum := cpc.Spec.PKIChecksum
+		oldEtcdChecksum := cpc.Spec.Components.Etcd.Checksum
+
+		// Update PKI secret
+		pkiSecret := &corev1.Secret{}
+		err = suite.client.Get(suite.ctx, client.ObjectKey{
+			Name:      "d8-pki",
+			Namespace: "kube-system",
+		}, pkiSecret)
+		require.NoError(suite.T(), err)
+
+		// Modify PKI secret data
+		pkiSecret.Data["ca.crt"] = []byte("LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk5FVyBDQSBDRVJUCi0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0=")
+		err = suite.client.Update(suite.ctx, pkiSecret)
+		require.NoError(suite.T(), err)
+
+		// Second reconcile
+		_, err = suite.controller.Reconcile(
+			suite.ctx,
+			reconcile.Request{
+				NamespacedName: client.ObjectKey{
+					Name: "control-plane",
+				},
+			},
+		)
+		require.NoError(suite.T(), err)
+
+		// Get updated checksums
+		err = suite.client.Get(suite.ctx, client.ObjectKey{Name: "control-plane"}, cpc)
+		require.NoError(suite.T(), err)
+
+		// PKI checksum should have changed
+		require.NotEqual(suite.T(), oldPKIChecksum, cpc.Spec.PKIChecksum, "PKI checksum should change after secret update")
+
+		// Etcd checksum should remain the same (we didn't update its secret data)
+		require.Equal(suite.T(), oldEtcdChecksum, cpc.Spec.Components.Etcd.Checksum, "Etcd checksum should remain unchanged")
+
+		suite.T().Logf("Old PKI Checksum: %s", oldPKIChecksum)
+		suite.T().Logf("New PKI Checksum: %s", cpc.Spec.PKIChecksum)
+	})
+}
+
+func (suite *ControllerTestSuite) TestReconcileComponentChecksumChange() {
+	suite.Run("When component manifest is updated, only its checksum should change", func() {
+		suite.setupController(suite.fetchTestFileData("basic-config.yaml"))
+
+		// First reconcile
+		_, err := suite.controller.Reconcile(
+			suite.ctx,
+			reconcile.Request{
+				NamespacedName: client.ObjectKey{
+					Name: "control-plane",
+				},
+			},
+		)
+		require.NoError(suite.T(), err)
+
+		// Get initial checksums
+		cpc := &controlplanev1alpha1.ControlPlaneConfiguration{}
+		err = suite.client.Get(suite.ctx, client.ObjectKey{Name: "control-plane"}, cpc)
+		require.NoError(suite.T(), err)
+
+		oldEtcdChecksum := cpc.Spec.Components.Etcd.Checksum
+		oldAPIServerChecksum := cpc.Spec.Components.KubeAPIServer.Checksum
+
+		// Update etcd manifest in config secret
+		configSecret := &corev1.Secret{}
+		err = suite.client.Get(suite.ctx, client.ObjectKey{
+			Name:      "d8-control-plane-manager-config",
+			Namespace: "kube-system",
+		}, configSecret)
+		require.NoError(suite.T(), err)
+
+		// Modify etcd.yaml
+		configSecret.Data["etcd.yaml"] = []byte("YXBpVmVyc2lvbjogdjIKa2luZDogUG9kCm1ldGFkYXRhOgogIG5hbWU6IGV0Y2QtdXBkYXRlZA==")
+		err = suite.client.Update(suite.ctx, configSecret)
+		require.NoError(suite.T(), err)
+
+		// Second reconcile
+		_, err = suite.controller.Reconcile(
+			suite.ctx,
+			reconcile.Request{
+				NamespacedName: client.ObjectKey{
+					Name: "control-plane",
+				},
+			},
+		)
+		require.NoError(suite.T(), err)
+
+		// Get updated checksums
+		err = suite.client.Get(suite.ctx, client.ObjectKey{Name: "control-plane"}, cpc)
+		require.NoError(suite.T(), err)
+
+		// Etcd checksum should have changed
+		require.NotEqual(suite.T(), oldEtcdChecksum, cpc.Spec.Components.Etcd.Checksum, "Etcd checksum should change after manifest update")
+
+		// API Server checksum should remain the same
+		require.Equal(suite.T(), oldAPIServerChecksum, cpc.Spec.Components.KubeAPIServer.Checksum, "KubeAPIServer checksum should remain unchanged")
+
+		suite.T().Logf("Old Etcd Checksum: %s", oldEtcdChecksum)
+		suite.T().Logf("New Etcd Checksum: %s", cpc.Spec.Components.Etcd.Checksum)
+	})
+}
 
 func (suite *ControllerTestSuite) TearDownSubTest() {
 	if !suite.T().Failed() {
@@ -134,29 +290,34 @@ func (suite *ControllerTestSuite) parseManifests(data string) []client.Object {
 	manifests := mDelimiter.Split(data, -1)
 	var objs []client.Object
 
-	for _, manifest := range manifests {
+	for i, manifest := range manifests {
 		manifest = strings.TrimSpace(manifest)
 		if manifest == "" {
 			continue
 		}
 
-		obj := &runtime.Unknown{}
-		err := yaml.Unmarshal([]byte(manifest), obj)
-		require.NoError(suite.T(), err, "failed to unmarshal manifest")
+		metaType := &runtime.TypeMeta{}
+		err := yaml.Unmarshal([]byte(manifest), metaType)
+		require.NoError(suite.T(), err, "failed to unmarshal manifest %d", i)
 
-		switch obj.TypeMeta.Kind {
+		if metaType.Kind == "" {
+			suite.T().Logf("manifest %d has empty kind, skipping", i)
+			continue
+		}
+
+		switch metaType.Kind {
 		case "Secret":
 			secret := &corev1.Secret{}
 			err = yaml.Unmarshal([]byte(manifest), secret)
-			require.NoError(suite.T(), err)
+			require.NoError(suite.T(), err, "failed to unmarshal Secret")
 			objs = append(objs, secret)
 		case "ControlPlaneConfiguration":
 			cpc := &controlplanev1alpha1.ControlPlaneConfiguration{}
 			err = yaml.Unmarshal([]byte(manifest), cpc)
-			require.NoError(suite.T(), err)
+			require.NoError(suite.T(), err, "failed to unmarshal ControlPlaneConfiguration")
 			objs = append(objs, cpc)
 		default:
-			suite.T().Logf("unknown kind: %s", obj.TypeMeta.Kind)
+			suite.T().Logf("unknown kind: %s", metaType.Kind)
 		}
 	}
 
