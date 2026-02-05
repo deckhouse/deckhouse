04608e919cc43dcc5ab550b8527e1b32cec2b1a5
Arman Toreniyazov <armangokka@gmail.com>
2026-02-05 11:28:37 +0300

    [global-hooks] add HA preservation mechanism
    Signed-off-by: Arman Toreniyazov <armangokka@gmail.com>

--- dhctl/pkg/operations/converge/context/converge_state.go
+++ dhctl/pkg/operations/converge/context/converge_state.go
@@ -36,8 +36,9 @@ const (
 )
 
 type State struct {
-	Phase               phases.OperationPhase   `json:"phase"`
-	NodeUserCredentials *v1.NodeUserCredentials `json:"nodeUserCredentials"`
+	Phase                  phases.OperationPhase   `json:"phase"`
+	NodeUserCredentials    *v1.NodeUserCredentials `json:"nodeUserCredentials"`
+	PreserveExistingHAMode bool                    `json:"preserveExistingHAMode"`
 }
 
 type stateStore interface {
