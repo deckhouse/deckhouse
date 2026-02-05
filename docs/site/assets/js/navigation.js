4992b1c465504e09cd3715bc21fbe6b04788e688
Polina Polyntseva <pvsskk@mail.ru>
2026-02-05 10:09:02 +0300

    add adaptive header
    Signed-off-by: Polina Polyntseva <pvsskk@mail.ru>

--- docs/site/assets/js/navigation.js
+++ docs/site/assets/js/navigation.js
@@ -2,59 +2,64 @@ $(window).on('load', function() {
     const navigationContainer = document.querySelector('.navigation__container');
 
     if (navigationContainer) {
-        const sidebarAndToc = document.querySelectorAll('.sidebar__wrapper-inner');
-        let lastScroll = window.scrollY;
-        const headerHeight = document.querySelector('.header-container').offsetHeight;
-        const navigationHeight = navigationContainer.offsetHeight;
-        const newTop = headerHeight + navigationHeight;
-        const tocSidebarLinks = document.querySelectorAll('.toc-sidebar__item-link');
+        if (window.innerWidth > 1024) {
+            navigationContainer.classList.add('fixed-navigation');
+            const sidebarAndToc = document.querySelectorAll('.sidebar__wrapper-inner');
+            let lastScroll = window.scrollY;
+            const headerHeight = document.querySelector('.header-container').offsetHeight;
+            const navigationHeight = navigationContainer.offsetHeight;
+            const newTop = headerHeight + navigationHeight;
+            const tocSidebarLinks = document.querySelectorAll('.toc-sidebar__item-link');
 
-        let isScroll = true;
+            let isScroll = true;
 
-        function hideNavigation() {
-            navigationContainer.classList.add('hidden');
-            lastScroll = window.scrollY;
-            sidebarAndToc.forEach(e => {
-                e.classList.remove('top');
-                e.style.removeProperty('--scroll-top');
-            });
-        }
-
-        function scrollHandler() {
-            if (!isScroll) return;
-
-            const scrollWindowTop = window.scrollY;
-            if (scrollWindowTop > lastScroll) {
+            function hideNavigation() {
                 navigationContainer.classList.add('hidden');
-                lastScroll = scrollWindowTop;
+                lastScroll = window.scrollY;
                 sidebarAndToc.forEach(e => {
                     e.classList.remove('top');
                     e.style.removeProperty('--scroll-top');
                 });
-            } else {
-                navigationContainer.classList.remove('hidden');
-                lastScroll = scrollWindowTop;
-                sidebarAndToc.forEach(e => {
-                    e.classList.add('top');
-                    e.style.setProperty('--scroll-top', `${newTop}px`);
-                });
             }
-        }
 
-        tocSidebarLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                isScroll = false;
-                hideNavigation();
-                setTimeout(() => {
-                    isScroll = true;
-                }, 500);
+            function scrollHandler() {
+                if (!isScroll) return;
+
+                const scrollWindowTop = window.scrollY;
+                if (scrollWindowTop > lastScroll) {
+                    navigationContainer.classList.add('hidden');
+                    lastScroll = scrollWindowTop;
+                    sidebarAndToc.forEach(e => {
+                        e.classList.remove('top');
+                        e.style.removeProperty('--scroll-top');
+                    });
+                } else {
+                    navigationContainer.classList.remove('hidden');
+                    lastScroll = scrollWindowTop;
+                    sidebarAndToc.forEach(e => {
+                        e.classList.add('top');
+                        e.style.setProperty('--scroll-top', `${newTop}px`);
+                    });
+                }
+            }
+
+            tocSidebarLinks.forEach(link => {
+                link.addEventListener('click', () => {
+                    isScroll = false;
+                    hideNavigation();
+                    setTimeout(() => {
+                        isScroll = true;
+                    }, 500);
+                });
             });
-        });
 
-        window.addEventListener('scroll', scrollHandler);
+            window.addEventListener('scroll', scrollHandler);
 
-        if (window.scrollY > 0) {
-            hideNavigation();
+            if (window.scrollY > 0) {
+                hideNavigation();
+            }
+        } else {
+            navigationContainer.classList.remove('fixed-navigation');
         }
     }
 });
