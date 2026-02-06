2efe9227309f6692e5a8c69a043acaaa1d36f4f3
Andrey Polovov <andrey.polovov@flant.com>
2026-02-06 18:22:55 +0300

    Update ee/se-plus/modules/021-cni-cilium/images/egress-gateway-agent/src/internal/layer2/announcer.go
    Signed-off-by: Andrey Polovov <andrey.polovov@flant.com>

--- ee/se-plus/modules/021-cni-cilium/images/egress-gateway-agent/src/internal/layer2/announcer.go
+++ ee/se-plus/modules/021-cni-cilium/images/egress-gateway-agent/src/internal/layer2/announcer.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-Modifications copyright (C) 2026 Flant JSC
+Modifications copyright (C) 2024 Flant JSC
 
 The Announce struct is extended with GetIPSMap() and GetInterfaces() methods
 */
