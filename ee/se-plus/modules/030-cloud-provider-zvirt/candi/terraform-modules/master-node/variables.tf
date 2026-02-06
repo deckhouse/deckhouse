abe53704d37cc1af7193dcf4057a564652e810ce
Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>
2026-02-06 11:26:32 +0300

    add customNetworkConfig
    Signed-off-by: Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>

--- ee/se-plus/modules/030-cloud-provider-zvirt/candi/terraform-modules/master-node/variables.tf
+++ ee/se-plus/modules/030-cloud-provider-zvirt/candi/terraform-modules/master-node/variables.tf
@@ -39,11 +39,35 @@ locals {
   master_root_disk_size = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "rootDiskSizeGb", 50)*1024*1024*1024
   master_etcd_disk_size = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "etcdDiskSizeGb", 10)*1024*1024*1024
 
+  use_cloud_config_network = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "customNetworkConfig", false)
+  cloud_config_network = {
+    "network" = {
+      "varsion" = 1,
+      "config" = [
+        {
+          "type" = "physical",
+          "name" = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "networkInterfaceName", ""),
+          "subnets" = [
+            {
+              "type" = "static",
+              "address" = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass.networkInterfaceAddress, var.nodeIndex, ""),
+              "gateway" = lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "networkInterfaceGateway", ""),
+            }
+          ],
+        },
+        {
+          "type" = "nameserver",
+          "address" = [lookup(var.providerClusterConfiguration.masterNodeGroup.instanceClass, "networkInterfaceDNS", "")],
+        },
+      ],
+    }
+  }
+
   master_cloud_init_script = yamlencode(merge({
     "hostname": local.master_node_name,
     "create_hostname_file": true,
     "ssh_deletekeys": true,
     "ssh_genkeytypes": ["rsa", "ecdsa", "ed25519"],
     "ssh_authorized_keys" : [local.ssh_pubkey]
-  }, length(var.cloudConfig) > 0 ? yamldecode(base64decode(var.cloudConfig)) : tomap({})))
+  }, length(var.cloudConfig) > 0 ? yamldecode(base64decode(var.cloudConfig)) : tomap({}), local.use_cloud_config_network ? local.cloud_config_network : tomap({})))
 }
