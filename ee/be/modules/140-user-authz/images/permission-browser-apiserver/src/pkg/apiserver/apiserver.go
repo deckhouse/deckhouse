6623a82bd3bde62d62221cb6656e55ceeaaf1f6e
Ivan Zvyagintsev <ivan.zvyagintsev@flant.com>
2026-02-10 12:57:22 +0300

    Fix permission-browser-apiserver scope cache
    Signed-off-by: Ivan Zvyagintsev <ivan.zvyagintsev@flant.com>

--- ee/be/modules/140-user-authz/images/permission-browser-apiserver/src/pkg/apiserver/apiserver.go
+++ ee/be/modules/140-user-authz/images/permission-browser-apiserver/src/pkg/apiserver/apiserver.go
@@ -8,6 +8,7 @@ package apiserver
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -15,6 +16,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
+	"k8s.io/apiserver/pkg/server/healthz"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
@@ -231,6 +233,22 @@ func (c completedConfig) New() (*PermissionBrowserServer, error) {
 		go mtEngine.StartRenewConfigLoop(ctx.Done())
 	}
 
+	// Create resource scope cache for background discovery refresh
+	var scopeCache *resolver.ResourceScopeCache
+	if initRes.clientset != nil {
+		scopeCache = resolver.NewResourceScopeCache(initRes.clientset.Discovery())
+		go scopeCache.StartRefreshLoop(ctx.Done())
+		klog.Info("Resource scope cache initialized and refresh loop started")
+
+		// Ensure readiness fails until the cache has been populated at least once.
+		genericServer.AddReadyzChecks(healthz.NamedCheck("resource-scope-cache", func(_ *http.Request) error {
+			if !scopeCache.HasData() {
+				return fmt.Errorf("resource scope cache is empty")
+			}
+			return nil
+		}))
+	}
+
 	// Create namespace resolver for AccessibleNamespace API
 	var nsResolver *resolver.NamespaceResolver
 	if initRes.informerFactory != nil {
@@ -241,7 +259,7 @@ func (c completedConfig) New() (*PermissionBrowserServer, error) {
 			rbacInformers.RoleBindings().Lister(),
 			rbacInformers.ClusterRoles().Lister(),
 			rbacInformers.ClusterRoleBindings().Lister(),
-			initRes.clientset.Discovery(),
+			scopeCache,
 			mtEngine,
 		)
 		klog.Info("Namespace resolver initialized for AccessibleNamespace API")
