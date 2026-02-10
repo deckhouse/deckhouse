d6270ac46e68a2d2d73be9ad77d02a832a722454
Sergey Kurbatov <sergey.kurbatov@flant.com>
2026-02-10 14:08:53 +0300

    fix tabs
    Signed-off-by: Sergey Kurbatov <sergey.kurbatov@flant.com>

--- modules/110-istio/hooks/compatibility_version_istio_k8s_monitoring_test.go
+++ modules/110-istio/hooks/compatibility_version_istio_k8s_monitoring_test.go
@@ -31,7 +31,7 @@ istio:
   internal:
     istioToK8sCompatibilityMap:
       "1.16": ["1.22", "1.23", "1.24", "1.25"]
-	  "1.21": ["1.30", "1.31", "1.32", "1.33", "1.34"]
+      "1.21": ["1.30", "1.31", "1.32", "1.33", "1.34"]
 `
 	f := HookExecutionConfigInit(initValues, "")
 
