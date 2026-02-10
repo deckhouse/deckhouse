4b99843e08d9351fdaba76d6fc8c28fcf3a55013
Roman Berezkin <roman.berezkin@flant.com>
2026-02-10 12:54:30 +0300

    Create 3 new resources and basic controllers (skeletons) for them
    - Module v1alpha2
    - ModulePackageVersion
    - ModulePackage
    
    Signed-off-by: Roman Berezkin <roman.berezkin@flant.com>

---
+++ deckhouse-controller/pkg/apis/deckhouse.io/v1alpha2/module.go
@@ -0,0 +1,223 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha2
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/apis/deckhouse.io/v1alpha1"
+)
+
+const (
+	ModuleResource = "modules"
+	ModuleKind     = "Module"
+
+	// ModuleConditionTypeProcessed changes only by module controller
+	ModuleConditionTypeProcessed                    = "Processed"
+	ModuleConditionReasonVersionNotFound            = "VersionNotFound"
+	ModuleConditionReasonModulePackageNotFound       = "ModulePackageNotFound"
+	ModuleConditionReasonVersionIsDraft             = "VersionIsDraft"
+	ModuleConditionReasonVersionSpecIsCorrupted     = "VersionSpecIsCorrupted"
+
+	ModuleFinalizerStatisticRegistered = "module.deckhouse.io/statistic-registered"
+
+	ModuleAnnotationRegistrySpecChanged = "packages.deckhouse.io/registry-spec-changed"
+)
+
+var (
+	ModuleGVR = schema.GroupVersionResource{
+		Group:    SchemeGroupVersion.Group,
+		Version:  SchemeGroupVersion.Version,
+		Resource: ModuleResource,
+	}
+	ModuleGVK = schema.GroupVersionKind{
+		Group:   SchemeGroupVersion.Group,
+		Version: SchemeGroupVersion.Version,
+		Kind:    ModuleKind,
+	}
+)
+
+var _ runtime.Object = (*Module)(nil)
+
+// +genclient
+// +genclient:nonNamespaced
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:printcolumn:name=Package,type=string,JSONPath=.spec.packageName
+// +kubebuilder:printcolumn:name=Version,type=string,JSONPath=.spec.packageVersion
+// +kubebuilder:printcolumn:name=Repository,type=string,JSONPath=.spec.packageRepositoryName,priority=1
+// +kubebuilder:printcolumn:name=Installed,type=string,JSONPath=.status.conditions[?(@.type=='Installed')].status
+// +kubebuilder:printcolumn:name=Ready,type=string,JSONPath=.status.conditions[?(@.type=='Ready')].status
+// +kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].message"
+// +kubebuilder:printcolumn:name=Age,type=date,JSONPath=.metadata.creationTimestamp
+
+// Module represents a module instance managed via the package system.
+type Module struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Spec defines the behavior of a Module.
+	Spec ModuleSpec `json:"spec"`
+
+	// Status of a Module.
+	Status ModuleStatus `json:"status,omitempty"`
+}
+
+type ModuleSpec struct {
+	// Name of the module package to install.
+	PackageName string `json:"packageName"`
+
+	// Name of the repository where the package is located.
+	// If not specified, the default repository is used.
+	// +optional
+	PackageRepositoryName string `json:"packageRepositoryName,omitempty"`
+
+	// Version of the module package to install.
+	PackageVersion string `json:"packageVersion"`
+
+	// Release channel for the module package.
+	// +optional
+	ReleaseChannel string `json:"releaseChannel,omitempty"`
+
+	// Configuration settings for the module.
+	// +kubebuilder:pruning:PreserveUnknownFields
+	// +optional
+	Settings *v1alpha1.MappedFields `json:"settings,omitempty"`
+}
+
+type ModuleStatus struct {
+	// Information about the currently installed version.
+	// +optional
+	CurrentVersion *ModuleStatusVersion `json:"currentVersion,omitempty"`
+
+	// Conditions represent the latest available observations of the module's state.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions []ModuleStatusCondition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
+	// InternalConditions represent internal conditions of the module.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	InternalConditions []ModuleStatusInternalCondition `json:"internalConditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
+	// ResourceConditions represent conditions related to module resources.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	ResourceConditions []ModuleStatusResourceCondition `json:"resourceConditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+type ModuleStatusVersion struct {
+	// Semantic version of the installed module.
+	// +optional
+	Version string `json:"version,omitempty"`
+
+	// Release channel from which the version was installed.
+	// +optional
+	Channel string `json:"channel,omitempty"`
+}
+
+type ModuleStatusCondition struct {
+	// Type of module condition.
+	Type string `json:"type"`
+
+	// Status of the condition, one of True, False, Unknown.
+	Status corev1.ConditionStatus `json:"status"`
+
+	// Programmatic identifier indicating the reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+
+	// Human readable message indicating details about the transition.
+	// +optional
+	Message string `json:"message,omitempty"`
+
+	// Last time the condition was probed.
+	// +optional
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+
+	// Last time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+}
+
+type ModuleStatusInternalCondition struct {
+	// Type of internal module condition.
+	Type string `json:"type"`
+
+	// Status of the condition, one of True, False, Unknown.
+	Status corev1.ConditionStatus `json:"status"`
+
+	// Programmatic identifier indicating the reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+
+	// Human readable message indicating details about the transition.
+	// +optional
+	Message string `json:"message,omitempty"`
+
+	// Last time the condition was probed.
+	// +optional
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+
+	// The last time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+}
+
+type ModuleStatusResourceCondition struct {
+	// Type of resource condition.
+	Type string `json:"type"`
+
+	// Status of the condition, one of True, False, Unknown.
+	Status corev1.ConditionStatus `json:"status"`
+
+	// Programmatic identifier indicating the reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+
+	// Human readable message indicating details about the transition.
+	// +optional
+	Message string `json:"message,omitempty"`
+
+	// Last time the condition was probed.
+	// +optional
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+
+	// Last time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ModuleList is a list of Module resources
+type ModuleList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []Module `json:"items"`
+}
---
+++ deckhouse-controller/pkg/controller/packages/module-package-version/controller.go
@@ -0,0 +1,92 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package modulepackageversion
+
+import (
+	"context"
+	"log/slog"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/deckhouse/deckhouse/deckhouse-controller/pkg/apis/deckhouse.io/v1alpha1"
+	"github.com/deckhouse/deckhouse/go_lib/dependency"
+	"github.com/deckhouse/deckhouse/pkg/log"
+)
+
+type reconciler struct {
+	client client.Client
+	dc     dependency.Container
+	logger *log.Logger
+}
+
+func RegisterController(
+	runtimeManager manager.Manager,
+	dc dependency.Container,
+	logger *log.Logger,
+) error {
+	r := &reconciler{
+		client: runtimeManager.GetClient(),
+		dc:     dc,
+		logger: logger,
+	}
+
+	return ctrl.NewControllerManagedBy(runtimeManager).
+		For(&v1alpha1.ModulePackageVersion{}).
+		Complete(r)
+}
+
+func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := r.logger.With(slog.String("name", req.Name))
+
+	logger.Info("reconcile module package version")
+
+	mpv := new(v1alpha1.ModulePackageVersion)
+	if err := r.client.Get(ctx, req.NamespacedName, mpv); err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Debug("module package version not found")
+
+			return ctrl.Result{}, nil
+		}
+
+		logger.Warn("failed to get module package version", log.Err(err))
+
+		return ctrl.Result{}, err
+	}
+
+	// handle delete event
+	if !mpv.DeletionTimestamp.IsZero() {
+		return ctrl.Result{}, nil
+	}
+
+	// handle create/update events
+	if err := r.handleCreateOrUpdate(ctx, logger, mpv); err != nil {
+		logger.Warn("failed to handle module package version", log.Err(err))
+
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *reconciler) handleCreateOrUpdate(_ context.Context, logger *log.Logger, _ *v1alpha1.ModulePackageVersion) error {
+	logger.Debug("handle create or update module package version")
+
+	return nil
+}
