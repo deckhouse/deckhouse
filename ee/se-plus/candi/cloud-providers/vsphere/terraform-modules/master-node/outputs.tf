d99963d438e9e6e243c248b03bde1a0bdd013c82
deckhouse-BOaTswain <89150800+deckhouse-BOaTswain@users.noreply.github.com>
2026-02-16 11:04:41 +0300

    [cloud-provider-vsphere] fix maping for kubernetes data disk (#17859) (#17990)
    ---------
    
    Signed-off-by: Alexey Sulimenko <alexey.sulimenko@flant.com>
    Co-authored-by: Aleksey Sulimenko <alexey.sulimenko@flant.com>

--- ee/se-plus/candi/cloud-providers/vsphere/terraform-modules/master-node/outputs.tf
+++ ee/se-plus/candi/cloud-providers/vsphere/terraform-modules/master-node/outputs.tf
@@ -1,5 +1,13 @@
 # Copyright 2021 Flant JSC
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
+locals {
+  kubernetes_data_device_uuid_list = [
+    for disk in vsphere_virtual_machine.master.disk :
+    disk.uuid if disk.label == "disk1"
+  ]
+  kubernetes_data_device_uuid = replace(length(local.kubernetes_data_device_uuid_list) > 0 ? local.kubernetes_data_device_uuid_list[0] : "", "-", "")
+  kubernetes_data_device_path = local.kubernetes_data_device_uuid != "" ? "/dev/disk/by-id/wwn-0x${local.kubernetes_data_device_uuid}" : "/dev/sdb"
+}
 
 output "master_ip_address_for_ssh" {
   value = vsphere_virtual_machine.master.default_ip_address
@@ -10,5 +18,6 @@ output "node_internal_ip_address" {
 }
 
 output "kubernetes_data_device_path" {
-  value = "/dev/sdb"
+  value = local.kubernetes_data_device_path
 }
+
