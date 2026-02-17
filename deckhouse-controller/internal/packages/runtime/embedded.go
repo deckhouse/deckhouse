4bc14d416b2f32827cdcf73af3f075111c30f9f4
Stepan Paksashvili <stepan.paksashvili@flant.com>
2026-02-17 16:58:09 +0300

    [deckhouse] embedded processing
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- deckhouse-controller/internal/packages/runtime/embedded.go
+++ deckhouse-controller/internal/packages/runtime/embedded.go
@@ -1,3 +1,17 @@
+// Copyright 2026 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package runtime
 
 import (
@@ -7,7 +21,6 @@ import (
 	"path/filepath"
 	"slices"
 
-	moduletypes "github.com/deckhouse/deckhouse/deckhouse-controller/pkg/controller/moduleloader/types"
 	"gopkg.in/yaml.v3"
 
 	"github.com/deckhouse/deckhouse/deckhouse-controller/internal/packages/modules"
@@ -18,6 +31,7 @@ import (
 	taskrun "github.com/deckhouse/deckhouse/deckhouse-controller/internal/packages/runtime/tasks/run"
 	"github.com/deckhouse/deckhouse/deckhouse-controller/internal/packages/status"
 	"github.com/deckhouse/deckhouse/deckhouse-controller/internal/queue"
+	moduletypes "github.com/deckhouse/deckhouse/deckhouse-controller/pkg/controller/moduleloader/types"
 )
 
 // UpdateEmbedded reconciles an embedded (built-in) module by detecting version or settings
