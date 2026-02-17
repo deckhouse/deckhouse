ecb750443ad8131b6d0ef18503b8454cb472f181
dmitry.denisenko <dmitry.denisenko@flant.com>
2026-02-17 12:51:25 +0400

    node-controller update approval hook
    Signed-off-by: Denisenko Dmitry <dmitry.denisenko@flant.com>
    Signed-off-by: Dmitry Denisenko <dmitry.denisenko@flant.com>
    Signed-off-by: Denisenko Dmitry <dmitry.denisenko@flant.com>

--- modules/040-node-manager/images/node-controller/src/cmd/main.go
+++ modules/040-node-manager/images/node-controller/src/cmd/main.go
@@ -14,9 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-//go:generate go run ../hack/gen-controllers.go
-
-package cmd
+package main
 
 import (
 	"flag"
