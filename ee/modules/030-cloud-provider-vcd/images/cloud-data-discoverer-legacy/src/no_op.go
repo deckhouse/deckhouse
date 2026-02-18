010d2d7991408c5f9b65bd5736b740a77e196c16
Sergey Petrov <sergey.petrov@flant.com>
2026-02-18 18:45:48 +0700

    add mock files for exclusion linter error
    Signed-off-by: Sergey Petrov <sergey.petrov@flant.com>

---
+++ ee/modules/030-cloud-provider-vcd/images/cloud-data-discoverer-legacy/src/no_op.go
@@ -0,0 +1,3 @@
+// The cloud-data-discoverer-legacy project files will be added when building the image from the cloud-data-discoverer project.
+// This file was added to exclude linter errors when checking the project.
+package no_op
---
+++ ee/modules/030-cloud-provider-vcd/images/infra-controller-manager-legacy/src/no_op.go
@@ -0,0 +1,3 @@
+// The infra-controller-manager-legacy project files will be added when building the image from the infra-controller-manager project.
+// This file was added to exclude linter errors when checking the project.
+package no_op
