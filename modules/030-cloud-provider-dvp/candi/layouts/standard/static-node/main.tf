e388461c36f51b0d1189064adb87dde5dea25e58
Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>
2026-02-25 16:23:04 +0300

    add owner_Refs for additional disks
    Signed-off-by: Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>

--- modules/030-cloud-provider-dvp/candi/layouts/standard/static-node/main.tf
+++ modules/030-cloud-provider-dvp/candi/layouts/standard/static-node/main.tf
@@ -94,6 +94,24 @@ locals {
   ]
 }
 
+resource "kubernetes_owner_references_v1" "additional-disk" {
+  for_each = module.additional-disk
+
+  api_version = "virtualization.deckhouse.io/v1alpha2"
+  kind        = "VirtualDisk"
+  metadata {
+    name      = each.value.name
+    namespace = local.namespace
+  }
+
+  owner_references {
+    api_version = "virtualization.deckhouse.io/v1alpha2"
+    kind        = "VirtualMachine"
+    name        = module.master.name
+    uid         = module.master.uid
+  }
+}
+
 module "static-node" {
   source                     = "../../../terraform-modules/static-node/"
   prefix                     = local.prefix
