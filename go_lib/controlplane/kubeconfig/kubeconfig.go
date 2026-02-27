906b5b137c2ea927a73420583bae57aa0fafcd34
Maxim Mazin <maksim.mazin@flant.com>
2026-02-27 19:03:43 +0300

    [go_lib/controlplane] ready to test now
    Signed-off-by: Maxim Mazin <maksim.mazin@flant.com>

--- go_lib/controlplane/kubeconfig/kubeconfig.go
+++ go_lib/controlplane/kubeconfig/kubeconfig.go
@@ -1,9 +1,13 @@
 package kubeconfig
 
 import (
+	"bytes"
 	"crypto"
 	"crypto/x509"
+	"encoding/pem"
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/deckhouse/deckhouse/go_lib/controlplane/constants"
@@ -15,7 +19,7 @@ import (
 	"k8s.io/client-go/util/keyutil"
 )
 
-type FileSpec struct {
+type fileSpec struct {
 	ClusterName             string
 	APIServer               string
 	ClientName              string
@@ -26,12 +30,12 @@ type FileSpec struct {
 	EncryptionAlgorithm     constants.EncryptionAlgorithmType
 }
 
-func CreateControlPlaneKubeConfigFiles(options ...Option) error {
+func CreateControlPlaneKubeConfigFiles(options ...option) error {
 	log.Info("creating kubeconfig files for control-plane")
 
-	opt, err := buildOptions(options...)
+	opt, err := prepareCoreOptions(options...)
 	if err != nil {
-		return fmt.Errorf("failed to build Options: %w", err)
+		return fmt.Errorf("failed to prepare Options: %w", err)
 	}
 
 	files := []File{
@@ -44,7 +48,7 @@ func CreateControlPlaneKubeConfigFiles(options ...Option) error {
 	return createKubeConfigFiles(opt, files...)
 }
 
-func createKubeConfigFiles(opt *Options, files ...File) error {
+func createKubeConfigFiles(opt *options, files ...File) error {
 	for _, file := range files {
 		fileSpec, err := getFileSpec(file, opt)
 		if err != nil {
@@ -56,18 +60,20 @@ func createKubeConfigFiles(opt *Options, files ...File) error {
 			return fmt.Errorf("failed to build kube config for %s: %w", file, err)
 		}
 
-		if err = clientcmd.WriteToFile(*config, "filename"); err != nil {
-			
+		kubeConfigFilePath := filepath.Join(opt.OutDir, string(file))
+
+		if err := writeKubeConfigFileIfNeeded(kubeConfigFilePath, config); err != nil {
+			return err
 		}
 	}
 
 	return nil
 }
 
-func getFileSpec(kind File, opt *Options) (*FileSpec, error) {
+func getFileSpec(kind File, opt *options) (*fileSpec, error) {
 	switch kind {
 	case Admin:
-		return &FileSpec{
+		return &fileSpec{
 			ClusterName:             opt.ClusterName,
 			APIServer:               opt.ControlPlaneEndpoint,
 			ClientName:              "kubernetes-admin",
@@ -78,7 +84,7 @@ func getFileSpec(kind File, opt *Options) (*FileSpec, error) {
 			EncryptionAlgorithm:     opt.EncryptionAlgorithm,
 		}, nil
 	case SuperAdmin:
-		return &FileSpec{
+		return &fileSpec{
 			ClusterName:             opt.ClusterName,
 			APIServer:               opt.ControlPlaneEndpoint,
 			ClientName:              "kubernetes-super-admin",
@@ -89,7 +95,7 @@ func getFileSpec(kind File, opt *Options) (*FileSpec, error) {
 			EncryptionAlgorithm:     opt.EncryptionAlgorithm,
 		}, nil
 	case ControllerManager:
-		return &FileSpec{
+		return &fileSpec{
 			ClusterName:         opt.ClusterName,
 			APIServer:           opt.LocalAPIEndpoint,
 			ClientName:          "system:kube-controller-manager",
@@ -99,7 +105,7 @@ func getFileSpec(kind File, opt *Options) (*FileSpec, error) {
 			EncryptionAlgorithm: opt.EncryptionAlgorithm,
 		}, nil
 	case Scheduler:
-		return &FileSpec{
+		return &fileSpec{
 			ClusterName:         opt.ClusterName,
 			APIServer:           opt.LocalAPIEndpoint,
 			ClientName:          "system:kube-scheduler",
@@ -109,9 +115,8 @@ func getFileSpec(kind File, opt *Options) (*FileSpec, error) {
 			EncryptionAlgorithm: opt.EncryptionAlgorithm,
 		}, nil
 	case Kubelet:
-		nodeName := ""
-		return &FileSpec{
-			ClusterName:             fmt.Sprintf("%s%s", "system:node:", nodeName),
+		return &fileSpec{
+			ClusterName:             fmt.Sprintf("%s%s", "system:node:", opt.NodeName),
 			APIServer:               opt.ControlPlaneEndpoint,
 			ClientName:              "kubernetes-super-admin",
 			ClientCertOrganizations: []string{"system:masters"},
@@ -125,7 +130,7 @@ func getFileSpec(kind File, opt *Options) (*FileSpec, error) {
 	}
 }
 
-func buildConfig(spec *FileSpec) (*clientcmdapi.Config, error) {
+func buildConfig(spec *fileSpec) (*clientcmdapi.Config, error) {
 	clientCertConfig := newClientCertConfig(spec)
 
 	clientCert, clientKey, err := pki.NewCertAndKey(spec.CACert, spec.CAKey, &clientCertConfig)
@@ -163,7 +168,7 @@ func buildConfig(spec *FileSpec) (*clientcmdapi.Config, error) {
 	return config, nil
 }
 
-func newClientCertConfig(spec *FileSpec) pki.CertConfig {
+func newClientCertConfig(spec *fileSpec) pki.CertConfig {
 	return pki.CertConfig{
 		Config: certutil.Config{
 			CommonName:   spec.ClientName,
@@ -173,4 +178,82 @@ func newClientCertConfig(spec *FileSpec) pki.CertConfig {
 		NotAfter:            spec.ClientCertNotAfter,
 		EncryptionAlgorithm: spec.EncryptionAlgorithm,
 	}
+}
+
+func writeKubeConfigFileIfNeeded(kubeConfigFilePath string, config *clientcmdapi.Config) error {
+	err := validateCurrentKubeConfig(kubeConfigFilePath, config)
+	if err == nil {
+		log.Info("Using existing kubeconfig file: %q", kubeConfigFilePath)
+		return nil
+	}
+
+	log.Info("Writing new %q kubeconfig file, because current is not valid: %v", kubeConfigFilePath, err)
+
+	if err := clientcmd.WriteToFile(*config, kubeConfigFilePath); err != nil {
+		return fmt.Errorf("failed to write kubeconfig %s: %w", kubeConfigFilePath, err)
+	}
+
+	return nil
+}
+
+func validateCurrentKubeConfig(kubeConfigFilePath string, desiredConfig *clientcmdapi.Config) error {
+	if _, err := os.Stat(kubeConfigFilePath); err != nil {
+		return fmt.Errorf("kubeconfig file not found: %w", err)
+	}
+
+	currentConfig, err := clientcmd.LoadFromFile(kubeConfigFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to load kubeconfig file: %w", err)
+	}
+
+	currentCtx, ok := currentConfig.Contexts[currentConfig.CurrentContext]
+	if !ok {
+		return fmt.Errorf("current context %q not found", currentConfig.CurrentContext)
+	}
+
+	currentCluster, ok := currentConfig.Clusters[currentCtx.Cluster]
+	if !ok {
+		return fmt.Errorf("cluster %q not found", currentCtx.Cluster)
+	}
+
+	desiredCtx := desiredConfig.Contexts[desiredConfig.CurrentContext]
+	desiredCluster := desiredConfig.Clusters[desiredCtx.Cluster]
+
+	if currentCluster.Server != desiredCluster.Server {
+		return fmt.Errorf("kubeconfig address field changed: expected %s, got %s", desiredCluster.Server, currentCluster.Server)
+	}
+
+	if !bytes.Equal(bytes.TrimSpace(currentCluster.CertificateAuthorityData), bytes.TrimSpace(desiredCluster.CertificateAuthorityData)) {
+		return fmt.Errorf("CA certificate changed")
+	}
+
+	currentAuth, ok := currentConfig.AuthInfos[currentCtx.AuthInfo]
+	if !ok {
+		return fmt.Errorf("auth info %q not found", currentCtx.AuthInfo)
+	}
+
+	certData := currentAuth.ClientCertificateData
+	if len(certData) == 0 {
+		return fmt.Errorf("client-certificate-data field is empty")
+	}
+
+	block, _ := pem.Decode(certData)
+	if block == nil || len(block.Bytes) == 0 {
+		return fmt.Errorf("cannot PEM decode client-certificate-data")
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return fmt.Errorf("cannot parse certificate from client-certificate-data: %w", err)
+	}
+
+	if certificateExpiresSoon(cert, 30*24*time.Hour) {
+		return fmt.Errorf("client certificate is expiring in less than 30 days")
+	}
+
+	return nil
+}
+
+func certificateExpiresSoon(cert *x509.Certificate, threshold time.Duration) bool {
+	return time.Now().Add(threshold).After(cert.NotAfter)
 }
--- go_lib/controlplane/kubeconfig/options.go
+++ go_lib/controlplane/kubeconfig/options.go
@@ -1,40 +1,46 @@
 package kubeconfig
 
 import (
+	"crypto"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"strings"
+
 	"github.com/deckhouse/deckhouse/go_lib/controlplane/constants"
+	"github.com/deckhouse/deckhouse/go_lib/controlplane/pki"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type Options struct {
+type options struct {
+	// core options
 	OutDir                    string
 	ClusterName               string
 	CertificatesDir           string
 	LocalAPIEndpoint          string
 	ControlPlaneEndpoint      string
-	NodeNamePath              string
 	EncryptionAlgorithm       constants.EncryptionAlgorithmType
 	CertificateValidityPeriod *metav1.Duration
 	CertProvider              CertProvider
+
+	// specific options
+	NodeName string
 }
 
-func buildOptions(options ...Option) (opt *Options, err error) {
-	opt.enrichWithOptions(options...)
+func prepareCoreOptions(opts ...option) (*options, error) {
+	var err error
+	opt := &options{}
 
-	if err = opt.setDefaultsIfNeeded(); err != nil {
-		return nil, err
-	}
-
-	return opt, nil
-}
-
-func (opt *Options) enrichWithOptions(options ...Option) {
-	for _, option := range options {
+	for _, option := range opts {
 		option(opt)
 	}
-}
 
-func (opt *Options) setDefaultsIfNeeded() error {
-	var err error
+	if opt.OutDir == "" {
+		opt.OutDir = DefaultOutDir
+	}
 
 	if opt.ClusterName == "" {
 		opt.ClusterName = DefaultClusterName
@@ -44,26 +50,149 @@ func (opt *Options) setDefaultsIfNeeded() error {
 		opt.CertificatesDir = DefaultCertificatesDir
 	}
 
+	if opt.LocalAPIEndpoint == "" {
+		opt.LocalAPIEndpoint = DefaultLocalAPIEndpoint
+	}
+
+	if opt.ControlPlaneEndpoint == "" {
+		controlPlaneEndpoint, err := os.ReadFile(constants.DiscoveredNodeIPPath)
+		if err != nil || len(controlPlaneEndpoint) == 0 {
+			return nil, fmt.Errorf("failed to read discovered control plane endpoint: %w", err)
+		}
+
+		opt.ControlPlaneEndpoint = fmt.Sprintf("https://%s:6443", strings.TrimSpace(string(controlPlaneEndpoint)))
+	}
+
 	if opt.EncryptionAlgorithm == "" {
 		opt.EncryptionAlgorithm = constants.EncryptionAlgorithmRSA2048
 	}
 
 	if opt.CertProvider == nil {
-		opt.CertProvider, err = newDiskCertProvider(opt.CertificatesDir, opt.CertificateValidityPeriod)
+		opt.CertProvider, err = newDefaultCertProvider(opt.CertificatesDir, opt.CertificateValidityPeriod)
 
 		if err != nil {
-			return err
+			return nil, err
 		}
+	}
+
+	return opt, nil
+}
+
+func (opt *options) ensureNodeNameProvided() error {
+	if opt.NodeName == "" {
+		name, err := os.ReadFile(constants.DiscoveredNodeNamePath)
+		if err != nil || len(name) == 0 {
+			return fmt.Errorf("failed to read discovered node name: %w", err)
+		}
+		opt.NodeName = strings.TrimSpace(string(name))
 	}
 
 	return nil
 }
 
-type Option func(*Options)
+type option func(*options)
+
+// WithOutDir is an option to set the output directory.
+func WithOutDir(outDir string) option {
+	return func(o *options) {
+		o.OutDir = outDir
+	}
+}
 
 // WithClusterName is an option to set the cluster name.
-func WithClusterName(clusterName string) Option {
-	return func(o *Options) {
+func WithClusterName(clusterName string) option {
+	return func(o *options) {
 		o.ClusterName = clusterName
 	}
+}
+
+// WithCertificatesDir is an option to set the certificates directory.
+func WithCertificatesDir(certificatesDir string) option {
+	return func(o *options) {
+		o.CertificatesDir = certificatesDir
+	}
+}
+
+// WithLocalAPIEndpoint is an option to set the local API endpoint.
+func WithLocalAPIEndpoint(localAPIEndpoint string) option {
+	return func(o *options) {
+		o.LocalAPIEndpoint = localAPIEndpoint
+	}
+}
+
+// WithControlPlaneEndpoint is an option to set the control plane endpoint.
+func WithControlPlaneEndpoint(controlPlaneEndpoint string) option {
+	return func(o *options) {
+		o.ControlPlaneEndpoint = controlPlaneEndpoint
+	}
+}
+
+// WithNodeNamePath is an option to set the node name path.
+func WithNodeNamePath(nodeName string) option {
+	return func(o *options) {
+		o.NodeName = nodeName
+	}
+}
+
+// WithEncryptionAlgorithm is an option to set the encryption algorithm.
+func WithEncryptionAlgorithm(encryptionAlgorithm constants.EncryptionAlgorithmType) option {
+	return func(o *options) {
+		o.EncryptionAlgorithm = encryptionAlgorithm
+	}
+}
+
+// WithCertificateValidityPeriod is an option to set the certificate validity period.
+func WithCertificateValidityPeriod(certificateValidityPeriod *metav1.Duration) option {
+	return func(o *options) {
+		o.CertificateValidityPeriod = certificateValidityPeriod
+	}
+}
+
+// WithCertProvider is an option to set the certificate provider.
+func WithCertProvider(certProvider CertProvider) option {
+	return func(o *options) {
+		o.CertProvider = certProvider
+	}
+}
+
+type CertProvider interface {
+	NotAfter() time.Time
+	CACert() *x509.Certificate
+	CAKey() crypto.Signer
+}
+
+type defaultCertProvider struct {
+	certStartTime  time.Time
+	validityPeriod *metav1.Duration
+	caCert         *x509.Certificate
+	caKey          crypto.Signer
+}
+
+func newDefaultCertProvider(certificatesDir string, validityPeriod *metav1.Duration) (*defaultCertProvider, error) {
+	caCert, caKey, err := pki.TryLoadCertAndKeyFromDisk(certificatesDir, constants.CACertAndKeyBaseName)
+	if os.IsNotExist(errors.Unwrap(err)) {
+		return nil, fmt.Errorf("the CA files do not exist in %s: %w", certificatesDir, err)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("the CA files couldn't be loaded from %s: %w", certificatesDir, err)
+	}
+
+	return &defaultCertProvider{
+		certStartTime:  time.Now().UTC(),
+		validityPeriod: validityPeriod,
+		caCert:         caCert,
+		caKey:          caKey,
+	}, nil
+}
+
+func (cp *defaultCertProvider) NotAfter() time.Time {
+	return cp.certStartTime.Add(cp.validityPeriod.Duration)
+}
+
+func (cp *defaultCertProvider) CACert() *x509.Certificate {
+	return cp.caCert
+}
+
+func (cp *defaultCertProvider) CAKey() crypto.Signer {
+	return cp.caKey
 }
