46ae389756e5dd6cd44141c8eb25043840b557c4
borg-z <me@zykov.tatar>
2026-03-02 11:13:14 +0300

    make generate
    Signed-off-by: borg-z <me@zykov.tatar>

--- deckhouse-controller/pkg/client/informers/externalversions/deckhouse.io/v1alpha1/deckhouserelease.go
+++ deckhouse-controller/pkg/client/informers/externalversions/deckhouse.io/v1alpha1/deckhouserelease.go
@@ -58,13 +58,25 @@ func NewFilteredDeckhouseReleaseInformer(client versioned.Interface, resyncPerio
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.DeckhouseV1alpha1().DeckhouseReleases().List(context.TODO(), options)
+				return client.DeckhouseV1alpha1().DeckhouseReleases().List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.DeckhouseV1alpha1().DeckhouseReleases().Watch(context.TODO(), options)
+				return client.DeckhouseV1alpha1().DeckhouseReleases().Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.DeckhouseV1alpha1().DeckhouseReleases().List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.DeckhouseV1alpha1().DeckhouseReleases().Watch(ctx, options)
 			},
 		},
 		&apisdeckhouseiov1alpha1.DeckhouseRelease{},
