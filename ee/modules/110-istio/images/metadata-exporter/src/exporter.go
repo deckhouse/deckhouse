b403b146bacd95e7ed5e3786f8fa2c3be196fc13
Denis Meshkov <mr.zuwzuw@gmail.com>
2026-03-04 15:56:51 +0300

    [istio] Metadata-exporter add custom domain
    Signed-off-by: Denis Meshkov <mr.zuwzuw@gmail.com>

--- ee/modules/110-istio/images/metadata-exporter/src/exporter.go
+++ ee/modules/110-istio/images/metadata-exporter/src/exporter.go
@@ -59,7 +59,10 @@ func New(namespace string, labelSelector string) (*Exporter, error) {
 	// Get environments
 
 	inlet := os.Getenv("INLET")
-	clusterDomain := os.Getenv("CLUSTER_DOMAIN")
+	clusterDomain := os.Getenv("CUSTOM_METADATA_EXPORTER_DOMAIN")
+	if clusterDomain == "" {
+		clusterDomain = os.Getenv("CLUSTER_DOMAIN")
+	}
 	clusterUUID := os.Getenv("CLUSTER_UUID")
 	multicluserNetworkName := os.Getenv("MULTICLUSTER_NETWORK_NAME")
 	multiclusterAPIHost := os.Getenv("MULTICLUSTER_API_HOST")
