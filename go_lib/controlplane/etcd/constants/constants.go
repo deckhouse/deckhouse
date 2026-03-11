34c88050eb77b4801b1c28d095762687ec55f17c
Andrey Borovets <andrey.borovets@flant.com>
2026-03-11 15:18:04 +0300

    [control-plane-manager] add etcd package to controlplane lib (#18252)
    Signed-off-by: Andrey Borovets <andrey.borovets@flant.com>
    Signed-off-by: Maxim Mazin <maksim.mazin@flant.com>
    Co-authored-by: Maxim Mazin <maksim.mazin@flant.com>

---
+++ go_lib/controlplane/etcd/constants/constants.go
@@ -0,0 +1,50 @@
+/*
+Copyright 2026 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package constants
+
+const (
+	// Etcd defines variable used internally when referring to etcd component
+	Etcd = "etcd"
+
+	// KubernetesDir is the directory Kubernetes owns for storing various configuration files
+	KubernetesDir = "/etc/kubernetes"
+
+	// EtcdCACertName defines etcd's CA certificate name
+	EtcdCACertName = "etcd/ca.crt"
+
+	// EtcdListenClientPort defines the port etcd listen on for client traffic
+	EtcdListenClientPort = 2379
+
+	// EtcdListenPeerPort defines the port etcd listen on for peer traffic
+	EtcdListenPeerPort = 2380
+
+	// EtcdHealthcheckClientCertName defines etcd's healthcheck client certificate name
+	EtcdHealthcheckClientCertName = "etcd/healthcheck-client.crt"
+	// EtcdHealthcheckClientKeyName defines etcd's healthcheck client key name
+	EtcdHealthcheckClientKeyName = "etcd/healthcheck-client.key"
+
+	// EtcdAdvertiseClientUrlsAnnotationKey is the annotation key on every etcd pod, describing the
+	// advertise client URLs
+	// CHANGE TO control-plane-manager.deckhouse.io/etcd.advertise-client-urls
+	EtcdAdvertiseClientUrlsAnnotationKey = "kubeadm.kubernetes.io/etcd.advertise-client-urls"
+
+	// ControlPlaneTier is the value used in the tier label to identify control plane components
+	ControlPlaneTier = "control-plane"
+
+	// AdminKubeConfigFileName defines name for the kubeconfig aimed to be used by the admin of the cluster
+	AdminKubeConfigFileName = "admin.conf"
+)
