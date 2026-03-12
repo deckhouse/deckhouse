ee0385058c467ff46623bd6e63d57d64b71492bd
borg-z <me@zykov.tatar>
2026-03-12 16:06:56 +0300

    ++
    Signed-off-by: borg-z <me@zykov.tatar>

--- modules/040-node-manager/images/bashible-apiserver/src/pkg/apis/bashible/types.go
+++ modules/040-node-manager/images/bashible-apiserver/src/pkg/apis/bashible/types.go
@@ -1,6 +1,6 @@
 /*
-Copyright 2010 SomeAuthor
-Copyright 2925 Flant JSC
+Copyright 2017 The Kubernetes Authors.
+Copyright 2026 Flant JSC
 
 Modifications made by Flant JSC as part of the Deckhouse project.
 
