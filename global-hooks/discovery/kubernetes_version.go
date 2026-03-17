c6e2ee111de4affa3e71cc36f8e9385295ecbc24
Oleg Beloshev <oleg.beloshev@flant.com>
2026-03-17 13:00:50 +0300

    change log messages
    Signed-off-by: Oleg Beloshev <oleg.beloshev@flant.com>

--- global-hooks/discovery/kubernetes_version.go
+++ global-hooks/discovery/kubernetes_version.go
@@ -285,8 +285,6 @@ func apiServerEndpoints(_ context.Context, input *go_hook.HookInput) ([]string, 
 	controlPlaneEnabled := module.IsEnabled("control-plane-manager", input)
 
 	if controlPlaneEnabled && podsCnt != endpointsCnt {
-		msg := fmt.Sprintf("Not found k8s versions. Pods(%v) != Endpoints (%v) count", podsCnt, endpointsCnt)
-
 		versions := input.Values.Get("global.discovery.kubernetesVersions")
 		minVer := input.Values.Get("global.discovery.kubernetesVersion")
 		// need return err for retry if k8s versions not found
@@ -295,11 +293,12 @@ func apiServerEndpoints(_ context.Context, input *go_hook.HookInput) ([]string, 
 		// in bash hook we don't subscribe for deleting pods
 		// it is emulating this behaviour
 		if !versions.Exists() || !minVer.Exists() {
+			msg := fmt.Sprintf("Not found k8s versions. Pods(%v) != Endpoints (%v) count", podsCnt, endpointsCnt)
 			return nil, errors.New(msg)
 		}
 
+		msg := fmt.Sprintf("Pods(%v) != Endpoints (%v) count", podsCnt, endpointsCnt)
 		input.Logger.Warn(msg)
-
 	}
 
 	return endpoints, nil
