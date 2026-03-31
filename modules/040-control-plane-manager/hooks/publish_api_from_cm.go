4fff36f7d6d4ff74af07678f832eac650c3ae7ac
Oleg Beloshev <oleg.beloshev@flant.com>
2026-03-31 11:30:49 +0300

    fix cm and alert desc
    Signed-off-by: Oleg Beloshev <oleg.beloshev@flant.com>

--- modules/040-control-plane-manager/hooks/publish_api_from_cm.go
+++ modules/040-control-plane-manager/hooks/publish_api_from_cm.go
@@ -94,6 +94,7 @@ func filterPublishAPIConfigMap(unstructured *unstructured.Unstructured) (go_hook
 func handlePublishAPIConfig(_ context.Context, input *go_hook.HookInput) error {
 	if input.ConfigValues.Get("controlPlaneManager.apiserver.publishAPI.ingress").Exists() {
 		input.Logger.Info("Publish API ingress settings are set in moduleconfig control-plane-manager, setting values from said moduleconfig")
+		fmt.Println(input.ConfigValues.Get("controlPlaneManager.apiserver.publishAPI.ingress").Value())
 		input.Values.Set("controlPlaneManager.apiserver.publishAPI.ingress", input.ConfigValues.Get("controlPlaneManager.apiserver.publishAPI.ingress").Value())
 		return nil
 	}
