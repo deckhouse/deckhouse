7e3376b5d297681682eab3bc8ae79a50894ca34a
Artem Darmanyan <artem.darmanyan@flant.com>
2026-04-06 11:20:08 +0300

    [cloud-provider-vcd] fixed tests
    Signed-off-by: Artem Darmanyan <artem.darmanyan@flant.com>

--- ee/modules/030-cloud-provider-vcd/template_tests/module_test.go
+++ ee/modules/030-cloud-provider-vcd/template_tests/module_test.go
@@ -352,7 +352,7 @@ spec:
 - --cloud-provider=vmware-cloud-director
 - --allow-untagged-cloud=true
 - --configure-cloud-routes=false
-- --controllers=cloud-node,cloud-node-lifecycle
+- --controllers=cloud-node-controller,cloud-node-lifecycle-controller
 - --v=4`))
 			Expect(ccmDeployment.Field("spec.template.spec.containers.0.volumeMounts").String()).To(MatchYAML(`
 - mountPath: /etc/cloud
@@ -622,7 +622,7 @@ node-role.deckhouse.io/control-plane: ""`))
 - --cloud-provider=vmware-cloud-director
 - --allow-untagged-cloud=true
 - --configure-cloud-routes=false
-- --controllers=cloud-node,cloud-node-lifecycle
+- --controllers=cloud-node-controller,cloud-node-lifecycle-controller
 - --v=4
 `))
 		})
@@ -649,7 +649,7 @@ node-role.deckhouse.io/control-plane: ""`))
 - --cloud-provider=vmware-cloud-director
 - --allow-untagged-cloud=true
 - --configure-cloud-routes=false
-- --controllers=cloud-node,cloud-node-lifecycle,service
+- --controllers=cloud-node-controller,cloud-node-lifecycle-controller,service-lb-controller
 - --v=4
 `))
 		})
