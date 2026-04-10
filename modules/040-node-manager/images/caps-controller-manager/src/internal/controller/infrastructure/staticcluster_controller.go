0225680a44e270f0589581dfb67bb118498ed6f3
Artem Darmanyan <artem.darmanyan@flant.com>
2026-04-10 15:36:07 +0300

    [node-manager] migrate caps-controller-manager to v1beta2 API
    Signed-off-by: Artem Darmanyan <artem.darmanyan@flant.com>

--- modules/040-node-manager/images/caps-controller-manager/src/internal/controller/infrastructure/staticcluster_controller.go
+++ modules/040-node-manager/images/caps-controller-manager/src/internal/controller/infrastructure/staticcluster_controller.go
@@ -33,7 +33,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	infrav1 "caps-controller-manager/api/infrastructure/v1alpha1"
+	infrav1 "caps-controller-manager/api/infrastructure/v1beta2"
 )
 
 // StaticClusterReconciler reconciles a StaticCluster object
