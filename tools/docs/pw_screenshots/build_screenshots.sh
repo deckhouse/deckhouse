effb571aeac1158cf5152a73216533e8a8fdc655
Zhbert <konstantin.nezhbert@flant.com>
2026-04-15 12:11:46 +0300

    Updates
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- tools/docs/pw_screenshots/build_screenshots.sh
+++ tools/docs/pw_screenshots/build_screenshots.sh
@@ -8,4 +8,11 @@ docker run --rm -it --network host \
   -v "$REPO_ROOT:/work" \
   -w /work/tools/docs/pw_screenshots \
   d8_playwrite \
-  bash -lc "pytest get_screenshots.py -v"
+  bash -lc "pytest get_screenshots.py -v && if [ -d /work/tools/docs/pw_screenshots/screenshots ]; then chown -R $(id -u):$(id -g) /work/tools/docs/pw_screenshots/screenshots; fi"
+
+SRC_SCREENSHOTS="$REPO_ROOT/tools/docs/pw_screenshots/screenshots"
+DST_IMAGES="$REPO_ROOT/docs/site/images/gs/installer"
+
+mkdir -p "$DST_IMAGES"
+cp -a "$SRC_SCREENSHOTS/." "$DST_IMAGES/"
+rm -rf "$SRC_SCREENSHOTS"
