7a2dbf65946db8945077801e54a26e2934c6ed96
Zhbert <konstantin.nezhbert@flant.com>
2026-04-15 12:54:52 +0300

    Updates
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- docs/site/assets/js/details.js
+++ docs/site/assets/js/details.js
@@ -1,5 +1,16 @@
 $( document ).ready(function() {
   $('.details__summary').on('click tap', function() {
-    $(this).closest('.details').toggleClass('active');
+    var $details = $(this).closest('.details');
+    $details.toggleClass('active');
+    if ($details.hasClass('active')) {
+      requestAnimationFrame(function() {
+        $details.find('.slick-initialized').each(function() {
+          $(this).slick('setPosition');
+        });
+        if (window.refreshInstallerCarousels) {
+          window.refreshInstallerCarousels();
+        }
+      });
+    }
   });
 });
