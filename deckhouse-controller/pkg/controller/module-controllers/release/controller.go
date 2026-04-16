fcd3269149615de7a275b84b6d90a3a4c7588936
Artem Kuleshov <artem.kuleshov@flant.com>
2026-04-16 16:20:10 +0300

    [deckhouse] fix
    Signed-off-by: Artem Kuleshov <artem.kuleshov@flant.com>

--- deckhouse-controller/pkg/controller/module-controllers/release/controller.go
+++ deckhouse-controller/pkg/controller/module-controllers/release/controller.go
@@ -1164,6 +1164,7 @@ func (r *reconciler) runReleaseDeploy(ctx context.Context, release *v1alpha1.Mod
 	err = ctrlutils.UpdateWithRetry(ctx, r.client, release, func() error {
 		annotations := map[string]string{
 			v1alpha1.ModuleReleaseAnnotationIsUpdating: "true",
+			v1alpha1.ModuleReleaseAnnotationNotified:   "true",
 		}
 
 		if len(release.Annotations) == 0 {
