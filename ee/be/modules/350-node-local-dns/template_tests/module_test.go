84036c83e11708b3f482015632431afabd1f5df8
Maksim Garmonov <maksim.garmonov@flant.com>
2026-04-20 15:43:56 +0300

    [node-local-dns] Add option to disable ip resolve via IPv6
    Signed-off-by: Maksim Garmonov <maksim.garmonov@flant.com>

--- ee/be/modules/350-node-local-dns/template_tests/module_test.go
+++ ee/be/modules/350-node-local-dns/template_tests/module_test.go
@@ -6,6 +6,7 @@ Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https
 package template_tests
 
 import (
+	"strings"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -31,6 +32,44 @@ var _ = Describe("Module :: node-local-dns :: helm template", func() {
 		It("Should successful render helm", func() {
 			hec.HelmRender()
 			Expect(hec.RenderError).ShouldNot(HaveOccurred())
+		})
+
+		It("Should not render IPv6 disabling templates by default", func() {
+			hec.HelmRender()
+			Expect(hec.RenderError).ShouldNot(HaveOccurred())
+
+			configMap := hec.KubernetesResource("ConfigMap", "kube-system", "node-local-dns")
+			Expect(configMap.Exists()).To(BeTrue())
+
+			corefile := configMap.Field("data.Corefile").String()
+			Expect(strings.Contains(corefile, "template IN AAAA .")).To(BeFalse())
+			Expect(strings.Contains(corefile, "template IN PTR ip6.arpa")).To(BeFalse())
+		})
+
+		It("Should render IPv6 disabling templates when enabled", func() {
+			hec.ValuesSet("nodeLocalDns.disableIPv6", true)
+			hec.HelmRender()
+			Expect(hec.RenderError).ShouldNot(HaveOccurred())
+
+			configMap := hec.KubernetesResource("ConfigMap", "kube-system", "node-local-dns")
+			Expect(configMap.Exists()).To(BeTrue())
+
+			corefile := configMap.Field("data.Corefile").String()
+			Expect(strings.Contains(corefile, "template IN AAAA . {\n          rcode NOERROR\n      }")).To(BeTrue())
+			Expect(strings.Contains(corefile, "template IN PTR ip6.arpa {\n          rcode NXDOMAIN\n      }")).To(BeTrue())
+		})
+
+		It("Should bind coredns to IPv4 only with cilium when IPv6 is disabled", func() {
+			hec.ValuesSetFromYaml("global.enabledModules", `["cni-cilium"]`)
+			hec.ValuesSet("nodeLocalDns.disableIPv6", true)
+			hec.HelmRender()
+			Expect(hec.RenderError).ShouldNot(HaveOccurred())
+
+			configMap := hec.KubernetesResource("ConfigMap", "kube-system", "node-local-dns")
+			Expect(configMap.Exists()).To(BeTrue())
+
+			corefile := configMap.Field("data.Corefile").String()
+			Expect(strings.Contains(corefile, "bind 0.0.0.0")).To(BeTrue())
 		})
 
 	})
