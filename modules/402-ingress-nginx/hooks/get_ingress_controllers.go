b84ae9d4d4423460508f10c7726314c524c5f055
Mikhail Scherba <mikhail.scherba@flant.com>
2026-04-28 12:33:20 +0300

    fix lint
    Signed-off-by: Mikhail Scherba <mikhail.scherba@flant.com>

--- modules/402-ingress-nginx/hooks/get_ingress_controllers.go
+++ modules/402-ingress-nginx/hooks/get_ingress_controllers.go
@@ -124,6 +124,10 @@ func applyControllerFilter(obj *unstructured.Unstructured) (go_hook.FilterResult
 	setDefaultEmptyObject("memory", vpa)
 
 	vpamode, _, err := unstructured.NestedString(vpa, "mode")
+	if err != nil {
+		return nil, fmt.Errorf("cannot get resourcesManagement.vpa.mode from ingress controller spec: %v", err)
+	}
+
 	if vpamode == "" {
 		err := unstructured.SetNestedField(vpa, "Initial", "mode")
 		if err != nil {
