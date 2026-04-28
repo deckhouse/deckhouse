15fb11393aa2a5d1c81c80adae88bc725f483c96
YuryLysov <yuriy.lysov@flant.com>
2026-04-28 14:29:54 +0400

    [dhctl] Refactor bootstrap command (#19063)
    Signed-off-by: YuryLysov <yuriy.lysov@flant.com>

--- dhctl/pkg/preflight/suites/cloud_post.go
+++ dhctl/pkg/preflight/suites/cloud_post.go
@@ -15,19 +15,20 @@
 package suites
 
 import (
+	libcon "github.com/deckhouse/lib-connection/pkg"
+
 	"github.com/deckhouse/deckhouse/dhctl/pkg/config"
 	preflight "github.com/deckhouse/deckhouse/dhctl/pkg/preflight"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/preflight/checks"
-	"github.com/deckhouse/deckhouse/dhctl/pkg/system/node"
 )
 
 type PostCloudDeps struct {
-	MetaConfig *config.MetaConfig
-	Node       node.Interface
+	MetaConfig  *config.MetaConfig
+	SSHProvider libcon.SSHProvider
 }
 
 func NewPostCloudSuite(deps PostCloudDeps) preflight.Suite {
 	return preflight.NewSuite(
-		checks.CloudAPIAccess(deps.MetaConfig, deps.Node),
+		checks.CloudAPIAccess(deps.MetaConfig, deps.SSHProvider),
 	)
 }
