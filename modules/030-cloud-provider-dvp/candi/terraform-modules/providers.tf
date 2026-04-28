9f33a51ae888f2c05395d317f092dbaec03796bb
Alexey Voronov <alexey.voronov@flant.com>
2026-04-29 01:19:12 +0700

    [cloud-provider-dvp] add golang models, minor fixes (#20421) try to not overide destination directory copy module openapi to another dir to not override candi specs move instance class openapi schema add conversion in hook and fix tests make conversions for config values add converions documentation fix conversions refactor migration functions migrate providerclusterconfiguration fix linter issues minor fixes fix linter issue fix tests add conditional components deployment fix dmt-lint issue realted with rbac in csi add untracked review make generate update config-values spec move ipaddresses generation to moduleconfig fix conversions fix values setter fix credential secret generation tests fix tests fix dhctl schema fix add migration layer to terraform modules rewrite migration module fix terraform migration outputs
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>
    Signed-off-by: borg-z <me@zykov.tatar>

--- modules/030-cloud-provider-dvp/candi/terraform-modules/providers.tf
+++ modules/030-cloud-provider-dvp/candi/terraform-modules/providers.tf
@@ -12,7 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+locals {
+  _has_pcc                = var.providerClusterConfiguration != null
+  _has_credential_secret  = var.secrets != null && length(var.secrets) > 0
+  _has_node_groups        = var.nodeGroups != null && length(var.nodeGroups) > 0
+  _has_instance_classes   = var.instanceClasses != null && length(var.instanceClasses) > 0
+  _new_resources_complete = local._has_credential_secret && local._has_node_groups && local._has_instance_classes
+  _use_pcc                = local._has_pcc && !local._new_resources_complete
+
+  _pcc_kubeconfig = try(var.providerClusterConfiguration.provider.kubeconfigDataBase64, "")
+  _secret_kubeconfig = try(
+    [
+      for name, s in var.secrets : s.stringData.secret
+      if try(s.stringData.secret, null) != null && try(s.type, "") == "cloud-provider.deckhouse.io/credentials"
+    ][0],
+    ""
+  )
+  _kubeconfig_base64 = local._use_pcc ? local._pcc_kubeconfig : local._secret_kubeconfig
+}
+
 provider "kubernetes" {
-  config_data_base64 = var.providerClusterConfiguration.provider.kubeconfigDataBase64
+  config_data_base64 = local._kubeconfig_base64
   # insecure           = true
 }
