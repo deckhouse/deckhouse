345d925d2c0603899b00408960217eed51865c03
Vladislav Denisov <vlad144denisov@gmail.com>
2026-04-28 17:37:27 +0300

    +
    +
    
    Signed-off-by: Vladislav Denisov <vlad144denisov@gmail.com>

--- modules/030-cloud-provider-yandex/images/capyandex-controller-manager/src/cmd/main.go
+++ modules/030-cloud-provider-yandex/images/capyandex-controller-manager/src/cmd/main.go
@@ -62,7 +62,9 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-	ycClient, err := yc.NewClient(ctrl.SetupSignalHandler(), os.Getenv("YC_SA_KEY_JSON"))
+	ctx := ctrl.SetupSignalHandler()
+
+	ycClient, err := yc.NewClient(ctx, os.Getenv("YC_SA_KEY_JSON"))
 	if err != nil {
 		setupLog.Error(err, "unable to initialize YC client")
 		os.Exit(1)
@@ -173,7 +175,7 @@ func main() {
 	}
 
 	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
