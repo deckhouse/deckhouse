0f68cf60916b45d1cb0fc14de9a8217fa99ba816
Artem Kladov <artem.kladov@flant.com>
2026-04-29 22:52:58 +0300

    Updates
    Signed-off-by: Artem Kladov <artem.kladov@flant.com>

--- docs/site/assets/js/filter.js
+++ docs/site/assets/js/filter.js
@@ -104,7 +104,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  const uppercaseAcronyms = { 'ml': 'ML', 'ai': 'AI', 'ci': 'CI', 'cd': 'CD', 'api': 'API' , 'ssdcl': 'SSDLC' };
+  const uppercaseAcronyms = { 'ml': 'ML', 'ai': 'AI', 'ci': 'CI', 'cd': 'CD', 'api': 'API' , 'ssdlc': 'SSDLC' };
 
   function capitalizeWords(value) {
     return value.trim().split(/(\s+|(?=[/])|(?<=[/]))/).map(part => {
