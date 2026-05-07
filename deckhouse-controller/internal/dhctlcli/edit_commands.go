42535b5748f41882aa669a302b4a5c81237494e1
Artem Kuleshov <41912276+Fkuloff@users.noreply.github.com>
2026-05-07 08:35:24 -0700

    [deckhouse] decouple from dhctl/cmd/commands (#19624)
    Signed-off-by: Artem Kuleshov <artem.kuleshov@flant.com>

---
+++ deckhouse-controller/internal/dhctlcli/edit_commands.go
@@ -0,0 +1,120 @@
+// Copyright 2021 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Mirror of dhctl/cmd/dhctl/commands/edit.go.
+// Drift is enforced by tools/check-dhctl-cmd-drift.sh.
+
+package dhctlcli
+
+import (
+	"fmt"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	"github.com/deckhouse/deckhouse/dhctl/pkg/app"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/kpcontext"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/kubernetes/client"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/operations"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/system/providerinitializer"
+)
+
+func connectionFlags(parent *kingpin.CmdClause) {
+	app.DefineKubeFlags(parent)
+	app.DefineSSHFlags(parent, nil)
+	app.DefineBecomeFlags(parent)
+}
+
+func baseEditConfigCMD(parent *kingpin.CmdClause, name, secret, dataKey string) *kingpin.CmdClause {
+	cmd := parent.Command(name, fmt.Sprintf("Edit %s in Kubernetes cluster.", name))
+	app.DefineEditorConfigFlags(cmd)
+	app.DefineSanityFlags(cmd)
+
+	return cmd.Action(func(c *kingpin.ParseContext) error {
+		ctx := kpcontext.ExtractContext(c)
+
+		params, err := app.DefaultProviderParams()
+		if err != nil {
+			return err
+		}
+		sshProviderInitializer, kubeProvider, err := providerinitializer.GetProviders(
+			ctx,
+			params,
+			providerinitializer.WithKubeFlagsDefined(app.KubeFlagsDefined()),
+			providerinitializer.WithRequiredKubeProvider(),
+		)
+		if err != nil {
+			return err
+		}
+		if kubeProvider == nil {
+			return fmt.Errorf("kubernetes provider is not initialized")
+		}
+		if sshProviderInitializer != nil {
+			defer sshProviderInitializer.Cleanup(ctx) //nolint:errcheck // best-effort cleanup, mirrors dhctl source
+		}
+
+		kube, err := kubeProvider.Client(ctx)
+		if err != nil {
+			return err
+		}
+		kubeCl := &client.KubernetesClient{KubeClient: kube}
+
+		return operations.SecretEdit(
+			ctx,
+			kubeCl,
+			name, "kube-system", secret, dataKey, map[string]string{
+				"name": name,
+			},
+			app.GetDirConfig(),
+		)
+	})
+}
+
+func DefineEditCommands(parent *kingpin.CmdClause, wConnFlags bool) {
+	clusterCmd := DefineEditClusterConfigurationCommand(parent)
+	providerCmd := DefineEditProviderClusterConfigurationCommand(parent)
+	staticCmd := DefineEditStaticClusterConfigurationCommand(parent)
+
+	if wConnFlags {
+		connectionFlags(clusterCmd)
+		connectionFlags(providerCmd)
+		connectionFlags(staticCmd)
+	}
+}
+
+func DefineEditClusterConfigurationCommand(parent *kingpin.CmdClause) *kingpin.CmdClause {
+	return baseEditConfigCMD(
+		parent,
+		"cluster-configuration",
+		"d8-cluster-configuration",
+		"cluster-configuration.yaml",
+	)
+}
+
+func DefineEditProviderClusterConfigurationCommand(parent *kingpin.CmdClause) *kingpin.CmdClause {
+	return baseEditConfigCMD(
+		parent,
+		"provider-cluster-configuration",
+		"d8-provider-cluster-configuration",
+		"cloud-provider-cluster-configuration.yaml",
+	)
+}
+
+func DefineEditStaticClusterConfigurationCommand(parent *kingpin.CmdClause) *kingpin.CmdClause {
+	return baseEditConfigCMD(
+		parent,
+		"static-cluster-configuration",
+		"d8-static-cluster-configuration",
+		"static-cluster-configuration.yaml",
+	)
+}
