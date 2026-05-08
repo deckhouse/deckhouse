b6f500804fd5a9470dd1c76ad2ac83cb52e4b69a
Sergey Petrov <sergey.petrov@flant.com>
2026-05-08 14:30:12 +0700

    Delete annotation from template to vm
    Signed-off-by: Sergey Petrov <sergey.petrov@flant.com>

--- ee/se-plus/modules/030-cloud-provider-vsphere/candi/terraform-modules/static-node/main.tf
+++ ee/se-plus/modules/030-cloud-provider-vsphere/candi/terraform-modules/static-node/main.tf
@@ -157,7 +157,6 @@ resource "vsphere_virtual_machine" "node" {
   memory_share_count = lookup(local.runtime_options, "memoryShares", null)
 
   extra_config = local.vm_extra_config
-  annotation = data.vsphere_virtual_machine.template.annotation
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
