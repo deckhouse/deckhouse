e13ea9596aa4d03c18afc61b0efae098b5258208
Artem Kladov <artem.kladov@flant.com>
2026-05-12 06:55:28 +0300

    [docs] Update copyright headers
    Signed-off-by: Artem Kladov <artem.kladov@flant.com>

--- tools/docs/pdf/get_pdf_page.py
+++ tools/docs/pdf/get_pdf_page.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Copyright 2025 Flant JSC
+# Copyright 2026 Flant JSC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
