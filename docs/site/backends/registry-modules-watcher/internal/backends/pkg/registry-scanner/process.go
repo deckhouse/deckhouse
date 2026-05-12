e77e326954485ee162a174f043dc3801c4272c53
Ruslan Gorbunov <ruslan.gorbunov@flant.com>
2026-05-12 17:06:15 +0300

    [chore] reorder imports in registry-scanner/process.go
    Signed-off-by: Ruslan Gorbunov <ruslan.gorbunov@flant.com>

--- docs/site/backends/registry-modules-watcher/internal/backends/pkg/registry-scanner/process.go
+++ docs/site/backends/registry-modules-watcher/internal/backends/pkg/registry-scanner/process.go
@@ -28,13 +28,13 @@ import (
 
 	crv1 "github.com/google/go-containerregistry/pkg/v1"
 	"gopkg.in/yaml.v3"
+
+	"github.com/deckhouse/deckhouse/pkg/log"
+	"github.com/deckhouse/module-sdk/pkg/dependency/cr"
+
 	"registry-modules-watcher/internal"
 	"registry-modules-watcher/internal/backends"
 	"registry-modules-watcher/internal/metrics"
-
-	"github.com/deckhouse/module-sdk/pkg/dependency/cr"
-
-	"github.com/deckhouse/deckhouse/pkg/log"
 )
 
 const (
