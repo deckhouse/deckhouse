5b06337752fee2fab2041a44e0532e6860be18cc
YuryLysov <yuriy.lysov@flant.com>
2026-05-15 15:14:22 +0400

    Fix confirmation, add pb cleanup to finalizer
    Signed-off-by: YuryLysov <yuriy.lysov@flant.com>

--- dhctl/cmd/dhctl/action.go
+++ dhctl/cmd/dhctl/action.go
@@ -31,6 +31,7 @@ import (
 	"github.com/deckhouse/deckhouse/dhctl/pkg/util/cache"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/util/fs"
 	"github.com/deckhouse/deckhouse/dhctl/pkg/util/input"
+	"github.com/deckhouse/deckhouse/dhctl/pkg/util/progressbar"
 )
 
 type (
@@ -147,6 +148,8 @@ func (i *actionIniter) init(c *kingpin.ParseContext) error {
 	i.registerOnShutdown("Cleanup providers from default cache", func() {
 		infrastructureprovider.CleanupProvidersFromDefaultCache(log.GetDefaultLoggerProvider())
 	})
+
+	i.registerOnShutdown("Cleanup progressbar", i.cleanupProgressbar())
 
 	return nil
 }
@@ -395,4 +398,15 @@ func disableCleanupOnInterrupted(s os.Signal) {
 	}
 	// disable tmp cleaning if user pass ctrl + c
 	cache.GetGlobalTmpCleaner().DisableCleanup("Interrupted by signal " + s.String())
+}
+
+func (i *actionIniter) cleanupProgressbar() onShutdownFunc {
+	return func() {
+		pb := progressbar.GetDefaultPb()
+		if pb != nil {
+			pb.ProgressBarPrinter.Stop()
+			pb.MultiPrinter.Stop()
+		}
+	}
+
 }
