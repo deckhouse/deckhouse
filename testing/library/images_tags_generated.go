41956fa4213f8a1023a683ea7e6e23ca739de5a4
Alexander Timofeev <68970161+bloodNtears@users.noreply.github.com>
2026-05-18 11:07:10 +0300

    [operator-prometheus] suppress CVE-2026-40179 via VEX (#19859)
    Signed-off-by: Alexandr Timofeev <alexandr.timofeev@flant.com>
    Co-authored-by: Alexandr Timofeev <alexandr.timofeev@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -440,8 +440,10 @@ var DefaultImagesDigests = map[string]interface{}{
 		"pmacct":              "imageHash-openvpn-pmacct",
 	},
 	"operatorPrometheus": map[string]interface{}{
-		"prometheusConfigReloader": "imageHash-operatorPrometheus-prometheusConfigReloader",
-		"prometheusOperator":       "imageHash-operatorPrometheus-prometheusOperator",
+		"prometheusConfigReloader":            "imageHash-operatorPrometheus-prometheusConfigReloader",
+		"prometheusConfigReloaderVexArtifact": "imageHash-operatorPrometheus-prometheusConfigReloaderVexArtifact",
+		"prometheusOperator":                  "imageHash-operatorPrometheus-prometheusOperator",
+		"prometheusOperatorVexArtifact":       "imageHash-operatorPrometheus-prometheusOperatorVexArtifact",
 	},
 	"operatorTrivy": map[string]interface{}{
 		"nodeCollector":       "imageHash-operatorTrivy-nodeCollector",
