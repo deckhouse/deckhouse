8c3b1af2d4cfda7216fc6c38eb87a2b8f0f7ff81
YuryLysov <yuriy.lysov@flant.com>
2026-05-22 13:11:26 +0400

    [dhctl] Fix confirmation (#19917)
    Signed-off-by: YuryLysov <yuriy.lysov@flant.com>

--- dhctl/pkg/util/progressbar/progressbar.go
+++ dhctl/pkg/util/progressbar/progressbar.go
@@ -51,6 +51,7 @@ type Pb struct {
 	ProgressBarPrinter *pterm.ProgressbarPrinter
 	MultiPrinter       *pterm.MultiPrinter
 	SpinnerPrinter     *pterm.SpinnerPrinter
+	StopCh             chan struct{}
 }
 
 func InitProgressBarWithDeferredFunc(name string, logger log.Logger) (func(), chan phases.Progress, error) {
@@ -107,6 +108,8 @@ func InitProgressBar(param *PbParam) error {
 		return err
 	}
 
+	stopChan := make(chan struct{}, 2)
+
 	defaultpb = &Pb{
 		ProgressBarPrinter: p,
 		MultiPrinter:       &multi,
@@ -115,7 +118,7 @@ func InitProgressBar(param *PbParam) error {
 
 	log.WithProgressBar()
 
-	go updateProgress(p, param.labelChan, param.phasesChan, staticSpinner, &multi)
+	go updateProgress(p, param.labelChan, param.phasesChan, stopChan, staticSpinner, &multi)
 
 	return nil
 }
@@ -124,6 +127,7 @@ func updateProgress(
 	p *pterm.ProgressbarPrinter,
 	labelChan chan string,
 	successChan chan phases.Progress,
+	stopChan chan struct{},
 	spinner *pterm.SpinnerPrinter,
 	mp *pterm.MultiPrinter,
 ) {
@@ -140,6 +144,8 @@ func updateProgress(
 
 	for {
 		select {
+		case <-stopChan:
+			return
 		case msg, ok := <-labelChan:
 			if !ok {
 				return
@@ -276,6 +282,10 @@ func FinishDefaultProgressBar() {
 	if pb == nil {
 		return
 	}
+
+	// stopping the updateProgress goroutine
+	pb.StopCh <- struct{}{}
+	time.Sleep(50 * time.Millisecond)
 
 	pb.ProgressBarPrinter.Add(100 - pb.ProgressBarPrinter.Current)
 	if _, err := pb.MultiPrinter.Stop(); err != nil {
