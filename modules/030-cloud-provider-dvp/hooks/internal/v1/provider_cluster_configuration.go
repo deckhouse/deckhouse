05df356583a771eabb515d4f4ff7768d27b4ffe5
Alexey Voronov <alexey.voronov@flant.com>
2026-05-26 15:50:36 +0700

    fix linter issues
    Signed-off-by: Alexey Voronov <alexey.voronov@flant.com>

--- modules/030-cloud-provider-dvp/hooks/internal/v1/provider_cluster_configuration.go
+++ modules/030-cloud-provider-dvp/hooks/internal/v1/provider_cluster_configuration.go
@@ -55,7 +55,7 @@ type DvpProviderParameters struct {
 
 // DvpNodesV2 represents the v2 schema nodes section (from ModuleConfig).
 type DvpNodesV2 struct {
-	Enabled    *bool            `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Enabled    *bool               `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	Parameters *DvpNodesParameters `json:"parameters,omitempty" yaml:"parameters,omitempty"`
 }
 
@@ -68,7 +68,7 @@ type DvpNodesParameters struct {
 
 // DvpStorageV2 represents the v2 schema storage section (from ModuleConfig).
 type DvpStorageV2 struct {
-	Enabled    *bool             `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Enabled    *bool                 `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	Parameters *DvpStorageParameters `json:"parameters,omitempty" yaml:"parameters,omitempty"`
 }
 
