6e49738b208e4ba01892c54147593f8040606266
Artem Darmanyan <artem.darmanyan@flant.com>
2026-05-26 12:27:09 +0300

    [cloud-provider-dvp] fix Go lint
    Signed-off-by: Artem Darmanyan <artem.darmanyan@flant.com>

--- modules/030-cloud-provider-dvp/images/dvp-common/api/compute.go
+++ modules/030-cloud-provider-dvp/images/dvp-common/api/compute.go
@@ -390,7 +390,6 @@ func (c *ComputeService) getVMBDA(ctx context.Context, diskName string, vmHostna
 	return &vmbdas.Items[0], nil
 }
 
-
 func (c *ComputeService) CreateCloudInitProvisioningSecret(ctx context.Context, clusterUUID, vmHostname, name string, userData []byte, vmName string, vmUID types.UID) error {
 	s := &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
