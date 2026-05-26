3f5d1a70ac51f0509e6029f7b4f94f6469736238
Vladislav Denisov <vlad144denisov@gmail.com>
2026-05-26 15:14:29 +0300

    +
    +
    
    Signed-off-by: Vladislav Denisov <vlad144denisov@gmail.com>

--- dhctl/pkg/operations/destroy/deckhouse/destroyer.go
+++ dhctl/pkg/operations/destroy/deckhouse/destroyer.go
@@ -171,16 +171,6 @@ func (d *Destroyer) deleteEntities(ctx context.Context, kubeCl *client.Kubernete
 		return err
 	}
 
-	err = deckhouse.DeleteDeckhouseClusters(ctx, kubeCl)
-	if err != nil {
-		return err
-	}
-
-	err = deckhouse.WaitForDeckhouseClustersDeletion(ctx, kubeCl)
-	if err != nil {
-		return err
-	}
-
 	err = deckhouse.DeletePDBs(ctx, kubeCl)
 	if err != nil {
 		return err
