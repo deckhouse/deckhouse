2b60a184fd275d7a565cdcf273d296e899c2450e
Artem Kladov <6360800+z9r5@users.noreply.github.com>
2026-05-28 00:09:33 +0700

    [docs] Upgrade Hugo to v0.162.0 with API changes (#20131)
    Signed-off-by: Artem Kladov <artem.kladov@flant.com>

--- tools/docs/external-module-docs.sh
+++ tools/docs/external-module-docs.sh
@@ -9,7 +9,7 @@ OUTPUT_DIR="${OUTPUT_DIR:-${TEMPLATE_DIR}/public}"
 MODULE_PATH="${MODULE_PATH:-}"
 CHANNEL="${CHANNEL:-alpha}"
 MODULE_VERSION="${MODULE_VERSION:-v0.1.0}"
-HUGO_IMAGE="${HUGO_IMAGE:-ghcr.io/gohugoio/hugo:v0.159.2}"
+HUGO_IMAGE="${HUGO_IMAGE:-ghcr.io/gohugoio/hugo:v0.162.0}"
 MODE="${MODE:-build}"
 POLL_INTERVAL="${POLL_INTERVAL:-700ms}"
 
