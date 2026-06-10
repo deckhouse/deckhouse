f4f79ddc02a1c2c06361212fc45872405080f914
Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>
2026-06-10 15:54:44 +0300

    fixes
    Signed-off-by: Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>

--- ee/modules/030-cloud-provider-openstack/candi/terraform-modules/static-node/providers.tf
+++ ee/modules/030-cloud-provider-openstack/candi/terraform-modules/static-node/providers.tf
@@ -2,16 +2,16 @@
 # Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 
 provider "openstack" {
-  auth_url = var.providerClusterConfiguration.provider.authURL
-  domain_name = var.providerClusterConfiguration.provider.domainName
-  cacert_file = lookup(var.providerClusterConfiguration.provider, "caCert", "")
-  tenant_name = lookup(var.providerClusterConfiguration.provider, "tenantName", "")
-  tenant_id = lookup(var.providerClusterConfiguration.provider, "tenantID", "")
-  user_name = var.providerClusterConfiguration.provider.username
-  password = var.providerClusterConfiguration.provider.password
-  region = var.providerClusterConfiguration.provider.region
-  application_credential_id = lookup(var.providerClusterConfiguration.provider, "applicationCredentialId", "")
-  application_credential_name = lookup(var.providerClusterConfiguration.provider, "applicationCredentialName", "")
+  auth_url                      = var.providerClusterConfiguration.provider.authURL
+  domain_name                   = lookup(var.providerClusterConfiguration.provider, "domainName", "")
+  cacert_file                   = lookup(var.providerClusterConfiguration.provider, "caCert", "")
+  tenant_name                   = lookup(var.providerClusterConfiguration.provider, "tenantName", "")
+  tenant_id                     = lookup(var.providerClusterConfiguration.provider, "tenantID", "")
+  user_name                     = lookup(var.providerClusterConfiguration.provider, "username", "")
+  password                      = lookup(var.providerClusterConfiguration.provider, "password", "")
+  region                        = var.providerClusterConfiguration.provider.region
+  application_credential_id     = lookup(var.providerClusterConfiguration.provider, "applicationCredentialId", "")
+  application_credential_name   = lookup(var.providerClusterConfiguration.provider, "applicationCredentialName", "")
   application_credential_secret = lookup(var.providerClusterConfiguration.provider, "applicationCredentialSecret", "")
-  token = lookup(var.providerClusterConfiguration.provider, "token", "")
+  token                         = lookup(var.providerClusterConfiguration.provider, "token", "")
 }
