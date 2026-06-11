0f539e35b8b8893678d448ec4bb18a90da510b47
Vladislav Denisov <vlad144denisov@gmail.com>
2026-06-11 12:52:26 +0300

    gci and go imports conflict resolve
    +
    
    Signed-off-by: Vladislav Denisov <vlad144denisov@gmail.com>

--- modules/030-cloud-provider-dvp/images/capdvp-controller-manager/src/cmd/main.go
+++ modules/030-cloud-provider-dvp/images/capdvp-controller-manager/src/cmd/main.go
@@ -25,10 +25,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	_ "k8s.io/client-go/plugin/pkg/client/auth" // Import all Kubernetes client auth plugins for exec-entrypoint and run.
 	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
@@ -38,10 +35,11 @@ import (
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
-	infrastructurev1alpha1 "cluster-api-provider-dvp/api/v1alpha1"
-	"cluster-api-provider-dvp/internal/controller"
 	dvpapi "dvp-common/api"
 	"dvp-common/config"
+
+	infrastructurev1alpha1 "cluster-api-provider-dvp/api/v1alpha1"
+	"cluster-api-provider-dvp/internal/controller"
 )
 
 var (
