cb5aeec3b6bfd425f3e2ec743ab97e26bbab659a
Vladislav Denisov <vlad144denisov@gmail.com>
2026-06-16 14:19:03 +0300

    +
    +
    
    Signed-off-by: Vladislav Denisov <vlad144denisov@gmail.com>

--- modules/030-cloud-provider-dvp/candi/layouts/standard/base-infrastructure/variables.tf
+++ modules/030-cloud-provider-dvp/candi/layouts/standard/base-infrastructure/variables.tf
@@ -80,7 +80,11 @@ locals {
       }]
     },
     {
-      from  = [{ ipBlock = { cidr = "0.0.0.0/0" } }, { namespaceSelector = {} }]
+      from  = [{ ipBlock = { cidr = "0.0.0.0/0" } }]
+      ports = [{ port = 22, protocol = "TCP" }]
+    },
+    {
+      from  = [{ namespaceSelector = {} }]
       ports = [{ port = 22, protocol = "TCP" }]
     },
     {
