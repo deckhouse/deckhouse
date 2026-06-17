57f435ef81ba0c3a495e1439f85317d680a8e3b1
Maxim Mazin <maksim.mazin@flant.com>
2026-06-17 13:05:26 +0300

    improve reconcile logic & add new deployment flow
    Signed-off-by: Maxim Mazin <maksim.mazin@flant.com>

---
+++ modules/040-control-plane-manager/images/control-plane-manager/src/internal/manager/configurator_virtual.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2026 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package manager
+
+import (
+	controlplanev1alpha1 "control-plane-manager/api/v1alpha1"
+	"control-plane-manager/internal/constants"
+	virtualcontrolplaneconfiguration "control-plane-manager/internal/controllers/virtual-control-plane-configuration"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	controllerruntime "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	runtimemanager "sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+type virtualConfigurator struct{}
+
+func (c *virtualConfigurator) configureOptions(opts *controllerruntime.Options) {
+	opts.LeaderElection = true
+	opts.LeaderElectionID = constants.VirtualControlPlaneManagerName
+	opts.Client = client.Options{
+		Cache: &client.CacheOptions{
+			DisableFor: []client.Object{
+				&corev1.Namespace{},
+				&corev1.Secret{},
+				&corev1.Service{},
+			},
+		},
+	}
+
+	opts.Cache = cache.Options{
+		ReaderFailOnMissingInformer: false,
+		DefaultTransform:            cache.TransformStripManagedFields(),
+		ByObject: map[client.Object]cache.ByObject{
+			&corev1.Secret{}: {
+				Namespaces: map[string]cache.Config{
+					constants.KubeSystemNamespace: {},
+				},
+			},
+			&controlplanev1alpha1.ControlPlaneNode{}: {
+				Namespaces: map[string]cache.Config{
+					constants.KubeSystemNamespace: {},
+				},
+			},
+		},
+	}
+}
+func (c *virtualConfigurator) configureRuntimeManager(runtimeManager runtimemanager.Manager) error {
+	if err := virtualcontrolplaneconfiguration.BuildController(runtimeManager); err != nil {
+		return fmt.Errorf("build virtual-control-plane-configuration controller: %w", err)
+	}
+
+	return nil
+}
