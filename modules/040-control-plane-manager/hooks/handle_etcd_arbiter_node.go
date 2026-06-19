0bfcb9df918039707d55d4d8e530ebd4373eb7d2
Oleg Beloshev <oleg.beloshev@flant.com>
2026-06-19 17:49:55 +0300

    copy hook to monitoring-kubernetes-control-plane, add tests
    Signed-off-by: Oleg Beloshev <oleg.beloshev@flant.com>

--- modules/040-control-plane-manager/hooks/handle_etcd_arbiter_node.go
+++ modules/040-control-plane-manager/hooks/handle_etcd_arbiter_node.go
@@ -1,16 +1,18 @@
-// Copyright 2026 Flant JSC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright 2026 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package hooks
 
@@ -25,6 +27,8 @@ import (
 )
 
 var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Queue:        moduleQueue,
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 10},
 	Kubernetes: []go_hook.KubernetesConfig{
 		{
 			Name:       "etcd_arbiter_node",
@@ -51,7 +55,7 @@ func handleCheckEtcdArbiterNode(_ context.Context, input *go_hook.HookInput) err
 		return fmt.Errorf("etcd-arbiter label must be present only for one node, found %d nodes", len(etcdArbiterNodes))
 	}
 
-	input.Values.Set("global.discovery.hasEtcdArbiterNode", len(etcdArbiterNodes) == 1)
+	input.Values.Set("controlPlaneManager.internal.hasEtcdArbiterNode", len(etcdArbiterNodes) == 1)
 
 	return nil
 }
