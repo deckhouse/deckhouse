42635b2dcdb7e0cae7e2e0e287a5f9cf7071f247
Oleg Beloshev <oleg.beloshev@flant.com>
2026-06-19 11:41:06 +0300

    switch to global hook and global value
    Signed-off-by: Oleg Beloshev <oleg.beloshev@flant.com>

--- global-hooks/discovery/cluster_etcd_arbiter.go
+++ global-hooks/discovery/cluster_etcd_arbiter.go
@@ -1,18 +1,16 @@
-/*
-Copyright 2025 Flant JSC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2021 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package hooks
 
@@ -27,8 +25,6 @@ import (
 )
 
 var _ = sdk.RegisterFunc(&go_hook.HookConfig{
-	Queue:        moduleQueue,
-	OnBeforeHelm: &go_hook.OrderedConfig{Order: 10},
 	Kubernetes: []go_hook.KubernetesConfig{
 		{
 			Name:       "etcd_arbiter_node",
@@ -55,7 +51,6 @@ func handleCheckEtcdArbiterNode(_ context.Context, input *go_hook.HookInput) err
 		return fmt.Errorf("etcd-arbiter label must be present only for one node, found %d nodes", len(etcdArbiterNodes))
 	}
 
-	input.Values.Set("controlPlaneManager.internal.hasEtcdArbiterNode", len(etcdArbiterNodes) == 1)
 	input.Values.Set("global.discovery.hasEtcdArbiterNode", len(etcdArbiterNodes) == 1)
 
 	return nil
