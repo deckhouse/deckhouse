2572ff6c17fe1f225f346c9cf9eaba125bd0b7ac
Oleg Beloshev <oleg.beloshev@flant.com>
2026-06-30 18:10:03 +0300

    configurator watch optimizations
    Signed-off-by: Oleg Beloshev <oleg.beloshev@flant.com>

--- ee/cse/modules/040-node-manager/images/integrity-containerd-configurator/src/internal/controller/controller.go
+++ ee/cse/modules/040-node-manager/images/integrity-containerd-configurator/src/internal/controller/controller.go
@@ -8,6 +8,7 @@ package controller
 import (
 	"context"
 	"fmt"
+	"slices"
 
 	deckhousev1alpha1 "integrity-controller/api/deckhouse.io/v1alpha1"
 	//nolint:goimports
@@ -16,6 +17,8 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"integrity-containerd-configurator/internal/configwriter"
@@ -65,5 +68,29 @@ func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reco
 func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&deckhousev1alpha1.ContainerdIntegrityPolicy{}).
+		WithEventFilter(predicate.Funcs{
+			CreateFunc: func(event.CreateEvent) bool {
+				return true
+			},
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				oldPolicy, okOld := e.ObjectOld.(*deckhousev1alpha1.ContainerdIntegrityPolicy)
+				newPolicy, okNew := e.ObjectNew.(*deckhousev1alpha1.ContainerdIntegrityPolicy)
+				if !okOld || !okNew {
+					return true
+				}
+
+				if oldPolicy.Spec.CA != newPolicy.Spec.CA {
+					return true
+				}
+
+				return !slices.Equal(oldPolicy.Status.ProtectedNamespaces, newPolicy.Status.ProtectedNamespaces)
+			},
+			DeleteFunc: func(event.DeleteEvent) bool {
+				return true
+			},
+			GenericFunc: func(event.GenericEvent) bool {
+				return true
+			},
+		}).
 		Complete(r)
 }
