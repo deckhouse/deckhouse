554ec65de48b618d764e9dd2f365c213c788b223
Stepan Paksashvili <stepan.paksashvili@flant.com>
2026-07-02 21:03:35 +0300

    [deckhouse] fix package flow
    Signed-off-by: Stepan Paksashvili <stepan.paksashvili@flant.com>

--- deckhouse-controller/internal/nelm/client.go
+++ deckhouse-controller/internal/nelm/client.go
@@ -332,7 +332,9 @@ func (c *Client) Install(ctx context.Context, namespace, releaseName string, opt
 			RootSetJSON: valuesSet,
 		},
 		TrackingOptions: common.TrackingOptions{
-			NoPodLogs: true,
+			NoPodLogs:                    true,
+			NoFinalTracking:              true,
+			LegacyHelmCompatibleTracking: true,
 		},
 		Chart:                  opts.Path,
 		DefaultChartName:       releaseName,
