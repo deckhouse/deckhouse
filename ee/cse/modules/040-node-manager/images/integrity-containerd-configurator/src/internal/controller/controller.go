914ef994664bbee3c6284ea0b6bb0b361a1ce8ef
Oleg Beloshev <oleg.beloshev@flant.com>
2026-07-03 14:04:04 +0300

    remove namespace logging for CIP
    Signed-off-by: Oleg Beloshev <oleg.beloshev@flant.com>

--- ee/cse/modules/040-node-manager/images/integrity-containerd-configurator/src/internal/controller/controller.go
+++ ee/cse/modules/040-node-manager/images/integrity-containerd-configurator/src/internal/controller/controller.go
@@ -13,6 +13,7 @@ import (
 	"slices"
 
 	deckhousev1alpha1 "integrity-controller/api/deckhouse.io/v1alpha1"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -36,7 +37,7 @@ type Reconciler struct {
 // +kubebuilder:rbac:groups=deckhouse.io,resources=containerdintegritypolicies/status,verbs=get
 
 func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
-	logger := log.Default().With("reconcile", req.NamespacedName)
+	logger := log.Default().With("reconcile")
 
 	policyList := &deckhousev1alpha1.ContainerdIntegrityPolicyList{}
 	if err := r.List(ctx, policyList); err != nil {
