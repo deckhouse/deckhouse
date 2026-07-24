cfee227b7ae19bc1c02be8287a0fec3cd1e58c15
Sergey Petrov <sergey.petrov@flant.com>
2026-07-24 19:48:30 +0700

    [cloud-provider-yandex] feat: add Golang types for API and generate CRDs
    - Add Go structs for YandexInstanceClass (v1, v1alpha1) and ModuleConfigSettings
    - Implement openapigen tool to generate CRD and OpenAPI schemas from Go types
    - Replace hand-crafted CRDs with auto-generated YAML in crds/
    - Update ensure_crds hook to use new CRD location
    - Update werf config and generation tool for new CRD paths
    - Update dependabot config for new go modules
    - Add openapi tests for new ModuleConfig settings
    - Add exclude rules for enums (zones in MC settings, platform and diskType in InstanceClass)
    - Add conversions from MC v1 to MC v2
    
    Signed-off-by: Sergey Petrov <sergey.petrov@flant.com>

--- testing/openapi_validation/validators/enum.go
+++ testing/openapi_validation/validators/enum.go
@@ -239,8 +239,16 @@ var (
 			"properties.internal.properties.providerClusterConfiguration.properties.edgeGateway.properties.NSX-V.properties.externalNetworkType",
 		},
 		"modules/030-cloud-provider-yandex/openapi/config-values.yaml": {
-			// ignore Yandex Cloud disk types
-			"properties.storageClass.properties.provision.items.properties.type",
+			// ignore internal values
+			"properties.nodes.properties.parameters.properties.zones.items",
+			"properties.storage.properties.parameters.properties.provisionedStorageClasses.items.properties.type",
+		},
+		"modules/030-cloud-provider-yandex/crds/instance_class.yaml": {
+			// ignore internal values
+			"spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.platform",
+			"spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.diskType",
+			"spec.versions[1].schema.openAPIV3Schema.properties.spec.properties.platform",
+			"spec.versions[1].schema.openAPIV3Schema.properties.spec.properties.diskType",
 		},
 		"modules/030-cloud-provider-yandex/openapi/values.yaml": {
 			// ignore internal values
