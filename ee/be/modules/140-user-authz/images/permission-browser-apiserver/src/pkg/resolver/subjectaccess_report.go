2c1bb62f41505e6741fa4b4e563e81c478bb1632
Ivan Zvyagintsev <ivan.zvyagintsev@flant.com>
2026-07-27 14:04:29 +0300

    permission-browser: set the copyright year of the new files to 2026
    Signed-off-by: Ivan Zvyagintsev <ivan.zvyagintsev@flant.com>

--- ee/be/modules/140-user-authz/images/permission-browser-apiserver/src/pkg/resolver/subjectaccess_report.go
+++ ee/be/modules/140-user-authz/images/permission-browser-apiserver/src/pkg/resolver/subjectaccess_report.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2025 Flant JSC
+Copyright 2026 Flant JSC
 Licensed under the Deckhouse Platform Enterprise Edition (EE) license. See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 */
 
