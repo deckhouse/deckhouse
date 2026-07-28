eea2912cab85b8ef742d4f6de5040ed2e1d1b3ee
Maxim Mazin <maksim.mazin@flant.com>
2026-07-28 11:00:24 +0300

    [control-plane-manager] Konnectivity static images (#21587)
    Signed-off-by: Maxim Mazin <maksim.mazin@flant.com>

--- modules/040-control-plane-manager/images/control-plane-manager/src/internal/controllers/virtual-control-plane-configuration/reconcile_bashible-apiserver.go
+++ modules/040-control-plane-manager/images/control-plane-manager/src/internal/controllers/virtual-control-plane-configuration/reconcile_bashible-apiserver.go
@@ -698,15 +698,15 @@ func buildTargetBashibleService(vcp *controlplanev1alpha1.VirtualControlPlane) *
 			Name:      constants.VirtualResourceName(bashibleServiceName, vcp.Name),
 			Namespace: vcp.Namespace,
 			Labels: map[string]string{
-				"app":                                      bashibleAppLabel,
-				constants.HeritageLabelKey:                 constants.HeritageLabelValue,
+				"app":                      bashibleAppLabel,
+				constants.HeritageLabelKey: constants.HeritageLabelValue,
 				constants.VirtualControlPlaneScopeLabelKey: vcp.Name,
 			},
 		},
 		Spec: corev1.ServiceSpec{
 			Type: corev1.ServiceTypeClusterIP,
 			Selector: map[string]string{
-				"app":                                      bashibleAppLabel,
+				"app": bashibleAppLabel,
 				constants.VirtualControlPlaneScopeLabelKey: vcp.Name,
 			},
 			Ports: []corev1.ServicePort{
