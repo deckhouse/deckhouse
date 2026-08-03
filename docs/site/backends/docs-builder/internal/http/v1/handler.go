7f710541f40ea568f10e89bb0792f18e5bce8028
Smyslov Maxim <maksim.smyslov@flant.com>
2026-08-03 11:48:40 +0300

    test that buid is leaks memory
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- docs/site/backends/docs-builder/internal/http/v1/handler.go
+++ docs/site/backends/docs-builder/internal/http/v1/handler.go
@@ -46,9 +46,9 @@ func NewHandler(docsService *docs.Service, logger *log.Logger) *DocsBuilderHandl
 	r.HandleFunc("/readyz", h.handleReadyZ)
 	r.HandleFunc("/healthz", h.handleHealthZ)
 
-	r.HandleFunc("GET /api/v1/doc", h.handleGetDocsInfo)
-	r.HandleFunc("POST /api/v1/doc/{moduleName}/{version}", h.handleUpload)
-	r.HandleFunc("DELETE /api/v1/doc/{moduleName}", h.handleDelete)
+	// r.HandleFunc("GET /api/v1/doc", h.handleGetDocsInfo)
+	// r.HandleFunc("POST /api/v1/doc/{moduleName}/{version}", h.handleUpload)
+	// r.HandleFunc("DELETE /api/v1/doc/{moduleName}", h.handleDelete)
 	r.HandleFunc("POST /api/v1/build", h.handleBuild)
 
 	return h
