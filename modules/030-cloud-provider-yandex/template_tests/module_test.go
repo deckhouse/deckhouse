461d6e0d73e8c0347cdcdb38bc3f506402010802
Sergey Petrov <sergey.petrov@flant.com>
2026-08-07 02:08:05 +0700

    [cloud-provider-yandex] fix: gate nat-instance dashboard on prometheus-crd
    - Add a check for `prometheus-crd` in `.Values.global.enabledModules` before rendering the NAT instance Grafana dashboard, so the dashboard is only included when the monitoring stack is actually available
    
    Signed-off-by: Sergey Petrov <sergey.petrov@flant.com>

--- modules/030-cloud-provider-yandex/template_tests/module_test.go
+++ modules/030-cloud-provider-yandex/template_tests/module_test.go
@@ -51,7 +51,7 @@ const bashibleLabelKey = "cloud-provider\\.deckhouse\\.io/bashible"
 // TODO: remove fake crd modules
 const globalValues = `
   clusterIsBootstrapped: true
-  enabledModules: ["vertical-pod-autoscaler", "vertical-pod-autoscaler-crd", "cloud-provider-yandex", "operator-prometheus", "operator-prometheus-crd"]
+  enabledModules: ["vertical-pod-autoscaler", "vertical-pod-autoscaler-crd", "cloud-provider-yandex", "operator-prometheus", "operator-prometheus-crd", "prometheus", "prometheus-crd"]
   clusterConfiguration:
     apiVersion: deckhouse.io/v1
     cloud:
