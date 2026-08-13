c87a5848f863622dc026b2488877d231dea266b8
Aleksandr Radchenko <102186217+dacetascien@users.noreply.github.com>
2026-08-13 17:12:41 +0300

    [docs] Add Kubernetes conformance test results page (#21845)
    Signed-off-by: Aleksandr Radchenko <dacetascien@gmail.com>
    Signed-off-by: Aleksandr Radchenko <102186217+dacetascien@users.noreply.github.com>
    Co-authored-by: Konstantin Nezhbert <konstantin.nezhbert@flant.com>

---
+++ docs/documentation/_plugins/kubernetes_conformance_generator.rb
@@ -0,0 +1,63 @@
+module KubernetesConformance
+  class Generator < Jekyll::Generator
+    safe true
+    priority :high
+
+    def generate(site)
+      conformance_dir = File.join(site.source, 'assets', 'conformance')
+
+      site.data['kubernetes_conformance'] = {
+        'results' => conformance_results(conformance_dir),
+        'readme' => {
+          'en' => conformance_readme(conformance_dir, 'README.md'),
+          'ru' => conformance_readme(conformance_dir, 'README_RU.md')
+        }
+      }
+    end
+
+    private
+
+    def conformance_results(conformance_dir)
+      return [] unless Dir.exist?(conformance_dir)
+
+      Dir.children(conformance_dir).each_with_object([]) do |version, results|
+        next unless version.match?(/\A\d+\.\d+\z/)
+
+        xml_path = File.join(conformance_dir, version, 'junit_01.xml')
+        next unless File.file?(xml_path)
+
+        results << {
+          'version' => version,
+          'xml_path' => "/assets/conformance/#{version}/junit_01.xml"
+        }
+      end.sort_by { |result| result['version'].split('.').map(&:to_i) }.reverse
+    end
+
+    def conformance_readme(conformance_dir, filename)
+      readme_path = File.join(conformance_dir, filename)
+      return '' unless File.file?(readme_path)
+
+      demote_markdown_headings(File.read(readme_path))
+    end
+
+    def demote_markdown_headings(markdown)
+      fence_character = nil
+
+      markdown.each_line.map do |line|
+        fence = line.match(/\A\s*(`{3,}|~{3,})/)
+
+        if fence_character
+          fence_character = nil if line.match?(/\A\s*#{Regexp.escape(fence_character)}{3,}\s*\z/)
+          line
+        elsif fence
+          fence_character = fence[1][0]
+          line
+        elsif line.match?(/\A\#{1,5}\s/)
+          "##{line}"
+        else
+          line
+        end
+      end.join
+    end
+  end
+end
