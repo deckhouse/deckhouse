8c56781ecb105623039af25fe91ab1f8e4d4c856
Artem Darmanyan <artem.darmanyan@flant.com>
2026-08-18 12:15:39 +0300

    [cloud-provider-yandex] remove create_before_destroy from node instances
    Signed-off-by: Artem Darmanyan <artem.darmanyan@flant.com>

--- modules/030-cloud-provider-yandex/candi/terraform-modules/master-node/main.tf
+++ modules/030-cloud-provider-yandex/candi/terraform-modules/master-node/main.tf
@@ -165,8 +165,6 @@ resource "yandex_compute_instance" "master" {
       condition     = local._network_type_raw == "" || local.network_type != null
       error_message = "ERROR: unknown YandexInstanceClass networkType '${local._network_type_raw}' on instance class '${local._instance_class_name}': expected one of ${join(", ", keys(var.network_types))}."
     }
-
-    create_before_destroy = true
   }
 
   timeouts {
