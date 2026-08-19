bfdc4142fcc19de9654d92112d2593df9efe7bd3
Vladislav Denisov <vlad144denisov@gmail.com>
2026-08-19 13:48:36 +0300

    +
    +
    
    Signed-off-by: Vladislav Denisov <vlad144denisov@gmail.com>

---
+++ ee/modules/030-cloud-provider-metal3/images/metal3-instance-manager/src/cmd/metal3-instance-manager/main_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func TestUpdateBareMetalHostSpecPreservesUnmanagedFields(t *testing.T) {
+	bmh := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"spec": map[string]interface{}{
+				"architecture": "x86_64",
+				"rootDeviceHints": map[string]interface{}{
+					"deviceName": "/dev/sda",
+				},
+				"online":                false,
+				"automatedCleaningMode": "disabled",
+				"bmc": map[string]interface{}{
+					"address":         "redfish+http://old/redfish/v1/Systems/system",
+					"credentialsName": "old-bmc",
+				},
+			},
+		},
+	}
+
+	spec := instanceSpec{
+		Online:                          true,
+		AutomatedCleaningMode:           "metadata",
+		BootMACAddress:                  "aa:bb:cc:dd:ee:ff",
+		BMCAddress:                      "redfish+http://new/redfish/v1/Systems/system",
+		BMCDisableCertificateValidation: true,
+	}
+
+	updated, err := updateBareMetalHostSpec(bmh, spec, "new-bmc")
+	if err != nil {
+		t.Fatalf("update BareMetalHost spec: %v", err)
+	}
+	if !updated {
+		t.Fatal("expected BareMetalHost spec to be updated")
+	}
+
+	if got, _, _ := unstructured.NestedString(bmh.Object, "spec", "architecture"); got != "x86_64" {
+		t.Fatalf("expected architecture to be preserved, got %q", got)
+	}
+	if got, _, _ := unstructured.NestedString(bmh.Object, "spec", "rootDeviceHints", "deviceName"); got != "/dev/sda" {
+		t.Fatalf("expected rootDeviceHints to be preserved, got %q", got)
+	}
+	if got, _, _ := unstructured.NestedBool(bmh.Object, "spec", "online"); !got {
+		t.Fatal("expected online to be true")
+	}
+	if got, _, _ := unstructured.NestedString(bmh.Object, "spec", "automatedCleaningMode"); got != "metadata" {
+		t.Fatalf("expected automatedCleaningMode to be metadata, got %q", got)
+	}
+	if got, _, _ := unstructured.NestedString(bmh.Object, "spec", "bootMACAddress"); got != "aa:bb:cc:dd:ee:ff" {
+		t.Fatalf("expected bootMACAddress to be set, got %q", got)
+	}
+	if got, _, _ := unstructured.NestedString(bmh.Object, "spec", "bmc", "address"); got != spec.BMCAddress {
+		t.Fatalf("expected bmc address to be updated, got %q", got)
+	}
+	if got, _, _ := unstructured.NestedString(bmh.Object, "spec", "bmc", "credentialsName"); got != "new-bmc" {
+		t.Fatalf("expected bmc credentialsName to be updated, got %q", got)
+	}
+	if got, _, _ := unstructured.NestedBool(bmh.Object, "spec", "bmc", "disableCertificateVerification"); !got {
+		t.Fatal("expected disableCertificateVerification to be true")
+	}
+}
