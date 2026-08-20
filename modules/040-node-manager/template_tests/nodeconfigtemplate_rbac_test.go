ebd905cce62e87e35f613c9e2c3d1d0bf194e929
borg-z <me@zykov.tatar>
2026-08-20 16:21:13 +0300

    guard the template behind node-adding rights
    Signed-off-by: borg-z <me@zykov.tatar>
    (cherry picked from commit 967d459b5677f358bf7a6bf2fd46c2a31234adc5)

---
+++ modules/040-node-manager/template_tests/nodeconfigtemplate_rbac_test.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2026 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package template_tests
+
+import (
+	"encoding/json"
+	"slices"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/deckhouse/deckhouse/testing/helm"
+	"github.com/deckhouse/deckhouse/testing/library/object_store"
+)
+
+var _ = Describe("Module :: node-manager :: helm template :: NodeConfigTemplate rights", func() {
+	f := SetupHelmConfig(``)
+
+	BeforeEach(func() {
+		f.ValuesSetFromYaml("global", globalValues)
+		f.ValuesSet("global.modulesImages", GetModulesImages())
+		f.ValuesSetFromYaml("nodeManager", nodeManagerConfigValues+nodeManagerValues)
+		setBashibleAPIServerTLSValues(f)
+		f.HelmRender()
+	})
+
+	It("hands the template to whoever may add a node, and to nobody else", func() {
+		Expect(f.RenderError).ShouldNot(HaveOccurred())
+
+		edit := f.KubernetesGlobalResource("ClusterRole", "d8:manage:permission:module:node-manager:edit")
+		view := f.KubernetesGlobalResource("ClusterRole", "d8:manage:permission:module:node-manager:view")
+		Expect(edit.Exists()).To(BeTrue())
+		Expect(view.Exists()).To(BeTrue())
+
+		Expect(nodeConfigTemplateVerbs(edit)).To(ConsistOf("get", "list"))
+		// A rendered template carries a live bootstrap token: a read-only role
+		// that could fetch one could join machines to the cluster.
+		Expect(nodeConfigTemplateVerbs(view)).To(BeEmpty())
+	})
+})
+
+func nodeConfigTemplateVerbs(role object_store.KubeObject) []string {
+	var rules []struct {
+		APIGroups []string `json:"apiGroups"`
+		Resources []string `json:"resources"`
+		Verbs     []string `json:"verbs"`
+	}
+	Expect(json.Unmarshal([]byte(role.Field("rules").String()), &rules)).To(Succeed())
+
+	for _, rule := range rules {
+		if !slices.Contains(rule.APIGroups, "internal.deckhouse.io") {
+			continue
+		}
+		if slices.Contains(rule.Resources, "nodeconfigtemplates") {
+			return rule.Verbs
+		}
+	}
+	return nil
+}
