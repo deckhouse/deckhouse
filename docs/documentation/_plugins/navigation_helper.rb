87a15ae2aa98c327e4dce57b731cdccb8bccab78
Aleksandr Radchenko <102186217+dacetascien@users.noreply.github.com>
2026-08-23 18:20:57 +0300

    [docs] Add previous page and next page buttons to modules documentation (#21702)
    Signed-off-by: Aleksandr Radchenko <dacetascien@gmail.com>

--- docs/documentation/_plugins/navigation_helper.rb
+++ docs/documentation/_plugins/navigation_helper.rb
@@ -94,6 +94,45 @@ module Jekyll
       flattened
     end
 
+    def self.embedded_module_entry_title(site, entry, lang = 'en')
+      return nil unless entry && entry['url']
+
+      if entry['url'].end_with?('/')
+        site.data.dig('modules', 'sidebar', 'titles', 'overview', lang)
+      else
+        site.data.dig('modules', 'sidebar', 'titles', File.basename(entry['url']), lang) || entry.dig('title', lang)
+      end
+    end
+
+    def self.flatten_embedded_module_entries(site, page, lang = 'en')
+      module_name = page['module-kebab-name'] || page['moduleName']
+      return [] if module_name.nil? || module_name.to_s.strip.empty?
+
+      module_sidebar = site.data.dig('sidebars', 'embedded-modules', module_name)
+      return [] unless module_sidebar && module_sidebar['folders'].is_a?(Array)
+
+      module_title = module_name
+      flattened = []
+
+      module_sidebar['folders'].sort_by { |entry| entry['weight'] || 100 }.each do |entry|
+        next if entry['draft'] == true
+
+        title = embedded_module_entry_title(site, entry, lang)
+        next if title.nil? || title.to_s.strip.empty?
+
+        display_title = module_title.to_s.strip.empty? ? title : "#{module_title} / #{title}"
+
+        flattened << {
+          'title' => title,
+          'display_title' => display_title,
+          'url' => entry['url'],
+          'external_url' => entry['external_url']
+        }
+      end
+
+      flattened
+    end
+
     def self.get_relative_url(path, current_page_url)
       # Remove first slash if exists
       page_path_relative = current_page_url.gsub(%r!^/!, "")
@@ -103,12 +142,38 @@ module Jekyll
       prefix + path.sub(%r!^/!, "./")
     end
 
+    def self.get_embedded_module_url(path, current_page_url)
+      return nil if path.nil?
+
+      normalized_path = path.start_with?('/') ? path : "/#{path}"
+      current_url = current_page_url.to_s.sub(%r{^/(en|ru)/}, '/')
+      channel_pattern = '(?:v[0-9]+\.[0-9]+|alpha|beta|early-access|stable|rock-solid|latest)'
+      current_match = current_url.match(%r{\A/modules/([^/]+)/(#{channel_pattern})(?:/.*)?\z})
+
+      return normalized_path unless current_match
+
+      module_name = current_match[1]
+      channel = current_match[2]
+      module_prefix = "/modules/#{module_name}/"
+
+      return normalized_path unless normalized_path.start_with?(module_prefix)
+
+      entry_suffix = normalized_path.sub(module_prefix, '')
+      return normalized_path if entry_suffix.match?(%r{\A#{channel_pattern}(?:/|\z)})
+
+      "/modules/#{module_name}/#{channel}/#{entry_suffix}"
+    end
+
     def self.find_navigation_pages(site, page, sidebar_name = 'main')
       return { 'prev' => nil, 'next' => nil } unless site.data['sidebars'][sidebar_name]
 
       lang = page['lang'] || 'en'
-      entries = site.data['sidebars'][sidebar_name]['entries']
-      flattened = flatten_sidebar_entries(entries, lang)
+      flattened = if sidebar_name == 'embedded-modules'
+                    flatten_embedded_module_entries(site, page, lang)
+                  else
+                    entries = site.data['sidebars'][sidebar_name]['entries']
+                    flatten_sidebar_entries(entries, lang)
+                  end
 
       return { 'prev' => nil, 'next' => nil } if flattened.nil? || flattened.empty?
 
@@ -134,11 +199,19 @@ module Jekyll
 
       # Convert to relative URLs
       if prev_page
-        prev_page['full_url'] = get_relative_url(prev_page['url'], page['url'])
+        prev_page['full_url'] = if sidebar_name == 'embedded-modules'
+                                  get_embedded_module_url(prev_page['url'], page['url'])
+                                else
+                                  get_relative_url(prev_page['url'], page['url'])
+                                end
       end
 
       if next_page
-        next_page['full_url'] = get_relative_url(next_page['url'], page['url'])
+        next_page['full_url'] = if sidebar_name == 'embedded-modules'
+                                  get_embedded_module_url(next_page['url'], page['url'])
+                                else
+                                  get_relative_url(next_page['url'], page['url'])
+                                end
       end
 
       { 'prev' => prev_page, 'next' => next_page }
