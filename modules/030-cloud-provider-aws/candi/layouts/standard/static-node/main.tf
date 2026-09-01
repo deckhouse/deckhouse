002b1fd9f946b3b4ae7c4ba6439144629b685351
Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>
2026-09-01 16:19:26 +0300

    [cloud-provider-aws] IMDS v2 support (#21988)
    Signed-off-by: Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>
    Signed-off-by: mortelumina98 <ekaterina.matseevska@flant.com>
    Co-authored-by: mortelumina98 <ekaterina.matseevska@flant.com>

--- modules/030-cloud-provider-aws/candi/layouts/standard/static-node/main.tf
+++ modules/030-cloud-provider-aws/candi/layouts/standard/static-node/main.tf
@@ -18,6 +18,8 @@ module "static-node" {
   cluster_uuid = var.clusterUUID
   node_index = var.nodeIndex
   node_group = local.node_group
+  imdsv2 = lookup(var.providerClusterConfiguration, "imdsv2", false)
+  imdsv2_managed = contains(keys(var.providerClusterConfiguration), "imdsv2")
   root_volume_size = local.root_volume_size
   root_volume_type = local.root_volume_type
   additional_security_groups = local.additional_security_groups
