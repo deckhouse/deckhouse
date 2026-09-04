273438309067ffe24d3813808ae3f1e3b1844bf8
Maxim Mazin <maksim.mazin@flant.com>
2026-09-04 11:58:34 +0300

    clean out 1.32 from .github
    Signed-off-by: Maxim Mazin <maksim.mazin@flant.com>

--- .github/scripts/validation_bashible.sh
+++ .github/scripts/validation_bashible.sh
@@ -126,7 +126,7 @@ clusterBootstrap:
   clusterDNSAddress: 10.222.0.10
   clusterDomain: cluster.local
   nodeIP: 192.168.199.23
-kubernetesVersion: "1.32"
+kubernetesVersion: "1.33"
 cri: "Containerd"
 nodeGroup:
   cloudInstances:
