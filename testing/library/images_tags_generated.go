93c10659ac9022ad2a2b844db6388fbea677fb3e
Ilshat.Khisamov <ilshat.khisamov@flant.com>
2026-09-14 17:26:31 +0500

    [cert-manager] Regenerate dependabot and image tags after Yandex webhook
    Signed-off-by: Ilshat.Khisamov <ilshat.khisamov@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -12,10 +12,10 @@ var DefaultImagesDigests = map[string]interface{}{
 		"nginx": "imageHash-basicAuth-nginx",
 	},
 	"certManager": map[string]interface{}{
-		"certManagerAcmeSolver":   "imageHash-certManager-certManagerAcmeSolver",
-		"certManagerCainjector":   "imageHash-certManager-certManagerCainjector",
-		"certManagerController":   "imageHash-certManager-certManagerController",
-		"certManagerWebhook":      "imageHash-certManager-certManagerWebhook",
+		"certManagerAcmeSolver":    "imageHash-certManager-certManagerAcmeSolver",
+		"certManagerCainjector":    "imageHash-certManager-certManagerCainjector",
+		"certManagerController":    "imageHash-certManager-certManagerController",
+		"certManagerWebhook":       "imageHash-certManager-certManagerWebhook",
 		"certManagerWebhookYandex": "imageHash-certManager-certManagerWebhookYandex",
 	},
 	"chrony": map[string]interface{}{
