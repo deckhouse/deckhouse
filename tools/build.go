eae8d3ac8687e2a6ebf782c0850ecc8fbd4b1cfd
Oleg Beloshev <oleg.beloshev@flant.com>
2026-09-16 10:42:27 +0300

    exclude e2e
    Signed-off-by: Oleg Beloshev <oleg.beloshev@flant.com>

--- tools/build.go
+++ tools/build.go
@@ -81,6 +81,7 @@ var defaultModulesExcludes = []string{
 	".namespace",
 	"values_matrix_test.yaml",
 	".build.yaml",
+	"e2e",
 }
 
 var nothingButGoHooksExcludes = []string{
@@ -828,11 +829,11 @@ func (e *executor) executeEdition(editionName string) {
 		// Process .build.yaml in an edition's global-hooks directory (if present)
 		// to add override entries to modules-with-exclude-<edition>.yaml.
 		writeGlobalHooksBuildYaml(writeSettings{
-			Edition:          editionName,
-			SaveTo:           modulesWithExcludeFileName,
-			Prefix:           prefix,
-			Dir:              "global-hooks",
-			ExcludePaths:     defaultModulesExcludes,
+			Edition:           editionName,
+			SaveTo:            modulesWithExcludeFileName,
+			Prefix:            prefix,
+			Dir:               "global-hooks",
+			ExcludePaths:      defaultModulesExcludes,
 			StageDependencies: stageDependenciesSetup,
 		})
 
