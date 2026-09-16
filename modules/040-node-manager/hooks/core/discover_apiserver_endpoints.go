88f14d3b5b9c728e289464c8a52e5d0973f36b84
borg-z <me@zykov.tatar>
2026-09-16 10:14:59 +0300

    node-manager hooks by domain folders
    Signed-off-by: borg-z <me@zykov.tatar>

---
+++ modules/040-node-manager/hooks/core/discover_apiserver_endpoints.go
@@ -0,0 +1,177 @@
+/*
+Copyright 2021 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package core
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	corev1 "k8s.io/api/core/v1"
+	discoveryv1 "k8s.io/api/discovery/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	sdkobjectpatch "github.com/deckhouse/module-sdk/pkg/object-patch"
+
+	"github.com/deckhouse/deckhouse/go_lib/set"
+)
+
+const (
+	apiserverPort              = 6443
+	packagesProxyPort          = 4219
+	packagesProxyBootstrapPort = 4282
+)
+
+var _ = sdk.RegisterFunc(&go_hook.HookConfig{
+	Queue: "/modules/node-manager",
+	Schedule: []go_hook.ScheduleConfig{
+		{
+			Name:    "discover_apiserver_endpoints",
+			Crontab: "*/5 * * * *",
+		},
+	},
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "kube_apiserver",
+			ApiVersion: "v1",
+			Kind:       "Pod",
+			LabelSelector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"component": "kube-apiserver",
+					"tier":      "control-plane",
+				},
+			},
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"kube-system"},
+				},
+			},
+			FilterFunc: apiserverPodFilter,
+		},
+		{
+			Name:       "apiserver_endpointSlice",
+			ApiVersion: "discovery.k8s.io/v1",
+			Kind:       "EndpointSlice",
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"default"},
+				},
+			},
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{"kubernetes"},
+			},
+			FilterFunc: apiEndpointsFilter,
+		},
+	},
+}, handleAPIEndpoints)
+
+func apiserverPodFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	var isReady bool
+
+	pod := &corev1.Pod{}
+	err := sdk.FromUnstructured(obj, pod)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse pod object from unstructured: %v", err)
+	}
+	for _, cond := range pod.Status.Conditions {
+		if cond.Type == corev1.PodReady && cond.Status == corev1.ConditionTrue {
+			isReady = true
+			break
+		}
+	}
+	if !isReady {
+		return "", nil
+	}
+	return fmt.Sprintf("%s:%d", pod.Status.PodIP, apiserverPort), nil
+}
+
+func apiEndpointsFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	var endpointSlice discoveryv1.EndpointSlice
+
+	err := sdk.FromUnstructured(obj, &endpointSlice)
+	if err != nil {
+		return nil, err
+	}
+
+	addresses := make([]string, 0)
+
+	ports := make([]int32, 0)
+	for _, port := range endpointSlice.Ports {
+		if *port.Name == "https" {
+			ports = append(ports, *port.Port)
+		}
+	}
+	for _, endpoint := range endpointSlice.Endpoints {
+		for _, addrObj := range endpoint.Addresses {
+			for _, port := range ports {
+				addr := net.JoinHostPort(addrObj, strconv.Itoa(int(port)))
+				addresses = append(addresses, addr)
+			}
+		}
+	}
+
+	return addresses, nil
+}
+
+func handleAPIEndpoints(_ context.Context, input *go_hook.HookInput) error {
+	endpointsSet := set.NewFromSnapshot(input.Snapshots.Get("kube_apiserver"))
+
+	for ep, err := range sdkobjectpatch.SnapshotIter[[]string](input.Snapshots.Get("apiserver_endpointSlice")) {
+		if err != nil {
+			return fmt.Errorf("cannot iterate over 'apiserver_endpointSlice' snapshot: %w", err)
+		}
+		endpointsSet.Add(ep...)
+	}
+	endpointsSet.Delete("") // clean faulty pods
+
+	endpointsList := endpointsSet.Slice() // sorted
+
+	if len(endpointsList) == 0 {
+		return errors.New("no kubernetes apiserver endpoints host:port specified")
+	}
+
+	clusterMasterEndpoints := make([]map[string]any, 0, len(endpointsList))
+	for _, endpoint := range endpointsList {
+		address, port, err := net.SplitHostPort(endpoint)
+		if err != nil {
+			return fmt.Errorf("cannot parse kubernetes apiserver endpoint %q: %w", endpoint, err)
+		}
+
+		kubeAPIPort, err := strconv.Atoi(port)
+		if err != nil {
+			return fmt.Errorf("cannot parse kubernetes apiserver port %q: %w", port, err)
+		}
+
+		clusterMasterEndpoints = append(clusterMasterEndpoints, map[string]any{
+			"address":                address,
+			"kubeApiPort":            kubeAPIPort,
+			"rppServerPort":          packagesProxyPort,
+			"rppBootstrapServerPort": packagesProxyBootstrapPort,
+		})
+	}
+
+	input.Values.Set("nodeManager.internal.clusterMasterAddresses", endpointsList)
+	input.Values.Set("nodeManager.internal.clusterMasterEndpoints", clusterMasterEndpoints)
+
+	return nil
+}
---
+++ modules/040-node-manager/hooks/core/get_crds.go
@@ -0,0 +1,277 @@
+/*
+Copyright 2021 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package core
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	cljson "github.com/clarketm/json"
+	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
+	"github.com/flant/addon-operator/sdk"
+	"github.com/flant/shell-operator/pkg/kube_events_manager/types"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	sdkobjectpatch "github.com/deckhouse/module-sdk/pkg/object-patch"
+
+	"github.com/deckhouse/deckhouse/go_lib/set"
+	ngv1 "github.com/deckhouse/deckhouse/modules/040-node-manager/hooks/internal/v1"
+)
+
+type NodeGroupCrdInfo struct {
+	Name   string
+	Spec   ngv1.NodeGroupSpec
+	Engine ngv1.NodeGroupEngine
+	UseMCM bool
+}
+
+// cloudInstancesForValues is the part of NodeGroup.spec.cloudInstances published to helm values.
+// The bounds stay nilable pointers with omitempty; handleClusterAutoscalerDeploymentRequirements
+// reads them as `ng.CloudInstances.MinPerZone == nil`.
+type cloudInstancesForValues struct {
+	MinPerZone *int32   `json:"minPerZone,omitempty"`
+	MaxPerZone *int32   `json:"maxPerZone,omitempty"`
+	Zones      []string `json:"zones"`
+}
+
+const useMCMAnnotation = "node.deckhouse.io/use-mcm"
+
+// applyNodeGroupCrdFilter returns name, spec, status.engine and use-mcm annotation from the NodeGroup.
+func applyNodeGroupCrdFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	var nodeGroup ngv1.NodeGroup
+	err := sdk.FromUnstructured(obj, &nodeGroup)
+	if err != nil {
+		return nil, err
+	}
+
+	return NodeGroupCrdInfo{
+		Name:   nodeGroup.GetName(),
+		Spec:   nodeGroup.Spec,
+		Engine: nodeGroup.Status.Engine,
+		UseMCM: nodeGroup.GetAnnotations()[useMCMAnnotation] != "",
+	}, nil
+}
+
+type MachineDeploymentCrdInfo struct {
+	Name string
+	Zone string
+}
+
+func applyMachineDeploymentCrdFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	return MachineDeploymentCrdInfo{
+		Name: obj.GetName(),
+		Zone: obj.GetAnnotations()["zone"],
+	}, nil
+}
+
+func applyCloudProviderSecretZonesFilter(obj *unstructured.Unstructured) (go_hook.FilterResult, error) {
+	secretData, err := decodeDataFromSecret(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	return map[string]interface{}{
+		"zones": secretData["zones"],
+	}, nil
+}
+
+var getCRDsHookConfig = &go_hook.HookConfig{
+	Queue:        "/modules/node-manager",
+	OnBeforeHelm: &go_hook.OrderedConfig{Order: 10},
+	Kubernetes: []go_hook.KubernetesConfig{
+		{
+			Name:       "ngs",
+			ApiVersion: "deckhouse.io/v1",
+			Kind:       "NodeGroup",
+			FilterFunc: applyNodeGroupCrdFilter,
+		},
+		{
+			Name:       "machine_deployments",
+			ApiVersion: "machine.sapcloud.io/v1alpha1",
+			Kind:       "MachineDeployment",
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"d8-cloud-instance-manager"},
+				},
+			},
+			LabelSelector: &metav1.LabelSelector{
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "heritage",
+						Operator: metav1.LabelSelectorOpIn,
+						Values:   []string{"deckhouse"},
+					},
+				},
+			},
+			FilterFunc: applyMachineDeploymentCrdFilter,
+		},
+		// kube-system/Secret/d8-node-manager-cloud-provider
+		{
+			Name:       "cloud_provider_secret",
+			ApiVersion: "v1",
+			Kind:       "Secret",
+			NamespaceSelector: &types.NamespaceSelector{
+				NameSelector: &types.NameSelector{
+					MatchNames: []string{"kube-system"},
+				},
+			},
+			NameSelector: &types.NameSelector{
+				MatchNames: []string{"d8-node-manager-cloud-provider"},
+			},
+			FilterFunc: applyCloudProviderSecretZonesFilter,
+		},
+	},
+	Schedule: []go_hook.ScheduleConfig{
+		{
+			Name:    "sync",
+			Crontab: "*/10 * * * *",
+		},
+	},
+}
+
+var _ = sdk.RegisterFunc(getCRDsHookConfig, getCRDsHandler)
+
+// getCRDsHandler builds the nodeManager.internal.nodeGroups blob: name, nodeType, engine,
+// gpu, fencing and the three read cloudInstances fields, with zones defaulted for
+// CloudEphemeral. Everything else about a NodeGroup is owned by node-controller.
+func getCRDsHandler(_ context.Context, input *go_hook.HookInput) error {
+	// Default zones. Take them from machine_deployments and cloud_provider_secret.zones.
+	defaultZones := set.New()
+	for machineInfo, err := range sdkobjectpatch.SnapshotIter[MachineDeploymentCrdInfo](input.Snapshots.Get("machine_deployments")) {
+		if err != nil {
+			return fmt.Errorf("failed to iterate over 'machine_deployments' snapshots: %w", err)
+		}
+
+		defaultZones.Add(machineInfo.Zone)
+	}
+
+	cloudProviderSecrets, err := sdkobjectpatch.UnmarshalToStruct[map[string]interface{}](input.Snapshots, "cloud_provider_secret")
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal 'cloud_provider_secret' snapshot: %w", err)
+	}
+	if len(cloudProviderSecrets) > 0 {
+		switch v := cloudProviderSecrets[0]["zones"].(type) {
+		case []string:
+			defaultZones.Add(v...)
+		case []interface{}:
+			for _, zoneUntyped := range v {
+				if s, ok := zoneUntyped.(string); ok {
+					defaultZones.Add(s)
+				}
+			}
+		case string:
+			defaultZones.Add(v)
+		}
+	}
+
+	finalNodeGroups := make([]interface{}, 0)
+
+	for nodeGroup, err := range sdkobjectpatch.SnapshotIter[NodeGroupCrdInfo](input.Snapshots.Get("ngs")) {
+		if err != nil {
+			return fmt.Errorf("failed to iterate over 'ngs' snapshots: %w", err)
+		}
+
+		ngForValues := nodeGroupForValues(nodeGroup.Spec.DeepCopy())
+		ngForValues["name"] = nodeGroup.Name
+		ngForValues["engine"] = string(calculateNodeGroupEngine(input, nodeGroup))
+
+		if nodeGroup.Spec.NodeType == ngv1.NodeTypeCloudEphemeral {
+			zones := nodeGroup.Spec.CloudInstances.Zones
+			if zones == nil {
+				zones = defaultZones.Slice()
+			}
+
+			if ngForValues["cloudInstances"] == nil {
+				ngForValues["cloudInstances"] = cloudInstancesForValues{}
+			}
+			cloudInstances := ngForValues["cloudInstances"].(cloudInstancesForValues)
+			cloudInstances.Zones = zones
+			ngForValues["cloudInstances"] = cloudInstances
+		}
+
+		ngBytes, _ := cljson.Marshal(ngForValues)
+		finalNodeGroups = append(finalNodeGroups, json.RawMessage(ngBytes))
+	}
+
+	if !input.Values.Exists("nodeManager.internal") {
+		input.Values.Set("nodeManager.internal", map[string]interface{}{})
+	}
+
+	input.Values.Set("nodeManager.internal.nodeGroups", finalNodeGroups)
+	return nil
+}
+
+func nodeGroupForValues(nodeGroupSpec *ngv1.NodeGroupSpec) map[string]interface{} {
+	res := make(map[string]interface{})
+
+	res["nodeType"] = nodeGroupSpec.NodeType
+	if !nodeGroupSpec.GPU.IsEmpty() {
+		res["gpu"] = nodeGroupSpec.GPU
+	}
+	if !nodeGroupSpec.CloudInstances.IsEmpty() {
+		res["cloudInstances"] = cloudInstancesForValues{
+			MinPerZone: nodeGroupSpec.CloudInstances.MinPerZone,
+			MaxPerZone: nodeGroupSpec.CloudInstances.MaxPerZone,
+			Zones:      nodeGroupSpec.CloudInstances.Zones,
+		}
+	}
+	if !nodeGroupSpec.Fencing.IsEmpty() {
+		res["fencing"] = nodeGroupSpec.Fencing
+	}
+	return res
+}
+
+func calculateNodeGroupEngine(input *go_hook.HookInput, nodeGroup NodeGroupCrdInfo) ngv1.NodeGroupEngine {
+	if nodeGroup.Engine != "" {
+		return nodeGroup.Engine
+	}
+
+	defaultEngine := defaultCloudEphemeralNodeGroupEngineForNewNodeGroups(input, nodeGroup.UseMCM)
+
+	switch nodeGroup.Spec.NodeType {
+	case ngv1.NodeTypeCloudEphemeral:
+		return defaultEngine
+	case ngv1.NodeTypeStatic:
+		if nodeGroup.Spec.StaticInstances != nil {
+			return ngv1.NodeGroupEngineCAPI
+		}
+		return ngv1.NodeGroupEngineNone
+	default:
+		return ngv1.NodeGroupEngineNone
+	}
+}
+
+func defaultCloudEphemeralNodeGroupEngineForNewNodeGroups(input *go_hook.HookInput, useMCM bool) ngv1.NodeGroupEngine {
+	hasMCM := valueExistsAndNotEmpty(input, "nodeManager.internal.cloudProvider.machineClassKind")
+	hasCAPI := valueExistsAndNotEmpty(input, "nodeManager.internal.cloudProvider.capiClusterKind")
+
+	switch {
+	case hasMCM && hasCAPI:
+		if useMCM {
+			return ngv1.NodeGroupEngineMCM
+		}
+		return ngv1.NodeGroupEngineCAPI
+	case hasMCM:
+		return ngv1.NodeGroupEngineMCM
+	case hasCAPI:
+		return ngv1.NodeGroupEngineCAPI
+	default:
+		return ngv1.NodeGroupEngineNone
+	}
+}
